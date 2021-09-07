fe3889c786c90ec7564257fdf506fe902ea8930a
Nathan Sobo <nathan@zed.dev>
2021-09-07 17:06:42 -0600

    Add bindings to adjust buffer font size

--- zed/src/lib.rs
+++ zed/src/lib.rs
@@ -22,7 +22,7 @@ pub mod worktree;
 
 use crate::util::TryFutureExt;
 use channel::ChannelList;
-use gpui::{action, ModelHandle};
+use gpui::{action, keymap::Binding, ModelHandle};
 use parking_lot::Mutex;
 use postage::watch;
 use std::sync::Arc;
@@ -32,6 +32,9 @@ pub use settings::Settings;
 action!(About);
 action!(Quit);
 action!(Authenticate);
+action!(AdjustBufferFontSize, f32);
+
+const MIN_FONT_SIZE: f32 = 6.0;
 
 pub struct AppState {
     pub settings_tx: Arc<Mutex<watch::Sender<Settings>>>,
@@ -54,6 +57,22 @@ pub fn init(app_state: &Arc<AppState>, cx: &mut gpui::MutableAppContext) {
                 .detach();
         }
     });
+
+    cx.add_global_action({
+        let settings_tx = app_state.settings_tx.clone();
+
+        move |action: &AdjustBufferFontSize, cx| {
+            let mut settings_tx = settings_tx.lock();
+            let new_size = (settings_tx.borrow().buffer_font_size + action.0).max(MIN_FONT_SIZE);
+            settings_tx.borrow_mut().buffer_font_size = new_size;
+            cx.refresh_windows();
+        }
+    });
+
+    cx.add_bindings(vec![
+        Binding::new("cmd-=", AdjustBufferFontSize(1.), None),
+        Binding::new("cmd--", AdjustBufferFontSize(-1.), None),
+    ])
 }
 
 fn quit(_: &Quit, cx: &mut gpui::MutableAppContext) {
