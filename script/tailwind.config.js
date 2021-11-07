c2b44537aae5afd9e7973dc68234c9ab897622c8
Nathan Sobo <nathan@zed.dev>
2021-11-07 07:32:26 -0700

    Fix path to templates that's used when minifying tailwind CSS

--- script/tailwind.config.js
+++ script/tailwind.config.js
@@ -11,18 +11,18 @@ module.exports = {
     theme: {
         fontFamily: {
             display: [
-                "Spectral", "Constantia", "Lucida Bright", "Lucidabright", "Lucida Serif", "Lucida", "DejaVu Serif", "Bitstream Vera Serif", 
+                "Spectral", "Constantia", "Lucida Bright", "Lucidabright", "Lucida Serif", "Lucida", "DejaVu Serif", "Bitstream Vera Serif",
                 "Liberation Serif", "Georgia", "serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
                 "Noto Color Emoji"
             ],
             body: [
-                "JetBrains Mono", "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", 
-                "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", "Courier New", "Apple Color Emoji", "Segoe UI Emoji", 
+                "JetBrains Mono", "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono",
+                "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", "Courier New", "Apple Color Emoji", "Segoe UI Emoji",
                 "Segoe UI Symbol", "Noto Color Emoji"
             ],
             mono: [
-                "JetBrains Mono", "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", 
-                "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", "Courier New", "Apple Color Emoji", "Segoe UI Emoji", 
+                "JetBrains Mono", "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono",
+                "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", "Courier New", "Apple Color Emoji", "Segoe UI Emoji",
                 "Segoe UI Symbol", "Noto Color Emoji"
             ],
         },
@@ -32,15 +32,15 @@ module.exports = {
             black: colors.black,
             white: colors.white,
             gray: colors.trueGray,
-          },
+        },
         extend: {},
     },
 
     variants: {
     },
-    darkMode: false,    
+    darkMode: false,
     purge: [
-        "../server/templates/**/*.hbs",
-        "../server/templates/*.hbs"
+        "../crates/server/templates/**/*.hbs",
+        "../crates/server/templates/*.hbs"
     ]
 }
