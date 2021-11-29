1a91aa8194c8ad7cd027a34a3dcc832db4bb271a
Max Brunsfeld <maxbrunsfeld@gmail.com>
2021-11-29 15:22:45 -0800

    Introduce a collections crate w/ deterministic hashmap, hashset in tests
    Co-Authored-By: Nathan Sobo <nathan@zed.dev>

--- crates/buffer/src/lib.rs
+++ crates/buffer/src/lib.rs
@@ -12,6 +12,7 @@ mod tests;
 pub use anchor::*;
 use anyhow::{anyhow, Result};
 use clock::ReplicaId;
+use collections::{HashMap, HashSet};
 use operation_queue::OperationQueue;
 pub use point::*;
 pub use point_utf16::*;
@@ -30,31 +31,6 @@ use std::{
 };
 pub use sum_tree::Bias;
 use sum_tree::{FilterCursor, SumTree};
-
-#[cfg(any(test, feature = "test-support"))]
-#[derive(Clone, Default)]
-pub struct DeterministicState;
-
-#[cfg(any(test, feature = "test-support"))]
-impl std::hash::BuildHasher for DeterministicState {
-    type Hasher = seahash::SeaHasher;
-
-    fn build_hasher(&self) -> Self::Hasher {
-        seahash::SeaHasher::new()
-    }
-}
-
-#[cfg(any(test, feature = "test-support"))]
-type HashMap<K, V> = std::collections::HashMap<K, V, DeterministicState>;
-
-#[cfg(any(test, feature = "test-support"))]
-type HashSet<T> = std::collections::HashSet<T, DeterministicState>;
-
-#[cfg(not(any(test, feature = "test-support")))]
-type HashMap<K, V> = std::collections::HashMap<K, V>;
-
-#[cfg(not(any(test, feature = "test-support")))]
-type HashSet<T> = std::collections::HashSet<T>;
 
 #[derive(Clone)]
 pub struct Buffer {
