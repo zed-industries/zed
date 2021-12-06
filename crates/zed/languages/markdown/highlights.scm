b1ed9c88a490e7929c39e7c1125c83b7ddfbdc71
Max Brunsfeld <maxbrunsfeld@gmail.com>
2021-12-05 21:37:31 -0800

    Add tree-sitter-markdown, set up simple markdown higlighting

---
+++ crates/zed/languages/markdown/highlights.scm
@@ -0,0 +1,24 @@
+(emphasis) @emphasis
+(strong_emphasis) @emphasis.strong
+
+[
+  (atx_heading)
+  (setext_heading)
+] @title
+
+[
+  (list_marker_plus)
+  (list_marker_minus)
+  (list_marker_star)
+  (list_marker_dot)
+  (list_marker_parenthesis)
+] @list_marker
+
+[
+  (indented_code_block)
+  (fenced_code_block)
+  (code_span)
+] @text.literal
+
+(link_destination) @link_uri
+(link_text) @link_text
