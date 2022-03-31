ae8b610d85aac1d46782113ee1ad38a0650279af
Nate Butler <iamnbutler@gmail.com>
2022-03-31 19:10:44 -0400

    WIP: Scaffold tokens in dark.ts, no real values yet.

---
+++ styles/dark.ts
@@ -0,0 +1,438 @@
+import Theme from "./theme";
+
+export default function dark(): Theme {
+  return {
+    backgroundColor: {
+      100: {
+        base: {
+          value: "#000000",
+        },
+        hovered: {
+          value: "#000000",
+        },
+        active: {
+          value: "#000000",
+        },
+        focused: {
+          value: "#000000",
+        },
+      },
+      300: {
+        base: {
+          value: "#000000",
+        },
+        hovered: {
+          value: "#000000",
+        },
+        active: {
+          value: "#000000",
+        },
+        focused: {
+          value: "#000000",
+        },
+      },
+      500: {
+        base: {
+          value: "#000000",
+        },
+        hovered: {
+          value: "#000000",
+        },
+        active: {
+          value: "#000000",
+        },
+        focused: {
+          value: "#000000",
+        },
+      },
+      ok: {
+        base: {
+          value: "#000000",
+        },
+        hovered: {
+          value: "#000000",
+        },
+        active: {
+          value: "#000000",
+        },
+        focused: {
+          value: "#000000",
+        },
+      },
+      error: {
+        base: {
+          value: "#000000",
+        },
+        hovered: {
+          value: "#000000",
+        },
+        active: {
+          value: "#000000",
+        },
+        focused: {
+          value: "#000000",
+        },
+      },
+      warning: {
+        base: {
+          value: "#000000",
+        },
+        hovered: {
+          value: "#000000",
+        },
+        active: {
+          value: "#000000",
+        },
+        focused: {
+          value: "#000000",
+        },
+      },
+      info: {
+        base: {
+          value: "#000000",
+        },
+        hovered: {
+          value: "#000000",
+        },
+        active: {
+          value: "#000000",
+        },
+        focused: {
+          value: "#000000",
+        },
+      },
+    },
+    borderColor: {
+      primary: {
+        value: "#000000",
+      },
+      secondary: {
+        value: "#000000",
+      },
+      muted: {
+        value: "#000000",
+      },
+      focused: {
+        value: "#000000",
+      },
+      active: {
+        value: "#000000",
+      },
+      ok: {
+        value: "#000000",
+      },
+      error: {
+        value: "#000000",
+      },
+      warning: {
+        value: "#000000",
+      },
+      info: {
+        value: "#000000",
+      },
+    },
+    textColor: {
+      primary: {
+        value: "#000000",
+      },
+      secondary: {
+        value: "#000000",
+      },
+      muted: {
+        value: "#000000",
+      },
+      placeholder: {
+        value: "#000000",
+      },
+      active: {
+        value: "#000000",
+      },
+      feature: {
+        value: "#000000",
+      },
+      ok: {
+        value: "#000000",
+      },
+      error: {
+        value: "#000000",
+      },
+      warning: {
+        value: "#000000",
+      },
+      info: {
+        value: "#000000",
+      },
+    },
+    iconColor: {
+      primary: {
+        value: "#000000",
+      },
+      secondary: {
+        value: "#000000",
+      },
+      muted: {
+        value: "#000000",
+      },
+      placeholder: {
+        value: "#000000",
+      },
+      active: {
+        value: "#000000",
+      },
+      feature: {
+        value: "#000000",
+      },
+      ok: {
+        value: "#000000",
+      },
+      error: {
+        value: "#000000",
+      },
+      warning: {
+        value: "#000000",
+      },
+      info: {
+        value: "#000000",
+      },
+    },
+    editor: {
+      background: {
+        value: "#000000",
+      },
+      indent_guide: {
+        value: "#000000",
+      },
+      indent_guide_active: {
+        value: "#000000",
+      },
+      line: {
+        active: {
+          value: "#000000",
+        },
+        highlighted: {
+          value: "#000000",
+        },
+        inserted: {
+          value: "#000000",
+        },
+        deleted: {
+          value: "#000000",
+        },
+        modified: {
+          value: "#000000",
+        },
+      },
+      highlight: {
+        selection: {
+          value: "#000000",
+        },
+        occurrence: {
+          value: "#000000",
+        },
+        activeOccurrence: {
+          value: "#000000",
+        },
+        matchingBracket: {
+          value: "#000000",
+        },
+        match: {
+          value: "#000000",
+        },
+        activeMatch: {
+          value: "#000000",
+        },
+        related: {
+          value: "#000000",
+        },
+      },
+      gutter: {
+        primary: {
+          value: "#000000",
+        },
+        active: {
+          value: "#000000",
+        },
+      },
+    },
+
+    syntax: {
+      primary: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      comment: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      punctuation: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      constant: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      keyword: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      function: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      type: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      variant: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      property: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      enum: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      operator: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      string: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      number: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      boolean: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+      predictive: {
+        color: { value: "000000" },
+        weight: { value: "normal" },
+      },
+    },
+    player: {
+      1: {
+        baseColor: {
+          value: "#000000",
+        },
+        cursorColor: {
+          value: "#000000",
+        },
+        selectionColor: {
+          value: "#000000",
+        },
+        borderColor: {
+          value: "#000000",
+        },
+      },
+      2: {
+        baseColor: {
+          value: "#000000",
+        },
+        cursorColor: {
+          value: "#000000",
+        },
+        selectionColor: {
+          value: "#000000",
+        },
+        borderColor: {
+          value: "#000000",
+        },
+      },
+      3: {
+        baseColor: {
+          value: "#000000",
+        },
+        cursorColor: {
+          value: "#000000",
+        },
+        selectionColor: {
+          value: "#000000",
+        },
+        borderColor: {
+          value: "#000000",
+        },
+      },
+      4: {
+        baseColor: {
+          value: "#000000",
+        },
+        cursorColor: {
+          value: "#000000",
+        },
+        selectionColor: {
+          value: "#000000",
+        },
+        borderColor: {
+          value: "#000000",
+        },
+      },
+      5: {
+        baseColor: {
+          value: "#000000",
+        },
+        cursorColor: {
+          value: "#000000",
+        },
+        selectionColor: {
+          value: "#000000",
+        },
+        borderColor: {
+          value: "#000000",
+        },
+      },
+      6: {
+        baseColor: {
+          value: "#000000",
+        },
+        cursorColor: {
+          value: "#000000",
+        },
+        selectionColor: {
+          value: "#000000",
+        },
+        borderColor: {
+          value: "#000000",
+        },
+      },
+      7: {
+        baseColor: {
+          value: "#000000",
+        },
+        cursorColor: {
+          value: "#000000",
+        },
+        selectionColor: {
+          value: "#000000",
+        },
+        borderColor: {
+          value: "#000000",
+        },
+      },
+      8: {
+        baseColor: {
+          value: "#000000",
+        },
+        cursorColor: {
+          value: "#000000",
+        },
+        selectionColor: {
+          value: "#000000",
+        },
+        borderColor: {
+          value: "#000000",
+        },
+      },
+    },
+    shadowAlpha: {
+      value: 0.32,
+    },
+  };
+}
