037decd42ecc8b2ccf718ccdb2b084d76255bc2e
Nate Butler <iamnbutler@gmail.com>
2022-04-03 12:27:07 -0400

    Tidy up the figma tokens script

--- styles/buildFigmaTokens.ts
+++ styles/buildFigmaTokens.ts
@@ -1,26 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
-import { default as dark } from "./themes/dark";
+import dark from "./themes/dark";
 import light from "./themes/light";
 import Theme from "./themes/theme";
 import { colors, fontFamilies, fontSizes, fontWeights } from "./tokens";
 
-const corePath = path.resolve(`${__dirname}/figma/core.json`);
-const themePath = path.resolve(`${__dirname}/figma`);
-
-function coreTokens(): Object {
-  return {
-    color: {
-      ...colors,
-    },
-    text: {
-      family: fontFamilies,
-      weight: fontWeights,
-    },
-    size: fontSizes,
-  };
-}
-
+// Organize theme tokens
 function themeTokens(theme: Theme): Object {
   return {
     text: {
@@ -32,21 +17,29 @@ function themeTokens(theme: Theme): Object {
   };
 }
 
-let themes = [
-  { dark: themeTokens(dark) },
-  { light: themeTokens(light) },
-];
+let themes = [{ dark: themeTokens(dark) }, { light: themeTokens(light) }];
 
+// Create {theme}.json
+const themePath = path.resolve(`${__dirname}/figma`);
 themes.forEach((theme) => {
   const name = Object.getOwnPropertyNames(theme);
   const tokenJSON = JSON.stringify(theme, null, 2);
   fs.writeFileSync(`${themePath}/${name}.json`, tokenJSON);
 });
 
+// Organize core tokens
+const coreTokens = {
+  color: {
+    ...colors,
+  },
+  text: {
+    family: fontFamilies,
+    weight: fontWeights,
+  },
+  size: fontSizes,
+};
 
 // Create core.json
-const coreTokenJSON = JSON.stringify(coreTokens(), null, 2);
+const corePath = path.resolve(`${__dirname}/figma/core.json`);
+const coreTokenJSON = JSON.stringify(coreTokens, null, 2);
 fs.writeFileSync(corePath, coreTokenJSON);
-
-// const tokenJSON = JSON.stringify(tokens, null, 2);
-// fs.writeFileSync(outPath, tokenJSON);
