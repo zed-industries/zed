0af129d50a594fc61985a0a3418e304747479ea5
Max Brunsfeld <maxbrunsfeld@gmail.com>
2022-04-18 18:14:45 -0700

    Use primary text color for keystrokes in command palette

--- styles/src/styleTree/commandPalette.ts
+++ styles/src/styleTree/commandPalette.ts
@@ -5,7 +5,7 @@ export default function commandPalette(theme: Theme) {
   return {
     keystrokeSpacing: 8,
     key: {
-      text: text(theme, "mono", "secondary", { size: "xs" }),
+      text: text(theme, "mono", "primary", { size: "xs" }),
       cornerRadius: 3,
       background: backgroundColor(theme, "info", "base"),
       border: border(theme, "info"),
