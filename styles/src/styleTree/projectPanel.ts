f4dbe4d9761ed62ecb8f7f1c69a6ef2ccdeb02b1
Nate Butler <iamnbutler@gmail.com>
2022-04-29 01:35:58 -0400

    Update projectBrowser styles

--- styles/src/styleTree/projectPanel.ts
+++ styles/src/styleTree/projectPanel.ts
@@ -6,7 +6,7 @@ import { backgroundColor, iconColor, text, TextColor } from "./components";
 export default function projectPanel(theme: Theme) {
   function entry(theme: Theme, textColor: TextColor, background?: Color) {
     return {
-      height: 22,
+      height: 24,
       background,
       iconColor: iconColor(theme, "muted"),
       iconSize: 8,
@@ -17,21 +17,18 @@ export default function projectPanel(theme: Theme) {
 
   return {
     ...panel,
-    entry: entry(theme, "secondary"),
+    entry: entry(theme, "muted"),
     hoveredEntry: entry(
       theme,
-      "secondary",
+      "primary",
       backgroundColor(theme, 300, "hovered")
     ),
     selectedEntry: entry(theme, "primary"),
     hoveredSelectedEntry: entry(
       theme,
-      "primary",
+      "active",
       backgroundColor(theme, 300, "hovered")
     ),
-    padding: {
-      top: 6,
-      left: 12,
-    },
+    padding: { left: 12, right: 12, top: 6, bottom: 6 },
   };
 }
