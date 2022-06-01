4003037ca87f7aa4127ccbfa3888488203507c2e
Isaac Clayton <slightknack@gmail.com>
2022-06-01 16:39:59 +0200

    Documented code, got basic example working

--- crates/runner/src/lib.rs
+++ crates/runner/src/lib.rs
@@ -1,40 +1,10 @@
-use std::collections::{HashMap, HashSet};
+use mlua::{Function, Lua, LuaSerdeExt, Value};
+use serde::{de::DeserializeOwned, Serialize};
 
-use mlua::{Error, FromLua, Function, Lua, LuaSerdeExt, ToLua, UserData, Value};
+pub use map_macro::{map, set};
 
 pub mod runtime;
 pub use runtime::*;
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
-impl Runtime for Lua {
-    type Module = String;
-
-    fn init(module: Self::Module) -> Option<Self> {
-        let lua = Lua::new();
-        lua.load(&module).exec().ok()?;
-        return Some(lua);
-    }
-
-    fn handles(&self) -> Handles {
-        let mut globals = HashSet::new();
-        for pair in self.globals().pairs::<String, Value>() {
-            if let Ok((k, _)) = pair {
-                globals.insert(k);
-            }
-        }
-
-        globals
-    }
-
-    fn val<T: DeserializeOwned>(&self, name: String) -> Option<T> {
-        let val: Value = self.globals().get(name).ok()?;
-        Some(self.from_value(val).ok()?)
-    }
-
-    fn call<T: Serialize + DeserializeOwned>(&self, name: String, arg: T) -> Option<T> {
-        let fun: Function = self.globals().get(name).ok()?;
-        let arg: Value = self.to_value(&arg).ok()?;
-        let result = fun.call(arg).ok()?;
-        Some(self.from_value(result).ok()?)
-    }
-}
+pub mod lua;
+pub use lua::*;
--- crates/runner/src/runtime.rs
+++ crates/runner/src/runtime.rs
@@ -1,29 +1,77 @@
-use std::collections::HashSet;
+// use std::Error;
 
-use mlua::{FromLua, Lua, ToLua, Value};
 use serde::{de::DeserializeOwned, Serialize};
 
-pub type Handles = HashSet<String>;
+/// Represents a handle to a constant or function in the Runtime.
+/// Should be constructed by calling [`Runtime::handle_for`].
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub struct Handle(String);
 
+impl Handle {
+    pub fn inner(&self) -> &str {
+        &self.0
+    }
+}
+
+/// Represents an interface that can be implemented by a plugin.
 pub trait Interface
 where
     Self: Sized,
 {
-    fn from_runtime<T: Runtime>(runtime: &T) -> Option<Self>;
+    /// Create an interface from a given runtime.
+    /// All handles to be used by the interface should be registered and stored in `Self`.
+    fn from_runtime<T: Runtime>(runtime: &mut T) -> Option<Self>;
 }
 
 pub trait Runtime
 where
     Self: Sized,
 {
-    type Module;
+    /// Represents a plugin to be loaded by the runtime,
+    /// e.g. some source code + anything else needed to set up.
+    type Plugin;
 
-    fn init(plugin: Self::Module) -> Option<Self>;
-    fn handles(&self) -> Handles;
-    fn val<T: DeserializeOwned>(&self, name: String) -> Option<T>;
-    fn call<T: Serialize + DeserializeOwned>(&self, name: String, arg: T) -> Option<T>;
+    /// The error type for this module.
+    /// Ideally should implement the [`std::err::Error`] trait.
+    type Error;
 
-    fn as_interface<T: Interface>(&self) -> Option<T> {
+    /// Initializes a plugin, returning a [`Runtime`] that can be queried.
+    /// Note that if you have any configuration,
+    fn init(plugin: Self::Plugin) -> Result<Self, Self::Error>;
+
+    /// Returns a top-level constant from the module.
+    /// This can be used to extract configuration information from the module, for example.
+    /// Before calling this function, get a handle into the runtime using [`handle_for`].
+    fn constant<T: DeserializeOwned>(&mut self, handle: &Handle) -> Result<T, Self::Error>;
+
+    /// Call a function defined in the module.
+    fn call<T: Serialize + DeserializeOwned>(
+        &mut self,
+        handle: &Handle,
+        arg: T,
+    ) -> Result<T, Self::Error>;
+
+    /// Registers a handle with the runtime.
+    /// This is a mutable item if needed, but generally
+    /// this should be an immutable operation.
+    /// Returns whether the handle exists/was successfully registered.
+    fn register_handle<T: AsRef<str>>(&mut self, name: T) -> bool;
+
+    /// Returns the handle for a given name if the handle is defined.
+    /// Will only return an error if there was an error while trying to register the handle.
+    /// This function uses [`register_handle`], no need to implement this one.
+    fn handle_for<T: AsRef<str>>(&mut self, name: T) -> Option<Handle> {
+        if self.register_handle(&name) {
+            Some(Handle(name.as_ref().to_string()))
+        } else {
+            None
+        }
+    }
+
+    /// Creates the given interface from the current module.
+    /// Returns [`Error`] if the provided plugin does not match the expected interface.
+    /// Essentially wraps the [`Interface`] trait.
+    fn as_interface<T: Interface>(&mut self) -> Option<T> {
         Interface::from_runtime(self)
     }
 }
