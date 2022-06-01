b63d965b4628c50d9d3f0a5b6d6bb493f3cb71d0
Antonio Scandurra <me@as-cii.com>
2022-06-01 10:08:25 +0200

    Space out tooltip a little bit to ensure it doesn't overlap cursor

--- styles/src/styleTree/tooltip.ts
+++ styles/src/styleTree/tooltip.ts
@@ -6,6 +6,7 @@ export default function tooltip(theme: Theme) {
     background: backgroundColor(theme, 500),
     border: border(theme, "primary"),
     padding: 6,
+    margin: { top: 8, left: 8 },
     shadow: shadow(theme),
     cornerRadius: 6,
     ...text(theme, "sans", "primary", { size: "xs" })
