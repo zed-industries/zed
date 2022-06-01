13e0ad7253b179229496c06888fe9e8e77114983
Isaac Clayton <slightknack@gmail.com>
2022-06-01 13:42:00 +0200

    Get Runtime working...

--- crates/runner/src/runtime.rs
+++ crates/runner/src/runtime.rs
@@ -1,8 +1,16 @@
 use std::collections::HashSet;
 
 use mlua::{FromLua, Lua, ToLua, Value};
+use serde::{de::DeserializeOwned, Serialize};
 
-pub type Interface = HashSet<String>;
+pub type Handles = HashSet<String>;
+
+pub trait Interface
+where
+    Self: Sized,
+{
+    fn from_runtime<T: Runtime>(runtime: &T) -> Option<Self>;
+}
 
 pub trait Runtime
 where
@@ -11,7 +19,11 @@ where
     type Module;
 
     fn init(plugin: Self::Module) -> Option<Self>;
-    fn interface(&self) -> Interface;
-    // fn val<'a, T>(&'a self, name: String) -> Option<T>;
-    // fn call<'a, A: MyFromLua<'a>, R>(&'a mut self, name: String, arg: A) -> Option<R>;
+    fn handles(&self) -> Handles;
+    fn val<T: DeserializeOwned>(&self, name: String) -> Option<T>;
+    fn call<T: Serialize + DeserializeOwned>(&self, name: String, arg: T) -> Option<T>;
+
+    fn as_interface<T: Interface>(&self) -> Option<T> {
+        Interface::from_runtime(self)
+    }
 }
