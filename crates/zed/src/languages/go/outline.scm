924e9648e99b401caa5d89207e870ec98df75ad0
Max Brunsfeld <maxbrunsfeld@gmail.com>
2022-06-09 13:31:30 -0700

    Tweak golang outline query

--- crates/zed/src/languages/go/outline.scm
+++ crates/zed/src/languages/go/outline.scm
@@ -5,22 +5,40 @@
 
 (function_declaration
     "func" @context
-    name: (identifier) @name) @item
+    name: (identifier) @name
+    parameters: (parameter_list
+      "(" @context
+      ")" @context)) @item
 
 (method_declaration
     "func" @context
     receiver: (parameter_list
+        "(" @context
         (parameter_declaration
-            type: (_) @context))
-    name: (field_identifier) @name) @item
+            type: (_) @context)
+        ")" @context)
+    name: (field_identifier) @name
+    parameters: (parameter_list
+      "(" @context
+      ")" @context)) @item
 
 (const_declaration
     "const" @context
     (const_spec
-        name: (identifier) @name)) @item
+        name: (identifier) @name) @item)
 
 (source_file
     (var_declaration
         "var" @context
         (var_spec
-            name: (identifier) @name)) @item)
+            name: (identifier) @name) @item))
+
+(method_spec
+    name: (_) @name
+    parameters: (parameter_list
+      "(" @context
+      ")" @context)) @item
+
+(field_declaration
+    name: (_) @name
+    type: (_) @context) @item
