5bc0acd88cf90f1d14035cac53ce76520da1549e
Mikayla Maki <mikayla.c.maki@icloud.com>
2022-06-29 18:34:02 -0700

    Directly qualified function makes cargo happy

--- crates/terminal/src/terminal_element.rs
+++ crates/terminal/src/terminal_element.rs
@@ -417,7 +417,7 @@ mod tests {
     fn test_rgb_for_index() {
         //Test every possible value in the color cube
         for i in 16..=231 {
-            let (r, g, b) = rgb_for_index(&(i as u8));
+            let (r, g, b) = crate::terminal_element::rgb_for_index(&(i as u8));
             assert_eq!(i, 16 + 36 * r + 6 * g + b);
         }
     }
