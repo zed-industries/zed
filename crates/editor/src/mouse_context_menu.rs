d796b543e0f2a8255e46b7862396f790f51317a2
Keith Simmons <keith@zed.dev>
2022-07-11 14:14:33 -0700

    WIP add basic context menu and make progress toward adding quick actions to it

---
+++ crates/editor/src/mouse_context_menu.rs
@@ -0,0 +1,61 @@
+use context_menu::{ContextMenu, ContextMenuItem};
+use gpui::{
+    geometry::vector::Vector2F, impl_internal_actions, MutableAppContext, Task, ViewContext,
+    ViewHandle,
+};
+
+use crate::{
+    DisplayPoint, Editor, EditorMode, FindAllReferences, GoToDefinition, Rename, SelectMode,
+};
+
+#[derive(Clone, PartialEq)]
+pub struct DeployMouseContextMenu {
+    pub position: Vector2F,
+    pub point: DisplayPoint,
+}
+
+impl_internal_actions!(editor, [DeployMouseContextMenu]);
+
+pub fn init(cx: &mut MutableAppContext) {
+    cx.add_action(deploy_context_menu);
+}
+
+pub struct MouseContextMenuState {
+    pub context_menu: ViewHandle<ContextMenu>,
+    pub task: Option<Task<()>>,
+}
+
+pub fn deploy_context_menu(
+    editor: &mut Editor,
+    &DeployMouseContextMenu { position, point }: &DeployMouseContextMenu,
+    cx: &mut ViewContext<Editor>,
+) {
+    // Don't show context menu for inline editors
+    if editor.mode() != EditorMode::Full {
+        return;
+    }
+
+    // Don't show the context menu if there isn't a project associated with this editor
+    if editor.project.is_none() {
+        return;
+    }
+
+    // Move the cursor to the clicked location so that dispatched actions make sense
+    editor.change_selections(None, cx, |s| {
+        s.clear_disjoint();
+        s.set_pending_display_range(point..point, SelectMode::Character);
+    });
+
+    editor.mouse_context_menu.update(cx, |menu, cx| {
+        menu.show(
+            position,
+            vec![
+                ContextMenuItem::item("Rename Symbol", Rename),
+                ContextMenuItem::item("Go To Definition", GoToDefinition),
+                ContextMenuItem::item("Find All References", FindAllReferences),
+            ],
+            cx,
+        );
+    });
+    cx.notify();
+}
