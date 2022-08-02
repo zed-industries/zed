42db566ff6add0c303bb18f816346d4aea675f63
Antonio Scandurra <me@as-cii.com>
2022-08-02 19:09:34 +0200

    Remove terminal integration test
    Creating a full-fledged terminal is flaky and causes tests to either
    hang or outright panic. The only test that requires creating a terminal
    was `test_terminal` but we think the value added by that test is not
    worth the flakiness, so we're removing it.
    
    Co-Authored-By: Mikayla Maki <mikayla@zed.dev>

--- crates/terminal/src/tests/terminal_test_context.rs
+++ crates/terminal/src/tests/terminal_test_context.rs
@@ -1,72 +1,16 @@
-use std::{path::Path, time::Duration};
-
-use gpui::{
-    geometry::vector::vec2f, AppContext, ModelHandle, ReadModelWith, TestAppContext, ViewHandle,
-};
-use itertools::Itertools;
+use gpui::{ModelHandle, TestAppContext, ViewHandle};
 use project::{Entry, Project, ProjectPath, Worktree};
+use std::{path::Path, time::Duration};
 use workspace::{AppState, Workspace};
-
-use crate::{
-    connected_el::TermDimensions,
-    model::{Terminal, TerminalBuilder},
-    DEBUG_CELL_WIDTH, DEBUG_LINE_HEIGHT, DEBUG_TERMINAL_HEIGHT, DEBUG_TERMINAL_WIDTH,
-};
 
 pub struct TerminalTestContext<'a> {
     pub cx: &'a mut TestAppContext,
-    pub connection: Option<ModelHandle<Terminal>>,
 }
 
 impl<'a> TerminalTestContext<'a> {
-    pub fn new(cx: &'a mut TestAppContext, term: bool) -> Self {
+    pub fn new(cx: &'a mut TestAppContext) -> Self {
         cx.set_condition_duration(Some(Duration::from_secs(5)));
-
-        let size_info = TermDimensions::new(
-            DEBUG_CELL_WIDTH,
-            DEBUG_LINE_HEIGHT,
-            vec2f(DEBUG_TERMINAL_WIDTH, DEBUG_TERMINAL_HEIGHT),
-        );
-
-        let connection = term.then(|| {
-            cx.add_model(|cx| {
-                TerminalBuilder::new(None, None, None, size_info)
-                    .unwrap()
-                    .subscribe(cx)
-            })
-        });
-
-        TerminalTestContext { cx, connection }
-    }
-
-    pub async fn execute_and_wait<F>(&mut self, command: &str, f: F) -> String
-    where
-        F: Fn(String, &AppContext) -> bool,
-    {
-        let connection = self.connection.take().unwrap();
-
-        let command = command.to_string();
-        connection.update(self.cx, |connection, _| {
-            connection.write_to_pty(command);
-            connection.write_to_pty("\r".to_string());
-        });
-
-        connection
-            .condition(self.cx, |conn, cx| {
-                let content = Self::grid_as_str(conn);
-                f(content, cx)
-            })
-            .await;
-
-        let res = self
-            .cx
-            .read_model_with(&connection, &mut |conn, _: &AppContext| {
-                Self::grid_as_str(conn)
-            });
-
-        self.connection = Some(connection);
-
-        res
+        TerminalTestContext { cx }
     }
 
     ///Creates a worktree with 1 file: /root.txt
@@ -138,17 +82,6 @@ impl<'a> TerminalTestContext<'a> {
             };
             project.update(cx, |project, cx| project.set_active_path(Some(p), cx));
         });
-    }
-
-    fn grid_as_str(connection: &Terminal) -> String {
-        connection.render_lock(None, |content, _| {
-            let lines = content.display_iter.group_by(|i| i.point.line.0);
-            lines
-                .into_iter()
-                .map(|(_, line)| line.map(|i| i.c).collect::<String>())
-                .collect::<Vec<String>>()
-                .join("\n")
-        })
     }
 }
 
