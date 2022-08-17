443432606eac8f4a975cbf7cc403c4701bb88e0d
Nathan Sobo <nathan@zed.dev>
2022-08-17 09:33:48 -0600

    Fix focus regression in contact finder
    Co-Authored-By: Antonio Scandurra <me@as-cii.com>

--- crates/contacts_panel/src/contact_finder.rs
+++ crates/contacts_panel/src/contact_finder.rs
@@ -43,7 +43,9 @@ impl View for ContactFinder {
     }
 
     fn on_focus_in(&mut self, _: AnyViewHandle, cx: &mut ViewContext<Self>) {
-        cx.focus(&self.picker);
+        if cx.is_self_focused() {
+            cx.focus(&self.picker);
+        }
     }
 }
 
