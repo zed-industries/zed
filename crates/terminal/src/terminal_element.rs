a8b800398063938b75927850987ebf0546767aca
Mikayla Maki <mikayla@zed.dev>
2022-08-31 18:27:25 -0700

    ??

--- crates/terminal/src/terminal_element.rs
+++ crates/terminal/src/terminal_element.rs
@@ -618,11 +618,11 @@ impl Element for TerminalElement {
 
         // searches, highlights to a single range representations
         let mut relative_highlighted_ranges = Vec::new();
-        for search_match in search_matches {
-            relative_highlighted_ranges.push((search_match, match_color))
-        }
         if let Some(selection) = selection {
             relative_highlighted_ranges.push((selection.start..=selection.end, selection_color));
+        }
+        for search_match in search_matches {
+            relative_highlighted_ranges.push((search_match, match_color))
         }
 
         // then have that representation be converted to the appropriate highlight data structure
