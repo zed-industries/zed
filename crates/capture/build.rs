5fec784580e6d43689cbfc7046f96149c71e499b
Antonio Scandurra <me@as-cii.com>
2022-09-02 11:42:39 +0200

    Link Swift stdlib

--- crates/capture/build.rs
+++ crates/capture/build.rs
@@ -1,6 +1,62 @@
+use serde::Deserialize;
 use std::{env, path::PathBuf, process::Command};
 
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SwiftTargetInfo {
+    pub triple: String,
+    pub unversioned_triple: String,
+    pub module_triple: String,
+    pub swift_runtime_compatibility_version: String,
+    #[serde(rename = "librariesRequireRPath")]
+    pub libraries_require_rpath: bool,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SwiftPaths {
+    pub runtime_library_paths: Vec<String>,
+    pub runtime_library_import_paths: Vec<String>,
+    pub runtime_resource_path: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SwiftTarget {
+    pub target: SwiftTargetInfo,
+    pub paths: SwiftPaths,
+}
+
+const MACOS_TARGET_VERSION: &str = "12";
+
+pub fn link_swift_libs() {
+    let mut arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    if arch == "aarch64" {
+        arch = "arm64".into();
+    }
+    let target = format!("{}-apple-macosx{}", arch, MACOS_TARGET_VERSION);
+
+    let swift_target_info_str = Command::new("swift")
+        .args(&["-target", &target, "-print-target-info"])
+        .output()
+        .unwrap()
+        .stdout;
+
+    let swift_target_info: SwiftTarget = serde_json::from_slice(&swift_target_info_str).unwrap();
+    if swift_target_info.target.libraries_require_rpath {
+        panic!("Libraries require RPath! Change minimum MacOS value to fix.")
+    }
+
+    swift_target_info
+        .paths
+        .runtime_library_paths
+        .iter()
+        .for_each(|path| {
+            println!("cargo:rustc-link-search=native={}", path);
+        });
+}
+
 fn main() {
+    link_swift_libs();
     println!("cargo:rerun-if-changed=/Users/as-cii/Library/Developer/Xcode/DerivedData/LiveKitObjC-ftgpxknhsgkrocbhhgjkyyvkgkbj/Build/Products/Debug/libLiveKitObjC.a");
     println!("cargo:rustc-link-search=/Users/as-cii/Library/Developer/Xcode/DerivedData/LiveKitObjC-ftgpxknhsgkrocbhhgjkyyvkgkbj/Build/Products/Debug");
     println!("cargo:rustc-link-lib=static=LiveKitObjC");
