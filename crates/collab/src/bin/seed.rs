0f4598a2435f34f15ed739a7dd75419eff05d4c5
Antonio Scandurra <me@as-cii.com>
2022-11-17 15:34:35 +0100

    Fix seed script

--- crates/collab/src/bin/seed.rs
+++ crates/collab/src/bin/seed.rs
@@ -1,9 +1,7 @@
 use collab::{Error, Result};
-use db::{Db, PostgresDb, UserId};
-use rand::prelude::*;
+use db::{DefaultDb, UserId};
 use serde::{de::DeserializeOwned, Deserialize};
 use std::fmt::Write;
-use time::{Duration, OffsetDateTime};
 
 #[allow(unused)]
 #[path = "../db.rs"]
@@ -18,9 +16,8 @@ struct GitHubUser {
 
 #[tokio::main]
 async fn main() {
-    let mut rng = StdRng::from_entropy();
     let database_url = std::env::var("DATABASE_URL").expect("missing DATABASE_URL env var");
-    let db = PostgresDb::new(&database_url, 5)
+    let db = DefaultDb::new(&database_url, 5)
         .await
         .expect("failed to connect to postgres database");
     let github_token = std::env::var("GITHUB_TOKEN").expect("missing GITHUB_TOKEN env var");
@@ -103,57 +100,6 @@ async fn main() {
                 .user_id,
             );
         }
-    }
-
-    let zed_org_id = if let Some(org) = db
-        .find_org_by_slug("zed")
-        .await
-        .expect("failed to fetch org")
-    {
-        org.id
-    } else {
-        db.create_org("Zed", "zed")
-            .await
-            .expect("failed to insert org")
-    };
-
-    let general_channel_id = if let Some(channel) = db
-        .get_org_channels(zed_org_id)
-        .await
-        .expect("failed to fetch channels")
-        .iter()
-        .find(|c| c.name == "General")
-    {
-        channel.id
-    } else {
-        let channel_id = db
-            .create_org_channel(zed_org_id, "General")
-            .await
-            .expect("failed to insert channel");
-
-        let now = OffsetDateTime::now_utc();
-        let max_seconds = Duration::days(100).as_seconds_f64();
-        let mut timestamps = (0..1000)
-            .map(|_| now - Duration::seconds_f64(rng.gen_range(0_f64..=max_seconds)))
-            .collect::<Vec<_>>();
-        timestamps.sort();
-        for timestamp in timestamps {
-            let sender_id = *zed_user_ids.choose(&mut rng).unwrap();
-            let body = lipsum::lipsum_words(rng.gen_range(1..=50));
-            db.create_channel_message(channel_id, sender_id, &body, timestamp, rng.gen())
-                .await
-                .expect("failed to insert message");
-        }
-        channel_id
-    };
-
-    for user_id in zed_user_ids {
-        db.add_org_member(zed_org_id, user_id, true)
-            .await
-            .expect("failed to insert org membership");
-        db.add_channel_member(general_channel_id, user_id, true)
-            .await
-            .expect("failed to insert channel membership");
     }
 }
 
