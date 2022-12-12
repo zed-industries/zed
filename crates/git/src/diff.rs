7c3dc1e3dca12bd7c1729524f76754bf9d2e75cc
Julia <floc@unpromptedtirade.com>
2022-12-12 11:57:14 -0500

    Cleanup

--- crates/git/src/diff.rs
+++ crates/git/src/diff.rs
@@ -77,9 +77,6 @@ impl BufferDiff {
         buffer: &'a BufferSnapshot,
         reversed: bool,
     ) -> impl 'a + Iterator<Item = DiffHunk<u32>> {
-        // let start = buffer.anchor_before(Point::new(query_row_range.start, 0));
-        // let end = buffer.anchor_after(Point::new(query_row_range.end, 0));
-
         let mut cursor = self.tree.filter::<_, DiffHunkSummary>(move |summary| {
             let before_start = summary.buffer_range.end.cmp(&range.start, buffer).is_lt();
             let after_end = summary.buffer_range.start.cmp(&range.end, buffer).is_gt();
