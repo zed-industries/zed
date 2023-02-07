fe25994fb31823b33322de5c9a5147faf65ce93a
Kay Simmons <kay@zed.dev>
2023-02-07 14:20:23 -0800

    fix highlights, indents, and tab size for yaml

--- crates/zed/src/languages/yaml/highlights.scm
+++ crates/zed/src/languages/yaml/highlights.scm
@@ -1,17 +1,29 @@
 (boolean_scalar) @boolean
 (null_scalar) @constant.builtin
-(double_quote_scalar) @string
-(single_quote_scalar) @string
-((block_scalar) @string (#set! "priority" 99))
-(string_scalar) @string
+
+[
+  (double_quote_scalar)
+  (single_quote_scalar)
+  (block_scalar)
+  (string_scalar)
+] @string
+
 (escape_sequence) @string.escape
-(integer_scalar) @number
-(float_scalar) @number
+
+[
+  (integer_scalar)
+  (float_scalar)
+] @number
+
 (comment) @comment
-(anchor_name) @type
-(alias_name) @type
-(tag) @type
-(ERROR) @error
+
+[
+  (anchor_name)
+  (alias_name)
+  (tag) 
+] @type
+
+key: (flow_node (plain_scalar (string_scalar) @property)) 
 
 [
  ","
