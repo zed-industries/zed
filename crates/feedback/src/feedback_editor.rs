bbe82972975c24a90a3f94fc425db3ae543fd935
Joseph Lyons <JosephTLyons@gmail.com>
2023-02-08 11:23:44 -0500

    Add feedback information text

--- crates/feedback/src/feedback_editor.rs
+++ crates/feedback/src/feedback_editor.rs
@@ -509,3 +509,55 @@ impl ToolbarItemView for SubmitFeedbackButton {
         }
     }
 }
+
+pub struct FeedbackInfoText {
+    active_item: Option<ViewHandle<FeedbackEditor>>,
+}
+
+impl FeedbackInfoText {
+    pub fn new() -> Self {
+        Self {
+            active_item: Default::default(),
+        }
+    }
+}
+
+impl Entity for FeedbackInfoText {
+    type Event = ();
+}
+
+impl View for FeedbackInfoText {
+    fn ui_name() -> &'static str {
+        "FeedbackInfoText"
+    }
+
+    fn render(&mut self, cx: &mut RenderContext<Self>) -> ElementBox {
+        let theme = cx.global::<Settings>().theme.clone();
+        Label::new(
+            "We read whatever you submit here. For issues and discussions, visit the community repo on GitHub.".to_string(),
+            theme.workspace.titlebar.outdated_warning.text.clone(),
+        )
+        .contained()
+        .with_style(theme.workspace.titlebar.outdated_warning.container)
+        .aligned()
+        .boxed()
+    }
+}
+
+impl ToolbarItemView for FeedbackInfoText {
+    fn set_active_pane_item(
+        &mut self,
+        active_pane_item: Option<&dyn ItemHandle>,
+        cx: &mut ViewContext<Self>,
+    ) -> workspace::ToolbarItemLocation {
+        cx.notify();
+        if let Some(feedback_editor) = active_pane_item.and_then(|i| i.downcast::<FeedbackEditor>())
+        {
+            self.active_item = Some(feedback_editor);
+            ToolbarItemLocation::PrimaryLeft { flex: None }
+        } else {
+            self.active_item = None;
+            ToolbarItemLocation::Hidden
+        }
+    }
+}
