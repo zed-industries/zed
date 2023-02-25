2d8adf4c56327fb42594d4b2bfd323b06d663459
Nate Butler <iamnbutler@gmail.com>
2023-02-25 11:27:18 -0500

    Remove theme tool for now
    This is likely the biggest thing I'm unsure about for this PR, so I'm going to pull it into a seperate branch so I can merge the system color library in.
    
    I think a better approach for this will be one of two things:
    - Have a single next app that covers everything to do with themes (previewing ramps, generated themes, components etc.)
    - Create a mono or turborepo that splits things up into packages (system, theme, themes, theme-tool, etc)

--- styles/theme-tool/app/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Zed Color Tool</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-    </>
-  );
-}
