88599add563e78b25c89aa3807c3e08586fa4cbf
Mikayla Maki <mikayla.c.maki@gmail.com>
2023-03-13 12:54:15 -0700

    Remove UI for invite link in prep for beta launch

--- styles/src/styleTree/contactsPopover.ts
+++ styles/src/styleTree/contactsPopover.ts
@@ -7,23 +7,11 @@ export default function contactsPopover(colorScheme: ColorScheme) {
     return {
         background: background(layer),
         cornerRadius: 6,
-        padding: { top: 6 },
+        padding: { top: 6, bottom: 6 },
         margin: { top: -6 },
         shadow: colorScheme.popoverShadow,
         border: border(layer),
         width: 300,
         height: 400,
-        inviteRowHeight: 28,
-        inviteRow: {
-            padding: {
-                left: sidePadding,
-                right: sidePadding,
-            },
-            border: border(layer, { top: true }),
-            text: text(layer, "sans", "variant", { size: "sm" }),
-            hover: {
-                text: text(layer, "sans", "hovered", { size: "sm" }),
-            },
-        },
     }
 }
