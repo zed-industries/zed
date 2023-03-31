0f4f3bfbdeab0d5a37bbdb3b9767e0009bfb5c3e
Julia <floc@unpromptedtirade.com>
2023-03-31 15:31:50 -0400

    Revert "Merge pull request #2335 from zed-industries/dont-delete-json-server-after-downloading-it"
    This reverts commit 516e77906e100b64021a3941193977ccf8f0ec86, reversing
    changes made to 54582fd77fec3eb0620ee6b687efa7e046bb33c4.

--- crates/zed/src/languages/json.rs
+++ crates/zed/src/languages/json.rs
@@ -16,7 +16,7 @@ use std::{
     sync::Arc,
 };
 use theme::ThemeRegistry;
-use util::http::HttpClient;
+use util::{fs::remove_matching, http::HttpClient};
 use util::{paths, ResultExt, StaffMode};
 
 const SERVER_PATH: &'static str =
@@ -83,6 +83,8 @@ impl LspAdapter for JsonLspAdapter {
                     &version_dir,
                 )
                 .await?;
+
+            remove_matching(&container_dir, |entry| entry != server_path).await;
         }
 
         Ok(LanguageServerBinary {
