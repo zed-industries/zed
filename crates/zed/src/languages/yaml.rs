588b2da40ae9db3bb28a8d5a0782e2e1828dfc79
Antonio Scandurra <me@as-cii.com>
2023-04-03 20:02:20 +0200

    Disable key ordering diagnostic for YAML language server

--- crates/zed/src/languages/yaml.rs
+++ crates/zed/src/languages/yaml.rs
@@ -106,6 +106,9 @@ impl LspAdapter for YamlLspAdapter {
         let settings = cx.global::<Settings>();
         Some(
             future::ready(serde_json::json!({
+                "yaml": {
+                    "keyOrdering": false
+                },
                 "[yaml]": {
                     "editor.tabSize": settings.tab_size(Some("YAML"))
                 }
