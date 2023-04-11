20ec9f6daf34e57201c1d671f43f4521570e0ec0
Nate Butler <iamnbutler@gmail.com>
2023-04-11 12:37:20 -0400

    Add meta fields to `ayu`

--- styles/src/themes/ayu-light.ts
+++ styles/src/themes/ayu-light.ts
@@ -1,19 +1,16 @@
 import { createColorScheme } from "./common/ramps"
-import { ayu, buildTheme } from "./common/ayu-common"
+import { ayu, meta as themeMeta, buildTheme } from "./common/ayu-common"
 
-const name = "Ayu"
-const author = "Konstantin Pschera <me@kons.ch>"
-const url = "https://github.com/ayu-theme/ayu-colors"
-const license = {
-    type: "MIT",
-    url: "https://github.com/ayu-theme/ayu-colors/blob/master/license",
+export const meta = {
+    ...themeMeta,
+    name: `${themeMeta.name} Light`
 }
 
 const variant = ayu.light
 const theme = buildTheme(variant, true)
 
 export const light = createColorScheme(
-    `${name} Light`,
+    meta.name,
     true,
     theme.ramps,
     theme.syntax
