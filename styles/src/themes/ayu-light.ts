527f0310e2ba610442f24d4fe03e4bef2711e989
Nate Butler <iamnbutler@gmail.com>
2023-04-11 12:22:49 -0400

    Update + publish the Ayu theme

---
+++ styles/src/themes/ayu-light.ts
@@ -0,0 +1,15 @@
+import { createColorScheme } from "../common/ramps"
+import { ayu, buildTheme } from "../common/ayu-common"
+
+const name = "Ayu"
+const author = "Konstantin Pschera <me@kons.ch>"
+const url = "https://github.com/ayu-theme/ayu-colors"
+const license = {
+    type: "MIT",
+    url: "https://github.com/ayu-theme/ayu-colors/blob/master/license",
+}
+
+const variant = ayu.light
+const theme = buildTheme(variant, true)
+
+export const light = createColorScheme(`${name} Light`, true, theme.ramps, theme.syntax)
---
+++ styles/src/themes/ayu-mirage.ts
@@ -0,0 +1,15 @@
+import { createColorScheme } from "../common/ramps"
+import { ayu, buildTheme } from "../common/ayu-common"
+
+const name = "Ayu"
+const author = "Konstantin Pschera <me@kons.ch>"
+const url = "https://github.com/ayu-theme/ayu-colors"
+const license = {
+    type: "MIT",
+    url: "https://github.com/ayu-theme/ayu-colors/blob/master/license",
+}
+
+const variant = ayu.mirage
+const theme = buildTheme(variant, false)
+
+export const dark = createColorScheme(`${name} Mirage`, false, theme.ramps, theme.syntax)
