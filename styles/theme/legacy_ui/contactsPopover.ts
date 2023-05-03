fe01deacf73121b82cf66cd1111fbac85ead9585
Nate Butler <iamnbutler@gmail.com>
2023-05-02 22:24:17 -0400

    Improve contactsPopover

--- styles/theme/legacy_ui/contactsPopover.ts
+++ styles/theme/legacy_ui/contactsPopover.ts
@@ -1,20 +1,16 @@
-import { useSurfaceStyle } from "@components/surface"
 import { Theme } from "@theme*"
-import { ContainerStyle, containerStyle } from "@theme/container"
+import { popoverContainerStyle } from "@theme/container/popover"
 import { padding } from "@theme/padding"
-import { shadow } from "@theme/shadow"
 
 export default function contactsPopover(theme: Theme) {
-    const surfaceStyle = useSurfaceStyle(theme, 'popover')
-
-    const container: ContainerStyle = containerStyle({
-        background: surfaceStyle.background,
-        border: surfaceStyle.border,
-        borderRadius: 6,
-        width: 300,
-        height: 400,
-        padding: padding(0, 6),
-        shadow: shadow(theme, "popover"),
+    const container = popoverContainerStyle({
+        theme,
+        options: {
+            borderRadius: 6,
+            width: 300,
+            height: 400,
+            padding: padding(0, 6),
+        }
     })
 
     const legacy_properties = {
