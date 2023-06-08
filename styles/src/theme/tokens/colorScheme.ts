d4a4151646fafc71be1b01342ab0047c35c898bd
Nate Butler <iamnbutler@gmail.com>
2023-06-08 11:15:59 -0400

    Build layer tokens for each theme

--- styles/src/theme/tokens/colorScheme.ts
+++ styles/src/theme/tokens/colorScheme.ts
@@ -1,12 +1,19 @@
 import { ColorScheme } from "../colorScheme"
+import { LayerToken, layerToken } from "./layer"
 import { PlayerTokens, players } from "./players"
 
 interface ColorSchemeTokens {
+    lowest: LayerToken
+    middle: LayerToken
+    highest: LayerToken
     players: PlayerTokens
 }
 
 export function colorSchemeTokens(colorScheme: ColorScheme): ColorSchemeTokens {
     return {
+        lowest: layerToken(colorScheme.lowest, "lowest"),
+        middle: layerToken(colorScheme.middle, "middle"),
+        highest: layerToken(colorScheme.highest, "highest"),
         players: players(colorScheme),
     }
 }
