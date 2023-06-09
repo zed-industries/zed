57ff173e297be5331a0408f5144b17d8c3cad0b2
Kevin Hovsäter <kevin@hovsater.com>
2023-06-09 07:31:09 +0200

    Add outline support for Ruby singleton methods

--- crates/zed/src/languages/ruby/outline.scm
+++ crates/zed/src/languages/ruby/outline.scm
@@ -6,6 +6,12 @@
     "def" @context
     name: (_) @name) @item
 
+(singleton_method
+    "def" @context
+    object: (_) @context
+    "." @context
+    name: (_) @name) @item
+
 (module
     "module" @context
     name: (_) @name) @item
