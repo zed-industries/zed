a9fec7f15b28e4bf6d8ffe3aba9739ff4db06b55
Sergey Onufrienko <sergey@onufrienko.com>
2023-06-21 16:13:04 +0200

    fix: create schemas folder

--- crates/theme/src/theme.rs
+++ crates/theme/src/theme.rs
@@ -1092,6 +1092,7 @@ mod tests {
     fn export_schema() {
         let theme = schema_for!(Theme);
         let output = serde_json::to_string_pretty(&theme).unwrap();
+        std::fs::create_dir("schemas").ok();
         std::fs::write("schemas/theme.json", output).ok();
     }
 }
