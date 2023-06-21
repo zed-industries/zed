d3a333b8733a6283b686dcd20bd4fb02a73a13f1
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2023-06-21 19:06:34 +0200

    Tidy up xtask

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -1,11 +1,29 @@
 mod cli;
 
-use schemars::schema_for;
+use std::path::PathBuf;
 
+use anyhow::Result;
+use clap::Parser;
+use schemars::schema_for;
 use theme::Theme;
-fn main() {
+
+fn build_themes(mut out_dir: PathBuf, file_name: PathBuf) -> Result<()> {
     let theme = schema_for!(Theme);
-    let output = serde_json::to_string_pretty(&theme).unwrap();
-    std::fs::create_dir("schemas").ok();
-    std::fs::write("schemas/theme.json", output).ok();
+    let output = serde_json::to_string_pretty(&theme)?;
+
+    std::fs::create_dir(&out_dir)?;
+
+    let mut file_path = out_dir;
+    out_dir.push(file_name);
+
+    std::fs::write(file_path, output)?;
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = cli::Cli::parse();
+    match args.command {
+        cli::Commands::BuildThemeTypes { out_dir, file_name } => build_themes(out_dir, file_name),
+    }
 }
