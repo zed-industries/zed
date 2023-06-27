02a426b6578423806128d0e206294190679ea7bf
Max Brunsfeld <maxbrunsfeld@gmail.com>
2023-06-27 15:17:29 -0700

    Fix bugs in handling combined injections exposed by HEEx (#2652)
    Fixes
    https://linear.app/zed-industries/issue/Z-2481/heex-this-snippet-triggers-a-hard-crash
    
    Release Notes:
    
    - Fixed a crash that would sometimes occur when editing a HEEx file
    ([#1703](https://github.com/zed-industries/community/issues/1703)).

--- crates/zed/src/languages/heex/injections.scm
+++ crates/zed/src/languages/heex/injections.scm
@@ -9,7 +9,5 @@
   (#set! combined)
 )
 
-; expressions live within HTML tags, and do not need to be combined
-;     <link href={ Routes.static_path(..) } />
 ((expression (expression_value) @content)
  (#set! language "elixir"))
