4a4dd398750add1b93c7db41f7e4739405043e22
Max Brunsfeld <maxbrunsfeld@gmail.com>
2023-07-11 15:02:19 -0700

    Fix TSX embedding query

--- crates/zed/src/languages/tsx/embedding.scm
+++ crates/zed/src/languages/tsx/embedding.scm
@@ -1,13 +1,5 @@
-; (internal_module
-;     "namespace" @context
-    ; name: (_) @name) @item
-
 (enum_declaration
     "enum" @context
-    name: (_) @name) @item
-
-; (type_alias_declaration
-;     "type" @context
     name: (_) @name) @item
 
 (function_declaration
@@ -18,12 +10,6 @@
 (interface_declaration
     "interface" @context
     name: (_) @name) @item
-
-; (export_statement
-;     (lexical_declaration
-;         ["let" "const"] @context
-;         (variable_declarator
-;             name: (_) @name) @item))
 
 (program
     (lexical_declaration
@@ -47,13 +33,3 @@
         (accessibility_modifier)
         ]* @context
     name: (_) @name) @item
-
-; (public_field_definition
-;     [
-;         "declare"
-;         "readonly"
-;         "abstract"
-;         "static"
-;         (accessibility_modifier)
-;         ]* @context
-;     name: (_) @name) @item
