ff8a89a0758119ba62eb64ef5120361b90c24edc
Mikayla Maki <mikayla@zed.dev>
2023-07-13 22:30:41 -0700

    Refine svelte queries to work with zed-style highlights Bump scheme dependency:

--- crates/zed/src/languages/svelte/indents.scm
+++ crates/zed/src/languages/svelte/indents.scm
@@ -6,15 +6,3 @@
   (script_element)
   (style_element)
 ] @indent
-
-[
-  (end_tag)
-  (else_statement)
-  (if_end_expr)
-  (each_end_expr)
-  (await_end_expr)
-  ">"
-  "/>"
-] @branch
-
-(comment) @ignore
