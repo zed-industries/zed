3a625d15d30ba26c4500f88de7a16dc980bc0019
KCaverly <kyle@zed.dev>
2023-07-14 11:33:49 -0400

    update c embedding query for preceding comments

--- crates/zed/src/languages/c/embedding.scm
+++ crates/zed/src/languages/c/embedding.scm
@@ -1,39 +1,43 @@
-(declaration
-    (type_qualifier)? @context
-    type: (_)? @context
-    declarator: [
-        (function_declarator
-            declarator: (_) @name)
-        (pointer_declarator
-            "*" @context
-            declarator: (function_declarator
-                declarator: (_) @name))
-        (pointer_declarator
-            "*" @context
-            declarator: (pointer_declarator
-                "*" @context
+(
+    (comment)* @context
+    .
+    (declaration
+        declarator: [
+            (function_declarator
+                declarator: (_) @name)
+            (pointer_declarator
+                "*" @name
                 declarator: (function_declarator
-                    declarator: (_) @name)))
-    ]
-) @item
+                    declarator: (_) @name))
+            (pointer_declarator
+                "*" @name
+                declarator: (pointer_declarator
+                    "*" @name
+                    declarator: (function_declarator
+                        declarator: (_) @name)))
+            ]
+        ) @item
+    )
 
-(function_definition
-    (type_qualifier)? @context
-    type: (_)? @context
-    declarator: [
-        (function_declarator
-            declarator: (_) @name
-                )
-        (pointer_declarator
-            "*" @context
-            declarator: (function_declarator
+(
+    (comment)* @context
+    .
+    (function_definition
+        declarator: [
+            (function_declarator
                 declarator: (_) @name
-                    ))
-        (pointer_declarator
-            "*" @context
-            declarator: (pointer_declarator
-                "*" @context
+                )
+            (pointer_declarator
+                "*" @name
                 declarator: (function_declarator
-                    declarator: (_) @name)))
-    ]
-) @item
+                    declarator: (_) @name
+                    ))
+            (pointer_declarator
+                "*" @name
+                declarator: (pointer_declarator
+                    "*" @name
+                    declarator: (function_declarator
+                        declarator: (_) @name)))
+            ]
+        ) @item
+    )
