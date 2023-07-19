8b4da4c929394e1bd903f33acadf78479f33122b
Nate Butler <iamnbutler@gmail.com>
2023-07-18 20:41:39 -0400

    Start on swift highlighting

---
+++ crates/zed/src/languages/swift/brackets.scm
@@ -0,0 +1,7 @@
+("(" @open ")" @close)
+("[" @open "]" @close)
+("{" @open "}" @close)
+("<" @open ">" @close) ; for generics
+("\"" @open "\"" @close)
+("/*" @open "*/" @close)
+("?" @maybe "?" @close) ; for optionals
