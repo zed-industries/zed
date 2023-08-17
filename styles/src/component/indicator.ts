8b1322745d6d21b17bcb971411d85019f85e2331
Nate Butler <iamnbutler@gmail.com>
2023-08-16 22:50:02 -0400

    Fix collab indicators

--- styles/src/component/indicator.ts
+++ styles/src/component/indicator.ts
@@ -1,9 +1,9 @@
-import { background } from "../style_tree/components"
+import { foreground } from "../style_tree/components"
 import { Layer, StyleSets } from "../theme"
 
 export const indicator = ({ layer, color }: { layer: Layer, color: StyleSets }) => ({
     corner_radius: 4,
     padding: 4,
     margin: { top: 12, left: 12 },
-    background: background(layer, color),
+    background: foreground(layer, color),
 })
