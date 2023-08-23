d4d6071b1cee2fd4c53c7b82af7f0983036dcc8c
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2023-08-23 16:10:06 +0200

    cargo fmt again

--- crates/project/src/project.rs
+++ crates/project/src/project.rs
@@ -5143,7 +5143,8 @@ impl Project {
                             let mut buffers_rx = buffers_rx.clone();
                             scope.spawn(async move {
                                 while let Some((entry, buffer_index)) = buffers_rx.next().await {
-                                    let buffer_matches = if let Some((_, snapshot)) = entry.as_ref() {
+                                    let buffer_matches = if let Some((_, snapshot)) = entry.as_ref()
+                                    {
                                         if query.file_matches(
                                             snapshot.file().map(|file| file.path().as_ref()),
                                         ) {
@@ -5423,7 +5424,8 @@ impl Project {
                 })
                 .detach();
             }
-        }).detach();
+        })
+        .detach();
         (sorted_buffers_rx, buffers_rx)
     }
 
