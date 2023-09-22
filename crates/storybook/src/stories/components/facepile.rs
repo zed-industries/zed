5728f1efaddde2d91695f3b6d441b8af14c6d9d8
Marshall Bowers <elliott.codes@gmail.com>
2023-09-22 14:43:35 -0400

    Cut down on duplication in Facepile stories

--- crates/storybook/src/stories/components/facepile.rs
+++ crates/storybook/src/stories/components/facepile.rs
@@ -13,6 +13,12 @@ impl FacepileStory {
     fn render<V: 'static>(&mut self, _: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
         let theme = theme(cx);
 
+        let avatars = vec![
+            avatar("https://avatars.githubusercontent.com/u/1714999?v=4"),
+            avatar("https://avatars.githubusercontent.com/u/482957?v=4"),
+            avatar("https://avatars.githubusercontent.com/u/1789?v=4"),
+        ];
+
         Story::container()
             .child(Story::title(std::any::type_name::<ui::Facepile>()))
             .child(Story::label("Default"))
@@ -20,39 +26,25 @@ impl FacepileStory {
                 div()
                     .flex()
                     .gap_3()
-                    .child(facepile(vec![avatar(
-                        "https://avatars.githubusercontent.com/u/1714999?v=4",
-                    )]))
-                    .child(facepile(vec![
-                        avatar("https://avatars.githubusercontent.com/u/1714999?v=4"),
-                        avatar("https://avatars.githubusercontent.com/u/482957?v=4"),
-                    ]))
-                    .child(facepile(vec![
-                        avatar("https://avatars.githubusercontent.com/u/1714999?v=4"),
-                        avatar("https://avatars.githubusercontent.com/u/482957?v=4"),
-                        avatar("https://avatars.githubusercontent.com/u/1789?v=4"),
-                    ])),
+                    .child(facepile(avatars.clone().into_iter().take(1)))
+                    .child(facepile(avatars.clone().into_iter().take(2)))
+                    .child(facepile(avatars.clone().into_iter().take(3))),
             )
             .child(Story::label("Rounded rectangle avatars"))
             .child({
                 let shape = Shape::RoundedRectangle;
 
+                let avatars = avatars
+                    .clone()
+                    .into_iter()
+                    .map(|avatar| avatar.shape(Shape::RoundedRectangle));
+
                 div()
                     .flex()
                     .gap_3()
-                    .child(facepile(vec![avatar(
-                        "https://avatars.githubusercontent.com/u/1714999?v=4",
-                    )
-                    .shape(shape)]))
-                    .child(facepile(vec![
-                        avatar("https://avatars.githubusercontent.com/u/1714999?v=4").shape(shape),
-                        avatar("https://avatars.githubusercontent.com/u/482957?v=4").shape(shape),
-                    ]))
-                    .child(facepile(vec![
-                        avatar("https://avatars.githubusercontent.com/u/1714999?v=4").shape(shape),
-                        avatar("https://avatars.githubusercontent.com/u/482957?v=4").shape(shape),
-                        avatar("https://avatars.githubusercontent.com/u/1789?v=4").shape(shape),
-                    ]))
+                    .child(facepile(avatars.clone().take(1)))
+                    .child(facepile(avatars.clone().take(2)))
+                    .child(facepile(avatars.clone().take(3)))
             })
     }
 }
