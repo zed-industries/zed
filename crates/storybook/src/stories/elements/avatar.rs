0a9e83aeaab33d63bdc9c38f91453b46057a2f34
Marshall Bowers <elliott.codes@gmail.com>
2023-09-21 21:22:23 -0400

    Iterate on `Avatar` story

--- crates/storybook/src/stories/elements/avatar.rs
+++ crates/storybook/src/stories/elements/avatar.rs
@@ -1,6 +1,7 @@
 use gpui2::elements::div;
 use gpui2::style::StyleHelpers;
-use gpui2::{Element, IntoElement, ParentElement, ViewContext};
+use gpui2::{rgb, Element, Hsla, IntoElement, ParentElement, ViewContext};
+use ui::prelude::*;
 use ui::{avatar, theme};
 
 #[derive(Element, Default)]
@@ -10,8 +11,29 @@ impl AvatarStory {
     fn render<V: 'static>(&mut self, _: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
         let theme = theme(cx);
 
-        div().size_full().flex().child(avatar(
-            "https://avatars.githubusercontent.com/u/1714999?v=4",
-        ))
+        div()
+            .size_full()
+            .flex()
+            .flex_col()
+            .pt_2()
+            .px_4()
+            .child(
+                div()
+                    .text_2xl()
+                    .text_color(rgb::<Hsla>(0xffffff))
+                    .child(std::any::type_name::<ui::Avatar>()),
+            )
+            .child(
+                div()
+                    .flex()
+                    .gap_3()
+                    .child(avatar(
+                        "https://avatars.githubusercontent.com/u/1714999?v=4",
+                    ))
+                    .child(
+                        avatar("https://avatars.githubusercontent.com/u/1714999?v=4")
+                            .shape(Shape::RoundedRectangle),
+                    ),
+            )
     }
 }
