6e1f2869eee56c330d6db60961bdbe940c49ab9a
Marshall Bowers <elliott.codes@gmail.com>
2023-09-25 15:45:55 -0400

    Use constructor style for `Details`

--- crates/ui/src/elements/details.rs
+++ crates/ui/src/elements/details.rs
@@ -10,11 +10,11 @@ pub struct Details {
     meta: Option<&'static str>,
 }
 
-pub fn details(text: &'static str) -> Details {
-    Details { text, meta: None }
-}
-
 impl Details {
+    pub fn new(text: &'static str) -> Self {
+        Self { text, meta: None }
+    }
+
     pub fn meta_text(mut self, meta: &'static str) -> Self {
         self.meta = Some(meta);
         self
