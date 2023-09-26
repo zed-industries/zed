9cb7d133cedd302c85c9fa6948bf7dc64ed51c4e
Marshall Bowers <elliott.codes@gmail.com>
2023-09-26 17:27:54 -0400

    Add `Stack` trait

--- crates/ui/src/elements/stack.rs
+++ crates/ui/src/elements/stack.rs
@@ -1,12 +1,28 @@
 use gpui2::elements::div::{div, Div};
 use gpui2::style::StyleHelpers;
 
+pub trait Stack: Sized + StyleHelpers {
+    /// Horizontally stacks elements.
+    fn h_stack(self) -> Self {
+        self.flex().flex_row().items_center()
+    }
+
+    /// Vertically stacks elements.
+    fn v_stack(self) -> Self {
+        self.flex().flex_col()
+    }
+}
+
+impl<V> Stack for Div<V> {}
+
 /// Horizontally stacks elements
+#[deprecated = "Use `Stack::h_stack` instead."]
 pub fn h_stack<V: 'static>() -> Div<V> {
     div().flex().flex_row().items_center()
 }
 
 /// Vertically stacks elements
+#[deprecated = "Use `Stack::v_stack` instead."]
 pub fn v_stack<V: 'static>() -> Div<V> {
     div().flex().flex_col()
 }
