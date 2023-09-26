b52be51926a8da27be1862fe83c61d454ddc3cf1
Marshall Bowers <elliott.codes@gmail.com>
2023-09-26 13:36:42 -0400

    Push implementation details further down in the file

--- crates/ui/src/components/keybinding.rs
+++ crates/ui/src/components/keybinding.rs
@@ -7,6 +7,73 @@ use strum::{EnumIter, IntoEnumIterator};
 
 use crate::theme;
 
+#[derive(Element, Clone)]
+pub struct Keybinding {
+    /// A keybinding consists of a key and a set of modifier keys.
+    /// More then one keybinding produces a chord.
+    ///
+    /// This should always contain at least one element.
+    keybinding: Vec<(String, ModifierKeys)>,
+}
+
+impl Keybinding {
+    pub fn new(key: String, modifiers: ModifierKeys) -> Self {
+        Self {
+            keybinding: vec![(key, modifiers)],
+        }
+    }
+
+    pub fn new_chord(chord: Vec<(String, ModifierKeys)>) -> Self {
+        Self { keybinding: chord }
+    }
+
+    fn render<V: 'static>(&mut self, _: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
+        div()
+            .flex()
+            .gap_2()
+            .children(self.keybinding.iter().map(|(key, modifiers)| {
+                div()
+                    .flex()
+                    .gap_1()
+                    .children(ModifierKey::iter().filter_map(|modifier| {
+                        if modifiers.0.contains(&modifier) {
+                            Some(Key::new(modifier.glyph()))
+                        } else {
+                            None
+                        }
+                    }))
+                    .child(Key::new(key.clone()))
+            }))
+    }
+}
+
+#[derive(Element)]
+pub struct Key {
+    key: String,
+}
+
+impl Key {
+    pub fn new<K>(key: K) -> Self
+    where
+        K: Into<String>,
+    {
+        Self { key: key.into() }
+    }
+
+    fn render<V: 'static>(&mut self, _: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
+        let theme = theme(cx);
+
+        div()
+            .px_2()
+            .py_0()
+            .rounded_md()
+            .text_sm()
+            .text_color(theme.lowest.on.default.foreground)
+            .fill(theme.lowest.on.default.background)
+            .child(self.key.clone())
+    }
+}
+
 // NOTE: The order the modifier keys appear in this enum impacts the order in
 // which they are rendered in the UI.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, EnumIter)]
@@ -84,72 +151,5 @@ impl ModifierKeys {
         }
 
         self
-    }
-}
-
-#[derive(Element, Clone)]
-pub struct Keybinding {
-    /// A keybinding consists of a key and a set of modifier keys.
-    /// More then one keybinding produces a chord.
-    ///
-    /// This should always contain at least one element.
-    keybinding: Vec<(String, ModifierKeys)>,
-}
-
-impl Keybinding {
-    pub fn new(key: String, modifiers: ModifierKeys) -> Self {
-        Self {
-            keybinding: vec![(key, modifiers)],
-        }
-    }
-
-    pub fn new_chord(chord: Vec<(String, ModifierKeys)>) -> Self {
-        Self { keybinding: chord }
-    }
-
-    fn render<V: 'static>(&mut self, _: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
-        div()
-            .flex()
-            .gap_2()
-            .children(self.keybinding.iter().map(|(key, modifiers)| {
-                div()
-                    .flex()
-                    .gap_1()
-                    .children(ModifierKey::iter().filter_map(|modifier| {
-                        if modifiers.0.contains(&modifier) {
-                            Some(Key::new(modifier.glyph()))
-                        } else {
-                            None
-                        }
-                    }))
-                    .child(Key::new(key.clone()))
-            }))
-    }
-}
-
-#[derive(Element)]
-pub struct Key {
-    key: String,
-}
-
-impl Key {
-    pub fn new<K>(key: K) -> Self
-    where
-        K: Into<String>,
-    {
-        Self { key: key.into() }
-    }
-
-    fn render<V: 'static>(&mut self, _: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
-        let theme = theme(cx);
-
-        div()
-            .px_2()
-            .py_0()
-            .rounded_md()
-            .text_sm()
-            .text_color(theme.lowest.on.default.foreground)
-            .fill(theme.lowest.on.default.background)
-            .child(self.key.clone())
     }
 }
