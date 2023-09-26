1068836f100143c0f0de10432cc61465ddba488b
Nate Butler <iamnbutler@gmail.com>
2023-09-26 16:58:37 -0400

    Add panes & pane groups
    Co-Authored-By: Marshall Bowers <1486634+maxdeviant@users.noreply.github.com>

---
+++ crates/ui/src/components/panes.rs
@@ -0,0 +1,123 @@
+use std::marker::PhantomData;
+
+use gpui2::{
+    elements::{div, div::ScrollState},
+    geometry::{Length, Size},
+    hsla,
+    style::StyleHelpers,
+    Hsla, ParentElement, ViewContext,
+};
+use gpui2::{Element, IntoElement};
+
+use crate::theme;
+
+#[derive(Default, PartialEq)]
+pub enum SplitDirection {
+    #[default]
+    Horizontal,
+    Vertical,
+}
+
+#[derive(Element)]
+pub struct Pane<V: 'static> {
+    view_type: PhantomData<V>,
+    scroll_state: ScrollState,
+    size: Size<Length>,
+    fill: Hsla,
+}
+
+impl<V: 'static> Pane<V> {
+    pub fn new(scroll_state: ScrollState, size: Size<Length>) -> Self {
+        Self {
+            view_type: PhantomData,
+            scroll_state,
+            size,
+            fill: hsla(0., 1., 0.5, 1.),
+        }
+    }
+
+    pub fn fill(mut self, fill: Hsla) -> Self {
+        self.fill = fill;
+        self
+    }
+
+    fn render(&mut self, view: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
+        let theme = theme(cx);
+
+        div()
+            .flex()
+            .flex_initial()
+            .fill(self.fill)
+            .w(self.size.width)
+            .h(self.size.height)
+            .overflow_y_scroll(self.scroll_state.clone())
+    }
+}
+
+#[derive(Element)]
+pub struct PaneGroup<V: 'static> {
+    view_type: PhantomData<V>,
+    groups: Vec<PaneGroup<V>>,
+    panes: Vec<Pane<V>>,
+    split_direction: SplitDirection,
+}
+
+impl<V: 'static> PaneGroup<V> {
+    pub fn new_groups(groups: Vec<PaneGroup<V>>, split_direction: SplitDirection) -> Self {
+        Self {
+            view_type: PhantomData,
+            groups,
+            panes: Vec::new(),
+            split_direction,
+        }
+    }
+
+    pub fn new_panes(panes: Vec<Pane<V>>, split_direction: SplitDirection) -> Self {
+        Self {
+            view_type: PhantomData,
+            groups: Vec::new(),
+            panes,
+            split_direction,
+        }
+    }
+
+    fn render(&mut self, view: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
+        let theme = theme(cx);
+
+        if !self.panes.is_empty() {
+            let el = div()
+                .flex()
+                .flex_1()
+                .gap_px()
+                .w_full()
+                .h_full()
+                .fill(theme.lowest.accent.default.background)
+                .children(self.panes.iter_mut().map(|pane| pane.render(view, cx)));
+
+            if self.split_direction == SplitDirection::Horizontal {
+                return el;
+            } else {
+                return el.flex_col();
+            }
+        }
+
+        if !self.groups.is_empty() {
+            let el = div()
+                .flex()
+                .flex_1()
+                .gap_px()
+                .w_full()
+                .h_full()
+                .fill(theme.lowest.accent.default.background)
+                .children(self.groups.iter_mut().map(|group| group.render(view, cx)));
+
+            if self.split_direction == SplitDirection::Horizontal {
+                return el;
+            } else {
+                return el.flex_col();
+            }
+        }
+
+        unreachable!()
+    }
+}
--- crates/ui/src/components/workspace.rs
+++ crates/ui/src/components/workspace.rs
@@ -1,6 +1,8 @@
-use crate::{theme, ChatPanel, CollabPanel, StatusBar, TabBar, TitleBar, Toolbar};
+use crate::{theme, ChatPanel, CollabPanel, Pane, PaneGroup, SplitDirection, StatusBar, TitleBar};
 use gpui2::{
     elements::{div, div::ScrollState},
+    geometry::{relative, rems, Size},
+    hsla,
     style::StyleHelpers,
     Element, IntoElement, ParentElement, ViewContext,
 };
@@ -14,6 +16,88 @@ pub struct WorkspaceElement {
 
 impl WorkspaceElement {
     fn render<V: 'static>(&mut self, _: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
+        let temp_size = rems(36.).into();
+
+        let root_group = PaneGroup::new_groups(
+            vec![
+                PaneGroup::new_panes(
+                    vec![
+                        Pane::new(
+                            ScrollState::default(),
+                            Size {
+                                width: relative(1.).into(),
+                                height: temp_size,
+                            },
+                        )
+                        .fill(hsla(0.6, 0.5, 0.5, 1.)),
+                        Pane::new(
+                            ScrollState::default(),
+                            Size {
+                                width: relative(1.).into(),
+                                height: temp_size,
+                            },
+                        )
+                        .fill(hsla(0.5, 0.5, 0.5, 1.)),
+                    ],
+                    SplitDirection::Vertical,
+                ),
+                PaneGroup::new_groups(
+                    vec![
+                        PaneGroup::new_panes(
+                            vec![Pane::new(
+                                ScrollState::default(),
+                                Size {
+                                    width: relative(1.).into(),
+                                    height: temp_size,
+                                },
+                            )
+                            .fill(hsla(0.6, 0.5, 0.5, 1.))],
+                            SplitDirection::Horizontal,
+                        ),
+                        PaneGroup::new_panes(
+                            vec![
+                                Pane::new(
+                                    ScrollState::default(),
+                                    Size {
+                                        width: relative(1.).into(),
+                                        height: temp_size,
+                                    },
+                                )
+                                .fill(hsla(0.3, 0.2, 0.6, 1.)),
+                                Pane::new(
+                                    ScrollState::default(),
+                                    Size {
+                                        width: relative(1.).into(),
+                                        height: temp_size,
+                                    },
+                                )
+                                .fill(hsla(0.7, 0.2, 0.6, 1.)),
+                                Pane::new(
+                                    ScrollState::default(),
+                                    Size {
+                                        width: relative(1.).into(),
+                                        height: temp_size,
+                                    },
+                                )
+                                .fill(hsla(0.9, 0.2, 0.6, 1.)),
+                                Pane::new(
+                                    ScrollState::default(),
+                                    Size {
+                                        width: relative(1.).into(),
+                                        height: temp_size,
+                                    },
+                                )
+                                .fill(hsla(0.7, 0.5, 0.5, 1.)),
+                            ],
+                            SplitDirection::Horizontal,
+                        ),
+                    ],
+                    SplitDirection::Vertical,
+                ),
+            ],
+            SplitDirection::Horizontal,
+        );
+
         let theme = theme(cx).clone();
 
         div()
@@ -35,20 +119,7 @@ impl WorkspaceElement {
                     .flex_row()
                     .overflow_hidden()
                     .child(CollabPanel::new(self.left_scroll_state.clone()))
-                    .child(
-                        div()
-                            .h_full()
-                            .flex_1()
-                            .fill(theme.highest.base.default.background)
-                            .child(
-                                div()
-                                    .flex()
-                                    .flex_col()
-                                    .flex_1()
-                                    .child(TabBar::new(self.tab_bar_scroll_state.clone()))
-                                    .child(Toolbar::new()),
-                            ),
-                    )
+                    .child(root_group)
                     .child(ChatPanel::new(self.right_scroll_state.clone())),
             )
             .child(StatusBar::new())
