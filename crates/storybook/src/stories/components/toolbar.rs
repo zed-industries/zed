edf3f9ff3bb3d55b73f47310566ea3d09d46ed29
Marshall Bowers <elliott.codes@gmail.com>
2023-09-29 12:27:06 -0400

    Render symbols in `Breadcrumb` in `Toolbar` story

--- crates/storybook/src/stories/components/toolbar.rs
+++ crates/storybook/src/stories/components/toolbar.rs
@@ -1,8 +1,9 @@
 use std::path::PathBuf;
 use std::str::FromStr;
+use std::sync::Arc;
 
 use ui::prelude::*;
-use ui::{Breadcrumb, Icon, IconButton, Toolbar};
+use ui::{theme, Breadcrumb, HighlightColor, HighlightedText, Icon, IconButton, Symbol, Toolbar};
 
 use crate::story::Story;
 
@@ -11,18 +12,51 @@ pub struct ToolbarStory {}
 
 impl ToolbarStory {
     fn render<V: 'static>(&mut self, _: &mut V, cx: &mut ViewContext<V>) -> impl IntoElement<V> {
+        let theme = theme(cx);
+
+        struct LeftItemsPayload {
+            pub theme: Arc<Theme>,
+        }
+
         Story::container(cx)
             .child(Story::title_for::<_, Toolbar<V>>(cx))
             .child(Story::label(cx, "Default"))
             .child(Toolbar::new(
-                |_, _| {
+                |_, payload| {
+                    let payload = payload.downcast_ref::<LeftItemsPayload>().unwrap();
+
+                    let theme = payload.theme.clone();
+
                     vec![Breadcrumb::new(
                         PathBuf::from_str("crates/ui/src/components/toolbar.rs").unwrap(),
-                        vec![],
+                        vec![
+                            Symbol(vec![
+                                HighlightedText {
+                                    text: "impl ".to_string(),
+                                    color: HighlightColor::Keyword.hsla(&theme),
+                                },
+                                HighlightedText {
+                                    text: "ToolbarStory".to_string(),
+                                    color: HighlightColor::Function.hsla(&theme),
+                                },
+                            ]),
+                            Symbol(vec![
+                                HighlightedText {
+                                    text: "fn ".to_string(),
+                                    color: HighlightColor::Keyword.hsla(&theme),
+                                },
+                                HighlightedText {
+                                    text: "render".to_string(),
+                                    color: HighlightColor::Function.hsla(&theme),
+                                },
+                            ]),
+                        ],
                     )
                     .into_any()]
                 },
-                Box::new(()),
+                Box::new(LeftItemsPayload {
+                    theme: theme.clone(),
+                }),
                 |_, _| {
                     vec![
                         IconButton::new(Icon::InlayHint).into_any(),
