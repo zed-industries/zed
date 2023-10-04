0323a60d85ee0981d943704460423679f36c6bab
Marshall Bowers <elliott.codes@gmail.com>
2023-10-04 13:10:56 -0400

    Remove unneeded `theme` function
    This also fixes the panic when trying to declare the `GPUIApplication` class.

--- crates/storybook2/src/ui.rs
+++ crates/storybook2/src/ui.rs
@@ -2,11 +2,9 @@ mod children;
 mod components;
 mod elements;
 pub mod prelude;
-mod theme;
 mod tokens;
 
 pub use children::*;
 pub use components::*;
 pub use elements::*;
-pub use theme::*;
 pub use tokens::*;
