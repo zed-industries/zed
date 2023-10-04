d09767a90b0903585a99fece6b3d9d115908ee11
Max Brunsfeld <maxbrunsfeld@gmail.com>
2023-10-04 14:38:59 -0700

    Ensure chat messages are retrieved in order of id

--- crates/collab/src/db/queries.rs
+++ crates/collab/src/db/queries.rs
@@ -9,13 +9,3 @@ pub mod projects;
 pub mod rooms;
 pub mod servers;
 pub mod users;
-
-fn max_assign<T: Ord>(max: &mut Option<T>, val: T) {
-    if let Some(max_val) = max {
-        if val > *max_val {
-            *max = Some(val);
-        }
-    } else {
-        *max = Some(val);
-    }
-}
