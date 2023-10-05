a63eccf18839ecbfd35f45bd2da87b0c73a4cb2d
Conrad Irwin <conrad.irwin@gmail.com>
2023-10-04 22:46:28 -0600

    Add url schemes to Zed

--- crates/util/src/channel.rs
+++ crates/util/src/channel.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 use lazy_static::lazy_static;
+use url::Url;
 
 lazy_static! {
     pub static ref RELEASE_CHANNEL_NAME: String = if cfg!(debug_assertions) {
@@ -15,6 +16,23 @@ lazy_static! {
         "stable" => ReleaseChannel::Stable,
         _ => panic!("invalid release channel {}", *RELEASE_CHANNEL_NAME),
     };
+
+    static ref URL_SCHEME: Url = Url::parse(match RELEASE_CHANNEL_NAME.as_str() {
+        "dev" => "zed-dev:/",
+        "preview" => "zed-preview:/",
+        "stable" => "zed:/",
+        // NOTE: this must be kept in sync with ./script/bundle and https://zed.dev.
+        _ => unreachable!(),
+    })
+    .unwrap();
+    static ref LINK_PREFIX: Url = Url::parse(match RELEASE_CHANNEL_NAME.as_str() {
+        "dev" => "http://localhost:3000/dev/",
+        "preview" => "https://zed.dev/preview/",
+        "stable" => "https://zed.dev/",
+        // NOTE: this must be kept in sync with https://zed.dev.
+        _ => unreachable!(),
+    })
+    .unwrap();
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Default)]
@@ -40,5 +58,13 @@ impl ReleaseChannel {
             ReleaseChannel::Preview => "preview",
             ReleaseChannel::Stable => "stable",
         }
+    }
+
+    pub fn url_scheme(&self) -> &'static Url {
+        &URL_SCHEME
+    }
+
+    pub fn link_prefix(&self) -> &'static Url {
+        &LINK_PREFIX
     }
 }
