f8fd77b83e80743d12a38a47e960fd98e0bfddf4
Conrad Irwin <conrad.irwin@gmail.com>
2023-10-13 15:08:09 -0600

    fix migration

--- crates/collab/migrations/20231011214412_add_guest_role.sql
+++ crates/collab/migrations/20231011214412_add_guest_role.sql
@@ -1,4 +1,4 @@
 ALTER TABLE channel_members ADD COLUMN role TEXT;
 UPDATE channel_members SET role = CASE WHEN admin THEN 'admin' ELSE 'member' END;
 
-ALTER TABLE channels ADD COLUMN visibility TEXT NOT NULL DEFAULT 'channel_members';
+ALTER TABLE channels ADD COLUMN visibility TEXT NOT NULL DEFAULT 'members';
