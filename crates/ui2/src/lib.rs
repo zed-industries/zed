c9d214e8ef5a2e8c99a7943c3d27a409d080753d
Nate Butler <iamnbutler@gmail.com>
2023-10-23 10:21:30 -0400

    Start crate doc

--- crates/ui2/src/lib.rs
+++ crates/ui2/src/lib.rs
@@ -1,3 +1,19 @@
+//! # UI – Zed UI Primitives & Components
+//!
+//! This crate provides a set of UI primitives and components that are used to build all of the elements in Zed's UI.
+//!
+//! ## Work in Progress
+//!
+//! This crate is still a work in progress. The initial primitives and components are built for getting all the UI on the screen,
+//! much of the state and functionality is mocked or hard codeded, and performance has not been a focus.
+//!
+//! Expect some inconsistencies from component to component as we work out the best way to build these components.
+//!
+//! ## Design Philosophy
+//!
+//! Work in Progress!
+//!
+
 #![allow(dead_code, unused_variables)]
 
 mod components;
