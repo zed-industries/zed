6f8947a3d343a3cb8cc178fa7a95aef7ace6652b
Kirill Bulatov <kirill@zed.dev>
2023-11-03 18:42:37 +0200

    Fix a compilation error

--- crates/editor/src/editor_tests.rs
+++ crates/editor/src/editor_tests.rs
@@ -34,7 +34,7 @@ use util::{
     test::{marked_text_ranges, marked_text_ranges_by, sample_text, TextRangeMarker},
 };
 use workspace::{
-    item::{FollowableItem, Item},
+    item::{FollowableItem, Item, ItemHandle},
     NavigationEntry, ViewId,
 };
 
