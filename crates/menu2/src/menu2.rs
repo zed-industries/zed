d25f48ed6b1bf097f3d3eae0c57935d856f3341b
Max Brunsfeld <maxbrunsfeld@gmail.com>
2023-11-08 09:35:19 -0800

    Register menu2 actions using actions macro

--- crates/menu2/src/menu2.rs
+++ crates/menu2/src/menu2.rs
@@ -1,25 +1,12 @@
-use serde_derive::Deserialize;
+use gpui::actions;
 
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct Cancel;
-
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct Confirm;
-
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct SecondaryConfirm;
-
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct SelectPrev;
-
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct SelectNext;
-
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct SelectFirst;
-
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct SelectLast;
-
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct ShowContextMenu;
+actions!(
+    Cancel,
+    Confirm,
+    SecondaryConfirm,
+    SelectPrev,
+    SelectNext,
+    SelectFirst,
+    SelectLast,
+    ShowContextMenu
+);
