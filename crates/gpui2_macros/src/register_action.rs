c5d5571ad35f6aa15a146550f5d81fe269a579c2
Antonio Scandurra <me@as-cii.com>
2023-11-08 12:31:35 +0100

    Fix warnings when generating function name for `ctor`
    Co-Authored-By: Piotr <piotr@zed.dev>

--- crates/gpui2_macros/src/register_action.rs
+++ crates/gpui2_macros/src/register_action.rs
@@ -18,7 +18,7 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn register_action(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
     let type_name = &input.ident;
-    let ctor_fn_name = format_ident!("register_{}_builder", type_name);
+    let ctor_fn_name = format_ident!("register_{}_builder", type_name.to_string().to_lowercase());
 
     let expanded = quote! {
         #input
