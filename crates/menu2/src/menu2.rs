408a495aaf7d7746453cc7621ee3c25b5f3aae82
Nathan Sobo <nathan@zed.dev>
2023-11-09 13:14:11 -0700

    Call init

--- crates/menu2/src/menu2.rs
+++ crates/menu2/src/menu2.rs
@@ -1,7 +1,10 @@
 use gpui::actions;
 
-// If the zed binary doesn't use anything in this crate, it will be optimized out
-// and the actions won't initialize. So we just call an empty initialization function.
+// If the zed binary doesn't use anything in this crate, it will be optimized away
+// and the actions won't initialize. So we just provide an empty initialization function
+// to be called from main.
+//
+// These may provide relevant context:
 // https://github.com/rust-lang/rust/issues/47384
 // https://github.com/mmastrac/rust-ctor/issues/280
 pub fn init() {}
