5dca5caf9fe06d34b902a13bc99b5b8af0ad279e
Nate Butler <iamnbutler@gmail.com>
2023-11-10 16:12:14 -0500

    Add elevation to StyledExt
    Co-Authored-By: Marshall Bowers <1486634+maxdeviant@users.noreply.github.com>

--- crates/ui2/src/styled_ext.rs
+++ crates/ui2/src/styled_ext.rs
@@ -1,6 +1,15 @@
-use gpui::{Div, ElementFocus, ElementInteractivity, Styled};
+use gpui::{Div, ElementFocus, ElementInteractivity, Styled, UniformList, ViewContext};
+use theme2::ActiveTheme;
 
-use crate::UITextSize;
+use crate::{ElevationIndex, UITextSize};
+
+fn elevated<E: Styled, V: 'static>(this: E, cx: &mut ViewContext<V>, index: ElevationIndex) -> E {
+    this.bg(cx.theme().colors().elevated_surface_background)
+        .rounded_lg()
+        .border()
+        .border_color(cx.theme().colors().border_variant)
+        .shadow(index.shadow())
+}
 
 /// Extends [`Styled`](gpui::Styled) with Zed specific styling methods.
 pub trait StyledExt: Styled {
@@ -64,6 +73,48 @@ pub trait StyledExt: Styled {
 
         self.text_size(size)
     }
+
+    /// The [`Surface`](ui2::ElevationIndex::Surface) elevation level, located above the app background, is the standard level for all elements
+    ///
+    /// Sets `bg()`, `rounded_lg()`, `border()`, `border_color()`, `shadow()`
+    ///
+    /// Example Elements: Title Bar, Panel, Tab Bar, Editor
+    fn elevation_1<V: 'static>(self, cx: &mut ViewContext<V>) -> Self
+    where
+        Self: Styled + Sized,
+    {
+        elevated(self, cx, ElevationIndex::Surface)
+    }
+
+    /// Non-Modal Elevated Surfaces appear above the [`Surface`](ui2::ElevationIndex::Surface) layer and is used for things that should appear above most UI elements like an editor or panel, but not elements like popovers, context menus, modals, etc.
+    ///
+    /// Sets `bg()`, `rounded_lg()`, `border()`, `border_color()`, `shadow()`
+    ///
+    /// Examples: Notifications, Palettes, Detached/Floating Windows, Detached/Floating Panels
+    fn elevation_2<V: 'static>(self, cx: &mut ViewContext<V>) -> Self
+    where
+        Self: Styled + Sized,
+    {
+        elevated(self, cx, ElevationIndex::ElevatedSurface)
+    }
+
+    // There is no elevation 3, as the third elevation level is reserved for wash layers. See [`Elevation`](ui2::Elevation).
+
+    /// Modal Surfaces are used for elements that should appear above all other UI elements and are located above the wash layer. This is the maximum elevation at which UI elements can be rendered in their default state.
+    ///
+    /// Elements rendered at this layer should have an enforced behavior: Any interaction outside of the modal will either dismiss the modal or prompt an action (Save your progress, etc) then dismiss the modal.
+    ///
+    /// If the element does not have this behavior, it should be rendered at the [`Elevated Surface`](ui2::ElevationIndex::ElevatedSurface) layer.
+    ///
+    /// Sets `bg()`, `rounded_lg()`, `border()`, `border_color()`, `shadow()`
+    ///
+    /// Examples: Settings Modal, Channel Management, Wizards/Setup UI, Dialogs
+    fn elevation_4<V: 'static>(self, cx: &mut ViewContext<V>) -> Self
+    where
+        Self: Styled + Sized,
+    {
+        elevated(self, cx, ElevationIndex::ModalSurface)
+    }
 }
 
 impl<V, I, F> StyledExt for Div<V, I, F>
@@ -72,3 +123,5 @@ where
     F: ElementFocus<V>,
 {
 }
+
+impl<V> StyledExt for UniformList<V> {}
