3d28495c678a51bd07133c480b77ff5254cf5c66
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2023-11-20 18:35:39 +0100

    fixup! Remove dbg statements

--- crates/editor2/src/editor.rs
+++ crates/editor2/src/editor.rs
@@ -2320,7 +2320,6 @@ impl Editor {
         self.blink_manager.update(cx, BlinkManager::pause_blinking);
         cx.emit(Event::SelectionsChanged { local });
         cx.emit(SearchEvent::MatchesInvalidated);
-        dbg!(cx.entity_id());
 
         if self.selections.disjoint_anchors().len() == 1 {
             cx.emit(SearchEvent::ActiveMatchChanged)
