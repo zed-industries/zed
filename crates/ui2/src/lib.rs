5c0f2e75ea7e1c5391fedd1f211e0e4def636b94
Conrad Irwin <conrad.irwin@gmail.com>
2023-11-20 16:30:14 -0700

    Remove all the settings

--- crates/ui2/src/lib.rs
+++ crates/ui2/src/lib.rs
@@ -17,34 +17,15 @@
 mod components;
 mod elevation;
 pub mod prelude;
-pub mod settings;
-// mod static_data;
 mod styled_ext;
-mod to_extract;
 pub mod utils;
 
 pub use components::*;
-use gpui::actions;
 pub use prelude::*;
 // pub use static_data::*;
 pub use styled_ext::*;
-pub use to_extract::*;
-
-// This needs to be fully qualified with `crate::` otherwise we get a panic
-// at:
-//   thread '<unnamed>' panicked at crates/gpui2/src/platform/mac/platform.rs:66:81:
-//   called `Option::unwrap()` on a `None` value
-//
-// AFAICT this is something to do with conflicting names between crates and modules that
-// interfaces with declaring the `ClassDecl`.
-pub use crate::settings::*;
 
 #[cfg(feature = "stories")]
 mod story;
 #[cfg(feature = "stories")]
 pub use story::*;
-actions!(NoAction);
-
-pub fn binding(key: &str) -> gpui::KeyBinding {
-    gpui::KeyBinding::new(key, NoAction {}, None)
-}
