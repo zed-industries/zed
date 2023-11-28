979ff70196ddd7286dedf8d2a27ebba3b160a57f
Nate Butler <iamnbutler@gmail.com>
2023-11-27 20:12:20 -0500

    Update popover.rs

--- crates/ui2/src/components/popover.rs
+++ crates/ui2/src/components/popover.rs
@@ -1,5 +1,5 @@
 use gpui::{
-    AnyElement, Div, Element, ElementId, IntoElement, ParentElement, RenderOnce, Styled,
+    div, AnyElement, Div, Element, ElementId, IntoElement, ParentElement, RenderOnce, Styled,
     WindowContext,
 };
 use smallvec::SmallVec;
@@ -44,23 +44,16 @@ impl RenderOnce for Popover {
     type Rendered = Div;
 
     fn render(self, cx: &mut WindowContext) -> Self::Rendered {
-        v_stack()
-            .relative()
-            .elevation_2(cx)
-            .p_1()
-            .children(self.children)
+        div()
+            .flex()
+            .gap_1()
+            .child(v_stack().elevation_2(cx).px_1().children(self.children))
             .when_some(self.aside, |this, aside| {
-                // TODO: This will statically position the aside to the top right of the popover.
-                // We should update this to use gpui2::overlay avoid collisions with the window edges.
                 this.child(
                     v_stack()
-                        .top_0()
-                        .left_full()
-                        .ml_1()
-                        .absolute()
                         .elevation_2(cx)
                         .bg(cx.theme().colors().surface_background)
-                        .p_1()
+                        .px_1()
                         .child(aside),
                 )
             })
