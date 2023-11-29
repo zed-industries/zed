06bbcd16ade272130dbd702685a38b4607a008d8
Marshall Bowers <elliott.codes@gmail.com>
2023-11-29 12:54:49 -0500

    Extract `Disableable` trait

--- crates/ui2/src/fixed.rs
+++ crates/ui2/src/fixed.rs
@@ -1,6 +1,10 @@
 use gpui::DefiniteLength;
 
+/// A trait for elements that have a fixed with.
 pub trait FixedWidth {
+    /// Sets the width of the element.
     fn width(self, width: DefiniteLength) -> Self;
+
+    /// Sets the element's width to the full width of its container.
     fn full_width(self) -> Self;
 }
