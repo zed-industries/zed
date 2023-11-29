c8760904bb6d58c4628564e932fbb38d71b186aa
Marshall Bowers <elliott.codes@gmail.com>
2023-11-29 17:26:56 -0500

    Delete `OldButton`

--- crates/ui2/src/prelude.rs
+++ crates/ui2/src/prelude.rs
@@ -8,5 +8,5 @@ pub use crate::clickable::*;
 pub use crate::disableable::*;
 pub use crate::fixed::*;
 pub use crate::selectable::*;
-pub use crate::{ButtonCommon, ButtonVariant, Color, StyledExt};
+pub use crate::{ButtonCommon, Color, StyledExt};
 pub use theme::ActiveTheme;
