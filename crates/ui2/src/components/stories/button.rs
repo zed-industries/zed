4b23c5c658351a2132a5c0af5aab86b0782c56b9
Marshall Bowers <elliott.codes@gmail.com>
2023-12-01 13:29:11 -0500

    Add `selected_label` to `Button` (#3478)
    This PR adds a new `selected_label` method to `Button`.
    
    This can be used to set a different label that should be rendered when
    the `Button` is selected.
    
    Release Notes:
    
    - N/A

--- crates/ui2/src/components/stories/button.rs
+++ crates/ui2/src/components/stories/button.rs
@@ -16,6 +16,12 @@ impl Render for ButtonStory {
             .child(Button::new("default_filled", "Click me"))
             .child(Story::label("Selected"))
             .child(Button::new("selected_filled", "Click me").selected(true))
+            .child(Story::label("Selected with `selected_label`"))
+            .child(
+                Button::new("selected_label_filled", "Click me")
+                    .selected(true)
+                    .selected_label("I have been selected"),
+            )
             .child(Story::label("With `label_color`"))
             .child(Button::new("filled_with_label_color", "Click me").color(Color::Created))
             .child(Story::label("Default (Subtle)"))
