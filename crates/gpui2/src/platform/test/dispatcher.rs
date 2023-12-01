aed11ee8cbeb25fca0a089b3d1b921c41a60d3e2
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2023-12-01 17:22:12 +0100

    editor tests: Reintroduce block_on_ticks.
    Co-authored-by: Antonio <antonio@zed.dev>

--- crates/gpui2/src/platform/test/dispatcher.rs
+++ crates/gpui2/src/platform/test/dispatcher.rs
@@ -7,6 +7,7 @@ use parking_lot::Mutex;
 use rand::prelude::*;
 use std::{
     future::Future,
+    ops::RangeInclusive,
     pin::Pin,
     sync::Arc,
     task::{Context, Poll},
@@ -36,6 +37,7 @@ struct TestDispatcherState {
     allow_parking: bool,
     waiting_backtrace: Option<Backtrace>,
     deprioritized_task_labels: HashSet<TaskLabel>,
+    block_on_ticks: RangeInclusive<usize>,
 }
 
 impl TestDispatcher {
@@ -53,6 +55,7 @@ impl TestDispatcher {
             allow_parking: false,
             waiting_backtrace: None,
             deprioritized_task_labels: Default::default(),
+            block_on_ticks: 0..=1000,
         };
 
         TestDispatcher {
@@ -82,8 +85,8 @@ impl TestDispatcher {
     }
 
     pub fn simulate_random_delay(&self) -> impl 'static + Send + Future<Output = ()> {
-        pub struct YieldNow {
-            count: usize,
+        struct YieldNow {
+            pub(crate) count: usize,
         }
 
         impl Future for YieldNow {
@@ -141,6 +144,16 @@ impl TestDispatcher {
 
     pub fn rng(&self) -> StdRng {
         self.state.lock().random.clone()
+    }
+
+    pub fn set_block_on_ticks(&self, range: std::ops::RangeInclusive<usize>) {
+        self.state.lock().block_on_ticks = range;
+    }
+
+    pub fn gen_block_on_ticks(&self) -> usize {
+        let mut lock = self.state.lock();
+        let block_on_ticks = lock.block_on_ticks.clone();
+        lock.random.gen_range(block_on_ticks)
     }
 }
 
