7a0aa1e5df32396f71ab84fbaaf4b31d8df93eaf
Nate Butler <iamnbutler@gmail.com>
2023-12-04 11:32:25 -0500

    Clean up ButtonLike
    Co-Authored-By: Marshall Bowers <1486634+maxdeviant@users.noreply.github.com>

--- crates/ui2/src/components/button/button_like.rs
+++ crates/ui2/src/components/button/button_like.rs
@@ -323,23 +323,13 @@ impl RenderOnce for ButtonLike {
             .id(self.id.clone())
             .h(self.size.height())
             .rounded_md()
-            .when(!self.disabled, |el| el.cursor_pointer())
             .gap_1()
             .px_1()
             .bg(self.style.enabled(cx).background)
-            .hover(|hover| {
-                hover.bg(if self.disabled {
-                    self.style.disabled(cx).background
-                } else {
-                    self.style.hovered(cx).background
-                })
-            })
-            .active(|active| {
-                active.bg(if self.disabled {
-                    self.style.disabled(cx).background
-                } else {
-                    self.style.active(cx).background
-                })
+            .when(!self.disabled, |this| {
+                this.cursor_pointer()
+                    .hover(|hover| hover.bg(self.style.hovered(cx).background))
+                    .active(|active| active.bg(self.style.active(cx).background))
             })
             .when_some(
                 self.on_click.filter(|_| !self.disabled),
