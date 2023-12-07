901bd6ac87b675299cb59d2325b4c0b03f2df5ce
Max Brunsfeld <maxbrunsfeld@gmail.com>
2023-12-07 14:22:36 -0800

    Order face pile front to back, left to right

--- crates/collab_ui2/src/face_pile.rs
+++ crates/collab_ui2/src/face_pile.rs
@@ -16,7 +16,10 @@ impl RenderOnce for FacePile {
         let player_list = self.faces.into_iter().enumerate().map(|(ix, player)| {
             let isnt_last = ix < player_count - 1;
 
-            div().when(isnt_last, |div| div.neg_mr_1()).child(player)
+            div()
+                .z_index((player_count - ix) as u32)
+                .when(isnt_last, |div| div.neg_mr_1())
+                .child(player)
         });
         div().p_1().flex().items_center().children(player_list)
     }
