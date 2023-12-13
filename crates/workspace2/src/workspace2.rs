b24385269f418a52b4512b407c87277294eaf5c6
Marshall Bowers <elliott.codes@gmail.com>
2023-12-12 20:24:43 -0500

    Remove debug tab bar

--- crates/workspace2/src/workspace2.rs
+++ crates/workspace2/src/workspace2.rs
@@ -3589,43 +3589,6 @@ struct DockDragState(Option<DockPosition>);
 #[derive(Default)]
 struct DockClickReset(Option<Task<()>>);
 
-fn debug_tab_bar() -> impl IntoElement {
-    let tab_count = 20;
-    let selected_tab_index = 3;
-
-    let tabs = (0..tab_count)
-        .map(|index| {
-            Tab::new(index)
-                .selected(index == selected_tab_index)
-                .position(if index == 0 {
-                    TabPosition::First
-                } else if index == tab_count - 1 {
-                    TabPosition::Last
-                } else {
-                    TabPosition::Middle(index.cmp(&selected_tab_index))
-                })
-                .child(Label::new(format!("Tab {}", index + 1)).color(
-                    if index == selected_tab_index {
-                        Color::Default
-                    } else {
-                        Color::Muted
-                    },
-                ))
-        })
-        .collect::<Vec<_>>();
-
-    TabBar::new("tab_bar_1")
-        .start_child(
-            IconButton::new("navigate_backward", Icon::ArrowLeft).icon_size(IconSize::Small),
-        )
-        .start_child(
-            IconButton::new("navigate_forward", Icon::ArrowRight).icon_size(IconSize::Small),
-        )
-        .end_child(IconButton::new("new", Icon::Plus).icon_size(IconSize::Small))
-        .end_child(IconButton::new("split_pane", Icon::Split).icon_size(IconSize::Small))
-        .children(tabs)
-}
-
 impl Render for Workspace {
     type Element = Div;
 
