dad9f01339280637b824f0b98142cad79604a721
Marshall Bowers <elliott.codes@gmail.com>
2023-12-15 14:36:14 -0500

    Format code

--- crates/workspace2/src/pane.rs
+++ crates/workspace2/src/pane.rs
@@ -1897,9 +1897,7 @@ impl Render for Pane {
                     .on_drag_move::<ProjectEntryId>(cx.listener(Self::handle_drag_move))
                     .map(|div| {
                         if let Some(item) = self.active_item() {
-                            v_stack()
-                                .child(self.toolbar.clone())
-                                .child(item.to_any())
+                            v_stack().child(self.toolbar.clone()).child(item.to_any())
                         } else {
                             div.flex()
                                 .flex_row()
