14066c4f0ed0abf209c08d737c5fa7b00d555b3a
Julia <floc@unpromptedtirade.com>
2024-01-02 17:56:19 -0500

    Add `icon_tooltip` for panel buttons rather than using persistent name

--- crates/collab_ui2/src/collab_panel.rs
+++ crates/collab_ui2/src/collab_panel.rs
@@ -2347,6 +2347,10 @@ impl Panel for CollabPanel {
             .then(|| ui::Icon::Collab)
     }
 
+    fn icon_tooltip(&self, _cx: &WindowContext) -> Option<&'static str> {
+        Some("Collab Panel")
+    }
+
     fn toggle_action(&self) -> Box<dyn gpui::Action> {
         Box::new(ToggleFocus)
     }
