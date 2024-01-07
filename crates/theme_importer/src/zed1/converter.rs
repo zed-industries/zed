03b3f73fc0604f9e829b762d59e39363af7d93e0
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-01-07 13:38:39 +0100

    Fix up tests

--- crates/theme_importer/src/zed1/converter.rs
+++ crates/theme_importer/src/zed1/converter.rs
@@ -1,5 +1,5 @@
-use anyhow::{Context, Result};
-use gpui::{serde_json, Hsla, Rgba};
+use anyhow::Result;
+use gpui::{Hsla, Rgba};
 use theme::{
     color_alpha, Appearance, PlayerColor, PlayerColors, StatusColorsRefinement,
     ThemeColorsRefinement, UserFontStyle, UserFontWeight, UserHighlightStyle, UserSyntaxTheme,
