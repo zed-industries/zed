00c143274f2326f9ec7562cd632f7c71958dca1d
Marshall Bowers <elliott.codes@gmail.com>
2024-01-09 10:39:27 -0500

    Add `SharedUrl` type

--- crates/gpui/src/shared_string.rs
+++ crates/gpui/src/shared_string.rs
@@ -99,3 +99,24 @@ impl<'de> Deserialize<'de> for SharedString {
         Ok(SharedString::from(s))
     }
 }
+
+#[derive(Deref, DerefMut, Default)]
+pub struct SharedUrl(SharedString);
+
+impl std::fmt::Debug for SharedUrl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl std::fmt::Display for SharedUrl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0.as_ref())
+    }
+}
+
+impl<T: Into<SharedString>> From<T> for SharedUrl {
+    fn from(value: T) -> Self {
+        Self(value.into())
+    }
+}
