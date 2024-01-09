c19551d9746522019e5364ea5a5894fbfe4cf388
Nate Butler <iamnbutler@gmail.com>
2024-01-09 14:21:29 -0500

    Document free functions

--- crates/ui/src/components/stack.rs
+++ crates/ui/src/components/stack.rs
@@ -2,17 +2,13 @@ use gpui::{div, Div};
 
 use crate::StyledExt;
 
-/// Horizontally stacks elements.
-///
-/// Sets `flex()`, `flex_row()`, `items_center()`
+/// Horizontally stacks elements. Sets `flex()`, `flex_row()`, `items_center()`
 #[track_caller]
 pub fn h_stack() -> Div {
     div().h_flex()
 }
 
-/// Vertically stacks elements.
-///
-/// Sets `flex()`, `flex_col()`
+/// Vertically stacks elements. Sets `flex()`, `flex_col()`
 #[track_caller]
 pub fn v_stack() -> Div {
     div().v_flex()
