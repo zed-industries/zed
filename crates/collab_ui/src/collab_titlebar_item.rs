b878514f4a1750f29aa79358b946ec501b4a803f
Marshall Bowers <elliott.codes@gmail.com>
2024-01-16 16:59:41 -0500

    Add a note about why we're drawing a transparent border

--- crates/collab_ui/src/collab_titlebar_item.rs
+++ crates/collab_ui/src/collab_titlebar_item.rs
@@ -489,6 +489,8 @@ impl CollabTitlebarItem {
                     .border_color(if is_speaking {
                         cx.theme().status().info_border
                     } else {
+                        // We draw the border in a transparent color rather to avoid
+                        // the layout shift that would come with adding/removing the border.
                         gpui::transparent_black()
                     })
                     .when(is_muted, |avatar| {
