ff3654129030fb63cbed84a3eb6edca0f4c05804
Sean Aye <hello@seanaye.ca>
2024-01-25 00:23:09 -0500

    add hello world example

---
+++ crates/gpui/examples/hello_world.rs
@@ -0,0 +1,29 @@
+use gpui::*;
+ 
+struct HelloWorld {
+    text: SharedString,
+}
+ 
+impl Render for HelloWorld {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .bg(rgb(0x2e7d32))
+            .size_full()
+            .justify_center()
+            .items_center()
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("Hello, {}!", &self.text))
+    }
+}
+ 
+fn main() {
+    App::new().run(|cx: &mut AppContext| {
+        cx.open_window(WindowOptions::default(), |cx| {
+            cx.new_view(|_cx| HelloWorld {
+                text: "World".into(),
+            })
+        });
+    });
+}
