9ebe798da4e87e37c20dd2bf1fe0e9ab43ce3afb
Iván Molina Rebolledo <ivan@ivmoreau.com>
2024-01-27 21:38:06 -0600

    [Language] Add LSP support for PureScript.

--- crates/zed/src/languages.rs
+++ crates/zed/src/languages.rs
@@ -23,6 +23,7 @@ mod language_plugin;
 mod lua;
 mod nu;
 mod php;
+mod purescript;
 mod python;
 mod ruby;
 mod rust;
@@ -258,7 +259,13 @@ pub fn init(
         ],
     );
 
-    language("purescript", tree_sitter_purescript::language(), vec![]);
+    language(
+        "purescript",
+        tree_sitter_purescript::language(),
+        vec![Arc::new(purescript::PurescriptLspAdapter::new(
+            node_runtime.clone(),
+        ))],
+    );
     language("elm", tree_sitter_elm::language(), vec![]);
     language("glsl", tree_sitter_glsl::language(), vec![]);
     language("nix", tree_sitter_nix::language(), vec![]);
