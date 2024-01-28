773a725046ea08cf1a0e1e27d3ed5dfb836ec70f
Fred Minkowski <fminkowski@gmail.com>
2024-01-28 16:01:16 -0500

    Shorten matchers so they fit in modal

--- crates/zed/src/languages/csharp/outline.scm
+++ crates/zed/src/languages/csharp/outline.scm
@@ -1,39 +1,28 @@
 (class_declaration
-    (modifier)? @context
     "class" @context
     name: (identifier) @name
 ) @item
 
 (constructor_declaration
-    (modifier)? @context
     name: (identifier) @name
 ) @item
 
 (property_declaration
-    (modifier)? @context
     type: (identifier)? @context
     type: (predefined_type)? @context
     name: (identifier) @name
 ) @item
 
 (field_declaration
-    (modifier)? @context
-    (modifier)? @context
     (variable_declaration) @context
 ) @item
 
 (method_declaration
-    (modifier)? @context
-    (modifier)? @context
-    type: (predefined_type)? @context
-    type: (identifier)? @context
-    type: (generic_name)? @context
     name: (identifier) @name
     parameters: (parameter_list) @context
 ) @item
 
 (enum_declaration
-    (modifier)? @context
     "enum" @context
     name: (identifier) @name
 ) @item
@@ -44,7 +33,6 @@
 ) @item
 
 (interface_declaration
-    (modifier)? @context
     "interface" @context
     name: (identifier) @name
 ) @item
