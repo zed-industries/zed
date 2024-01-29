27e69a7174cba055acd2b9ccbb3143b15e1140f0
Marshall Bowers <elliott.codes@gmail.com>
2024-01-29 11:49:15 -0500

    Update message

--- script/danger/dangerfile.ts
+++ script/danger/dangerfile.ts
@@ -3,11 +3,25 @@ import { danger, warn } from "danger";
 const RELEASE_NOTES_PATTERN = new RegExp("Release Notes:\\r?\\n\\s+-", "gm");
 
 const hasReleaseNotes = RELEASE_NOTES_PATTERN.test(danger.github.pr.body);
-console.log(JSON.stringify(danger.github.pr.body), hasReleaseNotes);
 if (!hasReleaseNotes) {
   warn(
-    "This PR is missing release notes. " +
-      'Please add a "Release Notes" section that describes the change. ' +
-      'If your change is not user-facing, you can use "N/A" for the release notes.',
+    [
+      "This PR is missing release notes.",
+      "",
+      'Please add a "Release Notes" section that describes the change:',
+      "",
+      "```",
+      "Release Notes:",
+      "",
+      "- (Added|Fixed|Improved) ... ([#<public_issue_number_if_exists>](https://github.com/zed-industries/zed/issues/<public_issue_number_if_exists>)).",
+      "```",
+      "",
+      'If your change is not user-facing, you can use "N/A" for the entry:',
+      "```",
+      "Release Notes:",
+      "",
+      "- N/A",
+      "```",
+    ].join("\n"),
   );
 }
