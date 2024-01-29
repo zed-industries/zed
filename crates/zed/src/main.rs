dba6649a8acfd0f905a263253ad22941c23ccd2e
Marshall Bowers <elliott.codes@gmail.com>
2024-01-29 11:54:31 -0500

    Revert test change

--- crates/zed/src/main.rs
+++ crates/zed/src/main.rs
@@ -148,8 +148,6 @@ fn main() {
 
         cx.set_global(client.clone());
 
-        // A test change.
-
         zed::init(cx);
         theme::init(theme::LoadThemes::All(Box::new(Assets)), cx);
         project::Project::init(&client, cx);
