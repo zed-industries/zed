6cd83a6ae2b16e50269852120149b6e5c13012d0
Conrad Irwin <conrad.irwin@gmail.com>
2024-01-29 18:23:04 -0700

    Allow per-environment settings

--- crates/settings/src/settings_store.rs
+++ crates/settings/src/settings_store.rs
@@ -13,7 +13,9 @@ use std::{
     str,
     sync::Arc,
 };
-use util::{merge_non_null_json_value_into, RangeExt, ResultExt as _};
+use util::{
+    channel::RELEASE_CHANNEL_NAME, merge_non_null_json_value_into, RangeExt, ResultExt as _,
+};
 
 /// A value that can be defined as a user setting.
 ///
@@ -195,6 +197,15 @@ impl SettingsStore {
                 .log_err()
             {
                 user_values_stack = vec![user_settings];
+            }
+
+            if let Some(release_settings) = &self.raw_user_settings.get(&*RELEASE_CHANNEL_NAME) {
+                if let Some(release_settings) = setting_value
+                    .deserialize_setting(&release_settings)
+                    .log_err()
+                {
+                    user_values_stack.push(release_settings);
+                }
             }
 
             if let Some(setting) = setting_value
@@ -484,6 +495,15 @@ impl SettingsStore {
             }
         }
 
+        for release_stage in ["dev", "nightly", "stable", "preview"] {
+            let schema = combined_schema.schema.clone();
+            combined_schema
+                .schema
+                .object()
+                .properties
+                .insert(release_stage.to_string(), schema.into());
+        }
+
         serde_json::to_value(&combined_schema).unwrap()
     }
 
@@ -507,6 +527,16 @@ impl SettingsStore {
             {
                 user_settings_stack.push(user_settings);
                 paths_stack.push(None);
+            }
+
+            if let Some(release_settings) = &self.raw_user_settings.get(&*RELEASE_CHANNEL_NAME) {
+                if let Some(release_settings) = setting_value
+                    .deserialize_setting(&release_settings)
+                    .log_err()
+                {
+                    user_settings_stack.push(release_settings);
+                    paths_stack.push(None);
+                }
             }
 
             // If the global settings file changed, reload the global value for the field.
