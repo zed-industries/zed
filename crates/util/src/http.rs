3df7da236d54c0011ae85297e76422bf5a5da486
Conrad Irwin <conrad.irwin@gmail.com>
2024-01-31 21:40:12 -0700

    Also add proxy to zed http client (#7184)
    Follow up to #6765 because I couldn't figure out how to add to that PR.
    
    Release Notes:
    
    - N/A

--- crates/util/src/http.rs
+++ crates/util/src/http.rs
@@ -44,6 +44,7 @@ pub fn zed_client(zed_host: &str) -> Arc<ZedHttpClient> {
             isahc::HttpClient::builder()
                 .connect_timeout(Duration::from_secs(5))
                 .low_speed_timeout(100, Duration::from_secs(5))
+                .proxy(http_proxy_from_env())
                 .build()
                 .unwrap(),
         ),
