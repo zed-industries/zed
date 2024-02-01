acd6bf2f10fa3056879ce6908e1346c8e65eb947
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2024-02-01 18:17:02 +0300

    cleanup

--- crates/zed/src/languages/dart.rs
+++ crates/zed/src/languages/dart.rs
@@ -3,7 +3,6 @@ use async_trait::async_trait;
 use language::{LanguageServerName, LspAdapter, LspAdapterDelegate};
 use lsp::LanguageServerBinary;
 use std::{any::Any, path::PathBuf};
-// use util::ResultExt;
 use futures::{StreamExt};
 
 pub struct DartLanguageServer;
@@ -39,17 +38,10 @@ impl LspAdapter for DartLanguageServer {
         _: PathBuf,
         _: &dyn LspAdapterDelegate,
     ) -> Option<LanguageServerBinary> {
-        // get_cached_server_binary(container_dir).await
-
-        // println!("{:?}", container_dir);
         Some(LanguageServerBinary {
             path: "dart".into(),
             arguments: vec!["language-server".into(), "--protocol=lsp".into()],
         })
-        // Some(LanguageServerBinary {
-        //     path: "typescript-language-server".into(),
-        //     arguments: vec!["--stdio".into()],
-        // })
     }
 
     fn can_be_reinstalled(&self) -> bool {
@@ -58,29 +50,5 @@ impl LspAdapter for DartLanguageServer {
 
     async fn installation_test_binary(&self, _: PathBuf) -> Option<LanguageServerBinary> {
         None
-        //     get_cached_server_binary(container_dir)
-        //         .await
-        //         .map(|mut binary| {
-        //             binary.arguments = vec!["--help".into()];
-        //             binary
-        //         })
     }
 }
-
-// async fn get_cached_server_binary(container_dir: PathBuf) -> Option<LanguageServerBinary> {
-//     (|| async move {
-//         let mut last = None;
-//         let mut entries = smol::fs::read_dir(&container_dir).await?;
-//         while let Some(entry) = entries.next().await {
-//             last = Some(entry?.path());
-//         }
-//
-//         anyhow::Ok(LanguageServerBinary {
-//             path: last.ok_or_else(|| anyhow!("no cached binary"))?,
-//             arguments: Default::default(),
-//         })
-//     })()
-//         .await
-//         .log_err()
-// }
-
