94594f8396e4fdf344c98d79552d13a4ed3a9038
Thorsten Ball <mrnugget@gmail.com>
2024-02-02 10:23:14 +0100

    Revert "Avoid excessive blocking of main thread when rendering in direct mode (#7253)"
    This reverts commit 020c38a8916c063cba36c2c88a69b5e287269d5a.

--- crates/gpui/src/platform/mac/metal_renderer.rs
+++ crates/gpui/src/platform/mac/metal_renderer.rs
@@ -314,7 +314,7 @@ impl MetalRenderer {
         command_buffer.commit();
         self.sprite_atlas.clear_textures(AtlasTextureKind::Path);
 
-        command_buffer.wait_until_scheduled();
+        command_buffer.wait_until_completed();
         drawable.present();
     }
 
