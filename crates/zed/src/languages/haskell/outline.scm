f1953b567e8b25fc1679c06ed6c5e57610847326
Pseudomata <pseudomata@proton.me>
2024-02-03 12:16:20 -0500

    Add `outline.scm` for Haskell
    Enabled buffer symbol search in Haskell projects.

--- crates/zed/src/languages/haskell/outline.scm
+++ crates/zed/src/languages/haskell/outline.scm
@@ -1,1 +1,22 @@
+(adt
+  "data" @context
+  name: (type) @name) @item
 
+(type_alias
+  "type" @context
+  name: (type) @name) @item
+
+(newtype
+  "newtype" @context
+  name: (type) @name) @item
+
+(signature
+  name: (variable) @name) @item
+
+(class
+  "class" @context
+  (class_head) @name) @item
+
+(foreign_import
+  "foreign" @context
+  (impent) @name) @item
