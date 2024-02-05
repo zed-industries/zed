f00f173016667e615739ac2c73958550a0f95bf8
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2024-02-05 16:09:05 +0300

    spelling

--- crates/zed/src/languages/dart/highlights.scm
+++ crates/zed/src/languages/dart/highlights.scm
@@ -137,7 +137,7 @@
   (hex_integer_literal)
   (decimal_integer_literal)
   (decimal_floating_point_literal)
-  ; TODO: inaccessbile nodes
+  ; TODO: inaccessible nodes
   ; (octal_integer_literal)
   ; (hex_floating_point_literal)
   ] @number
