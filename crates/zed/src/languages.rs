9561a32abac67a8f32547872889619b2f7394660
h3mosphere <h3mosphere@proton.me>
2024-02-08 14:58:38 +1100

    disable purescript fully, otherwise causing a linking error on main

--- crates/zed/src/languages.rs
+++ crates/zed/src/languages.rs
@@ -98,7 +98,7 @@ pub fn init(
         ),
         ("php", tree_sitter_php::language_php()),
         ("proto", tree_sitter_proto::language()),
-        ("purescript", tree_sitter_purescript::language()),
+        // ("purescript", tree_sitter_purescript::language()),
         ("python", tree_sitter_python::language()),
         ("racket", tree_sitter_racket::language()),
         ("ruby", tree_sitter_ruby::language()),
