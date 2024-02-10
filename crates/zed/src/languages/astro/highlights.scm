e0ecbbf496eebe6941b4ad621787105b2281107a
Alvaro <alvgaona@gmail.com>
2024-02-10 20:38:06 -0300

    fix: update zed Cargo and astro scheme

--- crates/zed/src/languages/astro/highlights.scm
+++ crates/zed/src/languages/astro/highlights.scm
@@ -1,16 +1,25 @@
-; inherits: html
-"---" @punctuation.delimiter
+(tag_name) @tag
+(erroneous_end_tag_name) @keyword
+(doctype) @constant
+(attribute_name) @property
+(attribute_value) @string
+(comment) @comment
+
+[
+  (attribute_value)
+  (quoted_attribute_value)
+] @string
+
+"=" @operator
 
 [
   "{"
   "}"
-] @punctuation.special
+] @punctuation.bracket
 
-((start_tag
-  (tag_name) @type))
-
-((end_tag
-  (tag_name) @type))
-
-((erroneous_end_tag
-  (erroneous_end_tag_name) @type))
+[
+  "<"
+  ">"
+  "</"
+  "/>"
+] @tag.delimiter
--- crates/zed/src/languages/astro/injections.scm
+++ crates/zed/src/languages/astro/injections.scm
@@ -12,12 +12,5 @@
   (#set! "language" "typescript"))
 
 (style_element
-  (start_tag
-    (attribute
-      (attribute_name) @_lang_attr
-      (quoted_attribute_value
-        (attribute_value) @_lang_value)))
   (raw_text) @content
-  (#eq? @_lang_attr "lang")
-  (#eq? @_lang_value "scss")
-  (#set! "language" "scss"))
+  (#set! "language" "css"))
