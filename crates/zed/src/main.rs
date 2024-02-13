41c1806d3e6315da00036bf6c46f8d16818b406f
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-02-12 16:24:49 -0800

    Rename extensions crate to extensions_ui
    Co-authored-by: Marshall <marshall@zed.dev>
    Co-authored-by: Carlos <foxkdev@gmail.com>

--- crates/zed/src/main.rs
+++ crates/zed/src/main.rs
@@ -261,7 +261,7 @@ fn main() {
         feedback::init(cx);
         markdown_preview::init(cx);
         welcome::init(cx);
-        extensions::init(cx);
+        extensions_ui::init(cx);
 
         cx.set_menus(app_menus());
         initialize_workspace(app_state.clone(), cx);
