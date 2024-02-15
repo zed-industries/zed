d79ea4f72d61aab4cd447de3817a5de863612a90
rpfaeffle <67913738+rpfaeffle@users.noreply.github.com>
2024-02-15 18:06:04 +0100

    Add regex syntax highlighting for JS and TS

--- crates/languages/src/lib.rs
+++ crates/languages/src/lib.rs
@@ -107,6 +107,7 @@ pub fn init(
         ("purescript", tree_sitter_purescript::language()),
         ("python", tree_sitter_python::language()),
         ("racket", tree_sitter_racket::language()),
+        ("regex", tree_sitter_regex::language()),
         ("ruby", tree_sitter_ruby::language()),
         ("rust", tree_sitter_rust::language()),
         ("scheme", tree_sitter_scheme::language()),
@@ -312,6 +313,7 @@ pub fn init(
     );
     language!("scheme");
     language!("racket");
+    language!("regex");
     language!("lua", vec![Arc::new(lua::LuaLspAdapter)]);
     language!(
         "yaml",
