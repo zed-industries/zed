87fef65c094eae5a1315e8649dd94b7412e9490c
Max <contact@slymax.com>
2024-02-21 17:03:16 +0100

    fix alignment of traffic lights

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -90,7 +90,7 @@ pub fn build_window_options(
         titlebar: Some(TitlebarOptions {
             title: None,
             appears_transparent: true,
-            traffic_light_position: Some(point(px(8.), px(8.))),
+            traffic_light_position: Some(point(px(8.), px(10.))),
         }),
         center: false,
         focus: false,
