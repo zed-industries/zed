9b44ba9382c079eef35fcf70ec7ce4b293cba9f6
Paul Berg <naydex.mc+github@gmail.com>
2024-02-24 02:03:00 +0100

    linux/text: fix invalid span creation (#8286)
    This fixes a crash when showing completions.
    
    Release Notes:
    
    - N/A

--- crates/gpui/src/platform/linux/text_system.rs
+++ crates/gpui/src/platform/linux/text_system.rs
@@ -329,7 +329,7 @@ impl LinuxTextSystemState {
             let font = &self.fonts[run.font_id.0];
             let font = self.font_system.db().face(font.id()).unwrap();
             attrs_list.add_span(
-                offs..run.len,
+                offs..offs + run.len,
                 Attrs::new()
                     .family(Family::Name(&font.families.first().unwrap().0))
                     .stretch(font.stretch)
