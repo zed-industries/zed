19f6c3fa10f4ffd2b49506a24e93851e6e645d81
Sai Gokula Krishnan <saigklkrishnan@gmail.com>
2024-02-25 02:38:41 +0530

    Fixed systax highlighting issue

--- extensions/dart/languages/dart/indents.scm
+++ extensions/dart/languages/dart/indents.scm
@@ -1,7 +1,45 @@
 [
-  (if_statement)
-  (for_statement)
-] @indent
+  (class_body)
+  (function_body)
+  (function_expression_body)
+  (declaration
+    (initializers))
+  (switch_block)
+  (formal_parameter_list)
+  (formal_parameter)
+  (list_literal)
+  (return_statement)
+  (arguments)
+  (try_statement)
+] @indent.begin
 
-(_ "{" "}" @end) @indent
-(_ "(" ")" @end) @indent
+(switch_block
+  (_) @indent.begin
+  (#set! indent.immediate 1)
+  (#set! indent.start_at_same_line 1))
+
+[
+  (switch_statement_case)
+  (switch_statement_default)
+] @indent.branch
+
+[
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+] @indent.branch
+
+"}" @indent.end
+
+(return_statement
+  ";" @indent.end)
+
+(break_statement
+  ";" @indent.end)
+
+(comment) @indent.ignore
+
+(if_statement) @indent.auto
