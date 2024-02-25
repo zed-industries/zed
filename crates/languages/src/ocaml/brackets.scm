dbe1f48f95558aa5764c5ed0c87022291fb5800b
rmu <r.muhairi@pm.me>
2024-02-25 04:06:25 +0400

    ocaml: Small query improvements and fix autoclose brackets (#7769)
    Turns out auto-closing words was a bad idea. win**do**w, **struct**ure,
    **sig**n and so on
    
    They don't serve any purpose in `config.toml` nor `brackets.scm` at this
    point, so I removed them>
    
    Release Notes:
    - N/A

--- crates/languages/src/ocaml/brackets.scm
+++ crates/languages/src/ocaml/brackets.scm
@@ -5,8 +5,3 @@
 ("<" @open ">" @close)
 ("\"" @open "\"" @close)
 
-("begin" @open "end" @close)
-("struct" @open "end" @close)
-("sig" @open "end" @close)
-("object" @open "end" @close)
-("do" @open "done" @close)
