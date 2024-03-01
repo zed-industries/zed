7b72e894fe1588d493c5b725b8f062bae0e2c16c
Joel Selvaraj <joelselvaraj.oss@gmail.com>
2024-03-01 14:54:20 -0600

    linux: wayland: implement cursor style handling - don't panic

--- crates/gpui/src/platform/linux/wayland/cursor.rs
+++ crates/gpui/src/platform/linux/wayland/cursor.rs
@@ -1,3 +1,4 @@
+use wayland_backend::client::InvalidId;
 use wayland_client::{Connection, QueueHandle};
 use wayland_client::protocol::wl_compositor::WlCompositor;
 use wayland_client::protocol::wl_pointer::WlPointer;
@@ -7,18 +8,17 @@ use wayland_cursor::{CursorImageBuffer, CursorTheme};
 use crate::platform::linux::wayland::WaylandClientState;
 
 pub(crate) struct Cursor {
-    theme: CursorTheme,
+    theme: Result<CursorTheme, InvalidId>,
     current_icon_name: String,
     surface: WlSurface,
     serial_id: u32,
 }
 
 impl Cursor {
-    pub fn new(connection: &Connection, compositor: &WlCompositor, qh: &QueueHandle<WaylandClientState>, shm: &WlShm, size: u32) -> Self {
-        let mut cursor_theme = CursorTheme::load(&connection, shm.clone(), size)
-            .expect("Could not load cursor theme");
+    pub fn new(connection: &Connection, compositor: &WlCompositor,
+               qh: &QueueHandle<WaylandClientState>, shm: &WlShm, size: u32) -> Self {
         Self {
-            theme: cursor_theme,
+            theme: CursorTheme::load(&connection, shm.clone(), size),
             current_icon_name: "".to_string(),
             surface: compositor.create_surface(qh, ()),
             serial_id: 0,
@@ -31,18 +31,24 @@ impl Cursor {
 
     pub fn set_icon(&mut self, wl_pointer: &WlPointer, cursor_icon_name: String) {
         if self.current_icon_name != cursor_icon_name {
-            let cursor = self.theme.get_cursor(cursor_icon_name.as_str()).expect("Cursor not provided by theme");
-            let buffer: &CursorImageBuffer = &cursor[0];
-            let (width, height) = buffer.dimensions();
-            let (hot_x, hot_y) = buffer.hotspot();
+            if self.theme.is_ok() {
+                if let Some(cursor) = self.theme.as_mut().unwrap().get_cursor(&cursor_icon_name) {
+                    let buffer: &CursorImageBuffer = &cursor[0];
+                    let (width, height) = buffer.dimensions();
+                    let (hot_x, hot_y) = buffer.hotspot();
 
-            wl_pointer.set_cursor(self.serial_id, Some(&self.surface), hot_x as i32, hot_y as i32);
+                    wl_pointer.set_cursor(self.serial_id, Some(&self.surface), hot_x as i32, hot_y as i32);
+                    self.surface.attach(Some(&buffer), 0, 0);
+                    self.surface.damage(0, 0, width as i32, height as i32);
+                    self.surface.commit();
 
-            self.surface.attach(Some(&buffer), 0, 0);
-            self.surface.damage(0, 0, width as i32, height as i32);
-            self.surface.commit();
-
-            self.current_icon_name = cursor_icon_name;
+                    self.current_icon_name = cursor_icon_name;
+                } else {
+                    log::warn!("Linux: Wayland: Unable to get cursor icon: {}", cursor_icon_name);
+                }
+            } else {
+                log::warn!("Linux: Wayland: Unable to load cursor themes");
+            }
         }
     }
 }
