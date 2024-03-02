bc9059fe42a84b635b8ad308ea1b80891f4a1c8f
Ivan Buryak <ivan.buryak@gmail.com>
2024-03-02 13:58:52 +0500

    fixup! Add outlines for css

--- crates/languages/src/css.rs
+++ crates/languages/src/css.rs
@@ -135,7 +135,7 @@ mod tests {
 
     #[gpui::test]
     async fn test_outline(cx: &mut TestAppContext) {
-        let language = crate::language("css", tree_sitter_css::language(), None).await;
+        let language = crate::language("css", tree_sitter_css::language());
 
         let text = r#"
             /* Import statement */
