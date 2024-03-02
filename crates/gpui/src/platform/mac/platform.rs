5d098681028e9e189564f72a94e404bb84c5c55b
Liam Murphy <liampm32@gmail.com>
2024-03-02 11:53:03 +1100

    Fix unsafe precondition violation when building with nightly `rustc`
    Fixes #8658

--- crates/gpui/src/platform/mac/platform.rs
+++ crates/gpui/src/platform/mac/platform.rs
@@ -5,7 +5,7 @@ use crate::{
     MenuItem, PathPromptOptions, Platform, PlatformDisplay, PlatformInput, PlatformTextSystem,
     PlatformWindow, Result, SemanticVersion, Task, WindowAppearance, WindowOptions,
 };
-use anyhow::anyhow;
+use anyhow::{anyhow, bail};
 use block::ConcreteBlock;
 use cocoa::{
     appkit::{
@@ -685,6 +685,9 @@ impl Platform for MacPlatform {
                 Err(anyhow!("app is not running inside a bundle"))
             } else {
                 let version: id = msg_send![bundle, objectForInfoDictionaryKey: ns_string("CFBundleShortVersionString")];
+                if version.is_null() {
+                    bail!("bundle does not have version");
+                }
                 let len = msg_send![version, lengthOfBytesUsingEncoding: NSUTF8StringEncoding];
                 let bytes = version.UTF8String() as *const u8;
                 let version = str::from_utf8(slice::from_raw_parts(bytes, len)).unwrap();
