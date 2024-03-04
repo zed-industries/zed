0e60d5231e934f54b054bc2248df263965b86c8e
Marshall Bowers <elliott.codes@gmail.com>
2024-03-04 11:08:06 -0500

    Remove rule suppression

--- tooling/xtask/src/main.rs
+++ tooling/xtask/src/main.rs
@@ -85,7 +85,6 @@ fn run_clippy(args: ClippyArgs) -> Result<()> {
         "clippy::cast_abs_to_unsigned",
         "clippy::cmp_owned",
         "clippy::crate_in_macro_def",
-        "clippy::derivable_impls",
         "clippy::derive_ord_xor_partial_ord",
         "clippy::eq_op",
         "clippy::implied_bounds_in_impls",
