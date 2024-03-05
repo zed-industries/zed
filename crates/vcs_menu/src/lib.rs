fae5e83d93c9dfc794e0d80e4f9df95b78a576b8
Jason Lee <huacnlee@gmail.com>
2024-03-05 17:32:57 +0800

    Fix all Picker Item cursor to use `Pointer`. (#8877)
    Before:
    
    https://github.com/zed-industries/zed/assets/5518/84874858-7847-4fa4-b7a3-41ecc65a2f7d
    
    After: 
    
    https://github.com/zed-industries/zed/assets/5518/d395ea96-aa26-4de1-8bfc-73cc43ee75cf
    
    
    Release Notes:
    
    - Made picker items to use `Pointer` cursor style

--- crates/vcs_menu/src/lib.rs
+++ crates/vcs_menu/src/lib.rs
@@ -67,7 +67,6 @@ impl Render for BranchList {
     fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
         v_flex()
             .w(rems(self.rem_width))
-            .cursor_pointer()
             .child(self.picker.clone())
             .on_mouse_down_out(cx.listener(|this, _, cx| {
                 this.picker.update(cx, |this, cx| {
