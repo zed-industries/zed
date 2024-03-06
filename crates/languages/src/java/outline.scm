7b5cdfe57412297257e9fab955c5ebe0cf778f78
Valentine Briese <valentinegb@icloud.com>
2024-03-05 18:40:28 -0800

    Add method declarations to Java outline

--- crates/languages/src/java/outline.scm
+++ crates/languages/src/java/outline.scm
@@ -25,10 +25,52 @@
     name: (_) @name
     parameters: (formal_parameters) @context) @item
 
+; TODO: definitely needs more testing in projects with methods that actually use
+;       all of these rules
 (method_declaration
-    type: (type_identifier)? @context
+    (modifiers _+ @context)?
+    (_
+      type_parameters: (_) @context
+      [
+        (marker_annotation) @context
+        (annotation) @context
+      ]*
+    )?
+    type: (_) @context
     name: (_) @name
-    parameters: (formal_parameters) @context) @item
+    ; TODO: fix multiline parameters causing space between parenthesis
+    parameters: (formal_parameters
+      "(" @context
+      [
+        (receiver_parameter) @context
+        (
+          (
+            (receiver_parameter) @context
+            "," @context
+          )?
+          (
+            [
+              (formal_parameter) @context
+              (spread_parameter) @context
+            ]
+            (
+              "," @context
+              [
+                (formal_parameter) @context
+                (spread_parameter) @context
+              ]
+            )*
+          )?
+        )
+      ]
+      ")" @context)
+    dimensions: (dimensions
+      (
+        (_)* @context
+        "[" @context
+        "]" @context
+      )+)?
+    (throws)? @context) @item
 
 (record_declaration
     (modifiers)? @context
