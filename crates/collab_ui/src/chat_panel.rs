7a81d95beffcd9aefbb6c7adf53e74934e785bf5
evrsen <146845123+evrsen@users.noreply.github.com>
2024-03-08 00:55:59 +0100

    Smaller datetime label size

--- crates/collab_ui/src/chat_panel.rs
+++ crates/collab_ui/src/chat_panel.rs
@@ -500,13 +500,17 @@ impl ChatPanel {
                                         .child(Label::new(message.sender.github_login.clone())),
                                 )
                                 .child(
-                                    Label::new(time_format::format_localized_timestamp(
-                                        OffsetDateTime::now_utc(),
-                                        message.timestamp,
-                                        self.local_timezone,
-                                    ))
-                                    .size(LabelSize::Small)
-                                    .color(Color::Muted),
+                                    div()
+                                        .child(
+                                            Label::new(time_format::format_localized_timestamp(
+                                                OffsetDateTime::now_utc(),
+                                                message.timestamp,
+                                                self.local_timezone,
+                                            ))
+                                            .size(LabelSize::XSmall)
+                                            .color(Color::Muted),
+                                        )
+                                        .pt_0p5(),
                                 ),
                         )
                     })
