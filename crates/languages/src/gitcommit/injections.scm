b475eea1298b13813894fe39e3383e31a7258258
d1y <chenhonzhou@gmail.com>
2024-03-08 02:07:11 +0800

    remove git_commit syntax highlighting
    Fallback to extension https://github.com/zed-industries/extensions/pull/307
    
    Co-Authored-By: William Desportes <williamdes@wdes.fr>
    Co-Authored-By: Kirill Bulatov <mail4score@gmail.com>
    Co-Authored-By: Marshall Bowers <1486634+maxdeviant@users.noreply.github.com>

--- crates/languages/src/gitcommit/injections.scm
+++
@@ -1,8 +0,0 @@
-((diff) @injection.content
- (#set! injection.combined)
- (#set! injection.language "diff"))
-
-((rebase_command) @injection.content
- (#set! injection.combined)
- (#set! injection.language "git_rebase"))
-
