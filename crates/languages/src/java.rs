f2456842963243cf11a29e702cfacc5da291093d
Valentine Briese <valentinegb@icloud.com>
2024-03-07 20:20:26 -0800

    Remove unnecessary newline from error message in `JavaLspAdapter`

--- crates/languages/src/java.rs
+++ crates/languages/src/java.rs
@@ -34,7 +34,7 @@ impl LspAdapter for JavaLspAdapter {
             .assets
             .iter()
             .find(|asset| asset.name == asset_name)
-            .ok_or_else(|| anyhow!("no asset found matching {:?} \n", asset_name))?;
+            .ok_or_else(|| anyhow!("no asset found matching {:?}", asset_name))?;
         let version = GitHubLspBinaryVersion {
             name: release.tag_name,
             url: asset.browser_download_url.clone(),
