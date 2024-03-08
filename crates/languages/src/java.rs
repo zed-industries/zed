a8b97dbbf744cc3a0acd3252a9629cbdaa906012
Valentine Briese <valentinegb@icloud.com>
2024-03-07 20:23:36 -0800

    Simplify `JavaLspAdapter::fetch_latest_server_version` return value

--- crates/languages/src/java.rs
+++ crates/languages/src/java.rs
@@ -35,12 +35,11 @@ impl LspAdapter for JavaLspAdapter {
             .iter()
             .find(|asset| asset.name == asset_name)
             .ok_or_else(|| anyhow!("no asset found matching {:?}", asset_name))?;
-        let version = GitHubLspBinaryVersion {
+
+        Ok(Box::new(GitHubLspBinaryVersion {
             name: release.tag_name,
             url: asset.browser_download_url.clone(),
-        };
-
-        Ok(Box::new(version) as Box<_>)
+        }))
     }
 
     async fn fetch_server_binary(
