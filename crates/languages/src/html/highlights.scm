d9c2a5f4c20e2e2bef6731fa874e6ebcdd67014b
Max <contact@slymax.com>
2024-03-09 16:16:00 +0100

    improve html syntax highlighting

--- crates/languages/src/html/highlights.scm
+++ crates/languages/src/html/highlights.scm
@@ -1,11 +1,13 @@
 (tag_name) @keyword
 (erroneous_end_tag_name) @keyword
 (doctype) @constant
-(attribute_name) @property
+(attribute_name) @attribute
 (attribute_value) @string
 (comment) @comment
+(text) @text
 
 "=" @operator
+"\"" @punctuation.delimiter
 
 [
   "<"
