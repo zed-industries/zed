2db26f0732bdd8107843e9517c1315290fc0358b
the-jasoney <jasoney.music@gmail.com>
2024-03-08 18:50:42 -0800

    cargo fmt

--- crates/gpui/src/platform/windows/platform.rs
+++ crates/gpui/src/platform/windows/platform.rs
@@ -5,7 +5,7 @@ use std::{
     cell::{Cell, RefCell},
     collections::HashSet,
     ffi::{c_uint, c_void, OsString},
-    os::windows::ffi::OsStrExt,
+    os::windows::ffi::{OsStrExt, OsStringExt},
     path::{Path, PathBuf},
     rc::Rc,
     sync::Arc,
@@ -21,7 +21,7 @@ use parking_lot::Mutex;
 use time::UtcOffset;
 use util::{ResultExt, SemanticVersion};
 use windows::{
-    core::{HSTRING, PCWSTR, HRESULT, IUnknown, PWSTR},
+    core::{IUnknown, HRESULT, HSTRING, PCWSTR, PWSTR},
     Wdk::System::SystemServices::RtlGetVersion,
     Win32::{
         Foundation::{CloseHandle, BOOL, HANDLE, HWND, LPARAM, TRUE},
@@ -35,9 +35,9 @@ use windows::{
         UI::{
             Input::KeyboardAndMouse::GetDoubleClickTime,
             Shell::{
-                FileSaveDialog, IFileSaveDialog, SHCreateItemFromParsingName,
-                ShellExecuteW, FileOpenDialog, IFileOpenDialog, IShellItem,
-                FILEOPENDIALOGOPTIONS, FOS_ALLOWMULTISELECT, FOS_FILEMUSTEXIST, FOS_PICKFOLDERS, SIGDN_FILESYSPATH
+                FileOpenDialog, FileSaveDialog, IFileOpenDialog, IFileSaveDialog, IShellItem,
+                SHCreateItemFromParsingName, ShellExecuteW, FILEOPENDIALOGOPTIONS,
+                FOS_ALLOWMULTISELECT, FOS_FILEMUSTEXIST, FOS_PICKFOLDERS, SIGDN_FILESYSPATH,
             },
             WindowsAndMessaging::{
                 DispatchMessageW, EnumThreadWindows, LoadImageW, PeekMessageW, PostQuitMessage,
@@ -345,63 +345,69 @@ impl Platform for WindowsPlatform {
     fn prompt_for_paths(&self, options: PathPromptOptions) -> Receiver<Option<Vec<PathBuf>>> {
         let (tx, rx) = oneshot::channel();
 
-        self.foreground_executor().spawn(async move {
-            let tx = Cell::new(Some(tx));
+        self.foreground_executor()
+            .spawn(async move {
+                let tx = Cell::new(Some(tx));
 
-            // create file open dialog
-            let folder_dialog: IFileOpenDialog = unsafe {
-                CoCreateInstance::<std::option::Option<&IUnknown>, IFileOpenDialog>(
-                    &FileOpenDialog,
-                    None,
-                    CLSCTX_ALL
-                ).unwrap()
-            };
+                // create file open dialog
+                let folder_dialog: IFileOpenDialog = unsafe {
+                    CoCreateInstance::<std::option::Option<&IUnknown>, IFileOpenDialog>(
+                        &FileOpenDialog,
+                        None,
+                        CLSCTX_ALL,
+                    )
+                    .unwrap()
+                };
 
-            // dialog options
-            let mut dialog_options: FILEOPENDIALOGOPTIONS = FOS_FILEMUSTEXIST;
-            if options.multiple {
-                dialog_options |= FOS_ALLOWMULTISELECT;
-            }
-            if options.directories {
-                dialog_options |= FOS_PICKFOLDERS;
-            }
+                // dialog options
+                let mut dialog_options: FILEOPENDIALOGOPTIONS = FOS_FILEMUSTEXIST;
+                if options.multiple {
+                    dialog_options |= FOS_ALLOWMULTISELECT;
+                }
+                if options.directories {
+                    dialog_options |= FOS_PICKFOLDERS;
+                }
 
-            unsafe {
-                folder_dialog.SetOptions(dialog_options).unwrap();
-                folder_dialog.SetTitle(&HSTRING::from(OsString::from("Select a folder"))).unwrap();
-            }
+                unsafe {
+                    folder_dialog.SetOptions(dialog_options).unwrap();
+                    folder_dialog
+                        .SetTitle(&HSTRING::from(OsString::from("Select a folder")))
+                        .unwrap();
+                }
 
-            let hr = unsafe { folder_dialog.Show(None) };
+                let hr = unsafe { folder_dialog.Show(None) };
 
-            if hr.is_err() {
-                if hr.unwrap_err().code() == HRESULT(0x800704C7u32 as i32)  { // user canceled error
-                    if let Some(tx) = tx.take() {
-                        tx.send(None).unwrap();
+                if hr.is_err() {
+                    if hr.unwrap_err().code() == HRESULT(0x800704C7u32 as i32) {
+                        // user canceled error
+                        if let Some(tx) = tx.take() {
+                            tx.send(None).unwrap();
+                        }
+                        return;
                     }
-                    return;
                 }
-            }
 
-            let mut results = unsafe { folder_dialog.GetResults().unwrap() };
+                let mut results = unsafe { folder_dialog.GetResults().unwrap() };
 
-            let mut paths: Vec<PathBuf> = Vec::new();
-            for i in 0..unsafe { results.GetCount().unwrap() } {
-                let mut item: IShellItem = unsafe { results.GetItemAt(i).unwrap() };
-                let mut path: PWSTR = unsafe { item.GetDisplayName(SIGDN_FILESYSPATH).unwrap() };
-                let mut path_os_string = OsString::from_wide(unsafe { path.as_wide() });
+                let mut paths: Vec<PathBuf> = Vec::new();
+                for i in 0..unsafe { results.GetCount().unwrap() } {
+                    let mut item: IShellItem = unsafe { results.GetItemAt(i).unwrap() };
+                    let mut path: PWSTR =
+                        unsafe { item.GetDisplayName(SIGDN_FILESYSPATH).unwrap() };
+                    let mut path_os_string = OsString::from_wide(unsafe { path.as_wide() });
 
-                paths.push(PathBuf::from(path_os_string));
-            }
-
-
-            if let Some(tx) = tx.take() {
-                if paths.len() == 0 {
-                    tx.send(None).unwrap();
-                } else {
-                    tx.send(Some(paths)).unwrap();
+                    paths.push(PathBuf::from(path_os_string));
                 }
-            }
-        }).detach();
+
+                if let Some(tx) = tx.take() {
+                    if paths.len() == 0 {
+                        tx.send(None).unwrap();
+                    } else {
+                        tx.send(Some(paths)).unwrap();
+                    }
+                }
+            })
+            .detach();
 
         rx
     }
