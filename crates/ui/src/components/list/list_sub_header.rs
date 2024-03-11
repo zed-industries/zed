912c08d297f9d387e1f74a5dd7870df747261198
Marshall Bowers <elliott.codes@gmail.com>
2024-03-11 16:14:01 -0400

    Inset `ContextMenu` headers

--- crates/ui/src/components/list/list_sub_header.rs
+++ crates/ui/src/components/list/list_sub_header.rs
@@ -21,6 +21,11 @@ impl ListSubHeader {
         self.start_slot = left_icon;
         self
     }
+
+    pub fn inset(mut self, inset: bool) -> Self {
+        self.inset = inset;
+        self
+    }
 }
 
 impl RenderOnce for ListSubHeader {
