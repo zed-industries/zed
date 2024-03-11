2e5e22b53920a1a778310b980b0eb64329a2e5fc
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-03-10 20:24:09 -0700

    Rename project_core crate to worktree
    Make the crate more exclusively about worktree stuff. Separate project
    settings from worktree settings, move the project settings back into
    the project crate.

--- crates/project/src/project_settings.rs
+++ crates/project/src/project_settings.rs
@@ -20,25 +20,6 @@ pub struct ProjectSettings {
     /// Configuration for Git-related features
     #[serde(default)]
     pub git: GitSettings,
-
-    /// Completely ignore files matching globs from `file_scan_exclusions`
-    ///
-    /// Default: [
-    ///   "**/.git",
-    ///   "**/.svn",
-    ///   "**/.hg",
-    ///   "**/CVS",
-    ///   "**/.DS_Store",
-    ///   "**/Thumbs.db",
-    ///   "**/.classpath",
-    ///   "**/.settings"
-    /// ]
-    #[serde(default)]
-    pub file_scan_exclusions: Option<Vec<String>>,
-
-    /// Treat the files matching these globs as `.env` files.
-    /// Default: [ "**/.env*" ]
-    pub private_files: Option<Vec<String>>,
 }
 
 #[derive(Copy, Clone, Debug, Default, Serialize, Deserialize, JsonSchema)]
