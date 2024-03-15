dd33330648ffc0c600a96c24d2aa8398e985d1ad
Conrad Irwin <conrad.irwin@gmail.com>
2024-03-15 13:04:48 -0600

    Finish migration to role instead of `is_admin` (#9414)
    Release Notes:
    
    - Fixed a bug signing in for five users
    ([#4323](https://github.com/zed-industries/zed/issues/4323)).

--- crates/collab/migrations.sqlite/20221109000000_test_schema.sql
+++ crates/collab/migrations.sqlite/20221109000000_test_schema.sql
@@ -198,7 +198,7 @@ CREATE TABLE "channels" (
     "name" VARCHAR NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "visibility" VARCHAR NOT NULL,
-    "parent_path" TEXT,
+    "parent_path" TEXT NOT NULL,
     "requires_zed_cla" BOOLEAN NOT NULL DEFAULT FALSE
 );
 
@@ -237,8 +237,7 @@ CREATE TABLE "channel_members" (
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "channel_id" INTEGER NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
     "user_id" INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    "admin" BOOLEAN NOT NULL DEFAULT false,
-    "role" VARCHAR,
+    "role" VARCHAR NOT NULL,
     "accepted" BOOLEAN NOT NULL DEFAULT false,
     "updated_at" TIMESTAMP NOT NULL DEFAULT now
 );
