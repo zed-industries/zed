f8d3d37990b6aa171b21dbb6dcbaaa595ab8c1c9
Mayfield <git@mayfieldiv.com>
2024-03-22 14:01:10 -0400

    optimize regex initialization in text search replacements using OnceLock

--- crates/project/src/search.rs
+++ crates/project/src/search.rs
@@ -10,9 +10,11 @@ use std::{
     io::{BufRead, BufReader, Read},
     ops::Range,
     path::Path,
-    sync::Arc,
+    sync::{Arc, OnceLock},
 };
 use util::paths::PathMatcher;
+
+static TEXT_REPLACEMENT_SPECIAL_CHARACTERS_REGEX: OnceLock<Regex> = OnceLock::new();
 
 #[derive(Clone, Debug)]
 pub struct SearchInputs {
@@ -231,15 +233,16 @@ impl SearchQuery {
                 regex, replacement, ..
             } => {
                 if let Some(replacement) = replacement {
-                    let replacement = Regex::new(r"\\\\|\\n|\\t").unwrap().replace_all(
-                        replacement,
-                        |c: &Captures| match c.get(0).unwrap().as_str() {
-                            r"\\" => "\\",
-                            r"\n" => "\n",
-                            r"\t" => "\t",
-                            x => unreachable!("Unexpected escape sequence: {}", x),
-                        },
-                    );
+                    let replacement = TEXT_REPLACEMENT_SPECIAL_CHARACTERS_REGEX
+                        .get_or_init(|| Regex::new(r"\\\\|\\n|\\t").unwrap())
+                        .replace_all(replacement, |c: &Captures| {
+                            match c.get(0).unwrap().as_str() {
+                                r"\\" => "\\",
+                                r"\n" => "\n",
+                                r"\t" => "\t",
+                                x => unreachable!("Unexpected escape sequence: {}", x),
+                            }
+                        });
                     Some(regex.replace(text, replacement))
                 } else {
                     None
