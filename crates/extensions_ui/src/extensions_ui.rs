60d4b392e7a3e5fef03ed1b4603667d72907d6d1
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-03-27 16:22:09 -0700

    Start work on "install other version"
    Co-authored-by: Marshall <marshall@zed.dev>

--- crates/extensions_ui/src/extensions_ui.rs
+++ crates/extensions_ui/src/extensions_ui.rs
@@ -8,16 +8,17 @@ use editor::{Editor, EditorElement, EditorStyle};
 use extension::{ExtensionManifest, ExtensionOperation, ExtensionStore};
 use fuzzy::{match_strings, StringMatchCandidate};
 use gpui::{
-    actions, canvas, uniform_list, AnyElement, AppContext, EventEmitter, FocusableView, FontStyle,
-    FontWeight, InteractiveElement, KeyContext, ParentElement, Render, Styled, Task, TextStyle,
-    UniformListScrollHandle, View, ViewContext, VisualContext, WeakView, WhiteSpace, WindowContext,
+    actions, canvas, uniform_list, AnyElement, AppContext, ClickEvent, DismissEvent, EventEmitter,
+    FocusableView, FontStyle, FontWeight, InteractiveElement, KeyContext, ParentElement, Point,
+    Render, Styled, Subscription, Task, TextStyle, UniformListScrollHandle, View, ViewContext,
+    VisualContext, WeakView, WhiteSpace, WindowContext,
 };
 use settings::Settings;
 use std::ops::DerefMut;
 use std::time::Duration;
 use std::{ops::Range, sync::Arc};
 use theme::ThemeSettings;
-use ui::{prelude::*, ToggleButton, Tooltip};
+use ui::{prelude::*, ContextMenu, ToggleButton, Tooltip};
 use util::ResultExt as _;
 use workspace::{
     item::{Item, ItemEvent},
@@ -112,6 +113,7 @@ pub struct ExtensionsPage {
     filtered_remote_extension_indices: Vec<usize>,
     query_editor: View<Editor>,
     query_contains_error: bool,
+    context_menu: Option<(View<ContextMenu>, Point<Pixels>, Subscription)>,
     _subscriptions: [gpui::Subscription; 2],
     extension_fetch_task: Option<Task<()>>,
 }
@@ -149,6 +151,7 @@ impl ExtensionsPage {
                 remote_extension_entries: Vec::new(),
                 query_contains_error: false,
                 extension_fetch_task: None,
+                context_menu: None,
                 _subscriptions: subscriptions,
                 query_editor,
             };
@@ -485,9 +488,63 @@ impl ExtensionsPage {
                             }
                         }))
                         .tooltip(move |cx| Tooltip::text(repository_url.clone(), cx)),
+                    )
+                    .child(
+                        IconButton::new(
+                            SharedString::from(format!("more-{}", extension.id)),
+                            IconName::Ellipsis,
+                        )
+                        .icon_color(Color::Accent)
+                        .icon_size(IconSize::Small)
+                        .style(ButtonStyle::Filled)
+                        .on_click(cx.listener({
+                            let extension_id = extension.id.clone();
+                            move |this, event: &ClickEvent, cx| {
+                                this.deploy_context_menu(
+                                    event.down.position,
+                                    extension_id.clone(),
+                                    cx,
+                                )
+                            }
+                        }))
+                        .tooltip(move |cx| Tooltip::text("More...", cx)),
                     ),
             )
     }
+
+    fn deploy_context_menu(
+        &mut self,
+        position: Point<Pixels>,
+        extension_id: Arc<str>,
+        cx: &mut ViewContext<Self>,
+    ) {
+        let this = cx.view().clone();
+        let context_menu = ContextMenu::build(cx, |context_menu, cx| {
+            context_menu.entry(
+                "Install Other Version",
+                None,
+                cx.handler_for(&this, move |this, cx| {
+                    this.show_extension_version_list(extension_id.clone(), cx)
+                }),
+            )
+        });
+
+        cx.focus_view(&context_menu);
+        let subscription =
+            cx.subscribe(&context_menu, |this, _, _: &DismissEvent, cx| {
+                if this.context_menu.as_ref().is_some_and(|context_menu| {
+                    context_menu.0.focus_handle(cx).contains_focused(cx)
+                }) {
+                    cx.focus_self();
+                }
+                this.context_menu.take();
+                cx.notify();
+            });
+
+        self.context_menu = Some((context_menu, position, subscription));
+    }
+
+    fn show_extension_version_list(&mut self, extension_id: Arc<str>, cx: &mut ViewContext<Self>) {}
 
     fn buttons_for_entry(
         &self,
