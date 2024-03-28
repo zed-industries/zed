ff685b299db35890c466382b36a09d2e311f5965
Marshall Bowers <elliott.codes@gmail.com>
2024-03-27 20:56:30 -0400

    Extract Zig support into an extension (#9893)
    This PR extracts Zig support into an extension and removes the built-in
    Zig support from Zed.
    
    There's a small workaround necessary in order for us to set the file
    permissions on the `zls` binary so that it can be run. Eventually we'll
    want to build this into the extension API, but for now we're just
    hard-coding it on the host side.
    
    Release Notes:
    
    - Removed built-in support for Zig, in favor of making it available as
    an extension. The Zig extension will be suggested for download when you
    open a `.zig` file.

--- crates/extensions_ui/src/extension_suggest.rs
+++ crates/extensions_ui/src/extension_suggest.rs
@@ -48,6 +48,7 @@ pub fn suggested_extension(file_extension_or_name: &str) -> Option<Arc<str>> {
                 ("swift", "swift"),
                 ("templ", "templ"),
                 ("wgsl", "wgsl"),
+                ("zig", "zig"),
             ]
             .into_iter()
             .map(|(name, file)| (file, name.into()))
