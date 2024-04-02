c3c9c4781f05b7f50eddc563baa23473ca8bccca
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-04-02 15:37:38 -0700

    Remove unnecessary .into() call
    Co-authored-by: Marshall <marshall@zed.dev>

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -10194,12 +10194,7 @@ impl CompletionProvider for Model<Project> {
         cx: &mut ViewContext<Editor>,
     ) -> Task<Result<Option<language::Transaction>>> {
         self.update(cx, |project, cx| {
-            project.apply_additional_edits_for_completion(
-                buffer,
-                completion.into(),
-                push_to_history,
-                cx,
-            )
+            project.apply_additional_edits_for_completion(buffer, completion, push_to_history, cx)
         })
     }
 }
