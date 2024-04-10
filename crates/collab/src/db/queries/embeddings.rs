661b8ca305e7f4f6a644ce7e6d7d9512371cd823
Nathan Sobo <nathan@zed.dev>
2024-04-10 09:39:12 -0600

    Test embedding queries

--- crates/collab/src/db/queries/embeddings.rs
+++ crates/collab/src/db/queries/embeddings.rs
@@ -9,18 +9,39 @@ impl Database {
         digests: &[Vec<u8>],
     ) -> Result<HashMap<Vec<u8>, Vec<f32>>> {
         self.weak_transaction(|tx| async move {
-            let mut db_embeddings = embedding::Entity::find()
-                .filter(embedding::Column::Provider.eq(provider).and(
-                    embedding::Column::Digest.is_in(digests.iter().map(|digest| digest.as_slice())),
-                ))
-                .stream(&*tx)
-                .await?;
+            let embeddings = {
+                let mut db_embeddings = embedding::Entity::find()
+                    .filter(
+                        embedding::Column::Provider.eq(provider).and(
+                            embedding::Column::Digest
+                                .is_in(digests.iter().map(|digest| digest.as_slice())),
+                        ),
+                    )
+                    .stream(&*tx)
+                    .await?;
 
-            let mut embeddings = HashMap::default();
-            while let Some(db_embedding) = db_embeddings.next().await {
-                let db_embedding = db_embedding?;
-                embeddings.insert(db_embedding.digest, db_embedding.dimensions);
+                let mut embeddings = HashMap::default();
+                while let Some(db_embedding) = db_embeddings.next().await {
+                    let db_embedding = db_embedding?;
+                    embeddings.insert(db_embedding.digest, db_embedding.dimensions);
+                }
+                embeddings
+            };
+
+            if !embeddings.is_empty() {
+                let now = OffsetDateTime::now_utc();
+                let retrieved_at = PrimitiveDateTime::new(now.date(), now.time());
+
+                embedding::Entity::update_many()
+                    .filter(
+                        embedding::Column::Digest
+                            .is_in(embeddings.keys().map(|digest| digest.as_slice())),
+                    )
+                    .col_expr(embedding::Column::RetrievedAt, Expr::value(retrieved_at))
+                    .exec(&*tx)
+                    .await?;
             }
+
             Ok(embeddings)
         })
         .await
@@ -61,7 +82,7 @@ impl Database {
             embedding::Entity::delete_many()
                 .filter(
                     embedding::Column::RetrievedAt
-                        .lte(OffsetDateTime::now_utc() - Duration::days(7)),
+                        .lte(OffsetDateTime::now_utc() - Duration::days(60)),
                 )
                 .exec(&*tx)
                 .await?;
