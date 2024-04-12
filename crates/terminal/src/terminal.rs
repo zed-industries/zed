855275d9e3aa90c0c280cb6ec045e449fb9d116e
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-04-12 13:08:27 +0200

    task: use full task label to distinguish a terminal

--- crates/terminal/src/terminal.rs
+++ crates/terminal/src/terminal.rs
@@ -288,6 +288,7 @@ impl Display for TerminalError {
 
 pub struct SpawnTask {
     pub id: TaskId,
+    pub full_label: String,
     pub label: String,
     pub command: String,
     pub args: Vec<String>,
@@ -594,6 +595,7 @@ pub struct Terminal {
 
 pub struct TaskState {
     pub id: TaskId,
+    pub full_label: String,
     pub label: String,
     pub status: TaskStatus,
     pub completion_rx: Receiver<()>,
