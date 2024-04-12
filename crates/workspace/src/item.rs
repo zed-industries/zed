065f15e9a679b3fe5e44c9f4e2fb04f4cce9f505
Mikayla Maki <mikayla@zed.dev>
2024-04-12 12:29:00 -0700

    Use buffer font when rendering editor breadcrumbs and diagnostics (#10488)
    Before:
    
    <img width="592" alt="Screenshot 2024-04-12 at 12 00 00 PM"
    src="https://github.com/zed-industries/zed/assets/2280405/3251743e-4f2c-4ca3-9bc5-88f37660f7b9">
    
    After:
    
    <img width="673" alt="Screenshot 2024-04-12 at 12 11 37 PM"
    src="https://github.com/zed-industries/zed/assets/2280405/6a8ac597-261a-45d9-bf2a-a673b6f26b0e">
    
    
    Release Notes:
    
    - N/A

--- crates/workspace/src/item.rs
+++ crates/workspace/src/item.rs
@@ -14,7 +14,7 @@ use client::{
 use futures::{channel::mpsc, StreamExt};
 use gpui::{
     AnyElement, AnyView, AppContext, Entity, EntityId, EventEmitter, FocusHandle, FocusableView,
-    HighlightStyle, Model, Pixels, Point, SharedString, Task, View, ViewContext, WeakView,
+    Font, HighlightStyle, Model, Pixels, Point, SharedString, Task, View, ViewContext, WeakView,
     WindowContext,
 };
 use project::{Project, ProjectEntryId, ProjectPath};
@@ -122,6 +122,7 @@ pub enum ItemEvent {
 pub struct BreadcrumbText {
     pub text: String,
     pub highlights: Option<Vec<(Range<usize>, HighlightStyle)>>,
+    pub font: Option<Font>,
 }
 
 #[derive(Debug, Clone, Copy)]
