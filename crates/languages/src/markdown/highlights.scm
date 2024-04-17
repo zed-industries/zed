d0649250a31c039232f0b88900c35bd6967fb4fc
Nate Butler <iamnbutler@gmail.com>
2024-04-17 10:48:51 -0400

    Add an initial set of captures for blockquotes

--- crates/languages/src/markdown/highlights.scm
+++ crates/languages/src/markdown/highlights.scm
@@ -20,5 +20,28 @@
   (info_string
     (language) @text.literal))
 
+; hack to deal with incorrect grammar parsing
+(atx_heading
+  (block_quote_marker) @punctuation.block_quote_marker
+)
+
+; hack to deal with incorrect grammar parsing
+(paragraph
+  (block_quote_marker) @punctuation.block_quote_marker
+)
+
+; hack to deal with incorrect grammar parsing
+(list_item
+  (block_quote_marker) @punctuation.block_quote_marker
+)
+
+(block_quote
+  (block_quote_marker) @punctuation.block_quote_marker
+)
+
+(block_quote
+  (paragraph) @text.block_quote
+)
+
 (link_destination) @link_uri
 (link_text) @link_text
