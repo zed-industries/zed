bf8cf561d57bda31dd9ee45afc6be0fbe42d76bd
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2024-04-19 21:16:29 -0400

    fix: improve jsdoc injection in comments

--- crates/languages/src/javascript/injections.scm
+++ crates/languages/src/javascript/injections.scm
@@ -1,5 +1,9 @@
-((comment) @content
+(((comment) @_jsdoc_comment
+  (#match? @_jsdoc_comment "(?s)^/[*][*][^*].*[*]/$")) @content
   (#set! "language" "jsdoc"))
-  
+
+((comment) @content
+  (#set! "language" "comment"))
+
 ((regex) @content
   (#set! "language" "regex"))
