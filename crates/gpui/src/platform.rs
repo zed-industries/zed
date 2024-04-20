326b6fb4bf9f0f2e24a63144a37b8ba53f00aaf2
Junkui Zhang <364772080@qq.com>
2024-04-20 12:45:05 +0800

    remove unneeded `is_minimized`

--- crates/gpui/src/platform.rs
+++ crates/gpui/src/platform.rs
@@ -184,7 +184,6 @@ unsafe impl Send for DisplayId {}
 pub(crate) trait PlatformWindow: HasWindowHandle + HasDisplayHandle {
     fn bounds(&self) -> Bounds<DevicePixels>;
     fn is_maximized(&self) -> bool;
-    fn is_minimized(&self) -> bool;
     fn restore_status(&self) -> WindowOpenStatus;
     fn content_size(&self) -> Size<Pixels>;
     fn scale_factor(&self) -> f32;
