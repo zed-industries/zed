f39e977363e0a78d2e710f5dc6db1265ff3b9a72
Junkui Zhang <364772080@qq.com>
2024-04-20 12:32:35 +0800

    remove unneeded restore bounds

--- crates/gpui/src/platform/linux/wayland/window.rs
+++ crates/gpui/src/platform/linux/wayland/window.rs
@@ -655,8 +655,7 @@ impl PlatformWindow for WaylandWindow {
     }
 
     fn toggle_fullscreen(&self) {
-        let mut state = self.borrow_mut();
-        state.restore_bounds = state.bounds.map(|p| DevicePixels(p as i32));
+        let state = self.borrow();
         if !state.fullscreen {
             state.toplevel.set_fullscreen(None);
         } else {
