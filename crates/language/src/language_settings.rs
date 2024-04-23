1e5a696e56c0bda0839259f271c53b91043a2b89
Kyle Kelley <rgbkrk@gmail.com>
2024-04-23 10:03:11 -0700

    rip out the feature_enabled flag for now

--- crates/language/src/language_settings.rs
+++ crates/language/src/language_settings.rs
@@ -179,8 +179,6 @@ pub enum InlineCompletionProvider {
 /// or [Supermaven](https://supermaven.com).
 #[derive(Clone, Debug, Default)]
 pub struct InlineCompletionSettings {
-    /// Deprecated - use the root level show_inline_completions instead.
-    feature_enabled: bool,
     /// The provider that supplies inline completions.
     pub provider: InlineCompletionProvider,
     /// A list of globs representing files that inline completions should be disabled for.
