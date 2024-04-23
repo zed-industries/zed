949a6e4eec475722bbcaf2b9804e471e9112f866
Kyle Kelley <rgbkrk@gmail.com>
2024-04-23 10:17:53 -0700

    clean up debug

--- crates/text/src/text.rs
+++ crates/text/src/text.rs
@@ -1974,7 +1974,6 @@ impl BufferSnapshot {
                 .item()
                 .filter(|insertion| insertion.timestamp == anchor.timestamp)
             else {
-                self.insertions.items(&());
                 panic!(
                     "invalid anchor {:?}. buffer id: {}, version: {:?}",
                     anchor, self.remote_id, self.version
