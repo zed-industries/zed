ce4b2fa8e440dd6b34ed67beee829eab88ffc3bf
Marshall Bowers <elliott.codes@gmail.com>
2024-04-24 16:10:23 -0400

    Comment out problematic queries

--- extensions/elixir/languages/elixir/highlights.scm
+++ extensions/elixir/languages/elixir/highlights.scm
@@ -1,153 +1,153 @@
-["when" "and" "or" "not" "in" "not in" "fn" "do" "end" "catch" "rescue" "after" "else"] @keyword
+; ["when" "and" "or" "not" "in" "not in" "fn" "do" "end" "catch" "rescue" "after" "else"] @keyword
 
-(unary_operator
-  operator: "&"
-  operand: (integer) @operator)
+; (unary_operator
+;   operator: "&"
+;   operand: (integer) @operator)
 
-(operator_identifier) @operator
+; (operator_identifier) @operator
 
-(unary_operator
-  operator: _ @operator)
+; (unary_operator
+;   operator: _ @operator)
 
-(binary_operator
-  operator: _ @operator)
+; (binary_operator
+;   operator: _ @operator)
 
-(dot
-  operator: _ @operator)
+; (dot
+;   operator: _ @operator)
 
-(stab_clause
-  operator: _ @operator)
+; (stab_clause
+;   operator: _ @operator)
 
-[
-  (boolean)
-  (nil)
-] @constant
+; [
+;   (boolean)
+;   (nil)
+; ] @constant
 
-[
-  (integer)
-  (float)
-] @number
+; [
+;   (integer)
+;   (float)
+; ] @number
 
-(alias) @type
+; (alias) @type
 
-(call
-  target: (dot
-    left: (atom) @type))
+; (call
+;   target: (dot
+;     left: (atom) @type))
 
-(char) @constant
+; (char) @constant
 
-(escape_sequence) @string.escape
+; (escape_sequence) @string.escape
 
-[
-  (atom)
-  (quoted_atom)
-  (keyword)
-  (quoted_keyword)
-] @string.special.symbol
+; [
+;   (atom)
+;   (quoted_atom)
+;   (keyword)
+;   (quoted_keyword)
+; ] @string.special.symbol
 
-[
-  (string)
-  (charlist)
-] @string
+; [
+;   (string)
+;   (charlist)
+; ] @string
 
-(sigil
-  (sigil_name) @__name__
-  quoted_start: _ @string
-  quoted_end: _ @string
-  (#match? @__name__ "^[sS]$")) @string
+; (sigil
+;   (sigil_name) @__name__
+;   quoted_start: _ @string
+;   quoted_end: _ @string
+;   (#match? @__name__ "^[sS]$")) @string
 
-(sigil
-  (sigil_name) @__name__
-  quoted_start: _ @string.regex
-  quoted_end: _ @string.regex
-  (#match? @__name__ "^[rR]$")) @string.regex
+; (sigil
+;   (sigil_name) @__name__
+;   quoted_start: _ @string.regex
+;   quoted_end: _ @string.regex
+;   (#match? @__name__ "^[rR]$")) @string.regex
 
-(sigil
-  (sigil_name) @__name__
-  quoted_start: _ @string.special
-  quoted_end: _ @string.special) @string.special
+; (sigil
+;   (sigil_name) @__name__
+;   quoted_start: _ @string.special
+;   quoted_end: _ @string.special) @string.special
 
-(
-  (identifier) @comment.unused
-  (#match? @comment.unused "^_")
-)
+; (
+;   (identifier) @comment.unused
+;   (#match? @comment.unused "^_")
+; )
 
-(call
-  target: [
-    (identifier) @function
-    (dot
-      right: (identifier) @function)
-  ])
+; (call
+;   target: [
+;     (identifier) @function
+;     (dot
+;       right: (identifier) @function)
+;   ])
 
-(call
-  target: (identifier) @keyword
-  (arguments
-    [
-      (identifier) @function
-      (binary_operator
-        left: (identifier) @function
-        operator: "when")
-      (binary_operator
-        operator: "|>"
-        right: (identifier))
-    ])
-  (#match? @keyword "^(def|defdelegate|defguard|defguardp|defmacro|defmacrop|defn|defnp|defp)$"))
+; (call
+;   target: (identifier) @keyword
+;   (arguments
+;     [
+;       (identifier) @function
+;       (binary_operator
+;         left: (identifier) @function
+;         operator: "when")
+;       (binary_operator
+;         operator: "|>"
+;         right: (identifier))
+;     ])
+;   (#match? @keyword "^(def|defdelegate|defguard|defguardp|defmacro|defmacrop|defn|defnp|defp)$"))
 
-(binary_operator
-  operator: "|>"
-  right: (identifier) @function)
+; (binary_operator
+;   operator: "|>"
+;   right: (identifier) @function)
 
-(call
-  target: (identifier) @keyword
-  (#match? @keyword "^(def|defdelegate|defexception|defguard|defguardp|defimpl|defmacro|defmacrop|defmodule|defn|defnp|defoverridable|defp|defprotocol|defstruct)$"))
+; (call
+;   target: (identifier) @keyword
+;   (#match? @keyword "^(def|defdelegate|defexception|defguard|defguardp|defimpl|defmacro|defmacrop|defmodule|defn|defnp|defoverridable|defp|defprotocol|defstruct)$"))
 
-(call
-  target: (identifier) @keyword
-  (#match? @keyword "^(alias|case|cond|else|for|if|import|quote|raise|receive|require|reraise|super|throw|try|unless|unquote|unquote_splicing|use|with)$"))
+; (call
+;   target: (identifier) @keyword
+;   (#match? @keyword "^(alias|case|cond|else|for|if|import|quote|raise|receive|require|reraise|super|throw|try|unless|unquote|unquote_splicing|use|with)$"))
 
-(
-  (identifier) @constant.builtin
-  (#match? @constant.builtin "^(__MODULE__|__DIR__|__ENV__|__CALLER__|__STACKTRACE__)$")
-)
+; (
+;   (identifier) @constant.builtin
+;   (#match? @constant.builtin "^(__MODULE__|__DIR__|__ENV__|__CALLER__|__STACKTRACE__)$")
+; )
 
-(unary_operator
-  operator: "@" @comment.doc
-  operand: (call
-    target: (identifier) @__attribute__ @comment.doc
-    (arguments
-      [
-        (string)
-        (charlist)
-        (sigil)
-        (boolean)
-      ] @comment.doc))
-  (#match? @__attribute__ "^(moduledoc|typedoc|doc)$"))
+; (unary_operator
+;   operator: "@" @comment.doc
+;   operand: (call
+;     target: (identifier) @__attribute__ @comment.doc
+;     (arguments
+;       [
+;         (string)
+;         (charlist)
+;         (sigil)
+;         (boolean)
+;       ] @comment.doc))
+;   (#match? @__attribute__ "^(moduledoc|typedoc|doc)$"))
 
-(comment) @comment
+; (comment) @comment
 
-[
- "%"
-] @punctuation
+; [
+;  "%"
+; ] @punctuation
 
-[
- ","
- ";"
-] @punctuation.delimiter
+; [
+;  ","
+;  ";"
+; ] @punctuation.delimiter
 
-[
-  "("
-  ")"
-  "["
-  "]"
-  "{"
-  "}"
-  "<<"
-  ">>"
-] @punctuation.bracket
+; [
+;   "("
+;   ")"
+;   "["
+;   "]"
+;   "{"
+;   "}"
+;   "<<"
+;   ">>"
+; ] @punctuation.bracket
 
-(interpolation "#{" @punctuation.special "}" @punctuation.special) @embedded
+; (interpolation "#{" @punctuation.special "}" @punctuation.special) @embedded
 
-((sigil
-  (sigil_name) @_sigil_name
-  (quoted_content) @embedded)
- (#eq? @_sigil_name "H"))
+; ((sigil
+;   (sigil_name) @_sigil_name
+;   (quoted_content) @embedded)
+;  (#eq? @_sigil_name "H"))
--- extensions/elixir/languages/elixir/outline.scm
+++ extensions/elixir/languages/elixir/outline.scm
@@ -1,46 +1,46 @@
-(call
-  target: (identifier) @context
-  (arguments (alias) @name)
-  (#match? @context "^(defmodule|defprotocol)$")) @item
+; (call
+;   target: (identifier) @context
+;   (arguments (alias) @name)
+;   (#match? @context "^(defmodule|defprotocol)$")) @item
 
-(unary_operator
-  operator: "@" @name
-  operand: (call
-  target: (identifier) @context
-    (arguments
-      [
-        (binary_operator
-          left: (identifier) @name)
-        (binary_operator
-          left: (call
-          target: (identifier) @name
-          (arguments
-            "(" @context.extra
-            _* @context.extra
-            ")" @context.extra)))
-      ]
-    )
-)
-(#match? @context "^(callback|type|typep)$")) @item
+; (unary_operator
+;   operator: "@" @name
+;   operand: (call
+;   target: (identifier) @context
+;     (arguments
+;       [
+;         (binary_operator
+;           left: (identifier) @name)
+;         (binary_operator
+;           left: (call
+;           target: (identifier) @name
+;           (arguments
+;             "(" @context.extra
+;             _* @context.extra
+;             ")" @context.extra)))
+;       ]
+;     )
+; )
+; (#match? @context "^(callback|type|typep)$")) @item
 
-(call
-  target: (identifier) @context
-  (arguments
-    [
-      (identifier) @name
-      (call
-          target: (identifier) @name
-          (arguments
-              "(" @context.extra
-              _* @context.extra
-              ")" @context.extra))
-      (binary_operator
-        left: (call
-            target: (identifier) @name
-            (arguments
-                "(" @context.extra
-                _* @context.extra
-                ")" @context.extra))
-        operator: "when")
-    ])
-  (#match? @context "^(def|defp|defdelegate|defguard|defguardp|defmacro|defmacrop|defn|defnp)$")) @item
+; (call
+;   target: (identifier) @context
+;   (arguments
+;     [
+;       (identifier) @name
+;       (call
+;           target: (identifier) @name
+;           (arguments
+;               "(" @context.extra
+;               _* @context.extra
+;               ")" @context.extra))
+;       (binary_operator
+;         left: (call
+;             target: (identifier) @name
+;             (arguments
+;                 "(" @context.extra
+;                 _* @context.extra
+;                 ")" @context.extra))
+;         operator: "when")
+;     ])
+;   (#match? @context "^(def|defp|defdelegate|defguard|defguardp|defmacro|defmacrop|defn|defnp)$")) @item
