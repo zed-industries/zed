95f31e52bd35f3642b6ca44368c9504788fd69d1
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2024-04-25 18:23:23 -0400

    chore: remove unnecessary injections

--- crates/languages/src/typescript/injections.scm
+++ crates/languages/src/typescript/injections.scm
@@ -2,8 +2,5 @@
   (#match? @_jsdoc_comment "(?s)^/[*][*][^*].*[*]/$")) @content
   (#set! "language" "jsdoc"))
 
-((comment) @content
-  (#set! "language" "comment"))
-
 ((regex) @content
   (#set! "language" "regex"))
