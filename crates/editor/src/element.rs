6486bb7cc4ae7b11dfdbe58bdab44c03fd67326d
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-04-26 17:29:56 +0200

    Add elided lifetime to make merging with code-actions seamless

--- crates/editor/src/element.rs
+++ crates/editor/src/element.rs
@@ -3070,7 +3070,7 @@ fn prepaint_gutter_button(
     gutter_dimensions: &GutterDimensions,
     scroll_pixel_position: gpui::Point<Pixels>,
     gutter_hitbox: &Hitbox,
-    cx: &mut WindowContext,
+    cx: &mut WindowContext<'_>,
 ) -> AnyElement {
     let mut button = button.into_any_element();
     let available_space = size(
