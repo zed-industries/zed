38f889ac97fe6c92a5d0897b17cbbd3097261fec
Junkui Zhang <364772080@qq.com>
2024-04-27 10:06:47 +0800

    update examples

--- crates/gpui/examples/hello_world.rs
+++ crates/gpui/examples/hello_world.rs
@@ -26,7 +26,7 @@ fn main() {
         let bounds = Bounds::centered(None, size(px(600.0), px(600.0)), cx);
         cx.open_window(
             WindowOptions {
-                window_bounds: WindowBounds::Windowed(Some(bounds)),
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
                 ..Default::default()
             },
             |cx| {
