ca124172d14c5cfcf0425816d90675f10264581c
Kyle Kelley <rgbkrk@gmail.com>
2024-05-02 15:30:29 -0700

    first pass at AttachmentContexts
    Co-Authored-By: Marshall <marshall@zed.dev>

--- crates/assistant2/src/contexts.rs
+++ crates/assistant2/src/contexts.rs
@@ -1,11 +1,19 @@
-use std::any::TypeId;
+use std::{
+    any::TypeId,
+    sync::{mpsc, Arc},
+};
 
 use crate::completion_provider::CompletionMessage;
 use anyhow::{anyhow, Result};
 use collections::HashMap;
-use editor::MultiBuffer;
-use gpui::{AnyView, AppContext, Model, ModelContext, Render, Subscription, Task, View};
-use ui::WindowContext;
+use editor::{Editor, MultiBuffer};
+use gpui::{
+    AnyView, AppContext, EventEmitter, Model, ModelContext, Render, Subscription, Task, View,
+    WeakView,
+};
+use ui::{prelude::*, WindowContext};
+use util::maybe;
+use workspace::Workspace;
 
 // Immutable already done attached sometime ago
 pub struct UserAttachment {
@@ -67,111 +75,100 @@ pub trait AttachmentTool {
     fn view(output: Result<Self::Output>, cx: &mut WindowContext) -> View<Self::View>;
 }
 
-// I envision this to be similar to the tool calls in that we'll have a registry of scopes
-// that we can enable/disable. A "run" of the scope will be an immutable instance of a scope run.
-// Gosh, maybe it's exactly like the LanguageModelTool. The difference is that these are user initiated.
-//
-// They'll include the:
-// - active buffer
-// - project diagnostics
-//
+struct ActiveEditorAttachment {
+    filename: Arc<str>,
+    language: Arc<str>,
+    text: Arc<str>,
+}
 
-// cx.defer(|this, cx| this.update_active_buffer(workspace, cx));
-//
-// fn update_active_buffer(
-//     &mut self,
-//     workspace: View<Workspace>,
-//     cx: &mut ViewContext<'_, ConversationEditor>,
-// ) {
-//     let active_buffer = workspace
-//         .read(cx)
-//         .active_item(cx)
-//         .and_then(|item| Some(item.act_as::<Editor>(cx)?.read(cx).buffer().clone()));
+struct FileAttachmentView {
+    output: Result<ActiveEditorAttachment>,
+}
 
-//     self.conversation.update(cx, |conversation, cx| {
-//         conversation
-//             .embedded_scope
-//             .set_active_buffer(active_buffer.clone(), cx);
+impl Render for FileAttachmentView {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        match &self.output {
+            Ok(attachment) => {
+                let filename = attachment.filename.clone();
+                div().child(SharedString::from(filename))
+            }
+            Err(err) => div().child(err.to_string()),
+        }
+    }
+}
 
-//         conversation.count_remaining_tokens(cx);
-//         cx.notify();
-//     });
-// }
+struct ActiveEditorAttachmentTool {
+    workspace: WeakView<Workspace>,
+}
 
-// #[derive(Default)]
-// pub struct ActiveBufferContext {
-//     active_buffer: Option<Model<MultiBuffer>>,
-//     active_buffer_enabled: bool,
-//     active_buffer_subscription: Option<Subscription>,
-// }
+impl ActiveEditorAttachmentTool {
+    pub fn new(workspace: WeakView<Workspace>, _cx: &mut WindowContext) -> Self {
+        Self { workspace }
+    }
+}
 
-// impl ActiveBufferContext {
-//     pub fn new() -> Self {
-//         Self {
-//             active_buffer: None,
-//             active_buffer_enabled: true,
-//             active_buffer_subscription: None,
-//         }
-//     }
+impl AttachmentTool for ActiveEditorAttachmentTool {
+    type Output = ActiveEditorAttachment;
+    type View = FileAttachmentView;
 
-//     pub fn set_active_buffer(
-//         &mut self,
-//         buffer: Option<Model<MultiBuffer>>,
-//         cx: &mut WindowContext,
-//         // cx: &mut ModelContext<Conversation>,
-//     ) {
-//         self.active_buffer_subscription.take();
+    fn run(&self, cx: &mut WindowContext) -> Task<Result<ActiveEditorAttachment>> {
+        Task::ready(maybe!({
+            let active_buffer = self
+                .workspace
+                .update(cx, |workspace, cx| {
+                    workspace
+                        .active_item(cx)
+                        .and_then(|item| Some(item.act_as::<Editor>(cx)?.read(cx).buffer().clone()))
+                })?
+                .ok_or_else(|| anyhow!("no active buffer"))?;
 
-//         // todo!(): This is where we should set up a subscription to the buffer to count tokens
+            let buffer = active_buffer.read(cx);
 
-//         self.active_buffer = buffer;
-//     }
+            if let Some(singleton) = buffer.as_singleton() {
+                let singleton = singleton.read(cx);
 
-//     pub fn active_buffer(&self) -> Option<&Model<MultiBuffer>> {
-//         self.active_buffer.as_ref()
-//     }
+                let filename = singleton
+                    .file()
+                    .map(|file| file.path().to_string_lossy())
+                    .unwrap_or("Untitled".into());
 
-//     pub fn active_buffer_enabled(&self) -> bool {
-//         self.active_buffer_enabled
-//     }
+                let text = singleton.text();
 
-//     pub fn set_active_buffer_enabled(&mut self, enabled: bool) {
-//         self.active_buffer_enabled = enabled;
-//     }
+                let language = singleton
+                    .language()
+                    .map(|l| {
+                        let name = l.code_fence_block_name();
+                        name.to_string()
+                    })
+                    .unwrap_or_default();
 
-//     /// Provide a message for the language model based on the active buffer.
-//     pub fn message(&self, cx: &AppContext) -> Option<CompletionMessage> {
-//         if !self.active_buffer_enabled {
-//             return None;
-//         }
+                return Ok(ActiveEditorAttachment {
+                    filename: filename.into(),
+                    language: language.into(),
+                    text: text.into(),
+                });
+            }
 
-//         let active_buffer = self.active_buffer.as_ref()?;
-//         let buffer = active_buffer.read(cx);
+            Err(anyhow!("no active buffer"))
+        }))
+    }
 
-//         if let Some(singleton) = buffer.as_singleton() {
-//             let singleton = singleton.read(cx);
+    fn message(output: &Result<Self::Output>) -> Option<CompletionMessage> {
+        let output = output.as_ref().ok()?;
 
-//             let filename = singleton
-//                 .file()
-//                 .map(|file| file.path().to_string_lossy())
-//                 .unwrap_or("Untitled".into());
+        let filename = &output.filename;
+        let language = &output.language;
+        let text = &output.text;
 
-//             let text = singleton.text();
+        let markdown_content =
+            format!("User's active file `{filename}`:\n\n```{language}\n{text}```\n\n");
 
-//             let language = singleton
-//                 .language()
-//                 .map(|l| {
-//                     let name = l.code_fence_block_name();
-//                     name.to_string()
-//                 })
-//                 .unwrap_or_default();
+        return Some(CompletionMessage::System {
+            content: markdown_content,
+        });
+    }
 
-//             let markdown =
-//                 format!("User's active file `{filename}`:\n\n```{language}\n{text}```\n\n");
-
-//             return Some(CompletionMessage::System { content: markdown });
-//         }
-
-//         None
-//     }
-// }
+    fn view(output: Result<Self::Output>, cx: &mut WindowContext) -> View<Self::View> {
+        cx.new_view(|_cx| FileAttachmentView { output })
+    }
+}
