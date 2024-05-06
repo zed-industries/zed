c1fbf56fff4f28020d9d03749814e309b280a5de
Nate Butler <iamnbutler@gmail.com>
2024-05-06 16:17:47 -0400

    wip

--- crates/assistant2/src/ui/chat_message.rs
+++ crates/assistant2/src/ui/chat_message.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 
 use client::User;
-use gpui::{AnyElement, ClickEvent};
-use ui::{prelude::*, Avatar};
+use gpui::{hsla, AnyElement, ClickEvent};
+use ui::{prelude::*, Avatar, Tooltip};
 
 use crate::MessageId;
 
@@ -43,11 +43,19 @@ impl ChatMessage {
 
 impl RenderOnce for ChatMessage {
     fn render(self, cx: &mut WindowContext) -> impl IntoElement {
+        let message_group = SharedString::from(format!("{}_group", self.id.0));
+
         let collapse_handle_id = SharedString::from(format!("{}_collapse_handle", self.id.0));
 
         let content_padding = Spacing::Large.rems(cx);
         // Clamp the message height to exactly 1.5 lines when collapsed.
         let collapsed_height = content_padding.to_pixels(cx.rem_size()) + cx.line_height() * 1.5;
+
+        let background_color = if let UserOrAssistant::User(_) = &self.player {
+            Some(cx.theme().colors().surface_background)
+        } else {
+            None
+        };
 
         let (username, avatar_uri) = match self.player {
             UserOrAssistant::Assistant => (
@@ -61,14 +69,13 @@ impl RenderOnce for ChatMessage {
         };
 
         v_flex()
+            .group(message_group.clone())
             .w_full()
             .flex_none()
-            // .debug_bg_cyan()
             .gap(Spacing::Small.rems(cx))
             .py(Spacing::Small.rems(cx))
             .child(
                 h_flex()
-                    // .debug_bg_red()
                     .justify_between()
                     .w_full()
                     .flex_none()
@@ -83,23 +90,35 @@ impl RenderOnce for ChatMessage {
                                     this.child(div().size(avatar_size))
                                 }
                             })
-                            .child(Label::new(username).color(Color::Default)),
+                            .child(Label::new(username).color(Color::Muted)),
                     )
                     .child(
-                        h_flex().child(
-                            IconButton::new(
-                                collapse_handle_id.clone(),
-                                if self.collapsed.clone() {
-                                    IconName::ArrowUp
-                                } else {
-                                    IconName::ArrowDown
-                                },
+                        h_flex()
+                            .visible_on_hover(message_group)
+                            .child(
+                                IconButton::new(
+                                    collapse_handle_id.clone(),
+                                    if self.collapsed.clone() {
+                                        IconName::ArrowUp
+                                    } else {
+                                        IconName::ArrowDown
+                                    },
+                                )
+                                .icon_size(IconSize::XSmall)
+                                .icon_color(Color::Muted)
+                                .on_click(self.on_collapse_handle_click)
+                                .tooltip(|cx| Tooltip::text("Collapse Message", cx)),
                             )
-                            .icon_size(IconSize::XSmall)
-                            .on_click(self.on_collapse_handle_click),
-                        ), // .when(!self.contexts.is_empty(), |this| {
-                           //     this.child(Label::new(self.contexts.len().to_string()).color(Color::Muted))
-                           // })
+                            .child(
+                                IconButton::new("copy-message", IconName::Copy)
+                                    .icon_color(Color::Muted)
+                                    .icon_size(IconSize::XSmall),
+                            )
+                            .child(
+                                IconButton::new("menu", IconName::Ellipsis)
+                                    .icon_color(Color::Muted)
+                                    .icon_size(IconSize::XSmall),
+                            ),
                     ),
             )
             .when(self.message.is_some() || self.tools_used.is_some(), |el| {
@@ -112,6 +131,9 @@ impl RenderOnce for ChatMessage {
                             .p(content_padding)
                             .gap_3()
                             .rounded_lg()
+                            .when_some(background_color, |this, background_color| {
+                                this.bg(background_color)
+                            })
                             .when(self.collapsed, |this| this.h(collapsed_height))
                             .children(self.message)
                             .when_some(self.tools_used, |this, tools_used| this.child(tools_used)),
