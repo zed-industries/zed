4bcb0b9858570473555070bb298410b8823ae427
Nate Butler <iamnbutler@gmail.com>
2024-05-06 12:27:06 -0400

    wip

---
+++ crates/assistant2/delete_before_merging.md
@@ -0,0 +1,1 @@
+Can you tell me what the assitant2 crate is for in my project?
--- crates/assistant2/src/ui/chat_message.rs
+++ crates/assistant2/src/ui/chat_message.rs
@@ -16,6 +16,7 @@ pub struct ChatMessage {
     id: MessageId,
     player: UserOrAssistant,
     message: Option<AnyElement>,
+    placeholder_message: Option<SharedString>,
     tools_used: Option<AnyElement>,
     collapsed: bool,
     on_collapse_handle_click: Box<dyn Fn(&ClickEvent, &mut WindowContext) + 'static>,
@@ -34,6 +35,7 @@ impl ChatMessage {
             id,
             player,
             message,
+            placeholder_message: None,
             tools_used,
             collapsed,
             on_collapse_handle_click,
@@ -43,71 +45,32 @@ impl ChatMessage {
 
 impl RenderOnce for ChatMessage {
     fn render(self, cx: &mut WindowContext) -> impl IntoElement {
-        let collapse_handle_id = SharedString::from(format!("{}_collapse_handle", self.id.0));
-        let collapse_handle = h_flex()
-            .id(collapse_handle_id.clone())
-            .group(collapse_handle_id.clone())
-            .flex_none()
-            .justify_center()
-            .w_1()
-            .mx_2()
-            .h_full()
-            .on_click(self.on_collapse_handle_click)
-            .child(
-                div()
-                    .w_px()
-                    .h_full()
-                    .rounded_lg()
-                    .overflow_hidden()
-                    .bg(cx.theme().colors().element_background)
-                    .group_hover(collapse_handle_id, |this| {
-                        this.bg(cx.theme().colors().element_hover)
-                    }),
-            );
+        // let collapse_handle_id = SharedString::from(format!("{}_collapse_handle", self.id.0));
+        // let collapse_handle = h_flex()
+        //     .id(collapse_handle_id.clone())
+        //     .group(collapse_handle_id.clone())
+        //     .flex_none()
+        //     .justify_center()
+        //     .w_1()
+        //     .mx_2()
+        //     .h_full()
+        //     .on_click(self.on_collapse_handle_click)
+        //     .child(
+        //         div()
+        //             .w_px()
+        //             .h_full()
+        //             .rounded_lg()
+        //             .overflow_hidden()
+        //             .bg(cx.theme().colors().element_background)
+        //             .group_hover(collapse_handle_id, |this| {
+        //                 this.bg(cx.theme().colors().element_hover)
+        //             }),
+        //     );
 
-        let content_padding = rems(1.);
+        let content_padding = Spacing::Medium.rems(cx);
         // Clamp the message height to exactly 1.5 lines when collapsed.
         let collapsed_height = content_padding.to_pixels(cx.rem_size()) + cx.line_height() * 1.5;
 
-        v_flex()
-            .gap_1()
-            .child(ChatMessageHeader::new(self.player))
-            .when(self.message.is_some() || self.tools_used.is_some(), |el| {
-                el.child(
-                    h_flex().gap_3().child(collapse_handle).child(
-                        div()
-                            .overflow_hidden()
-                            .w_full()
-                            .p(content_padding)
-                            .gap_3()
-                            .rounded_lg()
-                            .when(self.collapsed, |this| this.h(collapsed_height))
-                            .bg(cx.theme().colors().surface_background)
-                            .children(self.message)
-                            .children(self.tools_used),
-                    ),
-                )
-            })
-    }
-}
-
-#[derive(IntoElement)]
-struct ChatMessageHeader {
-    player: UserOrAssistant,
-    contexts: Vec<()>,
-}
-
-impl ChatMessageHeader {
-    fn new(player: UserOrAssistant) -> Self {
-        Self {
-            player,
-            contexts: Vec::new(),
-        }
-    }
-}
-
-impl RenderOnce for ChatMessageHeader {
-    fn render(self, _cx: &mut WindowContext) -> impl IntoElement {
         let (username, avatar_uri) = match self.player {
             UserOrAssistant::Assistant => (
                 "Assistant".into(),
@@ -119,23 +82,63 @@ impl RenderOnce for ChatMessageHeader {
             UserOrAssistant::User(None) => ("You".into(), None),
         };
 
-        h_flex()
-            .justify_between()
+        let footer_height = rems_from_px(20.);
+        let neg_bottom_pos = footer_height - rems_from_px(12.);
+
+        v_flex()
+            .gap_1()
             .child(
                 h_flex()
-                    .gap_3()
-                    .map(|this| {
-                        let avatar_size = rems_from_px(20.);
-                        if let Some(avatar_uri) = avatar_uri {
-                            this.child(Avatar::new(avatar_uri).size(avatar_size))
-                        } else {
-                            this.child(div().size(avatar_size))
-                        }
-                    })
-                    .child(Label::new(username).color(Color::Default)),
+                    .justify_between()
+                    .child(
+                        h_flex()
+                            .gap_2()
+                            .map(|this| {
+                                let avatar_size = rems_from_px(20.);
+                                if let Some(avatar_uri) = avatar_uri {
+                                    this.child(Avatar::new(avatar_uri).size(avatar_size))
+                                } else {
+                                    this.child(div().size(avatar_size))
+                                }
+                            })
+                            .child(Label::new(username).color(Color::Default)),
+                    )
+                    .child(
+                        div(), // .when(!self.contexts.is_empty(), |this| {
+                               //     this.child(Label::new(self.contexts.len().to_string()).color(Color::Muted))
+                               // })
+                    ),
             )
-            .child(div().when(!self.contexts.is_empty(), |this| {
-                this.child(Label::new(self.contexts.len().to_string()).color(Color::Muted))
-            }))
+            .when(self.message.is_some() || self.tools_used.is_some(), |el| {
+                el.child(
+                    h_flex()
+                        .pb(footer_height)
+                        // .gap_3().child(collapse_handle)
+                        .child(
+                            div()
+                                .relative()
+                                // .overflow_hidden()
+                                .w_full()
+                                .p(content_padding)
+                                .gap_3()
+                                .rounded_lg()
+                                .when(self.collapsed, |this| this.h(collapsed_height))
+                                .bg(cx.theme().colors().surface_background)
+                                .children(self.message)
+                                .child(
+                                    h_flex()
+                                        .justify_between()
+                                        .absolute()
+                                        .neg_bottom_3()
+                                        // .neg_bottom(neg_bottom_pos)
+                                        .h(footer_height)
+                                        .w_full()
+                                        .debug_bg_red()
+                                        .child(div())
+                                        .children(self.tools_used),
+                                ),
+                        ),
+                )
+            })
     }
 }
