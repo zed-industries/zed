2f584bb34d86edcd5ec6174fa3221c0d84d3efd2
Marshall Bowers <elliott.codes@gmail.com>
2024-05-07 16:40:44 -0400

    Wire up general structure of `SavedConversationPicker`

---
+++ crates/assistant2/src/saved_conversation_picker.rs
@@ -0,0 +1,134 @@
+use std::sync::Arc;
+
+use fuzzy::StringMatch;
+use gpui::{AppContext, DismissEvent, EventEmitter, FocusHandle, FocusableView, View, WeakView};
+use picker::{Picker, PickerDelegate};
+use ui::{prelude::*, HighlightedLabel, ListItem, ListItemSpacing};
+use util::ResultExt;
+use workspace::{ModalView, Workspace};
+
+use crate::saved_conversation::SavedConversation;
+use crate::ToggleSavedConversations;
+
+pub struct SavedConversationPicker {
+    picker: View<Picker<SavedConversationPickerDelegate>>,
+}
+
+impl EventEmitter<DismissEvent> for SavedConversationPicker {}
+
+impl ModalView for SavedConversationPicker {}
+
+impl FocusableView for SavedConversationPicker {
+    fn focus_handle(&self, cx: &AppContext) -> FocusHandle {
+        self.picker.focus_handle(cx)
+    }
+}
+
+impl SavedConversationPicker {
+    pub fn register(workspace: &mut Workspace, _cx: &mut ViewContext<Workspace>) {
+        workspace.register_action(|workspace, _: &ToggleSavedConversations, cx| {
+            workspace.toggle_modal(cx, move |cx| {
+                let delegate = SavedConversationPickerDelegate::new(cx.view().downgrade());
+                Self::new(delegate, cx)
+            });
+        });
+    }
+
+    pub fn new(delegate: SavedConversationPickerDelegate, cx: &mut ViewContext<Self>) -> Self {
+        let picker = cx.new_view(|cx| Picker::uniform_list(delegate, cx));
+        Self { picker }
+    }
+}
+
+impl Render for SavedConversationPicker {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        v_flex().w(rems(34.)).child(self.picker.clone())
+    }
+}
+
+pub struct SavedConversationPickerDelegate {
+    view: WeakView<SavedConversationPicker>,
+    saved_conversations: Vec<SavedConversation>,
+    selected_index: usize,
+    matches: Vec<StringMatch>,
+}
+
+impl SavedConversationPickerDelegate {
+    pub fn new(weak_view: WeakView<SavedConversationPicker>) -> Self {
+        let saved_conversations = Vec::new();
+        let matches = Vec::new();
+
+        Self {
+            view: weak_view,
+            saved_conversations,
+            selected_index: 0,
+            matches,
+        }
+    }
+}
+
+impl PickerDelegate for SavedConversationPickerDelegate {
+    type ListItem = ui::ListItem;
+
+    fn placeholder_text(&self, _cx: &mut WindowContext) -> Arc<str> {
+        "Select saved conversation...".into()
+    }
+
+    fn match_count(&self) -> usize {
+        self.matches.len()
+    }
+
+    fn selected_index(&self) -> usize {
+        self.selected_index
+    }
+
+    fn set_selected_index(&mut self, ix: usize, _cx: &mut ViewContext<Picker<Self>>) {
+        self.selected_index = ix;
+    }
+
+    fn update_matches(
+        &mut self,
+        query: String,
+        cx: &mut ViewContext<Picker<Self>>,
+    ) -> gpui::Task<()> {
+        cx.spawn(move |this, cx| async move {
+            // TODO: Implement match updating.
+        })
+    }
+
+    fn confirm(&mut self, secondary: bool, cx: &mut ViewContext<Picker<Self>>) {
+        if self.matches.is_empty() {
+            self.dismissed(cx);
+            return;
+        }
+
+        // TODO: Implement selecting a saved conversation.
+    }
+
+    fn dismissed(&mut self, cx: &mut ui::prelude::ViewContext<Picker<Self>>) {
+        self.view
+            .update(cx, |_, cx| cx.emit(DismissEvent))
+            .log_err();
+    }
+
+    fn render_match(
+        &self,
+        ix: usize,
+        selected: bool,
+        _cx: &mut ViewContext<Picker<Self>>,
+    ) -> Option<Self::ListItem> {
+        let conversation_match = &self.matches[ix];
+        let _conversation = &self.saved_conversations[conversation_match.candidate_id];
+
+        Some(
+            ListItem::new(ix)
+                .inset(true)
+                .spacing(ListItemSpacing::Sparse)
+                .selected(selected)
+                .child(HighlightedLabel::new(
+                    conversation_match.string.clone(),
+                    conversation_match.positions.clone(),
+                )),
+        )
+    }
+}
