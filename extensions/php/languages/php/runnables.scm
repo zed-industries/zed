fb882a44c5a9fc4d5bbec116d3ba73cf71f5b23e
Remco Smits <djsmits12@gmail.com>
2024-05-07 22:31:50 +0200

    Add runnables query + task for running phpunit tests

---
+++ extensions/php/languages/php/runnables.scm
@@ -0,0 +1,31 @@
+(
+    ; Class that follow the naming convention of PHPUnit test classes
+    ; and that doesn't have the abstract modifier
+    ; and have a method that follow the naming convention of PHPUnit test methods
+    ; and the method is public
+    (class_declaration
+        modifier: (_)? @modifier
+        (#not-eq? @modifier "abstract")
+        name: (_) @name
+        (#match? @name ".*Test")
+        body: (declaration_list
+            (method_declaration
+                (visibility_modifier)? @visibility
+                (#eq? @visibility "public")
+                name: (_) @run
+                (#match? @run "test.*")
+            )
+        )
+    )
+) @php-test
+
+; Class that follow the naming convention of PHPUnit test classes
+; and that doesn't have the abstract modifier
+(
+    (class_declaration
+        modifier: (_)? @modifier
+        (#not-eq? @modifier "abstract")
+        name: (_) @run
+        (#match? @run ".*Test")
+    )
+) @php-test
