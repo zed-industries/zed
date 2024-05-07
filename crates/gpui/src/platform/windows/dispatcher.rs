94233f2e6c4a1c6f16f24c52e772997e1cea5df9
Junkui Zhang <364772080@qq.com>
2024-05-07 16:23:36 +0800

    init

--- crates/gpui/src/platform/windows/dispatcher.rs
+++ crates/gpui/src/platform/windows/dispatcher.rs
@@ -1,72 +1,129 @@
 use std::{
+    ffi::c_void,
     sync::{
         atomic::{AtomicIsize, Ordering},
         Arc,
     },
     thread::{current, ThreadId},
+    time::Duration,
 };
 
 use async_task::Runnable;
 use flume::Sender;
 use parking::Parker;
 use parking_lot::Mutex;
-use windows::Win32::{Foundation::*, System::Threading::*};
+use util::ResultExt;
+use windows::{
+    Foundation::TimeSpan,
+    System::{
+        DispatcherQueue, DispatcherQueueController, DispatcherQueueHandler,
+        Threading::{
+            ThreadPool, ThreadPoolTimer, TimerElapsedHandler, WorkItemHandler, WorkItemOptions,
+            WorkItemPriority,
+        },
+    },
+    Win32::{
+        Foundation::*,
+        System::{
+            Threading::*,
+            WinRT::{
+                CreateDispatcherQueueController, DispatcherQueueOptions, DQTAT_COM_NONE,
+                DQTYPE_THREAD_CURRENT,
+            },
+        },
+    },
+};
 
 use crate::{PlatformDispatcher, TaskLabel};
 
 pub(crate) struct WindowsDispatcher {
-    threadpool: PTP_POOL,
-    main_sender: Sender<Runnable>,
+    controller: DispatcherQueueController,
+    main_queue: DispatcherQueue,
     parker: Mutex<Parker>,
     main_thread_id: ThreadId,
-    dispatch_event: HANDLE,
 }
+
+unsafe impl Send for WindowsDispatcher {}
+unsafe impl Sync for WindowsDispatcher {}
 
 impl WindowsDispatcher {
     pub(crate) fn new(main_sender: Sender<Runnable>, dispatch_event: HANDLE) -> Self {
         let parker = Mutex::new(Parker::new());
-        let threadpool = unsafe {
-            let ret = CreateThreadpool(None);
-            if ret.0 == 0 {
-                panic!(
-                    "unable to initialize a thread pool: {}",
-                    std::io::Error::last_os_error()
-                );
-            }
-            // set minimum 1 thread in threadpool
-            let _ = SetThreadpoolThreadMinimum(ret, 1)
-                .inspect_err(|_| log::error!("unable to configure thread pool"));
-
-            ret
+        // Windows 10 Fall Creators Update (introduced in 10.0.16299.0)
+        let controller = unsafe {
+            let options = DispatcherQueueOptions {
+                dwSize: std::mem::size_of::<DispatcherQueueOptions>() as u32,
+                threadType: DQTYPE_THREAD_CURRENT,
+                apartmentType: DQTAT_COM_NONE,
+            };
+            CreateDispatcherQueueController(options).unwrap()
         };
+        let main_queue = controller.DispatcherQueue().unwrap();
         let main_thread_id = current().id();
+
         WindowsDispatcher {
-            threadpool,
-            main_sender,
+            controller,
+            main_queue,
             parker,
             main_thread_id,
-            dispatch_event,
         }
     }
 
-    fn dispatch_on_threadpool(&self, runnable: Runnable) {
-        unsafe {
-            let ptr = Box::into_raw(Box::new(runnable));
-            let environment = get_threadpool_environment(self.threadpool);
-            let Ok(work) =
-                CreateThreadpoolWork(Some(threadpool_runner), Some(ptr as _), Some(&environment))
-                    .inspect_err(|_| {
-                        log::error!(
-                            "unable to dispatch work on thread pool: {}",
-                            std::io::Error::last_os_error()
-                        )
-                    })
-            else {
-                return;
-            };
-            SubmitThreadpoolWork(work);
+    fn dispatch_on_threadpool(&self, runnable: Runnable, duration: Option<Duration>) {
+        if let Some(duration) = duration {
+            let task_wrapper = TaskWrapper(runnable.into_raw().as_ptr() as *mut c_void);
+            let handler = TimerElapsedHandler::new(move |param| {
+                let task = unsafe {
+                    let captured = task_wrapper;
+                    Runnable::<()>::from_raw(std::ptr::NonNull::new_unchecked(
+                        captured.0 as *mut (),
+                    ))
+                };
+                task.run();
+                Ok(())
+            });
+            let timer =
+            // A time period expressed in 100-nanosecond units.
+            // 10,000,000 ticks per second
+                ThreadPoolTimer::CreateTimer(&handler, TimeSpan { Duration: (duration.as_nanos() / 100) as i64}).unwrap();
+        } else {
+            let task_wrapper = TaskWrapper(runnable.into_raw().as_ptr() as *mut c_void);
+            let handler = WorkItemHandler::new(move |param| {
+                let task = unsafe {
+                    let captured = task_wrapper;
+                    Runnable::<()>::from_raw(std::ptr::NonNull::new_unchecked(
+                        captured.0 as *mut (),
+                    ))
+                };
+                task.run();
+                Ok(())
+            });
+            let x = ThreadPool::RunWithPriorityAndOptionsAsync(
+                &handler,
+                WorkItemPriority::High,
+                WorkItemOptions::TimeSliced,
+            )
+            .unwrap();
         }
     }
+}
+
+impl Drop for WindowsDispatcher {
+    fn drop(&mut self) {
+        self.controller.ShutdownQueueAsync().log_err();
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct TaskWrapper(*mut c_void);
+
+unsafe impl Send for TaskWrapper {}
+unsafe impl Sync for TaskWrapper {}
+
+#[derive(Debug)]
+struct ThreadpoolTaskWrapper {
+    runnable: Runnable,
+    duration: Option<Duration>,
 }
 
 impl PlatformDispatcher for WindowsDispatcher {
@@ -75,45 +132,27 @@ impl PlatformDispatcher for WindowsDispatcher {
     }
 
     fn dispatch(&self, runnable: Runnable, label: Option<TaskLabel>) {
-        self.dispatch_on_threadpool(runnable);
+        self.dispatch_on_threadpool(runnable, None);
         if let Some(label) = label {
             log::debug!("TaskLabel: {label:?}");
         }
     }
 
     fn dispatch_on_main_thread(&self, runnable: Runnable) {
-        self.main_sender
-            .send(runnable)
-            .inspect_err(|e| log::error!("Dispatch failed: {e}"))
-            .ok();
-        unsafe { SetEvent(self.dispatch_event) }.ok();
+        let task_wrapper = TaskWrapper(runnable.into_raw().as_ptr() as *mut c_void);
+        let runner = DispatcherQueueHandler::new(move || {
+            let task = unsafe {
+                let captured = task_wrapper;
+                Runnable::<()>::from_raw(std::ptr::NonNull::new_unchecked(captured.0 as *mut ()))
+            };
+            task.run();
+            Ok(())
+        });
+        self.main_queue.TryEnqueue(&runner).log_err();
     }
 
     fn dispatch_after(&self, duration: std::time::Duration, runnable: Runnable) {
-        if duration.as_millis() == 0 {
-            self.dispatch_on_threadpool(runnable);
-            return;
-        }
-        unsafe {
-            let mut handle = std::mem::zeroed();
-            let task = Arc::new(DelayedTask::new(runnable));
-            let _ = CreateTimerQueueTimer(
-                &mut handle,
-                None,
-                Some(timer_queue_runner),
-                Some(Arc::into_raw(task.clone()) as _),
-                duration.as_millis() as u32,
-                0,
-                WT_EXECUTEONLYONCE,
-            )
-            .inspect_err(|_| {
-                log::error!(
-                    "unable to dispatch delayed task: {}",
-                    std::io::Error::last_os_error()
-                )
-            });
-            task.raw_timer_handle.store(handle.0, Ordering::SeqCst);
-        }
+        self.dispatch_on_threadpool(runnable, Some(duration));
     }
 
     fn tick(&self, _background_only: bool) -> bool {
@@ -129,37 +168,22 @@ impl PlatformDispatcher for WindowsDispatcher {
     }
 }
 
+fn main_queue_runner() -> windows::core::Result<()> {
+    Ok(())
+}
+
 extern "system" fn threadpool_runner(
     _: PTP_CALLBACK_INSTANCE,
     ptr: *mut std::ffi::c_void,
     _: PTP_WORK,
 ) {
     unsafe {
-        let runnable = Box::from_raw(ptr as *mut Runnable);
-        runnable.run();
-    }
-}
-
-unsafe extern "system" fn timer_queue_runner(ptr: *mut std::ffi::c_void, _: BOOLEAN) {
-    let task = Arc::from_raw(ptr as *mut DelayedTask);
-    task.runnable.lock().take().unwrap().run();
-    unsafe {
-        let timer = task.raw_timer_handle.load(Ordering::SeqCst);
-        let _ = DeleteTimerQueueTimer(None, HANDLE(timer), None);
-    }
-}
-
-struct DelayedTask {
-    runnable: Mutex<Option<Runnable>>,
-    raw_timer_handle: AtomicIsize,
-}
-
-impl DelayedTask {
-    pub fn new(runnable: Runnable) -> Self {
-        DelayedTask {
-            runnable: Mutex::new(Some(runnable)),
-            raw_timer_handle: AtomicIsize::new(0),
+        let ThreadpoolTaskWrapper { runnable, duration } =
+            *Box::from_raw(ptr as *mut ThreadpoolTaskWrapper);
+        if let Some(duration) = duration {
+            std::thread::sleep(duration);
         }
+        runnable.run();
     }
 }
 
@@ -168,7 +192,7 @@ fn get_threadpool_environment(pool: PTP_POOL) -> TP_CALLBACK_ENVIRON_V3 {
     TP_CALLBACK_ENVIRON_V3 {
         Version: 3, // Win7+, otherwise this value should be 1
         Pool: pool,
-        CallbackPriority: TP_CALLBACK_PRIORITY_NORMAL,
+        CallbackPriority: TP_CALLBACK_PRIORITY_HIGH,
         Size: std::mem::size_of::<TP_CALLBACK_ENVIRON_V3>() as _,
         ..Default::default()
     }
