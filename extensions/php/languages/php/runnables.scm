74ea4deaa4a3c01e305ddefb5f92620fa537876f
Remco Smits <djsmits12@gmail.com>
2024-05-08 10:06:34 +0200

    Add support for @test annotation

--- extensions/php/languages/php/runnables.scm
+++ extensions/php/languages/php/runnables.scm
@@ -21,6 +21,29 @@
 
 ; Class that follow the naming convention of PHPUnit test classes
 ; and that doesn't have the abstract modifier
+; and have a method that has the @test annotation
+; and the method is public
+(
+    (class_declaration
+        modifier: (_)? @modifier
+        (#not-eq? @modifier "abstract")
+        name: (_) @name
+        (#match? @name ".*Test")
+        body: (declaration_list
+            ((comment) @comment
+                (#match? @comment ".*@test.*")
+            .
+            (method_declaration
+                (visibility_modifier)? @visibility
+                (#eq? @visibility "public")
+                name: (_) @run
+            ))
+        )
+    )
+) @phpunit-test
+
+; Class that follow the naming convention of PHPUnit test classes
+; and that doesn't have the abstract modifier
 (
     (class_declaration
         modifier: (_)? @modifier
