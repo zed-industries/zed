eaa7d3d6ed73fa431d1ad13e0241d378a54f512a
Remco Smits <djsmits12@gmail.com>
2024-05-08 20:59:18 +0200

    Add `describe`, `it` and `test` function call expression to PHP outline scheme
    This will allow to run the Pest runnable. Because before this change
    These function call expressions were not recognized as symbols.
    
    This resulted in that the Symbol key inside the task context not being set.

--- extensions/php/languages/php/outline.scm
+++ extensions/php/languages/php/outline.scm
@@ -27,3 +27,31 @@
     "trait" @context
     name: (_) @name
     ) @item
+
+; Add support for Pest runnable (non chainable method)
+(expression_statement
+    (function_call_expression
+        function: (_) @context
+        (#any-of? @context "it" "test" "describe")
+        arguments: (arguments
+            (argument
+                (encapsed_string (string_value) @name)
+            )
+        )
+    )
+) @item
+
+; Add support for Pest runnable (chainable method)
+(expression_statement
+    (member_call_expression
+        object: (function_call_expression
+            function: (_) @context
+            (#any-of? @context "it" "test" "describe")
+            arguments: (arguments
+                (argument
+                    (encapsed_string (string_value) @name)
+                )
+            )
+        )
+    )
+) @item
