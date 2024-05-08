ec9422245f4f4071bec1bae5ddf4420c1c65d162
Remco Smits <djsmits12@gmail.com>
2024-05-08 23:50:53 +0200

    Clean up runnable queries

--- extensions/php/languages/php/runnables.scm
+++ extensions/php/languages/php/runnables.scm
@@ -1,20 +1,18 @@
-(
-    ; Class that follow the naming convention of PHPUnit test classes
-    ; and that doesn't have the abstract modifier
-    ; and have a method that follow the naming convention of PHPUnit test methods
-    ; and the method is public
-    (class_declaration
-        modifier: (_)? @modifier
-        (#not-eq? @modifier "abstract")
-        name: (_) @name
-        (#match? @name ".*Test$")
-        body: (declaration_list
-            (method_declaration
-                (visibility_modifier)? @visibility
-                (#eq? @visibility "public")
-                name: (_) @run
-                (#match? @run "^test.*")
-            )
+; Class that follow the naming convention of PHPUnit test classes
+; and that doesn't have the abstract modifier
+; and have a method that follow the naming convention of PHPUnit test methods
+; and the method is public
+(class_declaration
+    modifier: (_)? @modifier
+    (#not-eq? @modifier "abstract")
+    name: (_) @name
+    (#match? @name ".*Test$")
+    body: (declaration_list
+        (method_declaration
+            (visibility_modifier)? @visibility
+            (#eq? @visibility "public")
+            name: (_) @run
+            (#match? @run "^test.*")
         )
     )
 ) @phpunit-test
@@ -23,35 +21,31 @@
 ; and that doesn't have the abstract modifier
 ; and have a method that has the @test annotation
 ; and the method is public
-(
-    (class_declaration
-        modifier: (_)? @modifier
-        (#not-eq? @modifier "abstract")
-        name: (_) @name
-        (#match? @name ".*Test$")
-        body: (declaration_list
-            ((comment) @comment
-                (#match? @comment ".*@test\\b.*")
-            .
-            (method_declaration
-                (visibility_modifier)? @visibility
-                (#eq? @visibility "public")
-                name: (_) @run
-                (#not-match? @run "^test.*")
-            ))
-        )
+(class_declaration
+    modifier: (_)? @modifier
+    (#not-eq? @modifier "abstract")
+    name: (_) @name
+    (#match? @name ".*Test$")
+    body: (declaration_list
+        ((comment) @comment
+            (#match? @comment ".*@test\\b.*")
+        .
+        (method_declaration
+            (visibility_modifier)? @visibility
+            (#eq? @visibility "public")
+            name: (_) @run
+            (#not-match? @run "^test.*")
+        ))
     )
 ) @phpunit-test
 
 ; Class that follow the naming convention of PHPUnit test classes
 ; and that doesn't have the abstract modifier
-(
-    (class_declaration
-        modifier: (_)? @modifier
-        (#not-eq? @modifier "abstract")
-        name: (_) @run
-        (#match? @run ".*Test$")
-    )
+(class_declaration
+    modifier: (_)? @modifier
+    (#not-eq? @modifier "abstract")
+    name: (_) @run
+    (#match? @run ".*Test$")
 ) @phpunit-test
 
 ; Add support for Pest runnable (non chainable method)
