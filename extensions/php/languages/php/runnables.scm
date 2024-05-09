2fac4f8256819c31b8d356c6f7d7f34bc641659d
Remco Smits <djsmits12@gmail.com>
2024-05-09 17:54:19 +0200

    Add _ to all not needed captures

--- extensions/php/languages/php/runnables.scm
+++ extensions/php/languages/php/runnables.scm
@@ -3,14 +3,14 @@
 ; and have a method that follow the naming convention of PHPUnit test methods
 ; and the method is public
 (class_declaration
-    modifier: (_)? @modifier
-    (#not-eq? @modifier "abstract")
-    name: (_) @name
-    (#match? @name ".*Test$")
+    modifier: (_)? @_modifier
+    (#not-eq? @_modifier "abstract")
+    name: (_) @_name
+    (#match? @_name ".*Test$")
     body: (declaration_list
         (method_declaration
-            (visibility_modifier)? @visibility
-            (#eq? @visibility "public")
+            (visibility_modifier)? @_visibility
+            (#eq? @_visibility "public")
             name: (_) @run
             (#match? @run "^test.*")
         )
@@ -22,17 +22,17 @@
 ; and have a method that has the @test annotation
 ; and the method is public
 (class_declaration
-    modifier: (_)? @modifier
-    (#not-eq? @modifier "abstract")
-    name: (_) @name
-    (#match? @name ".*Test$")
+    modifier: (_)? @_modifier
+    (#not-eq? @_modifier "abstract")
+    name: (_) @_name
+    (#match? @_name ".*Test$")
     body: (declaration_list
-        ((comment) @comment
-            (#match? @comment ".*@test\\b.*")
+        ((comment) @_comment
+            (#match? @_comment ".*@test\\b.*")
         .
         (method_declaration
-            (visibility_modifier)? @visibility
-            (#eq? @visibility "public")
+            (visibility_modifier)? @_visibility
+            (#eq? @_visibility "public")
             name: (_) @run
             (#not-match? @run "^test.*")
         ))
@@ -45,20 +45,20 @@
 ; and have a method that has the #[Test] attribute
 ; and the method is public
 (class_declaration
-    modifier: (_)? @modifier
-    (#not-eq? @modifier "abstract")
-    name: (_) @name
-    (#match? @name ".*Test$")
+    modifier: (_)? @_modifier
+    (#not-eq? @_modifier "abstract")
+    name: (_) @_name
+    (#match? @_name ".*Test$")
     body: (declaration_list
         (method_declaration
             (attribute_list
                 (attribute_group
-                    (attribute (name) @attribute)
+                    (attribute (name) @_attribute)
                 )
             )
-            (#eq? @attribute "Test")
-            (visibility_modifier)? @visibility
-            (#eq? @visibility "public")
+            (#eq? @_attribute "Test")
+            (visibility_modifier)? @_visibility
+            (#eq? @_visibility "public")
             name: (_) @run
             (#not-match? @run "^test.*")
         )
@@ -68,8 +68,8 @@
 ; Class that follow the naming convention of PHPUnit test classes
 ; and that doesn't have the abstract modifier
 (class_declaration
-    modifier: (_)? @modifier
-    (#not-eq? @modifier "abstract")
+    modifier: (_)? @_modifier
+    (#not-eq? @_modifier "abstract")
     name: (_) @run
     (#match? @run ".*Test$")
 ) @phpunit-test
@@ -77,8 +77,8 @@
 ; Add support for Pest runnable
 ; Function expression that has `it`, `test` or `describe` as the function name
 (function_call_expression
-    function: (_) @name
-    (#any-of? @name "it" "test" "describe")
+    function: (_) @_name
+    (#any-of? @_name "it" "test" "describe")
     arguments: (arguments
         (argument
             (encapsed_string (string_value) @run)
