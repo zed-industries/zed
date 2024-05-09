41b304c70af20c22cd80be71a5b9845fef854c7e
Remco Smits <djsmits12@gmail.com>
2024-05-09 14:51:49 +0200

    Add support for PHPUnit #[Test] attributes

--- extensions/php/languages/php/runnables.scm
+++ extensions/php/languages/php/runnables.scm
@@ -39,6 +39,32 @@
     )
 ) @phpunit-test
 
+
+; Class that follow the naming convention of PHPUnit test classes
+; and that doesn't have the abstract modifier
+; and have a method that has the #[Test] attribute
+; and the method is public
+(class_declaration
+    modifier: (_)? @modifier
+    (#not-eq? @modifier "abstract")
+    name: (_) @name
+    (#match? @name ".*Test$")
+    body: (declaration_list
+        (method_declaration
+            (attribute_list
+                (attribute_group
+                    (attribute (name) @attribute)
+                )
+            )
+            (#eq? @attribute "Test")
+            (visibility_modifier)? @visibility
+            (#eq? @visibility "public")
+            name: (_) @run
+            (#not-match? @run "^test.*")
+        )
+    )
+) @phpunit-test
+
 ; Class that follow the naming convention of PHPUnit test classes
 ; and that doesn't have the abstract modifier
 (class_declaration
