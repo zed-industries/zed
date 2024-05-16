645334593f6be61691ddccd353012f4f6d2e556e
Marshall Bowers <elliott.codes@gmail.com>
2024-05-16 12:41:00 -0400

    Update doc comment

--- crates/gpui/src/global.rs
+++ crates/gpui/src/global.rs
@@ -2,12 +2,23 @@ use crate::{AppContext, BorrowAppContext};
 
 /// A marker trait for types that can be stored in GPUI's global state.
 ///
-/// This trait exists to provide type-safe access to globals by restricting
-/// the scope from which they can be accessed. For instance, the actual type
-/// that implements [`Global`] can be private, with public accessor functions
-/// that enforce correct usage.
+/// This trait exists to provide type-safe access to globals by ensuring only
+/// types that implement [`Global`] can be used with the accessor methods. For
+/// example, trying to access a global with a type that does not implement
+/// [`Global`] will result in a compile-time error.
 ///
 /// Implement this on types you want to store in the context as a global.
+///
+/// ## Restricting Access to Globals
+///
+/// In some situations you may need to store some global state, but want to
+/// restrict access to reading it or writing to it.
+///
+/// In these cases, Rust's visibility system can be used to restrict access to
+/// a global value. For example, you can create a private struct that implements
+/// [`Global`] and holds the global state. Then create a newtype struct that wraps
+/// the global type and create custom accessor methods to expose the desired subset
+/// of operations.
 pub trait Global: 'static {
     // This trait is intentionally left empty, by virtue of being a marker trait.
     //
