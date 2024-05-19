45d6ec16ad316ec93f5d40422c1ffc6c81224c2e
blufony <code@blufony.dev>
2024-05-19 22:31:00 +0200

    helix: refactor motion

--- crates/vim/src/helix.rs
+++ crates/vim/src/helix.rs
@@ -1,4 +1,5 @@
 use editor::{movement, scroll::Autoscroll};
+use language::{char_kind, CharKind};
 use ui::WindowContext;
 
 use crate::{motion::Motion, normal::normal_motion, visual::visual_motion, Vim};
@@ -20,7 +21,7 @@ pub fn helix_normal_motion(motion: Motion, times: Option<usize>, cx: &mut Window
             next_word(motion, cx);
         }
         Motion::PreviousWordStart { .. } => {
-            // TODO not accurate
+            // TODO same problem as NextWordEnd (single punctuation in word)
             clear_selection(cx);
             normal_motion(Motion::Left, None, None, cx);
             visual_motion(motion.to_owned(), None, cx);
@@ -34,53 +35,40 @@ pub fn helix_normal_motion(motion: Motion, times: Option<usize>, cx: &mut Window
 
 // calling this with motion other than NextWordStart or NextWordEnd panicks
 fn next_word(motion: Motion, cx: &mut WindowContext) {
-    // TODO: inner WORD boundries (like . in abc.abc) cause massive problems
+    // TODO: single punctuation in word are skipped in NextWordEnd
 
     // exceptions from simple "select from next char to delimiter"
     let mut select_cur = false;
     let mut rev = false;
-    Vim::update(cx, |vim, cx| {
-        vim.update_active_editor(cx, |_, editor, cx| {
-            editor.change_selections(Some(Autoscroll::fit()), cx, |s| {
-                s.move_with(|map, selection| {
-                    let mut current_char = ' ';
-                    let mut next_char = ' ';
-
-                    // find chars around cursor
-                    let mut chars = map.display_chars_at(selection.start);
-                    if !selection.reversed {
-                        for ch in chars {
-                            if ch.1 == selection.head() {
-                                next_char = ch.0;
-                                break;
-                            } else {
-                                current_char = ch.0;
-                            }
-                        }
-                        match motion {
-                            Motion::NextWordStart { .. } => {
-                                if !(current_char == ' ' && next_char != ' ') {
-                                    select_cur = true;
-                                }
-                            }
-                            Motion::NextWordEnd { .. } => {
-                                if !(current_char != ' ' && next_char == ' ') {
-                                    select_cur = true;
-                                }
-                            }
-                            _ => todo!("error"),
-                        }
-                    } else {
-                        if let (Some(cur), Some(next)) = (chars.next(), chars.next()) {
-                            if cur.0 == ' ' && next.0 != ' ' {
-                                rev = true;
-                            }
-                        }
-                    }
-                });
-            });
-        });
-    });
+    let (left_kind, right_kind) = get_left_right_kind(cx);
+    if !is_selection_reverse(cx) {
+        match motion {
+            Motion::NextWordStart { .. } => {
+                if !(left_kind == CharKind::Whitespace && right_kind != CharKind::Whitespace) {
+                    select_cur = true;
+                }
+            }
+            Motion::NextWordEnd { .. } => {
+                if !(left_kind != CharKind::Whitespace && right_kind == CharKind::Whitespace) {
+                    select_cur = true;
+                }
+            }
+            _ => todo!("error"),
+        }
+        if (left_kind == CharKind::Punctuation || right_kind == CharKind::Punctuation)
+            && left_kind != right_kind
+        {
+            select_cur = false;
+        }
+    } else {
+        if left_kind == CharKind::Whitespace && right_kind != CharKind::Whitespace {
+            rev = true;
+        }
+    }
+    println!(
+        "{:?}, {:?}; rev: {rev}, sel_cur: {select_cur}",
+        left_kind, right_kind
+    );
     if select_cur {
         select_current(cx);
     } else if rev {
@@ -94,6 +82,55 @@ fn next_word(motion: Motion, cx: &mut WindowContext) {
         Motion::NextWordStart { .. } => visual_motion(Motion::Left, None, cx),
         _ => {}
     }
+}
+
+fn is_selection_reverse(cx: &mut WindowContext) -> bool {
+    let mut rev = false;
+    Vim::update(cx, |vim, cx| {
+        vim.update_active_editor(cx, |_, editor, cx| {
+            editor.change_selections(Some(Autoscroll::fit()), cx, |s| {
+                s.move_with(|_, selection| {
+                    rev = selection.reversed;
+                });
+            });
+        });
+    });
+    rev
+}
+
+fn get_left_right_kind(cx: &mut WindowContext) -> (CharKind, CharKind) {
+    let mut right_kind = CharKind::Whitespace;
+    let mut left_kind = CharKind::Whitespace;
+    Vim::update(cx, |vim, cx| {
+        vim.update_active_editor(cx, |_, editor, cx| {
+            editor.change_selections(Some(Autoscroll::fit()), cx, |s| {
+                s.move_with(|map, selection| {
+                    let scope = map
+                        .buffer_snapshot
+                        .language_scope_at(selection.end.to_point(map));
+
+                    // find chars around cursor
+                    let mut chars = map.display_chars_at(selection.start);
+                    if !selection.reversed {
+                        for ch in chars {
+                            if ch.1 == selection.head() {
+                                right_kind = char_kind(&scope, ch.0);
+                                break;
+                            } else {
+                                left_kind = char_kind(&scope, ch.0);
+                            }
+                        }
+                    } else {
+                        if let (Some(left), Some(right)) = (chars.next(), chars.next()) {
+                            (left_kind, right_kind) =
+                                (char_kind(&scope, left.0), char_kind(&scope, right.0));
+                        }
+                    }
+                });
+            });
+        });
+    });
+    (left_kind, right_kind)
 }
 
 fn clear_selection(cx: &mut WindowContext) {
