1e18bcb949f02b398e21c10bc873d60806caceff
CharlesChen0823 <yongchen0823@gmail.com>
2024-05-21 09:17:11 +0800

    vim: Fix %s replace not working more than twice (#12045)
    close: #11981 
    
    Release Notes:
    
    - N/A
    
    ---------
    
    Co-authored-by: Conrad Irwin <conrad.irwin@gmail.com>

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -9983,6 +9983,10 @@ impl Editor {
         )
     }
 
+    pub fn clear_search_within_ranges(&mut self, cx: &mut ViewContext<Self>) {
+        self.clear_background_highlights::<SearchWithinRange>(cx);
+    }
+
     pub fn highlight_background<T: 'static>(
         &mut self,
         ranges: &[Range<Anchor>],
