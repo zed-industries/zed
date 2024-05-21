a6313fbb4890b71e40db7d406db8e455fb58e15c
Conrad Irwin <conrad.irwin@gmail.com>
2024-05-20 21:01:04 -0600

    Do some ssh tango to support `gh cs ssh` etc.

--- crates/recent_projects/src/dev_servers.rs
+++ crates/recent_projects/src/dev_servers.rs
@@ -1,5 +1,8 @@
 use std::env;
 use std::env::temp_dir;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::time::Duration;
 
 use anyhow::Context;
@@ -303,22 +306,16 @@ impl DevServerProjects {
                             format!(r#"cd; (curl -sSL https://zed.dev/install.sh || wget -qO- https://zed.dev/install.sh) | bash && ~/.local/bin/zed --dev-server-token {}"#, dev_server.access_token)
                         };
 
-                        if let Ok(real_ssh) = which::which("ssh") {
-                            let tmpd = temp_dir();
-                            let file = temp_dir().join("ssh");
-                            dbg!(&file);
-                            if let Ok(mut file) = File::create(file) {
-                                if let Err(e) = file.write_all("#!/bin/bash\n{} \"$@\" -R 8080:localhost:8080 sh -c {}", real_ssh, shlex::try_quote(install_command).unwrap()) {
-                                    eprintln!("Failed to write to file: {}", e);
-                                }
-                            } else {
-                                eprintln!("Failed to create file");
-                            }
-                            let mut perms = file.metadata()?.permissions();
-                            perms.set_mode(0o755); // Set the executable bit
-                            file.set_permissions(perms)?;
-
-                            let  path = format!("{}:{}", tmpd.to_string_lossy(), os::env::var("PATH").unwrap())
+                        let real_ssh = which::which("ssh")?;
+                        let tmpd = temp_dir();
+                        let path = temp_dir().join("ssh");
+                        dbg!(&path);
+                        let mut file = File::create(path.clone())?;
+                        write!(&mut file, "#!/bin/bash\nexec {} \"$@\" -R 8080:localhost:8080 sh -c {}", real_ssh.to_string_lossy(), shlex::try_quote(&install_command)?)?;
+                        let mut perms = file.metadata()?.permissions();
+                        perms.set_mode(0o755); // Set the executable bit
+                        std::fs::set_permissions(path, perms)?;
+                        let  path = format!("{}:{}", tmpd.to_string_lossy(), env::var("PATH")?);
 
                         let mut args = shlex::split(&ssh_connection_string).unwrap_or_default();
                         let command = args.drain(0..1).next().unwrap_or("ssh".to_string());
@@ -333,7 +330,7 @@ impl DevServerProjects {
                                     args,
                                     command_label: ssh_connection_string,
                                     cwd: None,
-                                    env: [("PATH".to_string(), path)].iter().collect(),
+                                    env: [("PATH".to_string(), path)].into_iter().collect(),
                                     use_new_terminal: true,
                                     allow_concurrent_runs: false,
                                     reveal: RevealStrategy::Always,
@@ -342,10 +339,8 @@ impl DevServerProjects {
                                 cx,
                             );
                         })?;
-                        }
                     } else {
                         dbg!("noo!");
-                    }
                     }
 
                     this.update(&mut cx, |this, cx| {
@@ -354,7 +349,9 @@ impl DevServerProjects {
                                 creating: false,
                                 dev_server: Some(dev_server.clone()),
                         });
-                    })
+                    })?;
+                }
+                Ok(())
             }
             Err(e) => {
                 this.update(&mut cx, |this, cx| {
