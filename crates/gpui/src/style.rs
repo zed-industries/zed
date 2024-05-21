0588039f544d36c124081c1246c34d1dbd38d66d
Junkui Zhang <364772080@qq.com>
2024-05-21 20:29:39 +0800

    fix

--- crates/gpui/src/style.rs
+++ crates/gpui/src/style.rs
@@ -6,9 +6,9 @@ use std::{
 
 use crate::{
     black, phi, point, quad, rems, AbsoluteLength, Bounds, ContentMask, Corners, CornersRefinement,
-    CursorStyle, DefiniteLength, Edges, EdgesRefinement, Font, FontFeatures, FontStyle, FontWeight,
-    Hsla, Length, Pixels, Point, PointRefinement, Rgba, SharedString, Size, SizeRefinement, Styled,
-    TextRun, WindowContext,
+    CursorStyle, DefiniteLength, Edges, EdgesRefinement, Font, FontFallbacks, FontFeatures,
+    FontStyle, FontWeight, Hsla, Length, Pixels, Point, PointRefinement, Rgba, SharedString, Size,
+    SizeRefinement, Styled, TextRun, WindowContext,
 };
 use collections::HashSet;
 use refineable::Refineable;
@@ -203,6 +203,9 @@ pub struct TextStyle {
 
     /// How to handle whitespace in the text
     pub white_space: WhiteSpace,
+
+    /// TODO:
+    pub fallbacks: FontFallbacks,
 }
 
 impl Default for TextStyle {
@@ -224,6 +227,7 @@ impl Default for TextStyle {
             underline: None,
             strikethrough: None,
             white_space: WhiteSpace::Normal,
+            fallbacks: FontFallbacks::None,
         }
     }
 }
@@ -269,6 +273,7 @@ impl TextStyle {
             features: self.font_features.clone(),
             weight: self.font_weight,
             style: self.font_style,
+            fallbacks: self.fallbacks,
         }
     }
 
@@ -286,6 +291,7 @@ impl TextStyle {
                 features: Default::default(),
                 weight: self.font_weight,
                 style: self.font_style,
+                fallbacks: self.fallbacks,
             },
             color: self.color,
             background_color: self.background_color,
