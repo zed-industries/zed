40b8f6371f05b35eb7dd18a0653675d8c7e088cb
Owen Law <owenlaw222@gmail.com>
2024-05-22 17:54:42 -0400

    Cleanup `convert-release-notes.py`

--- script/flatpak/convert-release-notes.py
+++ script/flatpak/convert-release-notes.py
@@ -1,88 +1,92 @@
 import re
 import requests
 import sys
-import json
+import textwrap
+import os
 
-def convert_line(line: str, wrap_in_code: bool) -> (bool, bool, bool, str):
+def clean_line(line: str) -> str:
+    line = re.sub(r"<", "&lt;", line)
+    line = re.sub(r">", "&gt;", line)
+    line = re.sub(r"\(\[(#\d+)\]\([\w|\d\:|\/|\.|\-|_]*\)\)", lambda match: f"[{match.group(1)}]", line)
+    line = re.sub(r"\[(#\d+)\]\([\w|\d\:|\/|\.|\-|_]*\)", lambda match: f"[{match.group(1)}]", line)
     line = line.strip()
-    if not line:
-        return (True, False, False, "")
-    
-    line = re.sub(r'<', '&lt;', line)
-    line = re.sub(r'>', '&gt;', line)
 
-    is_code_fence = re.search(r'^```\w*', line)
-    wrap_in_code = wrap_in_code or is_code_fence
-    if not wrap_in_code:
-        line = re.sub(r'(`[^`]*`)', lambda match: f'<code>{match.group(1)}</code>', line)
-    
-    line = re.sub(r'\(\[#\d+\]\([\w|\d|:|\/|\.|\-|_]*\)\)', "", line)
-    line = re.sub(r'\s(\.)$', lambda match: match.group(1), line.strip())
-    
-    if re.search(r'\[[\w|\d|:|\/|\.|\-|_]*\]\([\w|\d|:|\/|\.|\-|_]*\)', line):
-        return (True, False, False, "")
-
-    match line[0]:
-        case "-":
-            return (False, True, False, f"    <li>{line[1:].strip()}</li>")
-        case "#":
-            return (False, False, False, f"<p><em>{line}</em></p>")
-        case _:
-            return (False, False, is_code_fence, f"<li><code>    {line}</code></li>" if wrap_in_code else f"<p>{line}</p>")
+    return line
 
 
-def get_release_info(tag, channel):
+def convert_body(body: str) -> str:
+    formatted = ""
+
+    in_code_fence = False
+    in_list = False
+    for line in body.splitlines():
+        line = clean_line(line)
+        if not line:
+            continue
+        if re.search(r'\[[\w|\d|:|\/|\.|\-|_]*\]\([\w|\d|:|\/|\.|\-|_]*\)', line):
+            continue
+        line = re.sub(r"(?<!\`)`([^`\n]+)`(?!`)", lambda match: f"<code>{match.group(1)}</code>", line)
+
+        contains_code_fence = bool(re.search(r"```", line))
+        is_list = bool(re.search(r"^-\s*", line))
+
+        if in_list and not is_list:
+            formatted += "</ul>\n"
+        if (not in_code_fence and contains_code_fence) or (not in_list and is_list):
+            formatted += "<ul>\n"
+        in_list = is_list
+        in_code_fence = contains_code_fence != in_code_fence
+
+        if is_list:
+            line = re.sub(r"^-\s*", "", line)
+            line = f"    <li>{line}</li>"
+        elif in_code_fence:
+            line = f"    <li><code>    {line}</code></li>"
+        else:
+            line = f"<p>{line}</p>"
+        formatted += f"{line}\n"
+
+        if (not in_code_fence and contains_code_fence):
+            formatted += "</ul>\n"
+    if in_code_fence or in_list:
+        formatted += "</ul>\n"
+    return formatted
+
+def get_release_info(tag: str):
     url = f"https://api.github.com/repos/zed-industries/zed/releases/tags/{tag}"
     response = requests.get(url)
-
     if response.status_code == 200:
-        release_info = response.json()
-        version = tag.removeprefix("v")
-        date = release_info["published_at"]
-
-        indent = " " * 8
-        print(f"{indent}<release version=\"{version}\" date=\"{date}\">")
-        print(f"{indent}    <description>")
-
-        in_list = False
-        in_code_fence = False
-        for line in release_info["body"].splitlines():
-            empty, is_list, is_code_fence, content = convert_line(line, in_code_fence)
-            if empty:
-                continue
-
-            if not in_list and is_list:
-                print(f"{indent}        <ul>")
-            elif in_list and not is_list:
-                print(f"{indent}        </ul>")
-            in_list = is_list
-
-            if is_code_fence:
-                if in_code_fence:
-                    print(f"{indent}        {content}")
-                    print(f"{indent}        </ul>")
-                    in_code_fence = False
-                    continue
-                else:
-                    print(f"{indent}        <ul>") # we make code blocks list because there is no way to force a single line break
-                    in_code_fence = True
-            
-            print(f"{indent}        {content}")
-        if in_list:
-            print(f"{indent}        </ul>")
-
-        print(f"{indent}    </description>")
-        print(f"{indent}    <url>https://github.com/zed-industries/zed/releases/tag/{tag}</url>")
-        print(f"{indent}</release>")
+        return response.json()
     else:
         print(f"Failed to fetch release info for tag '{tag}'. Status code: {response.status_code}")
+        quit()
+
 
 if __name__ == "__main__":
+    os.chdir(sys.path[0])
+
     if len(sys.argv) != 3:
-        print("Usage: python script.py <release_tag> <release_channel>")
+        print("Usage: python convert-release-notes.py <tag> <channel>")
         sys.exit(1)
 
-    release_tag = sys.argv[1]
-    release_channel = sys.argv[2]
+    tag = sys.argv[1]
+    channel = sys.argv[2]
 
-    get_release_info(release_tag, release_channel)
+    release_info = get_release_info(tag)
+    body = convert_body(release_info["body"])
+    version = tag.removeprefix("v")
+    date = release_info["published_at"]
+
+    release_info_str = f"<release version=\"{version}\" date=\"{date}\">\n"
+    release_info_str += f"    <description>\n"
+    release_info_str += textwrap.indent(body, " " * 8)
+    release_info_str += f"    </description>\n"
+    release_info_str += f"    <url>https://github.com/zed-industries/zed/releases/tag/{tag}</url>\n"
+    release_info_str += "</release>\n"
+
+    channel_releases_file = f"../../crates/zed/resources/flatpak/release-info/{channel}"
+    with open(channel_releases_file) as f:
+        old_release_info = f.read()
+    with open(channel_releases_file, "w") as f:
+        f.write(textwrap.indent(release_info_str, " " * 8) + old_release_info)
+    print(f"Added release notes from {tag} to '{channel_releases_file}'")
