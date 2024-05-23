64a15be98f2e91913b7b75bf5c414201179bc9c1
Junkui Zhang <364772080@qq.com>
2024-05-23 21:38:47 +0800

    remove unused

--- crates/gpui/src/platform/mac/open_type.rs
+++ crates/gpui/src/platform/mac/open_type.rs
@@ -25,33 +25,6 @@ use core_text::{
 use font_kit::font::Font;
 use std::ptr;
 
-pub fn apply_features(font: &mut Font, features: &FontFeatures) {
-    unsafe {
-        let native_font = font.native_font();
-        let feature_array = generate_feature_array(features);
-        let attrs = CFDictionaryCreate(
-            kCFAllocatorDefault,
-            &kCTFontFeatureSettingsAttribute as *const _ as _,
-            &feature_array as *const _ as _,
-            1,
-            &kCFTypeDictionaryKeyCallBacks,
-            &kCFTypeDictionaryValueCallBacks,
-        );
-        CFRelease(feature_array as *const _ as _);
-        let new_descriptor = CTFontDescriptorCreateWithAttributes(attrs);
-        CFRelease(attrs as _);
-        let new_descriptor = CTFontDescriptor::wrap_under_create_rule(new_descriptor);
-        let new_font = CTFontCreateCopyWithAttributes(
-            font.native_font().as_concrete_TypeRef(),
-            0.0,
-            ptr::null(),
-            new_descriptor.as_concrete_TypeRef(),
-        );
-        let new_font = CTFont::wrap_under_create_rule(new_font);
-        *font = Font::from_native_font(&new_font);
-    }
-}
-
 pub fn generate_feature_array(features: &FontFeatures) -> CFMutableArrayRef {
     unsafe {
         let mut feature_array =
