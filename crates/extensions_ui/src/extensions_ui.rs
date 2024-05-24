92784e2ce6610c816b2ac876fd0f052449f965d3
Junkui Zhang <364772080@qq.com>
2024-05-24 16:23:38 +0800

    fix all

--- crates/extensions_ui/src/extensions_ui.rs
+++ crates/extensions_ui/src/extensions_ui.rs
@@ -802,6 +802,7 @@ impl ExtensionsPage {
             },
             font_family: settings.ui_font.family.clone(),
             font_features: settings.ui_font.features.clone(),
+            font_fallbacks: settings.ui_font.fallbacks.clone(),
             font_size: rems(0.875).into(),
             font_weight: settings.ui_font.weight,
             line_height: relative(1.3),
