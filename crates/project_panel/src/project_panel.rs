34101f280683026fbb10a68f96138a4caab84a37
Patryck <PatryckG@outlook.com>
2024-05-24 19:51:17 -0400

    Tooltip text with canon url for symlinks

--- crates/project_panel/src/project_panel.rs
+++ crates/project_panel/src/project_panel.rs
@@ -29,7 +29,7 @@ use std::{
     sync::Arc,
 };
 use theme::ThemeSettings;
-use ui::{prelude::*, v_flex, ContextMenu, Icon, KeyBinding, Label, ListItem};
+use ui::{prelude::*, v_flex, ContextMenu, Icon, KeyBinding, Label, ListItem, Tooltip};
 use unicase::UniCase;
 use util::{maybe, NumericPrefixWithSuffix, ResultExt, TryFutureExt};
 use workspace::{
@@ -103,7 +103,7 @@ pub struct EntryDetails {
     is_cut: bool,
     git_status: Option<GitFileStatus>,
     is_private: bool,
-    is_symlink: bool,
+    canonical_path: Option<PathBuf>,
 }
 
 #[derive(PartialEq, Clone, Default, Debug, Deserialize)]
@@ -1647,7 +1647,7 @@ impl ProjectPanel {
                             .map_or(false, |e| e.is_cut() && e.entry_id() == entry.id),
                         git_status: status,
                         is_private: entry.is_private,
-                        is_symlink: entry.is_symlink(),
+                        canonical_path: entry.canonical_path.clone(),
                     };
 
                     if let Some(edit_state) = &self.edit_state {
@@ -1741,7 +1741,10 @@ impl ProjectPanel {
             }
         }
 
-        let is_symlink = details.is_symlink;
+        let canonical_path = details
+            .canonical_path
+            .clone()
+            .map(|f| f.to_string_lossy().to_string());
 
         let depth = details.depth;
         div()
@@ -1764,8 +1767,9 @@ impl ProjectPanel {
                     .indent_level(depth)
                     .indent_step_size(px(settings.indent_size))
                     .selected(is_selected)
-                    .when(is_symlink, |item| {
-                        item.end_slot::<Icon>(Icon::new(IconName::ArrowRight))
+                    .when_some(canonical_path, |this, path| {
+                        this.end_slot::<Icon>(Icon::new(IconName::ArrowRight))
+                            .tooltip(move |cx| Tooltip::text(path.clone(), cx))
                     })
                     .child(if let Some(icon) = &icon {
                         h_flex().child(Icon::from_path(icon.to_string()).color(filename_text_color))
