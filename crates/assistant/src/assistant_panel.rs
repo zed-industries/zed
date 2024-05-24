b67155e91197dbdc08ca0a2c6f70290fcd55b31d
Marshall Bowers <elliott.codes@gmail.com>
2024-05-24 14:50:49 -0400

    Initialize `lsp_adapter_delegate` when deserializing and in tests

--- crates/assistant/src/assistant_panel.rs
+++ crates/assistant/src/assistant_panel.rs
@@ -1129,6 +1129,13 @@ impl AssistantPanel {
         let slash_commands = self.slash_commands.clone();
         let languages = self.languages.clone();
         let telemetry = self.telemetry.clone();
+
+        let lsp_adapter_delegate = workspace
+            .update(cx, |workspace, cx| {
+                make_lsp_adapter_delegate(workspace.project(), cx)
+            })
+            .log_err();
+
         cx.spawn(|this, mut cx| async move {
             let saved_conversation = SavedConversation::load(&path, fs.as_ref()).await?;
             let model = this.update(&mut cx, |this, _| this.model.clone())?;
@@ -1139,6 +1146,7 @@ impl AssistantPanel {
                 languages,
                 slash_commands,
                 Some(telemetry),
+                lsp_adapter_delegate,
                 &mut cx,
             )
             .await?;
@@ -1579,6 +1587,7 @@ impl Conversation {
         language_registry: Arc<LanguageRegistry>,
         slash_command_registry: Arc<SlashCommandRegistry>,
         telemetry: Option<Arc<Telemetry>>,
+        lsp_adapter_delegate: Option<Arc<dyn LspAdapterDelegate>>,
         cx: &mut AsyncAppContext,
     ) -> Result<Model<Self>> {
         let id = match saved_conversation.id {
@@ -1638,8 +1647,7 @@ impl Conversation {
                 telemetry,
                 language_registry,
                 slash_command_registry,
-                // TODO: Initialize this when deserializing.
-                lsp_adapter_delegate: None,
+                lsp_adapter_delegate,
             };
             this.set_language(cx);
             this.reparse_edit_suggestions(cx);
@@ -2740,14 +2748,7 @@ impl ConversationEditor {
     ) -> Self {
         let telemetry = workspace.read(cx).client().telemetry().clone();
         let project = workspace.read(cx).project().clone();
-        let delegate = project.update(cx, |project, cx| {
-            // TODO: Find the right worktree.
-            let worktree = project
-                .worktrees()
-                .next()
-                .expect("expected at least one worktree");
-            ProjectLspAdapterDelegate::new(project, &worktree, cx)
-        });
+        let lsp_adapter_delegate = make_lsp_adapter_delegate(&project, cx);
 
         let conversation = cx.new_model(|cx| {
             Conversation::new(
@@ -2755,7 +2756,7 @@ impl ConversationEditor {
                 language_registry,
                 slash_command_registry,
                 Some(telemetry),
-                Some(delegate),
+                Some(lsp_adapter_delegate),
                 cx,
             )
         });
@@ -3929,6 +3930,20 @@ fn merge_ranges(ranges: &mut Vec<Range<Anchor>>, buffer: &MultiBufferSnapshot) {
     }
 }
 
+fn make_lsp_adapter_delegate(
+    project: &Model<Project>,
+    cx: &mut AppContext,
+) -> Arc<dyn LspAdapterDelegate> {
+    project.update(cx, |project, cx| {
+        // TODO: Find the right worktree.
+        let worktree = project
+            .worktrees()
+            .next()
+            .expect("expected at least one worktree");
+        ProjectLspAdapterDelegate::new(project, &worktree, cx)
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use std::{cell::RefCell, path::Path, rc::Rc};
@@ -4317,6 +4332,15 @@ mod tests {
             prompt_library.clone(),
         ));
 
+        let lsp_adapter_delegate = project.update(cx, |project, cx| {
+            // TODO: Find the right worktree.
+            let worktree = project
+                .worktrees()
+                .next()
+                .expect("expected at least one worktree");
+            ProjectLspAdapterDelegate::new(project, &worktree, cx)
+        });
+
         let registry = Arc::new(LanguageRegistry::test(cx.executor()));
         let conversation = cx.new_model(|cx| {
             Conversation::new(
@@ -4324,7 +4348,7 @@ mod tests {
                 registry.clone(),
                 slash_command_registry,
                 None,
-                None,
+                Some(lsp_adapter_delegate),
                 cx,
             )
         });
@@ -4668,6 +4692,7 @@ mod tests {
             Default::default(),
             registry.clone(),
             Default::default(),
+            None,
             None,
             &mut cx.to_async(),
         )
