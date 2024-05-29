5bcb9ed0174cd6cebe4bb087a23914d4eb7d5d25
Marshall Bowers <elliott.codes@gmail.com>
2024-05-29 16:05:16 -0400

    Add `rustdoc_to_markdown` crate (#12445)
    This PR adds a new crate for converting rustdoc output to Markdown.
    
    We're leveraging Servo's `html5ever` to parse the Markdown content, and
    then walking the DOM nodes to convert it to a Markdown string.
    
    The Markdown output will be continued to be refined, but it's in a place
    where it should be reasonable.
    
    Release Notes:
    
    - N/A

---
+++ crates/rustdoc_to_markdown/src/rustdoc_to_markdown.rs
@@ -0,0 +1,36 @@
+//! Provides conversion from rustdoc's HTML output to Markdown.
+
+#![deny(missing_docs)]
+
+mod markdown_writer;
+
+use anyhow::{Context, Result};
+use html5ever::driver::ParseOpts;
+use html5ever::parse_document;
+use html5ever::tendril::TendrilSink;
+use html5ever::tree_builder::TreeBuilderOpts;
+use markup5ever_rcdom::RcDom;
+
+use crate::markdown_writer::MarkdownWriter;
+
+/// Converts the provided rustdoc HTML to Markdown.
+pub fn convert_rustdoc_to_markdown(html: &str) -> Result<String> {
+    let parse_options = ParseOpts {
+        tree_builder: TreeBuilderOpts {
+            drop_doctype: true,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+    let dom = parse_document(RcDom::default(), parse_options)
+        .from_utf8()
+        .read_from(&mut html.as_bytes())
+        .context("failed to parse rustdoc HTML")?;
+
+    let markdown_writer = MarkdownWriter::new();
+    let markdown = markdown_writer
+        .run(&dom.document)
+        .context("failed to convert rustdoc to HTML")?;
+
+    Ok(markdown)
+}
