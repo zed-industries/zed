ac0d4e8978e15b82caff33e28947b9e61d63b799
Marshall Bowers <elliott.codes@gmail.com>
2024-05-29 13:37:45 -0400

    Expand `Visitor`

--- crates/rustdoc_to_markdown/src/markdown_writer.rs
+++ crates/rustdoc_to_markdown/src/markdown_writer.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use markup5ever_rcdom::{Handle, NodeData};
+use markup5ever_rcdom::Handle;
 
 use crate::{walk_node, Visitor};
 
@@ -61,32 +61,35 @@ impl Visitor for MarkdownWriter {
     type Error = ();
 
     fn visit_node(&mut self, node: &Handle) -> Result<(), Self::Error> {
-        let mut tag_name = String::new();
-
-        match node.data {
-            NodeData::Document
-            | NodeData::Doctype { .. }
-            | NodeData::ProcessingInstruction { .. }
-            | NodeData::Comment { .. } => {}
-            NodeData::Element { ref name, .. } => {
-                tag_name = name.local.to_string();
-            }
-            NodeData::Text { ref contents } => {
-                let text = contents.borrow().to_string();
-
-                if self.is_inside_heading() {
-                    self.push_str(&format!("# {text}"));
-                    self.push_newline();
-                } else {
-                    self.push_str(&format!("{text}"));
-                    self.push_newline();
-                }
-            }
-        }
-
-        self.push_tag(tag_name);
         walk_node(self, node)?;
         self.pop_tag();
+
+        Ok(())
+    }
+
+    fn visit_element(
+        &mut self,
+        name: &html5ever::QualName,
+        _attrs: &std::cell::RefCell<Vec<html5ever::Attribute>>,
+    ) -> Result<(), Self::Error> {
+        self.push_tag(name.local.to_string());
+
+        Ok(())
+    }
+
+    fn visit_text(
+        &mut self,
+        contents: &std::cell::RefCell<html5ever::tendril::StrTendril>,
+    ) -> Result<(), Self::Error> {
+        let text = contents.borrow().to_string();
+
+        if self.is_inside_heading() {
+            self.push_str(&format!("# {text}"));
+            self.push_newline();
+        } else {
+            self.push_str(&format!("{text}"));
+            self.push_newline();
+        }
 
         Ok(())
     }
