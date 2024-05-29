5bddf28551b467f4ddd29fdd3a6c55e6b24859a3
Marshall Bowers <elliott.codes@gmail.com>
2024-05-29 11:53:47 -0400

    Track the tag hierarchy

--- crates/rustdoc_to_markdown/src/rustdoc_to_markdown.rs
+++ crates/rustdoc_to_markdown/src/rustdoc_to_markdown.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use html5ever::driver::ParseOpts;
 use html5ever::parse_document;
 use html5ever::tendril::TendrilSink;
@@ -18,26 +20,91 @@ pub fn convert_rustdoc_to_markdown() {
         .from_utf8()
         .read_from(&mut html.as_bytes())
         .unwrap();
-    walk(&dom.document);
+    let mut markdown_writer = MarkdownWriter::new();
 
-    dbg!(&dom.errors);
+    walk(&dom.document, &mut markdown_writer);
+
+    println!("{}", markdown_writer.markdown);
 }
 
-fn walk(input: &Handle) {
-    match input.data {
-        NodeData::Document | NodeData::Doctype { .. } | NodeData::ProcessingInstruction { .. } => {}
-        NodeData::Comment { .. } => {}
-        NodeData::Text { ref contents } => {
-            let mut text = contents.borrow().to_string();
-            println!("{text}");
-        }
+fn walk(node: &Handle, output: &mut MarkdownWriter) {
+    let mut tag_name = String::new();
+
+    match node.data {
+        NodeData::Document
+        | NodeData::Doctype { .. }
+        | NodeData::ProcessingInstruction { .. }
+        | NodeData::Comment { .. } => {}
         NodeData::Element { ref name, .. } => {
-            //
+            tag_name = name.local.to_string();
         }
-        _ => {}
+        NodeData::Text { ref contents } => {
+            let text = contents.borrow().to_string();
+
+            if output.is_inside_heading() {
+                output.push_str(&format!("# {text}"));
+                output.push_newline();
+            } else {
+                output.push_str(&format!("{text}"));
+                output.push_newline();
+            }
+        }
     }
 
-    for child in input.children.borrow().iter() {
-        walk(child);
+    output.push_tag(tag_name);
+
+    for child in node.children.borrow().iter() {
+        walk(child, output);
+    }
+
+    output.pop_tag();
+}
+
+struct MarkdownWriter {
+    current_tag_stack: VecDeque<String>,
+    /// The Markdown output.
+    markdown: String,
+}
+
+impl MarkdownWriter {
+    pub fn new() -> Self {
+        Self {
+            current_tag_stack: VecDeque::new(),
+            markdown: String::new(),
+        }
+    }
+
+    pub fn push_tag(&mut self, tag: String) {
+        if tag.is_empty() {
+            return;
+        }
+
+        self.current_tag_stack.push_back(tag);
+    }
+
+    pub fn pop_tag(&mut self) {
+        self.current_tag_stack.pop_back();
+    }
+
+    pub fn is_inside(&self, tag: &str) -> bool {
+        self.current_tag_stack
+            .iter()
+            .any(|parent_tag| parent_tag.contains(tag))
+    }
+
+    pub fn is_inside_heading(&self) -> bool {
+        ["h1", "h2", "h3", "h4", "h5", "h6"]
+            .into_iter()
+            .any(|heading| self.is_inside(heading))
+    }
+
+    /// Appends the given string slice onto the end of the Markdown output.
+    pub fn push_str(&mut self, str: &str) {
+        self.markdown.push_str(str);
+    }
+
+    /// Appends a newline to the end of the Markdown output.
+    pub fn push_newline(&mut self) {
+        self.push_str("\n");
     }
 }
