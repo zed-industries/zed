fb85ea3ac0c94e5d3dc444a354f2983587af6d61
Antonio Scandurra <me@as-cii.com>
2024-05-30 11:27:44 +0200

    Simplify assistant settings

--- crates/assistant/src/assistant_settings.rs
+++ crates/assistant/src/assistant_settings.rs
@@ -144,30 +144,19 @@ pub enum AssistantDockPosition {
     Bottom,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize, JsonSchema, PartialEq)]
-#[serde(tag = "name", rename_all = "snake_case")]
+#[derive(Debug, PartialEq)]
 pub enum AssistantProvider {
-    #[serde(rename = "zed.dev")]
     ZedDotDev {
-        #[serde(default, alias = "default_model")]
         model: ZedDotDevModel,
     },
-    #[serde(rename = "openai")]
     OpenAi {
-        #[serde(default, alias = "default_model")]
         model: OpenAiModel,
-        #[serde(default = "open_ai_url")]
         api_url: String,
-        #[serde(default)]
         low_speed_timeout_in_seconds: Option<u64>,
     },
-    #[serde(rename = "anthropic")]
     Anthropic {
-        #[serde(default, alias = "default_model")]
         model: AnthropicModel,
-        #[serde(default = "anthropic_api_url")]
         api_url: String,
-        #[serde(default)]
         low_speed_timeout_in_seconds: Option<u64>,
     },
 }
@@ -176,21 +165,34 @@ impl Default for AssistantProvider {
     fn default() -> Self {
         Self::OpenAi {
             model: OpenAiModel::default(),
-            api_url: open_ai_url(),
+            api_url: open_ai::OPEN_AI_API_URL.into(),
             low_speed_timeout_in_seconds: None,
         }
     }
 }
 
-fn open_ai_url() -> String {
-    open_ai::OPEN_AI_API_URL.to_string()
+#[derive(Clone, Debug, Serialize, Deserialize, JsonSchema, PartialEq)]
+#[serde(tag = "name", rename_all = "snake_case")]
+pub enum AssistantProviderContent {
+    #[serde(rename = "zed.dev")]
+    ZedDotDev {
+        default_model: Option<ZedDotDevModel>,
+    },
+    #[serde(rename = "openai")]
+    OpenAi {
+        default_model: Option<OpenAiModel>,
+        api_url: Option<String>,
+        low_speed_timeout_in_seconds: Option<u64>,
+    },
+    #[serde(rename = "anthropic")]
+    Anthropic {
+        default_model: Option<AnthropicModel>,
+        api_url: Option<String>,
+        low_speed_timeout_in_seconds: Option<u64>,
+    },
 }
 
-fn anthropic_api_url() -> String {
-    anthropic::ANTHROPIC_API_URL.to_string()
-}
-
-#[derive(Default, Debug, Deserialize, Serialize)]
+#[derive(Debug, Default)]
 pub struct AssistantSettings {
     pub enabled: bool,
     pub button: bool,
@@ -241,16 +243,16 @@ impl AssistantSettingsContent {
                 default_width: settings.default_width,
                 default_height: settings.default_height,
                 provider: if let Some(open_ai_api_url) = settings.openai_api_url.as_ref() {
-                    Some(AssistantProvider::OpenAi {
-                        model: settings.default_open_ai_model.clone().unwrap_or_default(),
-                        api_url: open_ai_api_url.clone(),
+                    Some(AssistantProviderContent::OpenAi {
+                        default_model: settings.default_open_ai_model.clone(),
+                        api_url: Some(open_ai_api_url.clone()),
                         low_speed_timeout_in_seconds: None,
                     })
                 } else {
                     settings.default_open_ai_model.clone().map(|open_ai_model| {
-                        AssistantProvider::OpenAi {
-                            model: open_ai_model,
-                            api_url: open_ai_url(),
+                        AssistantProviderContent::OpenAi {
+                            default_model: Some(open_ai_model),
+                            api_url: None,
                             low_speed_timeout_in_seconds: None,
                         }
                     })
@@ -275,28 +277,52 @@ impl AssistantSettingsContent {
     pub fn set_model(&mut self, new_model: LanguageModel) {
         match self {
             AssistantSettingsContent::Versioned(settings) => match settings {
-                VersionedAssistantSettingsContent::V1(settings) => {
-                    let provider = settings
-                        .provider
-                        .get_or_insert_with(AssistantProvider::default);
-                    match provider {
-                        AssistantProvider::ZedDotDev { model } => {
-                            if let LanguageModel::ZedDotDev(new_model) = new_model {
-                                *model = new_model;
-                            }
-                        }
-                        AssistantProvider::OpenAi { model, .. } => {
-                            if let LanguageModel::OpenAi(new_model) = new_model {
-                                *model = new_model;
-                            }
-                        }
-                        AssistantProvider::Anthropic { model, .. } => {
-                            if let LanguageModel::Anthropic(new_model) = new_model {
-                                *model = new_model;
-                            }
+                VersionedAssistantSettingsContent::V1(settings) => match &mut settings.provider {
+                    Some(AssistantProviderContent::ZedDotDev {
+                        default_model: model,
+                    }) => {
+                        if let LanguageModel::ZedDotDev(new_model) = new_model {
+                            *model = Some(new_model);
                         }
                     }
-                }
+                    Some(AssistantProviderContent::OpenAi {
+                        default_model: model,
+                        ..
+                    }) => {
+                        if let LanguageModel::OpenAi(new_model) = new_model {
+                            *model = Some(new_model);
+                        }
+                    }
+                    Some(AssistantProviderContent::Anthropic {
+                        default_model: model,
+                        ..
+                    }) => {
+                        if let LanguageModel::Anthropic(new_model) = new_model {
+                            *model = Some(new_model);
+                        }
+                    }
+                    provider => match new_model {
+                        LanguageModel::ZedDotDev(model) => {
+                            *provider = Some(AssistantProviderContent::ZedDotDev {
+                                default_model: Some(model),
+                            })
+                        }
+                        LanguageModel::OpenAi(model) => {
+                            *provider = Some(AssistantProviderContent::OpenAi {
+                                default_model: Some(model),
+                                api_url: None,
+                                low_speed_timeout_in_seconds: None,
+                            })
+                        }
+                        LanguageModel::Anthropic(model) => {
+                            *provider = Some(AssistantProviderContent::Anthropic {
+                                default_model: Some(model),
+                                api_url: None,
+                                low_speed_timeout_in_seconds: None,
+                            })
+                        }
+                    },
+                },
             },
             AssistantSettingsContent::Legacy(settings) => {
                 if let LanguageModel::OpenAi(model) = new_model {
@@ -353,7 +379,7 @@ pub struct AssistantSettingsContentV1 {
     ///
     /// This can either be the internal `zed.dev` service or an external `openai` service,
     /// each with their respective default models and configurations.
-    provider: Option<AssistantProvider>,
+    provider: Option<AssistantProviderContent>,
 }
 
 #[derive(Clone, Serialize, Deserialize, JsonSchema, Debug)]
@@ -412,11 +438,11 @@ impl Settings for AssistantSettings {
                 match (&mut settings.provider, provider) {
                     (
                         AssistantProvider::ZedDotDev { model },
-                        AssistantProvider::ZedDotDev {
-                            model: model_override,
+                        AssistantProviderContent::ZedDotDev {
+                            default_model: model_override,
                         },
                     ) => {
-                        *model = model_override;
+                        merge(model, model_override);
                     }
                     (
                         AssistantProvider::OpenAi {
@@ -424,18 +450,69 @@ impl Settings for AssistantSettings {
                             api_url,
                             low_speed_timeout_in_seconds,
                         },
-                        AssistantProvider::OpenAi {
-                            model: model_override,
+                        AssistantProviderContent::OpenAi {
+                            default_model: model_override,
                             api_url: api_url_override,
                             low_speed_timeout_in_seconds: low_speed_timeout_in_seconds_override,
                         },
                     ) => {
-                        *model = model_override;
-                        *api_url = api_url_override;
-                        *low_speed_timeout_in_seconds = low_speed_timeout_in_seconds_override;
+                        merge(model, model_override);
+                        merge(api_url, api_url_override);
+                        if let Some(low_speed_timeout_in_seconds_override) =
+                            low_speed_timeout_in_seconds_override
+                        {
+                            *low_speed_timeout_in_seconds =
+                                Some(low_speed_timeout_in_seconds_override);
+                        }
                     }
-                    (merged, provider_override) => {
-                        *merged = provider_override;
+                    (
+                        AssistantProvider::Anthropic {
+                            model,
+                            api_url,
+                            low_speed_timeout_in_seconds,
+                        },
+                        AssistantProviderContent::Anthropic {
+                            default_model: model_override,
+                            api_url: api_url_override,
+                            low_speed_timeout_in_seconds: low_speed_timeout_in_seconds_override,
+                        },
+                    ) => {
+                        merge(model, model_override);
+                        merge(api_url, api_url_override);
+                        if let Some(low_speed_timeout_in_seconds_override) =
+                            low_speed_timeout_in_seconds_override
+                        {
+                            *low_speed_timeout_in_seconds =
+                                Some(low_speed_timeout_in_seconds_override);
+                        }
+                    }
+                    (provider, provider_override) => {
+                        *provider = match provider_override {
+                            AssistantProviderContent::ZedDotDev {
+                                default_model: model,
+                            } => AssistantProvider::ZedDotDev {
+                                model: model.unwrap_or_default(),
+                            },
+                            AssistantProviderContent::OpenAi {
+                                default_model: model,
+                                api_url,
+                                low_speed_timeout_in_seconds,
+                            } => AssistantProvider::OpenAi {
+                                model: model.unwrap_or_default(),
+                                api_url: api_url.unwrap_or_else(|| open_ai::OPEN_AI_API_URL.into()),
+                                low_speed_timeout_in_seconds,
+                            },
+                            AssistantProviderContent::Anthropic {
+                                default_model: model,
+                                api_url,
+                                low_speed_timeout_in_seconds,
+                            } => AssistantProvider::Anthropic {
+                                model: model.unwrap_or_default(),
+                                api_url: api_url
+                                    .unwrap_or_else(|| anthropic::ANTHROPIC_API_URL.into()),
+                                low_speed_timeout_in_seconds,
+                            },
+                        };
                     }
                 }
             }
@@ -445,7 +522,7 @@ impl Settings for AssistantSettings {
     }
 }
 
-fn merge<T: Copy>(target: &mut T, value: Option<T>) {
+fn merge<T>(target: &mut T, value: Option<T>) {
     if let Some(value) = value {
         *target = value;
     }
@@ -469,7 +546,7 @@ mod tests {
             AssistantSettings::get_global(cx).provider,
             AssistantProvider::OpenAi {
                 model: OpenAiModel::FourOmni,
-                api_url: open_ai_url(),
+                api_url: open_ai::OPEN_AI_API_URL.into(),
                 low_speed_timeout_in_seconds: None,
             }
         );
@@ -511,7 +588,7 @@ mod tests {
             AssistantSettings::get_global(cx).provider,
             AssistantProvider::OpenAi {
                 model: OpenAiModel::Four,
-                api_url: open_ai_url(),
+                api_url: open_ai::OPEN_AI_API_URL.into(),
                 low_speed_timeout_in_seconds: None,
             }
         );
