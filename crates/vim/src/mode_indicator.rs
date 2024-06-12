bcd3422bce359226f19387c809b9dc084aae9c33
Conrad Irwin <conrad.irwin@gmail.com>
2024-06-12 10:16:53 -0600

    clipppppppppy

--- crates/vim/src/mode_indicator.rs
+++ crates/vim/src/mode_indicator.rs
@@ -42,24 +42,14 @@ impl ModeIndicator {
             .pre_count
             .map(|count| format!("{}", count))
             .into_iter()
-            .chain(
-                vim.state()
-                    .selected_register
-                    .map(|reg| format!("\"{reg}"))
-                    .into_iter(),
-            )
+            .chain(vim.state().selected_register.map(|reg| format!("\"{reg}")))
             .chain(
                 vim.state()
                     .operator_stack
                     .iter()
                     .map(|item| item.id().to_string()),
             )
-            .chain(
-                vim.state()
-                    .post_count
-                    .map(|count| format!("{}", count))
-                    .into_iter(),
-            )
+            .chain(vim.state().post_count.map(|count| format!("{}", count)))
             .collect::<Vec<_>>()
             .join("")
     }
