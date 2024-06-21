d321320ae1f308f2e143b19018606ae3db713537
Junkui Zhang <364772080@qq.com>
2024-06-21 17:58:02 +0800

    fix

--- crates/language/src/outline.rs
+++ crates/language/src/outline.rs
@@ -162,6 +162,7 @@ pub fn render_item<T>(
         color: cx.theme().colors().text,
         font_family: settings.buffer_font.family.clone(),
         font_features: settings.buffer_font.features.clone(),
+        font_fallbacks: settings.buffer_font.fallbacks.clone(),
         font_size: settings.buffer_font_size(cx).into(),
         font_weight: settings.buffer_font.weight,
         line_height: relative(1.),
