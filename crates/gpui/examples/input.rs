618bec24e62b9948adf6094caff8389b6c1ca47f
Conrad Irwin <conrad.irwin@gmail.com>
2024-06-25 20:10:25 -0600

    Add an input example to gpui

---
+++ crates/gpui/examples/input.rs
@@ -0,0 +1,425 @@
+use std::ops::Range;
+
+use gpui::*;
+
+actions!(text_input, [Backspace, Delete, Left, Right, Home, End]);
+
+struct TextInput {
+    focus_handle: FocusHandle,
+    content: SharedString,
+    selected_range: Range<usize>,
+    marked_range: Option<Range<usize>>, // New field for marked text range
+    last_layout: Option<ShapedLine>,
+}
+
+impl TextInput {
+    fn offset_utf16to8(&self, offset: usize) -> usize {
+        let mut utf8_offset = 0;
+        let mut utf16_count = 0;
+
+        for ch in self.content.chars() {
+            if utf16_count >= offset {
+                break;
+            }
+            utf16_count += ch.len_utf16();
+            utf8_offset += ch.len_utf8();
+        }
+
+        utf8_offset
+    }
+
+    fn offset_utf8to16(&self, offset: usize) -> usize {
+        let mut utf16_offset = 0;
+        let mut utf8_count = 0;
+
+        for ch in self.content.chars() {
+            if utf8_count >= offset {
+                break;
+            }
+            utf8_count += ch.len_utf8();
+            utf16_offset += ch.len_utf16();
+        }
+
+        utf16_offset
+    }
+    fn offset_utf32to8(&self, offset: usize) -> usize {
+        let mut utf8_offset = 0;
+        let mut utf32_count = 0;
+
+        for ch in self.content.chars() {
+            if utf32_count >= offset {
+                break;
+            }
+            utf32_count += 1;
+            utf8_offset += ch.len_utf8();
+        }
+
+        utf8_offset
+    }
+
+    fn offset_utf8to32(&self, offset: usize) -> usize {
+        let mut utf32_offset = 0;
+        let mut utf8_count = 0;
+
+        for ch in self.content.chars() {
+            if utf8_count >= offset {
+                break;
+            }
+            utf8_count += ch.len_utf8();
+            utf32_offset += 1;
+        }
+
+        utf32_offset
+    }
+
+    fn range_utf8to32(&self, range: &Range<usize>) -> Range<usize> {
+        self.offset_utf8to32(range.start)..self.offset_utf8to32(range.end)
+    }
+
+    fn range_utf32to8(&self, range_utf32: &Range<usize>) -> Range<usize> {
+        self.offset_utf32to8(range_utf32.start)..self.offset_utf32to8(range_utf32.end)
+    }
+    fn range_utf8to16(&self, range: &Range<usize>) -> Range<usize> {
+        self.offset_utf8to16(range.start)..self.offset_utf8to16(range.end)
+    }
+
+    fn range_utf16to8(&self, range_utf16: &Range<usize>) -> Range<usize> {
+        self.offset_utf16to8(range_utf16.start)..self.offset_utf16to8(range_utf16.end)
+    }
+
+    fn left(&mut self, _: &Left, cx: &mut ViewContext<Self>) {
+        dbg!("left");
+        let new_pos = self.offset_utf32to8(
+            self.offset_utf8to32(self.selected_range.end)
+                .saturating_sub(1),
+        );
+        dbg!("new_pos");
+        self.selected_range = new_pos..new_pos;
+        cx.notify()
+    }
+
+    fn right(&mut self, _: &Right, cx: &mut ViewContext<Self>) {
+        let new_pos = self.offset_utf32to8(self.offset_utf8to32(self.selected_range.end) + 1);
+        self.selected_range = new_pos..new_pos;
+        cx.notify()
+    }
+
+    fn home(&mut self, _: &Home, cx: &mut ViewContext<Self>) {
+        self.selected_range = 0..0;
+        cx.notify()
+    }
+
+    fn end(&mut self, _: &End, cx: &mut ViewContext<Self>) {
+        self.selected_range = self.content.len()..self.content.len();
+        cx.notify()
+    }
+
+    fn backspace(&mut self, _: &Backspace, cx: &mut ViewContext<Self>) {
+        if self.selected_range.start == self.selected_range.end {
+            self.selected_range.start = self.offset_utf32to8(
+                self.offset_utf8to32(self.selected_range.end)
+                    .saturating_sub(1),
+            );
+        }
+        self.replace_text_in_range(None, "", cx)
+    }
+
+    fn delete(&mut self, _: &Delete, cx: &mut ViewContext<Self>) {
+        if self.selected_range.start == self.selected_range.end {
+            self.selected_range.end =
+                self.offset_utf32to8(self.offset_utf8to32(self.selected_range.end) + 1);
+        }
+        self.replace_text_in_range(None, "", cx)
+    }
+}
+
+impl ViewInputHandler for TextInput {
+    fn text_for_range(
+        &mut self,
+        range_utf16: Range<usize>,
+        cx: &mut ViewContext<Self>,
+    ) -> Option<String> {
+        let range = self.range_utf16to8(&range_utf16);
+        if range.start <= range.end && range.end <= self.content.len() {
+            Some(self.content[range].to_string())
+        } else {
+            None
+        }
+    }
+
+    fn selected_text_range(&mut self, cx: &mut ViewContext<Self>) -> Option<Range<usize>> {
+        Some(self.range_utf8to16(&self.selected_range))
+    }
+
+    fn marked_text_range(&self, cx: &mut ViewContext<Self>) -> Option<Range<usize>> {
+        self.marked_range
+            .as_ref()
+            .map(|range| self.range_utf8to16(range))
+    }
+
+    fn unmark_text(&mut self, cx: &mut ViewContext<Self>) {
+        self.marked_range = None;
+    }
+
+    fn replace_text_in_range(
+        &mut self,
+        range_utf16: Option<Range<usize>>,
+        new_text: &str,
+        cx: &mut ViewContext<Self>,
+    ) {
+        let range = range_utf16
+            .as_ref()
+            .map(|range_utf16| self.range_utf16to8(range_utf16))
+            .or(self.marked_range.clone())
+            .unwrap_or(self.selected_range.clone());
+
+        self.content =
+            (self.content[0..range.start].to_owned() + new_text + &self.content[range.end..])
+                .into();
+        self.selected_range = range.start + new_text.len()..range.start + new_text.len();
+        self.marked_range.take();
+        cx.notify();
+    }
+
+    fn replace_and_mark_text_in_range(
+        &mut self,
+        range_utf16: Option<Range<usize>>,
+        new_text: &str,
+        new_selected_range_utf16: Option<Range<usize>>,
+        cx: &mut ViewContext<Self>,
+    ) {
+        let range = range_utf16
+            .as_ref()
+            .map(|range_utf16| self.range_utf16to8(range_utf16))
+            .or(self.marked_range.clone())
+            .unwrap_or(self.selected_range.clone());
+
+        self.content =
+            (self.content[0..range.start].to_owned() + new_text + &self.content[range.end..])
+                .into();
+        self.marked_range = Some(range.start..range.start + new_text.len());
+        self.selected_range = new_selected_range_utf16
+            .as_ref()
+            .map(|range_utf16| self.range_utf16to8(range_utf16))
+            .unwrap_or_else(|| range.start + new_text.len()..range.start + new_text.len());
+
+        cx.notify();
+    }
+
+    fn bounds_for_range(
+        &mut self,
+        range_utf16: Range<usize>,
+        bounds: Bounds<Pixels>,
+        cx: &mut ViewContext<Self>,
+    ) -> Option<Bounds<Pixels>> {
+        let Some(last_layout) = self.last_layout.as_ref() else {
+            return None;
+        };
+        let range = self.range_utf16to8(&range_utf16);
+        Some(Bounds::from_corners(
+            point(
+                bounds.left() + last_layout.x_for_index(range.start),
+                bounds.top(),
+            ),
+            point(
+                bounds.left() + last_layout.x_for_index(range.end),
+                bounds.bottom(),
+            ),
+        ))
+    }
+}
+
+struct TextElement {
+    input: View<TextInput>,
+}
+
+struct PrepaintState {
+    hitbox: Hitbox,
+    line: ShapedLine,
+    focus_handle: FocusHandle,
+    cursor: PaintQuad,
+}
+
+impl IntoElement for TextElement {
+    type Element = Self;
+
+    fn into_element(self) -> Self::Element {
+        self
+    }
+}
+
+impl Element for TextElement {
+    type RequestLayoutState = ();
+
+    type PrepaintState = PrepaintState;
+
+    fn id(&self) -> Option<ElementId> {
+        None
+    }
+
+    fn request_layout(
+        &mut self,
+        _id: Option<&GlobalElementId>,
+        cx: &mut WindowContext,
+    ) -> (LayoutId, Self::RequestLayoutState) {
+        let mut style = Style::default();
+        style.size.width = relative(1.).into();
+        style.size.height = relative(1.).into();
+        (cx.request_layout(style, []), ())
+    }
+
+    fn prepaint(
+        &mut self,
+        id: Option<&GlobalElementId>,
+        bounds: Bounds<Pixels>,
+        _request_layout: &mut Self::RequestLayoutState,
+        cx: &mut WindowContext,
+    ) -> Self::PrepaintState {
+        let input = self.input.read(cx);
+        let content = input.content.clone();
+        let focus_handle = input.focus_handle.clone();
+        let selection = input.selected_range.clone();
+        let style = cx.text_style();
+        let run = TextRun {
+            len: input.content.len(),
+            font: style.font(),
+            color: style.color,
+            background_color: None,
+            underline: None,
+            strikethrough: None,
+        };
+        let runs = if let Some(marked_range) = input.marked_range.as_ref() {
+            vec![
+                TextRun {
+                    len: marked_range.start,
+                    ..run.clone()
+                },
+                TextRun {
+                    len: marked_range.end - marked_range.start,
+                    underline: Some(UnderlineStyle {
+                        color: Some(run.color),
+                        thickness: px(1.0),
+                        wavy: false,
+                    }),
+                    ..run.clone()
+                },
+                TextRun {
+                    len: input.content.len() - marked_range.end,
+                    ..run.clone()
+                },
+            ]
+        } else {
+            vec![run]
+        };
+
+        let font_size = style.font_size.to_pixels(cx.rem_size());
+        let line_height = cx.line_height();
+        drop(input);
+        let line = cx
+            .text_system()
+            .shape_line(content, font_size, &runs)
+            .unwrap();
+
+        let cursor_pos = line.x_for_index(selection.end);
+        let cursor = fill(
+            Bounds::new(
+                point(bounds.left() + cursor_pos, bounds.top()),
+                size(px(2.), bounds.bottom() - px(2.)),
+            ),
+            gpui::blue(),
+        );
+        PrepaintState {
+            hitbox: cx.insert_hitbox(bounds, true),
+            line,
+            focus_handle,
+            cursor,
+        }
+    }
+
+    fn paint(
+        &mut self,
+        id: Option<&GlobalElementId>,
+        bounds: Bounds<Pixels>,
+        _request_layout: &mut Self::RequestLayoutState,
+        prepaint: &mut Self::PrepaintState,
+        cx: &mut WindowContext,
+    ) {
+        cx.handle_input(
+            &prepaint.focus_handle,
+            ElementInputHandler::new(bounds, self.input.clone()),
+        );
+        prepaint
+            .line
+            .paint(bounds.origin, cx.line_height(), cx)
+            .unwrap();
+        cx.paint_quad(prepaint.cursor.clone());
+        self.input.update(cx, |input, cx| {
+            input.last_layout = Some(prepaint.line.clone());
+        });
+    }
+}
+
+impl Render for TextInput {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .id("hi")
+            .key_context("TextInput")
+            .track_focus(&self.focus_handle)
+            .on_action(cx.listener(Self::backspace))
+            .on_action(cx.listener(Self::delete))
+            .on_action(cx.listener(Self::left))
+            .on_action(cx.listener(Self::right))
+            .on_action(cx.listener(Self::home))
+            .on_action(cx.listener(Self::end))
+            .bg(rgb(0xeeeeee))
+            .size_full()
+            .text_xl()
+            .child(
+                div()
+                    .h(px(36.))
+                    .w_full()
+                    .p_1()
+                    .bg(white())
+                    .child(TextElement {
+                        input: cx.view().clone(),
+                    }),
+            )
+    }
+}
+
+fn main() {
+    App::new().run(|cx: &mut AppContext| {
+        let bounds = Bounds::centered(None, size(px(300.0), px(300.0)), cx);
+        cx.bind_keys([
+            KeyBinding::new("backspace", Backspace, None),
+            KeyBinding::new("delete", Delete, None),
+            KeyBinding::new("left", Left, None),
+            KeyBinding::new("right", Right, None),
+            KeyBinding::new("home", Home, None),
+            KeyBinding::new("end", End, None),
+        ]);
+        let window = cx
+            .open_window(
+                WindowOptions {
+                    window_bounds: Some(WindowBounds::Windowed(bounds)),
+                    ..Default::default()
+                },
+                |cx| {
+                    cx.new_view(|cx| TextInput {
+                        focus_handle: cx.focus_handle(),
+                        content: "".into(),
+                        selected_range: 0..0,
+                        marked_range: None,
+                        last_layout: None,
+                    })
+                },
+            )
+            .unwrap();
+        window
+            .update(cx, |view, cx| {
+                view.focus_handle.focus(cx);
+            })
+            .unwrap();
+    });
+}
