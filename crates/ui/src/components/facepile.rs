71d7607aa0c754809185c6c91c7eb02715f4387f
Nate Butler <iamnbutler@gmail.com>
2024-06-27 09:39:21 -0400

    Add Facepile component to ui

---
+++ crates/ui/src/components/facepile.rs
@@ -0,0 +1,54 @@
+use crate::prelude::*;
+use gpui::AnyElement;
+use smallvec::SmallVec;
+
+/// A facepile is a collection of faces stacked horizontally–
+/// always with the leftmost face on top and descending in z-index
+///
+/// Facepiles are used to display a group of people or things,
+/// such as a list of participants in a collaboration session.
+#[derive(IntoElement)]
+pub struct Facepile {
+    base: Div,
+    faces: SmallVec<[AnyElement; 2]>,
+}
+
+impl Facepile {
+    pub fn empty() -> Self {
+        Self::new(SmallVec::new())
+    }
+
+    pub fn new(faces: SmallVec<[AnyElement; 2]>) -> Self {
+        Self { base: div(), faces }
+    }
+}
+
+impl RenderOnce for Facepile {
+    fn render(self, _cx: &mut WindowContext) -> impl IntoElement {
+        // Lay the faces out in reverse so they overlap in the desired order (left to right, front to back)
+        self.base
+            .flex()
+            .flex_row_reverse()
+            .items_center()
+            .justify_start()
+            .children(
+                self.faces
+                    .into_iter()
+                    .enumerate()
+                    .rev()
+                    .map(|(ix, player)| div().when(ix > 0, |div| div.ml_neg_1()).child(player)),
+            )
+    }
+}
+
+impl ParentElement for Facepile {
+    fn extend(&mut self, elements: impl IntoIterator<Item = AnyElement>) {
+        self.faces.extend(elements);
+    }
+}
+
+impl Styled for Facepile {
+    fn style(&mut self) -> &mut gpui::StyleRefinement {
+        self.base.style()
+    }
+}
