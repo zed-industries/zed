e71b5b5820102fc8f0c4d97dd10cc11920849785
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2024-06-30 18:28:31 +0300

    Flutter runnables and tasks

---
+++ extensions/dart/languages/dart/runnables.scm
@@ -0,0 +1,45 @@
+; Flutter main
+(
+    (
+        (import_or_export
+            (library_import
+                (import_specification
+                    ("import"
+                        (configurable_uri
+                            (uri
+                                (string_literal) @import
+                                (#match? @import "package:flutter/(material|widgets|cupertino).dart")
+                                (#not-match? @import "package:flutter_test/flutter_test.dart")
+                                (#not-match? @import "package:test/test.dart")
+        ))))))
+        (
+            (function_signature
+                name: (_) @run
+            )
+            (#eq? @run "main")
+        )
+        (#set! tag flutter-main)
+    )
+) @flutter-main
+
+; Flutter test main
+(
+    (
+        (import_or_export
+            (library_import
+                (import_specification
+                    ("import"
+                        (configurable_uri
+                            (uri
+                                (string_literal) @import
+                                (#match? @import "package:flutter_test/flutter_test.dart")
+        ))))))
+        (
+            (function_signature
+                name: (_) @run
+            )
+            (#eq? @run "main")
+        )
+        (#set! tag flutter-test-main)
+    )
+) @flutter-test-main
