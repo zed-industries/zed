c516b8f038d285abba60730b8f154dc58a2dac5c
Marshall Bowers <elliott.codes@gmail.com>
2024-07-01 14:45:42 -0400

    zig: Revert `464a4439f7c71e867da481e99e22ad99cc23807e` (#13712)
    This PR reverts the changes from #13709, now that we've published a new
    version of the Zig extension with them.
    
    This reverts commit 464a4439f7c71e867da481e99e22ad99cc23807e.
    
    Release Notes:
    
    - N/A

--- extensions/zig/src/zig.rs
+++ extensions/zig/src/zig.rs
@@ -43,15 +43,7 @@ impl ZigExtension {
         // them, at time of writing.
         //
         // ZLS tracking issue: https://github.com/zigtools/zls/issues/1879
-        // let release = zed::github_release_by_tag_name("zigtools/zls", "0.11.0")?;
-
-        let release = zed::latest_github_release(
-            "zigtools/zls",
-            zed::GithubReleaseOptions {
-                require_assets: true,
-                pre_release: false,
-            },
-        )?;
+        let release = zed::github_release_by_tag_name("zigtools/zls", "0.11.0")?;
 
         let (platform, arch) = zed::current_platform();
         let asset_name = format!(
