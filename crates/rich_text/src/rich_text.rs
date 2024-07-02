e9b2de770cc67d18f75edd5dacb98100ab187736
tomoikey <55743826+tomoikey@users.noreply.github.com>
2024-07-02 14:06:01 +0900

    add rich_text

--- crates/rich_text/src/rich_text.rs
+++ crates/rich_text/src/rich_text.rs
@@ -39,7 +39,7 @@ pub struct RichText {
     pub link_urls: Arc<[String]>,
 
     pub custom_ranges: Vec<Range<usize>>,
-    custom_ranges_tooltip_fn:
+    pub custom_ranges_tooltip_fn:
         Option<Arc<dyn Fn(usize, Range<usize>, &mut WindowContext) -> Option<AnyView>>>,
 }
 
