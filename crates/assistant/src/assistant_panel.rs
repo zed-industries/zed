c4bca874b6c846cf241532eb01b02235e8985235
Marshall Bowers <elliott.codes@gmail.com>
2024-07-09 17:08:29 -0400

    assistant: Replace margin with gap (#14027)
    This PR replaces a usage of margin with a gap. This allows us to remove
    an extra wrapping `div`.
    
    Release Notes:
    
    - N/A

--- crates/assistant/src/assistant_panel.rs
+++ crates/assistant/src/assistant_panel.rs
@@ -3304,14 +3304,13 @@ impl ContextEditorToolbarItem {
                                     let command_name = command_name.clone();
                                     move |_cx| {
                                         h_flex()
+                                            .gap_4()
                                             .w_full()
                                             .justify_between()
                                             .child(Label::new(menu_text.clone()))
                                             .child(
-                                                div().ml_4().child(
-                                                    Label::new(format!("/{command_name}"))
-                                                        .color(Color::Muted),
-                                                ),
+                                                Label::new(format!("/{command_name}"))
+                                                    .color(Color::Muted),
                                             )
                                             .into_any()
                                     }
