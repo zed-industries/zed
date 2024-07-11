9e6c3faf84936b072db4fcc1a80320f61c6292d8
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-07-11 13:26:21 +0200

    Enable autodetection for typescript-language-server

--- crates/languages/src/typescript.rs
+++ crates/languages/src/typescript.rs
@@ -72,6 +72,18 @@ impl TypeScriptLspAdapter {
     pub fn new(node: Arc<dyn NodeRuntime>) -> Self {
         TypeScriptLspAdapter { node }
     }
+    async fn tsdk_path(adapter: &Arc<dyn LspAdapterDelegate>) -> &'static str {
+        let is_yarn = adapter
+            .read_text_file(PathBuf::from(".yarn/sdks/typescript/lib/typescript.js"))
+            .await
+            .is_ok();
+
+        if is_yarn {
+            ".yarn/sdks/typescript/lib"
+        } else {
+            "node_modules/typescript/lib"
+        }
+    }
 }
 
 struct TypeScriptVersions {
@@ -196,13 +208,14 @@ impl LspAdapter for TypeScriptLspAdapter {
 
     async fn initialization_options(
         self: Arc<Self>,
-        _: &Arc<dyn LspAdapterDelegate>,
+        adapter: &Arc<dyn LspAdapterDelegate>,
     ) -> Result<Option<serde_json::Value>> {
+        let tsdk_path = Self::tsdk_path(adapter).await;
         Ok(Some(json!({
             "provideFormatter": true,
             "hostInfo": "zed",
             "tsserver": {
-                "path": "node_modules/typescript/lib",
+                "path": tsdk_path,
             },
             "preferences": {
                 "includeInlayParameterNameHints": "all",
