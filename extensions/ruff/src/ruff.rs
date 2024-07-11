b9db5a0e5514932a672035e8d2e215ed91dd5684
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-07-11 15:14:14 +0200

    Scaffold Ruff extension

---
+++ extensions/ruff/src/ruff.rs
@@ -0,0 +1,55 @@
+use serde_json::json;
+use std::fs;
+use zed::LanguageServerId;
+use zed_extension_api::{self as zed, settings::LspSettings, Result};
+
+struct RuffExtension {
+    cached_binary_path: Option<String>,
+}
+
+impl RuffExtension {
+    fn language_server_binary_path(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<String> {
+        if let Some(path) = worktree.which("ruff") {
+            return Ok(path);
+        }
+        unreachable!();
+    }
+}
+
+impl zed::Extension for RuffExtension {
+    fn new() -> Self {
+        Self {
+            cached_binary_path: None,
+        }
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        Ok(zed::Command {
+            command: self.language_server_binary_path(language_server_id, worktree)?,
+            args: vec!["server".into(), "--preview".into()],
+            env: vec![],
+        })
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        server_id: &LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+}
+
+zed::register_extension!(RuffExtension);
