dc10579390223634838a3feb50b9bc4927cdf848
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-07-12 11:43:48 -0700

    Add `outline` slash command
    Co-authored-by: Antonio <antonio@zed.dev>

--- crates/assistant/src/slash_command.rs
+++ crates/assistant/src/slash_command.rs
@@ -24,6 +24,7 @@ pub mod docs_command;
 pub mod fetch_command;
 pub mod file_command;
 pub mod now_command;
+pub mod outline_command;
 pub mod project_command;
 pub mod prompt_command;
 pub mod search_command;
