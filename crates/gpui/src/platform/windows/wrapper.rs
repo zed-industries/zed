268bf0905d05c85c2ebb3cf0fa837002bd706a8b
Junkui Zhang <364772080@qq.com>
2024-07-18 16:38:12 +0800

    fix cursor

--- crates/gpui/src/platform/windows/wrapper.rs
+++ crates/gpui/src/platform/windows/wrapper.rs
@@ -1,6 +1,6 @@
 use std::ops::Deref;
 
-use windows::Win32::Foundation::HANDLE;
+use windows::Win32::{Foundation::HANDLE, UI::WindowsAndMessaging::HCURSOR};
 
 #[derive(Debug, Clone, Copy)]
 pub(crate) struct SafeHandle {
@@ -18,6 +18,28 @@ impl From<HANDLE> for SafeHandle {
 
 impl Deref for SafeHandle {
     type Target = HANDLE;
+
+    fn deref(&self) -> &Self::Target {
+        &self.raw
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct SafeCursor {
+    raw: HCURSOR,
+}
+
+unsafe impl Send for SafeCursor {}
+unsafe impl Sync for SafeCursor {}
+
+impl From<HCURSOR> for SafeCursor {
+    fn from(value: HCURSOR) -> Self {
+        SafeCursor { raw: value }
+    }
+}
+
+impl Deref for SafeCursor {
+    type Target = HCURSOR;
 
     fn deref(&self) -> &Self::Target {
         &self.raw
