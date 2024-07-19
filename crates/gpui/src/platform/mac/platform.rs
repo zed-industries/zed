3e1846b3fb6e320761fbe0a2c2eb5ec0af096f6f
Conrad Irwin <conrad.irwin@gmail.com>
2024-07-19 12:59:21 -0600

    Fix other platforms

--- crates/gpui/src/platform/mac/platform.rs
+++ crates/gpui/src/platform/mac/platform.rs
@@ -1,9 +1,9 @@
 use super::{events::key_to_native, BoolExt};
 use crate::{
     Action, AnyWindowHandle, BackgroundExecutor, ClipboardItem, CursorStyle, ForegroundExecutor,
-    GPUSpecs, Keymap, MacDispatcher, MacDisplay, MacTextSystem, MacWindow, Menu, MenuItem,
-    PathPromptOptions, Platform, PlatformDisplay, PlatformTextSystem, PlatformWindow, Result,
-    SemanticVersion, Task, WindowAppearance, WindowParams,
+    Keymap, MacDispatcher, MacDisplay, MacTextSystem, MacWindow, Menu, MenuItem, PathPromptOptions,
+    Platform, PlatformDisplay, PlatformTextSystem, PlatformWindow, Result, SemanticVersion, Task,
+    WindowAppearance, WindowParams,
 };
 use anyhow::anyhow;
 use block::ConcreteBlock;
@@ -1027,10 +1027,6 @@ impl Platform for MacPlatform {
             }
             Ok(())
         })
-    }
-
-    fn gpu_specs(&self) -> Option<GPUSpecs> {
-        None
     }
 }
 
--- crates/gpui/src/platform/windows/platform.rs
+++ crates/gpui/src/platform/windows/platform.rs
@@ -602,10 +602,6 @@ impl Platform for WindowsPlatform {
     fn register_url_scheme(&self, _: &str) -> Task<anyhow::Result<()>> {
         Task::ready(Err(anyhow!("register_url_scheme unimplemented")))
     }
-
-    fn gpu_specs(&self) -> Option<GPUSpecs> {
-        None
-    }
 }
 
 impl Drop for WindowsPlatform {
