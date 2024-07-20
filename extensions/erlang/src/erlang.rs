f904169c20de33468224617abf1d569e5912faef
Fabian <fabian@fmbb.se>
2024-07-20 21:24:50 +0200

    Add Erlang Language Platform support

--- extensions/erlang/src/erlang.rs
+++ extensions/erlang/src/erlang.rs
@@ -9,19 +9,39 @@ impl zed::Extension for ErlangExtension {
 
     fn language_server_command(
         &mut self,
-        _config: zed::LanguageServerConfig,
+        config: zed::LanguageServerConfig,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        let path = worktree
-            .which("erlang_ls")
-            .ok_or_else(|| "erlang_ls must be installed and available on your $PATH".to_string())?;
-
-        Ok(zed::Command {
-            command: path,
-            args: Vec::new(),
-            env: Default::default(),
-        })
+        match config.name.as_str() {
+            "erlang-ls" => erlang_ls_server_command(worktree),
+            "elp" => elp_server_command(worktree),
+            _ => Err(format!("Unsupported language server: {}", config.name)),
+        }
     }
+}
+
+fn erlang_ls_server_command(worktree: &zed::Worktree) -> Result<zed::Command> {
+    let path = worktree
+        .which("erlang_ls")
+        .ok_or_else(|| "erlang_ls must be installed and available on your $PATH".to_string())?;
+
+    Ok(zed::Command {
+        command: path,
+        args: Vec::new(),
+        env: Default::default(),
+    })
+}
+
+fn elp_server_command(worktree: &zed::Worktree) -> Result<zed::Command> {
+    let path = worktree
+        .which("elp")
+        .ok_or_else(|| "elp must be installed and available on your $PATH".to_string())?;
+
+    Ok(zed::Command {
+        command: path,
+        args: vec!["server".to_string()],
+        env: Default::default(),
+    })
 }
 
 zed::register_extension!(ErlangExtension);
