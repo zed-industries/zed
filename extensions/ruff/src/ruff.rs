0a0aec5799ac730b12151192a730bc459971c8a9
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-07-20 15:27:54 +0200

    Ruff: pass initialization_options from settings

--- extensions/ruff/src/ruff.rs
+++ extensions/ruff/src/ruff.rs
@@ -106,6 +106,17 @@ impl zed::Extension for RuffExtension {
         })
     }
 
+    fn language_server_initialization_options(
+        &mut self,
+        _language_server_id: &LanguageServerId,
+        _worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.initialization_options.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
     fn language_server_workspace_configuration(
         &mut self,
         server_id: &LanguageServerId,
