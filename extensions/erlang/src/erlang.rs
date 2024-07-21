7b88fc5cda9dbf1dfa748fb20126337e395f305b
Fabian Bergström <fabian.bergstrom@gmail.com>
2024-07-21 17:38:24 +0200

    erlang: Add Erlang Language Platform support (#14879)
    Added support for the [Erlang Language
    Platform](https://whatsapp.github.io/erlang-language-platform/) language
    server to the Erlang extension.
    
    Release Notes:
    
    - N/A
    
    Co-authored-by: Marshall Bowers <elliott.codes@gmail.com>

--- extensions/erlang/src/erlang.rs
+++ extensions/erlang/src/erlang.rs
@@ -2,15 +2,19 @@ mod language_servers;
 
 use zed_extension_api::{self as zed, Result};
 
-use crate::language_servers::ErlangLs;
+use crate::language_servers::{ErlangLanguagePlatform, ErlangLs};
 
 struct ErlangExtension {
     erlang_ls: Option<ErlangLs>,
+    erlang_language_platform: Option<ErlangLanguagePlatform>,
 }
 
 impl zed::Extension for ErlangExtension {
     fn new() -> Self {
-        Self { erlang_ls: None }
+        Self {
+            erlang_ls: None,
+            erlang_language_platform: None,
+        }
     }
 
     fn language_server_command(
@@ -27,6 +31,12 @@ impl zed::Extension for ErlangExtension {
                     args: vec![],
                     env: Default::default(),
                 })
+            }
+            ErlangLanguagePlatform::LANGUAGE_SERVER_ID => {
+                let erlang_language_platform = self
+                    .erlang_language_platform
+                    .get_or_insert_with(|| ErlangLanguagePlatform::new());
+                erlang_language_platform.language_server_command(language_server_id, worktree)
             }
             language_server_id => Err(format!("unknown language server: {language_server_id}")),
         }
