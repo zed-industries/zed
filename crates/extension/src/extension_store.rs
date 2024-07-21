6759b45faf9b171d2dd7ed1a7d1eac3cc7dc0d28
Congyu WANG <leonwang998@gmail.com>
2024-07-21 23:19:32 +0800

    create new non-fake http-client in extension-store

--- crates/extension/src/extension_store.rs
+++ crates/extension/src/extension_store.rs
@@ -243,7 +243,10 @@ impl ExtensionStore {
             extension_index: Default::default(),
             installed_dir,
             index_path,
-            builder: Arc::new(ExtensionBuilder::new(build_dir, http_client.clone())),
+            builder: Arc::new(ExtensionBuilder::new(
+                build_dir,
+                http::client(http_client.proxy()),
+            )),
             outstanding_operations: Default::default(),
             modified_extensions: Default::default(),
             reload_complete_senders: Vec::new(),
