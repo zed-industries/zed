73888b513be07138d94bdcb11c26ec8f2214bc52
Bennet Bo Fenner <bennet@zed.dev>
2024-07-22 12:46:58 +0200

    cleanup settings

--- crates/language_model/src/settings.rs
+++ crates/language_model/src/settings.rs
@@ -24,8 +24,28 @@ pub struct AllLanguageModelSettings {
 
 #[derive(Default, Clone, Debug, Serialize, Deserialize, PartialEq, JsonSchema)]
 pub struct AllLanguageModelSettingsContent {
-    #[serde(default)]
-    pub language_models: Vec<LanguageModelSettingsContent>,
+    anthropic: Option<AnthropicSettingsContent>,
+    ollama: Option<OllamaSettingsContent>,
+    open_ai: Option<OpenAiSettingsContent>,
+}
+
+#[derive(Default, Clone, Debug, Serialize, Deserialize, PartialEq, JsonSchema)]
+pub struct AnthropicSettingsContent {
+    api_url: Option<String>,
+    low_speed_timeout_in_seconds: Option<u64>,
+}
+
+#[derive(Default, Clone, Debug, Serialize, Deserialize, PartialEq, JsonSchema)]
+pub struct OllamaSettingsContent {
+    api_url: Option<String>,
+    low_speed_timeout_in_seconds: Option<u64>,
+}
+
+#[derive(Default, Clone, Debug, Serialize, Deserialize, PartialEq, JsonSchema)]
+pub struct OpenAiSettingsContent {
+    api_url: Option<String>,
+    low_speed_timeout_in_seconds: Option<u64>,
+    available_models: Option<Vec<open_ai::Model>>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, JsonSchema, PartialEq)]
@@ -50,7 +70,7 @@ pub enum LanguageModelSettingsContent {
 }
 
 impl settings::Settings for AllLanguageModelSettings {
-    const KEY: Option<&'static str> = None;
+    const KEY: Option<&'static str> = Some("language_models");
 
     type FileContent = AllLanguageModelSettingsContent;
 
@@ -64,42 +84,51 @@ impl settings::Settings for AllLanguageModelSettings {
         let mut settings = AllLanguageModelSettings::default();
 
         for value in sources.defaults_and_customizations() {
-            for setting in value.language_models.clone() {
-                match setting {
-                    LanguageModelSettingsContent::OpenAi {
-                        api_url,
-                        low_speed_timeout_in_seconds,
-                        available_models,
-                    } => {
-                        merge(&mut settings.open_ai.api_url, api_url);
-                        if let Some(low_speed_timeout_in_seconds) = low_speed_timeout_in_seconds {
-                            settings.open_ai.low_speed_timeout =
-                                Some(Duration::from_secs(low_speed_timeout_in_seconds));
-                        }
-                        merge(&mut settings.open_ai.available_models, available_models);
-                    }
-                    LanguageModelSettingsContent::Anthropic {
-                        api_url,
-                        low_speed_timeout_in_seconds,
-                    } => {
-                        merge(&mut settings.anthropic.api_url, api_url);
-                        if let Some(low_speed_timeout_in_seconds) = low_speed_timeout_in_seconds {
-                            settings.anthropic.low_speed_timeout =
-                                Some(Duration::from_secs(low_speed_timeout_in_seconds));
-                        }
-                    }
-                    LanguageModelSettingsContent::Ollama {
-                        api_url,
-                        low_speed_timeout_in_seconds,
-                    } => {
-                        merge(&mut settings.ollama.api_url, api_url);
-                        if let Some(low_speed_timeout_in_seconds) = low_speed_timeout_in_seconds {
-                            settings.ollama.low_speed_timeout =
-                                Some(Duration::from_secs(low_speed_timeout_in_seconds));
-                        }
-                    }
-                }
+            merge(
+                &mut settings.anthropic.api_url,
+                value.anthropic.as_ref().and_then(|s| s.api_url.clone()),
+            );
+            if let Some(low_speed_timeout_in_seconds) = value
+                .anthropic
+                .as_ref()
+                .and_then(|s| s.low_speed_timeout_in_seconds.clone())
+            {
+                settings.anthropic.low_speed_timeout =
+                    Some(Duration::from_secs(low_speed_timeout_in_seconds));
             }
+
+            merge(
+                &mut settings.ollama.api_url,
+                value.ollama.as_ref().and_then(|s| s.api_url.clone()),
+            );
+            if let Some(low_speed_timeout_in_seconds) = value
+                .ollama
+                .as_ref()
+                .and_then(|s| s.low_speed_timeout_in_seconds.clone())
+            {
+                settings.ollama.low_speed_timeout =
+                    Some(Duration::from_secs(low_speed_timeout_in_seconds));
+            }
+
+            merge(
+                &mut settings.open_ai.api_url,
+                value.open_ai.as_ref().and_then(|s| s.api_url.clone()),
+            );
+            if let Some(low_speed_timeout_in_seconds) = value
+                .open_ai
+                .as_ref()
+                .and_then(|s| s.low_speed_timeout_in_seconds.clone())
+            {
+                settings.open_ai.low_speed_timeout =
+                    Some(Duration::from_secs(low_speed_timeout_in_seconds));
+            }
+            merge(
+                &mut settings.open_ai.available_models,
+                value
+                    .open_ai
+                    .as_ref()
+                    .and_then(|s| s.available_models.clone()),
+            );
         }
 
         Ok(settings)
