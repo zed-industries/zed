228732be35145e43cddb6f5dc102059918f58ae4
Junkui Zhang <364772080@qq.com>
2024-07-23 01:53:59 +0800

    install

--- extensions/purescript/src/purescript.rs
+++ extensions/purescript/src/purescript.rs
@@ -2,21 +2,23 @@ use std::{env, fs};
 use zed_extension_api::{self as zed, Result};
 
 const SERVER_PATH: &str = "node_modules/.bin/purescript-language-server";
+const SERVER_PATH_WIN: &str = "node_modules/.bin/purescript-language-server.ps1";
 const PACKAGE_NAME: &str = "purescript-language-server";
 
 struct PurescriptExtension {
     did_find_server: bool,
+    server_path: String,
 }
 
 impl PurescriptExtension {
     fn server_exists(&self) -> bool {
-        fs::metadata(SERVER_PATH).map_or(false, |stat| stat.is_file())
+        fs::metadata(&self.server_path).map_or(false, |stat| stat.is_file())
     }
 
     fn server_script_path(&mut self, config: &zed::LanguageServerId) -> Result<String> {
         let server_exists = self.server_exists();
         if self.did_find_server && server_exists {
-            return Ok(SERVER_PATH.to_string());
+            return Ok(self.server_path.clone());
         }
 
         zed::set_language_server_installation_status(
@@ -37,7 +39,8 @@ impl PurescriptExtension {
                 Ok(()) => {
                     if !self.server_exists() {
                         Err(format!(
-                            "installed package '{PACKAGE_NAME}' did not contain expected path '{SERVER_PATH}'",
+                            "installed package '{PACKAGE_NAME}' did not contain expected path '{}'",
+                            self.server_path,
                         ))?;
                     }
                 }
@@ -50,14 +53,20 @@ impl PurescriptExtension {
         }
 
         self.did_find_server = true;
-        Ok(SERVER_PATH.to_string())
+        Ok(self.server_path.clone())
     }
 }
 
 impl zed::Extension for PurescriptExtension {
     fn new() -> Self {
+        let server_path = match zed::current_platform().0 {
+            zed_extension_api::Os::Windows => SERVER_PATH_WIN,
+            _ => SERVER_PATH,
+        }
+        .to_string();
         Self {
             did_find_server: false,
+            server_path,
         }
     }
 
