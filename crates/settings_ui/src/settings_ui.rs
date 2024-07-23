8a24c495f4686bdd7e1ae977e466b5a068c8a69d
Marshall Bowers <elliott.codes@gmail.com>
2024-07-23 12:18:54 -0400

    Add shell for settings page

--- crates/settings_ui/src/settings_ui.rs
+++ crates/settings_ui/src/settings_ui.rs
@@ -1,14 +1,74 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use gpui::{actions, AppContext, EventEmitter, FocusHandle, FocusableView, View};
+use ui::prelude::*;
+use workspace::item::{Item, ItemEvent};
+use workspace::Workspace;
+
+actions!(zed, [OpenSettingsEditor]);
+
+pub fn init(cx: &mut AppContext) {
+    cx.observe_new_views(|workspace: &mut Workspace, _cx| {
+        workspace.register_action(|workspace, _: &OpenSettingsEditor, cx| {
+            let existing = workspace
+                .active_pane()
+                .read(cx)
+                .items()
+                .find_map(|item| item.downcast::<SettingsPage>());
+
+            if let Some(existing) = existing {
+                workspace.activate_item(&existing, true, true, cx);
+            } else {
+                let settings_page = SettingsPage::new(workspace, cx);
+                workspace.add_item_to_active_pane(Box::new(settings_page), None, true, cx)
+            }
+        });
+    })
+    .detach();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct SettingsPage {
+    focus_handle: FocusHandle,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl SettingsPage {
+    pub fn new(_workspace: &Workspace, cx: &mut ViewContext<Workspace>) -> View<Self> {
+        cx.new_view(|cx| Self {
+            focus_handle: cx.focus_handle(),
+        })
+    }
+}
+
+impl EventEmitter<ItemEvent> for SettingsPage {}
+
+impl FocusableView for SettingsPage {
+    fn focus_handle(&self, _cx: &AppContext) -> FocusHandle {
+        self.focus_handle.clone()
+    }
+}
+
+impl Item for SettingsPage {
+    type Event = ItemEvent;
+
+    fn tab_icon(&self, _cx: &WindowContext) -> Option<Icon> {
+        Some(Icon::new(IconName::Settings))
+    }
+
+    fn tab_content_text(&self, _cx: &WindowContext) -> Option<SharedString> {
+        Some("Settings".into())
+    }
+
+    fn show_toolbar(&self) -> bool {
+        false
+    }
+
+    fn to_item_events(event: &Self::Event, mut f: impl FnMut(ItemEvent)) {
+        f(*event)
+    }
+}
+
+impl Render for SettingsPage {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        v_flex()
+            .size_full()
+            .child(Label::new("Settings").size(LabelSize::Large))
     }
 }
