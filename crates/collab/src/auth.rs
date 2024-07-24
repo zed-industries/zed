744e5e39010fc9c02fa10ffb1503cac72f374c89
Marshall Bowers <elliott.codes@gmail.com>
2024-07-23 21:25:25 -0400

    rpc: Add support for OAEP-based encryption format (#15058)
    This PR adds support for a new encryption format for exchanging access
    tokens during the authentication flow.
    
    The new format uses Optimal Asymmetric Encryption Padding (OAEP) instead
    of PKCS#1 v1.5, which is known to be vulnerable to side-channel attacks.
    
    **Note: We are not yet encrypting access tokens using the new format, as
    this is a breaking change between the client and the server. This PR
    only adds support for it, and makes it so the client and server can
    decrypt either format moving forward.**
    
    This required bumping the RSA key size from 1024 bits to 2048 bits. This
    is necessary to be able to encode the access token into the ciphertext
    when using OAEP.
    
    This also follows OWASP recommendations:
    
    > If ECC is not available and RSA must be used, then ensure that the key
    is at least 2048 bits.
    >
    > —
    [source](https://cheatsheetseries.owasp.org/cheatsheets/Cryptographic_Storage_Cheat_Sheet.html#algorithms)
    
    Release Notes:
    
    - N/A

--- crates/collab/src/auth.rs
+++ crates/collab/src/auth.rs
@@ -164,10 +164,21 @@ pub fn hash_access_token(token: &str) -> String {
 /// Encrypts the given access token with the given public key to avoid leaking it on the way
 /// to the client.
 pub fn encrypt_access_token(access_token: &str, public_key: String) -> Result<String> {
+    use rpc::auth::EncryptionFormat;
+
+    /// The encryption format to use for the access token.
+    ///
+    /// Currently we're using the original encryption format to avoid
+    /// breaking compatibility with older clients.
+    ///
+    /// Once enough clients are capable of decrypting the newer encryption
+    /// format we can start encrypting with `EncryptionFormat::V1`.
+    const ENCRYPTION_FORMAT: EncryptionFormat = EncryptionFormat::V0;
+
     let native_app_public_key =
         rpc::auth::PublicKey::try_from(public_key).context("failed to parse app public key")?;
     let encrypted_access_token = native_app_public_key
-        .encrypt_string(access_token)
+        .encrypt_string(access_token, ENCRYPTION_FORMAT)
         .context("failed to encrypt access token with public key")?;
     Ok(encrypted_access_token)
 }
