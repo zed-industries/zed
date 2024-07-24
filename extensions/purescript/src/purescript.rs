84af74df289f10bd74b66fae6789e791c09b5547
Junkui Zhang <364772080@qq.com>
2024-07-24 16:23:37 +0800

    revert

--- extensions/purescript/src/purescript.rs
+++ extensions/purescript/src/purescript.rs
@@ -1,28 +1,26 @@
-use std::fs;
+use std::{env, fs};
 use zed_extension_api::{self as zed, Result};
 
 const SERVER_PATH: &str = "node_modules/.bin/purescript-language-server";
-const SERVER_PATH_WIN: &str = "node_modules/.bin/purescript-language-server.ps1";
 const PACKAGE_NAME: &str = "purescript-language-server";
 
 struct PurescriptExtension {
     did_find_server: bool,
-    server_path: String,
 }
 
 impl PurescriptExtension {
     fn server_exists(&self) -> bool {
-        fs::metadata(&self.server_path).map_or(false, |stat| stat.is_file())
+        fs::metadata(SERVER_PATH).map_or(false, |stat| stat.is_file())
     }
 
-    fn server_script_path(&mut self, config: &zed::LanguageServerId) -> Result<String> {
+    fn server_script_path(&mut self, config: zed::LanguageServerConfig) -> Result<String> {
         let server_exists = self.server_exists();
         if self.did_find_server && server_exists {
-            return Ok(self.server_path.clone());
+            return Ok(SERVER_PATH.to_string());
         }
 
         zed::set_language_server_installation_status(
-            &config,
+            &config.name,
             &zed::LanguageServerInstallationStatus::CheckingForUpdate,
         );
         let version = zed::npm_package_latest_version(PACKAGE_NAME)?;
@@ -31,7 +29,7 @@ impl PurescriptExtension {
             || zed::npm_package_installed_version(PACKAGE_NAME)?.as_ref() != Some(&version)
         {
             zed::set_language_server_installation_status(
-                &config,
+                &config.name,
                 &zed::LanguageServerInstallationStatus::Downloading,
             );
             let result = zed::npm_install_package(PACKAGE_NAME, &version);
@@ -39,8 +37,7 @@ impl PurescriptExtension {
                 Ok(()) => {
                     if !self.server_exists() {
                         Err(format!(
-                            "installed package '{PACKAGE_NAME}' did not contain expected path '{}'",
-                            self.server_path,
+                            "installed package '{PACKAGE_NAME}' did not contain expected path '{SERVER_PATH}'",
                         ))?;
                     }
                 }
@@ -53,77 +50,49 @@ impl PurescriptExtension {
         }
 
         self.did_find_server = true;
-        Ok(self.server_path.clone())
+        Ok(SERVER_PATH.to_string())
     }
 }
 
 impl zed::Extension for PurescriptExtension {
     fn new() -> Self {
-        let server_path = match zed::current_platform().0 {
-            zed_extension_api::Os::Windows => SERVER_PATH_WIN,
-            _ => SERVER_PATH,
-        }
-        .to_string();
         Self {
             did_find_server: false,
-            server_path,
         }
     }
 
     fn language_server_command(
         &mut self,
-        config: &zed::LanguageServerId,
+        config: zed::LanguageServerConfig,
         _worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
         let server_path = self.server_script_path(config)?;
-        match zed::current_platform().0 {
-            zed_extension_api::Os::Windows => {
-                let command = zed::CommandType::Shell("powershell.exe".into());
-                let env = vec![("PATH".to_string(), zed::node_environment_path()?)];
-                println!("==> Env: {:?}", env);
-                Ok(zed::Command {
-                    command,
-                    args: vec![
-                        zed_extension_api::current_dir()
-                            .unwrap()
-                            .join(&server_path)
-                            .to_string_lossy()
-                            .to_string(),
-                        "--stdio".to_string(),
-                    ],
-                    env,
-                })
-            }
-            _ => {
-                let command = zed::CommandType::Other(zed::node_binary_path()?);
-                Ok(zed::Command {
-                    command,
-                    args: vec![
-                        zed_extension_api::current_dir()
-                            .unwrap()
-                            .join(&server_path)
-                            .to_string_lossy()
-                            .to_string(),
-                        "--stdio".to_string(),
-                    ],
-                    env: Default::default(),
-                })
-            }
-        }
+        Ok(zed::Command {
+            command: zed::node_binary_path()?,
+            args: vec![
+                env::current_dir()
+                    .unwrap()
+                    .join(&server_path)
+                    .to_string_lossy()
+                    .to_string(),
+                "--stdio".to_string(),
+            ],
+            env: Default::default(),
+        })
     }
 
     fn language_server_initialization_options(
         &mut self,
-        _config: &zed::LanguageServerId,
+        _config: zed::LanguageServerConfig,
         _worktree: &zed::Worktree,
-    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+    ) -> Result<Option<String>> {
         let initialization_options = r#"{
             "purescript": {
                 "addSpagoSources": true
             }
         }"#;
 
-        Ok(Some(initialization_options.into()))
+        Ok(Some(initialization_options.to_string()))
     }
 }
 
