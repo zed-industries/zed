32445550c575dd48f74c648f9a29efc90784843b
Marshall Bowers <elliott.codes@gmail.com>
2024-07-24 10:27:18 -0400

    Add some doc comments

--- crates/settings_ui/src/theme_settings_ui.rs
+++ crates/settings_ui/src/theme_settings_ui.rs
@@ -6,7 +6,10 @@ use theme::ThemeSettings;
 use ui::{prelude::*, CheckboxWithLabel, NumericStepper};
 
 pub trait EditableSetting: RenderOnce {
+    /// The type of the setting value.
     type Value: Send;
+
+    /// The settings type to which this setting belongs.
     type Settings: Settings;
 
     /// Returns the name of this setting.
@@ -17,16 +20,20 @@ pub trait EditableSetting: RenderOnce {
         None
     }
 
+    /// Returns a new instance of this setting.
     fn new(cx: &AppContext) -> Self;
 
-    fn update(settings: &mut <Self::Settings as Settings>::FileContent, value: Self::Value);
+    /// Applies the given setting file to the settings file contents.
+    ///
+    /// This will be called when writing the setting value back to the settings file.
+    fn apply(settings: &mut <Self::Settings as Settings>::FileContent, value: Self::Value);
 
     /// Writes the given setting value to the settings files.
     fn write(value: Self::Value, cx: &AppContext) {
         let fs = <dyn Fs>::global(cx);
 
         update_settings_file::<Self::Settings>(fs, cx, move |settings, _cx| {
-            Self::update(settings, value);
+            Self::apply(settings, value);
         });
     }
 }
@@ -52,7 +59,7 @@ impl EditableSetting for UiFontSizeSetting {
         Self(settings.ui_font_size)
     }
 
-    fn update(settings: &mut <Self::Settings as Settings>::FileContent, value: Self::Value) {
+    fn apply(settings: &mut <Self::Settings as Settings>::FileContent, value: Self::Value) {
         settings.ui_font_size = Some(value.into());
     }
 }
@@ -103,7 +110,7 @@ impl EditableSetting for BufferFontSizeSetting {
         Self(settings.buffer_font_size)
     }
 
-    fn update(settings: &mut <Self::Settings as Settings>::FileContent, value: Self::Value) {
+    fn apply(settings: &mut <Self::Settings as Settings>::FileContent, value: Self::Value) {
         settings.buffer_font_size = Some(value.into());
     }
 }
@@ -149,7 +156,7 @@ impl EditableSetting for InlineGitBlameSetting {
         Self(settings.git.inline_blame_enabled())
     }
 
-    fn update(settings: &mut <Self::Settings as Settings>::FileContent, value: Self::Value) {
+    fn apply(settings: &mut <Self::Settings as Settings>::FileContent, value: Self::Value) {
         if let Some(inline_blame) = settings.git.inline_blame.as_mut() {
             inline_blame.enabled = value;
         } else {
