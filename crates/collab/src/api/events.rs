215cafd4950e405d251435a88f44b7c2d5bfa007
Joseph T Lyons <JosephTLyons@gmail.com>
2024-07-26 00:30:25 -0400

    Use guard clause

--- crates/collab/src/api/events.rs
+++ crates/collab/src/api/events.rs
@@ -638,21 +638,23 @@ impl ToUpload {
         rows: &[T],
         clickhouse_client: &clickhouse::Client,
     ) -> anyhow::Result<()> {
-        if !rows.is_empty() {
-            let mut insert = clickhouse_client.insert(table)?;
-
-            for event in rows {
-                insert.write(event).await?;
-            }
-
-            insert.end().await?;
-
-            let event_count = rows.len();
-            log::info!(
-                "wrote {event_count} {event_specifier} to '{table}'",
-                event_specifier = if event_count == 1 { "event" } else { "events" }
-            );
+        if rows.is_empty() {
+            return Ok(());
         }
+
+        let mut insert = clickhouse_client.insert(table)?;
+
+        for event in rows {
+            insert.write(event).await?;
+        }
+
+        insert.end().await?;
+
+        let event_count = rows.len();
+        log::info!(
+            "wrote {event_count} {event_specifier} to '{table}'",
+            event_specifier = if event_count == 1 { "event" } else { "events" }
+        );
 
         Ok(())
     }
