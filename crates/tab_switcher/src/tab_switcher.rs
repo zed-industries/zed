6ab75d0ec802f0c0a1bdce40c5b2f8488f349a58
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-07-30 13:13:49 +0200

    tab_switcher: Add support for tab switcher in assistant panel
    Additionally, I've generalized the implementation of tab switcher so that - instead of explicitly listing panels it supports (at the time of writing it was just the terminal panel and nothing else), it now relies on Panel::pane trait method. As long as that's implemented, you get a tab switcher support for free.

--- crates/tab_switcher/src/tab_switcher.rs
+++ crates/tab_switcher/src/tab_switcher.rs
@@ -3,7 +3,7 @@ mod tab_switcher_tests;
 
 use collections::HashMap;
 use gpui::{
-    actions, impl_actions, rems, Action, AnyElement, AppContext, DismissEvent, EntityId,
+    actions, impl_actions, rems, Action, AnyElement, AppContext, DismissEvent, Entity, EntityId,
     EventEmitter, FocusHandle, FocusableView, Modifiers, ModifiersChangedEvent, MouseButton,
     MouseUpEvent, ParentElement, Render, Styled, Task, View, ViewContext, VisualContext, WeakView,
 };
@@ -57,16 +57,25 @@ impl TabSwitcher {
     }
 
     fn open(action: &Toggle, workspace: &mut Workspace, cx: &mut ViewContext<Workspace>) {
-        let terminal = workspace.panel::<terminal_view::terminal_panel::TerminalPanel>(cx);
-        let terminal_pane = terminal.and_then(|terminal| {
-            terminal
-                .focus_handle(cx)
-                .contains_focused(cx)
-                .then(|| terminal.read(cx).pane())
-        });
-        let weak_pane = terminal_pane
-            .unwrap_or_else(|| workspace.active_pane())
-            .downgrade();
+        let mut weak_pane = workspace.active_pane().downgrade();
+        for dock in [
+            workspace.left_dock(),
+            workspace.bottom_dock(),
+            workspace.right_dock(),
+        ] {
+            dock.update(cx, |this, cx| {
+                let Some(panel) = this
+                    .active_panel()
+                    .filter(|panel| panel.focus_handle(cx).contains_focused(cx))
+                else {
+                    return;
+                };
+                if let Some(pane) = panel.pane(cx) {
+                    weak_pane = pane.downgrade();
+                }
+            })
+        }
+
         workspace.toggle_modal(cx, |cx| {
             let delegate = TabSwitcherDelegate::new(action, cx.view().downgrade(), weak_pane, cx);
             TabSwitcher::new(delegate, cx)
