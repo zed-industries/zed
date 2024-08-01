0bdd6216d3ed595b7fa5053966a05a787cccc97d
Marshall Bowers <elliott.codes@gmail.com>
2024-08-01 15:32:19 -0400

    Fix `.env.toml` paths

--- crates/collab/src/bin/dotenv.rs
+++ crates/collab/src/bin/dotenv.rs
@@ -1,7 +1,7 @@
 use collab::env::get_dotenv_vars;
 
 fn main() -> anyhow::Result<()> {
-    for (key, value) in get_dotenv_vars()? {
+    for (key, value) in get_dotenv_vars(".")? {
         println!("export {}=\"{}\"", key, value);
     }
     Ok(())
