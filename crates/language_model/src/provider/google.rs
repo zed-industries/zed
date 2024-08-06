3f487c11d87e0a73983e399c163f16506ff0754e
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-08-06 13:37:01 +0200

    Fix clippy violation (unused import)

--- crates/language_model/src/provider/google.rs
+++ crates/language_model/src/provider/google.rs
@@ -14,7 +14,7 @@ use http_client::HttpClient;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use settings::{Settings, SettingsStore};
-use std::{future, sync::Arc, time::Duration};
+use std::{sync::Arc, time::Duration};
 use strum::IntoEnumIterator;
 use theme::ThemeSettings;
 use ui::{prelude::*, Indicator};
