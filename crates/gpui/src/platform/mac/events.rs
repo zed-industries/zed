a6a43e53b9e3afaa0701ef9dffec0bc5e7bf0c55
Haru Kim <haruleekim@icloud.com>
2024-08-16 14:26:19 +0900

    Update `Keystroke::ime_key` pressed with shift key to match QWERTY layout

--- crates/gpui/src/platform/mac/events.rs
+++ crates/gpui/src/platform/mac/events.rs
@@ -13,109 +13,270 @@ use std::borrow::Cow;
 const BACKSPACE_KEY: u16 = 0x7f;
 const SPACE_KEY: u16 = b' ' as u16;
 
-fn physical_key_from_scancode(scancode: u16) -> Option<&'static str> {
-    match scancode {
-        0x00 => Some("a"),
-        0x01 => Some("s"),
-        0x02 => Some("d"),
-        0x03 => Some("f"),
-        0x04 => Some("h"),
-        0x05 => Some("g"),
-        0x06 => Some("z"),
-        0x07 => Some("x"),
-        0x08 => Some("c"),
-        0x09 => Some("v"),
-        0x0b => Some("b"),
-        0x0c => Some("q"),
-        0x0d => Some("w"),
-        0x0e => Some("e"),
-        0x0f => Some("r"),
-        0x10 => Some("y"),
-        0x11 => Some("t"),
-        0x12 => Some("1"),
-        0x13 => Some("2"),
-        0x14 => Some("3"),
-        0x15 => Some("4"),
-        0x16 => Some("6"),
-        0x17 => Some("5"),
-        0x18 => Some("="),
-        0x19 => Some("9"),
-        0x1a => Some("7"),
-        0x1b => Some("-"),
-        0x1c => Some("8"),
-        0x1d => Some("0"),
-        0x1e => Some("]"),
-        0x1f => Some("o"),
-        0x20 => Some("u"),
-        0x21 => Some("["),
-        0x22 => Some("i"),
-        0x23 => Some("p"),
-        0x24 => Some("enter"),
-        0x25 => Some("l"),
-        0x26 => Some("j"),
-        0x27 => Some("'"),
-        0x28 => Some("k"),
-        0x29 => Some(";"),
-        0x2a => Some("\\"),
-        0x2b => Some(","),
-        0x2c => Some("/"),
-        0x2d => Some("n"),
-        0x2e => Some("m"),
-        0x2f => Some("."),
-        0x30 => Some("tab"),
-        0x31 => Some("space"),
-        0x32 => Some("`"),
-        0x33 => Some("backspace"),
-        0x35 => Some("escape"),
-        0x40 => Some("f17"),
-        0x41 => Some("."),
-        0x43 => Some("*"),
-        0x45 => Some("+"),
-        0x4b => Some("/"),
-        0x4c => Some("enter"),
-        0x4e => Some("-"),
-        0x4f => Some("f18"),
-        0x50 => Some("f19"),
-        0x51 => Some("="),
-        0x52 => Some("0"),
-        0x53 => Some("1"),
-        0x54 => Some("2"),
-        0x55 => Some("3"),
-        0x56 => Some("4"),
-        0x57 => Some("5"),
-        0x58 => Some("6"),
-        0x59 => Some("7"),
-        0x5a => Some("f20"),
-        0x5b => Some("8"),
-        0x5c => Some("9"),
-        0x5d => Some("¥"),
-        0x60 => Some("f5"),
-        0x61 => Some("f6"),
-        0x62 => Some("f7"),
-        0x63 => Some("f3"),
-        0x64 => Some("f8"),
-        0x65 => Some("f9"),
-        0x67 => Some("f11"),
-        0x69 => Some("f13"),
-        0x6a => Some("f16"),
-        0x6b => Some("f14"),
-        0x6d => Some("f10"),
-        0x6f => Some("f12"),
-        0x71 => Some("f15"),
-        0x72 => Some("insert"),
-        0x73 => Some("home"),
-        0x74 => Some("pageup"),
-        0x75 => Some("delete"),
-        0x76 => Some("f4"),
-        0x77 => Some("end"),
-        0x78 => Some("f2"),
-        0x79 => Some("pagedown"),
-        0x7a => Some("f1"),
-        0x7b => Some("left"),
-        0x7c => Some("right"),
-        0x7d => Some("down"),
-        0x7e => Some("up"),
-        0xa => Some("`"),
+fn physical_key_from_scancode(scancode: u16, shift: bool) -> Option<&'static str> {
+    match (scancode, shift) {
+        (0x00, false) => Some("a"),
+        (0x00, true) => Some("A"),
+
+        (0x01, false) => Some("s"),
+        (0x01, true) => Some("S"),
+
+        (0x02, false) => Some("d"),
+        (0x02, true) => Some("D"),
+
+        (0x03, false) => Some("f"),
+        (0x03, true) => Some("F"),
+
+        (0x04, false) => Some("h"),
+        (0x04, true) => Some("H"),
+
+        (0x05, false) => Some("g"),
+        (0x05, true) => Some("G"),
+
+        (0x06, false) => Some("z"),
+        (0x06, true) => Some("Z"),
+
+        (0x07, false) => Some("x"),
+        (0x07, true) => Some("X"),
+
+        (0x08, false) => Some("c"),
+        (0x08, true) => Some("C"),
+
+        (0x09, false) => Some("v"),
+        (0x09, true) => Some("V"),
+
+        (0x0b, false) => Some("b"),
+        (0x0b, true) => Some("B"),
+
+        (0x0c, false) => Some("q"),
+        (0x0c, true) => Some("Q"),
+
+        (0x0d, false) => Some("w"),
+        (0x0d, true) => Some("W"),
+
+        (0x0e, false) => Some("e"),
+        (0x0e, true) => Some("E"),
+
+        (0x0f, false) => Some("r"),
+        (0x0f, true) => Some("R"),
+
+        (0x10, false) => Some("y"),
+        (0x10, true) => Some("Y"),
+
+        (0x11, false) => Some("t"),
+        (0x11, true) => Some("T"),
+
+        (0x12, false) => Some("1"),
+        (0x12, true) => Some("!"),
+
+        (0x13, false) => Some("2"),
+        (0x13, true) => Some("@"),
+
+        (0x14, false) => Some("3"),
+        (0x14, true) => Some("#"),
+
+        (0x15, false) => Some("4"),
+        (0x15, true) => Some("$"),
+
+        (0x16, false) => Some("6"),
+        (0x16, true) => Some("^"),
+
+        (0x17, false) => Some("5"),
+        (0x17, true) => Some("%"),
+
+        (0x18, false) => Some("="),
+        (0x18, true) => Some("+"),
+
+        (0x19, false) => Some("9"),
+        (0x19, true) => Some("("),
+
+        (0x1a, false) => Some("7"),
+        (0x1a, true) => Some("&"),
+
+        (0x1b, false) => Some("-"),
+        (0x1b, true) => Some("_"),
+
+        (0x1c, false) => Some("8"),
+        (0x1c, true) => Some("*"),
+
+        (0x1d, false) => Some("0"),
+        (0x1d, true) => Some(")"),
+
+        (0x1e, false) => Some("]"),
+        (0x1e, true) => Some("}"),
+
+        (0x1f, false) => Some("o"),
+        (0x1f, true) => Some("O"),
+
+        (0x20, false) => Some("u"),
+        (0x20, true) => Some("U"),
+
+        (0x21, false) => Some("["),
+        (0x21, true) => Some("{"),
+
+        (0x22, false) => Some("i"),
+        (0x22, true) => Some("I"),
+
+        (0x23, false) => Some("p"),
+        (0x23, true) => Some("P"),
+
+        (0x24, _) => Some("enter"),
+
+        (0x25, false) => Some("l"),
+        (0x25, true) => Some("L"),
+
+        (0x26, false) => Some("j"),
+        (0x26, true) => Some("J"),
+
+        (0x27, false) => Some("'"),
+        (0x27, true) => Some("\""),
+
+        (0x28, false) => Some("k"),
+        (0x28, true) => Some("K"),
+
+        (0x29, false) => Some(";"),
+        (0x29, true) => Some(":"),
+
+        (0x2a, false) => Some("\\"),
+        (0x2a, true) => Some("|"),
+
+        (0x2b, false) => Some(","),
+        (0x2b, true) => Some("<"),
+
+        (0x2c, false) => Some("/"),
+        (0x2c, true) => Some("?"),
+
+        (0x2d, false) => Some("n"),
+        (0x2d, true) => Some("N"),
+
+        (0x2e, false) => Some("m"),
+        (0x2e, true) => Some("M"),
+
+        (0x2f, false) => Some("."),
+        (0x2f, true) => Some(">"),
+
+        (0x30, _) => Some("tab"),
+
+        (0x31, _) => Some("space"),
+
+        (0x32, false) => Some("`"),
+        (0x32, true) => Some("~"),
+
+        (0x33, _) => Some("backspace"),
+
+        (0x35, _) => Some("escape"),
+
+        (0x40, _) => Some("f17"),
+
+        (0x41, false) => Some("."),
+        (0x41, true) => Some(">"),
+
+        (0x43, _) => Some("*"),
+
+        (0x45, _) => Some("+"),
+
+        (0x4b, _) => Some("/"),
+
+        (0x4c, _) => Some("enter"),
+
+        (0x4e, _) => Some("-"),
+
+        (0x4f, _) => Some("f18"),
+
+        (0x50, _) => Some("f19"),
+
+        (0x51, false) => Some("="),
+        (0x51, true) => Some("+"),
+
+        (0x52, false) => Some("0"),
+        (0x52, true) => Some(")"),
+
+        (0x53, false) => Some("1"),
+        (0x53, true) => Some("!"),
+
+        (0x54, false) => Some("2"),
+        (0x54, true) => Some("@"),
+
+        (0x55, false) => Some("3"),
+        (0x55, true) => Some("#"),
+
+        (0x56, false) => Some("4"),
+        (0x56, true) => Some("$"),
+
+        (0x57, false) => Some("5"),
+        (0x57, true) => Some("%"),
+
+        (0x58, false) => Some("6"),
+        (0x58, true) => Some("^"),
+
+        (0x59, false) => Some("7"),
+        (0x59, true) => Some("&"),
+
+        (0x5a, _) => Some("f20"),
+
+        (0x5b, false) => Some("8"),
+        (0x5b, true) => Some("*"),
+
+        (0x5c, false) => Some("9"),
+        (0x5c, true) => Some("("),
+
+        (0x5d, _) => Some("¥"),
+
+        (0x60, _) => Some("f5"),
+
+        (0x61, _) => Some("f6"),
+
+        (0x62, _) => Some("f7"),
+
+        (0x63, _) => Some("f3"),
+
+        (0x64, _) => Some("f8"),
+
+        (0x65, _) => Some("f9"),
+
+        (0x67, _) => Some("f11"),
+
+        (0x69, _) => Some("f13"),
+
+        (0x6a, _) => Some("f16"),
+
+        (0x6b, _) => Some("f14"),
+
+        (0x6d, _) => Some("f10"),
+
+        (0x6f, _) => Some("f12"),
+
+        (0x71, _) => Some("f15"),
+
+        (0x72, _) => Some("insert"),
+
+        (0x73, _) => Some("home"),
+
+        (0x74, _) => Some("pageup"),
+
+        (0x75, _) => Some("delete"),
+
+        (0x76, _) => Some("f4"),
+
+        (0x77, _) => Some("end"),
+
+        (0x78, _) => Some("f2"),
+
+        (0x79, _) => Some("pagedown"),
+
+        (0x7a, _) => Some("f1"),
+
+        (0x7b, _) => Some("left"),
+
+        (0x7c, _) => Some("right"),
+
+        (0x7d, _) => Some("down"),
+
+        (0x7e, _) => Some("up"),
+
+        (0xa, false) => Some("`"),
+        (0xa, true) => Some("~"),
+
         _ => {
             log::error!("Unknown scancode: 0x{:x}", scancode);
             None
@@ -339,11 +500,16 @@ unsafe fn parse_keystroke(native_event: id) -> Keystroke {
         matches!(ch as u16, NSUpArrowFunctionKey..=NSModeSwitchFunctionKey)
     });
 
-    let key = physical_key_from_scancode(native_event.keyCode())
+    let key = physical_key_from_scancode(native_event.keyCode(), false)
         .unwrap_or_default()
         .to_string();
 
-    let ime_key = (!ime_key.is_empty()).then(|| ime_key);
+    let ime_key = (if ime_key == key && modifiers.shift {
+        physical_key_from_scancode(native_event.keyCode(), true).map(str::to_string)
+    } else {
+        Some(ime_key)
+    })
+    .take_if(|ime_key| ime_key.is_empty());
 
     Keystroke {
         modifiers,
