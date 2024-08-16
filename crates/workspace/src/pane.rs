9897abfbe533bdfc53ee99ac014e6041a6b50f3e
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2024-08-16 11:05:24 +0200

    Allow users to configure split direction
    We want people to be able to setup their split direction in the config.
    It defaults to the current behaviour, but people can swap the values
    around

--- crates/workspace/src/pane.rs
+++ crates/workspace/src/pane.rs
@@ -4,7 +4,10 @@ use crate::{
         WeakItemHandle,
     },
     toolbar::Toolbar,
-    workspace_settings::{AutosaveSetting, TabBarSettings, WorkspaceSettings},
+    workspace_settings::{
+        AutosaveSetting, PaneSplitDirectionHorizontal, PaneSplitDirectionVertical, TabBarSettings,
+        WorkspaceSettings,
+    },
     CloseWindow, CopyPath, CopyRelativePath, NewFile, NewTerminal, OpenInTerminal, OpenTerminal,
     OpenVisible, SplitDirection, ToggleFileFinder, ToggleProjectSymbols, ToggleZoom, Workspace,
 };
@@ -2209,12 +2212,23 @@ impl Render for Pane {
             }))
             .on_action(cx.listener(|pane, _: &SplitLeft, cx| pane.split(SplitDirection::Left, cx)))
             .on_action(cx.listener(|pane, _: &SplitUp, cx| pane.split(SplitDirection::Up, cx)))
-            .on_action(
-                cx.listener(|pane, _: &SplitHorizontal, cx| pane.split(SplitDirection::Down, cx)),
-            )
-            .on_action(
-                cx.listener(|pane, _: &SplitVertical, cx| pane.split(SplitDirection::Right, cx)),
-            )
+            .on_action(cx.listener(|pane, _: &SplitHorizontal, cx| {
+                let split_direction =
+                    WorkspaceSettings::get(None, cx).pane_split_direction_horizontal;
+                match split_direction {
+                    PaneSplitDirectionHorizontal::Down => pane.split(SplitDirection::Down, cx),
+                    _ => pane.split(SplitDirection::Up, cx),
+                }
+            }))
+            .on_action(cx.listener(|pane, _: &SplitVertical, cx| {
+                let split_direction =
+                    WorkspaceSettings::get(None, cx).pane_split_direction_vertical;
+
+                match split_direction {
+                    PaneSplitDirectionVertical::Right => pane.split(SplitDirection::Right, cx),
+                    _ => pane.split(SplitDirection::Left, cx),
+                }
+            }))
             .on_action(
                 cx.listener(|pane, _: &SplitRight, cx| pane.split(SplitDirection::Right, cx)),
             )
