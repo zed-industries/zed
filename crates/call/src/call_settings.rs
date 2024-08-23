e434a41583a088976fcdd4938fddeb82d2282cbd
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-08-23 13:12:07 +0200

    call: Merge setting types

--- crates/call/src/call_settings.rs
+++ crates/call/src/call_settings.rs
@@ -4,30 +4,29 @@ use schemars::JsonSchema;
 use serde_derive::{Deserialize, Serialize};
 use settings::{Settings, SettingsSources};
 
-#[derive(Deserialize, Debug)]
+/// Configuration of voice calls in Zed.
+#[derive(Clone, Debug, Deserialize, Serialize, JsonSchema)]
+#[serde(default)]
 pub struct CallSettings {
+    /// Whether the microphone should be muted when joining a channel or a call.
     pub mute_on_join: bool,
+    /// Whether your current project should be shared when joining an empty channel.
     pub share_on_join: bool,
 }
 
-/// Configuration of voice calls in Zed.
-#[derive(Clone, Default, Serialize, Deserialize, JsonSchema, Debug)]
-pub struct CallSettingsContent {
-    /// Whether the microphone should be muted when joining a channel or a call.
-    ///
-    /// Default: false
-    pub mute_on_join: Option<bool>,
-
-    /// Whether your current project should be shared when joining an empty channel.
-    ///
-    /// Default: true
-    pub share_on_join: Option<bool>,
+impl Default for CallSettings {
+    fn default() -> Self {
+        Self {
+            mute_on_join: false,
+            share_on_join: false,
+        }
+    }
 }
 
 impl Settings for CallSettings {
     const KEY: Option<&'static str> = Some("calls");
 
-    type FileContent = CallSettingsContent;
+    type FileContent = Self;
 
     fn load(sources: SettingsSources<Self::FileContent>, _: &mut AppContext) -> Result<Self> {
         sources.json_merge()
