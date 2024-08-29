232de88023532f47c700730bb0c1f37f1e4a1bf6
Nate Butler <iamnbutler@gmail.com>
2024-08-29 17:17:15 -0400

    Unify Story/StoryContainers

--- crates/storybook/src/stories/text.rs
+++ crates/storybook/src/stories/text.rs
@@ -15,98 +15,106 @@ impl TextStory {
 
 impl Render for TextStory {
     fn render(&mut self, cx: &mut gpui::ViewContext<Self>) -> impl IntoElement {
-        StoryContainer::new("Text Story", "crates/storybook/src/stories/text.rs")
-            .children(
-                vec![
-
-            StorySection::new()
-                .child(
-                    StoryItem::new("Default", div().bg(gpui::blue()).child("Hello World!"))
+        Story::container()
+            .child(Story::title("Text"))
+            .children(vec![
+                StorySection::new()
+                    .child(
+                        StoryItem::new("Default", div().bg(gpui::blue()).child("Hello World!"))
+                            .usage(indoc! {r##"
+                                div()
+                                    .child("Hello World!")
+                                "##
+                            }),
+                    )
+                    .child(
+                        StoryItem::new(
+                            "Wrapping Text",
+                            div().max_w_96().child(concat!(
+                                "The quick brown fox jumps over the lazy dog. ",
+                                "Meanwhile, the lazy dog decided it was time for a change. ",
+                                "He started daily workout routines, ate healthier and became the fastest dog in town.",
+                            )),
+                        )
+                        .description("Set a width or max-width to enable text wrapping.")
                         .usage(indoc! {r##"
                             div()
-                                .child("Hello World!")
+                                .max_w_96()
+                                .child("Some text that you want to wrap.")
                             "##
                         }),
-                )
-                .child(
-                    StoryItem::new("Wrapping Text",
-                        div().max_w_96()
-                            .child(
-                                concat!(
-                                    "The quick brown fox jumps over the lazy dog. ",
+                    )
+                    .child(
+                        StoryItem::new(
+                            "tbd",
+                            div().flex().w_96().child(
+                                div().overflow_hidden().child(concat!(
+                                    "flex-row. width 96. overflow-hidden. The quick brown fox jumps over the lazy dog. ",
                                     "Meanwhile, the lazy dog decided it was time for a change. ",
                                     "He started daily workout routines, ate healthier and became the fastest dog in town.",
-                                )
-                            )
+                                )),
+                            ),
+                        ),
                     )
-                    .description("Set a width or max-width to enable text wrapping.")
-                    .usage(indoc! {r##"
-                        div()
-                            .max_w_96()
-                            .child("Some text that you want to wrap.")
-                        "##
-                    })
-                )
-                .child(
-                    StoryItem::new("tbd",
-                    div().flex().w_96().child(div().overflow_hidden().child(concat!(
-                            "flex-row. width 96. overflow-hidden. The quick brown fox jumps over the lazy dog. ",
-                            "Meanwhile, the lazy dog decided it was time for a change. ",
-                            "He started daily workout routines, ate healthier and became the fastest dog in town.",
-                        )))
-                    )
-                )
-                .child(
-                    StoryItem::new("Text in Horizontal Flex",
-                        div().flex().w_96().bg(red()).child(concat!(
-                                        "flex-row. width 96. The quick brown fox jumps over the lazy dog. ",
-                                        "Meanwhile, the lazy dog decided it was time for a change. ",
-                                        "He started daily workout routines, ate healthier and became the fastest dog in town.",
-                                    ))
-                    )
-                    .usage(indoc! {r##"
-                        // NOTE: When rendering text in a horizontal flex container,
-                        // Taffy will not pass width constraints down from the parent.
-                        // To fix this, render text in a parent with overflow: hidden
+                    .child(
+                        StoryItem::new(
+                            "Text in Horizontal Flex",
+                            div().flex().w_96().bg(red()).child(concat!(
+                                "flex-row. width 96. The quick brown fox jumps over the lazy dog. ",
+                                "Meanwhile, the lazy dog decided it was time for a change. ",
+                                "He started daily workout routines, ate healthier and became the fastest dog in town.",
+                            )),
+                        )
+                        .usage(indoc! {r##"
+                            // NOTE: When rendering text in a horizontal flex container,
+                            // Taffy will not pass width constraints down from the parent.
+                            // To fix this, render text in a parent with overflow: hidden
 
-                        div()
-                            .max_w_96()
-                            .child("Some text that you want to wrap.")
-                        "##
-                    })
-                )
-                .child(
-                    StoryItem::new("Interactive Text",
-                        InteractiveText::new(
-                            "interactive",
-                            StyledText::new("Hello world, how is it going?").with_highlights(&cx.text_style(), [
-                                (6..11, HighlightStyle {
-                                    background_color: Some(green()),
-                                    ..Default::default()
-                                }),
-                            ]),
-                        )
-                        .on_click(vec![2..4, 1..3, 7..9], |range_ix, _cx| {
-                            println!("Clicked range {range_ix}");
-                        })
+                            div()
+                                .max_w_96()
+                                .child("Some text that you want to wrap.")
+                            "##
+                        }),
                     )
-                    .usage(indoc! {r##"
-                        InteractiveText::new(
-                            "interactive",
-                            StyledText::new("Hello world, how is it going?").with_highlights(&cx.text_style(), [
-                                (6..11, HighlightStyle {
-                                    background_color: Some(green()),
-                                    ..Default::default()
-                                }),
-                            ]),
+                    .child(
+                        StoryItem::new(
+                            "Interactive Text",
+                            InteractiveText::new(
+                                "interactive",
+                                StyledText::new("Hello world, how is it going?").with_highlights(
+                                    &cx.text_style(),
+                                    [
+                                        (
+                                            6..11,
+                                            HighlightStyle {
+                                                background_color: Some(green()),
+                                                ..Default::default()
+                                            },
+                                        ),
+                                    ],
+                                ),
+                            )
+                            .on_click(vec![2..4, 1..3, 7..9], |range_ix, _cx| {
+                                println!("Clicked range {range_ix}");
+                            }),
                         )
-                        .on_click(vec![2..4, 1..3, 7..9], |range_ix, _cx| {
-                            println!("Clicked range {range_ix}");
-                        })
-                        "##
-                    })
-                )
-        ]
-            ).into_element()
+                        .usage(indoc! {r##"
+                            InteractiveText::new(
+                                "interactive",
+                                StyledText::new("Hello world, how is it going?").with_highlights(&cx.text_style(), [
+                                    (6..11, HighlightStyle {
+                                        background_color: Some(green()),
+                                        ..Default::default()
+                                    }),
+                                ]),
+                            )
+                            .on_click(vec![2..4, 1..3, 7..9], |range_ix, _cx| {
+                                println!("Clicked range {range_ix}");
+                            })
+                            "##
+                        }),
+                    ),
+            ])
+            .into_element()
     }
 }
