02cd5128c77571026c6943c93602b03fb2c199f3
Kyle Kelley <rgbkrk@gmail.com>
2024-08-29 18:22:28 -0700

    repl: Make output buffer be readonly, never dirty (#17121)

--- crates/repl/src/outputs/markdown.rs
+++ crates/repl/src/outputs/markdown.rs
@@ -58,7 +58,10 @@ impl OutputContent for MarkdownView {
     fn buffer_content(&mut self, cx: &mut WindowContext) -> Option<Model<Buffer>> {
         let buffer = cx.new_model(|cx| {
             // todo!(): Bring in the language registry so we can set the language to markdown
-            Buffer::local(self.raw_text.clone(), cx).with_language(language::PLAIN_TEXT.clone(), cx)
+            let mut buffer = Buffer::local(self.raw_text.clone(), cx)
+                .with_language(language::PLAIN_TEXT.clone(), cx);
+            buffer.set_capability(language::Capability::ReadOnly, cx);
+            buffer
         });
         Some(buffer)
     }
