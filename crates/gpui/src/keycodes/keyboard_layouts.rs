664071cf8587e5de6ee7adf6768c76e1bbf01135
Junkui Zhang <364772080@qq.com>
2024-08-30 20:17:35 +0800

    basic layouts

---
+++ crates/gpui/src/keycodes/keyboard_layouts.rs
@@ -0,0 +1,8 @@
+pub(crate) mod czech;
+pub(crate) mod german;
+pub(crate) mod russian;
+
+// https://stackoverflow.com/questions/3202629/where-can-i-find-a-list-of-mac-virtual-key-codes
+pub(crate) use czech::*;
+pub(crate) use german::*;
+pub(crate) use russian::*;
---
+++ crates/gpui/src/keycodes/keyboard_layouts/russian.rs
@@ -0,0 +1,30 @@
+use std::{collections::HashMap, sync::LazyLock};
+
+use crate::VirtualKeyCode;
+
+pub(crate) static RUSSIAN_LAYOUT_ANSI: LazyLock<HashMap<u16, VirtualKeyCode>> =
+    LazyLock::new(|| {
+        let mut map = HashMap::new();
+        map.insert(0x32, VirtualKeyCode::OEM3);
+        map.insert(0x21, VirtualKeyCode::OEM4);
+        map.insert(0x1E, VirtualKeyCode::OEM6);
+        map.insert(0x2A, VirtualKeyCode::OEM5);
+        map.insert(0x29, VirtualKeyCode::OEM1);
+        map.insert(0x27, VirtualKeyCode::OEM7);
+        map.insert(0x2C, VirtualKeyCode::OEM2);
+        map
+    });
+
+pub(crate) static RUSSIAN_LAYOUT_ISO: LazyLock<HashMap<u16, VirtualKeyCode>> =
+    LazyLock::new(|| {
+        let mut map = HashMap::new();
+        map.insert(0x0A, VirtualKeyCode::OEM3);
+        map.insert(0x21, VirtualKeyCode::OEM4);
+        map.insert(0x1E, VirtualKeyCode::OEM6);
+        map.insert(0x2A, VirtualKeyCode::OEM5);
+        map.insert(0x29, VirtualKeyCode::OEM1);
+        map.insert(0x27, VirtualKeyCode::OEM7);
+        map.insert(0x2C, VirtualKeyCode::OEM2);
+        map.insert(0x32, VirtualKeyCode::OEM102);
+        map
+    });
