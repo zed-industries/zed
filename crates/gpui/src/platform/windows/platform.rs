7d5ea3b040bf5d476dae93a4ab231160ca09a72c
korigamik-hypr <korigamik@gmail.com>
2024-09-03 12:46:20 +0530

    fix conversion on windows

--- crates/gpui/src/platform/windows/platform.rs
+++ crates/gpui/src/platform/windows/platform.rs
@@ -406,7 +406,7 @@ impl Platform for WindowsPlatform {
         executor
             .spawn(async move {
                 let _ = std::process::Command::new("cmd")
-                    .args(&["/c", "start", "", path])
+                    .args(&["/c", "start", "", path.as_ref()])
                     .spawn()
                     .expect("Failed to open file");
             })
