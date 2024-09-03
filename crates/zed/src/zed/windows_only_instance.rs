508a6ef941a5c5c1ae76c8a50dfe750dc6854c11
Junkui Zhang <364772080@qq.com>
2024-09-03 23:40:24 +0800

    introduce app identifier

--- crates/zed/src/zed/windows_only_instance.rs
+++ crates/zed/src/zed/windows_only_instance.rs
@@ -1,3 +1,4 @@
+use gpui::app_identifier::{get_app_instance_event_identifier, register_app_identifier};
 use release_channel::ReleaseChannel;
 use windows::{
     core::HSTRING,
@@ -7,22 +8,31 @@ use windows::{
     },
 };
 
-fn retrieve_app_instance_event_identifier() -> &'static str {
+fn register_zed_identifier() {
     match *release_channel::RELEASE_CHANNEL {
-        ReleaseChannel::Dev => "Local\\Zed-Editor-Dev-Instance-Event",
-        ReleaseChannel::Nightly => "Local\\Zed-Editor-Nightly-Instance-Event",
-        ReleaseChannel::Preview => "Local\\Zed-Editor-Preview-Instance-Event",
-        ReleaseChannel::Stable => "Local\\Zed-Editor-Stable-Instance-Event",
+        ReleaseChannel::Dev => register_app_identifier("Zed-Editor-Dev"),
+        ReleaseChannel::Nightly => register_app_identifier("Zed-Editor-Nightly"),
+        ReleaseChannel::Preview => register_app_identifier("Zed-Editor-Preview"),
+        ReleaseChannel::Stable => register_app_identifier("Zed-Editor-Stable"),
     }
 }
 
 pub fn check_single_instance() -> bool {
+    if *db::ZED_STATELESS || *release_channel::RELEASE_CHANNEL == ReleaseChannel::Dev {
+        return true;
+    }
+
+    register_zed_identifier();
+    check_single_instance_event()
+}
+
+fn check_single_instance_event() -> bool {
     unsafe {
         CreateEventW(
             None,
             false,
             false,
-            &HSTRING::from(retrieve_app_instance_event_identifier()),
+            &HSTRING::from(get_app_instance_event_identifier()),
         )
         .expect("Unable to create instance sync event")
     };
