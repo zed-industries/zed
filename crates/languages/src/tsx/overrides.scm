a91f2bfcc48601bd8bcb755779236d943ac8670d
Conrad Irwin <conrad.irwin@gmail.com>
2024-09-11 16:15:50 -0400

    Slightly better

--- crates/languages/src/tsx/overrides.scm
+++ crates/languages/src/tsx/overrides.scm
@@ -5,11 +5,11 @@
   (template_string)
 ] @string
 
-(jsx_element) @element
+[
+(jsx_element)
+] @element
 
 [
-  (jsx_opening_element)
-  (jsx_closing_element)
-  (jsx_self_closing_element)
+  (jsx_attribute)
   (jsx_expression)
 ] @default
