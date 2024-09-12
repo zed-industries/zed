3613ebd93c137578d947b482fe108e81313a8b47
CharlesChen0823 <yongchen0823@gmail.com>
2024-09-13 04:55:59 +0800

    editor: Fix an error when cut with vim visual line select (#17591)
    Becuause in vim visual mode, we will always select next char, hit
    [here](https://github.com/zed-industries/zed/blob/66ef31882341852229c74996867916fbd4a2fe2a/crates/vim/src/visual.rs#L174),
    when using editor method
    for `cut` this selection, will hit this error.
    
    Closes #17585 
    
    Release Notes:
    
    - N/A
    
    ---------
    
    Co-authored-by: Conrad Irwin <conrad.irwin@gmail.com>

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -6671,7 +6671,11 @@ impl Editor {
                 let is_entire_line = selection.is_empty() || self.selections.line_mode;
                 if is_entire_line {
                     selection.start = Point::new(selection.start.row, 0);
-                    selection.end = cmp::min(max_point, Point::new(selection.end.row + 1, 0));
+                    if !selection.is_empty() && selection.end.column == 0 {
+                        selection.end = cmp::min(max_point, selection.end);
+                    } else {
+                        selection.end = cmp::min(max_point, Point::new(selection.end.row + 1, 0));
+                    }
                     selection.goal = SelectionGoal::None;
                 }
                 if is_first {
