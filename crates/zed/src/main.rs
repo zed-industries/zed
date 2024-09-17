8b62f7c969d3b8f4fe9c86c3c77b6edccfb681c1
Joseph T Lyons <JosephTLyons@gmail.com>
2024-09-17 14:55:14 -0400

    WIP

--- crates/zed/src/main.rs
+++ crates/zed/src/main.rs
@@ -497,12 +497,14 @@ fn main() {
         );
         match (&system_id, &installation_id) {
             (Some(IdType::New(_)), Some(IdType::New(_))) => {
-                telemetry.report_app_event("first app open ever".to_string())
+                telemetry.report_app_event("first app open ever".to_string());
             }
             (Some(IdType::Existing(_)), Some(IdType::New(_))) => {
-                telemetry.report_app_event("first app open for release channel".to_string())
+                telemetry.report_app_event("first app open for release channel".to_string());
             }
-            (Some(_), Some(IdType::Existing(_))) => telemetry.report_app_event("open".to_string()),
+            (Some(_), Some(IdType::Existing(_))) => {
+                telemetry.report_app_event("open".to_string());
+            }
             _ => {}
         }
         let app_session = cx.new_model(|cx| AppSession::new(session, cx));
