93e94856ccbe40a6b71bae70414b2e6a05176f9f
Jason Lee <huacnlee@gmail.com>
2024-09-23 16:02:09 +0800

    Also fix show on macOS

--- crates/gpui/src/platform/mac/window.rs
+++ crates/gpui/src/platform/mac/window.rs
@@ -706,7 +706,7 @@ impl MacWindow {
                 }
             }
 
-            if focus {
+            if focus && show {
                 native_window.makeKeyAndOrderFront_(nil);
             } else if show {
                 native_window.orderFront_(nil);
