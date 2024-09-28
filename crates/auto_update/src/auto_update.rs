1021f0e28808285e3d301fcb9ccf9af40854ea98
Joseph T. Lyons <JosephTLyons@gmail.com>
2024-09-28 14:21:13 -0400

    Show release notes locally when showing update notification (#18486)
    Closes https://github.com/zed-industries/zed/issues/17527
    
    I think we are ok to switch to using the local action now. There are a
    few things we don't support, like media, but we don't include media
    directly too often, and I think this might help push the community to
    maybe add support for it. That being said, I updated the markdown coming
    back from the endpoint to include links to the web version of the
    release notes, so they can always hop over to that version, if they
    would like.
    
    
    https://github.com/user-attachments/assets/b4d207a7-1640-48f1-91d0-94537f74116c
    
    All forming of the Markdown happens in the endpoint, so if someone with
    a better eye wants to update this, you can do that here:
    
    
    https://github.com/zed-industries/zed.dev/blob/0e5923e3e7d1caa8b4bf32d0a7f8999b34dbe64c/src/pages/api/release_notes/v2/%5Bchannel_type%5D/%5Bversion%5D.ts#L50-L62
    
    Release Notes:
    
    - Changed the `view the release notes` button in the update toast to
    trigger the local release notes action.

--- crates/auto_update/src/auto_update.rs
+++ crates/auto_update/src/auto_update.rs
@@ -345,15 +345,17 @@ pub fn notify_of_any_new_update(cx: &mut ViewContext<Workspace>) -> Option<()> {
         let should_show_notification = should_show_notification.await?;
         if should_show_notification {
             workspace.update(&mut cx, |workspace, cx| {
+                let workspace_handle = workspace.weak_handle();
                 workspace.show_notification(
                     NotificationId::unique::<UpdateNotification>(),
                     cx,
-                    |cx| cx.new_view(|_| UpdateNotification::new(version)),
+                    |cx| cx.new_view(|_| UpdateNotification::new(version, workspace_handle)),
                 );
-                updater
-                    .read(cx)
-                    .set_should_show_update_notification(false, cx)
-                    .detach_and_log_err(cx);
+                updater.update(cx, |updater, cx| {
+                    updater
+                        .set_should_show_update_notification(false, cx)
+                        .detach_and_log_err(cx);
+                });
             })?;
         }
         anyhow::Ok(())
