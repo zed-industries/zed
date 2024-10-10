0d825b8c5c8df503cc85a681eebc41247c63c504
CharlesChen0823 <yongchen0823@gmail.com>
2024-10-10 20:39:33 +0800

    clippy

--- crates/evals/src/eval.rs
+++ crates/evals/src/eval.rs
@@ -26,7 +26,7 @@ use std::time::Duration;
 use std::{
     fs,
     path::Path,
-    process::{exit, Command, Stdio},
+    process::{exit, Stdio},
     sync::{
         atomic::{AtomicUsize, Ordering::SeqCst},
         Arc,
