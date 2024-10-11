6044f4fec07200122edbd3a5edbc4e2bec51789a
Kirill Bulatov <kirill@zed.dev>
2024-10-11 19:25:42 +0300

    Update terminals.rs
    Co-authored-by: Thorsten Ball <mrnugget@gmail.com>

--- crates/project/src/terminals.rs
+++ crates/project/src/terminals.rs
@@ -365,9 +365,9 @@ pub fn wrap_for_ssh(
         let path_string = path.to_string_lossy().to_string();
         // shlex will wrap the command in single quotes (''), disabling ~ expansion,
         // replace ith with something that works
-        let tilde_suffix = "~/";
-        if path.starts_with(tilde_suffix) {
-            let trimmed_path = &path_string[tilde_suffix.len()..];
+        let tilde_prefix = "~/";
+        if path.starts_with(tilde_prefix) {
+            let trimmed_path = &path_string[tilde_prefix.len()..];
             format!("cd \"$HOME/{trimmed_path}\"; {env_changes} {to_run}")
         } else {
             format!("cd {path:?}; {env_changes} {to_run}")
