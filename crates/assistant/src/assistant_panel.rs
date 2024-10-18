a49a0559d347bc2153f11d8c2f8366b41e4f7cd6
Junkui Zhang <364772080@qq.com>
2024-10-18 15:02:30 +0800

    remove more

--- crates/assistant/src/assistant_panel.rs
+++ crates/assistant/src/assistant_panel.rs
@@ -3242,7 +3242,6 @@ impl ContextEditor {
                 let tasks = paths
                     .clone()
                     .into_iter()
-                    .map(Into::into)
                     .map(|path| Workspace::project_path_for_path(project.clone(), &path, false, cx))
                     .collect::<Vec<_>>();
 
