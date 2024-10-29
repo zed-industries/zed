fe742f65dd12f0af6d30d16cbee7d886595b7fbd
Zorin FOSS <132728893+ZorinFoss@users.noreply.github.com>
2024-10-29 06:49:08 +0300

    Update highlights.scm

--- extensions/dart/languages/dart/highlights.scm
+++ extensions/dart/languages/dart/highlights.scm
@@ -1,30 +1,100 @@
-(dotted_identifier_list) @string
+; Variable
+(identifier) @variable
+
+; Keywords
+; --------------------
+[
+    (assert_builtin)
+    (break_builtin)
+    (const_builtin)
+    (part_of_builtin)
+    (rethrow_builtin)
+    (void_type)
+    "abstract"
+    "as"
+    "async"
+    "async*"
+    "await"
+    "base"
+    "case"
+    "catch"
+    "class"
+    "continue"
+    "covariant"
+    "default"
+    "deferred"
+    "do"
+    "dynamic"
+    "else"
+    "enum"
+    "export"
+    "extends"
+    "extension"
+    "external"
+    "factory"
+    "final"
+    "finally"
+    "for"
+    "Function"
+    "get"
+    "hide"
+    "if"
+    "implements"
+    "import"
+    "in"
+    "interface"
+    "is"
+    "late"
+    "library"
+    "mixin"
+    "new"
+    "on"
+    "operator"
+    "part"
+    "required"
+    "return"
+    "sealed"
+    "set"
+    "show"
+    "static"
+    "super"
+    "switch"
+    "sync*"
+    "throw"
+    "try"
+    "typedef"
+    "var"
+    "when"
+    "while"
+    "with"
+    "yield"
+] @keyword
 
 ; Methods
 ; --------------------
-(super) @function
 
-(function_expression_body (identifier) @type)
-; ((identifier)(selector (argument_part)) @function)
-
+; NOTE: This query is a bit of a work around for the fact that the dart grammar doesn't
+; specifically identify a node as a function call
 (((identifier) @function (#match? @function "^_?[a-z]"))
  . (selector . (argument_part))) @function
 
 ; Annotations
 ; --------------------
 (annotation
- name: (identifier) @attribute)
+  name: (identifier) @attribute)
 
 ; Operators and Tokens
 ; --------------------
 (template_substitution
- "$" @punctuation.special
- "{" @punctuation.special
- "}" @punctuation.special) @none
+  "$" @punctuation.special
+  "{" @punctuation.special
+  "}" @punctuation.special
+) @none
 
 (template_substitution
- "$" @punctuation.special
- (identifier_dollar_escaped) @variable) @none
+  "$" @punctuation.special
+  (identifier_dollar_escaped) @variable
+) @none
 
 (escape_sequence) @string.escape
 
@@ -44,62 +114,68 @@
  ">="
  "<="
  "||"
- (multiplicative_operator)
+ "~/"
  (increment_operator)
  (is_operator)
  (prefix_operator)
  (equality_operator)
  (additive_operator)
- ] @operator
+] @operator
+
+(type_arguments
+  "<" @punctuation.bracket
+  ">" @punctuation.bracket)
+
+(type_parameters
+  "<" @punctuation.bracket
+  ">" @punctuation.bracket)
 
 [
- "("
- ")"
- "["
- "]"
- "{"
- "}"
- ] @punctuation.bracket
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+]  @punctuation.bracket
 
 ; Delimiters
 ; --------------------
 [
- ";"
- "."
- ","
- ] @punctuation.delimiter
+  ";"
+  "."
+  ","
+] @punctuation.delimiter
 
 ; Types
 ; --------------------
+(type_identifier) @type
+((type_identifier) @type.builtin
+  (#match? @type.builtin "^(int|double|String|bool|List|Set|Map|Runes|Symbol)$"))
 (class_definition
- name: (identifier) @type)
+  name: (identifier) @type)
 (constructor_signature
- name: (identifier) @type)
+  name: (identifier) @type)
 (scoped_identifier
- scope: (identifier) @type)
+  scope: (identifier) @type)
 (function_signature
- name: (identifier) @function.method)
-
+  name: (identifier) @function)
 (getter_signature
- (identifier) @function.method)
-
+  (identifier) @function)
 (setter_signature
- name: (identifier) @function.method)
-(enum_declaration
- name: (identifier) @type)
-(enum_constant
- name: (identifier) @type)
-(void_type) @type
+  name: (identifier) @function)
 
 ((scoped_identifier
   scope: (identifier) @type
   name: (identifier) @type)
  (#match? @type "^[a-zA-Z]"))
 
-(type_identifier) @type
-
-(type_alias
- (type_identifier) @type.definition)
+; Enums
+; -------------------
+(enum_declaration
+  name: (identifier) @type)
+(enum_constant
+  name: (identifier) @identifier.constant)
 
 ; Variables
 ; --------------------
@@ -111,159 +187,60 @@
 
 ("Function" @type)
 
+(this) @variable.builtin
+
 ; properties
+
 (unconditional_assignable_selector
- (identifier) @property)
+  (identifier) @property)
 
 (conditional_assignable_selector
- (identifier) @property)
+  (identifier) @property)
+
+(cascade_section
+  (cascade_selector
+    (identifier) @property))
+
+((selector
+  (unconditional_assignable_selector (identifier) @function))
+  (selector (argument_part (arguments)))
+)
+
+(cascade_section
+  (cascade_selector (identifier) @function)
+  (argument_part (arguments))
+)
 
 ; assignments
 (assignment_expression
- left: (assignable_expression) @variable)
+  left: (assignable_expression) @variable)
 
 (this) @variable.builtin
 
 ; Parameters
 ; --------------------
 (formal_parameter
- name: (identifier) @variable.parameter)
+    name: (identifier) @identifier.parameter)
 
 (named_argument
- (label
-    (identifier) @variable.parameter))
+  (label (identifier) @identifier.parameter))
 
 ; Literals
 ; --------------------
 [
- (hex_integer_literal)
- (decimal_integer_literal)
- (decimal_floating_point_literal)
- ; TODO: inaccessible nodes
- ; (octal_integer_literal)
- ; (hex_floating_point_literal)
- ] @number
-
-(symbol_literal) @string.special.symbol
+    (hex_integer_literal)
+    (decimal_integer_literal)
+    (decimal_floating_point_literal)
+    ; TODO: inaccessbile nodes
+    ; (octal_integer_literal)
+    ; (hex_floating_point_literal)
+] @number
 
 (string_literal) @string
+(symbol_literal (identifier) @constant) @constant
 (true) @boolean
 (false) @boolean
-(null_literal) @constant.builtin
+(null_literal) @constant.null
 
+(documentation_comment) @comment
 (comment) @comment
-
-(documentation_comment) @comment.documentation
-
-; Keywords
-; --------------------
-[
- "import"
- "library"
- "export"
- "as"
- "show"
- "hide"
- ] @keyword.import
-
-; Reserved words (cannot be used as identifiers)
-[
-  (case_builtin)
- "late"
- "required"
- "extension"
- "on"
- "class"
- "enum"
- "extends"
- "in"
- "is"
- "new"
- "super"
- "with"
- ] @keyword
-
-"return" @keyword.return
-
-; Built in identifiers:
-; alone these are marked as keywords
-[
-  "deferred"
- "factory"
- "get"
- "implements"
- "interface"
- "library"
- "operator"
- "mixin"
- "part"
- "set"
- "typedef"
- ] @keyword
-
-[
-  "async"
-  "async*"
-  "sync*"
-  "await"
-  "yield"
-] @keyword.coroutine
-
-[
- (const_builtin)
- (final_builtin)
- "abstract"
-  "covariant"
- "dynamic"
- "external"
- "static"
- "final"
-  "base"
-  "sealed"
- ] @type.qualifier
-
-; when used as an identifier:
-((identifier) @variable.builtin
- (#any-of? @variable.builtin
-           "abstract"
-           "as"
-           "covariant"
-           "deferred"
-           "dynamic"
-           "export"
-           "external"
-           "factory"
-           "Function"
-           "get"
-           "implements"
-           "import"
-           "interface"
-           "library"
-           "operator"
-           "mixin"
-           "part"
-           "set"
-           "static"
-           "typedef"))
-
-[
-  "if"
-  "else"
-  "switch"
-  "default"
-] @keyword.conditional
-
-[
-  "try"
-  "throw"
-  "catch"
-  "finally"
-  (break_statement)
-] @keyword.exception
-
-[
-  "do"
-  "while"
-  "continue"
-  "for"
-] @keyword.repeat
