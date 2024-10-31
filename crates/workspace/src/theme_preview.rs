1457c3acf4b063630a650a87061bed1dcfdce37f
Nate Butler <iamnbutler@gmail.com>
2024-10-31 09:33:01 -0400

    Add avatars to theme preview

--- crates/workspace/src/theme_preview.rs
+++ crates/workspace/src/theme_preview.rs
@@ -1,8 +1,9 @@
-use std::fmt::Display;
-
 use gpui::{actions, AppContext, EventEmitter, FocusHandle, FocusableView, Hsla};
 use theme::all_theme_colors;
-use ui::{prelude::*, utils::calculate_contrast_ratio, ElevationIndex};
+use ui::{
+    prelude::*, utils::calculate_contrast_ratio, AudioStatus, Availability, Avatar,
+    AvatarAudioStatusIndicator, AvatarAvailabilityIndicator, ElevationIndex, Facepile, Indicator,
+};
 
 use crate::{Item, Workspace};
 
@@ -65,7 +66,76 @@ impl Item for ThemePreview {
     }
 }
 
+const AVATAR_URL: &str = "https://avatars.githubusercontent.com/u/1714999?v=4";
+
 impl ThemePreview {
+    fn preview_bg(cx: &WindowContext) -> Hsla {
+        cx.theme().colors().editor_background
+    }
+
+    fn render_avatars(&self, layer: ElevationIndex, cx: &ViewContext<Self>) -> impl IntoElement {
+        let base_avatar = Avatar::new(AVATAR_URL).size(px(24.)).into_any_element();
+
+        v_flex()
+            .gap_1()
+            .child(Headline::new("Avatars").size(HeadlineSize::Small))
+            .child(
+                h_flex()
+                    .gap_4()
+                    .child(Avatar::new(AVATAR_URL).size(px(24.)))
+                    .child(Avatar::new(AVATAR_URL).size(px(24.)).grayscale(true))
+                    .child(
+                        Avatar::new(AVATAR_URL)
+                            .size(px(24.))
+                            .indicator(AvatarAudioStatusIndicator::new(AudioStatus::Muted)),
+                    )
+                    .child(
+                        Avatar::new(AVATAR_URL)
+                            .size(px(24.))
+                            .indicator(AvatarAudioStatusIndicator::new(AudioStatus::Deafened)),
+                    )
+                    .child(
+                        Avatar::new(AVATAR_URL)
+                            .size(px(24.))
+                            .indicator(AvatarAvailabilityIndicator::new(Availability::Free)),
+                    )
+                    .child(
+                        Avatar::new(AVATAR_URL)
+                            .size(px(24.))
+                            .indicator(AvatarAvailabilityIndicator::new(Availability::Free)),
+                    )
+                    .child(
+                        div().py_3().px_1_5().bg(Self::preview_bg(cx)).child(
+                            Facepile::empty()
+                                .child(
+                                    Avatar::new(AVATAR_URL)
+                                        .border_color(Self::preview_bg(cx))
+                                        .size(rems(1.5))
+                                        .into_any_element(),
+                                )
+                                .child(
+                                    Avatar::new(AVATAR_URL)
+                                        .border_color(Self::preview_bg(cx))
+                                        .size(rems(1.5))
+                                        .into_any_element(),
+                                )
+                                .child(
+                                    Avatar::new(AVATAR_URL)
+                                        .border_color(Self::preview_bg(cx))
+                                        .size(rems(1.5))
+                                        .into_any_element(),
+                                )
+                                .child(
+                                    Avatar::new(AVATAR_URL)
+                                        .border_color(Self::preview_bg(cx))
+                                        .size(rems(1.5))
+                                        .into_any_element(),
+                                ),
+                        ),
+                    ),
+            )
+    }
+
     fn render_theme_layer(
         &self,
         layer: ElevationIndex,
@@ -83,7 +153,8 @@ impl ThemePreview {
         v_flex()
             .text_color(cx.theme().colors().text)
             .gap_2()
-            .child(Headline::new(layer.to_string()).size(HeadlineSize::Medium))
+            .child(Headline::new(layer.clone().to_string()).size(HeadlineSize::Medium))
+            .child(self.render_avatars(layer.clone(), cx))
             .child(
                 v_flex()
                     .w_full()
@@ -195,13 +266,13 @@ impl Render for ThemePreview {
     fn render(&mut self, cx: &mut ViewContext<Self>) -> impl ui::IntoElement {
         v_flex()
             .id("theme-preview")
+            .key_context("ThemePreview")
             .overflow_scroll()
             .size_full()
             .max_h_full()
             .p_4()
             .track_focus(&self.focus_handle)
-            .bg(cx.theme().colors().editor_background)
-            .key_context("ThemePreview")
+            .bg(Self::preview_bg(cx))
             .gap_4()
             .child(self.render_theme_layer(ElevationIndex::Background, cx))
             .child(self.render_theme_layer(ElevationIndex::Surface, cx))
