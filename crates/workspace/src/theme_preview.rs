81da9937f15d3564a0f92ff44ab6000f752bd75a
Nate Butler <iamnbutler@gmail.com>
2024-10-31 11:04:05 -0400

    Update theme_preview.rs

--- crates/workspace/src/theme_preview.rs
+++ crates/workspace/src/theme_preview.rs
@@ -1,5 +1,4 @@
 use gpui::{actions, AppContext, EventEmitter, FocusHandle, FocusableView, Hsla};
-use theme::all_theme_colors;
 use ui::{
     prelude::*, utils::calculate_contrast_ratio, AudioStatus, Availability, Avatar,
     AvatarAudioStatusIndicator, AvatarAvailabilityIndicator, ElevationIndex, Facepile, TintColor,
@@ -76,9 +75,14 @@ impl ThemePreview {
     fn render_avatars(&self, cx: &ViewContext<Self>) -> impl IntoElement {
         v_flex()
             .gap_1()
-            .child(Headline::new("Avatars").size(HeadlineSize::Small))
+            .child(
+                Headline::new("Avatars")
+                    .size(HeadlineSize::Small)
+                    .color(Color::Muted),
+            )
             .child(
                 h_flex()
+                    .items_start()
                     .gap_4()
                     .child(Avatar::new(AVATAR_URL).size(px(24.)))
                     .child(Avatar::new(AVATAR_URL).size(px(24.)).grayscale(true))
@@ -103,33 +107,31 @@ impl ThemePreview {
                             .indicator(AvatarAvailabilityIndicator::new(Availability::Free)),
                     )
                     .child(
-                        div().py_3().px_1_5().bg(Self::preview_bg(cx)).child(
-                            Facepile::empty()
-                                .child(
-                                    Avatar::new(AVATAR_URL)
-                                        .border_color(Self::preview_bg(cx))
-                                        .size(px(26.))
-                                        .into_any_element(),
-                                )
-                                .child(
-                                    Avatar::new(AVATAR_URL)
-                                        .border_color(Self::preview_bg(cx))
-                                        .size(px(26.))
-                                        .into_any_element(),
-                                )
-                                .child(
-                                    Avatar::new(AVATAR_URL)
-                                        .border_color(Self::preview_bg(cx))
-                                        .size(px(26.))
-                                        .into_any_element(),
-                                )
-                                .child(
-                                    Avatar::new(AVATAR_URL)
-                                        .border_color(Self::preview_bg(cx))
-                                        .size(px(26.))
-                                        .into_any_element(),
-                                ),
-                        ),
+                        Facepile::empty()
+                            .child(
+                                Avatar::new(AVATAR_URL)
+                                    .border_color(Self::preview_bg(cx))
+                                    .size(px(22.))
+                                    .into_any_element(),
+                            )
+                            .child(
+                                Avatar::new(AVATAR_URL)
+                                    .border_color(Self::preview_bg(cx))
+                                    .size(px(22.))
+                                    .into_any_element(),
+                            )
+                            .child(
+                                Avatar::new(AVATAR_URL)
+                                    .border_color(Self::preview_bg(cx))
+                                    .size(px(22.))
+                                    .into_any_element(),
+                            )
+                            .child(
+                                Avatar::new(AVATAR_URL)
+                                    .border_color(Self::preview_bg(cx))
+                                    .size(px(22.))
+                                    .into_any_element(),
+                            ),
                     ),
             )
     }
@@ -137,9 +139,14 @@ impl ThemePreview {
     fn render_buttons(&self, layer: ElevationIndex, cx: &ViewContext<Self>) -> impl IntoElement {
         v_flex()
             .gap_1()
-            .child(Headline::new("Buttons").size(HeadlineSize::Small))
+            .child(
+                Headline::new("Buttons")
+                    .size(HeadlineSize::Small)
+                    .color(Color::Muted),
+            )
             .child(
                 h_flex()
+                    .items_start()
                     .gap_px()
                     .child(
                         IconButton::new("icon_button_transparent", IconName::Check)
@@ -206,11 +213,7 @@ impl ThemePreview {
             )
     }
 
-    fn render_theme_layer(
-        &self,
-        layer: ElevationIndex,
-        cx: &ViewContext<Self>,
-    ) -> impl IntoElement {
+    fn render_text(&self, layer: ElevationIndex, cx: &ViewContext<Self>) -> impl IntoElement {
         let bg = layer.bg(cx);
 
         let label_with_contrast = |label: &str, fg: Hsla| {
@@ -218,104 +221,222 @@ impl ThemePreview {
             format!("{} ({:.2})", label, contrast)
         };
 
-        let all_colors = all_theme_colors(cx);
-
         v_flex()
+            .gap_1()
+            .child(Headline::new("Text").size(HeadlineSize::Small).color(Color::Muted))
+            .child(
+                h_flex()
+                    .items_start()
+                    .gap_4()
+                    .child(
+                        v_flex()
+                            .gap_1()
+                            .child(Headline::new("Headline Sizes").size(HeadlineSize::Small).color(Color::Muted))
+                            .child(Headline::new("XLarge Headline").size(HeadlineSize::XLarge))
+                            .child(Headline::new("Large Headline").size(HeadlineSize::Large))
+                            .child(Headline::new("Medium Headline").size(HeadlineSize::Medium))
+                            .child(Headline::new("Small Headline").size(HeadlineSize::Small))
+                            .child(Headline::new("XSmall Headline").size(HeadlineSize::XSmall)),
+                    )
+                    .child(
+                        v_flex()
+                            .gap_1()
+                            .child(Headline::new("Text Colors").size(HeadlineSize::Small).color(Color::Muted))
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Default Text",
+                                    Color::Default.color(cx),
+                                ))
+                                .color(Color::Default),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Accent Text",
+                                    Color::Accent.color(cx),
+                                ))
+                                .color(Color::Accent),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Conflict Text",
+                                    Color::Conflict.color(cx),
+                                ))
+                                .color(Color::Conflict),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Created Text",
+                                    Color::Created.color(cx),
+                                ))
+                                .color(Color::Created),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Deleted Text",
+                                    Color::Deleted.color(cx),
+                                ))
+                                .color(Color::Deleted),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Disabled Text",
+                                    Color::Disabled.color(cx),
+                                ))
+                                .color(Color::Disabled),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Error Text",
+                                    Color::Error.color(cx),
+                                ))
+                                .color(Color::Error),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Hidden Text",
+                                    Color::Hidden.color(cx),
+                                ))
+                                .color(Color::Hidden),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Hint Text",
+                                    Color::Hint.color(cx),
+                                ))
+                                .color(Color::Hint),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Ignored Text",
+                                    Color::Ignored.color(cx),
+                                ))
+                                .color(Color::Ignored),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Info Text",
+                                    Color::Info.color(cx),
+                                ))
+                                .color(Color::Info),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Modified Text",
+                                    Color::Modified.color(cx),
+                                ))
+                                .color(Color::Modified),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Muted Text",
+                                    Color::Muted.color(cx),
+                                ))
+                                .color(Color::Muted),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Placeholder Text",
+                                    Color::Placeholder.color(cx),
+                                ))
+                                .color(Color::Placeholder),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Selected Text",
+                                    Color::Selected.color(cx),
+                                ))
+                                .color(Color::Selected),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Success Text",
+                                    Color::Success.color(cx),
+                                ))
+                                .color(Color::Success),
+                            )
+                            .child(
+                                Label::new(label_with_contrast(
+                                    "Warning Text",
+                                    Color::Warning.color(cx),
+                                ))
+                                .color(Color::Warning),
+                            )
+                    )
+                    .child(
+                        v_flex()
+                            .gap_1()
+                            .child(Headline::new("Wrapping Text").size(HeadlineSize::Small).color(Color::Muted))
+                            .child(
+                                div().max_w(px(200.)).child(
+                                "This is a longer piece of text that should wrap to multiple lines. It demonstrates how text behaves when it exceeds the width of its container."
+                            ))
+                    )
+            )
+    }
+
+    fn render_theme_layer(
+        &self,
+        layer: ElevationIndex,
+        cx: &ViewContext<Self>,
+    ) -> impl IntoElement {
+        v_flex()
+            .p_4()
+            .bg(layer.bg(cx))
             .text_color(cx.theme().colors().text)
             .gap_2()
             .child(Headline::new(layer.clone().to_string()).size(HeadlineSize::Medium))
             .child(self.render_avatars(cx))
             .child(self.render_buttons(layer, cx))
-            .child(
-                v_flex()
-                    .w_full()
-                    .gap_px()
-                    .children(all_colors.into_iter().map(|(color, name)| {
-                        let fg = color;
-                        let contrast = (calculate_contrast_ratio(fg, bg) * 100.0).round() / 100.0;
+            .child(self.render_text(layer, cx))
+        // .child(
+        //     v_flex()
+        //         .w_full()
+        //         .gap_px()
+        //         .children(all_colors.into_iter().map(|(color, name)| {
+        //             let fg = color;
+        //             let contrast = (calculate_contrast_ratio(fg, bg) * 100.0).round() / 100.0;
 
-                        h_flex()
-                            .gap_2()
-                            .border_b_1()
-                            .text_xs()
-                            .border_color(cx.theme().colors().border)
-                            .py_1()
-                            .child(
-                                div()
-                                    .flex_none()
-                                    .size_8()
-                                    .bg(fg)
-                                    .border_1()
-                                    .border_color(cx.theme().colors().border)
-                                    .overflow_hidden(),
-                            )
-                            .child(
-                                div()
-                                    .w_64()
-                                    .flex_none()
-                                    .overflow_hidden()
-                                    .truncate()
-                                    .child(name),
-                            )
-                            .child(
-                                div()
-                                    .w_48()
-                                    .flex_none()
-                                    .overflow_hidden()
-                                    .truncate()
-                                    .child(fg.to_string()),
-                            )
-                            .child(
-                                div()
-                                    .w_16()
-                                    .flex_none()
-                                    .overflow_hidden()
-                                    .truncate()
-                                    .child(contrast.to_string()),
-                            )
-                    })),
-            )
-            .child(
-                v_flex()
-                    .bg(layer.bg(cx))
-                    .border_1()
-                    .border_color(cx.theme().colors().border)
-                    .p_2()
-                    .child(
-                        h_flex().items_start().child(
-                            v_flex()
-                                .border_1()
-                                .border_color(cx.theme().colors().border)
-                                .p_2()
-                                .child(
-                                    Headline::new("Text")
-                                        .size(HeadlineSize::Small)
-                                        .color(Color::Muted),
-                                )
-                                .child(
-                                    Label::new(label_with_contrast(
-                                        "Default Text",
-                                        Color::Default.color(cx),
-                                    ))
-                                    .color(Color::Default),
-                                )
-                                .child(
-                                    Label::new(label_with_contrast(
-                                        "Muted Text",
-                                        Color::Muted.color(cx),
-                                    ))
-                                    .color(Color::Muted),
-                                )
-                                .child(
-                                    Label::new(label_with_contrast(
-                                        "Placeholder Text",
-                                        Color::Placeholder.color(cx),
-                                    ))
-                                    .color(Color::Placeholder),
-                                ),
-                        ),
-                    ),
-            )
+        //             h_flex()
+        //                 .gap_2()
+        //                 .border_b_1()
+        //                 .text_xs()
+        //                 .border_color(cx.theme().colors().border)
+        //                 .py_1()
+        //                 .child(
+        //                     div()
+        //                         .flex_none()
+        //                         .size_8()
+        //                         .bg(fg)
+        //                         .border_1()
+        //                         .border_color(cx.theme().colors().border)
+        //                         .overflow_hidden(),
+        //                 )
+        //                 .child(
+        //                     div()
+        //                         .w_64()
+        //                         .flex_none()
+        //                         .overflow_hidden()
+        //                         .truncate()
+        //                         .child(name),
+        //                 )
+        //                 .child(
+        //                     div()
+        //                         .w_48()
+        //                         .flex_none()
+        //                         .overflow_hidden()
+        //                         .truncate()
+        //                         .child(fg.to_string()),
+        //                 )
+        //                 .child(
+        //                     div()
+        //                         .w_16()
+        //                         .flex_none()
+        //                         .overflow_hidden()
+        //                         .truncate()
+        //                         .child(contrast.to_string()),
+        //                 )
+        //         })),
+        // )
     }
 }
 
@@ -333,6 +454,7 @@ impl Render for ThemePreview {
             .gap_4()
             .child(self.render_theme_layer(ElevationIndex::Background, cx))
             .child(self.render_theme_layer(ElevationIndex::Surface, cx))
+            .child(self.render_theme_layer(ElevationIndex::EditorSurface, cx))
             .child(self.render_theme_layer(ElevationIndex::ElevatedSurface, cx))
     }
 }
