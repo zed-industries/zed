38868cb5ecc3468af6c3c2caae6e0bd4ed07b689
xdBronch <51252236+xdBronch@users.noreply.github.com>
2024-10-30 23:56:52 -0400

    new zig grammar

--- extensions/zig/languages/zig/outline.scm
+++ extensions/zig/languages/zig/outline.scm
@@ -1,27 +1,26 @@
-(Decl (
-    FnProto(
-        "fn" @context
-        function: (_) @name
-    )
-)
- ) @item
+(test_declaration "test" @context
+  [
+    (string)
+    (identifier)
+  ] @name) @item
 
-(
-    Decl (
-        VarDecl (
-                "const"
-                variable_type_function: (_) @name
-                (ErrorUnionExpr) @context
-            )
-    )
-) @item
 
-(
-    TestDecl (
-        "test" @context
-        [
-         (STRINGLITERALSINGLE)
-         (IDENTIFIER)
-        ]? @name
-    )
-) @item
+(function_declaration 
+  "pub"? @context
+  [
+    "extern"
+    "export"
+    "inline"
+    "noinline"
+  ]? @context
+  "fn" @context
+  name: (_) @name) @item
+
+(variable_declaration
+  "pub"? @context
+  [
+    "extern"
+    "export"
+  ]? @context
+  ["const" "var"] @context
+  (identifier) @name) @item
