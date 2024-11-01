a296553df6155c1ec79c098419da68c41c4f1d0d
Kirill Bulatov <kirill@zed.dev>
2024-11-01 18:21:11 +0200

    Properly exclude Windows

--- crates/live_kit_client/src/live_kit_client.rs
+++ crates/live_kit_client/src/live_kit_client.rs
@@ -21,9 +21,9 @@ use webrtc::{
     video_stream::native::NativeVideoStream,
 };
 
-#[cfg(not(any(test, feature = "test-support", target_os = windows)))]
+#[cfg(all(not(any(test, feature = "test-support")), not(target_os = "windows")))]
 pub use livekit::*;
-#[cfg(any(test, feature = "test-support", target_os = windows))]
+#[cfg(any(test, feature = "test-support", target_os = "windows"))]
 pub use test::*;
 
 pub use remote_video_track_view::{RemoteVideoTrackView, RemoteVideoTrackViewEvent};
