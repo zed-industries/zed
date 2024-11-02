9583c8ee1453163c940b95e0faa26634315493fc
mantou <709922234@qq.com>
2024-11-03 01:15:13 +0800

    Support inline html/css highlight

--- crates/languages/src/javascript/injections.scm
+++ crates/languages/src/javascript/injections.scm
@@ -58,3 +58,19 @@
   arguments: (arguments (template_string (string_fragment) @content
                               (#set! "language" "graphql")))
 )
+
+(call_expression
+  function: (identifier) @_name (#match? @_name "^styled(\\.\\w+)?$")
+  arguments: (template_string (string_fragment) @content
+                              (#set! "language" "css"))
+)
+
+((comment) @_css_comment
+  (#match? @_css_comment "/[*]+\\s*(css|style)\\s*[*]+/")
+  (template_string (string_fragment) @content
+    (#set! "language" "css")))
+
+((comment) @_html_comment
+  (#match? @_html_comment "/[*]+\\s*html\\s*[*]+/")
+  (template_string (string_fragment) @content
+    (#set! "language" "html")))
