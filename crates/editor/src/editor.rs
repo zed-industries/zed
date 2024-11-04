d67bb7934f4dbf0258c45e4b4deb4cd943b00a21
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-11-04 22:23:46 +0100

    lsp: use task-based approach for pulling diagnostics
    lsp: use task-based approach for pulling diagnostics

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -190,6 +190,8 @@ const MAX_SELECTION_HISTORY_LEN: usize = 1024;
 pub(crate) const CURSORS_VISIBLE_FOR: Duration = Duration::from_millis(2000);
 #[doc(hidden)]
 pub const CODE_ACTIONS_DEBOUNCE_TIMEOUT: Duration = Duration::from_millis(250);
+#[doc(hidden)]
+pub const DOCUMENT_DIAGNOSTICS_DEBOUNCE_TIMEOUT: Duration = Duration::from_millis(250);
 
 pub(crate) const FORMAT_TIMEOUT: Duration = Duration::from_secs(2);
 pub(crate) const SCROLL_CENTER_TOP_BOTTOM_DEBOUNCE_TIMEOUT: Duration = Duration::from_secs(1);
@@ -696,6 +698,7 @@ pub struct Editor {
     registered_buffers: HashMap<BufferId, OpenLspBufferHandle>,
     toggle_fold_multiple_buffers: Task<()>,
     _scroll_cursor_center_top_bottom_task: Task<()>,
+    _pull_document_diagnostics_task: Task<()>,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Default)]
@@ -1355,6 +1358,7 @@ impl Editor {
             _scroll_cursor_center_top_bottom_task: Task::ready(()),
             toggle_fold_multiple_buffers: Task::ready(()),
             text_style_refinement: None,
+            _pull_document_diagnostics_task: Task::ready(()),
         };
         this.tasks_update_task = Some(this.refresh_runnables(cx));
         this._subscriptions.extend(project_subscriptions);
@@ -10481,6 +10485,30 @@ impl Editor {
         }
     }
 
+    fn refresh_diagnostics(&mut self, cx: &mut ViewContext<Self>) -> Option<()> {
+        let project = self.project.clone()?;
+        let buffer = self.buffer.read(cx);
+        let newest_selection = self.selections.newest_anchor().clone();
+        let (start_buffer, start) = buffer.text_anchor_for_position(newest_selection.start, cx)?;
+        let (end_buffer, _) = buffer.text_anchor_for_position(newest_selection.end, cx)?;
+        if start_buffer != end_buffer {
+            return None;
+        }
+
+        self._pull_document_diagnostics_task = cx.spawn(|editor, mut cx| async move {
+            cx.background_executor()
+                .timer(DOCUMENT_DIAGNOSTICS_DEBOUNCE_TIMEOUT)
+                .await;
+
+            editor
+                .update(&mut cx, |_, cx| {
+                    project.diagnostics(&start_buffer, start, cx);
+                })
+                .ok();
+        });
+        None
+    }
+
     pub fn set_selections_from_remote(
         &mut self,
         selections: Vec<Selection<Anchor>>,
@@ -12314,7 +12342,9 @@ impl Editor {
             } => {
                 self.scrollbar_marker_state.dirty = true;
                 self.active_indent_guides_state.dirty = true;
+                self.refresh_diagnostics(cx);
                 self.refresh_active_diagnostics(cx);
+
                 self.refresh_code_actions(cx);
                 if self.has_active_inline_completion() {
                     self.update_visible_inline_completion(cx);
@@ -13625,6 +13655,15 @@ pub trait CodeActionProvider {
     ) -> Task<Result<ProjectTransaction>>;
 }
 
+pub trait DiagnosticsProvider {
+    fn diagnostics(
+        &self,
+        buffer: &Model<Buffer>,
+        position: text::Anchor,
+        cx: &mut WindowContext,
+    ) -> Option<Task<Result<Vec<lsp::Diagnostic>>>>;
+}
+
 impl CodeActionProvider for Model<Project> {
     fn id(&self) -> Arc<str> {
         "project".into()
@@ -13992,6 +14031,19 @@ impl SemanticsProvider for Model<Project> {
     ) -> Option<Task<Result<ProjectTransaction>>> {
         Some(self.update(cx, |project, cx| {
             project.perform_rename(buffer.clone(), position, new_name, cx)
+        }))
+    }
+}
+
+impl DiagnosticsProvider for Model<Project> {
+    fn diagnostics(
+        &self,
+        buffer: &Model<Buffer>,
+        position: text::Anchor,
+        cx: &mut WindowContext,
+    ) -> Option<Task<Result<Vec<lsp::Diagnostic>>>> {
+        Some(self.update(cx, |project, cx| {
+            project.document_diagnostics(buffer, position, cx)
         }))
     }
 }
