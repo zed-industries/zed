ffaaadf2b98458f29a030c9d3fcc95f5ce9a3fb5
Anthony Eid <hello@anthonyeid.me>
2024-11-08 03:03:10 -0500

    Get lldb-dap working on linux if already installed by user

--- crates/dap_adapters/src/lldb.rs
+++ crates/dap_adapters/src/lldb.rs
@@ -1,3 +1,5 @@
+use std::ffi::OsStr;
+
 use anyhow::Result;
 use async_trait::async_trait;
 use dap::transport::{StdioTransport, Transport};
@@ -25,17 +27,36 @@ impl DebugAdapter for LldbDebugAdapter {
         Box::new(StdioTransport::new())
     }
 
-    #[cfg(target_os = "macos")]
     async fn get_binary(
         &self,
-        _: &dyn DapDelegate,
+        delegate: &dyn DapDelegate,
         config: &DebugAdapterConfig,
-        _: Option<PathBuf>,
+        user_installed_path: Option<PathBuf>,
     ) -> Result<DebugAdapterBinary> {
-        let output = std::process::Command::new("xcrun")
-            .args(&["-f", "lldb-dap"])
-            .output()?;
-        let lldb_dap_path = String::from_utf8(output.stdout)?.trim().to_string();
+        let user_setting_path = user_installed_path
+            .filter(|p| p.exists())
+            .and_then(|p| p.to_str().map(|s| s.to_string()));
+
+        let lldb_dap_path = if cfg!(target_os = "macos") {
+            std::process::Command::new("xcrun")
+                .args(&["-f", "lldb-dap"])
+                .output()
+                .ok()
+                .and_then(|output| String::from_utf8(output.stdout).ok())
+                .map(|path| path.trim().to_string())
+                .ok_or(anyhow!("Failed to find lldb-dap in user's path"))
+        } else {
+            delegate
+                .which(OsStr::new("lldb-dap"))
+                .and_then(|p| p.to_str().map(|s| s.to_string()))
+                .ok_or(anyhow!("Could not find lldb-dap in path"))
+        };
+
+        if lldb_dap_path.is_err() && user_setting_path.is_none() {
+            bail!("Could not find lldb-dap path or it's not installed");
+        }
+
+        let lldb_dap_path = user_setting_path.unwrap_or(lldb_dap_path?);
 
         Ok(DebugAdapterBinary {
             command: lldb_dap_path,
@@ -46,28 +67,16 @@ impl DebugAdapter for LldbDebugAdapter {
         })
     }
 
-    #[cfg(not(target_os = "macos"))]
-    async fn get_binary(
-        &self,
-        _: &dyn DapDelegate,
-        _: &DebugAdapterConfig,
-        _: Option<PathBuf>,
-    ) -> Result<DebugAdapterBinary> {
-        Err(anyhow::anyhow!(
-            "LLDB-DAP is only supported on macOS (Right now)"
-        ))
-    }
-
     async fn install_binary(
         &self,
         _version: AdapterVersion,
         _delegate: &dyn DapDelegate,
     ) -> Result<()> {
-        bail!("LLDB debug adapter cannot be installed")
+        unimplemented!("LLDB debug adapter cannot be installed by Zed (yet)")
     }
 
     async fn fetch_latest_adapter_version(&self, _: &dyn DapDelegate) -> Result<AdapterVersion> {
-        bail!("Fetch latest adapter version not implemented for lldb (yet)")
+        unimplemented!("Fetch latest adapter version not implemented for lldb (yet)")
     }
 
     async fn get_installed_binary(
@@ -76,7 +85,7 @@ impl DebugAdapter for LldbDebugAdapter {
         _: &DebugAdapterConfig,
         _: Option<PathBuf>,
     ) -> Result<DebugAdapterBinary> {
-        bail!("LLDB debug adapter cannot be installed")
+        unimplemented!("LLDB debug adapter cannot be installed by Zed (yet)")
     }
 
     fn request_args(&self, config: &DebugAdapterConfig) -> Value {
