e002222b5e8d350c781e8d3036bd601c6515a2b0
Thorsten Ball <mrnugget@gmail.com>
2024-11-09 17:57:54 +0100

    rust: Add tucan injections to Rust

--- crates/languages/src/rust/injections.scm
+++ crates/languages/src/rust/injections.scm
@@ -5,3 +5,14 @@
 (macro_rule
   (token_tree) @content
   (#set! "language" "rust"))
+
+
+;; Inject the tucan grammar into Rust strings that are bound to `let tucan =`
+((let_declaration
+   pattern: (identifier) @_new
+   (#eq? @_new "tucan")
+   value: [(raw_string_literal
+             (string_content) @content)
+           (string_literal
+             (string_content) @content)])
+ (#set! "language" "tucan"))
