77ac0b2255609805859fa496f73a975c019698e8
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-11-09 21:54:43 +0100

    lsp: add struct for LSP pull-based diagnostics

--- crates/project/src/lsp_store.rs
+++ crates/project/src/lsp_store.rs
@@ -10,8 +10,9 @@ use crate::{
     toolchain_store::{EmptyToolchainStore, ToolchainStoreEvent},
     worktree_store::{WorktreeStore, WorktreeStoreEvent},
     yarn::YarnPathStore,
-    CodeAction, Completion, CoreCompletion, Hover, InlayHint, ProjectItem as _, ProjectPath,
-    ProjectTransaction, ResolveState, Symbol, ToolchainStore,
+    CodeAction, Completion, CoreCompletion, Hover, InlayHint, ProjectItem as _,
+    LspDiagnostics, ProjectPath, ProjectTransaction, ResolveState, Symbol,
+    ToolchainStore,
 };
 use anyhow::{anyhow, Context as _, Result};
 use async_trait::async_trait;
@@ -4533,7 +4534,7 @@ impl LspStore {
         buffer_handle: &Model<Buffer>,
         position: Anchor,
         cx: &mut ModelContext<Self>,
-    ) -> Task<Result<Vec<lsp::Diagnostic>>> {
+    ) -> Task<Result<Vec<LspDiagnostics>>> {
         let buffer = buffer_handle.read(cx);
         let buffer_id = buffer.remote_id();
 
@@ -4583,7 +4584,6 @@ impl LspStore {
                     .into_iter()
                     .collect::<Result<Vec<_>>>()?
                     .into_iter()
-                    .flat_map(|diagnostics| diagnostics.into_iter().flatten())
                     .collect())
             })
         } else {
@@ -4593,13 +4593,7 @@ impl LspStore {
                 GetDocumentDiagnostics { position },
                 cx,
             );
-            cx.spawn(|_, _| async move {
-                Ok(all_actions_task
-                    .await
-                    .into_iter()
-                    .flat_map(|diagnostics| diagnostics.into_iter().flatten())
-                    .collect())
-            })
+            cx.spawn(|_, _| async move { Ok(all_actions_task.await.into_iter().collect()) })
         }
     }
 
