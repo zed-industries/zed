a619bc6f95e3a765bbb308d0a904bc88601936bc
Bennet Bo Fenner <bennet@zed.dev>
2024-11-12 00:26:10 +0100

    adjust script

--- script/move-crate.py
+++ script/move-crate.py
@@ -4,13 +4,11 @@ import shutil
 
 crate_name = sys.argv[1]
 
-src_path = f"{crate_name}/src"
-dest_path = f"zed_common/{crate_name}"
+src_path = f"crates/{crate_name}/src"
+dest_path = f"crates/zed_common/{crate_name}/src"
 
 # Move the crate to zed_common
 if os.path.exists(src_path):
-    if not os.path.exists("zed_common"):
-        os.makedirs("zed_common")
     shutil.move(src_path, dest_path)
     print(f"Moved code to zed_common")
 else:
@@ -32,7 +30,7 @@ for root, dirs, files in os.walk(dest_path):
         if file.endswith(".rs"):
             with open(os.path.join(root, file), "r") as f:
                 content = f.read()
-                new_content = content.replace(f"use crate::", f"use crate::{crate_name}::")
+                new_content = content.replace(f"crate::", f"crate::{crate_name}::")
                 if new_content != content:
                     with open(os.path.join(root, file), "w") as f:
                         f.write(new_content)
@@ -40,13 +38,13 @@ for root, dirs, files in os.walk(dest_path):
 print("Fixed imports")
 
 # Remove crate from workspace file
-with open("../Cargo.toml", "r") as f:
+with open("Cargo.toml", "r") as f:
     lines = f.readlines()
 
 new_lines = [line for line in lines if not (f"crates/{crate_name}" in line or f"{crate_name}.workspace = true" in line)]
 
 if len(new_lines) != len(lines):
-    with open("../Cargo.toml", "w") as f:
+    with open("Cargo.toml", "w") as f:
         f.writelines(new_lines)
     print(f"Removed {crate_name} from workspace file")
 else:
