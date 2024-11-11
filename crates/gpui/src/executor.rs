0d01cc2dd2c55a13663124f2f1c4ec4f8641da0a
Thorsten Ball <mrnugget@gmail.com>
2024-11-11 08:56:13 +0100

    Make it only crate-pub

--- crates/gpui/src/executor.rs
+++ crates/gpui/src/executor.rs
@@ -301,7 +301,7 @@ impl BackgroundExecutor {
 
     /// Block the current thread until the given future resolves
     /// or `duration` has elapsed.
-    pub fn block_to_shutdown<R>(
+    pub(crate) fn block_to_shutdown<R>(
         &self,
         duration: Duration,
         future: impl Future<Output = R>,
