9923cd30e817cca4e5f2286f9c06547969846e7c
Conrad Irwin <conrad.irwin@gmail.com>
2024-11-21 05:04:31 +0000

    Clip UTF-16 offsets in text for range (#20968)
    When launching the Pinyin keyboard, macOS will sometimes try to peek one
    character back in the string.
    
    This caused a panic if the preceding character was an emoji. The docs
    say
    "don't assume the range is valid", so now we don't.
    
    Release Notes:
    
    - (macOS) Fixed a panic when using the Pinyin keyboard with emojis

--- crates/terminal_view/src/terminal_element.rs
+++ crates/terminal_view/src/terminal_element.rs
@@ -1001,6 +1001,7 @@ impl InputHandler for TerminalInputHandler {
     fn text_for_range(
         &mut self,
         _: std::ops::Range<usize>,
+        _: &mut Option<std::ops::Range<usize>>,
         _: &mut WindowContext,
     ) -> Option<String> {
         None
