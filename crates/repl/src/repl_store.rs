3f76f5a047e3abd8ff810a45333368b3c071ab74
Kyle Kelley <rgbkrk@gmail.com>
2024-11-20 22:36:01 -0800

    detect if `JUPYTER_SERVER` and `JUPYTER_TOKEN` are set

--- crates/repl/src/repl_store.rs
+++ crates/repl/src/repl_store.rs
@@ -144,35 +144,50 @@ impl ReplStore {
         })
     }
 
+    fn get_remote_kernel_specifications(
+        &self,
+        cx: &mut ModelContext<Self>,
+    ) -> Option<Task<Result<Vec<KernelSpecification>>>> {
+        match (
+            std::env::var("JUPYTER_SERVER"),
+            std::env::var("JUPYTER_TOKEN"),
+        ) {
+            (Ok(server), Ok(token)) => {
+                let remote_server = RemoteServer {
+                    base_url: server,
+                    token,
+                };
+                let http_client = cx.http_client();
+                Some(cx.spawn(|_, _| async move {
+                    list_remote_kernelspecs(remote_server, http_client)
+                        .await
+                        .map(|specs| specs.into_iter().map(KernelSpecification::Remote).collect())
+                }))
+            }
+            _ => None,
+        }
+    }
+
     pub fn refresh_kernelspecs(&mut self, cx: &mut ModelContext<Self>) -> Task<Result<()>> {
         let local_kernel_specifications = local_kernel_specifications(self.fs.clone());
 
-        // todo: iterate over all remote servers user has configured
-        let remote_server = RemoteServer {
-            base_url: std::env::var("JUPYTER_SERVER").expect("JUPYTER_TOKEN not set"),
-            token: std::env::var("JUPYTER_TOKEN").expect("JUPYTER_TOKEN not set"),
-        };
-
-        let http_client = cx.http_client();
-
-        let remote_kernel_specifications =
-            list_remote_kernelspecs(remote_server, http_client.clone());
+        let remote_kernel_specifications = self.get_remote_kernel_specifications(cx);
 
         cx.spawn(|this, mut cx| async move {
-            let local_kernel_specifications = local_kernel_specifications
+            let mut all_specs = local_kernel_specifications
                 .await?
                 .into_iter()
-                .map(KernelSpecification::Jupyter);
+                .map(KernelSpecification::Jupyter)
+                .collect::<Vec<_>>();
 
-            let remote_kernel_specifications = remote_kernel_specifications
-                .await?
-                .into_iter()
-                .map(KernelSpecification::Remote);
+            if let Some(remote_task) = remote_kernel_specifications {
+                if let Ok(remote_specs) = remote_task.await {
+                    all_specs.extend(remote_specs);
+                }
+            }
 
             this.update(&mut cx, |this, cx| {
-                this.kernel_specifications = local_kernel_specifications
-                    .chain(remote_kernel_specifications)
-                    .collect();
+                this.kernel_specifications = all_specs;
                 cx.notify();
             })
         })
