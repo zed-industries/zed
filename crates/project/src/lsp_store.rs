d5f2bca382e5b5d653f917ec9d52fdb028acee2b
Techatrix <techatrix@mailbox.org>
2024-11-22 13:01:00 +0100

    Filter LSP code actions based on the requested kinds (#20847)
    I've observed that Zed's implementation of [Code Actions On
    Format](https://zed.dev/docs/configuring-zed#code-actions-on-format)
    uses the
    [CodeActionContext.only](https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#codeActionContext)
    parameter to request specific code action kinds from the server. The
    issue is that it does not filter out code actions from the response,
    believing that the server will do it.
    
    The [LSP
    specification](https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#codeActionContext)
    says that the client is responsible for filtering out unwanted code
    actions:
    
    ```js
    /**
    * Requested kind of actions to return.
    *
    * Actions not of this kind are filtered out by the client before being
    * shown. So servers can omit computing them.
    */
    only?: CodeActionKind[];
    ```
    
    This PR will filter out unwanted code action on the client side.
    
    I have initially encountered this issue because the [ZLS language
    server](https://github.com/zigtools/zls) (until
    https://github.com/zigtools/zls/pull/2087) does not filter code action
    based on `CodeActionContext.only` so Zed runs all received code actions
    even if they are explicitly disabled in the `code_actions_on_format`
    setting.
    
    Release Notes:
    
    - Fix the `code_actions_on_format` setting when used with a language
    server like ZLS
    
    ---------
    
    Co-authored-by: Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>

--- crates/project/src/lsp_store.rs
+++ crates/project/src/lsp_store.rs
@@ -2015,6 +2015,7 @@ impl LspStore {
         &mut self,
         buffer_handle: &Model<Buffer>,
         range: Range<Anchor>,
+        kinds: Option<Vec<CodeActionKind>>,
         cx: &mut ModelContext<Self>,
     ) -> Task<Result<Vec<CodeAction>>> {
         if let Some((upstream_client, project_id)) = self.upstream_client() {
@@ -2028,7 +2029,7 @@ impl LspStore {
                 request: Some(proto::multi_lsp_query::Request::GetCodeActions(
                     GetCodeActions {
                         range: range.clone(),
-                        kinds: None,
+                        kinds: kinds.clone(),
                     }
                     .to_proto(project_id, buffer_handle.read(cx)),
                 )),
@@ -2054,7 +2055,7 @@ impl LspStore {
                         .map(|code_actions_response| {
                             GetCodeActions {
                                 range: range.clone(),
-                                kinds: None,
+                                kinds: kinds.clone(),
                             }
                             .response_from_proto(
                                 code_actions_response,
@@ -2079,7 +2080,7 @@ impl LspStore {
                 Some(range.start),
                 GetCodeActions {
                     range: range.clone(),
-                    kinds: None,
+                    kinds: kinds.clone(),
                 },
                 cx,
             );
