9adc3b4e82f1c3e825c597bedeb0b184876dcb78
Marshall Bowers <elliott.codes@gmail.com>
2024-11-23 11:24:52 -0500

    Break ground on `assistant2` (#21109)
    This PR breaks ground on a new `assistant2` crate.
    
    In order to see this new version of the assistant, both of the following
    must be true:
    1. The `assistant2` feature flag is enabled for your user
       - It is **not** currently enabled for all staff.
    2. You are running a development build of Zed
    
    The intent here is to enable the folks working on `assistant2` to
    incrementally land work onto `main` without breaking use of the current
    Assistant for anyone.
    
    <img width="1136" alt="Screenshot 2024-11-23 at 10 46 08 AM"
    src="https://github.com/user-attachments/assets/5723a13f-5be1-4486-9460-ead7329ba78e">
    
    Release Notes:
    
    - N/A

---
+++ crates/assistant2/src/assistant.rs
@@ -0,0 +1,40 @@
+mod assistant_panel;
+
+use command_palette_hooks::CommandPaletteFilter;
+use feature_flags::{Assistant2FeatureFlag, FeatureFlagAppExt};
+use gpui::{actions, AppContext};
+
+pub use crate::assistant_panel::AssistantPanel;
+
+actions!(assistant2, [ToggleFocus, NewChat]);
+
+const NAMESPACE: &str = "assistant2";
+
+/// Initializes the `assistant2` crate.
+pub fn init(cx: &mut AppContext) {
+    assistant_panel::init(cx);
+    feature_gate_assistant2_actions(cx);
+}
+
+fn feature_gate_assistant2_actions(cx: &mut AppContext) {
+    const ASSISTANT1_NAMESPACE: &str = "assistant";
+
+    CommandPaletteFilter::update_global(cx, |filter, _cx| {
+        filter.hide_namespace(NAMESPACE);
+    });
+
+    cx.observe_flag::<Assistant2FeatureFlag, _>(move |is_enabled, cx| {
+        if is_enabled {
+            CommandPaletteFilter::update_global(cx, |filter, _cx| {
+                filter.show_namespace(NAMESPACE);
+                filter.hide_namespace(ASSISTANT1_NAMESPACE);
+            });
+        } else {
+            CommandPaletteFilter::update_global(cx, |filter, _cx| {
+                filter.hide_namespace(NAMESPACE);
+                filter.show_namespace(ASSISTANT1_NAMESPACE);
+            });
+        }
+    })
+    .detach();
+}
