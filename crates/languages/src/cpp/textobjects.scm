43885fe77b1edf0389496af965c411d979f53479
Harrison Rouillard <harrisonrouillard@gmail.com>
2024-12-02 22:54:42 +1100

    C++ should include C

--- crates/languages/src/cpp/textobjects.scm
+++ crates/languages/src/cpp/textobjects.scm
@@ -1,4 +1,31 @@
 ; inherits: c
+(function_definition
+  body: (_) @function.inside) @function.around
+
+(struct_specifier
+  body: (_) @class.inside) @class.around
+
+(enum_specifier
+  body: (_) @class.inside) @class.around
+
+(union_specifier
+  body: (_) @class.inside) @class.around
+
+(parameter_list
+  ((_) @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(argument_list
+  ((_) @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(comment) @comment.inside
+
+(comment)+ @comment.around
+
+(enumerator
+  (_) @entry.inside) @entry.around
+
+(initializer_list
+  (_) @entry.around)
 
 (lambda_expression
   body: (_) @function.inside) @function.around
