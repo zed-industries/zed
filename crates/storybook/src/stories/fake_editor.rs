d907a698a2da04f5c37b15e2f83535e41d56ce46
Bennet Bo Fenner <bennet@zed.dev>
2024-12-05 19:15:32 +0100

    Use text style from editor and calculate line height correctly
    Co-Authored-by: Nate <nate@zed.dev>

--- crates/storybook/src/stories/fake_editor.rs
+++ crates/storybook/src/stories/fake_editor.rs
@@ -1,58 +1,91 @@
 use editor::*;
 use gpui::*;
-use language::Buffer;
-use settings::Settings;
-use theme::ThemeSettings;
 use ui::*;
 
-pub struct FakeEditor {
+const DEFAULT_LINE_HEIGHT: Pixels = Pixels(75.);
+
+pub struct EditorPrototype {
     editor: View<Editor>,
+    text_style: TextStyle,
     elements_above: Vec<(u32, Box<dyn Fn(Pixels, &mut WindowContext) -> AnyElement>)>,
 }
 
-// div().top(y_for_row(3)).into_element()
+impl EditorPrototype {
+    pub fn build(
+        cx: &mut WindowContext,
+        f: impl FnOnce(Self, &mut ViewContext<Self>) -> Self,
+    ) -> View<Self> {
+        let mut text_style = cx.text_style();
+        let refinement = TextStyleRefinement {
+            line_height: Some(DEFAULT_LINE_HEIGHT.into()),
+            background_color: Some(gpui::transparent_black()),
+            ..Default::default()
+        };
+        text_style.refine(&refinement);
 
-impl FakeEditor {
-    pub fn new(initial_text: &str, cx: &mut WindowContext) -> Self {
-        let editor = cx.new_view(|cx| {
-            let buffer = cx.new_model(|cx| Buffer::local(initial_text, cx));
-            let buffer = cx.new_model(|cx| MultiBuffer::singleton(buffer, cx));
-            Editor::new(EditorMode::Full, buffer, None, false, cx)
-        });
-        Self {
-            editor,
-            elements_above: Vec::new(),
-        }
+        cx.new_view(|cx| {
+            cx.refresh();
+            f(
+                Self {
+                    editor: cx.new_view(|cx| {
+                        let mut editor = Editor::multi_line(cx);
+                        editor.set_text_style_refinement(refinement);
+                        editor
+                    }),
+                    text_style,
+                    elements_above: Vec::new(),
+                },
+                cx,
+            )
+        })
+    }
+
+    pub fn line_height(mut self, line_height: Pixels, cx: &mut ViewContext<Self>) -> Self {
+        let refinement = TextStyleRefinement {
+            line_height: Some(line_height.into()),
+            ..Default::default()
+        };
+        self.text_style.refine(&refinement);
+        self.editor
+            .update(cx, |editor, _| editor.set_text_style_refinement(refinement));
+        self
+    }
+
+    pub fn text(self, initial_text: &str, cx: &mut ViewContext<Self>) -> Self {
+        self.editor
+            .update(cx, |editor, cx| editor.set_text(initial_text, cx));
+        self
     }
 
     fn element_above(
-        &mut self,
+        mut self,
         row: u32,
         element_fn: impl Fn(Pixels, &mut WindowContext) -> AnyElement + 'static,
-    ) {
+    ) -> Self {
         self.elements_above.push((row, Box::new(element_fn)));
-    }
-
-    fn line_height(&self, cx: &mut WindowContext) -> Option<Pixels> {
-        Some(
-            self.editor
-                .read(cx)
-                .style()?
-                .text
-                .line_height_in_pixels(cx.rem_size()),
-        )
+        self
     }
 }
 
-impl Render for FakeEditor {
+impl Render for EditorPrototype {
     fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
-        let line_height = self.line_height(cx).unwrap_or(px(12.));
+        let line_height = self
+            .text_style
+            .line_height
+            .to_pixels(self.text_style.font_size, cx.rem_size());
 
         div()
             .relative()
-            .flex_1()
+            .flex_shrink_0()
             .size_full()
-            .child(self.editor.clone())
+            .child(
+                div()
+                    .absolute()
+                    .top_0()
+                    .left_0()
+                    .size_full()
+                    .child(self.editor.clone()),
+            )
             .child(div().absolute().top_0().left_0().size_full().children(
                 self.elements_above.iter().map(|(row, element_fn)| {
                     div()
@@ -65,36 +98,64 @@ impl Render for FakeEditor {
 }
 
 pub struct FakeEditorStory {
-    fake_editor_1: View<FakeEditor>,
+    fake_editor_1: View<EditorPrototype>,
 }
 
 impl FakeEditorStory {
     pub fn view(cx: &mut WindowContext) -> View<Self> {
-        cx.new_view(|cx| {
-            let fake_editor_1 = cx.new_view(|cx| {
-                let editor_text = r###"fn main() {
+        let editor_text = r###"fn main() {
 
 .iter().sum();i32=numbers:sumlet
 4,5];,3,![1,2=vecnumberslet   println!("Sum of numbers: {}", sum);
 
-    let mut counter = 0;
-    while counter < 5 {
-        println!("Counter: {}", counter);
-        counter += 1;
-    }
+let mut counter = 0;
+while counter < 5 {
+println!("Counter: {}", counter);
+counter += 1;
+}
 
-    for i in 0..3 {
-        println!("Iteration: {}", i);
-    }
+for i in 0..3 {
+println!("Iteration: {}", i);
+}
 }"###;
 
-                let mut fake_editor = FakeEditor::new(editor_text, cx);
-                fake_editor.element_above(3, |line_height, _| {
-                    div().bg(hsla(0.5, 1.,1.,0.2)).w_16().h(line_height).into_any_element()
-                });
+        cx.new_view(|cx| {
+            let fake_editor_1 = EditorPrototype::build(cx, |fake_editor, cx| {
                 fake_editor
+                    .text(editor_text, cx)
+                    .element_above(0, |line_height, _| {
+                        div()
+                            .id("foo")
+                            .bg(gpui::yellow().opacity(0.7))
+                            .w_32()
+                            .h(line_height)
+                            .into_any_element()
+                    })
+                    .element_above(1, |line_height, _| {
+                        div()
+                            .id("foo")
+                            .bg(gpui::green().opacity(0.7))
+                            .w_32()
+                            .h(line_height)
+                            .into_any_element()
+                    })
+                    .element_above(2, |line_height, _| {
+                        div()
+                            .id("foo")
+                            .bg(gpui::red().opacity(0.7))
+                            .w_32()
+                            .h(line_height)
+                            .into_any_element()
+                    })
+                    .element_above(3, |line_height, _| {
+                        div()
+                            .id("foo")
+                            .bg(gpui::blue().opacity(0.7))
+                            .w_32()
+                            .h(line_height)
+                            .into_any_element()
+                    })
             });
-
             Self { fake_editor_1 }
         })
     }
