f770538ba39b7f5045abd5352bcdbe2d6f1f7e65
Nate Butler <iamnbutler@gmail.com>
2024-12-05 16:11:05 -0500

    Checkpoint

--- crates/storybook/src/stories/fake_editor.rs
+++ crates/storybook/src/stories/fake_editor.rs
@@ -10,6 +10,7 @@ pub struct EditorPrototype {
     editor: View<Editor>,
     text_style: TextStyle,
     elements_above: Vec<(u32, Box<dyn Fn(Pixels, &mut WindowContext) -> AnyElement>)>,
+    height: Length,
 }
 
 impl EditorPrototype {
@@ -29,17 +30,35 @@ impl EditorPrototype {
         };
         text_style.refine(&refinement);
 
+        let editor = cx.new_view(|cx| {
+            let mut editor = Editor::auto_height(20, cx);
+            editor.set_text_style_refinement(refinement);
+            editor.set_show_gutter(true, cx);
+            editor.set_show_line_numbers(true, cx);
+            editor
+        });
+
+        let mut height = Length::Auto;
+
+        editor.update(cx, |editor, cx| {
+            let line_count = editor.max_point(cx).row() + 1;
+            println!("line_count: {}", line_count.as_f32());
+            let line_height = line_height.0;
+            println!("line_height: {}", line_height);
+            height = px((line_count.as_f32() * line_height).round()).into();
+            println!("height: {:?}", height);
+        });
+
+        println!("final height: {:?}", height);
+
         cx.new_view(|cx| {
             cx.refresh();
             f(
                 Self {
-                    editor: cx.new_view(|cx| {
-                        let mut editor = Editor::auto_height(20, cx);
-                        editor.set_text_style_refinement(refinement);
-                        editor
-                    }),
+                    editor,
                     text_style,
                     elements_above: Vec::new(),
+                    height,
                 },
                 cx,
             )
@@ -52,9 +71,26 @@ impl EditorPrototype {
             .to_pixels(self.text_style.font_size, cx.rem_size())
     }
 
-    pub fn text(self, initial_text: &str, cx: &mut ViewContext<Self>) -> Self {
-        self.editor
-            .update(cx, |editor, cx| editor.set_text(initial_text, cx));
+    pub fn text(mut self, initial_text: &str, cx: &mut ViewContext<Self>) -> Self {
+        let mut height = self.height;
+        let line_height = self.line_height(cx);
+
+        self.editor.update(cx, |editor, cx| {
+            editor.set_text(initial_text, cx);
+            let line_count = editor.max_point(cx).row() + 1;
+            println!("line_count: {}", line_count.as_f32());
+            let line_height = line_height.0;
+            println!("line_height: {}", line_height);
+            height = px((line_count.as_f32() * line_height).round()).into();
+            println!("height: {:?}", height);
+        });
+
+        self.height = height;
+
+        println!("final height: {:?}", height);
+
+        cx.notify();
+
         self
     }
 
@@ -75,13 +111,16 @@ impl Render for EditorPrototype {
         div()
             .relative()
             .flex_shrink_0()
-            .size_full()
+            .w_full()
+            .h(self.height)
             .bg(cx.theme().colors().editor_background)
             .child(div().absolute().top_0().left_0().size_full().children(
                 self.elements_above.iter().map(|(row, element_fn)| {
                     div()
                         .absolute()
                         .top(px(*row as f32) * line_height)
+                        .left_0()
+                        .w_full()
                         .child(element_fn(line_height, cx))
                 }),
             ))
@@ -128,7 +167,7 @@ fn greet(name: &str) {
                 fake_editor
                     .text(editor_text, cx)
                     .element_below(3, |line_height, _| {
-                        let green_bg = hsla(142. / 360., 0.68, 0.45, 0.6);
+                        let green_bg = hsla(142. / 360., 0.68, 0.45, 0.06);
                         div()
                             .id("foo")
                             .bg(green_bg)
@@ -143,12 +182,17 @@ fn greet(name: &str) {
 }
 
 impl Render for FakeEditorStory {
-    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
-        v_flex()
-            .size_full()
-            .bg(white())
-            .text_sm()
-            .flex_1()
-            .child(self.fake_editor_1.clone())
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        v_flex().size_full().bg(white()).text_sm().flex_1().child(
+            div().size_full().p_4().child(
+                v_flex()
+                    .size_full()
+                    .rounded_md()
+                    .border_1()
+                    .border_color(cx.theme().colors().border)
+                    .bg(cx.theme().colors().background)
+                    .child(self.fake_editor_1.clone()),
+            ),
+        )
     }
 }
