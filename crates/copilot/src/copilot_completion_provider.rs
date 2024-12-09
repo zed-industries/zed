f9a7cb181d045099274b4cac68785e7f12eadee3
Thorsten Ball <mrnugget@gmail.com>
2024-12-09 12:46:46 +0100

    Focus editor in copilot test

--- crates/copilot/src/copilot_completion_provider.rs
+++ crates/copilot/src/copilot_completion_provider.rs
@@ -1003,6 +1003,7 @@ mod tests {
             multibuffer
         });
         let editor = cx.add_window(|cx| Editor::for_multibuffer(multibuffer, None, true, cx));
+        editor.update(cx, |editor, cx| editor.focus(cx)).unwrap();
         let copilot_provider = cx.new_model(|_| CopilotCompletionProvider::new(copilot));
         editor
             .update(cx, |editor, cx| {
