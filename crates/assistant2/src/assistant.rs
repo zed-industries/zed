c594ccb0af81e5c2fc717f824b3a969e0ac38188
Richard Feldman <oss@rtfeldman.com>
2024-12-11 14:32:30 -0500

    Inline assistant v2 (#21828)
    This is behind the Assistant v2 feature flag. As @maxdeviant and I
    discussed, the state is currently decoupled from the Assistant Panel's
    state, although in the future we plan to introduce a way to refer to
    conversations from the panel. Also, we're intentionally duplicating some
    code with the v2 panel right now; the plan is to do a future PR to make
    them share code more.
    
    
    https://github.com/user-attachments/assets/bb163bd3-a02d-4a91-8f8f-2a8e60acbc34
    
    It doesn't include the terminal inline assistant, which will be in a
    separate PR.
    
    Release Notes:
    
    - N/A

--- crates/assistant2/src/assistant.rs
+++ crates/assistant2/src/assistant.rs
@@ -1,16 +1,28 @@
 mod active_thread;
 mod assistant_panel;
+mod assistant_settings;
 mod context;
 mod context_picker;
+mod inline_assistant;
 mod message_editor;
+mod prompts;
+mod streaming_diff;
 mod thread;
 mod thread_history;
 mod thread_store;
 mod ui;
 
+use std::sync::Arc;
+
+use assistant_settings::AssistantSettings;
+use client::Client;
 use command_palette_hooks::CommandPaletteFilter;
 use feature_flags::{Assistant2FeatureFlag, FeatureFlagAppExt};
+use fs::Fs;
 use gpui::{actions, AppContext};
+use prompts::PromptLoadingParams;
+use settings::Settings as _;
+use util::ResultExt;
 
 pub use crate::assistant_panel::AssistantPanel;
 
@@ -21,15 +33,37 @@ actions!(
         NewThread,
         ToggleModelSelector,
         OpenHistory,
-        Chat
+        Chat,
+        ToggleInlineAssist,
+        CycleNextInlineAssist,
+        CyclePreviousInlineAssist
     ]
 );
 
 const NAMESPACE: &str = "assistant2";
 
 /// Initializes the `assistant2` crate.
-pub fn init(cx: &mut AppContext) {
+pub fn init(fs: Arc<dyn Fs>, client: Arc<Client>, stdout_is_a_pty: bool, cx: &mut AppContext) {
+    AssistantSettings::register(cx);
     assistant_panel::init(cx);
+
+    let prompt_builder = prompts::PromptBuilder::new(Some(PromptLoadingParams {
+        fs: fs.clone(),
+        repo_path: stdout_is_a_pty
+            .then(|| std::env::current_dir().log_err())
+            .flatten(),
+        cx,
+    }))
+    .log_err()
+    .map(Arc::new)
+    .unwrap_or_else(|| Arc::new(prompts::PromptBuilder::new(None).unwrap()));
+    inline_assistant::init(
+        fs.clone(),
+        prompt_builder.clone(),
+        client.telemetry().clone(),
+        cx,
+    );
+
     feature_gate_assistant2_actions(cx);
 }
 
