66a2258752f0895f915c1d10f648fa87ac02cc63
Jaagup Averin <jaagup.averin@gmail.com>
2024-12-13 11:15:33 +0200

    Update crates/languages/src/python/highlights.scm
    Co-authored-by: Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>

--- crates/languages/src/python/highlights.scm
+++ crates/languages/src/python/highlights.scm
@@ -68,8 +68,8 @@
    "^(abs|all|any|ascii|bin|bool|breakpoint|bytearray|bytes|callable|chr|classmethod|compile|complex|delattr|dict|dir|divmod|enumerate|eval|exec|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|isinstance|issubclass|iter|len|list|locals|map|max|memoryview|min|next|object|oct|open|ord|pow|print|property|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|vars|zip|__import__)$"))
 
 ((identifier) @type.builtin
-    (#match? @type.builtin "^(int|float|complex|bool|list|tuple|range|str|bytes|bytearray|memoryview|set|frozenset|dict)$"))
-
+    (#any-of? @type.builtin "int" "float" "complex" "bool" "list" "tuple" "range" "str" "bytes" "bytearray" "memoryview" "set" "frozenset" "dict"))
+    
 ; Literals
 
 [
