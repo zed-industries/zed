zed-industries/zed

commit f633b125b9bab98bc6d73432dd6e8f352cd7fcec

Fix git commit popup message bracket (#16279)

crates/git/src/commit.rs
@@ -43,7 +43,7 @@ pub fn get_messages(working_directory: &Path, shas: &[Oid]) -> Result<HashMap<Oi
             String::from_utf8_lossy(&output.stdout)
                 .trim()
                 .split_terminator(MARKER)
-                .map(|str| String::from(str.trim())),
+                .map(|str| str.trim().replace("<", "&lt;").replace(">", "&gt;")),
         )
         .collect::<HashMap<Oid, String>>())
 }

commit e66a653ea9ba61d1d88f3af5b760c3671ff047e3

fix(blame): fix incorrect rendering of angle brakets in git blame tooltip
This originally broke in f633b125b9bab98bc6d73432dd6e8f352cd7fcec, this
commit simply replaced `<` and `>` characters with their HTML encoded
equivalents. While this worked, it was not correct for the case where
`<` or `>` exist inside code blocks.

This also felt confusing that this was not happening where the markdown
was being parsed, but rather where where the blame message was retrieved
from git.

This commit fixes #22044, and **should not** cause a regression in
\#16220, by pushing any `InlineHtml` to the resulting string after
parsing the markdown.

crates/git/src/commit.rs
@@ -32,7 +32,7 @@ pub fn get_messages(working_directory: &Path, shas: &[Oid]) -> Result<HashMap<Oi
             String::from_utf8_lossy(&output.stdout)
                 .trim()
                 .split_terminator(MARKER)
-                .map(|str| str.trim().replace("<", "&lt;").replace(">", "&gt;")),
+                .map(|str| String::from(str.trim())),
         )
         .collect::<HashMap<Oid, String>>())
 }

crates/language/src/markdown.rs
@@ -306,6 +306,10 @@ pub async fn parse_markdown_block(
 
             Event::SoftBreak => text.push(' '),
 
+            Event::InlineHtml(inline_html) => {
+                text.push_str(&inline_html);
+            }
+
             _ => {}
         }
     }