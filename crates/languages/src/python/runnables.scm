fac5118f104fe907926428ee664bfc82bc92d171
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-12-21 14:11:52 +0100

    python: Fix decorated test detection (#22327)
    Follow-up to #22325, where I missed a couple points in the review that
    were actually quite relevant.
    
    Closes #ISSUE
    
    Release Notes:
    
    - N/A

--- crates/languages/src/python/runnables.scm
+++ crates/languages/src/python/runnables.scm
@@ -36,7 +36,7 @@
         (function_definition
             name: (identifier) @run @_pytest_method_name
             (#match? @_pytest_method_name "^test_")
-        ) @python-pytest-method
+        ) @_python-pytest-method
     )
     (#set! tag python-pytest-method)
 )
@@ -45,12 +45,12 @@
 (
     (module
         (decorated_definition
-            (decorator)+ @decorator
+            (decorator)+ @_decorator
             definition: (function_definition
-                name: (identifier) @run @_pytest_decorated_method_name
-                (#match? @_pytest_decorated_method_name "^test_")
+                name: (identifier) @run @_pytest_method_name
+                (#match? @_pytest_method_name "^test_")
                 )
-            ) @python-pytest-method
+            ) @_python-pytest-method
         )
     (#set! tag python-pytest-method)
 )
@@ -76,7 +76,7 @@
                     (function_definition
                         name: (identifier) @run @_pytest_method_name
                         (#match? @_pytest_method_name "^test")
-                    ) @python-pytest-method
+                    ) @_python-pytest-method
                     (#set! tag python-pytest-method)
             )
         )
