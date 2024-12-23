909fb66208d41de429c9d069255a856624ff4f6c
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-12-23 11:06:50 +0100

    Renames

--- crates/project/src/project_tree/path_trie.rs
+++ crates/project/src/project_tree/path_trie.rs
@@ -5,19 +5,24 @@ use std::{
     sync::Arc,
 };
 
-/// `PathTrie` is a Trie composed of worktree path components.
-pub(super) struct PathTrie {
+/// [RootPathTrie] is a workhorse of [super::ProjectTree]. It is responsible for determining the closest known project root for a given path.
+/// We can not
+/// It also determines how much of a given path is unexplored, thus letting callers fill in that gap if needed.
+/// A path is unexplored when the closest ancestor of a path is not the path itself; that means that we have not yet ran the scan on that path.
+/// For example, if there's a project root at path `python/project` and we query for a path `python/project/subdir/another_subdir/file.py`, there is
+/// a known root at `python/project` and the unexplored part is `subdir/another_subdir` - we need to run a scan on these 2 directories
+pub(super) struct RootPathTrie {
     path_component: Arc<OsStr>,
     value: Option<()>,
-    children: BTreeMap<Arc<OsStr>, PathTrie>,
+    children: BTreeMap<Arc<OsStr>, RootPathTrie>,
 }
 
-impl PathTrie {
+impl RootPathTrie {
     pub(crate) fn new() -> Self {
         Self::new_with_key(None)
     }
     fn new_with_key(value: Option<Arc<OsStr>>) -> Self {
-        PathTrie {
+        RootPathTrie {
             path_component: value.unwrap_or_else(|| Arc::from(OsStr::new(""))),
             value: None,
             children: BTreeMap::new(),
@@ -29,7 +34,7 @@ impl PathTrie {
         for key in path.0.iter() {
             current = match current.children.entry(key.clone()) {
                 Entry::Vacant(vacant_entry) => {
-                    vacant_entry.insert(PathTrie::new_with_key(Some(key.clone())))
+                    vacant_entry.insert(RootPathTrie::new_with_key(Some(key.clone())))
                 }
                 Entry::Occupied(occupied_entry) => occupied_entry.into_mut(),
             };
@@ -55,7 +60,7 @@ impl PathTrie {
     }
 }
 
-/// [TriePath] is a [Path] preprocessed for amortizing the cost of doing multiple lookups in distinct `PathTrie`s.
+/// [TriePath] is a [Path] preprocessed for amortizing the cost of doing multiple lookups in distinct [RootPathTrie]s.
 #[derive(Clone)]
 pub(crate) struct TriePath(Arc<[Arc<OsStr>]>);
 
