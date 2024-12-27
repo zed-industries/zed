b5b877bd26fa98cf46aeed222161d6b51d6bdd0d
Remco Smits <djsmits12@gmail.com>
2024-12-27 20:19:58 +0100

    Add test to validate we shutdown session when attach modal is dismissed

--- crates/debugger_ui/src/tests/attach_modal.rs
+++ crates/debugger_ui/src/tests/attach_modal.rs
@@ -2,7 +2,7 @@ use crate::*;
 use attach_modal::AttachModal;
 use dap::requests::{Attach, Disconnect, Initialize};
 use gpui::{BackgroundExecutor, TestAppContext, VisualTestContext};
-use menu::Confirm;
+use menu::{Cancel, Confirm};
 use project::{FakeFs, Project};
 use serde_json::json;
 use std::sync::{
@@ -194,4 +194,98 @@ async fn test_show_attach_modal_and_select_process(
     });
 
     shutdown_session.await.unwrap();
+}
+
+#[gpui::test]
+async fn test_shutdown_session_when_modal_is_dismissed(
+    executor: BackgroundExecutor,
+    cx: &mut TestAppContext,
+) {
+    init_test(cx);
+
+    let send_attach_request = Arc::new(AtomicBool::new(false));
+
+    let fs = FakeFs::new(executor.clone());
+
+    let project = Project::test(fs, [], cx).await;
+    let workspace = init_test_workspace(&project, cx).await;
+    let cx = &mut VisualTestContext::from_window(*workspace, cx);
+
+    let task = project.update(cx, |project, cx| {
+        project.dap_store().update(cx, |store, cx| {
+            store.start_debug_session(
+                task::DebugAdapterConfig {
+                    label: "test config".into(),
+                    kind: task::DebugAdapterKind::Fake,
+                    request: task::DebugRequestType::Attach(AttachConfig { process_id: None }),
+                    program: None,
+                    cwd: None,
+                    initialize_args: None,
+                },
+                cx,
+            )
+        })
+    });
+
+    let (session, client) = task.await.unwrap();
+
+    client
+        .on_request::<Initialize, _>(move |_, _| {
+            Ok(dap::Capabilities {
+                supports_step_back: Some(false),
+                ..Default::default()
+            })
+        })
+        .await;
+
+    client
+        .on_request::<Attach, _>({
+            let send_attach_request = send_attach_request.clone();
+            move |_, _| {
+                send_attach_request.store(true, Ordering::SeqCst);
+
+                Ok(())
+            }
+        })
+        .await;
+
+    client.on_request::<Disconnect, _>(move |_, _| Ok(())).await;
+
+    cx.run_until_parked();
+
+    // assert we show the attach modal
+    workspace
+        .update(cx, |workspace, cx| {
+            let attach_modal = workspace.active_modal::<AttachModal>(cx).unwrap();
+
+            let names = attach_modal.update(cx, |modal, cx| attach_modal::procss_names(&modal, cx));
+
+            // we filtered out all processes that are not the current process(zed itself)
+            assert_eq!(1, names.len());
+        })
+        .unwrap();
+
+    // close the modal
+    cx.dispatch_action(Cancel);
+
+    cx.run_until_parked();
+
+    // assert attach modal was dismissed
+    workspace
+        .update(cx, |workspace, cx| {
+            assert!(workspace.active_modal::<AttachModal>(cx).is_none());
+        })
+        .unwrap();
+
+    assert!(
+        !send_attach_request.load(std::sync::atomic::Ordering::SeqCst),
+        "Didn't expected to send attach request, because we closed the modal"
+    );
+
+    // assert debug session is shutdown
+    project.update(cx, |project, cx| {
+        project.dap_store().update(cx, |dap_store, cx| {
+            assert!(dap_store.session_by_id(&session.read(cx).id()).is_none())
+        });
+    });
 }
