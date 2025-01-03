d3981c4d07a37904d2bb12cb1a05d93cf88134ba
Junkui Zhang <364772080@qq.com>
2025-01-03 19:08:42 +0800

    fix `test_buffer_management`

--- crates/copilot/src/copilot.rs
+++ crates/copilot/src/copilot.rs
@@ -1062,6 +1062,17 @@ mod tests {
     use super::*;
     use gpui::TestAppContext;
 
+    #[cfg(not(target_os = "windows"))]
+    fn to_path_buf(path: &str) -> PathBuf {
+        PathBuf::from(path)
+    }
+
+    #[cfg(target_os = "windows")]
+    fn to_path_buf(path: &str) -> PathBuf {
+        let path = path.replace("/root", "C:/");
+        PathBuf::from(path)
+    }
+
     #[gpui::test(iterations = 10)]
     async fn test_buffer_management(cx: &mut TestAppContext) {
         let (copilot, mut lsp) = Copilot::fake(cx);
@@ -1123,7 +1134,7 @@ mod tests {
         buffer_1.update(cx, |buffer, cx| {
             buffer.file_updated(
                 Arc::new(File {
-                    abs_path: "/root/child/buffer-1".into(),
+                    abs_path: to_path_buf("/root/child/buffer-1"),
                     path: Path::new("child/buffer-1").into(),
                 }),
                 cx,
@@ -1136,7 +1147,7 @@ mod tests {
                 text_document: lsp::TextDocumentIdentifier::new(buffer_1_uri),
             }
         );
-        let buffer_1_uri = lsp::Url::from_file_path("/root/child/buffer-1").unwrap();
+        let buffer_1_uri = lsp::Url::from_file_path(to_path_buf("/root/child/buffer-1")).unwrap();
         assert_eq!(
             lsp.receive_notification::<lsp::notification::DidOpenTextDocument>()
                 .await,
