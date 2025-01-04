da9381fc00c324f4526263b5f3ca8eccb34a18a4
Junkui Zhang <364772080@qq.com>
2025-01-04 17:21:33 +0800

    fix `test_document_format_manual_trigger`

--- crates/editor/src/editor_tests.rs
+++ crates/editor/src/editor_tests.rs
@@ -7326,10 +7326,15 @@ async fn test_document_format_manual_trigger(cx: &mut gpui::TestAppContext) {
         ))
     });
 
+    let file_abs_path = if cfg!(windows) {
+        "C:/file.rs"
+    } else {
+        "/file.rs"
+    };
     let fs = FakeFs::new(cx.executor());
-    fs.insert_file("/file.rs", Default::default()).await;
+    fs.insert_file(file_abs_path, Default::default()).await;
 
-    let project = Project::test(fs, ["/file.rs".as_ref()], cx).await;
+    let project = Project::test(fs, [file_abs_path.as_ref()], cx).await;
 
     let language_registry = project.read_with(cx, |project, _| project.languages().clone());
     language_registry.add(Arc::new(Language::new(
@@ -7363,7 +7368,9 @@ async fn test_document_format_manual_trigger(cx: &mut gpui::TestAppContext) {
     );
 
     let buffer = project
-        .update(cx, |project, cx| project.open_local_buffer("/file.rs", cx))
+        .update(cx, |project, cx| {
+            project.open_local_buffer(file_abs_path, cx)
+        })
         .await
         .unwrap();
 
@@ -7389,7 +7396,7 @@ async fn test_document_format_manual_trigger(cx: &mut gpui::TestAppContext) {
         .handle_request::<lsp::request::Formatting, _, _>(move |params, _| async move {
             assert_eq!(
                 params.text_document.uri,
-                lsp::Url::from_file_path("/file.rs").unwrap()
+                lsp::Url::from_file_path(file_abs_path).unwrap()
             );
             assert_eq!(params.options.tab_size, 4);
             Ok(Some(vec![lsp::TextEdit::new(
@@ -7411,7 +7418,7 @@ async fn test_document_format_manual_trigger(cx: &mut gpui::TestAppContext) {
     fake_server.handle_request::<lsp::request::Formatting, _, _>(move |params, _| async move {
         assert_eq!(
             params.text_document.uri,
-            lsp::Url::from_file_path("/file.rs").unwrap()
+            lsp::Url::from_file_path(file_abs_path).unwrap()
         );
         futures::future::pending::<()>().await;
         unreachable!()
