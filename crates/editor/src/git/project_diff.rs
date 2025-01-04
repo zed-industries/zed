60abb8d63104d76180f457cc975206e9f2da711c
Junkui Zhang <364772080@qq.com>
2025-01-04 17:47:59 +0800

    fix `simple_edit_test`

--- crates/editor/src/git/project_diff.rs
+++ crates/editor/src/git/project_diff.rs
@@ -1123,12 +1123,20 @@ mod tests {
 
     #[gpui::test(iterations = 30)]
     async fn simple_edit_test(cx: &mut TestAppContext) {
+        fn to_path_string(path: &str) -> String {
+            if cfg!(target_os = "windows") {
+                path.replace("/root", "C:/root")
+            } else {
+                path.to_string()
+            }
+        }
+
         cx.executor().allow_parking();
         init_test(cx);
 
         let fs = fs::FakeFs::new(cx.executor().clone());
         fs.insert_tree(
-            "/root",
+            to_path_string("/root"),
             json!({
                 ".git": {},
                 "file_a": "This is file_a",
@@ -1137,14 +1145,17 @@ mod tests {
         )
         .await;
 
-        let project = Project::test(fs.clone(), [Path::new("/root")], cx).await;
+        let project = Project::test(fs.clone(), [Path::new(&to_path_string("/root"))], cx).await;
         let workspace = cx.add_window(|cx| Workspace::test_new(project.clone(), cx));
         let cx = &mut VisualTestContext::from_window(*workspace.deref(), cx);
 
         let file_a_editor = workspace
             .update(cx, |workspace, cx| {
-                let file_a_editor =
-                    workspace.open_abs_path(PathBuf::from("/root/file_a"), true, cx);
+                let file_a_editor = workspace.open_abs_path(
+                    PathBuf::from(to_path_string("/root/file_a")),
+                    true,
+                    cx,
+                );
                 ProjectDiffEditor::deploy(workspace, &Deploy, cx);
                 file_a_editor
             })
@@ -1212,7 +1223,7 @@ mod tests {
             });
         });
         fs.set_status_for_repo_via_git_operation(
-            Path::new("/root/.git"),
+            Path::new(&to_path_string("/root/.git")),
             &[(
                 Path::new("file_a"),
                 TrackedStatus {
