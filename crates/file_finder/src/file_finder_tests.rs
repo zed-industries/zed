835c4d5cac38fab5289f43361ff420a48b59465a
Junkui Zhang <364772080@qq.com>
2025-01-05 21:10:43 +0800

    fix `test_history_items_shown_in_order_of_open`

--- crates/file_finder/src/file_finder_tests.rs
+++ crates/file_finder/src/file_finder_tests.rs
@@ -1316,13 +1316,15 @@ async fn test_non_separate_history_items(cx: &mut TestAppContext) {
 
 #[gpui::test]
 async fn test_history_items_shown_in_order_of_open(cx: &mut TestAppContext) {
+    const PATTERN: &str = "/test";
+
     let app_state = init_test(cx);
 
     app_state
         .fs
         .as_fake()
         .insert_tree(
-            "/test",
+            to_path_string("/test", PATTERN),
             json!({
                 "test": {
                     "1.txt": "// One",
@@ -1333,7 +1335,12 @@ async fn test_history_items_shown_in_order_of_open(cx: &mut TestAppContext) {
         )
         .await;
 
-    let project = Project::test(app_state.fs.clone(), ["/test".as_ref()], cx).await;
+    let project = Project::test(
+        app_state.fs.clone(),
+        [to_path_string("/test", PATTERN).as_ref()],
+        cx,
+    )
+    .await;
     let (workspace, cx) = cx.add_window_view(|cx| Workspace::test_new(project, cx));
 
     open_queried_buffer("1", 1, "1.txt", &workspace, cx).await;
