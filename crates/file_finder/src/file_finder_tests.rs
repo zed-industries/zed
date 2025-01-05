27719f07a9069642567aef0816f9ca06a0b871bc
Junkui Zhang <364772080@qq.com>
2025-01-05 21:18:02 +0800

    fix `test_select_current_open_file_when_no_history`

--- crates/file_finder/src/file_finder_tests.rs
+++ crates/file_finder/src/file_finder_tests.rs
@@ -1139,13 +1139,15 @@ async fn test_search_sorts_history_items(cx: &mut gpui::TestAppContext) {
 
 #[gpui::test]
 async fn test_select_current_open_file_when_no_history(cx: &mut gpui::TestAppContext) {
+    const PATTERN: &str = "/root";
+
     let app_state = init_test(cx);
 
     app_state
         .fs
         .as_fake()
         .insert_tree(
-            "/root",
+            to_path_string("/root", PATTERN),
             json!({
                 "test": {
                     "1_qw": "",
@@ -1154,7 +1156,12 @@ async fn test_select_current_open_file_when_no_history(cx: &mut gpui::TestAppCon
         )
         .await;
 
-    let project = Project::test(app_state.fs.clone(), ["/root".as_ref()], cx).await;
+    let project = Project::test(
+        app_state.fs.clone(),
+        [to_path_string("/root", PATTERN).as_ref()],
+        cx,
+    )
+    .await;
     let (workspace, cx) = cx.add_window_view(|window, cx| Workspace::test_new(project, window, cx));
     // Open new buffer
     open_queried_buffer("1", 1, "1_qw", &workspace, cx).await;
