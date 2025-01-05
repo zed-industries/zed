62c03d3264bbe0c47c0551fd0a4d96b3701b3dac
Junkui Zhang <364772080@qq.com>
2025-01-05 13:49:27 +0800

    fix merge conflicts

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -5192,7 +5192,7 @@ impl Editor {
             }))
     }
 
-    #[cfg(feature = "test-support")]
+    #[cfg(any(feature = "test-support", test))]
     pub fn context_menu_visible(&self) -> bool {
         self.context_menu
             .borrow()
