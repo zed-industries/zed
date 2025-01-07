9585b321f2bda2db6c18c4767f5e0574b9f7ab2a
Michael Sloan <michael@zed.dev>
2025-01-06 20:14:53 -0700

    Bump git2 version + use commondir from it instead of walking fs

--- crates/git/src/repository.rs
+++ crates/git/src/repository.rs
@@ -58,8 +58,17 @@ pub trait GitRepository: Send + Sync {
 
     fn blame(&self, path: &Path, content: Rope) -> Result<crate::blame::Blame>;
 
-    /// Returns the path to the repository, typically the `.git` folder.
+    /// Returns the absolute path to the repository. For worktrees, this will be the path to the
+    /// worktree's gitdir within the main repository (typically `.git/worktrees/<name>`).
     fn path(&self) -> PathBuf;
+
+    /// Returns the absolute path to the ".git" dir for the main repository, typically a `.git`
+    /// folder. For worktrees, this will be the path to the repository the worktree was created
+    /// from. Otherwise, this is the same value as `path()`.
+    ///
+    /// Git documentation calls this the "commondir", and for git CLI is overridden by
+    /// `GIT_COMMON_DIR`.
+    fn main_repository_path(&self) -> PathBuf;
 
     /// Updates the index to match the worktree at the given paths.
     ///
@@ -112,6 +121,11 @@ impl GitRepository for RealGitRepository {
     fn path(&self) -> PathBuf {
         let repo = self.repository.lock();
         repo.path().into()
+    }
+
+    fn main_repository_path(&self) -> PathBuf {
+        let repo = self.repository.lock();
+        repo.commondir().into()
     }
 
     fn load_index_text(&self, path: &RepoPath) -> Option<String> {
@@ -408,6 +422,10 @@ impl GitRepository for FakeGitRepository {
     fn path(&self) -> PathBuf {
         let state = self.state.lock();
         state.path.clone()
+    }
+
+    fn main_repository_path(&self) -> PathBuf {
+        self.path()
     }
 
     fn status(&self, path_prefixes: &[RepoPath]) -> Result<GitStatus> {
