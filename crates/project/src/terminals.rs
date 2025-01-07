31306cc24297a0dd72a897592d80113fbbb7d7f1
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-01-07 21:30:50 +0100

    python: Check for activate script existence before running it

--- crates/project/src/terminals.rs
+++ crates/project/src/terminals.rs
@@ -433,6 +433,9 @@ impl Project {
             "windows" => "\r",
             _ => "\n",
         };
+        if smol::block_on(self.fs.load_bytes(path.as_ref())).is_err() {
+            return None;
+        }
         Some(format!(
             "{} {} ; clear{}",
             activate_keyword, quoted, line_ending
