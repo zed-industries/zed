24cde0973ce2613d4787f572912bfd6f5c14e6d6
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-01-13 16:25:41 +0100

    fixup! Remove unused variable

--- crates/project/src/project_tree.rs
+++ crates/project/src/project_tree.rs
@@ -13,7 +13,7 @@ use std::{
 };
 
 use collections::HashMap;
-use gpui::{AppContext, Context as _, Model, ModelContext, Subscription};
+use gpui::{AppContext, Context as _, EventEmitter, Model, ModelContext, Subscription};
 use language::{CachedLspAdapter, LanguageName, LanguageRegistry, LspAdapterDelegate};
 use lsp::LanguageServerName;
 use path_trie::{LabelPresence, RootPathTrie, TriePath};
