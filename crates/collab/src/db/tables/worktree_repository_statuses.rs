f73bed4a4f772b6871cfa3be9ab07befe2cdfd0f
Cole Miller <cole@zed.dev>
2025-01-13 21:49:50 -0500

    Get collab building

--- crates/collab/src/db/tables/worktree_repository_statuses.rs
+++ crates/collab/src/db/tables/worktree_repository_statuses.rs
@@ -12,9 +12,26 @@ pub struct Model {
     pub work_directory_id: i64,
     #[sea_orm(primary_key)]
     pub repo_path: String,
-    pub status: i64,
+    pub status_kind: StatusKind,
+    /// For unmerged entries, this is the `first_head` status. For tracked entries, this is the `index_status`.
+    pub first_status: Option<i64>,
+    /// For unmerged entries, this is the `second_head` status. For tracked entries, this is the `worktree_status`.
+    pub second_status: Option<i64>,
     pub scan_id: i64,
     pub is_deleted: bool,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, EnumIter, DeriveActiveEnum)]
+#[sea_orm(rs_type = "u32", db_type = "Integer")]
+pub enum StatusKind {
+    #[sea_orm(num_value = 0)]
+    Untracked,
+    #[sea_orm(num_value = 1)]
+    Ignored,
+    #[sea_orm(num_value = 2)]
+    Unmerged,
+    #[sea_orm(num_value = 3)]
+    Tracked,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
