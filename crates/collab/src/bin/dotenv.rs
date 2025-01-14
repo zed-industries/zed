ecd009b0f8edc65bd92b14c1447959a1fd4313ba
Junkui Zhang <364772080@qq.com>
2025-01-14 18:32:34 +0800

    better ps1

--- crates/collab/src/bin/dotenv.rs
+++ crates/collab/src/bin/dotenv.rs
@@ -3,7 +3,7 @@ use collab::env::get_dotenv_vars;
 fn main() -> anyhow::Result<()> {
     for (key, value) in get_dotenv_vars(".")? {
         if option_env!("POWERSHELL").is_some() {
-            println!("$env:{} = \"{}\"", key, value);
+            println!("$env:{}=\"{}\"", key, value);
         } else {
             println!("export {}=\"{}\"", key, value);
         }
