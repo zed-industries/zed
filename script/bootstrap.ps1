9a056cfca88d7ddaf3dda26edf9fa976ad615b04
Junkui Zhang <364772080@qq.com>
2025-01-14 18:32:34 +0800

    better ps1

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -1,4 +1,7 @@
-$env:POWERSHELL = "true"
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+
+$env:POWERSHELL = $true
 
 if (!(Get-Command sqlx -ErrorAction SilentlyContinue) -or (sqlx --version) -notlike "sqlx-cli 0.7.2") {
     Write-Output "sqlx-cli not found or not the required version, installing version 0.7.2..."
@@ -10,11 +13,9 @@ Set-Location .\crates\collab
 # Export contents of .env.toml
 $env = (cargo run --bin dotenv) -join "`n";
 Invoke-Expression $env
-# Invoke-Expression "cargo run --bin dotenv"
 
 Set-Location ../..
 
 Write-Output "creating databases..."
-sqlx database create --database-url "$DATABASE_URL"
-sqlx database create --database-url "$LLM_DATABASE_URL"
-
+sqlx database create --database-url "$env:DATABASE_URL"
+sqlx database create --database-url "$env:LLM_DATABASE_URL"
