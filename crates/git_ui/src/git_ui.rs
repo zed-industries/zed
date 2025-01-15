3485a48d696480565e73b4b05986d820ef766881
Cole Miller <cole@zed.dev>
2025-01-15 18:31:46 -0500

    WIP

--- crates/git_ui/src/git_ui.rs
+++ crates/git_ui/src/git_ui.rs
@@ -10,7 +10,6 @@ use project::{Project, WorktreeId};
 use std::sync::Arc;
 use sum_tree::SumTree;
 use ui::{Color, Icon, IconName, IntoElement, SharedString};
-use util::ResultExt as _;
 use worktree::RepositoryEntry;
 
 pub mod git_panel;
@@ -27,17 +26,20 @@ pub fn init(cx: &mut AppContext) {
 }
 
 pub struct GitState {
+    // optional composed string
+    // optional contextual string for current collaborators
+    // methods for adding or triming out collaborator string
+    // one or more optional suggested messages
+    // arrow up/down
     /// The current commit message being composed.
     commit_message: Option<SharedString>,
 
+    // needs to be on project
     /// When a git repository is selected, this is used to track which repository's changes
     /// are currently being viewed or modified in the UI.
     active_repository: Option<(WorktreeId, RepositoryEntry, Arc<dyn GitRepository>)>,
 
     update_sender: mpsc::UnboundedSender<(Arc<dyn GitRepository>, Vec<RepoPath>, StatusAction)>,
-
-    // TODO replace this with a list of names and IDs
-    _all_repositories: HashMap<WorktreeId, SumTree<RepositoryEntry>>,
 }
 
 impl GitState {
@@ -62,7 +64,6 @@ impl GitState {
             commit_message: None,
             active_repository: None,
             update_sender: updater_tx,
-            _all_repositories: HashMap::default(),
         }
     }
 
