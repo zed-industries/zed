9a07bd52d3ab3396e8357b5279b173f7cd55a707
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-01-16 17:45:57 +0100

    lsp: remove position from `GetDocumentDiagnostic` message

--- crates/project/src/lsp_store.rs
+++ crates/project/src/lsp_store.rs
@@ -4774,8 +4774,7 @@ impl LspStore {
 
     pub fn document_diagnostic(
         &mut self,
-        buffer_handle: &Model<Buffer>,
-        position: Anchor,
+        buffer_handle: Model<Buffer>,
         cx: &mut ModelContext<Self>,
     ) -> Task<Result<Vec<Option<LspDiagnostics>>>> {
         let buffer = buffer_handle.read(cx);
@@ -4790,8 +4789,7 @@ impl LspStore {
                     proto::AllLanguageServers {},
                 )),
                 request: Some(proto::multi_lsp_query::Request::GetDocumentDiagnostics(
-                    GetDocumentDiagnostics { position }
-                        .to_proto(upstream_project_id, buffer_handle.read(cx)),
+                    GetDocumentDiagnostics {}.to_proto(upstream_project_id, buffer_handle.read(cx)),
                 )),
             });
             let buffer = buffer_handle.clone();
@@ -4813,7 +4811,7 @@ impl LspStore {
                             }
                         })
                         .map(|diagnostics_response| {
-                            GetDocumentDiagnostics { position }.response_from_proto(
+                            GetDocumentDiagnostics {}.response_from_proto(
                                 diagnostics_response,
                                 project.clone(),
                                 buffer.clone(),
@@ -4831,9 +4829,9 @@ impl LspStore {
             })
         } else {
             let all_actions_task = self.request_multiple_lsp_locally(
-                buffer_handle,
-                Some(position),
-                GetDocumentDiagnostics { position },
+                &buffer_handle,
+                Some(0),
+                GetDocumentDiagnostics {},
                 cx,
             );
             cx.spawn(|_, _| async move { Ok(all_actions_task.await.into_iter().collect()) })
@@ -6222,7 +6220,7 @@ fn pull_diagnostic(
                     .update(&mut cx, |project, cx| {
                         project.request_multiple_lsp_locally(
                             &buffer,
-                            Some(get_document_diagnostics.position),
+                            Some(0),
                             get_document_diagnostics,
                             cx,
                         )
