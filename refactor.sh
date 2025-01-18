3a23a60cd567ce1a87a50cf9ebbc5847fe0c0fb8
Michael Sloan <michael@zed.dev>
2025-01-18 01:25:09 -0700

    Update refactor.sh with more methods moved to Window

--- refactor.sh
+++ refactor.sh
@@ -12,16 +12,23 @@
 #
 # * Run this script.
 
+dry=true
+if [ "$1" = "apply" ]; then
+    dry=false
+fi
+
 re() {
-    ruplacer "$1" "$2" crates/ --type *.rs --go
+    if [ "$dry" = true ]; then
+        ruplacer "$1" "$2" crates/ --type *.rs
+    else
+        ruplacer "$1" "$2" crates/ --type *.rs --go
+    fi
 }
 
-re_dry() {
-    ruplacer "$1" "$2" crates/ --type *.rs
-}
-
-re 'cx\.new_view'                    'cx.new_model'
+re 'cx\.new_view\('                  'cx.new_model('
+re 'cx\.view\('                      'cx.model('
 re 'View<'                           'Model<'
+re 'FocusableView'                   'Focusable'
 
 # closure parameters
 re ', &mut WindowContext\)'          ', &mut Window, &mut AppContext)'
@@ -30,6 +37,10 @@ re '\(&mut WindowContext\)'          '(&mut Window, &mut AppContext)'
 re '\(&mut ViewContext<([^>]+)>\)'   '(&mut Window, &mut ModelContext<$1>)'
 
 # function parameters
+re '_: &mut WindowContext\)'          '_window: &mut Window, _cx: &mut AppContext)'
+re '_: &mut ViewContext<([^>]+)>\)'   '_window: &mut Window, _cx: &mut ModelContext<$1>)'
+re '_: &mut WindowContext,'           '_window: &mut Window, _cx: &mut AppContext,'
+re '_: &mut ViewContext<([^>]+)>,'    '_window: &mut Window, _cx: &mut ModelContext<$1>,'
 re '_cx: &mut WindowContext\)'        '_window: &mut Window, _cx: &mut AppContext)'
 re '_cx: &mut ViewContext<([^>]+)>\)' '_window: &mut Window, _cx: &mut ModelContext<$1>)'
 re '_cx: &mut WindowContext,'         '_window: &mut Window, _cx: &mut AppContext,'
@@ -39,6 +50,10 @@ re 'cx: &mut ViewContext<([^>]+)>\)'  'window: &mut Window, cx: &mut ModelContex
 re 'cx: &mut WindowContext,'          'window: &mut Window, cx: &mut AppContext,'
 re 'cx: &mut ViewContext<([^>]+)>,'   'window: &mut Window, cx: &mut ModelContext<$1>,'
 
+re '_: &WindowContext\)'              '_window: &Window, _cx: &AppContext)'
+re '_: &ViewContext<([^>]+)>\)'       '_window: &Window, _cx: &ModelContext<$1>)'
+re '_: &WindowContext,'               '_window: &Window, _cx: &AppContext,'
+re '_: &ViewContext<([^>]+)>,'        '_window: &Window, _cx: &ModelContext<$1>,'
 re '_cx: &WindowContext\)'            '_window: &Window, _cx: &AppContext)'
 re '_cx: &ViewContext<([^>]+)>\)'     '_window: &Window, _cx: &ModelContext<$1>)'
 re '_cx: &WindowContext,'             '_window: &Window, _cx: &AppContext,'
@@ -48,9 +63,43 @@ re 'cx: &ViewContext<([^>]+)>\)'      'window: &Window, cx: &ModelContext<$1>)'
 re 'cx: &WindowContext,'              'window: &Window, cx: &AppContext,'
 re 'cx: &ViewContext<([^>]+)>,'       'window: &Window, cx: &ModelContext<$1>,'
 
-# context methods moved to window
+# VisualContext methods moved to window, that take context
+re 'cx.dismiss_view\(' 'window.dismiss_view(cx, '
+re 'cx.focus_view\(' 'window.focus_view(cx, '
+re 'cx.new_view\(' 'window.new_view(cx, '
+re 'cx.replace_root_view\(' 'window.replace_root_view(cx, '
+re 'cx.update_view\(' 'window.update_view(cx, '
+
+# AppContext methods moved to window, that take context
+re 'cx.appearance_changed\(\)' 'window.appearance_changed(cx)'
+re 'cx.available_actions\(\)' 'window.available_actions(cx)'
+re 'cx.dispatch_keystroke_observers\(' 'window.dispatch_keystroke_observers(cx, '
+re 'cx.display\(\)' 'window.display(cx)'
+re 'cx.focused\(\)' 'window.focused(cx)'
+re 'cx.handle_input\(' 'window.handle_input(cx, '
+re 'cx.paint_svg\(' 'window.paint_svg(cx, '
+re 'cx.prompt\(' 'window.prompt(cx, '
+re 'cx.request_layout\(' 'window.request_layout(cx, '
+re 'cx.use_asset\(' 'window.use_asset(cx, '
+
+# Subset of AppContext methods moved to window that don't take context
 re 'cx\.set_cursor_style\('           'window.set_cursor_style('
 re 'cx\.modifiers\('                  'window.modifiers('
 re 'cx\.mouse_position\('             'window.mouse_position('
 
-re 'ModelContext<\\' 'ModelContext<'
+# common closure patterns
+re 'cx.listener\(\|this, _, cx\|'     'cx.listener(|this, _, _, cx|'
+re 'cx.listener\(\|_, _, cx\|'        'cx.listener(|_, _, _, cx|'
+
+# cleanup imports
+re ' ViewContext,'                     ''
+re ' WindowContext,'                   ''
+re ' WeakView,'                        ''
+re ' View,'                            ''
+re ', ViewContext\}'                   '}'
+re ', WindowContext\}'                 '}'
+re ', WeakView\}'                      '}'
+re ', View\}'                          '}'
+
+# other patterns
+re '\.detach_and_notify_err\(cx'       '.detach_and_notify_err(window, cx'
