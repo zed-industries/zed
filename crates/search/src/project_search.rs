b6e01c4e983a727b4354ef4f2125de9655f61920
Junkui Zhang <364772080@qq.com>
2025-01-21 17:04:09 +0800

    fix `test_search_query_history_with_multiple_views`

--- crates/search/src/project_search.rs
+++ crates/search/src/project_search.rs
@@ -3314,13 +3314,13 @@ pub mod tests {
 
         let fs = FakeFs::new(cx.background_executor.clone());
         fs.insert_tree(
-            "/dir",
+            add_root_for_windows("/dir"),
             json!({
                 "one.rs": "const ONE: usize = 1;",
             }),
         )
         .await;
-        let project = Project::test(fs.clone(), ["/dir".as_ref()], cx).await;
+        let project = Project::test(fs.clone(), [add_root_for_windows("/dir").as_ref()], cx).await;
         let worktree_id = project.update(cx, |this, cx| {
             this.worktrees(cx).next().unwrap().read(cx).id()
         });
