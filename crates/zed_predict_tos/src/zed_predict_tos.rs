50d3ef52dbc1c2256dedddcc18d094c8c0f5488c
Agus Zubiaga <agus@zed.dev>
2025-01-22 19:01:31 -0300

    Checkpoint: Rearrange layout

--- crates/zed_predict_tos/src/zed_predict_tos.rs
+++ crates/zed_predict_tos/src/zed_predict_tos.rs
@@ -8,7 +8,7 @@ use gpui::{
     EventEmitter, FocusHandle, FocusableView, Model, MouseDownEvent, Render, View,
 };
 use settings::Settings;
-use ui::{prelude::*, ButtonLike, TintColor};
+use ui::{prelude::*, TintColor};
 use workspace::{ModalView, Workspace};
 
 /// Terms of acceptance for AI inline prediction.
@@ -87,7 +87,6 @@ impl Render for ZedPredictTos {
             .w_96()
             .p_4()
             .relative()
-            .items_center()
             .gap_2()
             .overflow_hidden()
             .elevation_3(cx)
@@ -112,6 +111,7 @@ impl Render for ZedPredictTos {
                     .top_0()
                     .left_0()
                     .right_0()
+                    // TODO: replace with single SVG
                     .children((0..254).enumerate().map(|(index, _)| {
                         let opacity = 0.24 - (index as f32 * 0.0016);
                         svg()
@@ -122,97 +122,70 @@ impl Render for ZedPredictTos {
                             .opacity(opacity.max(0.001))
                     })),
             )
-            .child({
-                let tab = |n: usize| {
-                    let text_color = cx.theme().colors().text;
-                    let border_color = cx.theme().colors().text_accent.opacity(0.4);
-
-                    h_flex().child(
-                        h_flex()
-                            .px_4()
-                            .py_0p5()
-                            .bg(cx.theme().colors().editor_background)
-                            .border_1()
-                            .border_color(border_color)
-                            .rounded_md()
-                            .font(theme::ThemeSettings::get_global(cx).buffer_font.clone())
-                            .text_size(TextSize::XSmall.rems(cx))
-                            .text_color(text_color)
-                            .child("tab")
-                            .with_animation(
-                                ElementId::Integer(n),
-                                Animation::new(Duration::from_millis(2400)).repeat(),
-                                move |tab, delta| {
-                                    let delta = (delta - 0.15 * n as f32) / 0.7;
-                                    let delta = 1.0 - (0.5 - delta).abs() * 2.;
-                                    let delta = ease_in_out(delta.clamp(0., 1.));
-                                    let delta = 0.1 + 0.9 * delta;
-
-                                    tab.border_color(border_color.opacity(delta))
-                                        .text_color(text_color.opacity(delta))
-                                },
-                            ),
-                    )
-                };
-
-                v_flex()
-                    .items_center()
-                    .gap_2()
-                    .w_full()
-                    .child(tab(0).ml_neg_20())
-                    .child(tab(1))
-                    .child(tab(2).ml_20())
-            })
             .child(
-                v_flex()
-                    .mt_2()
-                    .gap_1()
+                h_flex()
+                    .w_full()
+                    .mb_2()
+                    .justify_between()
                     .child(
                         v_flex()
-                            .w_full()
-                            .items_center()
-                            .gap_0p5()
                             .child(
-                                h_flex().child(
-                                    Label::new("Zed AI")
-                                        .size(LabelSize::Small)
-                                        .color(Color::Muted),
-                                ),
+                                Label::new("Introducing Zed AI")
+                                    .size(LabelSize::Small)
+                                    .color(Color::Muted),
                             )
                             .child(h_flex().child(Headline::new("Edit Prediction"))),
                     )
-                    .child(
-                        v_flex()
-                            .w_full()
-                            .items_center()
-                            .gap_0()
-                            .child(
-                                h_flex().child(
-                                    Label::new("To start using Edit Predictions, please read")
-                                        .color(Color::Muted),
-                                ),
-                            )
-                            .child(
+                    .child({
+                        let tab = |n: usize| {
+                            let text_color = cx.theme().colors().text;
+                            let border_color = cx.theme().colors().text_accent.opacity(0.4);
+
+                            h_flex().child(
                                 h_flex()
-                                    .child(Label::new("and accept our").color(Color::Muted))
-                                    .child(
-                                        ButtonLike::new("tos-link")
-                                            .child(
-                                                h_flex()
-                                                    .gap_1()
-                                                    .child(
-                                                        Label::new("Terms of Service.")
-                                                            .underline(true),
-                                                    )
-                                                    .child(
-                                                        Icon::new(IconName::ExternalLink)
-                                                            .size(IconSize::Indicator),
-                                                    ),
-                                            )
-                                            .on_click(cx.listener(Self::view_terms)),
+                                    .px_4()
+                                    .py_0p5()
+                                    .bg(cx.theme().colors().editor_background)
+                                    .border_1()
+                                    .border_color(border_color)
+                                    .rounded_md()
+                                    .font(theme::ThemeSettings::get_global(cx).buffer_font.clone())
+                                    .text_size(TextSize::XSmall.rems(cx))
+                                    .text_color(text_color)
+                                    .child("tab")
+                                    .with_animation(
+                                        ElementId::Integer(n),
+                                        Animation::new(Duration::from_millis(2400)).repeat(),
+                                        move |tab, delta| {
+                                            let delta = (delta - 0.15 * n as f32) / 0.7;
+                                            let delta = 1.0 - (0.5 - delta).abs() * 2.;
+                                            let delta = ease_in_out(delta.clamp(0., 1.));
+                                            let delta = 0.1 + 0.9 * delta;
+
+                                            tab.border_color(border_color.opacity(delta))
+                                                .text_color(text_color.opacity(delta))
+                                        },
                                     ),
-                            ),
-                    ),
+                            )
+                        };
+
+                        v_flex()
+                            .gap_2()
+                            .items_center()
+                            .child(tab(0).ml_neg_20())
+                            .child(tab(1))
+                            .child(tab(2).ml_20())
+                    }),
+            )
+            .child(h_flex().absolute().top_2().right_2().child(
+                IconButton::new("cancel", IconName::X).on_click(cx.listener(
+                    |_, _: &ClickEvent, cx| {
+                        cx.emit(DismissEvent);
+                    },
+                )),
+            ))
+            .child(
+                Label::new("Read and accept the terms of service to set Zed as your inline completions provider.").color(Color::Muted)
             )
             .child(
                 v_flex()
@@ -220,18 +193,19 @@ impl Render for ZedPredictTos {
                     .gap_1()
                     .w_full()
                     .child(
-                        Button::new("accept-tos", "Accept Terms of Service and Enable")
+                        Button::new("accept-tos", "Tab to Accept and Enable")
                             .style(ButtonStyle::Tinted(TintColor::Accent))
                             .full_width()
                             .on_click(cx.listener(Self::accept_terms)),
                     )
                     .child(
-                        Button::new("cancel", "Cancel")
+                        Button::new("view-tos", "Terms of Service")
                             .full_width()
-                            .on_click(cx.listener(|_, _: &ClickEvent, cx| {
-                                cx.emit(DismissEvent);
-                            })),
-                    ),
+                            .icon(IconName::ArrowUpRight)
+                            .icon_size(IconSize::Indicator)
+                            .icon_color(Color::Muted)
+                            .on_click(cx.listener(Self::view_terms)),
+                    )
             )
     }
 }
