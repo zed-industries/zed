d27252691b503d95f23776115118d859ac3a6093
Junkui Zhang <364772080@qq.com>
2025-01-23 19:35:21 +0800

    update macro

--- crates/util_macros/src/util_macros.rs
+++ crates/util_macros/src/util_macros.rs
@@ -4,42 +4,46 @@ use syn::{parse::Parse, parse_macro_input, LitInt, LitStr};
 
 struct ReplacePathInput {
     path: LitStr,
-    depth: LitInt,
+    index: Option<LitInt>,
 }
 
 impl Parse for ReplacePathInput {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let path: LitStr = input.parse()?;
-        input.parse::<syn::Token![,]>()?;
-        let depth: LitInt = input.parse()?;
-        Ok(ReplacePathInput { path, depth })
+        let index = input
+            .parse::<syn::Token![,]>()
+            .ok()
+            .and_then(|_| input.parse().ok());
+        Ok(ReplacePathInput { path, index })
     }
 }
 
 #[proc_macro]
-pub fn replace_path_slashes(input: TokenStream) -> TokenStream {
-    let ReplacePathInput { path, depth } = parse_macro_input!(input as ReplacePathInput);
+pub fn separator(input: TokenStream) -> TokenStream {
+    let ReplacePathInput { path, index } = parse_macro_input!(input as ReplacePathInput);
     let path = path.value();
-    let mut depth: usize = depth
-        .base10_parse()
-        .expect("Depth must be a positive integer");
+    let index = index.map(|idx| {
+        idx.base10_parse::<usize>()
+            .expect("Depth must be a positive integer")
+    });
 
     let mut components: Vec<String> = path.split('/').into_iter().map(String::from).collect();
-    components.reverse();
 
-    if depth == 0 {
-        depth = components.len() - 1;
-    }
-    for comp in components.iter_mut().skip(1) {
-        if depth > 0 {
+    let num_take = components.len() - 1;
+    if let Some(idx) = index {
+        for (i, comp) in components.iter_mut().take(num_take).enumerate() {
+            if i == idx {
+                comp.push('\\');
+            } else {
+                comp.push('/');
+            }
+        }
+    } else {
+        for comp in components.iter_mut().take(num_take) {
             comp.push('\\');
-            depth -= 1;
-        } else {
-            comp.push('/');
         }
     }
 
-    components.reverse();
     let new_path = components.concat();
 
     TokenStream::from(quote! {
