8826d6bdd168a58dd374e88be2c7e23d468a997a
Victor Quiroz <git@victorhqc.com>
2025-01-24 12:13:11 +0100

    chore: update highlights

--- extensions/prisma/languages/prisma/highlights.scm
+++ extensions/prisma/languages/prisma/highlights.scm
@@ -3,18 +3,24 @@
  "enum"
  "generator"
  "model"
+ "view"
 ] @keyword
 
 (comment) @comment
 (developer_comment) @comment
 
+(number) @number
+(string) @string
 (arguments) @property
-(attribute) @function
-(call_expression) @function
-(column_type) @type
+(call_expression (identifier) @function)
 (enumeral) @constant
 (identifier) @variable
 (string) @string
+(column_declaration (identifier) (column_type (identifier) @type))
+(attribute (identifier) @tag)
+(attribute (call_expression (identifier) @tag))
+(attribute (call_expression (member_expression (identifier) @tag)))
+(type_expression (identifier) @type)
 
 "(" @punctuation.bracket
 ")" @punctuation.bracket
