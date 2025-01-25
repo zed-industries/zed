2a061e6fc70b162d7d052fa65971f44a65a55496
MrSubidubi <dev@bahn.sh>
2025-01-25 17:25:10 +0100

    html: Add injections for `style` and event handler attributes

--- extensions/html/languages/html/injections.scm
+++ extensions/html/languages/html/injections.scm
@@ -5,3 +5,13 @@
 (style_element
   (raw_text) @injection.content
   (#set! injection.language "css"))
+
+(attribute
+    (attribute_name) @_attribute_name (#match? @_attribute_name "^style$")
+    (quoted_attribute_value (attribute_value) @injection.content)
+    (#set! injection.language "css"))
+
+(attribute
+    (attribute_name) @_attribute_name (#match? @_attribute_name "^on[a-z]+$")
+    (quoted_attribute_value (attribute_value) @injection.content)
+    (#set! injection.language "javascript"))
