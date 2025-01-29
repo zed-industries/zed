cd6d979b4844b04be02af40ad921f305c183f98f
Junkui Zhang <364772080@qq.com>
2025-01-29 21:51:27 +0800

    fix `remote_server`

--- crates/remote_server/src/headless_project.rs
+++ crates/remote_server/src/headless_project.rs
@@ -325,7 +325,13 @@ impl HeadlessProject {
         mut cx: AsyncApp,
     ) -> Result<proto::AddWorktreeResponse> {
         use client::ErrorCodeExt;
-        let path = shellexpand::tilde(&message.payload.path).to_string();
+        let path = message
+            .payload
+            .path
+            .clone()
+            .unwrap_or_default()
+            .to_native_string();
+        let path = shellexpand::tilde(&path).to_string();
 
         let fs = this.read_with(&mut cx, |this, _| this.fs.clone())?;
         let path = PathBuf::from(path);
@@ -363,7 +369,7 @@ impl HeadlessProject {
         let response = this.update(&mut cx, |_, cx| {
             worktree.update(cx, |worktree, _| proto::AddWorktreeResponse {
                 worktree_id: worktree.id().to_proto(),
-                canonicalized_path: canonicalized.to_string_lossy().to_string(),
+                canonicalized_path: Some(proto::CrossPlatformPath::from(canonicalized)),
             })
         })?;
 
@@ -418,7 +424,7 @@ impl HeadlessProject {
                 buffer_store.open_buffer(
                     ProjectPath {
                         worktree_id,
-                        path: PathBuf::from(message.payload.path).into(),
+                        path: message.payload.path.unwrap_or_default().into(),
                     },
                     cx,
                 )
@@ -559,7 +565,13 @@ impl HeadlessProject {
         envelope: TypedEnvelope<proto::ListRemoteDirectory>,
         cx: AsyncApp,
     ) -> Result<proto::ListRemoteDirectoryResponse> {
-        let expanded = shellexpand::tilde(&envelope.payload.path).to_string();
+        let path = envelope
+            .payload
+            .path
+            .clone()
+            .unwrap_or_default()
+            .to_native_string();
+        let expanded = shellexpand::tilde(&path).to_string();
         let fs = cx.read_entity(&this, |this, _| this.fs.clone())?;
 
         let mut entries = Vec::new();
@@ -578,7 +590,13 @@ impl HeadlessProject {
         cx: AsyncApp,
     ) -> Result<proto::GetPathMetadataResponse> {
         let fs = cx.read_entity(&this, |this, _| this.fs.clone())?;
-        let expanded = shellexpand::tilde(&envelope.payload.path).to_string();
+        let path = envelope
+            .payload
+            .path
+            .clone()
+            .unwrap_or_default()
+            .to_native_string();
+        let expanded = shellexpand::tilde(&path).to_string();
 
         let metadata = fs.metadata(&PathBuf::from(expanded.clone())).await?;
         let is_dir = metadata.map(|metadata| metadata.is_dir).unwrap_or(false);
@@ -586,7 +604,7 @@ impl HeadlessProject {
         Ok(proto::GetPathMetadataResponse {
             exists: metadata.is_some(),
             is_dir,
-            path: expanded,
+            path: Some(proto::CrossPlatformPath::from(expanded)),
         })
     }
 
