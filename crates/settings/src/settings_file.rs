cd485c6443e0da6aa22e82db43ac8d940bb1b7a0
Michael Sloan <michael@zed.dev>
2025-01-28 21:39:06 -0700

    Show settings file errors on startup

--- crates/settings/src/settings_file.rs
+++ crates/settings/src/settings_file.rs
@@ -3,7 +3,6 @@ use fs::Fs;
 use futures::{channel::mpsc, StreamExt};
 use gpui::{App, BackgroundExecutor, ReadGlobal, UpdateGlobal};
 use std::{path::PathBuf, sync::Arc, time::Duration};
-use util::ResultExt;
 
 pub const EMPTY_THEME_NAME: &str = "empty-theme";
 
@@ -73,18 +72,22 @@ pub fn handle_settings_file_changes(
         .block(user_settings_file_rx.next())
         .unwrap();
     SettingsStore::update_global(cx, |store, cx| {
-        store
-            .set_user_settings(&user_settings_content, cx)
-            .log_err();
+        let result = store.set_user_settings(&user_settings_content, cx);
+        if let Err(err) = &result {
+            log::error!("Failed to load user settings: {err}");
+        }
+        settings_changed(result.err(), cx);
     });
     cx.spawn(move |cx| async move {
         while let Some(user_settings_content) = user_settings_file_rx.next().await {
-            let result = cx.update_global(|store: &mut SettingsStore, cx| {
-                let result = store.set_user_settings(&user_settings_content, cx);
-                if let Err(err) = &result {
-                    log::error!("Failed to load user settings: {err}");
-                }
-                settings_changed(result.err(), cx);
+            let result = cx.update(|cx| {
+                SettingsStore::update_global(cx, |store, cx| {
+                    let result = store.set_user_settings(&user_settings_content, cx);
+                    if let Err(err) = &result {
+                        log::error!("Failed to load user settings: {err}");
+                    }
+                    settings_changed(result.err(), cx);
+                });
                 cx.refresh_windows();
             });
             if result.is_err() {
