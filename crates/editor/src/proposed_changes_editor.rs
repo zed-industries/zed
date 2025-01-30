399e2c1ed3482c6f325100594d6ed56eb6f91693
Max Brunsfeld <maxbrunsfeld@gmail.com>
2025-01-30 13:42:56 -0800

    Revert "project: Fine-grained language server management" (#23977)
    Reverts zed-industries/zed#23805

--- crates/editor/src/proposed_changes_editor.rs
+++ crates/editor/src/proposed_changes_editor.rs
@@ -472,7 +472,7 @@ impl SemanticsProvider for BranchBufferSemanticsProvider {
         self.0.resolve_inlay_hint(hint, buffer, server_id, cx)
     }
 
-    fn supports_inlay_hints(&self, buffer: &Entity<Buffer>, cx: &mut App) -> bool {
+    fn supports_inlay_hints(&self, buffer: &Entity<Buffer>, cx: &App) -> bool {
         if let Some(buffer) = self.to_base(&buffer, &[], cx) {
             self.0.supports_inlay_hints(&buffer, cx)
         } else {
