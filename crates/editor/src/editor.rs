zed-industries/zed

commit be4c3cfbd29db83888382414d88f0b0fc4bcdb0c

workspace: Make "New Window" bring app to foreground (#24015)
Closes #ISSUE

Release Notes:

- "New Window" action will now bring App to foreground.

crates/workspace/src/workspace.rs
@@ -1284,7 +1284,10 @@ impl Workspace {
                 .unwrap_or_default();
 
             window
-                .update(&mut cx, |_, window, _| window.activate_window())
+                .update(&mut cx, |_, window, cx| {
+                    window.activate_window();
+                    cx.activate(true);
+                })
                 .log_err();
             Ok((window, opened_items))
         })

commit 84bcdb70051c221798c6b036d1ea5682a27caf4e

workspace: Respect minimized state when deserializing workspaces
Fixes a regression from #24015 pointed out in #24093

crates/editor/src/editor.rs
@@ -332,6 +332,7 @@ pub fn init(cx: &mut App) {
                 app_state,
                 cx,
                 |workspace, window, cx| {
+                    cx.activate(true);
                     Editor::new_file(workspace, &Default::default(), window, cx)
                 },
             )

crates/workspace/src/workspace.rs
@@ -1284,10 +1284,7 @@ impl Workspace {
                 .unwrap_or_default();
 
             window
-                .update(&mut cx, |_, window, cx| {
-                    window.activate_window();
-                    cx.activate(true);
-                })
+                .update(&mut cx, |_, window, _| window.activate_window())
                 .log_err();
             Ok((window, opened_items))
         })

crates/zed/src/zed.rs
@@ -796,6 +796,7 @@ fn register_actions(
                         app_state,
                         cx,
                         |workspace, window, cx| {
+                            cx.activate(true);
                             Editor::new_file(workspace, &Default::default(), window, cx)
                         },
                     )