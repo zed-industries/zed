aa619a06ea1b99b1383d77e55b43322f49b2fb7c
Junkui Zhang <364772080@qq.com>
2025-02-04 22:16:43 +0800

    don not use DevDrive

--- script/setup-dev-driver.ps1
+++ script/setup-dev-driver.ps1
@@ -1,29 +1,18 @@
 # Configures a drive for testing in CI.
+# todo(windows)
+# The current version of the Windows runner is 10.0.20348 which does not support DevDrive option.
+# Ref: https://learn.microsoft.com/en-us/windows/dev-drive/
 
 $Volume = New-VHD -Path C:/zed_dev_drive.vhdx -SizeBytes 30GB |
                     Mount-VHD -Passthru |
                     Initialize-Disk -Passthru |
                     New-Partition -AssignDriveLetter -UseMaximumSize |
-                    Format-Volume -DevDrive -Confirm:$false -Force
+                    Format-Volume -FileSystem ReFS -Confirm:$false -Force
 
 $Drive = "$($Volume.DriveLetter):"
 
-# Set the drive as trusted
-# See https://learn.microsoft.com/en-us/windows/dev-drive/#how-do-i-designate-a-dev-drive-as-trusted
-fsutil devdrv trust $Drive
-
-# Disable antivirus filtering on dev drives
-# See https://learn.microsoft.com/en-us/windows/dev-drive/#how-do-i-configure-additional-filters-on-dev-drive
-fsutil devdrv enable /disallowAv
-
-# Remount so the changes take effect
-Dismount-VHD -Path C:/zed_dev_drive.vhdx
-Mount-VHD -Path C:/zed_dev_drive.vhdx
-
 # Show some debug information
 Write-Output $Volume
-fsutil devdrv query $Drive
-
 Write-Output "Using Dev Drive at $Volume"
     
 # Move Cargo to the dev drive
