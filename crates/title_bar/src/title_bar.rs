e093d067c709673f5853b16672f0460fe488a6ae
Beniamin Zagan <beniamin@zagan.be>
2025-02-07 23:10:20 +0100

    Added icons in the titlebar
    Added the icons option in the title bar between Themes and Extension.
    
    Also removed the ... after Themes.
    Seemed unnecessary.

--- crates/title_bar/src/title_bar.rs
+++ crates/title_bar/src/title_bar.rs
@@ -670,8 +670,12 @@ impl TitleBar {
                         .action("Settings", zed_actions::OpenSettings.boxed_clone())
                         .action("Key Bindings", Box::new(zed_actions::OpenKeymap))
                         .action(
-                            "Themes…",
+                            "Themes",
                             zed_actions::theme_selector::Toggle::default().boxed_clone(),
+                        )
+                        .action(
+                            "Icons",
+                            zed_actions::icon_theme_selector::Toggle::default().boxed_clone(),
                         )
                         .action("Extensions", zed_actions::Extensions.boxed_clone())
                         .separator()
@@ -713,8 +717,12 @@ impl TitleBar {
                         menu.action("Settings", zed_actions::OpenSettings.boxed_clone())
                             .action("Key Bindings", Box::new(zed_actions::OpenKeymap))
                             .action(
-                                "Themes…",
+                                "Themes",
                                 zed_actions::theme_selector::Toggle::default().boxed_clone(),
+                            )
+                            .action(
+                                "Icons",
+                                zed_actions::icon_theme_selector::Toggle::default().boxed_clone(),
                             )
                             .action("Extensions", zed_actions::Extensions.boxed_clone())
                             .separator()
