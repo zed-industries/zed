7f74d9ea638f68d9d67f2a4a8d5cf7b4f6791f31
5brian <brian.tan88@gmail.com>
2025-02-09 23:53:29 -0500

    remove leftover

--- crates/vim/src/normal/change.rs
+++ crates/vim/src/normal/change.rs
@@ -109,7 +109,6 @@ impl Vim {
                 if objects_found {
                     vim.copy_selections_content(editor, false, cx);
                     editor.insert("", window, cx);
-
                     editor.refresh_inline_completion(true, false, window, cx);
                 }
             });
