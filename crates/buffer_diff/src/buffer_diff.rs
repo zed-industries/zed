zed-industries/zed

commit 8f75fe25e5150ef1156436a19b1993acc27a4994

Add staged status information to diff hunks (#24475)
Release Notes:

- Render unstaged hunks in the project diff editor with a slashed
background

---------

Co-authored-by: maxbrunsfeld <max@zed.dev>
Co-authored-by: Max Brunsfeld <maxbrunsfeld@gmail.com>

crates/project/src/buffer_store.rs
@@ -6,9 +6,9 @@ use crate::{
 };
 use ::git::{parse_git_remote_url, BuildPermalinkParams, GitHostingProviderRegistry};
 use anyhow::{anyhow, bail, Context as _, Result};
+use buffer_diff::{BufferDiff, BufferDiffEvent};
 use client::Client;
 use collections::{hash_map, HashMap, HashSet};
-use diff::{BufferDiff, BufferDiffEvent, BufferDiffSnapshot};
 use fs::Fs;
 use futures::{channel::oneshot, future::Shared, Future, FutureExt as _, StreamExt};
 use git::{blame::Blame, repository::RepoPath};
@@ -207,72 +207,74 @@ impl BufferDiffState {
             _ => false,
         };
         self.recalculate_diff_task = Some(cx.spawn(|this, mut cx| async move {
+            let mut unstaged_changed_range = None;
             if let Some(unstaged_diff) = &unstaged_diff {
-                let snapshot = if index_changed || language_changed {
-                    cx.update(|cx| {
-                        BufferDiffSnapshot::build(
-                            buffer.clone(),
-                            index,
-                            language.clone(),
-                            language_registry.clone(),
-                            cx,
-                        )
-                    })?
-                    .await
-                } else {
-                    unstaged_diff
-                        .read_with(&cx, |changes, cx| {
-                            BufferDiffSnapshot::build_with_base_buffer(
-                                buffer.clone(),
-                                index,
-                                changes.snapshot.base_text.clone(),
-                                cx,
-                            )
-                        })?
-                        .await
-                };
+                unstaged_changed_range = BufferDiff::update_diff(
+                    unstaged_diff.clone(),
+                    buffer.clone(),
+                    index,
+                    index_changed,
+                    language_changed,
+                    language.clone(),
+                    language_registry.clone(),
+                    &mut cx,
+                )
+                .await?;
 
-                unstaged_diff.update(&mut cx, |unstaged_diff, cx| {
-                    unstaged_diff.set_state(snapshot, &buffer, cx);
+                unstaged_diff.update(&mut cx, |_, cx| {
                     if language_changed {
                         cx.emit(BufferDiffEvent::LanguageChanged);
+                    }
+                    if let Some(changed_range) = unstaged_changed_range.clone() {
+                        cx.emit(BufferDiffEvent::DiffChanged {
+                            changed_range: Some(changed_range),
+                        })
                     }
                 })?;
             }
 
             if let Some(uncommitted_diff) = &uncommitted_diff {
-                let snapshot =
+                let uncommitted_changed_range =
                     if let (Some(unstaged_diff), true) = (&unstaged_diff, index_matches_head) {
-                        unstaged_diff.read_with(&cx, |diff, _| diff.snapshot.clone())?
-                    } else if head_changed || language_changed {
-                        cx.update(|cx| {
-                            BufferDiffSnapshot::build(
-                                buffer.clone(),
-                                head,
-                                language.clone(),
-                                language_registry.clone(),
-                                cx,
-                            )
+                        uncommitted_diff.update(&mut cx, |uncommitted_diff, cx| {
+                            uncommitted_diff.update_diff_from(&buffer, unstaged_diff, cx)
                         })?
-                        .await
                     } else {
-                        uncommitted_diff
-                            .read_with(&cx, |changes, cx| {
-                                BufferDiffSnapshot::build_with_base_buffer(
-                                    buffer.clone(),
-                                    head,
-                                    changes.snapshot.base_text.clone(),
-                                    cx,
-                                )
-                            })?
-                            .await
+                        BufferDiff::update_diff(
+                            uncommitted_diff.clone(),
+                            buffer.clone(),
+                            head,
+                            head_changed,
+                            language_changed,
+                            language.clone(),
+                            language_registry.clone(),
+                            &mut cx,
+                        )
+                        .await?
                     };
 
-                uncommitted_diff.update(&mut cx, |diff, cx| {
-                    diff.set_state(snapshot, &buffer, cx);
+                uncommitted_diff.update(&mut cx, |uncommitted_diff, cx| {
                     if language_changed {
                         cx.emit(BufferDiffEvent::LanguageChanged);
                     }
+                    let changed_range = match (unstaged_changed_range, uncommitted_changed_range) {
+                        (None, None) => None,
+                        (Some(unstaged_range), None) => {
+                            uncommitted_diff.range_to_hunk_range(unstaged_range, &buffer, cx)
+                        }
+                        (None, Some(uncommitted_range)) => Some(uncommitted_range),
+                        (Some(unstaged_range), Some(uncommitted_range)) => maybe!({
+                            let expanded_range = uncommitted_diff.range_to_hunk_range(
+                                unstaged_range,
+                                &buffer,
+                                cx,
+                            )?;
+                            let start = expanded_range.start.min(&uncommitted_range.start, &buffer);
+                            let end = expanded_range.end.max(&uncommitted_range.end, &buffer);
+                            Some(start..end)
+                        }),
+                    };
+                    cx.emit(BufferDiffEvent::DiffChanged { changed_range });
                 })?;
             }
 
@@ -280,6 +282,7 @@ impl BufferDiffState {
                 this.update(&mut cx, |this, _| {
                     this.index_changed = false;
                     this.head_changed = false;
+                    this.language_changed = false;
                     for tx in this.diff_updated_futures.drain(..) {
                         tx.send(()).ok();
                     }
@@ -1478,29 +1481,19 @@ impl BufferStore {
                     diff_state.language = language;
                     diff_state.language_registry = language_registry;
 
-                    let diff = cx.new(|_| BufferDiff {
-                        buffer_id,
-                        snapshot: BufferDiffSnapshot::new(&text_snapshot),
-                        unstaged_diff: None,
-                    });
+                    let diff = cx.new(|_| BufferDiff::new(&text_snapshot));
                     match kind {
                         DiffKind::Unstaged => diff_state.unstaged_diff = Some(diff.downgrade()),
                         DiffKind::Uncommitted => {
                             let unstaged_diff = if let Some(diff) = diff_state.unstaged_diff() {
                                 diff
                             } else {
-                                let unstaged_diff = cx.new(|_| BufferDiff {
-                                    buffer_id,
-                                    snapshot: BufferDiffSnapshot::new(&text_snapshot),
-                                    unstaged_diff: None,
-                                });
+                                let unstaged_diff = cx.new(|_| BufferDiff::new(&text_snapshot));
                                 diff_state.unstaged_diff = Some(unstaged_diff.downgrade());
                                 unstaged_diff
                             };
 
-                            diff.update(cx, |diff, _| {
-                                diff.unstaged_diff = Some(unstaged_diff);
-                            });
+                            diff.update(cx, |diff, _| diff.set_secondary_diff(unstaged_diff));
                             diff_state.uncommitted_diff = Some(diff.downgrade())
                         }
                     };
@@ -2397,9 +2390,8 @@ impl BufferStore {
                 shared.diff = Some(diff.clone());
             }
         })?;
-        let staged_text = diff.read_with(&cx, |diff, _| {
-            diff.snapshot.base_text.as_ref().map(|buffer| buffer.text())
-        })?;
+        let staged_text =
+            diff.read_with(&cx, |diff, _| diff.base_text().map(|buffer| buffer.text()))?;
         Ok(proto::OpenUnstagedDiffResponse { staged_text })
     }
 
@@ -2430,14 +2422,13 @@ impl BufferStore {
             use proto::open_uncommitted_diff_response::Mode;
 
             let staged_buffer = diff
-                .unstaged_diff
-                .as_ref()
-                .and_then(|diff| diff.read(cx).snapshot.base_text.as_ref());
+                .secondary_diff()
+                .and_then(|diff| diff.read(cx).base_text());
 
             let mode;
             let staged_text;
             let committed_text;
-            if let Some(committed_buffer) = &diff.snapshot.base_text {
+            if let Some(committed_buffer) = diff.base_text() {
                 committed_text = Some(committed_buffer.text());
                 if let Some(staged_buffer) = staged_buffer {
                     if staged_buffer.remote_id() == committed_buffer.remote_id() {

Cargo.toml
@@ -34,7 +34,7 @@ members = [
     "crates/db",
     "crates/deepseek",
     "crates/diagnostics",
-    "crates/diff",
+    "crates/buffer_diff",
     "crates/docs_preprocessor",
     "crates/editor",
     "crates/evals",
@@ -235,7 +235,7 @@ copilot = { path = "crates/copilot" }
 db = { path = "crates/db" }
 deepseek = { path = "crates/deepseek" }
 diagnostics = { path = "crates/diagnostics" }
-diff = { path = "crates/diff" }
+buffer_diff = { path = "crates/buffer_diff" }
 editor = { path = "crates/editor" }
 extension = { path = "crates/extension" }
 extension_host = { path = "crates/extension_host" }

crates/buffer_diff/Cargo.toml
@@ -1,5 +1,5 @@
 [package]
-name = "diff"
+name = "buffer_diff"
 version = "0.1.0"
 edition.workspace = true
 publish.workspace = true
@@ -9,17 +9,17 @@ license = "GPL-3.0-or-later"
 workspace = true
 
 [lib]
-path = "src/diff.rs"
+path = "src/buffer_diff.rs"
 
 [features]
 test-support = []
 
 [dependencies]
+anyhow.workspace = true
 futures.workspace = true
 git2.workspace = true
 gpui.workspace = true
 language.workspace = true
-log.workspace = true
 rope.workspace = true
 sum_tree.workspace = true
 text.workspace = true
@@ -29,4 +29,5 @@ util.workspace = true
 pretty_assertions.workspace = true
 serde_json.workspace = true
 text = { workspace = true, features = ["test-support"] }
+gpui = { workspace = true, features = ["test-support"] }
 unindent.workspace = true

crates/collab/Cargo.toml
@@ -33,7 +33,7 @@ clock.workspace = true
 collections.workspace = true
 dashmap.workspace = true
 derive_more.workspace = true
-diff.workspace = true
+buffer_diff.workspace = true
 envy = "0.4.2"
 futures.workspace = true
 google_ai.workspace = true

crates/collab/src/tests/integration_tests.rs
@@ -8,6 +8,7 @@ use crate::{
 use anyhow::{anyhow, Result};
 use assistant_context_editor::ContextStore;
 use assistant_slash_command::SlashCommandWorkingSet;
+use buffer_diff::{assert_hunks, DiffHunkSecondaryStatus, DiffHunkStatus};
 use call::{room, ActiveCall, ParticipantLocation, Room};
 use client::{User, RECEIVE_TIMEOUT};
 use collections::{HashMap, HashSet};
@@ -2613,11 +2614,11 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(staged_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &diff.base_text_string().unwrap(),
-            &[(1..2, "", "two\n")],
+            &[(1..2, "", "two\n", DiffHunkStatus::added())],
         );
     });
 
@@ -2641,11 +2642,11 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(staged_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &diff.base_text_string().unwrap(),
-            &[(1..2, "", "two\n")],
+            &[(1..2, "", "two\n", DiffHunkStatus::added())],
         );
     });
 
@@ -2663,11 +2664,16 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(committed_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &diff.base_text_string().unwrap(),
-            &[(1..2, "TWO\n", "two\n")],
+            &[(
+                1..2,
+                "TWO\n",
+                "two\n",
+                DiffHunkStatus::Modified(DiffHunkSecondaryStatus::HasSecondaryHunk),
+            )],
         );
     });
 
@@ -2689,11 +2695,11 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(new_staged_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &diff.base_text_string().unwrap(),
-            &[(2..3, "", "three\n")],
+            &[(2..3, "", "three\n", DiffHunkStatus::added())],
         );
     });
 
@@ -2703,11 +2709,11 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(new_staged_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &diff.base_text_string().unwrap(),
-            &[(2..3, "", "three\n")],
+            &[(2..3, "", "three\n", DiffHunkStatus::added())],
         );
     });
 
@@ -2717,11 +2723,16 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(new_committed_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &diff.base_text_string().unwrap(),
-            &[(1..2, "TWO_HUNDRED\n", "two\n")],
+            &[(
+                1..2,
+                "TWO_HUNDRED\n",
+                "two\n",
+                DiffHunkStatus::Modified(DiffHunkSecondaryStatus::OverlapsWithSecondaryHunk),
+            )],
         );
     });
 
@@ -2763,11 +2774,11 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(staged_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &diff.base_text_string().unwrap(),
-            &[(1..2, "", "two\n")],
+            &[(1..2, "", "two\n", DiffHunkStatus::added())],
         );
     });
 
@@ -2790,11 +2801,11 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(staged_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &staged_text,
-            &[(1..2, "", "two\n")],
+            &[(1..2, "", "two\n", DiffHunkStatus::added())],
         );
     });
 
@@ -2812,11 +2823,11 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(new_staged_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &new_staged_text,
-            &[(2..3, "", "three\n")],
+            &[(2..3, "", "three\n", DiffHunkStatus::added())],
         );
     });
 
@@ -2826,11 +2837,11 @@ async fn test_git_diff_base_change(
             diff.base_text_string().as_deref(),
             Some(new_staged_text.as_str())
         );
-        diff::assert_hunks(
-            diff.snapshot.hunks_in_row_range(0..4, buffer),
+        assert_hunks(
+            diff.hunks_in_row_range(0..4, buffer, cx),
             buffer,
             &new_staged_text,
-            &[(2..3, "", "three\n")],
+            &[(2..3, "", "three\n", DiffHunkStatus::added())],
         );
     });
 }

commit 6e7416eb00e3517a65f7b113a9f762d72561b984

Fix stale hunks after commit (#24663)
Fixes a regression introduced in #24475.

Release Notes:

- N/A

crates/buffer_diff/src/buffer_diff.rs
@@ -587,18 +587,16 @@ impl BufferDiff {
         range: Range<Anchor>,
         buffer: &text::BufferSnapshot,
         cx: &App,
-    ) -> Option<Range<Anchor>> {
+    ) -> Range<Anchor> {
         let start = self
             .hunks_intersecting_range(range.clone(), &buffer, cx)
-            .next()?
-            .buffer_range
-            .start;
+            .next()
+            .map_or(Anchor::MIN, |hunk| hunk.buffer_range.start);
         let end = self
             .hunks_intersecting_range_rev(range.clone(), &buffer)
-            .next()?
-            .buffer_range
-            .end;
-        Some(start..end)
+            .next()
+            .map_or(Anchor::MAX, |hunk| hunk.buffer_range.end);
+        start..end
     }
 
     #[allow(clippy::too_many_arguments)]

crates/project/src/buffer_store.rs
@@ -260,15 +260,12 @@ impl BufferDiffState {
                     let changed_range = match (unstaged_changed_range, uncommitted_changed_range) {
                         (None, None) => None,
                         (Some(unstaged_range), None) => {
-                            uncommitted_diff.range_to_hunk_range(unstaged_range, &buffer, cx)
+                            Some(uncommitted_diff.range_to_hunk_range(unstaged_range, &buffer, cx))
                         }
                         (None, Some(uncommitted_range)) => Some(uncommitted_range),
                         (Some(unstaged_range), Some(uncommitted_range)) => maybe!({
-                            let expanded_range = uncommitted_diff.range_to_hunk_range(
-                                unstaged_range,
-                                &buffer,
-                                cx,
-                            )?;
+                            let expanded_range =
+                                uncommitted_diff.range_to_hunk_range(unstaged_range, &buffer, cx);
                             let start = expanded_range.start.min(&uncommitted_range.start, &buffer);
                             let end = expanded_range.end.max(&uncommitted_range.end, &buffer);
                             Some(start..end)