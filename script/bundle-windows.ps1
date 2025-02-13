dc7f46e38d25bc88e6df64677077172b54c505e2
Junkui Zhang <364772080@qq.com>
2025-02-13 21:15:46 +0800

    update comment

--- script/bundle-windows.ps1
+++ script/bundle-windows.ps1
@@ -32,6 +32,9 @@ switch ($channel) {
     }
 }
 
+# Windows runner 2022 default has iscc in PATH, https://github.com/actions/runner-images/blob/main/images/windows/Windows2022-Readme.md
+# Currently, we are using Windows 2022 runner.
+# Windows runner 2025 doesn't have iscc in PATH for now, https://github.com/actions/runner-images/issues/11228
 $innoSetupPath = "iscc.exe"
 
 $definitions = @{
