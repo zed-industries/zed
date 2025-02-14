756150b8aa2e351a31b721b883c868a406b8473c
Henrique Ferreiro <hferreiro@igalia.com>
2025-02-14 22:56:29 +0100

    Sort providers.rs and update commit_author_avatar_url argument type

--- crates/git_hosting_providers/src/providers.rs
+++ crates/git_hosting_providers/src/providers.rs
@@ -1,15 +1,15 @@
 mod bitbucket;
+mod chromium;
 mod codeberg;
 mod gitee;
 mod github;
 mod gitlab;
 mod sourcehut;
-mod chromium;
 
 pub use bitbucket::*;
+pub use chromium::*;
 pub use codeberg::*;
 pub use gitee::*;
 pub use github::*;
 pub use gitlab::*;
 pub use sourcehut::*;
-pub use chromium::*;
