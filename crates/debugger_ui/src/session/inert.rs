1d5fc3771f5deee6f2e428b1208355a73a22b793
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-02-18 13:15:44 +0100

    Change dap name on click in inert state

--- crates/debugger_ui/src/session/inert.rs
+++ crates/debugger_ui/src/session/inert.rs
@@ -1,46 +1,66 @@
 use gpui::{App, FocusHandle, Focusable};
 use ui::{
     div, h_flex, v_flex, Button, ButtonCommon, ButtonStyle, Context, ContextMenu, DropdownMenu,
-    Element, InteractiveElement, ParentElement, Render, Styled,
+    Element, InteractiveElement, ParentElement, Render, SharedString, Styled, Window,
 };
 
 pub(super) struct InertState {
     focus_handle: FocusHandle,
+    selected_debugger: Option<SharedString>,
 }
 
 impl InertState {
     pub(super) fn new(cx: &mut Context<Self>) -> Self {
         Self {
             focus_handle: cx.focus_handle(),
+            selected_debugger: None,
         }
     }
 }
 impl Focusable for InertState {
-    fn focus_handle(&self, cx: &App) -> FocusHandle {
+    fn focus_handle(&self, _cx: &App) -> FocusHandle {
         self.focus_handle.clone()
     }
 }
 
+static SELECT_DEBUGGER_LABEL: SharedString = SharedString::new_static("Select Debugger");
 impl Render for InertState {
     fn render(
         &mut self,
         window: &mut ui::Window,
         cx: &mut ui::Context<'_, Self>,
     ) -> impl ui::IntoElement {
+        let weak = cx.weak_entity();
         v_flex()
             .track_focus(&self.focus_handle)
             .size_full()
             .gap_1()
             .p_1()
-            .child(h_flex().child(DropdownMenu::new(
-                "dap-adapter-picker",
-                "Select Debug Adapter",
-                ContextMenu::build(window, cx, |this, _, _| {
-                    this.entry("GDB", None, |_, _| {})
-                        .entry("Delve", None, |_, _| {})
-                        .entry("LLDB", None, |_, _| {})
-                }),
-            )))
+            .child(
+                h_flex().child(DropdownMenu::new(
+                    "dap-adapter-picker",
+                    self.selected_debugger
+                        .as_ref()
+                        .unwrap_or_else(|| &SELECT_DEBUGGER_LABEL)
+                        .clone(),
+                    ContextMenu::build(window, cx, move |this, _, _| {
+                        let setter_for_name = |name: &'static str| {
+                            let weak = weak.clone();
+                            move |_: &mut Window, cx: &mut App| {
+                                let name = name;
+                                (&weak)
+                                    .update(cx, move |this, _| {
+                                        this.selected_debugger = Some(name.into());
+                                    })
+                                    .ok();
+                            }
+                        };
+                        this.entry("GDB", None, setter_for_name("GDB"))
+                            .entry("Delve", None, setter_for_name("Delve"))
+                            .entry("LLDB", None, setter_for_name("LLDB"))
+                    }),
+                )),
+            )
             .child(
                 h_flex()
                     .gap_1()
