b80c385420d9e8348dd88b4536f22b916d984a69
Angelk90 <angelo.k90@hotmail.it>
2025-02-20 23:37:48 +0100

    Fix multiple worktrees open

--- crates/project_panel/src/project_panel.rs
+++ crates/project_panel/src/project_panel.rs
@@ -2579,7 +2579,14 @@ impl ProjectPanel {
         let old_ancestors = std::mem::take(&mut self.ancestors);
         self.visible_entries.clear();
         let mut max_width_item = None;
-        for worktree in project.visible_worktrees(cx) {
+
+        let visible_worktrees: Vec<_> = project.visible_worktrees(cx).collect();
+        // If there is only one worktree, we respect the hide_root setting;
+        // if there is more than one, we force hide_root to false (to show the root of each worktree)
+        let hide_root_setting = ProjectPanelSettings::get_global(cx).hide_root;
+        let hide_root = hide_root_setting && visible_worktrees.len() == 1;
+
+        for worktree in visible_worktrees {
             let snapshot = worktree.read(cx).snapshot();
             let worktree_id = snapshot.id();
 
@@ -2613,7 +2620,6 @@ impl ProjectPanel {
             let mut entry_iter = snapshot.entries(true, 0).with_git_statuses();
             let mut auto_folded_ancestors = vec![];
             while let Some(entry) = entry_iter.entry() {
-                let hide_root = ProjectPanelSettings::get_global(cx).hide_root;
                 if hide_root && Some(entry.entry) == worktree.read(cx).root_entry() {
                     entry_iter.advance();
                     continue;
