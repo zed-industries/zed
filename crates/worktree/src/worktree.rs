6383dae7d8eb6512aa54444119250c0673cbc18b
Cole Miller <cole@zed.dev>
2025-02-20 10:59:05 -0500

    git: Take only the first line of MERGE_MSG

--- crates/worktree/src/worktree.rs
+++ crates/worktree/src/worktree.rs
@@ -5491,7 +5491,8 @@ impl BackgroundScanner {
                         entry.merge_message = std::fs::read_to_string(
                             local_repository.dot_git_dir_abs_path.join("MERGE_MSG"),
                         )
-                        .ok();
+                        .ok()
+                        .map(|merge_msg| merge_msg.lines().next().unwrap_or("").to_owned());
                         entry.status_scan_id += 1;
                     },
                 );
