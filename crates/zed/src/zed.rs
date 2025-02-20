78a80024155e4b6a8e449465d093a76ad8f2ffaf
0x2CA <2478557459@qq.com>
2025-02-20 23:47:51 +0800

    keymap: Fix `shift-` modifier symbol (#25238)
    Closes #25230
    
    ```
    The shift- modifier can only be used in combination with a letter to indicate the uppercase version. For example shift-g matches typing G. Although on many keyboards shift is used to type punctuation characters like (, the keypress is not considered to be modified and so shift-( does not match.
    ```
    
    [Document](https://zed.dev/docs/key-bindings#keybinding-syntax)
    
    Release Notes:
    
    - Fixed Keymap use `shift-` modifier symbol

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -3964,7 +3964,7 @@ mod tests {
         assert_key_bindings_for(
             workspace.into(),
             cx,
-            vec![("backspace", &B), ("[", &ActivatePrevItem)],
+            vec![("backspace", &B), ("{", &ActivatePrevItem)],
             line!(),
         );
     }
