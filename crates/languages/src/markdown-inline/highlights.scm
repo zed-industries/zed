9772a74040b123e2e503057b29fd636bff10bf95
chbk <chbk.git@gmail.com>
2025-02-21 13:33:10 +0100

    Improve Markdown syntax highlighting

--- crates/languages/src/markdown-inline/highlights.scm
+++ crates/languages/src/markdown-inline/highlights.scm
@@ -1,6 +1,20 @@
-(emphasis) @emphasis
-(strong_emphasis) @emphasis.strong
-(code_span) @text.literal
-(link_text) @link_text
-(link_label) @link_text
-(link_destination) @link_uri
+(emphasis) @markup.emphasis
+(strong_emphasis) @markup.emphasis.strong
+(code_span) @markup.raw
+(strikethrough) @markup.strikethrough
+
+[
+  (inline_link)
+  (shortcut_link)
+  (collapsed_reference_link)
+  (full_reference_link)
+  (image)
+] @markup.link
+
+(inline_link ["(" ")"] @markup.link.url)
+(image ["(" ")"] @markup.link.url)
+[
+  (link_destination)
+  (uri_autolink)
+  (email_autolink)
+] @markup.link.url
--- crates/languages/src/markdown/highlights.scm
+++ crates/languages/src/markdown/highlights.scm
@@ -1,7 +1,15 @@
 [
+  (paragraph)
+  (indented_code_block)
+  (pipe_table)
+] @text
+
+[
   (atx_heading)
   (setext_heading)
-] @title
+  (thematic_break)
+] @markup.heading
+(setext_heading (paragraph) @markup.heading)
 
 [
   (list_marker_plus)
@@ -9,8 +17,18 @@
   (list_marker_star)
   (list_marker_dot)
   (list_marker_parenthesis)
-] @punctuation.list_marker
+  (block_quote_marker)
+] @punctuation.markup
+
+(pipe_table_header "|" @punctuation.markup)
+(pipe_table_row "|" @punctuation.markup)
+(pipe_table_delimiter_row "|" @punctuation.markup)
+(pipe_table_delimiter_cell "-" @punctuation.markup)
 
 (fenced_code_block
   (info_string
-    (language) @text.literal))
+    (language) @punctuation.markup.embedded))
+(fenced_code_block_delimiter) @punctuation.markup.embedded
+
+(link_reference_definition) @markup.link
+(link_destination) @markup.link.url
