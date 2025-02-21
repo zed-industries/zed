f2ac3ffacbb1b7eb37388c861abfb5112ccb4c65
Junkui Zhang <364772080@qq.com>
2025-02-21 21:25:39 +0800

    windows

---
+++ crates/only_instance/src/windows.rs
@@ -0,0 +1,31 @@
+use release_channel::ReleaseChannel;
+use windows::{
+    core::HSTRING,
+    Win32::{
+        Foundation::{GetLastError, ERROR_ALREADY_EXISTS},
+        System::Threading::CreateEventW,
+    },
+};
+
+pub fn ensure_unique_instance() -> bool {
+    unsafe {
+        CreateEventW(
+            None,
+            false,
+            false,
+            &HSTRING::from(retrieve_app_instance_event_identifier()),
+        )
+        .expect("Unable to create instance sync event")
+    };
+    let last_err = unsafe { GetLastError() };
+    last_err != ERROR_ALREADY_EXISTS
+}
+
+fn retrieve_app_instance_event_identifier() -> &'static str {
+    match *release_channel::RELEASE_CHANNEL {
+        ReleaseChannel::Dev => "Local\\Zed-Editor-Dev-Instance-Event",
+        ReleaseChannel::Nightly => "Local\\Zed-Editor-Nightly-Instance-Event",
+        ReleaseChannel::Preview => "Local\\Zed-Editor-Preview-Instance-Event",
+        ReleaseChannel::Stable => "Local\\Zed-Editor-Stable-Instance-Event",
+    }
+}
