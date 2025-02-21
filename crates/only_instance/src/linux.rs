020291fcf5101c2f577c1fe478f7e22282e2f497
Junkui Zhang <364772080@qq.com>
2025-02-21 22:07:33 +0800

    macos and linux

---
+++ crates/only_instance/src/linux.rs
@@ -0,0 +1,22 @@
+use release_channel::RELEASE_CHANNEL_NAME;
+
+use std::io::Result;
+use std::os::unix::net::UnixDatagram;
+use std::path::Path;
+
+pub fn ensure_unique_instance() -> Result<UnixDatagram> {
+    let sock_path = paths::support_dir().join(format!("zed-{}.sock", *RELEASE_CHANNEL_NAME));
+    // remove the socket if the process listening on it has died
+    if let Err(e) = UnixDatagram::unbound()?.connect(&sock_path) {
+        if e.kind() == std::io::ErrorKind::ConnectionRefused {
+            std::fs::remove_file(&sock_path)?;
+        }
+    }
+    UnixDatagram::bind(&sock_path)
+}
+
+pub fn ensure_unique_instance_cli() -> Result<UnixDatagram> {
+    let sock_path = paths::support_dir().join(format!("zed-{}.sock", *RELEASE_CHANNEL_NAME));
+    let sock = UnixDatagram::unbound()?;
+    sock.connect(&sock_path).and(Ok(sock))
+}
--- crates/only_instance/src/only_instance.rs
+++ crates/only_instance/src/only_instance.rs
@@ -1,7 +1,13 @@
+#[cfg(any(target_os = "linux", target_os = "freebsd"))]
 pub mod linux;
+#[cfg(target_os = "macos")]
 pub mod macos;
+#[cfg(target_os = "windows")]
 pub mod windows;
 
+#[cfg(any(target_os = "linux", target_os = "freebsd"))]
 pub use linux::*;
+#[cfg(target_os = "macos")]
 pub use macos::*;
+#[cfg(target_os = "windows")]
 pub use windows::*;
