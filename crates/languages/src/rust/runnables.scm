dff47a843695d03160680502e6d94634e376698e
Coenen Benjamin <benjamin.coenen@hotmail.com>
2025-02-21 10:57:09 +0100

    rust: Add support for doctest runnables (#24806)
    Screenshot:
    
    
    ![image](https://github.com/user-attachments/assets/0ac88029-76c1-4135-bef2-373636e3587d)
    
    I would be happy to add tests if you point me to the right place to do
    it please.
    
    Release Notes:
    
    - Added support for doc test in tasks for Rust
    
    ---------
    
    Signed-off-by: Benjamin <5719034+bnjjj@users.noreply.github.com>

--- crates/languages/src/rust/runnables.scm
+++ crates/languages/src/rust/runnables.scm
@@ -1,4 +1,4 @@
-
+; Rust mod test
 (
     (mod_item
         name: (_) @run
@@ -7,6 +7,7 @@
     (#set! tag rust-mod-test)
 )
 
+; Rust test
 (
     (
         (attribute_item (attribute
@@ -26,6 +27,45 @@
         ) @_end
     )
     (#set! tag rust-test)
+)
+
+; Rust doc test
+(
+    (
+        (line_comment) *
+        (line_comment
+            doc: (_) @_comment_content
+        ) @start
+        (#match? @_comment_content "```")
+        (line_comment) *
+        (line_comment
+            doc: (_) @_end_comment_content
+        ) @_end_code_block
+        (#match? @_end_comment_content "```")
+        .
+        (attribute_item) *
+        .
+        [(line_comment) (block_comment)] *
+        .
+        [(function_item
+            name: (_) @run
+            body: _
+        ) (function_signature_item
+            name: (_) @run
+        ) (struct_item
+            name: (_) @run
+        ) (enum_item
+            name: (_) @run
+            body: _
+        ) (
+            (attribute_item) ?
+            (macro_definition
+                name: (_) @run)
+        ) (mod_item
+            name: (_) @run
+        )] @_end
+    )
+    (#set! tag rust-doc-test)
 )
 
 ; Rust main function
