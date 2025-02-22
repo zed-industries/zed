3cb4fe923601a5333589198794fd16944d906e1f
brian tan <brian.tan88@gmail.com>
2025-02-22 12:48:30 -0500

    reset index on update

--- crates/file_finder/src/open_path_prompt.rs
+++ crates/file_finder/src/open_path_prompt.rs
@@ -222,6 +222,7 @@ impl PickerDelegate for OpenPathDelegate {
                         *m,
                     )
                 });
+                this.delegate.selected_index = 0;
                 cx.notify();
             })
             .ok();
