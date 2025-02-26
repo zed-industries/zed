zed-industries/zed

commit 6fca1d2b0ba93cdbc3255657a990828a0f22b199

Eliminate GPUI View, ViewContext, and WindowContext types (#22632)
There's still a bit more work to do on this, but this PR is compiling
(with warnings) after eliminating the key types. When the tasks below
are complete, this will be the new narrative for GPUI:

- `Entity<T>` - This replaces `View<T>`/`Model<T>`. It represents a unit
of state, and if `T` implements `Render`, then `Entity<T>` implements
`Element`.
- `&mut App` This replaces `AppContext` and represents the app.
- `&mut Context<T>` This replaces `ModelContext` and derefs to `App`. It
is provided by the framework when updating an entity.
- `&mut Window` Broken out of `&mut WindowContext` which no longer
exists. Every method that once took `&mut WindowContext` now takes `&mut
Window, &mut App` and every method that took `&mut ViewContext<T>` now
takes `&mut Window, &mut Context<T>`

Not pictured here are the two other failed attempts. It's been quite a
month!

Tasks:

- [x] Remove `View`, `ViewContext`, `WindowContext` and thread through
`Window`
- [x] [@cole-miller @mikayla-maki] Redraw window when entities change
- [x] [@cole-miller @mikayla-maki] Get examples and Zed running
- [x] [@cole-miller @mikayla-maki] Fix Zed rendering
- [x] [@mikayla-maki] Fix todo! macros and comments
- [x] Fix a bug where the editor would not be redrawn because of view
caching
- [x] remove publicness window.notify() and replace with
`AppContext::notify`
- [x] remove `observe_new_window_models`, replace with
`observe_new_models` with an optional window
- [x] Fix a bug where the project panel would not be redrawn because of
the wrong refresh() call being used
- [x] Fix the tests
- [x] Fix warnings by eliminating `Window` params or using `_`
- [x] Fix conflicts
- [x] Simplify generic code where possible
- [x] Rename types
- [ ] Update docs

### issues post merge

- [x] Issues switching between normal and insert mode
- [x] Assistant re-rendering failure
- [x] Vim test failures

crates/vim/src/object.rs
@@ -10,12 +10,13 @@ use editor::{
     movement::{self, FindRange},
     Bias, DisplayPoint, Editor,
 };
-use gpui::{actions, impl_actions, ViewContext};
+use gpui::{actions, impl_actions, Window};
 use itertools::Itertools;
 use language::{BufferSnapshot, CharKind, Point, Selection, TextObject, TreeSitterOptions};
 use multi_buffer::MultiBufferRow;
 use schemars::JsonSchema;
 use serde::Deserialize;
+use ui::Context;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Deserialize, JsonSchema)]
 pub enum Object {
@@ -84,84 +85,88 @@ actions!(
     ]
 );
 
-pub fn register(editor: &mut Editor, cx: &mut ViewContext<Vim>) {
+pub fn register(editor: &mut Editor, cx: &mut Context<Vim>) {
     Vim::action(
         editor,
         cx,
-        |vim, &Word { ignore_punctuation }: &Word, cx| {
-            vim.object(Object::Word { ignore_punctuation }, cx)
+        |vim, &Word { ignore_punctuation }: &Word, window, cx| {
+            vim.object(Object::Word { ignore_punctuation }, window, cx)
         },
     );
     Vim::action(
         editor,
         cx,
-        |vim, &Subword { ignore_punctuation }: &Subword, cx| {
-            vim.object(Object::Subword { ignore_punctuation }, cx)
+        |vim, &Subword { ignore_punctuation }: &Subword, window, cx| {
+            vim.object(Object::Subword { ignore_punctuation }, window, cx)
         },
     );
-    Vim::action(editor, cx, |vim, _: &Tag, cx| vim.object(Object::Tag, cx));
-    Vim::action(editor, cx, |vim, _: &Sentence, cx| {
-        vim.object(Object::Sentence, cx)
+    Vim::action(editor, cx, |vim, _: &Tag, window, cx| {
+        vim.object(Object::Tag, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &Paragraph, cx| {
-        vim.object(Object::Paragraph, cx)
+    Vim::action(editor, cx, |vim, _: &Sentence, window, cx| {
+        vim.object(Object::Sentence, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &Quotes, cx| {
-        vim.object(Object::Quotes, cx)
+    Vim::action(editor, cx, |vim, _: &Paragraph, window, cx| {
+        vim.object(Object::Paragraph, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &BackQuotes, cx| {
-        vim.object(Object::BackQuotes, cx)
+    Vim::action(editor, cx, |vim, _: &Quotes, window, cx| {
+        vim.object(Object::Quotes, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &AnyQuotes, cx| {
-        vim.object(Object::AnyQuotes, cx)
+    Vim::action(editor, cx, |vim, _: &AnyQuotes, window, cx| {
+        vim.object(Object::AnyQuotes, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &DoubleQuotes, cx| {
-        vim.object(Object::DoubleQuotes, cx)
+    Vim::action(editor, cx, |vim, _: &DoubleQuotes, window, cx| {
+        vim.object(Object::DoubleQuotes, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &Parentheses, cx| {
-        vim.object(Object::Parentheses, cx)
+    Vim::action(editor, cx, |vim, _: &DoubleQuotes, window, cx| {
+        vim.object(Object::DoubleQuotes, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &SquareBrackets, cx| {
-        vim.object(Object::SquareBrackets, cx)
+    Vim::action(editor, cx, |vim, _: &Parentheses, window, cx| {
+        vim.object(Object::Parentheses, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &CurlyBrackets, cx| {
-        vim.object(Object::CurlyBrackets, cx)
+    Vim::action(editor, cx, |vim, _: &SquareBrackets, window, cx| {
+        vim.object(Object::SquareBrackets, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &AngleBrackets, cx| {
-        vim.object(Object::AngleBrackets, cx)
+    Vim::action(editor, cx, |vim, _: &CurlyBrackets, window, cx| {
+        vim.object(Object::CurlyBrackets, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &VerticalBars, cx| {
-        vim.object(Object::VerticalBars, cx)
+    Vim::action(editor, cx, |vim, _: &AngleBrackets, window, cx| {
+        vim.object(Object::AngleBrackets, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &Argument, cx| {
-        vim.object(Object::Argument, cx)
+    Vim::action(editor, cx, |vim, _: &VerticalBars, window, cx| {
+        vim.object(Object::VerticalBars, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &Method, cx| {
-        vim.object(Object::Method, cx)
+    Vim::action(editor, cx, |vim, _: &Argument, window, cx| {
+        vim.object(Object::Argument, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &Class, cx| {
-        vim.object(Object::Class, cx)
+    Vim::action(editor, cx, |vim, _: &Method, window, cx| {
+        vim.object(Object::Method, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &Comment, cx| {
+    Vim::action(editor, cx, |vim, _: &Class, window, cx| {
+        vim.object(Object::Class, window, cx)
+    });
+    Vim::action(editor, cx, |vim, _: &Comment, window, cx| {
         if !matches!(vim.active_operator(), Some(Operator::Object { .. })) {
-            vim.push_operator(Operator::Object { around: true }, cx);
+            vim.push_operator(Operator::Object { around: true }, window, cx);
         }
-        vim.object(Object::Comment, cx)
+        vim.object(Object::Comment, window, cx)
     });
     Vim::action(
         editor,
         cx,
-        |vim, &IndentObj { include_below }: &IndentObj, cx| {
-            vim.object(Object::IndentObj { include_below }, cx)
+        |vim, &IndentObj { include_below }: &IndentObj, window, cx| {
+            vim.object(Object::IndentObj { include_below }, window, cx)
         },
     );
 }
 
 impl Vim {
-    fn object(&mut self, object: Object, cx: &mut ViewContext<Self>) {
+    fn object(&mut self, object: Object, window: &mut Window, cx: &mut Context<Self>) {
         match self.mode {
-            Mode::Normal => self.normal_object(object, cx),
-            Mode::Visual | Mode::VisualLine | Mode::VisualBlock => self.visual_object(object, cx),
+            Mode::Normal => self.normal_object(object, window, cx),
+            Mode::Visual | Mode::VisualLine | Mode::VisualBlock => {
+                self.visual_object(object, window, cx)
+            }
             Mode::Insert | Mode::Replace | Mode::HelixNormal => {
                 // Shouldn't execute a text object in insert mode. Ignoring
             }

commit 089ea5da50aad5ec9a5273ccff13769514d1ce66

vim: Fix back quotes not recognized as object (#24999)
Currently back quotes ``` `` ``` not recognized as an object in vim
mode, so ```c i ` ```, ```d i ` ``` not working.

It seems to be a typo introduced in #22632 : The`DoubleQuotes` line was
doubled while the `BackQuotes` line was missing.

Release Notes:

- vim: Fixed back quotes ``` `` ``` not recognized as object.

Co-authored-by: Marshall Bowers <git@maxdeviant.com>

crates/vim/src/object.rs
@@ -312,8 +312,8 @@ pub fn register(editor: &mut Editor, cx: &mut Context<Vim>) {
     Vim::action(editor, cx, |vim, _: &AnyBrackets, window, cx| {
         vim.object(Object::AnyBrackets, window, cx)
     });
-    Vim::action(editor, cx, |vim, _: &DoubleQuotes, window, cx| {
-        vim.object(Object::DoubleQuotes, window, cx)
+    Vim::action(editor, cx, |vim, _: &BackQuotes, window, cx| {
+        vim.object(Object::BackQuotes, window, cx)
     });
     Vim::action(editor, cx, |vim, _: &DoubleQuotes, window, cx| {
         vim.object(Object::DoubleQuotes, window, cx)