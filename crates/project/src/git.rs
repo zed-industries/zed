3c22efa6835351f2dee1194f33457e3f9622f1d7
Max Brunsfeld <maxbrunsfeld@gmail.com>
2025-02-25 22:38:20 -0800

    Fix staging/unstaging of deleted and added files

--- crates/project/src/git.rs
+++ crates/project/src/git.rs
@@ -1071,7 +1071,13 @@ impl Repository {
                     };
                     let project_path = (self.worktree_id, path).into();
                     if let Some(buffer) = buffer_store.get_by_path(&project_path, cx) {
-                        save_futures.push(buffer_store.save_buffer(buffer, cx));
+                        if buffer
+                            .read(cx)
+                            .file()
+                            .map_or(false, |file| file.disk_state().exists())
+                        {
+                            save_futures.push(buffer_store.save_buffer(buffer, cx));
+                        }
                     }
                 }
             })
@@ -1110,7 +1116,13 @@ impl Repository {
                     };
                     let project_path = (self.worktree_id, path).into();
                     if let Some(buffer) = buffer_store.get_by_path(&project_path, cx) {
-                        save_futures.push(buffer_store.save_buffer(buffer, cx));
+                        if buffer
+                            .read(cx)
+                            .file()
+                            .map_or(false, |file| file.disk_state().exists())
+                        {
+                            save_futures.push(buffer_store.save_buffer(buffer, cx));
+                        }
                     }
                 }
             })
