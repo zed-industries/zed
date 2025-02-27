a0d1555470d260c2c1b0a831bd0f7233e910e01d
张小白 <364772080@qq.com>
2025-02-27 10:03:46 +0800

    windows: Fix terminal inline assistant (#25715)
    Closes #18518
    Closes #20546
    
    Release Notes:
    
    - N/A

--- crates/assistant/src/terminal_inline_assistant.rs
+++ crates/assistant/src/terminal_inline_assistant.rs
@@ -1054,7 +1054,10 @@ pub enum CodegenEvent {
 
 impl EventEmitter<CodegenEvent> for Codegen {}
 
+#[cfg(not(target_os = "windows"))]
 const CLEAR_INPUT: &str = "\x15";
+#[cfg(target_os = "windows")]
+const CLEAR_INPUT: &str = "\x03";
 const CARRIAGE_RETURN: &str = "\x0d";
 
 struct TerminalTransaction {
