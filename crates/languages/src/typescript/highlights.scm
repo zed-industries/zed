e00d737196f7f71bb8a5675577255b6f86743ca6
Devzeth <47153906+devzeth@users.noreply.github.com>
2025-03-02 01:43:51 +0100

    Add `constructor` highlighting for JS/TS/TSX (#25207)
    Closes #19267
    
    Adds highlight field specifically for `constructor` in JS/TS/TSX so that
    it can be highlighted as a different color than regular methods
    
    Release Notes:
    
    - N/A
    
    ---------
    
    Co-authored-by: Ben Kunkle <ben@zed.dev>

--- crates/languages/src/typescript/highlights.scm
+++ crates/languages/src/typescript/highlights.scm
@@ -57,6 +57,9 @@
   name: (identifier) @function)
 (method_definition
   name: (property_identifier) @function.method)
+(method_definition
+    name: (property_identifier) @constructor
+    (#eq? @constructor "constructor"))
 
 (pair
   key: (property_identifier) @function.method
