52cd01646955d9c1760511b48ebbcf8bd72fbc8f
smit <0xtimsb@gmail.com>
2025-03-03 12:33:31 +0530

    migration update

--- crates/migrator/src/migrator.rs
+++ crates/migrator/src/migrator.rs
@@ -92,7 +92,10 @@ const KEYMAP_MIGRATION_TRANSFORMATION_PATTERNS: MigrationPatterns = &[
     ),
     (ACTION_STRING_PATTERN, rename_string_action),
     (CONTEXT_PREDICATE_PATTERN, rename_context_key),
-    (ACTION_ARRAY_PATTERN, replace_first_string_of_array),
+    (
+        ACTION_STRING_OF_ARRAY_PATTERN,
+        replace_first_string_of_array,
+    ),
 ];
 
 static KEYMAP_MIGRATION_TRANSFORMATION_QUERY: LazyLock<Query> = LazyLock::new(|| {
@@ -266,24 +269,46 @@ static TRANSFORM_ARRAY: LazyLock<HashMap<(&str, &str), &str>> = LazyLock::new(||
     ])
 });
 
+const ACTION_STRING_OF_ARRAY_PATTERN: &str = r#"(document
+    (array
+   	    (object
+            (pair
+                key: (string (string_content) @name)
+                value: (
+                    (object
+                        (pair
+                            key: (string)
+                            value: ((array
+                                . (string (string_content) @action_name)
+                                . _
+                                .)) @array
+                        )
+                    )
+                )
+            )
+        )
+    )
+    (#eq? @name "bindings")
+)"#;
+
 // ["editor::GoToPrevHunk", { "center_cursor": true }] -> ["editor::GoToPreviousHunk", { "center_cursor": true }]
 fn replace_first_string_of_array(
     contents: &str,
     mat: &QueryMatch,
     query: &Query,
 ) -> Option<(Range<usize>, String)> {
-    let array_ix = query.capture_index_for_name("array")?;
     let action_name_ix = query.capture_index_for_name("action_name")?;
     let action_name = contents.get(
         mat.nodes_for_capture_index(action_name_ix)
             .next()?
             .byte_range(),
     )?;
-
     let replacement = STRING_OF_ARRAY_REPLACE.get(action_name)?;
     let replacement_as_string = format!("\"{replacement}\"");
-    let range_to_replace = mat.nodes_for_capture_index(array_ix).next()?.byte_range();
-
+    let range_to_replace = mat
+        .nodes_for_capture_index(action_name_ix)
+        .next()?
+        .byte_range();
     Some((range_to_replace, replacement_as_string))
 }
 
