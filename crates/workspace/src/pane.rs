8d16ddbad8a34372c604e538d706be2f6aba483d
smit <0xtimsb@gmail.com>
2025-03-03 21:19:25 +0530

    keymap: Update `Prev` to `Previous` follow-up (#25931)
    Follow-up for https://github.com/zed-industries/zed/pull/25909
    
    Add three more action replacements:
    
    ```
    1. "pane::ActivatePrevItem" -> "pane::ActivatePreviousItem"
    2. "vim::MoveToPrev" -> "vim::MoveToPrevious"
    3. "vim:MoveToPrevMatch" -> "vim:MoveToPreviousMatch"
    ```
    
    Release Notes:
    
    - N/A

--- crates/workspace/src/pane.rs
+++ crates/workspace/src/pane.rs
@@ -172,7 +172,7 @@ impl_actions!(
 actions!(
     pane,
     [
-        ActivatePrevItem,
+        ActivatePreviousItem,
         ActivateNextItem,
         ActivateLastItem,
         AlternateFile,
@@ -3148,7 +3148,7 @@ impl Render for Pane {
                 }),
             )
             .on_action(
-                cx.listener(|pane: &mut Pane, _: &ActivatePrevItem, window, cx| {
+                cx.listener(|pane: &mut Pane, _: &ActivatePreviousItem, window, cx| {
                     pane.activate_prev_item(true, window, cx);
                 }),
             )
