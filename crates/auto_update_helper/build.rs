3ab376be6b924ce82d37c5a650d56b6606f9db30
Junkui Zhang <364772080@qq.com>
2025-03-06 17:09:16 +0800

    Clippy

--- crates/auto_update_helper/build.rs
+++ crates/auto_update_helper/build.rs
@@ -1,11 +1,14 @@
 fn main() {
-    println!("cargo:rerun-if-changed=manifest.xml");
+    #[cfg(target_os = "windows")]
+    {
+        println!("cargo:rerun-if-changed=manifest.xml");
 
-    let mut res = winresource::WindowsResource::new();
-    res.set_manifest_file("manifest.xml");
+        let mut res = winresource::WindowsResource::new();
+        res.set_manifest_file("manifest.xml");
 
-    if let Err(e) = res.compile() {
-        eprintln!("{}", e);
-        std::process::exit(1);
+        if let Err(e) = res.compile() {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
     }
 }
