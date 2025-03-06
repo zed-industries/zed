0253777dcac65c1f96772d219741a819d79e0768
Junkui Zhang <364772080@qq.com>
2025-03-06 20:07:34 +0800

    Add icon

--- crates/auto_update_helper/build.rs
+++ crates/auto_update_helper/build.rs
@@ -5,6 +5,7 @@ fn main() {
 
         let mut res = winresource::WindowsResource::new();
         res.set_manifest_file("manifest.xml");
+        res.set_icon("app-icon.ico");
 
         if let Err(e) = res.compile() {
             eprintln!("{}", e);
