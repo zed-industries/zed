d17523b304aa3c690dfd78bae7b2e953335911ec
Junkui Zhang <364772080@qq.com>
2025-03-10 22:49:50 +0800

    Sync with `recent_projects`

--- crates/recent_projects/src/recent_projects.rs
+++ crates/recent_projects/src/recent_projects.rs
@@ -24,8 +24,8 @@ use std::{
 use ui::{prelude::*, tooltip_container, KeyBinding, ListItem, ListItemSpacing, Tooltip};
 use util::{paths::PathExt, ResultExt};
 use workspace::{
-    CloseIntent, ModalView, OpenOptions, SerializedWorkspaceLocation, Workspace, WorkspaceId,
-    WORKSPACE_DB,
+    CloseIntent, DeleteSource, HistoryManager, HistoryManagerEvent, ModalView, OpenOptions,
+    SerializedWorkspaceLocation, Workspace, WorkspaceId, WORKSPACE_DB,
 };
 use zed_actions::{OpenRecent, OpenRemote};
 
@@ -553,7 +553,12 @@ impl RecentProjectsDelegate {
                         .delegate
                         .set_selected_index(ix.saturating_sub(1), window, cx);
                     picker.delegate.reset_selected_match_index = false;
-                    picker.update_matches(picker.query(cx), window, cx)
+                    picker.update_matches(picker.query(cx), window, cx);
+                    if let Some(history_manager) = HistoryManager::global(cx) {
+                        history_manager.update(cx, |_, cx| {
+                            cx.emit(HistoryManagerEvent::Delete(DeleteSource::User))
+                        });
+                    }
                 })
             })
             .detach();
