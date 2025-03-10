5f7108f4d7f26546864e28f4df4f3ba90d7f95b0
Junkui Zhang <364772080@qq.com>
2025-03-10 22:46:25 +0800

    refactor

--- crates/workspace/src/history_manager.rs
+++ crates/workspace/src/history_manager.rs
@@ -8,54 +8,58 @@ pub fn init(cx: &mut App) {
     let manager = cx.new(|_| HistoryManager::new());
     HistoryManager::set_global(manager.clone(), cx);
     cx.subscribe(&manager, |manager, event, cx| match event {
-        HistoryManagerEvent::Update => cx
-            .spawn(|mut cx| async move {
-                let recent_folders = WORKSPACE_DB
-                    .recent_workspaces_on_disk()
-                    .await
-                    .unwrap_or_default()
-                    .into_iter()
-                    .map(|(id, location)| {
-                        (
-                            location
-                                .sorted_paths()
-                                .iter()
-                                .map(|path| path.compact().to_string_lossy().into_owned())
-                                .collect::<Vec<_>>()
-                                .join(""),
-                            id,
-                        )
-                    })
-                    .collect::<Vec<_>>();
-                let user_removed = update_jump_list(&recent_folders)
-                    .log_err()
-                    .unwrap_or_default();
-                let deleted_ids = recent_folders
-                    .into_iter()
-                    .filter_map(|(query, id)| {
-                        if user_removed.contains(&query) {
-                            Some(id)
-                        } else {
-                            None
-                        }
-                    })
-                    .collect::<Vec<_>>();
-                for id in deleted_ids.iter() {
-                    WORKSPACE_DB.delete_workspace_by_id(*id).await.log_err();
-                }
-                manager
-                    .update(&mut cx, |_, cx| {
-                        cx.emit(HistoryManagerEvent::Delete(DeleteSource::System))
-                    })
-                    .log_err();
-            })
-            .detach(),
+        HistoryManagerEvent::Update => perform_update(cx, manager),
         HistoryManagerEvent::Delete(DeleteSource::User) => {
             manager.update(cx, |_, cx| cx.emit(HistoryManagerEvent::Update));
         }
         _ => {}
     })
     .detach();
+    perform_update(cx, manager);
+}
+
+fn perform_update(cx: &mut App, manager: Entity<HistoryManager>) {
+    cx.spawn(|mut cx| async move {
+        let recent_folders = WORKSPACE_DB
+            .recent_workspaces_on_disk()
+            .await
+            .unwrap_or_default()
+            .into_iter()
+            .map(|(id, location)| {
+                (
+                    location
+                        .sorted_paths()
+                        .iter()
+                        .map(|path| path.compact().to_string_lossy().into_owned())
+                        .collect::<Vec<_>>()
+                        .join(""),
+                    id,
+                )
+            })
+            .collect::<Vec<_>>();
+        let user_removed = update_jump_list(&recent_folders)
+            .log_err()
+            .unwrap_or_default();
+        let deleted_ids = recent_folders
+            .into_iter()
+            .filter_map(|(query, id)| {
+                if user_removed.contains(&query) {
+                    Some(id)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        for id in deleted_ids.iter() {
+            WORKSPACE_DB.delete_workspace_by_id(*id).await.log_err();
+        }
+        manager
+            .update(&mut cx, |_, cx| {
+                cx.emit(HistoryManagerEvent::Delete(DeleteSource::System))
+            })
+            .log_err();
+    })
+    .detach()
 }
 
 pub struct HistoryManager;
