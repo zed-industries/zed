8b396b0b34325d17b436930e55a976910b197f20
Michael Sloan <michael@zed.dev>
2025-03-11 14:24:22 -0600

    Add loading of eval data + checking out repos
    Co-authored-by: Thomas <thomas@zed.dev>

--- crates/tool_evals/src/eval.rs
+++ crates/tool_evals/src/eval.rs
@@ -1,18 +1,9 @@
 use crate::headless_assistant::{authenticate_model_provider, find_model, HeadlessAssistant};
 use anyhow::anyhow;
-use assistant2::{Message, RequestKind, Thread, ThreadEvent, ThreadStore};
-use assistant_tool::ToolWorkingSet;
-use client::Client;
-use git::GitHostingProviderRegistry;
-use gpui::{prelude::*, App, Entity, Subscription, Task};
-use language::LanguageRegistry;
-use language_model::{
-    AuthenticateError, LanguageModel, LanguageModelProviderId, LanguageModelRegistry,
-};
-use project::{Project, RealFs};
-use prompt_store::PromptBuilder;
-use settings::SettingsStore;
-use smol::channel;
+use assistant2::{Message, RequestKind};
+use gpui::{App, Task};
+use language_model::{LanguageModelProviderId, LanguageModelRegistry};
+use serde::Deserialize;
 use std::{
     path::{Path, PathBuf},
     sync::Arc,
@@ -23,11 +14,40 @@ pub struct Eval {
     pub repo_path: PathBuf,
     pub system_prompt: Option<String>,
     pub user_query: String,
-    pub provider_id: String,
+    pub provider_id: LanguageModelProviderId,
     pub model_name: String,
 }
 
+#[derive(Deserialize)]
+struct EvalSetup {
+    pub url: String,
+    pub base_sha: String,
+}
+
 impl Eval {
+    /// Loads the eval from a path (typically in `evaluation_data`). Clones and checks out the repo
+    /// if necessary.
+    pub fn load(
+        eval_path: &Path,
+        repo_path: &Path,
+        provider_id: LanguageModelProviderId,
+        model_name: String,
+    ) -> anyhow::Result<Self> {
+        let user_query = std::fs::read_to_string(eval_path.join("prompt.txt"))?;
+        let setup_contents = std::fs::read_to_string(eval_path.join("setup.json"))?;
+        let setup = serde_json_lenient::from_str_lenient::<EvalSetup>(&setup_contents)?;
+
+        setup.checkout_repo(repo_path)?;
+
+        Ok(Eval {
+            repo_path: repo_path.to_path_buf(),
+            system_prompt: None,
+            user_query,
+            provider_id,
+            model_name,
+        })
+    }
+
     /// Runs the eval. Note that this cannot be run concurrently because
     /// LanguageModelRegistry.active_model is global state.
     pub fn run(
@@ -44,8 +64,7 @@ impl Eval {
             registry.set_active_model(Some(model.clone()), cx);
         });
 
-        let provider_id = LanguageModelProviderId(self.provider_id.clone().into());
-        let authenticate_task = authenticate_model_provider(provider_id, cx);
+        let authenticate_task = authenticate_model_provider(self.provider_id.clone(), cx);
 
         let repo_path = self.repo_path.clone();
         let system_prompt = self.system_prompt.clone();
@@ -91,5 +110,36 @@ impl Eval {
                     .collect::<Vec<_>>()
             })
         })
+    }
+}
+
+impl EvalSetup {
+    fn checkout_repo(&self, repo_path: &Path) -> anyhow::Result<()> {
+        if !repo_path.exists() {
+            if let Some(parent) = repo_path.parent() {
+                std::fs::create_dir_all(parent)?;
+            }
+            let mut child = std::process::Command::new("git")
+                .arg("clone")
+                .arg(&self.url)
+                .arg(repo_path)
+                .spawn()?;
+            let exit_status = child.wait().unwrap();
+            if !exit_status.success() {
+                panic!("git clone exited with failure status {exit_status}");
+            }
+        }
+
+        let mut child = std::process::Command::new("git")
+            .arg("checkout")
+            .arg(&self.base_sha)
+            .current_dir(repo_path)
+            .spawn()?;
+        let exit_status = child.wait().unwrap();
+        if !exit_status.success() {
+            panic!("git checkout exited with failure status {exit_status}");
+        }
+
+        Ok(())
     }
 }
--- crates/tool_evals/src/main.rs
+++ crates/tool_evals/src/main.rs
@@ -1,33 +1,76 @@
 mod eval;
 mod headless_assistant;
 
+use clap::{Parser, Subcommand};
 use eval::Eval;
 use gpui::Application;
-use language_model::ANTHROPIC_PROVIDER_ID;
+use language_model::{LanguageModelProviderId, ANTHROPIC_PROVIDER_ID};
 use reqwest_client::ReqwestClient;
 use std::{path::PathBuf, sync::Arc};
 
+#[derive(Parser, Debug)]
+#[command(
+    name = "tool_evals",
+    disable_version_flag = true,
+    before_help = "Tool eval runner"
+)]
+struct Args {
+    evals_to_run: Vec<String>,
+    /// Runs all evals in `evaluation_data`.
+    #[arg(long)]
+    all: bool,
+    /// Name of the model provider (default: "anthropic")
+    #[arg(long, default_value = ANTHROPIC_PROVIDER_ID)]
+    provider_id: String,
+    /// Name of the model (default: "claude-3-7-sonnet-latest")
+    #[arg(long, default_value = "claude-3-7-sonnet-latest")]
+    model_name: String,
+}
+
 fn main() {
+    let args = Args::parse();
     let http_client = Arc::new(ReqwestClient::new());
     let app = Application::headless().with_http_client(http_client.clone());
 
-    app.run(|cx| {
+    let crate_dir = PathBuf::from("../zed-agent-bench");
+    let evaluation_data_dir = crate_dir.join("evaluation_data");
+    let repos_dir = crate_dir.join("repos");
+
+    let evals_to_run = if args.all {
+        std::fs::read_dir(&evaluation_data_dir)
+            .unwrap()
+            .map(|path| path.unwrap().file_name().to_string_lossy().to_string())
+            .collect::<Vec<_>>()
+    } else {
+        args.evals_to_run
+    };
+
+    if evals_to_run.is_empty() {
+        panic!("Names of evals to run must be provided or `--all` specified");
+    }
+
+    let provider_id = LanguageModelProviderId(args.provider_id.into());
+
+    app.run(move |cx| {
         let app_state = headless_assistant::init(cx);
 
-        let eval = Eval {
-            repo_path: PathBuf::from("todo!"),
-            system_prompt: Some("You are a helpful assistant.".to_string()),
-            user_query: "write me a Limerick about code editors".to_string(),
-            provider_id: ANTHROPIC_PROVIDER_ID.to_string(),
-            model_name: "claude-3-sonnet-20240229".to_string(),
-        };
+        cx.spawn(move |cx| async move {
+            for eval_name in evals_to_run {
+                let eval_path = evaluation_data_dir.join(&eval_name);
+                let repo_path = repos_dir.join(&eval_name);
+                let eval = Eval::load(
+                    &eval_path,
+                    &repo_path,
+                    provider_id.clone(),
+                    args.model_name.clone(),
+                )
+                .unwrap();
 
-        let task = eval.run(app_state, cx);
-
-        cx.spawn(|_cx| async move {
-            match task.await {
-                Ok(response) => println!("Response: {:?}", response),
-                Err(err) => println!("Error: {}", err),
+                let task = cx.update(|cx| eval.run(app_state.clone(), cx)).unwrap();
+                match task.await {
+                    Ok(response) => println!("Response: {:?}", response),
+                    Err(err) => println!("Error: {}", err),
+                }
             }
         })
         .detach();
