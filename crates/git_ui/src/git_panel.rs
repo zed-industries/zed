39a0f266a746b9df4471ca62ab0532d1bb976b2e
Cole Miller <cole@zed.dev>
2025-03-11 19:29:49 -0400

    Fix git panel entries getting cut off

--- crates/git_ui/src/git_panel.rs
+++ crates/git_ui/src/git_panel.rs
@@ -3138,7 +3138,6 @@ impl GitPanel {
                     .flex_none()
                     .occlude()
                     .cursor_pointer()
-                    .ml_neg_0p5()
                     .child(
                         Checkbox::new(checkbox_id, is_staged)
                             .disabled(!has_write_access)
