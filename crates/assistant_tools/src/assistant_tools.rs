6692a081842aa1e9236d96eb0e80ca1a9ac6c449
Antonio Scandurra <me@as-cii.com>
2025-03-12 17:02:26 +0100

    Add RegexSearchTool

--- crates/assistant_tools/src/assistant_tools.rs
+++ crates/assistant_tools/src/assistant_tools.rs
@@ -2,6 +2,7 @@ mod edit_files_tool;
 mod list_directory_tool;
 mod now_tool;
 mod read_file_tool;
+mod regex_search;
 
 use assistant_tool::ToolRegistry;
 use gpui::App;
@@ -10,6 +11,7 @@ use crate::edit_files_tool::EditFilesTool;
 use crate::list_directory_tool::ListDirectoryTool;
 use crate::now_tool::NowTool;
 use crate::read_file_tool::ReadFileTool;
+use crate::regex_search::RegexSearchTool;
 
 pub fn init(cx: &mut App) {
     assistant_tool::init(cx);
@@ -19,4 +21,5 @@ pub fn init(cx: &mut App) {
     registry.register_tool(ReadFileTool);
     registry.register_tool(ListDirectoryTool);
     registry.register_tool(EditFilesTool);
+    registry.register_tool(RegexSearchTool);
 }
