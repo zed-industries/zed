zed-industries/zed

commit b9c48685e8abce35d4d6e7a882af7801722140c6

terminal: Support trailing `:description or error message` after file path (#26401)
Closes #25086

Release Notes:

- Fixed a bug where file paths in the built in terminal of the format
`path/to/file.ext:row:col:description or error message` would not be
correctly identified as file paths due to the colon & additional text at
the end

crates/terminal/src/terminal.rs
@@ -929,18 +929,51 @@ impl Terminal {
                 } else if let Some(word_match) = regex_match_at(term, point, &mut self.word_regex) {
                     let file_path = term.bounds_to_string(*word_match.start(), *word_match.end());
 
-                    let (sanitized_match, sanitized_word) =
+                    let (sanitized_match, sanitized_word) = 'sanitize: {
+                        let mut word_match = word_match;
+                        let mut file_path = file_path;
+
                         if is_path_surrounded_by_common_symbols(&file_path) {
-                            (
-                                Match::new(
-                                    word_match.start().add(term, Boundary::Cursor, 1),
-                                    word_match.end().sub(term, Boundary::Cursor, 1),
-                                ),
-                                file_path[1..file_path.len() - 1].to_owned(),
-                            )
-                        } else {
-                            (word_match, file_path)
-                        };
+                            word_match = Match::new(
+                                word_match.start().add(term, Boundary::Cursor, 1),
+                                word_match.end().sub(term, Boundary::Cursor, 1),
+                            );
+                            file_path = file_path[1..file_path.len() - 1].to_owned();
+                        }
+
+                        let mut colon_count = 0;
+                        for c in file_path.chars() {
+                            if c == ':' {
+                                colon_count += 1;
+                            }
+                        }
+                        // strip trailing comment after colon in case of
+                        // file/at/path.rs:row:column:description or error message
+                        // so that the file path is `file/at/path.rs:row:column`
+                        if colon_count > 2 {
+                            let last_index = file_path.rfind(':').unwrap();
+                            let prev_is_digit = last_index > 0
+                                && file_path
+                                    .chars()
+                                    .nth(last_index - 1)
+                                    .map_or(false, |c| c.is_ascii_digit());
+                            let next_is_digit = last_index < file_path.len() - 1
+                                && file_path
+                                    .chars()
+                                    .nth(last_index + 1)
+                                    .map_or(true, |c| c.is_ascii_digit());
+                            if prev_is_digit && !next_is_digit {
+                                let stripped_len = file_path.len() - last_index;
+                                word_match = Match::new(
+                                    *word_match.start(),
+                                    word_match.end().sub(term, Boundary::Cursor, stripped_len),
+                                );
+                                file_path = file_path[0..last_index].to_owned();
+                            }
+                        }
+
+                        break 'sanitize (word_match, file_path);
+                    };
 
                     Some((sanitized_word, false, sanitized_match))
                 } else {

commit 18fcdf1d2c00d21ef6d1ccd8d771bd956cdb9687

terminal: Fix issues with highlighted ranges of paths (#26695)
Fixes a few problems,

- Uses `Boundary::Grid` instead of `Boundary::Cursor` for highlighted
range adjustments.

This fixes quite a few wierd behaviors around highlighting paths that
had to be scrolled into view (i.e. were in the terminal history)
including the issue described in the release notes as well as a
regression caused by #26401 where the highlight range would span from
the start of the path to the cursor location in the shell prompt

- Strips all trailing `:`s from the paths, updating the highlighted
range accordingly.

This worked fine before and is just a visual improvement.


Release Notes:

- Fixed an issue where file paths in the terminal surrounded by `()` or
`[]` would not be highlighted properly

crates/terminal/src/terminal.rs
@@ -935,12 +935,19 @@ impl Terminal {
 
                         if is_path_surrounded_by_common_symbols(&file_path) {
                             word_match = Match::new(
-                                word_match.start().add(term, Boundary::Cursor, 1),
-                                word_match.end().sub(term, Boundary::Cursor, 1),
+                                word_match.start().add(term, Boundary::Grid, 1),
+                                word_match.end().sub(term, Boundary::Grid, 1),
                             );
                             file_path = file_path[1..file_path.len() - 1].to_owned();
                         }
 
+                        while file_path.ends_with(':') {
+                            file_path.pop();
+                            word_match = Match::new(
+                                *word_match.start(),
+                                word_match.end().sub(term, Boundary::Grid, 1),
+                            );
+                        }
                         let mut colon_count = 0;
                         for c in file_path.chars() {
                             if c == ':' {
@@ -966,7 +973,7 @@ impl Terminal {
                                 let stripped_len = file_path.len() - last_index;
                                 word_match = Match::new(
                                     *word_match.start(),
-                                    word_match.end().sub(term, Boundary::Cursor, stripped_len),
+                                    word_match.end().sub(term, Boundary::Grid, stripped_len),
                                 );
                                 file_path = file_path[0..last_index].to_owned();
                             }