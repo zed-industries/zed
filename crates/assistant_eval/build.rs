30710cd03742e836641b4497f63ca3e18b010cc4
Michael Sloan <michael@zed.dev>
2025-03-14 16:52:37 -0600

    Remove unnecessary icon code from assistant_eval/build.rs

--- crates/assistant_eval/build.rs
+++ crates/assistant_eval/build.rs
@@ -1,4 +1,4 @@
-// Copied from `crates/zed/build.rs`
+// Copied from `crates/zed/build.rs`, with removal of code for including the zed icon on windows.
 
 use std::process::Command;
 
@@ -56,26 +56,6 @@ fn main() {
         {
             // todo(windows): This is to avoid stack overflow. Remove it when solved.
             println!("cargo:rustc-link-arg=/stack:{}", 8 * 1024 * 1024);
-        }
-
-        let icon = std::path::Path::new("resources/windows/app-icon.ico");
-        println!("cargo:rerun-if-changed={}", icon.display());
-
-        let mut res = winresource::WindowsResource::new();
-
-        // Depending on the security applied to the computer, winresource might fail
-        // fetching the RC path. Therefore, we add a way to explicitly specify the
-        // toolkit path, allowing winresource to use a valid RC path.
-        if let Some(explicit_rc_toolkit_path) = std::env::var("ZED_RC_TOOLKIT_PATH").ok() {
-            res.set_toolkit_path(explicit_rc_toolkit_path.as_str());
-        }
-        res.set_icon(icon.to_str().unwrap());
-        res.set("FileDescription", "Zed");
-        res.set("ProductName", "Zed");
-
-        if let Err(e) = res.compile() {
-            eprintln!("{}", e);
-            std::process::exit(1);
         }
     }
 }
