ba8a8701e4f765d14131714806aec8746176199f
Conrad Irwin <conrad.irwin@gmail.com>
2025-03-14 14:14:05 -0600

    TEMP

--- crates/gpui/src/platform/mac/screen_capture.rs
+++ crates/gpui/src/platform/mac/screen_capture.rs
@@ -65,6 +65,9 @@ impl ScreenCaptureSource for MacScreenCaptureSource {
             let excluded_windows = NSArray::array(nil);
             let filter: id = msg_send![filter, initWithDisplay:self.sc_display excludingWindows:excluded_windows];
             let configuration: id = msg_send![configuration, init];
+            let _: id = msg_send![configuration, setScalesToFit: false];
+            let _: id = msg_send![configuration, setShowsCursor: false];
+            let _: id = msg_send![configuration, setCaptureResolution: 1];
             let delegate: id = msg_send![delegate, init];
             let output: id = msg_send![output, init];
 
