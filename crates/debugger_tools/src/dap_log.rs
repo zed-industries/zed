6d73fb722346c93e107c229e085acbea09f25ed7
Remco Smits <djsmits12@gmail.com>
2025-03-15 15:13:38 +0100

    Disable code actions, runnables and breakpoints inside dap log editor

--- crates/debugger_tools/src/dap_log.rs
+++ crates/debugger_tools/src/dap_log.rs
@@ -535,6 +535,12 @@ impl DapLogView {
             let mut editor = Editor::multi_line(window, cx);
             editor.set_text(log_contents, window, cx);
             editor.move_to_end(&editor::actions::MoveToEnd, window, cx);
+            editor.set_show_code_actions(false, cx);
+            editor.set_show_breakpoints(false, cx);
+            editor.set_show_git_diff_gutter(false, cx);
+            editor.set_show_runnables(false, cx);
+            editor.set_input_enabled(false);
+            editor.set_use_autoclose(false);
             editor.set_read_only(true);
             editor.set_show_edit_predictions(Some(false), window, cx);
             editor
