5b64a4c29c888c9806f228aa72f2fe51e1897008
KyleBarton <kjbarton4@gmail.com>
2025-03-17 13:24:19 -0700

    Expire invocations if not updated within 1 month.

--- crates/command_palette/src/persistence.rs
+++ crates/command_palette/src/persistence.rs
@@ -6,7 +6,7 @@ use db::{
 };
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub(crate) struct SerializedCommand {
     pub(crate) command_name: String,
     pub(crate) invocations: u16,
@@ -35,9 +35,15 @@ define_connection!(pub static ref COMMAND_PALETTE_HISTORY: CommandPaletteDB<()> 
     )];
 );
 
+const COMMAND_EXPIRY: &str = "-1 month";
 impl CommandPaletteDB {
     pub async fn write_command(&self, command_name: String) -> Result<()> {
-        self.write_command_internal(command_name, u16::MAX).await
+        self.write_command_internal(command_name, COMMAND_EXPIRY.to_string(), u16::MAX)
+            .await
+    }
+
+    pub fn list_commands_used(&self) -> Result<Vec<SerializedCommand>> {
+        self.list_commands_used_internal(COMMAND_EXPIRY)
     }
     query! {
         pub fn read_command_history(command: &str) -> Result<Option<SerializedCommand>> {
@@ -45,17 +51,24 @@ impl CommandPaletteDB {
         }
     }
     query! {
-        async fn write_command_internal(command_name: String, max_invocations: u16) -> Result<()> {
+        async fn write_command_internal(command_name: String, expired_statement: String, max_invocations: u16) -> Result<()> {
             // Upsert
             INSERT INTO command_palette_history(command_name, invocations) VALUES  ((?), 1)
             ON CONFLICT DO
-            UPDATE SET invocations = MIN(invocations + 1, (?))
+            UPDATE
+            SET invocations = CASE
+                WHEN last_invoked > datetime("now", (?)) THEN MIN(invocations + 1, (?))
+                    ELSE (1) END,
+                last_invoked = datetime("now")
         }
     }
 
     query! {
-        pub fn list_commands_used() -> Result<Vec<SerializedCommand>> {
-            SELECT command_name, invocations FROM command_palette_history ORDER BY invocations DESC
+        fn list_commands_used_internal(expired_statement: &str) -> Result<Vec<SerializedCommand>> {
+            SELECT command_name, invocations
+            FROM command_palette_history
+            WHERE last_invoked > datetime("now", (?))
+            ORDER BY invocations DESC
         }
     }
 }
@@ -64,7 +77,7 @@ impl CommandPaletteDB {
 mod tests {
     use db::sqlez_macros::sql;
 
-    use crate::persistence::CommandPaletteDB;
+    use crate::persistence::{CommandPaletteDB, SerializedCommand};
 
     #[gpui::test]
     async fn test_saves_hitcount_with_last_used() {
@@ -137,5 +150,47 @@ mod tests {
 
         assert!(some_command.is_some());
         assert_eq!(some_command.expect("is some").invocations, u16::MAX);
+    }
+
+    #[gpui::test]
+    async fn test_handles_expired_history() {
+        let db = CommandPaletteDB(db::open_test_db("test_handles_expired_history").await);
+        db.write(|conn| {
+            conn.exec_bound(
+                sql!(INSERT INTO command_palette_history(command_name, invocations, last_invoked ) VALUES ((?), (?), datetime("now", "-2 month")))
+            ).unwrap()(("expired_command", 100)).unwrap();
+            conn.exec_bound(
+                sql!(INSERT INTO command_palette_history(command_name, invocations) VALUES ((?), (?)))
+            ).unwrap()(("current_command", 10)).unwrap();
+        }).await;
+
+        // Ensure expired commands filter out
+        let commands = db.list_commands_used().expect("finds commands");
+        assert_eq!(
+            commands,
+            vec![SerializedCommand {
+                command_name: "current_command".to_string(),
+                invocations: 10,
+            }],
+        );
+
+        // Ensure expired commands re-set at 1 invocation
+        db.write_command("expired_command".to_string())
+            .await
+            .unwrap();
+        let commands = db.list_commands_used().expect("finds commands");
+        assert_eq!(
+            commands,
+            vec![
+                SerializedCommand {
+                    command_name: "current_command".to_string(),
+                    invocations: 10,
+                },
+                SerializedCommand {
+                    command_name: "expired_command".to_string(),
+                    invocations: 1,
+                }
+            ],
+        );
     }
 }
