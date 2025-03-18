e39ae07196ab7afd097e969342cc594add4da3ce
Julia Ryan <p1n3appl3@users.noreply.github.com>
2025-03-18 12:56:16 -0700

    nix: don't block nightly release on failure

--- flake.nix
+++ flake.nix
@@ -62,14 +62,14 @@
       overlays = overlays // {
         default = nixpkgs.lib.composeManyExtensions (builtins.attrValues overlays);
       };
-
-      nixConfig = {
-        extra-substituters = [
-          "https://app.cachix.org/cache/zed-industries"
-        ];
-        extra-trusted-public-keys = [
-          "zed-industries.cachix.org-1:QW3RoXK0Lm4ycmU5/3bmYRd3MLf4RbTGPqRulGlX5W0="
-        ];
-      };
     };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://zed-industries.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "zed-industries.cachix.org-1:QW3RoXK0Lm4ycmU5/3bmYRd3MLf4RbTGPqRulGlX5W0="
+    ];
+  };
 }
