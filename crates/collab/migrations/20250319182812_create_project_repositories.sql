6b21b102cca71d3e5e5669478cd6c8a14f47455e
Cole Miller <cole@zed.dev>
2025-03-19 15:09:44 -0400

    Add postgres migration

---
+++ crates/collab/migrations/20250319182812_create_project_repositories.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "project_repositories" (
+    "project_id" INTEGER NOT NULL,
+    "abs_path" VARCHAR,
+    "id" INTEGER NOT NULL,
+    "entry_ids" INTEGER NOT NULL ARRAY,
+    "branch" VARCHAR,
+    "scan_id" INTEGER NOT NULL,
+    "is_deleted" BOOL NOT NULL,
+    "current_merge_conflicts" VARCHAR,
+    "branch_summary" VARCHAR,
+);
+
+CREATE INDEX "index_project_repositories_on_project_id" ON "project_repositories" ("project_id");
+
+CREATE TABLE "project_repository_statuses" (
+    "project_id" INTEGER NOT NULL,
+    "id" INTEGER NOT NULL,
+    "repo_path" VARCHAR NOT NULL,
+    "status" INT8 NOT NULL,
+    "status_kind" INT4 NOT NULL,
+    "first_status" INT4 NULL,
+    "second_status" INT4 NULL,
+    "scan_id" INT8 NOT NULL,
+    "is_deleted" BOOL NOT NULL,
+    PRIMARY KEY (project_id, id, repo_path),
+);
+
+CREATE INDEX "index_project_repos_statuses_on_project_id" ON "worktree_repository_statuses" ("project_id");
+
+CREATE INDEX "index_project_repos_statuses_on_project_id_and_repo_id" ON "worktree_repository_statuses" ("project_id", "id");
