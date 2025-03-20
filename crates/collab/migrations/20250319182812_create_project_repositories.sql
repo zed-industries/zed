38b2c554dfed95447f87a5cfaefed34ecc84aed5
Cole Miller <cole@zed.dev>
2025-03-20 10:18:04 -0400

    More trivial fixes

--- crates/collab/migrations/20250319182812_create_project_repositories.sql
+++ crates/collab/migrations/20250319182812_create_project_repositories.sql
@@ -8,13 +8,14 @@ CREATE TABLE "project_repositories" (
     "is_deleted" BOOL NOT NULL,
     "current_merge_conflicts" VARCHAR,
     "branch_summary" VARCHAR,
+    PRIMARY KEY (project_id, id)
 );
 
 CREATE INDEX "index_project_repositories_on_project_id" ON "project_repositories" ("project_id");
 
 CREATE TABLE "project_repository_statuses" (
     "project_id" INTEGER NOT NULL,
-    "id" INTEGER NOT NULL,
+    "repository_id" INTEGER NOT NULL,
     "repo_path" VARCHAR NOT NULL,
     "status" INT8 NOT NULL,
     "status_kind" INT4 NOT NULL,
@@ -22,9 +23,9 @@ CREATE TABLE "project_repository_statuses" (
     "second_status" INT4 NULL,
     "scan_id" INT8 NOT NULL,
     "is_deleted" BOOL NOT NULL,
-    PRIMARY KEY (project_id, id, repo_path),
+    PRIMARY KEY (project_id, repository_id, repo_path)
 );
 
-CREATE INDEX "index_project_repos_statuses_on_project_id" ON "worktree_repository_statuses" ("project_id");
+CREATE INDEX "index_project_repos_statuses_on_project_id" ON "project_repository_statuses" ("project_id");
 
-CREATE INDEX "index_project_repos_statuses_on_project_id_and_repo_id" ON "worktree_repository_statuses" ("project_id", "id");
+CREATE INDEX "index_project_repos_statuses_on_project_id_and_repo_id" ON "project_repository_statuses" ("project_id", "repository_id");
