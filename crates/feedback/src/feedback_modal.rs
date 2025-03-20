68fdc0affd8b1cad968eb5062a4d6a080fa390fa
Danilo Leal <daniloleal09@gmail.com>
2025-03-20 04:03:10 -0300

    Iterate on modal design

--- crates/feedback/src/feedback_modal.rs
+++ crates/feedback/src/feedback_modal.rs
@@ -1,7 +1,5 @@
-use gpui::{
-    rems, App, Context, DismissEvent, EventEmitter, FocusHandle, Focusable, Render, Window,
-};
-use ui::{prelude::*, Button, ButtonStyle, IconPosition};
+use gpui::{App, Context, DismissEvent, EventEmitter, FocusHandle, Focusable, Render, Window};
+use ui::{prelude::*, IconPosition};
 use workspace::{ModalView, Workspace};
 use zed_actions::feedback::GiveFeedback;
 
@@ -45,17 +43,33 @@ impl Render for FeedbackModal {
             cx.listener(|_, _, window, cx| window.dispatch_action(Box::new(OpenZedRepo), cx));
 
         v_flex()
-            .elevation_3(cx)
             .key_context("GiveFeedback")
             .on_action(cx.listener(Self::cancel))
-            .min_w(rems(40.))
-            .max_w(rems(96.))
-            .h(rems(32.))
+            .elevation_3(cx)
+            .w_96()
+            .h_auto()
             .p_4()
             .gap_2()
-            .child(Headline::new("Give Feedback"))
             .child(
-                Button::new("file-a-bug-report", "File a bug report")
+                h_flex()
+                    .w_full()
+                    .justify_between()
+                    .child(Headline::new("Give Feedback"))
+                    .child(
+                        IconButton::new("close-btn", IconName::Close)
+                            .icon_color(Color::Muted)
+                            .on_click(cx.listener(move |_, _, window, cx| {
+                                cx.spawn_in(window, async move |this, cx| {
+                                    this.update(cx, |_, cx| cx.emit(DismissEvent)).ok();
+                                })
+                                .detach();
+                            })),
+                    ),
+            )
+            .child(Label::new("Thanks for using Zed! To share your experience with us, reach for the channel that's the most appropriate:"))
+            .child(
+                Button::new("file-a-bug-report", "File a Bug Report")
+                    .full_width()
                     .icon(IconName::Debug)
                     .icon_size(IconSize::XSmall)
                     .icon_color(Color::Muted)
@@ -65,7 +79,8 @@ impl Render for FeedbackModal {
                     })),
             )
             .child(
-                Button::new("request-a-feature", "Request a feature")
+                Button::new("request-a-feature", "Request a Feature")
+                    .full_width()
                     .icon(IconName::Sparkle)
                     .icon_size(IconSize::XSmall)
                     .icon_color(Color::Muted)
@@ -75,7 +90,8 @@ impl Render for FeedbackModal {
                     })),
             )
             .child(
-                Button::new("send-us_an-email", "Send us an email")
+                Button::new("send-us_an-email", "Send an Email")
+                    .full_width()
                     .icon(IconName::Envelope)
                     .icon_size(IconSize::XSmall)
                     .icon_color(Color::Muted)
@@ -85,30 +101,13 @@ impl Render for FeedbackModal {
                     })),
             )
             .child(
-                h_flex()
-                    .justify_between()
-                    .gap_1()
-                    .child(
-                        Button::new("zed_repository", "Zed Repository")
-                            .style(ButtonStyle::Transparent)
-                            .icon(IconName::ExternalLink)
-                            .icon_position(IconPosition::End)
-                            .icon_size(IconSize::Small)
-                            .on_click(open_zed_repo),
-                    )
-                    .child(
-                        h_flex().gap_1().child(
-                            Button::new("cancel_feedback", "Cancel")
-                                .style(ButtonStyle::Subtle)
-                                .color(Color::Muted)
-                                .on_click(cx.listener(move |_, _, window, cx| {
-                                    cx.spawn_in(window, async move |this, cx| {
-                                        this.update(cx, |_, cx| cx.emit(DismissEvent)).ok();
-                                    })
-                                    .detach();
-                                })),
-                        ),
-                    ),
+                Button::new("zed_repository", "GitHub Repository")
+                    .full_width()
+                    .icon(IconName::Github)
+                    .icon_size(IconSize::XSmall)
+                    .icon_color(Color::Muted)
+                    .icon_position(IconPosition::Start)
+                    .on_click(open_zed_repo),
             )
     }
 }
