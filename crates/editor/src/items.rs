a957d4c501f442082e432bd16ea8898c7aaca9bc
Nate Butler <iamnbutler@gmail.com>
2025-03-21 13:35:29 -0400

    Fix project panel git colors

--- crates/editor/src/items.rs
+++ crates/editor/src/items.rs
@@ -1662,13 +1662,13 @@ pub fn entry_diagnostic_aware_icon_decoration_and_color(
 pub fn entry_git_aware_label_color(git_status: GitSummary, ignored: bool, selected: bool) -> Color {
     let tracked = git_status.index + git_status.worktree;
     if ignored {
-        Color::VersionControlIgnored
+        Color::Ignored
     } else if git_status.conflict > 0 {
-        Color::VersionControlConflict
+        Color::Conflict
     } else if tracked.modified > 0 {
-        Color::VersionControlModified
+        Color::Modified
     } else if tracked.added > 0 || git_status.untracked > 0 {
-        Color::VersionControlAdded
+        Color::Created
     } else {
         entry_label_color(selected)
     }
