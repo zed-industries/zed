67d187cdcd759c03a295f41ccbcf29fcffbbae7c
Richard Feldman <oss@rtfeldman.com>
2025-03-26 10:42:00 -0400

    Don't use PathBuf unnecessarily

--- crates/assistant_tools/src/path_search_tool.rs
+++ crates/assistant_tools/src/path_search_tool.rs
@@ -7,7 +7,6 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::{path::PathBuf, sync::Arc};
 use ui::IconName;
-use util::markdown::MarkdownString;
 use util::paths::PathMatcher;
 use worktree::Snapshot;
 
