d64fcb167a3e0203e14f8ba3f3709b4a1a081fc9
Marshall Bowers <git@maxdeviant.com>
2025-03-26 17:27:11 -0400

    assistant2: Add profile picker

---
+++ crates/assistant2/src/assistant_configuration/manage_profiles_modal.rs
@@ -0,0 +1,76 @@
+use gpui::{prelude::*, DismissEvent, Entity, EventEmitter, FocusHandle, Focusable, WeakEntity};
+use ui::prelude::*;
+use workspace::{ModalView, Workspace};
+
+use crate::assistant_configuration::profile_picker::{ProfilePicker, ProfilePickerDelegate};
+use crate::ManageProfiles;
+
+enum Mode {
+    ChooseProfile(Entity<ProfilePicker>),
+}
+
+pub struct ManageProfilesModal {
+    workspace: WeakEntity<Workspace>,
+    focus_handle: FocusHandle,
+    mode: Mode,
+}
+
+impl ManageProfilesModal {
+    pub fn register(
+        workspace: &mut Workspace,
+        _window: Option<&mut Window>,
+        _cx: &mut Context<Workspace>,
+    ) {
+        workspace.register_action(|workspace, _: &ManageProfiles, window, cx| {
+            let workspace_handle = cx.entity().downgrade();
+            workspace.toggle_modal(window, cx, |window, cx| {
+                Self::new(workspace_handle, window, cx)
+            })
+        });
+    }
+
+    pub fn new(
+        workspace: WeakEntity<Workspace>,
+        window: &mut Window,
+        cx: &mut Context<Self>,
+    ) -> Self {
+        let focus_handle = cx.focus_handle();
+
+        Self {
+            workspace,
+            focus_handle,
+            mode: Mode::ChooseProfile(cx.new(|cx| {
+                let delegate = ProfilePickerDelegate::new(cx);
+                ProfilePicker::new(delegate, window, cx)
+            })),
+        }
+    }
+}
+
+impl ModalView for ManageProfilesModal {}
+
+impl Focusable for ManageProfilesModal {
+    fn focus_handle(&self, cx: &App) -> FocusHandle {
+        self.focus_handle.clone()
+    }
+}
+
+impl EventEmitter<DismissEvent> for ManageProfilesModal {}
+
+impl Render for ManageProfilesModal {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .elevation_3(cx)
+            .w(rems(34.))
+            .key_context("ManageProfilesModal")
+            // .on_action(cx.listener(|this, _: &menu::Cancel, _window, cx| this.cancel(cx)))
+            // .on_action(cx.listener(|this, _: &menu::Confirm, _window, cx| this.confirm(cx)))
+            .capture_any_mouse_down(cx.listener(|this, _, window, cx| {
+                this.focus_handle(cx).focus(window);
+            }))
+            .on_mouse_down_out(cx.listener(|_this, _, _, cx| cx.emit(DismissEvent)))
+            .child(match &self.mode {
+                Mode::ChooseProfile(profile_picker) => profile_picker.clone().into_any_element(),
+            })
+    }
+}
