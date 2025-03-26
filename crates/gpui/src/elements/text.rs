77856bf01758a0e51a52f65690f87f78b2ead290
Smit Barmase <heysmitbarmase@gmail.com>
2025-03-27 01:58:26 +0530

    Hide the mouse when the user is typing in the editor - take 2 (#27519)
    Closes #4461
    
    Take 2 on https://github.com/zed-industries/zed/pull/25040. 
    
    Fixes panic caused due to using `setHiddenUntilMouseMoves` return type
    to `set` cursor on macOS.
    
    Release Notes:
    
    - Now cursor hides when the user is typing in editor. It will stay
    hidden until it is moved again. This behavior is `true` by default, and
    can be configured with `hide_mouse_while_typing` in settings.
    
    ---------
    
    Co-authored-by: Peter Tripp <peter@zed.dev>
    Co-authored-by: Thomas Mickley-Doyle <thomas@zed.dev>
    Co-authored-by: Agus <agus@zed.dev>
    Co-authored-by: Kirill Bulatov <kirill@zed.dev>
    Co-authored-by: Agus Zubiaga <hi@aguz.me>
    Co-authored-by: Angelk90 <angelo.k90@hotmail.it>

--- crates/gpui/src/elements/text.rs
+++ crates/gpui/src/elements/text.rs
@@ -700,7 +700,7 @@ impl Element for InteractiveText {
                             .iter()
                             .any(|range| range.contains(&ix))
                         {
-                            window.set_cursor_style(crate::CursorStyle::PointingHand, hitbox)
+                            window.set_cursor_style(crate::CursorStyle::PointingHand, Some(hitbox))
                         }
                     }
 
