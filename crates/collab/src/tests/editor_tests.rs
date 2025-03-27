0d3684f1757405af3a586a2be028c670c5af7131
Junkui Zhang <364772080@qq.com>
2025-03-27 17:46:13 +0800

    Fix `test_on_input_format_from_guest_to_host`

--- crates/collab/src/tests/editor_tests.rs
+++ crates/collab/src/tests/editor_tests.rs
@@ -1421,14 +1421,14 @@ async fn test_on_input_format_from_guest_to_host(
     client_a
         .fs()
         .insert_tree(
-            "/a",
+            path!("/a"),
             json!({
                 "main.rs": "fn main() { a }",
                 "other.rs": "// Test file",
             }),
         )
         .await;
-    let (project_a, worktree_id) = client_a.build_local_project("/a", cx_a).await;
+    let (project_a, worktree_id) = client_a.build_local_project(path!("/a"), cx_a).await;
     let project_id = active_call_a
         .update(cx_a, |call, cx| call.share_project(project_a.clone(), cx))
         .await
@@ -1462,7 +1462,7 @@ async fn test_on_input_format_from_guest_to_host(
         .set_request_handler::<lsp::request::OnTypeFormatting, _, _>(|params, _| async move {
             assert_eq!(
                 params.text_document_position.text_document.uri,
-                lsp::Url::from_file_path("/a/main.rs").unwrap(),
+                lsp::Url::from_file_path(path!("/a/main.rs")).unwrap(),
             );
             assert_eq!(
                 params.text_document_position.position,
