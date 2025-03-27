957e17a90176a4a3bcd4e57277346dc0d7dc6938
Peter Tripp <peter@zed.dev>
2025-03-27 14:19:24 -0400

    Fix Terminal theming issue with background/foreground text
    - Closes: https://github.com/zed-industries/zed/issues/27427

--- crates/terminal/src/terminal.rs
+++ crates/terminal/src/terminal.rs
@@ -2081,8 +2081,9 @@ pub fn get_color_at_index(index: usize, theme: &Theme) -> Hsla {
             rgba_color(i * step, i * step, i * step) // Map the ANSI-grayscale components to the RGB-grayscale
         }
         // For compatibility with the alacritty::Colors interface
-        256 => colors.text,
-        257 => colors.background,
+        // See: https://github.com/alacritty/alacritty/blob/master/alacritty_terminal/src/term/color.rs
+        256 => colors.terminal_foreground,
+        257 => colors.terminal_background,
         258 => theme.players().local().cursor,
         259 => colors.terminal_ansi_dim_black,
         260 => colors.terminal_ansi_dim_red,
