bb39a16566e14e8e2bf96f1d82265c4a9045c3e7
Marshall Bowers <git@maxdeviant.com>
2025-03-28 09:40:48 -0400

    assistant2: Reload profile when making changes to the active profile's tools

--- crates/assistant2/src/assistant_configuration/manage_profiles_modal.rs
+++ crates/assistant2/src/assistant_configuration/manage_profiles_modal.rs
@@ -10,7 +10,10 @@ use assistant_tool::ToolWorkingSet;
 use convert_case::{Case, Casing as _};
 use editor::Editor;
 use fs::Fs;
-use gpui::{prelude::*, DismissEvent, Entity, EventEmitter, FocusHandle, Focusable, Subscription};
+use gpui::{
+    prelude::*, DismissEvent, Entity, EventEmitter, FocusHandle, Focusable, Subscription,
+    WeakEntity,
+};
 use settings::{update_settings_file, Settings as _};
 use ui::{prelude::*, ListItem, ListItemSpacing, ListSeparator, Navigable, NavigableEntry};
 use workspace::{ModalView, Workspace};
@@ -18,7 +21,7 @@ use workspace::{ModalView, Workspace};
 use crate::assistant_configuration::manage_profiles_modal::profile_modal_header::ProfileModalHeader;
 use crate::assistant_configuration::profile_picker::{ProfilePicker, ProfilePickerDelegate};
 use crate::assistant_configuration::tool_picker::{ToolPicker, ToolPickerDelegate};
-use crate::{AssistantPanel, ManageProfiles};
+use crate::{AssistantPanel, ManageProfiles, ThreadStore};
 
 enum Mode {
     ChooseProfile {
@@ -80,6 +83,7 @@ pub struct NewProfileMode {
 pub struct ManageProfilesModal {
     fs: Arc<dyn Fs>,
     tools: Arc<ToolWorkingSet>,
+    thread_store: WeakEntity<ThreadStore>,
     focus_handle: FocusHandle,
     mode: Mode,
 }
@@ -93,9 +97,12 @@ impl ManageProfilesModal {
         workspace.register_action(|workspace, _: &ManageProfiles, window, cx| {
             if let Some(panel) = workspace.panel::<AssistantPanel>(cx) {
                 let fs = workspace.app_state().fs.clone();
-                let thread_store = panel.read(cx).thread_store().read(cx);
-                let tools = thread_store.tools();
-                workspace.toggle_modal(window, cx, |window, cx| Self::new(fs, tools, window, cx))
+                let thread_store = panel.read(cx).thread_store();
+                let tools = thread_store.read(cx).tools();
+                let thread_store = thread_store.downgrade();
+                workspace.toggle_modal(window, cx, |window, cx| {
+                    Self::new(fs, tools, thread_store, window, cx)
+                })
             }
         });
     }
@@ -103,6 +110,7 @@ impl ManageProfilesModal {
     pub fn new(
         fs: Arc<dyn Fs>,
         tools: Arc<ToolWorkingSet>,
+        thread_store: WeakEntity<ThreadStore>,
         window: &mut Window,
         cx: &mut Context<Self>,
     ) -> Self {
@@ -111,6 +119,7 @@ impl ManageProfilesModal {
         Self {
             fs,
             tools,
+            thread_store,
             focus_handle,
             mode: Mode::choose_profile(window, cx),
         }
@@ -168,6 +177,7 @@ impl ManageProfilesModal {
             let delegate = ToolPickerDelegate::new(
                 self.fs.clone(),
                 self.tools.clone(),
+                self.thread_store.clone(),
                 profile_id.clone(),
                 profile,
                 cx,
