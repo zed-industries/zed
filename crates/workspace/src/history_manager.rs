64824d93b2024e6514b7935b99bfb7e007ce040e
Junkui Zhang <364772080@qq.com>
2025-03-29 15:56:42 +0800

    Use a dedicate type for entry

--- crates/workspace/src/history_manager.rs
+++ crates/workspace/src/history_manager.rs
@@ -1,21 +1,30 @@
+use std::path::PathBuf;
+
 use gpui::{AppContext, Entity, EventEmitter, Global};
+use smallvec::SmallVec;
 use ui::App;
 use util::{paths::PathExt, ResultExt};
 
-use crate::WORKSPACE_DB;
+use crate::{SerializedWorkspaceLocation, WorkspaceId, WORKSPACE_DB};
 
 pub fn init(cx: &mut App) {
     let manager = cx.new(|_| HistoryManager::new());
     HistoryManager::set_global(manager.clone(), cx);
-    cx.subscribe(&manager, |_, event, cx| match event {
-        HistoryManagerEvent::Update => perform_update(cx),
+    cx.subscribe(&manager, |this, event, cx| match event {
+        HistoryManagerEvent::Update => perform_update(this, cx),
     })
     .detach();
-    perform_update(cx);
+    // perform_update(cx);
+    HistoryManager::init(manager, cx);
 }
 
-fn perform_update(cx: &mut App) {
+fn perform_update(manager: Entity<HistoryManager>, cx: &mut App) {
     cx.spawn(async move |cx| {
+        manager
+            .update(cx, |this, cx| {
+                println!("History: {:#?}", this.history);
+            })
+            .log_err();
         let recent_folders = WORKSPACE_DB
             .recent_workspaces_on_disk()
             .await
@@ -58,7 +67,15 @@ fn perform_update(cx: &mut App) {
     .detach()
 }
 
-pub struct HistoryManager;
+pub struct HistoryManager {
+    history: Vec<HistoryManagerEntry>,
+}
+
+#[derive(Debug)]
+pub struct HistoryManagerEntry {
+    pub id: WorkspaceId,
+    pub path: SmallVec<[PathBuf; 2]>,
+}
 
 struct GlobalHistoryManager(Entity<HistoryManager>);
 
@@ -73,7 +90,25 @@ impl EventEmitter<HistoryManagerEvent> for HistoryManager {}
 
 impl HistoryManager {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            history: Vec::new(),
+        }
+    }
+
+    pub fn init(this: Entity<HistoryManager>, cx: &App) {
+        cx.spawn(async move |cx| {
+            let recent_folders = WORKSPACE_DB
+                .recent_workspaces_on_disk()
+                .await
+                .unwrap_or_default()
+                .into_iter()
+                .map(|(id, location)| HistoryManagerEntry::new(id, location))
+                .collect::<Vec<_>>();
+            this.update(cx, |this, _| {
+                this.history = recent_folders;
+            })
+        })
+        .detach();
     }
 
     pub fn global(cx: &App) -> Option<Entity<Self>> {
@@ -83,5 +118,16 @@ impl HistoryManager {
 
     pub fn set_global(history_manager: Entity<Self>, cx: &mut App) {
         cx.set_global(GlobalHistoryManager(history_manager));
+    }
+}
+
+impl HistoryManagerEntry {
+    pub fn new(id: WorkspaceId, location: SerializedWorkspaceLocation) -> Self {
+        let path = location
+            .sorted_paths()
+            .iter()
+            .map(|path| path.compact())
+            .collect::<SmallVec<[PathBuf; 2]>>();
+        Self { id, path }
     }
 }
