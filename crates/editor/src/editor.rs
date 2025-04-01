b59967bbdfe6867bc2334fbf3b27f1a9dbb59bc6
Max Brunsfeld <maxbrunsfeld@gmail.com>
2025-04-01 11:40:45 -0700

    Don't allow open excerpts in commit view for now (#27862)
    Follow-up to https://github.com/zed-industries/zed/pull/27636
    
    Release Notes:
    
    - N/A

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -17212,6 +17212,9 @@ impl Editor {
             }
         }
 
+        new_selections_by_buffer
+            .retain(|buffer, _| Self::can_open_excerpts_in_file(buffer.read(cx).file()));
+
         if new_selections_by_buffer.is_empty() {
             return;
         }
@@ -17278,6 +17281,12 @@ impl Editor {
                 }
             })
         });
+    }
+
+    // For now, don't allow opening excerpts in buffers that aren't backed by
+    // regular project files.
+    fn can_open_excerpts_in_file(file: Option<&Arc<dyn language::File>>) -> bool {
+        file.map_or(true, |file| project::File::from_dyn(Some(file)).is_some())
     }
 
     fn marked_text_ranges(&self, cx: &App) -> Option<Vec<Range<OffsetUtf16>>> {
