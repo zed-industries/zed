acbb5653e8457fbad3e83946dee4b4fea8a767ff
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-04-01 18:00:10 +0200

    fixup! workspace: Do not call set_active when deserializing a dock if that dock was not visible

--- crates/workspace/src/dock.rs
+++ crates/workspace/src/dock.rs
@@ -565,11 +565,9 @@ impl Dock {
 
     pub fn restore_state(&mut self, window: &mut Window, cx: &mut Context<Self>) -> bool {
         if let Some(serialized) = self.serialized_dock.clone() {
-            if let Some(active_panel) = serialized.active_panel {
+            if let Some(active_panel) = serialized.active_panel.filter(|_| serialized.visible) {
                 if let Some(idx) = self.panel_index_for_persistent_name(active_panel.as_str(), cx) {
-                    if serialized.visible {
-                        self.activate_panel(idx, window, cx);
-                    }
+                    self.activate_panel(idx, window, cx);
                 }
             }
 
