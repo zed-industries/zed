09a4a24af276dca0297a7bf4629b41a8f6948373
Cole Miller <cole@zed.dev>
2025-04-01 15:45:04 -0400

    Update project_tests.rs

--- crates/project/src/project_tests.rs
+++ crates/project/src/project_tests.rs
@@ -7486,10 +7486,10 @@ async fn test_conflicted_cherry_pick(cx: &mut gpui::TestAppContext) {
         git_status(&repo),
         collections::HashMap::from_iter([("a.txt".to_owned(), git2::Status::CONFLICTED)])
     );
+    tree.flush_fs_events(cx).await;
     cx.read(|cx| tree.read(cx).as_local().unwrap().scan_complete())
         .await;
     cx.executor().run_until_parked();
-    tree.flush_fs_events(cx).await;
     let conflicts = repository.update(cx, |repository, _| {
         repository
             .merge_conflicts
