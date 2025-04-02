e1a8a31fa40007141def8c862f0ec0e96f8c155a
Cole Miller <cole@zed.dev>
2025-04-02 13:43:48 -0400

    Fix flickering when staging and unstaging files (#27931)
    This fixes a bug in #27568 that caused flickering in the git panel's
    checkbox state when staging and unstaging entire files. The problem is
    that that stage/unstage action first saves the target path (if it's open
    as a buffer), and we do a targeted git status scan in response to that
    filesystem event, which makes its way to the git panel and causes it to
    clear its pending state before the actual stage or unstage has gone
    through.
    
    The fix is to not clear the panel's pending state for git repository
    events that originated from a targeted scan (i.e. one that was triggered
    by FS events for repo paths, as opposed to events inside `.git` which
    cause all statuses to be recomputed).
    
    Release Notes:
    
    - N/A

--- crates/project/src/git_store.rs
+++ crates/project/src/git_store.rs
@@ -258,7 +258,7 @@ pub enum RepositoryState {
 
 #[derive(Clone, Debug)]
 pub enum RepositoryEvent {
-    Updated,
+    Updated { full_scan: bool },
     MergeHeadsChanged,
 }
 
@@ -3521,7 +3521,7 @@ impl Repository {
         if update.is_last_update {
             self.snapshot.scan_id = update.scan_id;
         }
-        cx.emit(RepositoryEvent::Updated);
+        cx.emit(RepositoryEvent::Updated { full_scan: true });
         Ok(())
     }
 
@@ -3866,7 +3866,7 @@ impl Repository {
                                 .ok();
                         }
                     }
-                    cx.emit(RepositoryEvent::Updated);
+                    cx.emit(RepositoryEvent::Updated { full_scan: false });
                 })
             },
         );
@@ -4106,7 +4106,7 @@ async fn compute_snapshot(
         || branch != prev_snapshot.branch
         || statuses_by_path != prev_snapshot.statuses_by_path
     {
-        events.push(RepositoryEvent::Updated);
+        events.push(RepositoryEvent::Updated { full_scan: true });
     }
 
     let mut current_merge_conflicts = TreeSet::default();
