b306c37a96144ee27174721a638855043bf94dd1
Junkui Zhang <364772080@qq.com>
2025-04-02 23:00:11 +0800

    Simplify

--- crates/gpui/src/platform/keyboard_mapper.rs
+++ crates/gpui/src/platform/keyboard_mapper.rs
@@ -1,5 +1,3 @@
-use std::sync::LazyLock;
-
 use collections::HashMap;
 
 use super::{
@@ -74,355 +72,13 @@ impl KeyboardMapper {
             letter.insert("z".to_string(), KeyCode::Z);
         }
 
-        // 0x001d => KeyCode::Digital0,
-        let chars = generate_keymap_info(0x001d);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital0, c.clone());
+        for (scan_code, code) in ALL_CODES {
+            for (key, modifiers) in generate_keymap_info(scan_code) {
+                if modifiers == Modifiers::none() {
+                    code_to_char.insert(code, key.clone());
+                }
+                other.insert(key, (code, modifiers));
             }
-            other.insert(c, (KeyCode::Digital0, modifiers));
-        }
-        // 0x0012 => KeyCode::Digital1,
-        let chars = generate_keymap_info(0x0012);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital1, c.clone());
-            }
-            other.insert(c, (KeyCode::Digital1, modifiers));
-        }
-        // 0x0013 => KeyCode::Digital2,
-        let chars = generate_keymap_info(0x0013);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital2, c.clone());
-            }
-            other.insert(c, (KeyCode::Digital2, modifiers));
-        }
-        // 0x0014 => KeyCode::Digital3,
-        let chars = generate_keymap_info(0x0014);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital3, c.clone());
-            }
-            other.insert(c, (KeyCode::Digital3, modifiers));
-        }
-        // 0x0015 => KeyCode::Digital4,
-        let chars = generate_keymap_info(0x0015);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital4, c.clone());
-            }
-            other.insert(c, (KeyCode::Digital4, modifiers));
-        }
-        // 0x0017 => KeyCode::Digital5,
-        let chars = generate_keymap_info(0x0017);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital5, c.clone());
-            }
-            other.insert(c, (KeyCode::Digital5, modifiers));
-        }
-        // 0x0016 => KeyCode::Digital6,
-        let chars = generate_keymap_info(0x0016);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital6, c.clone());
-            }
-            other.insert(c, (KeyCode::Digital6, modifiers));
-        }
-        // 0x001a => KeyCode::Digital7,
-        let chars = generate_keymap_info(0x001a);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital7, c.clone());
-            }
-            other.insert(c, (KeyCode::Digital7, modifiers));
-        }
-        // 0x001c => KeyCode::Digital8,
-        let chars = generate_keymap_info(0x001c);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital8, c.clone());
-            }
-            other.insert(c, (KeyCode::Digital8, modifiers));
-        }
-        // 0x0019 => KeyCode::Digital9,
-        let chars = generate_keymap_info(0x0019);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Digital9, c.clone());
-            }
-            other.insert(c, (KeyCode::Digital9, modifiers));
-        }
-        // 0x0029 => KeyCode::Semicolon,
-        let chars = generate_keymap_info(0x0029);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Semicolon, c.clone());
-            }
-            other.insert(c, (KeyCode::Semicolon, modifiers));
-        }
-        // 0x0018 => KeyCode::Plus,
-        let chars = generate_keymap_info(0x0018);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Plus, c.clone());
-            }
-            other.insert(c, (KeyCode::Plus, modifiers));
-        }
-        // 0x002b => KeyCode::Comma,
-        let chars = generate_keymap_info(0x002b);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Comma, c.clone());
-            }
-            other.insert(c, (KeyCode::Comma, modifiers));
-        }
-        // 0x001b => KeyCode::Minus,
-        let chars = generate_keymap_info(0x001b);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Minus, c.clone());
-            }
-            other.insert(c, (KeyCode::Minus, modifiers));
-        }
-        // 0x002f => KeyCode::Period,
-        let chars = generate_keymap_info(0x002f);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Period, c.clone());
-            }
-            other.insert(c, (KeyCode::Period, modifiers));
-        }
-        // 0x002c => KeyCode::Slash,
-        let chars = generate_keymap_info(0x002c);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Slash, c.clone());
-            }
-            other.insert(c, (KeyCode::Slash, modifiers));
-        }
-        // 0x0032 => KeyCode::Tilde,
-        let chars = generate_keymap_info(0x0032);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Tilde, c.clone());
-            }
-            other.insert(c, (KeyCode::Tilde, modifiers));
-        }
-        // 0x0021 => KeyCode::LeftBracket,
-        let chars = generate_keymap_info(0x0021);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::LeftBracket, c.clone());
-            }
-            other.insert(c, (KeyCode::LeftBracket, modifiers));
-        }
-        // 0x002a => KeyCode::Backslash,
-        let chars = generate_keymap_info(0x002a);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Backslash, c.clone());
-            }
-            other.insert(c, (KeyCode::Backslash, modifiers));
-        }
-        // 0x001e => KeyCode::RightBracket,
-        let chars = generate_keymap_info(0x001e);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::RightBracket, c.clone());
-            }
-            other.insert(c, (KeyCode::RightBracket, modifiers));
-        }
-        // 0x0027 => KeyCode::Quote,
-        let chars = generate_keymap_info(0x0027);
-        for (c, modifiers) in chars {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Quote, c.clone());
-            }
-            other.insert(c, (KeyCode::Quote, modifiers));
-        }
-        // 0x0000 => KeyCode::A,
-        for (c, modifiers) in generate_keymap_info(0x0000) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::A, c.clone());
-            }
-            other.insert(c, (KeyCode::A, modifiers));
-        }
-        // 0x000b => KeyCode::B,
-        for (c, modifiers) in generate_keymap_info(0x000b) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::B, c.clone());
-            }
-            other.insert(c, (KeyCode::B, modifiers));
-        }
-        // 0x0008 => KeyCode::C,
-        for (c, modifiers) in generate_keymap_info(0x0008) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::C, c.clone());
-            }
-            other.insert(c, (KeyCode::C, modifiers));
-        }
-        // 0x0002 => KeyCode::D,
-        for (c, modifiers) in generate_keymap_info(0x0002) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::D, c.clone());
-            }
-            other.insert(c, (KeyCode::D, modifiers));
-        }
-        // 0x000e => KeyCode::E,
-        for (c, modifiers) in generate_keymap_info(0x000e) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::E, c.clone());
-            }
-            other.insert(c, (KeyCode::E, modifiers));
-        }
-        // 0x0003 => KeyCode::F,
-        for (c, modifiers) in generate_keymap_info(0x0003) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::F, c.clone());
-            }
-            other.insert(c, (KeyCode::F, modifiers));
-        }
-        // 0x0005 => KeyCode::G,
-        for (c, modifiers) in generate_keymap_info(0x0005) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::G, c.clone());
-            }
-            other.insert(c, (KeyCode::G, modifiers));
-        }
-        // 0x0004 => KeyCode::H,
-        for (c, modifiers) in generate_keymap_info(0x0004) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::H, c.clone());
-            }
-            other.insert(c, (KeyCode::H, modifiers));
-        }
-        // 0x0022 => KeyCode::I,
-        for (c, modifiers) in generate_keymap_info(0x0022) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::I, c.clone());
-            }
-            other.insert(c, (KeyCode::I, modifiers));
-        }
-        // 0x0026 => KeyCode::J,
-        for (c, modifiers) in generate_keymap_info(0x0026) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::J, c.clone());
-            }
-            other.insert(c, (KeyCode::J, modifiers));
-        }
-        // 0x0028 => KeyCode::K,
-        for (c, modifiers) in generate_keymap_info(0x0028) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::K, c.clone());
-            }
-            other.insert(c, (KeyCode::K, modifiers));
-        }
-        // 0x0025 => KeyCode::L,
-        for (c, modifiers) in generate_keymap_info(0x0025) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::L, c.clone());
-            }
-            other.insert(c, (KeyCode::L, modifiers));
-        }
-        // 0x002e => KeyCode::M,
-        for (c, modifiers) in generate_keymap_info(0x002e) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::M, c.clone());
-            }
-            other.insert(c, (KeyCode::M, modifiers));
-        }
-        // 0x002d => KeyCode::N,
-        for (c, modifiers) in generate_keymap_info(0x002d) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::N, c.clone());
-            }
-            other.insert(c, (KeyCode::N, modifiers));
-        }
-        // 0x001f => KeyCode::O,
-        for (c, modifiers) in generate_keymap_info(0x001f) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::O, c.clone());
-            }
-            other.insert(c, (KeyCode::O, modifiers));
-        }
-        // 0x0023 => KeyCode::P,
-        for (c, modifiers) in generate_keymap_info(0x0023) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::P, c.clone());
-            }
-            other.insert(c, (KeyCode::P, modifiers));
-        }
-        // 0x000c => KeyCode::Q,
-        for (c, modifiers) in generate_keymap_info(0x000c) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Q, c.clone());
-            }
-            other.insert(c, (KeyCode::Q, modifiers));
-        }
-        // 0x000f => KeyCode::R,
-        for (c, modifiers) in generate_keymap_info(0x000f) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::R, c.clone());
-            }
-            other.insert(c, (KeyCode::R, modifiers));
-        }
-        // 0x0001 => KeyCode::S,
-        for (c, modifiers) in generate_keymap_info(0x0001) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::S, c.clone());
-            }
-            other.insert(c, (KeyCode::S, modifiers));
-        }
-        // 0x0011 => KeyCode::T,
-        for (c, modifiers) in generate_keymap_info(0x0011) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::T, c.clone());
-            }
-            other.insert(c, (KeyCode::T, modifiers));
-        }
-        // 0x0020 => KeyCode::U,
-        for (c, modifiers) in generate_keymap_info(0x0020) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::U, c.clone());
-            }
-            other.insert(c, (KeyCode::U, modifiers));
-        }
-        // 0x0009 => KeyCode::V,
-        for (c, modifiers) in generate_keymap_info(0x0009) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::V, c.clone());
-            }
-            other.insert(c, (KeyCode::V, modifiers));
-        }
-        // 0x000d => KeyCode::W,
-        for (c, modifiers) in generate_keymap_info(0x000d) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::W, c.clone());
-            }
-            other.insert(c, (KeyCode::W, modifiers));
-        }
-        // 0x0007 => KeyCode::X,
-        for (c, modifiers) in generate_keymap_info(0x0007) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::X, c.clone());
-            }
-            other.insert(c, (KeyCode::X, modifiers));
-        }
-        // 0x0010 => KeyCode::Y,
-        for (c, modifiers) in generate_keymap_info(0x0010) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Y, c.clone());
-            }
-            other.insert(c, (KeyCode::Y, modifiers));
-        }
-        // 0x0006 => KeyCode::Z,
-        for (c, modifiers) in generate_keymap_info(0x0006) {
-            if modifiers == Modifiers::none() {
-                code_to_char.insert(KeyCode::Z, c.clone());
-            }
-            other.insert(c, (KeyCode::Z, modifiers));
         }
 
         Self {
@@ -508,6 +164,102 @@ fn generate_keymap_info(scan_code: u16) -> Vec<(String, Modifiers)> {
 }
 
 const NO_MOD: u32 = 0;
-const CMD_MOD: u32 = 1;
 const SHIFT_MOD: u32 = 2;
 const OPTION_MOD: u32 = 8;
+
+static ALL_CODES: [(u16, KeyCode); 47] = [
+    // 0x001d => KeyCode::Digital0,
+    (0x001d, KeyCode::Digital0),
+    // 0x0012 => KeyCode::Digital1,
+    (0x0012, KeyCode::Digital1),
+    // 0x0013 => KeyCode::Digital2,
+    (0x0013, KeyCode::Digital2),
+    // 0x0014 => KeyCode::Digital3,
+    (0x0014, KeyCode::Digital3),
+    // 0x0015 => KeyCode::Digital4,
+    (0x0015, KeyCode::Digital4),
+    // 0x0017 => KeyCode::Digital5,
+    (0x0017, KeyCode::Digital5),
+    // 0x0016 => KeyCode::Digital6,
+    (0x0016, KeyCode::Digital6),
+    // 0x001a => KeyCode::Digital7,
+    (0x001a, KeyCode::Digital7),
+    // 0x001c => KeyCode::Digital8,
+    (0x001c, KeyCode::Digital8),
+    // 0x0019 => KeyCode::Digital9,
+    (0x0019, KeyCode::Digital9),
+    // 0x0029 => KeyCode::Semicolon,
+    (0x0029, KeyCode::Semicolon),
+    // 0x0018 => KeyCode::Plus,
+    (0x0018, KeyCode::Plus),
+    // 0x002b => KeyCode::Comma,
+    (0x002b, KeyCode::Comma),
+    // 0x001b => KeyCode::Minus,
+    (0x001b, KeyCode::Minus),
+    // 0x002f => KeyCode::Period,
+    (0x002f, KeyCode::Period),
+    // 0x002c => KeyCode::Slash,
+    (0x002c, KeyCode::Slash),
+    // 0x0032 => KeyCode::Tilde,
+    (0x0032, KeyCode::Tilde),
+    // 0x0021 => KeyCode::LeftBracket,
+    (0x0021, KeyCode::LeftBracket),
+    // 0x002a => KeyCode::Backslash,
+    (0x002a, KeyCode::Backslash),
+    // 0x001e => KeyCode::RightBracket,
+    (0x001e, KeyCode::RightBracket),
+    // 0x0027 => KeyCode::Quote,
+    (0x0027, KeyCode::Quote),
+    // 0x0000 => KeyCode::A,
+    (0x0000, KeyCode::A),
+    // 0x000b => KeyCode::B,
+    (0x000b, KeyCode::B),
+    // 0x0008 => KeyCode::C,
+    (0x0008, KeyCode::C),
+    // 0x0002 => KeyCode::D,
+    (0x0002, KeyCode::D),
+    // 0x000e => KeyCode::E,
+    (0x000e, KeyCode::E),
+    // 0x0003 => KeyCode::F,
+    (0x0003, KeyCode::F),
+    // 0x0005 => KeyCode::G,
+    (0x0005, KeyCode::G),
+    // 0x0004 => KeyCode::H,
+    (0x0004, KeyCode::H),
+    // 0x0022 => KeyCode::I,
+    (0x0022, KeyCode::I),
+    // 0x0026 => KeyCode::J,
+    (0x0026, KeyCode::J),
+    // 0x0028 => KeyCode::K,
+    (0x0028, KeyCode::K),
+    // 0x0025 => KeyCode::L,
+    (0x0025, KeyCode::L),
+    // 0x002e => KeyCode::M,
+    (0x002e, KeyCode::M),
+    // 0x002d => KeyCode::N,
+    (0x002d, KeyCode::N),
+    // 0x001f => KeyCode::O,
+    (0x001f, KeyCode::O),
+    // 0x0023 => KeyCode::P,
+    (0x0023, KeyCode::P),
+    // 0x000c => KeyCode::Q,
+    (0x000c, KeyCode::Q),
+    // 0x000f => KeyCode::R,
+    (0x000f, KeyCode::R),
+    // 0x0001 => KeyCode::S,
+    (0x0001, KeyCode::S),
+    // 0x0011 => KeyCode::T,
+    (0x0011, KeyCode::T),
+    // 0x0020 => KeyCode::U,
+    (0x0020, KeyCode::U),
+    // 0x0009 => KeyCode::V,
+    (0x0009, KeyCode::V),
+    // 0x000d => KeyCode::W,
+    (0x000d, KeyCode::W),
+    // 0x0007 => KeyCode::X,
+    (0x0007, KeyCode::X),
+    // 0x0010 => KeyCode::Y,
+    (0x0010, KeyCode::Y),
+    // 0x0006 => KeyCode::Z,
+    (0x0006, KeyCode::Z),
+];
