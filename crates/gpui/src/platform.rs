919e04acadf910a7fd29f3ac44f53a65ee3222d7
Michael Sloan <michael@zed.dev>
2025-04-02 01:14:28 -0600

    Fix build of `remote-server`

--- crates/gpui/src/platform.rs
+++ crates/gpui/src/platform.rs
@@ -26,7 +26,7 @@ mod test;
 #[cfg(target_os = "windows")]
 mod windows;
 
-#[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "windows"))]
+#[cfg(any(feature = "wayland", feature = "x11", target_os = "windows"))]
 pub(crate) mod scap_screen_capture;
 
 use crate::{
