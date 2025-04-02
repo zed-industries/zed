038f911e184d7af273c8d45e01dde56d54fb6746
Conrad Irwin <conrad.irwin@gmail.com>
2025-04-02 10:54:05 -0600

    make ComponentPreview for diagnostic renderer
    Co-authored-by: Kirill Bulatov <mail4score@gmail.com>

---
+++ crates/diagnostics/src/diagnostic_renderer.rs
@@ -0,0 +1,348 @@
+use std::{
+    ops::Range,
+    sync::{Arc, OnceLock},
+};
+
+use editor::{
+    Direction, Editor, EditorElement, EditorStyle, MultiBuffer, MultiBufferSnapshot,
+    actions::{Cancel, GoToDiagnostic},
+    diagnostic_style,
+    display_map::{BlockContext, BlockPlacement, BlockProperties, BlockStyle, RenderBlock},
+};
+use gpui::{
+    AppContext, AvailableSpace, ClipboardItem, Entity, FontWeight, HighlightStyle, StyledText,
+    TextStyle,
+};
+use indoc;
+use language::{Buffer, Diagnostic, DiagnosticEntry, DiagnosticSet, PointUtf16};
+use lsp::DiagnosticSeverity;
+use settings::Settings;
+use theme::ThemeSettings;
+use ui::{
+    ActiveTheme, AnyElement, App, ButtonCommon, ButtonSize, ButtonStyle, Clickable, Color,
+    ComponentPreview, Element, FluentBuilder, IconButton, IconName, InteractiveElement,
+    IntoComponent, IntoElement, ParentElement, SharedString, StatefulInteractiveElement, Styled,
+    Tooltip, VisibleOnHover, Window, div, h_flex, px, relative, v_flex,
+};
+
+pub fn diagnostic_block_renderer(
+    diagnostic: Diagnostic,
+    max_message_rows: Option<u8>,
+    allow_closing: bool,
+) -> RenderBlock {
+    let (text_without_backticks, code_ranges) =
+        highlight_diagnostic_message(&diagnostic, max_message_rows);
+
+    Arc::new(move |cx: &mut BlockContext| {
+        let group_id: SharedString = cx.block_id.to_string().into();
+
+        let mut text_style = cx.window.text_style().clone();
+        text_style.color = diagnostic_style(diagnostic.severity, cx.theme().status());
+        let theme_settings = ThemeSettings::get_global(cx);
+        text_style.font_family = theme_settings.buffer_font.family.clone();
+        text_style.font_style = theme_settings.buffer_font.style;
+        text_style.font_features = theme_settings.buffer_font.features.clone();
+        text_style.font_weight = theme_settings.buffer_font.weight;
+
+        let multi_line_diagnostic = diagnostic.message.contains('\n');
+
+        let buttons = |diagnostic: &Diagnostic| {
+            if multi_line_diagnostic {
+                v_flex()
+            } else {
+                h_flex()
+            }
+            .when(allow_closing, |div| {
+                div.children(diagnostic.is_primary.then(|| {
+                    IconButton::new("close-block", IconName::XCircle)
+                        .icon_color(Color::Muted)
+                        .size(ButtonSize::Compact)
+                        .style(ButtonStyle::Transparent)
+                        .visible_on_hover(group_id.clone())
+                        .on_click(move |_click, window, cx| {
+                            window.dispatch_action(Box::new(Cancel), cx)
+                        })
+                        .tooltip(|window, cx| {
+                            Tooltip::for_action("Close Diagnostics", &Cancel, window, cx)
+                        })
+                }))
+            })
+            .child(
+                IconButton::new("copy-block", IconName::Copy)
+                    .icon_color(Color::Muted)
+                    .size(ButtonSize::Compact)
+                    .style(ButtonStyle::Transparent)
+                    .visible_on_hover(group_id.clone())
+                    .on_click({
+                        let message = diagnostic.message.clone();
+                        move |_click, _, cx| {
+                            cx.write_to_clipboard(ClipboardItem::new_string(message.clone()))
+                        }
+                    })
+                    .tooltip(Tooltip::text("Copy diagnostic message")),
+            )
+        };
+
+        let icon_size = buttons(&diagnostic).into_any_element().layout_as_root(
+            AvailableSpace::min_size(),
+            cx.window,
+            cx.app,
+        );
+
+        h_flex()
+            .id(cx.block_id)
+            .group(group_id.clone())
+            .relative()
+            .size_full()
+            .block_mouse_down()
+            .pl(cx.gutter_dimensions.width)
+            .w(cx.max_width - cx.gutter_dimensions.full_width())
+            .child(
+                div()
+                    .flex()
+                    .w(cx.anchor_x - cx.gutter_dimensions.width - icon_size.width)
+                    .flex_shrink(),
+            )
+            .child(buttons(&diagnostic))
+            .child(div().flex().flex_shrink_0().child(
+                StyledText::new(text_without_backticks.clone()).with_default_highlights(
+                    &text_style,
+                    code_ranges.iter().map(|range| {
+                        (
+                            range.clone(),
+                            HighlightStyle {
+                                font_weight: Some(FontWeight::BOLD),
+                                ..Default::default()
+                            },
+                        )
+                    }),
+                ),
+            ))
+            .into_any_element()
+    })
+}
+
+pub fn highlight_diagnostic_message(
+    diagnostic: &Diagnostic,
+    mut max_message_rows: Option<u8>,
+) -> (SharedString, Vec<Range<usize>>) {
+    let mut text_without_backticks = String::new();
+    let mut code_ranges = Vec::new();
+
+    if let Some(source) = &diagnostic.source {
+        text_without_backticks.push_str(source);
+        code_ranges.push(0..source.len());
+        text_without_backticks.push_str(": ");
+    }
+
+    let mut prev_offset = 0;
+    let mut in_code_block = false;
+    let has_row_limit = max_message_rows.is_some();
+    let mut newline_indices = diagnostic
+        .message
+        .match_indices('\n')
+        .filter(|_| has_row_limit)
+        .map(|(ix, _)| ix)
+        .fuse()
+        .peekable();
+
+    for (quote_ix, _) in diagnostic
+        .message
+        .match_indices('`')
+        .chain([(diagnostic.message.len(), "")])
+    {
+        let mut first_newline_ix = None;
+        let mut last_newline_ix = None;
+        while let Some(newline_ix) = newline_indices.peek() {
+            if *newline_ix < quote_ix {
+                if first_newline_ix.is_none() {
+                    first_newline_ix = Some(*newline_ix);
+                }
+                last_newline_ix = Some(*newline_ix);
+
+                if let Some(rows_left) = &mut max_message_rows {
+                    if *rows_left == 0 {
+                        break;
+                    } else {
+                        *rows_left -= 1;
+                    }
+                }
+                let _ = newline_indices.next();
+            } else {
+                break;
+            }
+        }
+        let prev_len = text_without_backticks.len();
+        let new_text = &diagnostic.message[prev_offset..first_newline_ix.unwrap_or(quote_ix)];
+        text_without_backticks.push_str(new_text);
+        if in_code_block {
+            code_ranges.push(prev_len..text_without_backticks.len());
+        }
+        prev_offset = last_newline_ix.unwrap_or(quote_ix) + 1;
+        in_code_block = !in_code_block;
+        if first_newline_ix.map_or(false, |newline_ix| newline_ix < quote_ix) {
+            text_without_backticks.push_str("...");
+            break;
+        }
+    }
+
+    (text_without_backticks.into(), code_ranges)
+}
+
+impl editor::DiagnosticRenderer for DiagnosticRenderer {
+    fn render_group(
+        &self,
+        _diagnostic_group: Vec<language::Diagnostic>,
+        _max_message_rows: Option<u8>,
+        _allow_closing: bool,
+        _cx: &ui::App,
+    ) -> editor::display_map::BlockProperties<editor::Anchor> {
+        todo!()
+    }
+
+    fn render_secondary(
+        &self,
+        buffer: &MultiBufferSnapshot,
+        entry: DiagnosticEntry<language::Point>,
+        max_message_rows: Option<u8>,
+        allow_closing: bool,
+        _cx: &ui::App,
+    ) -> editor::display_map::BlockProperties<editor::Anchor> {
+        let diagnostic = entry.diagnostic.clone();
+        let message_height = diagnostic.message.matches('\n').count() as u32 + 1;
+        BlockProperties {
+            style: BlockStyle::Fixed,
+            placement: BlockPlacement::Below(buffer.anchor_after(entry.range.start)),
+            height: message_height,
+            render: diagnostic_block_renderer(diagnostic, max_message_rows, allow_closing),
+            priority: 0,
+        }
+    }
+}
+#[derive(IntoComponent)]
+#[component(scope = "Version Control")]
+pub struct DiagnosticRenderer;
+
+fn new_entry(
+    range: Range<PointUtf16>,
+    severity: lsp::DiagnosticSeverity,
+    message: &str,
+    group_id: usize,
+    is_primary: bool,
+) -> DiagnosticEntry<PointUtf16> {
+    DiagnosticEntry {
+        range,
+        diagnostic: Diagnostic {
+            source: Some("rustc".to_string()),
+            code: None,
+            severity,
+            message: message.to_owned(),
+            group_id,
+            is_primary,
+            is_disk_based: false,
+            is_unnecessary: false,
+            data: None,
+        },
+    }
+}
+
+impl ComponentPreview for DiagnosticRenderer {
+    fn preview(window: &mut Window, cx: &mut App) -> AnyElement {
+        static EDITOR: OnceLock<Entity<Editor>> = OnceLock::new();
+        let editor = EDITOR.get_or_init(|| {
+            let buffer = cx.new(|cx| {
+                let mut buffer = Buffer::local(
+                    indoc::indoc! {
+                        r#" //
+                fn main() {
+                    println!("{}", foo(1, "Hello, world!"));
+                }
+
+                fn foo(_: i32) -> bool {
+                    true
+                }
+            "#
+                    },
+                    cx,
+                );
+                let entries = [
+                    new_entry(
+                        PointUtf16::new(2, 26)..PointUtf16::new(2, 42),
+                        DiagnosticSeverity::ERROR,
+                        "expected 1 argument, found 2",
+                        1,
+                        true,
+                    ),
+                    new_entry(
+                        PointUtf16::new(2, 19)..PointUtf16::new(2, 22),
+                        DiagnosticSeverity::ERROR,
+                        "this function takes 1 argument but 2 arguments were supplied",
+                        2,
+                        true,
+                    ),
+                    new_entry(
+                        PointUtf16::new(2, 26)..PointUtf16::new(2, 41),
+                        DiagnosticSeverity::HINT,
+                        "unexpected argument #2 of type `&'static str`",
+                        2,
+                        false,
+                    ),
+                    new_entry(
+                        PointUtf16::new(5, 3)..PointUtf16::new(5, 6),
+                        DiagnosticSeverity::HINT,
+                        "function defined here",
+                        2,
+                        false,
+                    ),
+                    new_entry(
+                        PointUtf16::new(2, 24)..PointUtf16::new(2, 41),
+                        DiagnosticSeverity::HINT,
+                        "remove the extra argument",
+                        2,
+                        false,
+                    ),
+                ];
+
+                let diagnostics = DiagnosticSet::new(entries, &buffer.snapshot());
+                buffer.update_diagnostics(lsp::LanguageServerId(1), diagnostics, cx);
+                buffer
+            });
+
+            cx.new(|cx| {
+                let mut editor = Editor::new(
+                    editor::EditorMode::Full,
+                    cx.new(|cx| MultiBuffer::singleton(buffer, cx)),
+                    None,
+                    window,
+                    cx,
+                );
+                editor.go_to_diagnostic_impl(Direction::Next, window, cx);
+
+                editor
+            })
+        });
+        let settings = ThemeSettings::get_global(cx);
+        let text_style = TextStyle {
+            color: cx.theme().colors().text,
+            font_family: settings.buffer_font.family.clone(),
+            font_fallbacks: settings.buffer_font.fallbacks.clone(),
+            font_size: settings.buffer_font_size(cx).into(),
+            font_weight: settings.buffer_font.weight,
+            line_height: relative(settings.buffer_line_height.value()),
+            ..TextStyle::default()
+        };
+        div()
+            .h(px(500.))
+            .w_full()
+            .child(EditorElement::new(
+                &editor,
+                EditorStyle {
+                    background: cx.theme().colors().editor_background,
+                    local_player: cx.theme().players().local(),
+                    text: text_style,
+                    ..EditorStyle::default()
+                },
+            ))
+            .into_any_element()
+    }
+}
