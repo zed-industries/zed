b18bd2d67b2e75a5a1daf25597dff6f9b4874658
Richard Feldman <oss@rtfeldman.com>
2025-04-08 16:33:55 -0400

    Revert "Use erased_serde for ToolOutput serialization"
    This reverts commit cad3ee8deffc452169e75a8b69f7d73763c312a2.

--- crates/language_model/src/tool_output.rs
+++ crates/language_model/src/tool_output.rs
@@ -1,10 +1,9 @@
-use gpui::{AnyElement, App, SharedString, Window};
+use gpui::{AnyElement, App, EntityId, SharedString, Window};
 use serde::{Deserialize, Serialize};
-use std::{fmt::Debug, hash::Hash, sync::Arc};
-use erased_serde::{Serialize as ErasedSerialize};
+use std::sync::Arc;
 
 /// Trait for tool output objects that can be provided to the language model
-pub trait ToolOutput: Send + Sync + Debug + PartialEq + Eq + Hash + ErasedSerialize {
+pub trait ToolOutput: Send + Sync {
     /// Returns a string that will be given to the model
     /// as the tool output.
     fn response_for_model(&self) -> SharedString;
@@ -23,8 +22,6 @@ impl ToolOutput for SharedString {
         self.clone()
     }
 }
-
-erased_serde::serialize_trait_object!(ToolOutput);
 
 /// A simple implementation of ToolOutput that wraps a string.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
