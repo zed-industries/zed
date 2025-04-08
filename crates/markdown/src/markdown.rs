c4d171569a1646d32796e97622b33e151b13b879
Fernando Tagawa <tagawafernando@gmail.com>
2025-04-08 07:22:35 -0300

    removing html comments

--- crates/markdown/src/markdown.rs
+++ crates/markdown/src/markdown.rs
@@ -1043,7 +1043,18 @@ impl Element for MarkdownElement {
                     builder.pop_text_style();
                 }
                 MarkdownEvent::Html => {
-                    builder.push_text(&parsed_markdown.source[range.clone()], range.clone());
+                    let html = &parsed_markdown.source[range.clone()];
+                    if html.starts_with("<!--") {
+                        builder.html_comment = true;
+                    }
+                    if html.trim_end().ends_with("-->") {
+                        builder.html_comment = false;
+                        continue;
+                    }
+                    if builder.html_comment {
+                        continue;
+                    }
+                    builder.push_text(html, range.start);
                 }
                 MarkdownEvent::InlineHtml => {
                     builder.push_text(&parsed_markdown.source[range.clone()], range.clone());
@@ -1260,6 +1271,7 @@ struct MarkdownElementBuilder {
     pending_line: PendingLine,
     rendered_links: Vec<RenderedLink>,
     current_source_index: usize,
+    html_comment: bool,
     base_text_style: TextStyle,
     text_style_stack: Vec<TextStyleRefinement>,
     code_block_stack: Vec<Option<Arc<Language>>>,
@@ -1287,6 +1299,7 @@ impl MarkdownElementBuilder {
             pending_line: PendingLine::default(),
             rendered_links: Vec::new(),
             current_source_index: 0,
+            html_comment: false,
             base_text_style,
             text_style_stack: Vec::new(),
             code_block_stack: Vec::new(),
@@ -1694,6 +1707,7 @@ mod tests {
             ],
         );
 
+<<<<<<< HEAD
         // Smart quotes.
         assert_mappings(&render_markdown("\"", cx), vec![vec![(0, 0), (3, 1)]]);
         assert_mappings(
@@ -1756,5 +1770,12 @@ mod tests {
                 );
             }
         }
+=======
+        let input = "```python\nprint('hello')\nprint('world')\n```";
+        assert_eq!(without_fences(input), "print('hello')\nprint('world')\n");
+
+        let input = "```<!--Comment Test-->```";
+        assert_eq!(without_fences(input), "");
+>>>>>>> c0355b6d9b (removing html comments)
     }
 }
