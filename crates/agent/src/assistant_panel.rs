17a271d4e28fa4440f7a8dc4cd054383ccca6029
Richard Feldman <oss@rtfeldman.com>
2025-04-09 11:24:53 -0400

    Revert to fix panic in inline assistant (#28428)
    This reverts commit f12a554f86fa1e2b9b937a9dd103aa7c9a019db9, which
    introduced a panic in inline assistant (cc @mgsloan) - I'm not sure what
    the motivation was for that change, but I figure we can revert to fix
    the inline assistant now and deal with that later. 😄
    
    Panic was:
    
    > Thread "main" panicked with "cannot read workspace::Workspace while it
    is already being updated" at
    /Users/rtfeldman/code/zed/crates/gpui/src/app/entity_map.rs:139:32
    
    
    Release Notes:
    
    - N/A

--- crates/agent/src/assistant_panel.rs
+++ crates/agent/src/assistant_panel.rs
@@ -227,14 +227,14 @@ impl AssistantPanel {
     ) -> Self {
         let thread = thread_store.update(cx, |this, cx| this.create_thread(cx));
         let fs = workspace.app_state().fs.clone();
-        let project = workspace.project();
+        let project = workspace.project().clone();
         let language_registry = project.read(cx).languages().clone();
         let workspace = workspace.weak_handle();
         let weak_self = cx.entity().downgrade();
 
         let message_editor_context_store = cx.new(|_cx| {
             crate::context_store::ContextStore::new(
-                project.downgrade(),
+                workspace.clone(),
                 Some(thread_store.downgrade()),
             )
         });
@@ -344,7 +344,7 @@ impl AssistantPanel {
 
         let message_editor_context_store = cx.new(|_cx| {
             crate::context_store::ContextStore::new(
-                self.project.downgrade(),
+                self.workspace.clone(),
                 Some(self.thread_store.downgrade()),
             )
         });
@@ -521,7 +521,7 @@ impl AssistantPanel {
                 this.set_active_view(thread_view, window, cx);
                 let message_editor_context_store = cx.new(|_cx| {
                     crate::context_store::ContextStore::new(
-                        this.project.downgrade(),
+                        this.workspace.clone(),
                         Some(this.thread_store.downgrade()),
                     )
                 });
