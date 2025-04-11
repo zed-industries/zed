5909d1258b9e14394873cc27d0452c0621e3e589
Cole Miller <cole@zed.dev>
2025-04-11 14:05:51 -0400

    Fix a panic in the git store (#28590)
    Closes #ISSUE
    
    Release Notes:
    
    - Fixed a panic that could occur when git statuses were updated.

--- crates/project/src/git_store.rs
+++ crates/project/src/git_store.rs
@@ -4048,7 +4048,7 @@ impl Repository {
                         for (repo_path, status) in &*statuses.entries {
                             changed_paths.remove(repo_path);
                             if cursor.seek_forward(&PathTarget::Path(repo_path), Bias::Left, &()) {
-                                if &cursor.item().unwrap().status == status {
+                                if cursor.item().is_some_and(|entry| entry.status == *status) {
                                     continue;
                                 }
                             }
