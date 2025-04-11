48464539f9e13619158e651a6325d62f92d53d8d
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-04-11 14:20:07 +0200

    Add separate member for tab rendering

--- crates/debugger_ui/src/session/running.rs
+++ crates/debugger_ui/src/session/running.rs
@@ -266,6 +266,7 @@ fn new_debugger_pane(
         })));
         pane.display_nav_history_buttons(None);
         pane.set_custom_drop_handle(cx, custom_drop_handle);
+        pane.set_should_display_tab_bar(|_, _| true);
         pane.set_render_tab_bar_buttons(cx, |_, _, _| (None, None));
         pane
     });
