062bc1d558bf5346703745178ac7b44a45b536b9
Junkui Zhang <364772080@qq.com>
2025-04-12 14:51:26 +0800

    Even simpler

--- crates/auto_update_helper/src/updater.rs
+++ crates/auto_update_helper/src/updater.rs
@@ -18,27 +18,25 @@ use crate::windows_impl::WM_JOB_UPDATED;
 // So if you add or remove a call to `retry_loop`, make sure to update this number too.
 pub(crate) const JOBS_COUNT: usize = 6;
 
-pub(crate) fn perform_update(app_dir: &Path, hwnd: Option<isize>) -> Result<()> {
-    let hwnd = hwnd.map(|ptr| HWND(ptr as _));
+type Job = fn(&Path) -> Result<()>;
 
-    // Delete old files
-    retry_loop(hwnd, || {
+#[cfg(not(test))]
+const JOBS: [Job; 6] = [
+    |app_dir| {
         let zed_executable = app_dir.join("Zed.exe");
         log::info!("Removing old file: {}", zed_executable.display());
         std::fs::remove_file(&zed_executable).context(format!(
             "Failed to remove old file {}",
             zed_executable.display()
         ))
-    })?;
-    retry_loop(hwnd, || {
+    },
+    |app_dir| {
         let zed_cli = app_dir.join("bin\\zed.exe");
         log::info!("Removing old file: {}", zed_cli.display());
         std::fs::remove_file(&zed_cli)
             .context(format!("Failed to remove old file {}", zed_cli.display()))
-    })?;
-
-    // Copy new files
-    retry_loop(hwnd, || {
+    },
+    |app_dir| {
         let zed_executable_source = app_dir.join("install\\Zed.exe");
         let zed_executable_dest = app_dir.join("Zed.exe");
         log::info!(
@@ -46,13 +44,15 @@ pub(crate) fn perform_update(app_dir: &Path, hwnd: Option<isize>) -> Result<()> 
             zed_executable_source.display(),
             zed_executable_dest.display()
         );
-        std::fs::copy(&zed_executable_source, &zed_executable_dest).context(format!(
-            "Failed to copy new file {} to {}",
-            zed_executable_source.display(),
-            zed_executable_dest.display()
-        ))
-    })?;
-    retry_loop(hwnd, || {
+        std::fs::copy(&zed_executable_source, &zed_executable_dest)
+            .map(|_| ())
+            .context(format!(
+                "Failed to copy new file {} to {}",
+                zed_executable_source.display(),
+                zed_executable_dest.display()
+            ))
+    },
+    |app_dir| {
         let zed_cli_source = app_dir.join("install\\bin\\zed.exe");
         let zed_cli_dest = app_dir.join("bin\\zed.exe");
         log::info!(
@@ -60,100 +60,82 @@ pub(crate) fn perform_update(app_dir: &Path, hwnd: Option<isize>) -> Result<()> 
             zed_cli_source.display(),
             zed_cli_dest.display()
         );
-        std::fs::copy(&zed_cli_source, &zed_cli_dest).context(format!(
-            "Failed to copy new file {} to {}",
-            zed_cli_source.display(),
-            zed_cli_dest.display()
-        ))
-    })?;
-
-    // Post cleanup jobs
-    retry_loop(hwnd, || {
+        std::fs::copy(&zed_cli_source, &zed_cli_dest)
+            .map(|_| ())
+            .context(format!(
+                "Failed to copy new file {} to {}",
+                zed_cli_source.display(),
+                zed_cli_dest.display()
+            ))
+    },
+    |app_dir| {
         let updates_folder = app_dir.join("updates");
         log::info!("Cleaning up: {}", updates_folder.display());
         std::fs::remove_dir_all(&updates_folder).context(format!(
             "Failed to remove updates folder {}",
             updates_folder.display()
         ))
-    })?;
-    retry_loop(hwnd, || {
+    },
+    |app_dir| {
         let installer_folder = app_dir.join("install");
         log::info!("Cleaning up: {}", installer_folder.display());
         std::fs::remove_dir_all(&installer_folder).context(format!(
             "Failed to remove installer folder {}",
             installer_folder.display()
         ))
-    })?;
-
-    Ok(())
-}
-
-#[cfg(not(test))]
-fn retry_loop<R>(hwnd: Option<HWND>, f: impl Fn() -> Result<R>) -> Result<()> {
-    let start = Instant::now();
-    while start.elapsed().as_secs() <= 2 {
-        match f() {
-            Ok(_) => {
-                unsafe { PostMessageW(hwnd, WM_JOB_UPDATED, WPARAM(0), LPARAM(0))? };
-                return Ok(());
-            }
-            Err(anyhow_err) => {
-                let io_err = anyhow_err.downcast_ref::<std::io::Error>().unwrap();
-                if io_err.kind() == std::io::ErrorKind::NotFound {
-                    log::warn!("File or folder not found.");
-                    unsafe { PostMessageW(hwnd, WM_JOB_UPDATED, WPARAM(0), LPARAM(0))? };
-                    return Ok(());
-                } else {
-                    println!("Operation failed: {}", anyhow_err);
-                    log::error!("Operation failed: {}", anyhow_err);
-                    // wait for a bit before retrying
-                    std::thread::sleep(Duration::from_millis(10));
-                }
-            }
-        }
-    }
-    Err(anyhow::anyhow!("Timed out"))
-}
+    },
+];
 
 #[cfg(test)]
-fn retry_loop<R>(hwnd: Option<HWND>, _: impl Fn() -> Result<R>) -> Result<()> {
-    let start = Instant::now();
-    while start.elapsed().as_secs() <= 2 {
-        let result: anyhow::Result<()> = if let Ok(config) = std::env::var("ZED_AUTO_UPDATE") {
-            match config.as_str() {
-                "err" => {
-                    std::thread::sleep(Duration::from_millis(500));
-                    Err(std::io::Error::new(
-                        std::io::ErrorKind::Other,
-                        "Simulated error",
-                    ))
-                    .context("Anyhow!")
-                }
-                _ => panic!("Unknown ZED_AUTO_UPDATE value: {}", config),
+const JOBS: [Job; 1] = [|_| {
+    if let Ok(config) = std::env::var("ZED_AUTO_UPDATE") {
+        match config.as_str() {
+            "err" => {
+                std::thread::sleep(Duration::from_millis(500));
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Simulated error",
+                ))
+                .context("Anyhow!")
             }
-        } else {
-            Ok(())
-        };
-        match result {
-            Ok(_) => {
-                unsafe { PostMessageW(hwnd, WM_JOB_UPDATED, WPARAM(0), LPARAM(0))? };
-                return Ok(());
+            _ => panic!("Unknown ZED_AUTO_UPDATE value: {}", config),
+        }
+    } else {
+        Ok(())
+    }
+}];
+
+pub(crate) fn perform_update(app_dir: &Path, hwnd: Option<isize>) -> Result<()> {
+    let hwnd = hwnd.map(|ptr| HWND(ptr as _));
+
+    for job in JOBS.iter() {
+        let start = Instant::now();
+        loop {
+            if start.elapsed().as_secs() > 2 {
+                return Err(anyhow::anyhow!("Timed out"));
             }
-            Err(anyhow_err) => {
-                let io_err = anyhow_err.downcast_ref::<std::io::Error>().unwrap();
-                if io_err.kind() == std::io::ErrorKind::NotFound {
-                    log::warn!("File or folder not found.");
+            match (*job)(app_dir) {
+                Ok(_) => {
                     unsafe { PostMessageW(hwnd, WM_JOB_UPDATED, WPARAM(0), LPARAM(0))? };
-                    return Ok(());
-                } else {
-                    log::error!("Operation failed: {}", anyhow_err);
-                    // wait for a bit before retrying
-                    std::thread::sleep(Duration::from_millis(10));
+                    break;
+                }
+                Err(err) => {
+                    // Check if it's a "not found" error
+                    let io_err = err.downcast_ref::<std::io::Error>().unwrap();
+                    if io_err.kind() == std::io::ErrorKind::NotFound {
+                        log::warn!("File or folder not found.");
+                        unsafe { PostMessageW(hwnd, WM_JOB_UPDATED, WPARAM(0), LPARAM(0))? };
+                        break;
+                    }
+
+                    log::error!("Operation failed: {}", err);
+                    std::thread::sleep(Duration::from_millis(50));
                 }
             }
         }
     }
-    Err(anyhow::anyhow!("Timed out"))
+
+    Ok(())
 }
 
 #[cfg(test)]
