b794919842c00662f9376dee0154909b84795f20
Richard Feldman <oss@rtfeldman.com>
2025-04-15 00:54:25 -0400

    Add contents_tool (#28738)
    This is a combination of the "read file" and "list directory contents"
    tools as part of a push to reduce our quantity of builtin tools by
    combining some of them.
    
    The functionality is all there for this tool, although there's room for
    improvement on the visuals side: it currently always shows the same icon
    and always says "Read" - so you can't tell at a glance when it's reading
    a directory vs an individual file. Changing this will require a change
    to the `Tool` trait, which can be in a separate PR. (FYI @danilo-leal!)
    
    <img width="606" alt="Screenshot 2025-04-14 at 11 56 27 PM"
    src="https://github.com/user-attachments/assets/bded72af-6476-4469-97c6-2f344629b0e4"
    />
    
    Release Notes:
    
    - Added `contents` tool

--- crates/assistant_tools/src/assistant_tools.rs
+++ crates/assistant_tools/src/assistant_tools.rs
@@ -1,6 +1,7 @@
 mod batch_tool;
 mod code_action_tool;
 mod code_symbols_tool;
+mod contents_tool;
 mod copy_path_tool;
 mod create_directory_tool;
 mod create_file_tool;
@@ -33,6 +34,7 @@ use move_path_tool::MovePathTool;
 use crate::batch_tool::BatchTool;
 use crate::code_action_tool::CodeActionTool;
 use crate::code_symbols_tool::CodeSymbolsTool;
+use crate::contents_tool::ContentsTool;
 use crate::create_directory_tool::CreateDirectoryTool;
 use crate::create_file_tool::CreateFileTool;
 use crate::delete_path_tool::DeletePathTool;
@@ -69,6 +71,7 @@ pub fn init(http_client: Arc<HttpClientWithUrl>, cx: &mut App) {
     registry.register_tool(NowTool);
     registry.register_tool(OpenTool);
     registry.register_tool(CodeSymbolsTool);
+    registry.register_tool(ContentsTool);
     registry.register_tool(PathSearchTool);
     registry.register_tool(ReadFileTool);
     registry.register_tool(RegexSearchTool);
