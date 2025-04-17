b750ee732260633b464ae5b273c369c5e51614c9
MrSubidubi <dev@bahn.sh>
2025-04-18 00:15:01 +0200

    Minimize Changes to `rem_size`

--- crates/editor/src/element.rs
+++ crates/editor/src/element.rs
@@ -6722,37 +6722,40 @@ impl EditorElement {
     ///
     /// This allows UI elements to scale based on the `buffer_font_size`.
     fn rem_size(&self, cx: &mut App) -> Option<Pixels> {
-        let is_some = match self.editor.read(cx).mode {
+        match self.editor.read(cx).mode {
             EditorMode::Full {
-                scale_ui_elements_with_buffer_font_size,
+                scale_ui_elements_with_buffer_font_size: true,
                 ..
-            } => !scale_ui_elements_with_buffer_font_size,
-            EditorMode::Minimap => true,
-            EditorMode::SingleLine { .. } | EditorMode::AutoHeight { .. } => false,
-        };
-        if is_some {
-            match self.style.text.font_size {
-                AbsoluteLength::Pixels(pixels) => {
-                    let rem_size_scale = {
-                        // Our default UI font size is 14px on a 16px base scale.
-                        // This means the default UI font size is 0.875rems.
-                        let default_font_size_scale = 14. / ui::BASE_REM_SIZE_IN_PX;
-
-                        // We then determine the delta between a single rem and the default font
-                        // size scale.
-                        let default_font_size_delta = 1. - default_font_size_scale;
-
-                        // Finally, we add this delta to 1rem to get the scale factor that
-                        // should be used to scale up the UI.
-                        1. + default_font_size_delta
-                    };
-
-                    Some(pixels * rem_size_scale)
-                }
-                AbsoluteLength::Rems(rems) => Some(rems.to_pixels(ui::BASE_REM_SIZE_IN_PX.into())),
             }
-        } else {
-            None
+            | EditorMode::Minimap => {
+                let buffer_font_size = self.style.text.font_size;
+                match buffer_font_size {
+                    AbsoluteLength::Pixels(pixels) => {
+                        let rem_size_scale = {
+                            // Our default UI font size is 14px on a 16px base scale.
+                            // This means the default UI font size is 0.875rems.
+                            let default_font_size_scale = 14. / ui::BASE_REM_SIZE_IN_PX;
+
+                            // We then determine the delta between a single rem and the default font
+                            // size scale.
+                            let default_font_size_delta = 1. - default_font_size_scale;
+
+                            // Finally, we add this delta to 1rem to get the scale factor that
+                            // should be used to scale up the UI.
+                            1. + default_font_size_delta
+                        };
+
+                        Some(pixels * rem_size_scale)
+                    }
+                    AbsoluteLength::Rems(rems) => {
+                        Some(rems.to_pixels(ui::BASE_REM_SIZE_IN_PX.into()))
+                    }
+                }
+            }
+            // We currently use single-line and auto-height editors in UI contexts,
+            // so we don't want to scale everything with the buffer font size, as it
+            // ends up looking off.
+            _ => None,
         }
     }
 }
