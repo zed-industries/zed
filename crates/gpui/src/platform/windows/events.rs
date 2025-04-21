acb79cc4ce962843858acb30c47bfb5af50215a3
张小白 <364772080@qq.com>
2025-04-21 16:06:49 +0800

    Use `.scale()` method

--- crates/gpui/src/platform/windows/events.rs
+++ crates/gpui/src/platform/windows/events.rs
@@ -146,15 +146,14 @@ fn handle_get_min_max_info_msg(
     state_ptr: Rc<WindowsWindowStatePtr>,
 ) -> Option<isize> {
     let lock = state_ptr.state.borrow();
-
     if let Some(min_size) = lock.min_size {
         let scale_factor = lock.scale_factor;
         drop(lock);
 
         unsafe {
             let minmax_info = &mut *(lparam.0 as *mut MINMAXINFO);
-            minmax_info.ptMinTrackSize.x = (min_size.width.0 * scale_factor) as i32;
-            minmax_info.ptMinTrackSize.y = (min_size.height.0 * scale_factor) as i32;
+            minmax_info.ptMinTrackSize.x = min_size.width.scale(scale_factor).0 as i32;
+            minmax_info.ptMinTrackSize.y = min_size.height.scale(scale_factor).0 as i32;
         }
     }
 
