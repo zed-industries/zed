f8ac6eef75b6797178990c57000c5b0cbc651694
Ho Chun Lau <39511811+kylelau519@users.noreply.github.com>
2025-04-21 16:39:17 +0100

    terminal: Add right-click in terminal to create a new selection if none is present (#29131)
    This PR adds functionality to right click in terminal create new
    selection if none present. The selection is identical with double click
    a text in terminal, plus the logic is moved from the double-click in the
    terminal::mouse_down.
    
    Closes #28237 
    
    Release Notes:
    - Adds functionality to right click in terminal create new selection if
    none present

--- crates/terminal_view/src/terminal_view.rs
+++ crates/terminal_view/src/terminal_view.rs
@@ -1278,6 +1278,11 @@ impl Render for TerminalView {
                 MouseButton::Right,
                 cx.listener(|this, event: &MouseDownEvent, window, cx| {
                     if !this.terminal.read(cx).mouse_mode(event.modifiers.shift) {
+                        if this.terminal.read(cx).last_content.selection.is_none() {
+                            this.terminal.update(cx, |terminal, _| {
+                                terminal.select_word_at_event_position(event);
+                            });
+                        };
                         this.deploy_context_menu(event.position, window, cx);
                         cx.notify();
                     }
