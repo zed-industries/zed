1277d4086aa214af25f09f5d466b60b097448a59
Danilo Leal <daniloleal09@gmail.com>
2025-04-25 18:07:09 -0300

    Enable horizontal scrolling
    Co-authored-by: Michael Sloan <mgsloan@gmail.com>
    Co-authored-by: Agus Zubiaga <hi@aguz.me>

--- crates/assistant_tools/src/edit_file_tool.rs
+++ crates/assistant_tools/src/edit_file_tool.rs
@@ -299,9 +299,10 @@ impl EditFileToolCard {
             editor.set_show_scrollbars(false, cx);
             editor.set_show_gutter(false, cx);
             editor.disable_inline_diagnostics();
-            editor.disable_scrolling(cx);
+            // // editor.disable_scrolling(cx);
             editor.disable_expand_excerpt_buttons(cx);
             editor.set_soft_wrap_mode(SoftWrap::None, cx);
+            editor.scroll_manager.forbid_vertical_scroll();
             editor.set_show_breakpoints(false, cx);
             editor.set_show_code_actions(false, cx);
             editor.set_show_git_diff_gutter(false, cx);
