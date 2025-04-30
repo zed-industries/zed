8f7d5ecf810f0e7f3c7345f6332e3f86e1087522
Junkui Zhang <364772080@qq.com>
2025-04-30 16:42:11 +0800

    add new trait

--- crates/gpui/src/platform/keyboard.rs
+++ crates/gpui/src/platform/keyboard.rs
@@ -1,7 +1,15 @@
+use super::Keystroke;
+
 /// A trait for platform-specific keyboard layouts
 pub trait PlatformKeyboardLayout {
     /// Get the keyboard layout ID, which should be unique to the layout
     fn id(&self) -> &str;
     /// Get the keyboard layout display name
     fn name(&self) -> &str;
+}
+
+/// TODO:
+pub trait PlatformKeyboardMapper {
+    /// TODO:
+    fn vscode_keystroke_to_gpui_keystroke(&self, keystroke: Keystroke) -> Keystroke;
 }
