932d776efd5bc6222d3421fae7ed246addd1b0eb
Junkui Zhang <364772080@qq.com>
2025-04-30 18:33:13 +0800

    macOS impl

--- crates/gpui/src/platform/mac/keyboard.rs
+++ crates/gpui/src/platform/mac/keyboard.rs
@@ -1,8 +1,9 @@
 use std::ffi::{CStr, c_void};
 
+use collections::HashMap;
 use objc::{msg_send, runtime::Object, sel, sel_impl};
 
-use crate::PlatformKeyboardLayout;
+use crate::{Keystroke, PlatformKeyboardLayout, PlatformKeyboardMapper};
 
 use super::{
     TISCopyCurrentKeyboardLayoutInputSource, TISGetInputSourceProperty, kTISPropertyInputSourceID,
@@ -26,24 +27,55 @@ impl PlatformKeyboardLayout for MacKeyboardLayout {
 
 impl MacKeyboardLayout {
     pub(crate) fn new() -> Self {
-        unsafe {
-            let current_keyboard = TISCopyCurrentKeyboardLayoutInputSource();
-
-            let id: *mut Object = TISGetInputSourceProperty(
-                current_keyboard,
-                kTISPropertyInputSourceID as *const c_void,
-            );
-            let id: *const std::os::raw::c_char = msg_send![id, UTF8String];
-            let id = CStr::from_ptr(id).to_str().unwrap().to_string();
-
-            let name: *mut Object = TISGetInputSourceProperty(
-                current_keyboard,
-                kTISPropertyLocalizedName as *const c_void,
-            );
+        let (keyboard, id) = get_keyboard_layout_id();
+        let name = unsafe {
+            let name: *mut Object =
+                TISGetInputSourceProperty(keyboard, kTISPropertyLocalizedName as *const c_void);
             let name: *const std::os::raw::c_char = msg_send![name, UTF8String];
-            let name = CStr::from_ptr(name).to_str().unwrap().to_string();
+            CStr::from_ptr(name).to_str().unwrap().to_string()
+        };
 
-            Self { id, name }
+        Self { id, name }
+    }
+}
+
+fn get_keyboard_layout_id() -> (*mut Object, String) {
+    unsafe {
+        let current_keyboard = TISCopyCurrentKeyboardLayoutInputSource();
+
+        let id: *mut Object =
+            TISGetInputSourceProperty(current_keyboard, kTISPropertyInputSourceID as *const c_void);
+        let id: *const std::os::raw::c_char = msg_send![id, UTF8String];
+        (
+            current_keyboard,
+            CStr::from_ptr(id).to_str().unwrap().to_string(),
+        )
+    }
+}
+
+pub(crate) struct MacKeyboardMapper {
+    key_to_code: HashMap<char, u32>,
+    code_to_shifted_key: HashMap<u32, char>,
+}
+
+impl MacKeyboardMapper {
+    pub(crate) fn new() -> Self {
+        let mut key_to_code = HashMap::default();
+        let mut code_to_shifted_key = HashMap::default();
+
+        // Populate the mappings here
+        key_to_code.insert('a', 0);
+        code_to_shifted_key.insert(0, 'A');
+
+        Self {
+            key_to_code,
+            code_to_shifted_key,
         }
+    }
+}
+
+impl PlatformKeyboardMapper for MacKeyboardMapper {
+    fn vscode_keystroke_to_gpui_keystroke(&self, keystroke: Keystroke) -> Keystroke {
+        keystroke
     }
 }
