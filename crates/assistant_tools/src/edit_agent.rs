9f3c7bdc151d3dc59c5767b282876f317d0f30be
Antonio Scandurra <me@as-cii.com>
2025-04-30 18:05:04 +0200

    Reindent new chunks

--- crates/assistant_tools/src/edit_agent.rs
+++ crates/assistant_tools/src/edit_agent.rs
@@ -13,13 +13,13 @@ use futures::{
     stream::BoxStream,
 };
 use gpui::{AppContext, AsyncApp, Entity, Task};
-use language::{Bias, Buffer, BufferSnapshot, Point};
+use language::{Bias, Buffer, BufferSnapshot, LineIndent, Point};
 use language_model::{
     LanguageModel, LanguageModelCompletionError, LanguageModelRequest, LanguageModelRequestMessage,
     MessageContent, Role,
 };
 use serde::Serialize;
-use std::{mem, ops::Range, path::PathBuf, sync::Arc};
+use std::{cmp, iter, mem, ops::Range, path::PathBuf, sync::Arc, task::Poll};
 use streaming_diff::{CharOperation, StreamingDiff};
 
 #[derive(Serialize)]
@@ -87,19 +87,39 @@ impl EditAgent {
             let snapshot = buffer.read_with(cx, |buffer, _cx| buffer.snapshot())?;
             let compute_edits = cx.background_spawn(async move {
                 if let Some(range) = Self::resolve_location(&snapshot, &old_text_query) {
+                    let buffer_start_indent =
+                        snapshot.line_indent_for_row(snapshot.offset_to_point(range.start).row);
+                    let query_start_indent = old_text_query
+                        .lines()
+                        .next()
+                        .map_or(buffer_start_indent, |line| {
+                            LineIndent::from_iter(line.chars())
+                        });
+                    let indent_delta = if buffer_start_indent.tabs > 0 {
+                        IndentDelta::Tabs(
+                            buffer_start_indent.tabs as isize - query_start_indent.tabs as isize,
+                        )
+                    } else {
+                        IndentDelta::Spaces(
+                            buffer_start_indent.spaces as isize
+                                - query_start_indent.spaces as isize,
+                        )
+                    };
+
                     let old_text = snapshot.text_for_range(range.clone()).collect::<String>();
                     let mut diff = StreamingDiff::new(old_text);
                     let mut edit_start = range.start;
-
-                    while let Some(edit_event) = edit_events.next().await {
-                        let EditParserEvent::NewTextChunk { chunk, done } = edit_event? else {
-                            break;
-                        };
-
-                        let mut char_operations = diff.push_new(&chunk);
-                        if done {
-                            char_operations.extend(mem::take(&mut diff).finish());
-                        }
+                    let mut new_text_chunks =
+                        Self::reindent_new_text_chunks(indent_delta, &mut edit_events);
+                    let mut done = false;
+                    while !done {
+                        let char_operations =
+                            if let Some(new_text_chunk) = new_text_chunks.next().await {
+                                diff.push_new(&new_text_chunk?)
+                            } else {
+                                done = true;
+                                mem::take(&mut diff).finish()
+                            };
 
                         for op in char_operations {
                             match op {
@@ -116,10 +136,6 @@ impl EditAgent {
                                 }
                                 CharOperation::Keep { bytes } => edit_start += bytes,
                             }
-                        }
-
-                        if done {
-                            break;
                         }
                     }
                 }
@@ -175,6 +191,85 @@ impl EditAgent {
             Ok(output)
         });
         (raw_output, rx)
+    }
+
+    fn reindent_new_text_chunks(
+        delta: IndentDelta,
+        mut stream: impl Unpin + Stream<Item = Result<EditParserEvent>>,
+    ) -> impl Stream<Item = Result<String>> {
+        let mut buffer = String::new();
+        let mut in_leading_whitespace = true;
+        let mut done = false;
+        futures::stream::poll_fn(move |cx| {
+            while !done {
+                let (chunk, is_last_chunk) = match stream.poll_next_unpin(cx) {
+                    Poll::Ready(Some(Ok(EditParserEvent::NewTextChunk { chunk, done }))) => {
+                        (chunk, done)
+                    }
+                    Poll::Ready(Some(Err(err))) => return Poll::Ready(Some(Err(err))),
+                    Poll::Pending => return Poll::Pending,
+                    _ => return Poll::Ready(None),
+                };
+
+                buffer.push_str(&chunk);
+
+                let mut indented_new_text = String::new();
+                let mut start_ix = 0;
+                let mut newlines = buffer.match_indices('\n').peekable();
+                loop {
+                    let (line_end, is_pending_line) = match newlines.next() {
+                        Some((ix, _)) => (ix, false),
+                        None => (buffer.len(), true),
+                    };
+                    let line = &buffer[start_ix..line_end];
+
+                    if in_leading_whitespace {
+                        if let Some(non_whitespace_ix) = line.find(|c| delta.character() != c) {
+                            // We found a non-whitespace character, adjust
+                            // indentation based on the delta.
+                            let new_indent_len =
+                                cmp::max(0, non_whitespace_ix as isize + delta.len()) as usize;
+                            indented_new_text
+                                .extend(iter::repeat(delta.character()).take(new_indent_len));
+                            indented_new_text.push_str(&line[non_whitespace_ix..]);
+                            in_leading_whitespace = false;
+                        } else if is_pending_line {
+                            // We're still in leading whitespace and this line is incomplete.
+                            // Stop processing until we receive more input.
+                            break;
+                        } else {
+                            // This line is entirely whitespace. Push it without indentation.
+                            indented_new_text.push_str(line);
+                        }
+                    } else {
+                        indented_new_text.push_str(line);
+                    }
+
+                    if is_pending_line {
+                        start_ix = line_end;
+                        break;
+                    } else {
+                        in_leading_whitespace = true;
+                        indented_new_text.push('\n');
+                        start_ix = line_end + 1;
+                    }
+                }
+                buffer.replace_range(..start_ix, "");
+
+                // This was the last chunk, push all the buffered content as-is.
+                if is_last_chunk {
+                    indented_new_text.push_str(&buffer);
+                    buffer.clear();
+                    done = true;
+                }
+
+                if !indented_new_text.is_empty() {
+                    return Poll::Ready(Some(Ok(indented_new_text)));
+                }
+            }
+
+            Poll::Ready(None)
+        })
     }
 
     async fn request_edits(
@@ -330,6 +425,28 @@ impl EditAgent {
     }
 }
 
+#[derive(Copy, Clone, Debug)]
+enum IndentDelta {
+    Spaces(isize),
+    Tabs(isize),
+}
+
+impl IndentDelta {
+    fn character(&self) -> char {
+        match self {
+            IndentDelta::Spaces(_) => ' ',
+            IndentDelta::Tabs(_) => '\t',
+        }
+    }
+
+    fn len(&self) -> isize {
+        match self {
+            IndentDelta::Spaces(n) => *n,
+            IndentDelta::Tabs(n) => *n,
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum SearchDirection {
     Up,
@@ -387,10 +504,56 @@ mod tests {
     use util::test::{generate_marked_text, marked_text_ranges};
 
     #[gpui::test(iterations = 10)]
+    async fn test_indentation(cx: &mut TestAppContext, mut rng: StdRng) {
+        let agent = init_test(cx).await;
+        let buffer = cx.new(|cx| {
+            Buffer::local(
+                indoc! {"
+                    lorem
+                            ipsum
+                            dolor
+                            sit
+                "},
+                cx,
+            )
+        });
+        let raw_edits = simulate_llm_output(
+            &mut rng,
+            indoc! {"
+                <old_text>
+                    ipsum
+                    dolor
+                    sit
+                </old_text>
+                <new_text>
+                    ipsum
+                    dolor
+                    sit
+                amet
+                </new_text>
+            "},
+        );
+        agent
+            .apply_edits(buffer.clone(), raw_edits, &mut cx.to_async())
+            .await
+            .unwrap();
+        pretty_assertions::assert_eq!(
+            buffer.read_with(cx, |buffer, _| buffer.snapshot().text()),
+            indoc! {"
+                lorem
+                        ipsum
+                        dolor
+                        sit
+                    amet
+            "}
+        );
+    }
+
+    #[gpui::test(iterations = 10)]
     async fn test_dependent_edits(cx: &mut TestAppContext, mut rng: StdRng) {
         let agent = init_test(cx).await;
         let buffer = cx.new(|cx| Buffer::local("abc\ndef\nghi", cx));
-        let raw_edits = to_random_chunk_stream(
+        let raw_edits = simulate_llm_output(
             &mut rng,
             indoc! {"
                 <old_text>
@@ -422,7 +585,7 @@ mod tests {
     async fn test_old_text_hallucination(cx: &mut TestAppContext, mut rng: StdRng) {
         let agent = init_test(cx).await;
         let buffer = cx.new(|cx| Buffer::local("abc\ndef\nghi", cx));
-        let raw_edits = to_random_chunk_stream(
+        let raw_edits = simulate_llm_output(
             &mut rng,
             indoc! {"
                 <old_text>
@@ -576,6 +739,89 @@ mod tests {
         );
     }
 
+    #[gpui::test(iterations = 100)]
+    async fn test_indent_new_text_chunks(mut rng: StdRng) {
+        let chunks = to_random_chunks(&mut rng, "    abc\n  def\n      ghi");
+        let new_text_chunks = stream::iter(chunks.iter().enumerate().map(|(index, chunk)| {
+            Ok(EditParserEvent::NewTextChunk {
+                chunk: chunk.clone(),
+                done: index == chunks.len() - 1,
+            })
+        }));
+        let indented_chunks =
+            EditAgent::reindent_new_text_chunks(IndentDelta::Spaces(2), new_text_chunks)
+                .collect::<Vec<_>>()
+                .await;
+        let new_text = indented_chunks
+            .into_iter()
+            .collect::<Result<String>>()
+            .unwrap();
+        assert_eq!(new_text, "      abc\n    def\n        ghi");
+    }
+
+    #[gpui::test(iterations = 100)]
+    async fn test_outdent_new_text_chunks(mut rng: StdRng) {
+        let chunks = to_random_chunks(&mut rng, "\t\t\t\tabc\n\t\tdef\n\t\t\t\t\t\tghi");
+        let new_text_chunks = stream::iter(chunks.iter().enumerate().map(|(index, chunk)| {
+            Ok(EditParserEvent::NewTextChunk {
+                chunk: chunk.clone(),
+                done: index == chunks.len() - 1,
+            })
+        }));
+        let indented_chunks =
+            EditAgent::reindent_new_text_chunks(IndentDelta::Tabs(-2), new_text_chunks)
+                .collect::<Vec<_>>()
+                .await;
+        let new_text = indented_chunks
+            .into_iter()
+            .collect::<Result<String>>()
+            .unwrap();
+        assert_eq!(new_text, "\t\tabc\ndef\n\t\t\t\tghi");
+    }
+
+    #[gpui::test(iterations = 100)]
+    async fn test_random_indents(mut rng: StdRng) {
+        let len = rng.gen_range(1..=100);
+        let new_text = util::RandomCharIter::new(&mut rng)
+            .with_simple_text()
+            .take(len)
+            .collect::<String>();
+        let new_text = new_text
+            .split('\n')
+            .map(|line| format!("{}{}", " ".repeat(rng.gen_range(0..=8)), line))
+            .collect::<Vec<_>>()
+            .join("\n");
+        let delta = IndentDelta::Spaces(rng.gen_range(-4..=4));
+
+        let chunks = to_random_chunks(&mut rng, &new_text);
+        let new_text_chunks = stream::iter(chunks.iter().enumerate().map(|(index, chunk)| {
+            Ok(EditParserEvent::NewTextChunk {
+                chunk: chunk.clone(),
+                done: index == chunks.len() - 1,
+            })
+        }));
+        let reindented_chunks = EditAgent::reindent_new_text_chunks(delta, new_text_chunks)
+            .collect::<Vec<_>>()
+            .await;
+        let actual_reindented_text = reindented_chunks
+            .into_iter()
+            .collect::<Result<String>>()
+            .unwrap();
+        let expected_reindented_text = new_text
+            .split('\n')
+            .map(|line| {
+                if let Some(ix) = line.find(|c| c != ' ') {
+                    let new_indent = cmp::max(0, ix as isize + delta.len()) as usize;
+                    format!("{}{}", " ".repeat(new_indent), &line[ix..])
+                } else {
+                    line.to_string()
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+        assert_eq!(actual_reindented_text, expected_reindented_text);
+    }
+
     #[track_caller]
     fn assert_location_resolution(text_with_expected_range: &str, query: &str, cx: &mut App) {
         let (text, _) = marked_text_ranges(text_with_expected_range, false);
@@ -587,10 +833,7 @@ mod tests {
         pretty_assertions::assert_eq!(text_with_actual_range, text_with_expected_range);
     }
 
-    fn to_random_chunk_stream(
-        rng: &mut StdRng,
-        input: &str,
-    ) -> impl 'static + Send + Stream<Item = Result<String, LanguageModelCompletionError>> {
+    fn to_random_chunks(rng: &mut StdRng, input: &str) -> Vec<String> {
         let chunk_count = rng.gen_range(1..=cmp::min(input.len(), 50));
         let mut chunk_indices = (0..input.len()).choose_multiple(rng, chunk_count);
         chunk_indices.sort();
@@ -599,10 +842,17 @@ mod tests {
         let mut chunks = Vec::new();
         let mut last_ix = 0;
         for chunk_ix in chunk_indices {
-            chunks.push(Ok(input[last_ix..chunk_ix].to_string()));
+            chunks.push(input[last_ix..chunk_ix].to_string());
             last_ix = chunk_ix;
         }
-        stream::iter(chunks)
+        chunks
+    }
+
+    fn simulate_llm_output(
+        rng: &mut StdRng,
+        output: &str,
+    ) -> impl 'static + Send + Stream<Item = Result<String, LanguageModelCompletionError>> {
+        stream::iter(to_random_chunks(rng, output).into_iter().map(Ok))
     }
 
     async fn init_test(cx: &mut TestAppContext) -> EditAgent {
