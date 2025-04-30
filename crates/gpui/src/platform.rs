87066e9a5688388aee2914e0884959f23d74428e
Junkui Zhang <364772080@qq.com>
2025-04-30 15:46:46 +0800

    fix windows

--- crates/gpui/src/platform.rs
+++ crates/gpui/src/platform.rs
@@ -1681,11 +1681,3 @@ impl From<String> for ClipboardString {
         }
     }
 }
-
-/// A trait for platform-specific keyboard layouts
-pub trait PlatformKeyboardLayout {
-    /// Get the keyboard layout ID, which should be unique to the layout
-    fn id(&self) -> &str;
-    /// Get the keyboard layout display name
-    fn name(&self) -> &str;
-}
