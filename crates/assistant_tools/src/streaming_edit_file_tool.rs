29ccc8b18f948c94aee095db4cb95284b1e70622
Antonio Scandurra <me@as-cii.com>
2025-05-04 14:39:42 +0200

    Update streaming_edit_file_tool.rs

--- crates/assistant_tools/src/streaming_edit_file_tool.rs
+++ crates/assistant_tools/src/streaming_edit_file_tool.rs
@@ -62,8 +62,8 @@ pub struct StreamingEditFileToolInput {
     /// If true, this tool will recreate the file from scratch.
     /// If false, this tool will produce granular edits to an existing file.
     ///
-    /// When a file already exists, always prefer editing it as opposed to
-    /// recreating it from scratch.
+    /// When a file already exists or you just created it, always prefer editing
+    /// it as opposed to recreating it from scratch.
     pub create_or_overwrite: bool,
 }
 
