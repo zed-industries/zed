b1a6c35e3f503c0651b23aa314fb21a243d43487
Ben Kunkle <ben@zed.dev>
2025-05-06 17:30:33 -0500

    update python/outline.scm for decorator support

--- crates/languages/src/python/outline.scm
+++ crates/languages/src/python/outline.scm
@@ -1,11 +1,52 @@
+; Capture decorators for standalone annotation highlighting
 (decorator) @annotation
 
-(class_definition
-    "class" @context
-    name: (identifier) @name
-    ) @item
+; class definitions
+[
+    (module
+        (class_definition
+            "class" @context
+            name: (identifier) @name
+            ) @item)
 
-(function_definition
-    "async"? @context
-    "def" @context
-    name: (_) @name) @item
+    (block
+        (class_definition
+            "class" @context
+            name: (identifier) @name
+            ) @item)
+
+    (decorated_definition
+        (decorator)+ @context.extra
+        definition:
+        (class_definition
+            "class" @context
+            name: (identifier) @name
+            ) )@item
+    ]
+
+; function definitions
+[
+    (module
+        (function_definition
+            "async"? @context
+            "def" @context
+            name: (_) @name
+            ) @item
+        )
+
+    (block
+        (function_definition
+            "async"? @context
+            "def" @context
+            name: (_) @name
+            ) @item
+        )
+
+    (decorated_definition
+        (decorator)+ @context.extra
+        definition: (function_definition
+            "async"? @context
+            "def" @context
+            name: (_) @name )
+        ) @item
+    ]
