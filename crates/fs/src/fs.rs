74a76b80dc32a894e1d475e4a787a601a2487c64
Smit Barmase <heysmitbarmase@gmail.com>
2025-05-08 02:32:19 -0700

    use fs write when atomic write fails on windows with os error 5

--- crates/fs/src/fs.rs
+++ crates/fs/src/fs.rs
@@ -546,7 +546,17 @@ impl Fs for RealFs {
                 NamedTempFile::new()
             }?;
             tmp_file.write_all(data.as_bytes())?;
-            tmp_file.persist(path)?;
+
+            let result = tmp_file.persist(&path);
+            if cfg!(target_os = "windows") {
+                if let Err(persist_err) = &result {
+                    if persist_err.error.raw_os_error() == Some(5) {
+                        return std::fs::write(&path, data.as_bytes()).map_err(Into::into);
+                    }
+                }
+            }
+            result?;
+
             Ok::<(), anyhow::Error>(())
         })
         .await?;
