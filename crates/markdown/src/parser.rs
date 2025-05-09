857134d6dc2451e644bd16c09d1f6e3ab830aa87
Michael Sloan <michael@zed.dev>
2025-05-09 13:12:54 +0200

    Fix handling of unicode when counting codeblock lines (#30364)
    Release Notes:
    
    - N/A

--- crates/markdown/src/parser.rs
+++ crates/markdown/src/parser.rs
@@ -80,8 +80,8 @@ pub fn parse_markdown(
                             content_range.start + range.start..content_range.end + range.start;
 
                         let line_count = text[content_range.clone()]
-                            .bytes()
-                            .filter(|c| *c == b'\n')
+                            .chars()
+                            .filter(|c| *c == '\n')
                             .count();
                         let metadata = CodeBlockMetadata {
                             content_range,
