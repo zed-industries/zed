cd426459dc04c5062bcee77bdf7a4d3856451070
Bennet Bo Fenner <bennet@zed.dev>
2025-05-12 15:38:28 +0200

    Fix tests

--- crates/zeta/src/zeta.rs
+++ crates/zeta/src/zeta.rs
@@ -1915,6 +1915,7 @@ mod tests {
             zeta.request_completion(None, &buffer, cursor, false, cx)
         });
 
+        server.receive::<proto::GetUsers>().await.unwrap();
         let token_request = server.receive::<proto::GetLlmToken>().await.unwrap();
         server.respond(
             token_request.receipt(),
