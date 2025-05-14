ef511976becef0dfb0cacec7afccb98de25feb2f
Rob McBroom <github@skurfer.com>
2025-05-14 12:19:09 -0400

    Add a separator before Quit in the application menu (#30697)
    macOS applications should have a separator between “Show All” and “Quit”
    in the application menu.

--- crates/zed/src/zed/app_menus.rs
+++ crates/zed/src/zed/app_menus.rs
@@ -44,6 +44,7 @@ pub fn app_menus() -> Vec<Menu> {
                 MenuItem::action("Hide Others", super::HideOthers),
                 #[cfg(target_os = "macos")]
                 MenuItem::action("Show All", super::ShowAll),
+                MenuItem::separator(),
                 MenuItem::action("Quit", Quit),
             ],
         },
