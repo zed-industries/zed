4b530c86eacf7861e1e1b9a6312d098df2fe3304
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-05-15 11:06:56 +0200

    clippy

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -594,6 +594,7 @@ pub(crate) enum EditDisplayMode {
     Inline,
 }
 
+#[expect(clippy::large_enum_variant)]
 enum InlineCompletion {
     Edit {
         edits: Vec<(Range<Anchor>, String)>,
@@ -20188,8 +20189,8 @@ impl EditorSnapshot {
         let participant_indices = collaboration_hub.user_participant_indices(cx);
         let collaborators_by_peer_id = collaboration_hub.collaborators(cx);
         let collaborators_by_replica_id = collaborators_by_peer_id
-            .iter()
-            .map(|(_, collaborator)| (collaborator.replica_id, collaborator))
+            .values()
+            .map(|collaborator| (collaborator.replica_id, collaborator))
             .collect::<HashMap<_, _>>();
         self.buffer_snapshot
             .selections_in_range(range, false)
