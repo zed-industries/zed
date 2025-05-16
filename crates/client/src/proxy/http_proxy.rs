8d9433b10b1f01fecb9898673ba7c5e2383b981c
Junkui Zhang <364772080@qq.com>
2025-05-16 16:08:52 +0800

    try auth

--- crates/client/src/proxy/http_proxy.rs
+++ crates/client/src/proxy/http_proxy.rs
@@ -1,5 +1,10 @@
 use anyhow::Result;
-use tokio::net::TcpStream;
+use base64::Engine;
+use httparse::{EMPTY_HEADER, Response};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 use url::Url;
 
 use super::AsyncReadWrite;
@@ -27,8 +32,79 @@ pub(crate) async fn connect_with_http_proxy(
     rpc_host: (&str, u16),
 ) -> Result<Box<dyn AsyncReadWrite>> {
     match http_proxy.auth {
-        Some(HttpProxyAuthorization { username, password }) => {}
-        None => {}
+        Some(HttpProxyAuthorization { username, password }) => {
+            connect_with_auth(stream, rpc_host, username, password).await
+        }
+        None => Err(anyhow::anyhow!("Not implemented")),
     }
-    Err(anyhow::anyhow!("Not implemented"))
+}
+
+async fn connect_with_auth(
+    mut stream: TcpStream,
+    target: (&str, u16),
+    username: &str,
+    password: &str,
+) -> Result<Box<dyn AsyncReadWrite>> {
+    let request = make_request_with_auth(target, username, password);
+    stream.write_all(request.as_bytes()).await?;
+    stream.flush().await?;
+
+    check_response(&mut stream).await?;
+    Ok(Box::new(stream))
+}
+
+fn make_request_with_auth(target: (&str, u16), username: &str, password: &str) -> String {
+    let mut request = make_request(target);
+    let auth =
+        base64::prelude::BASE64_STANDARD.encode(format!("{}:{}", username, password).as_bytes());
+    let proxy_authorization = format!("Proxy-Authorization: Basic {}\r\n", auth);
+    request.push_str(&proxy_authorization);
+    request.push_str("\r\n");
+    request
+}
+
+fn make_request(target: (&str, u16)) -> String {
+    let (host, port) = target;
+    format!(
+        "CONNECT {host}:{port} HTTP/1.1\r\n\
+         Host: {host}:{port}\r\n\
+         Proxy-Connection: Keep-Alive\r\n"
+    )
+}
+
+async fn check_response(stream: &mut TcpStream) -> Result<()> {
+    let response_string = get_response(stream).await?;
+    parse_and_check(&response_string)
+}
+
+const MAXIMUM_RESPONSE_HEADER_LENGTH: usize = 4096;
+const MAXIMUM_RESPONSE_HEADERS: usize = 16;
+
+async fn get_response(stream: &mut TcpStream) -> Result<String> {
+    let mut response = String::new();
+    loop {
+        if stream.read_to_string(&mut response).await? == 0 {
+            return Err(anyhow::anyhow!("End of stream"));
+        }
+
+        if MAXIMUM_RESPONSE_HEADER_LENGTH < response.len() {
+            return Err(anyhow::anyhow!("Maximum response header length exceeded"));
+        }
+
+        if response.ends_with("\r\n\r\n") {
+            return Ok(response);
+        }
+    }
+}
+
+fn parse_and_check(response_string: &str) -> Result<()> {
+    let mut response_headers = [EMPTY_HEADER; MAXIMUM_RESPONSE_HEADERS];
+    let mut response = Response::new(&mut response_headers);
+    response.parse(response_string.as_bytes())?;
+
+    if response.code.is_some_and(|code| code != 200) {
+        return Err(anyhow::anyhow!("Proxy response error"));
+    }
+
+    Ok(())
 }
