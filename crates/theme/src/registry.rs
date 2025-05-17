94865da1e8862f4e10eb1caacea712b72a89bc1e
Marshall Bowers <git@maxdeviant.com>
2025-05-17 13:10:15 +0200

    theme: Replace all fallback colors with `#ff00ff`

--- crates/theme/src/registry.rs
+++ crates/theme/src/registry.rs
@@ -91,7 +91,7 @@ impl ThemeRegistry {
 
         // We're loading the Zed default theme, as we need a theme to be loaded
         // for tests.
-        registry.insert_theme_families([crate::fallback_themes::zed_default_themes()]);
+        registry.insert_theme_families([crate::fallback_themes::zed_fallback_themes()]);
 
         let default_icon_theme = crate::default_icon_theme();
         registry
--- crates/theme/src/settings.rs
+++ crates/theme/src/settings.rs
@@ -1,4 +1,4 @@
-use crate::fallback_themes::zed_default_dark;
+use crate::fallback_themes::zed_fallback_theme;
 use crate::{
     Appearance, DEFAULT_ICON_THEME_NAME, IconTheme, IconThemeNotFoundError, SyntaxTheme, Theme,
     ThemeNotFoundError, ThemeRegistry, ThemeStyleContent,
@@ -832,7 +832,7 @@ impl settings::Settings for ThemeSettings {
             theme_selection: defaults.theme.clone(),
             active_theme: themes
                 .get(defaults.theme.as_ref().unwrap().theme(*system_appearance))
-                .or(themes.get(&zed_default_dark().name))
+                .or(themes.get(&zed_fallback_theme().name))
                 .unwrap(),
             theme_overrides: None,
             icon_theme_selection: defaults.icon_theme.clone(),
