605967deea89fef132dd160a194b31fc172df141
Michael Sloan <michael@zed.dev>
2025-05-20 11:46:16 -0600

    Use json language server with inspector style editor

--- crates/inspector_ui/src/inspector_ui.rs
+++ crates/inspector_ui/src/inspector_ui.rs
@@ -1,21 +1,23 @@
 use std::cell::RefCell;
 use std::path::Path;
 use std::rc::Rc;
-use std::sync::Arc;
 
 use anyhow::{Context as _, anyhow};
 use editor::{Editor, EditorEvent, EditorMode, MultiBuffer};
-use futures::FutureExt as _;
-use futures::future::Shared;
-use gpui::{App, DivInspectorState, Empty, Entity, InspectorElementId, IntoElement, Task, Window};
+use futures::{FutureExt as _, future::Shared};
+use gpui::{
+    App, AsyncWindowContext, DivInspectorState, Entity, InspectorElementId, IntoElement, Task,
+    Window,
+};
 use language::language_settings::SoftWrap;
-use language::{Buffer, Language, LanguageRegistry};
-use ui::{Button, Label, LabelSize, h_flex, v_flex};
+use project::ProjectPath;
 use ui::{CheckboxWithLabel, prelude::*};
+use ui::{Label, LabelSize, v_flex};
 use util::ResultExt as _;
 use util::command::new_smol_command;
+use workspace::Workspace;
 
-pub fn init(language_registry: Arc<LanguageRegistry>, cx: &mut App) {
+pub fn init(cx: &mut App) {
     // TODO: Instead toggle a global debug mode? Not all windows support the command pallete.
     cx.on_action(|_: &zed_actions::dev::ToggleInspector, cx| {
         let Some(active_window) = cx
@@ -33,22 +35,15 @@ pub fn init(language_registry: Arc<LanguageRegistry>, cx: &mut App) {
         });
     });
 
-    let json_language = cx
-        .background_spawn(
-            async move { language_registry.language_for_name("JSON").await.log_err() },
-        )
-        .shared();
-
     let inspector_settings = cx.new(|_cx| InspectorSettings {
         open_code_on_inspect: true,
     });
 
-    let last_div_inspector = Rc::new(RefCell::new(None));
+    let load_state = Rc::new(RefCell::new(None));
     cx.register_inspector_element(move |id, state, window, cx| {
-        div_inspector(
+        render_or_load_div_inspector(
             inspector_settings.clone(),
-            &last_div_inspector,
-            &json_language,
+            &load_state,
             id,
             state,
             window,
@@ -57,20 +52,177 @@ pub fn init(language_registry: Arc<LanguageRegistry>, cx: &mut App) {
     })
 }
 
-struct DivInspector {
+struct DivInspectorLoadState {
     id: Rc<InspectorElementId>,
+    task: Shared<Task<DivInspector>>,
+}
+
+#[derive(Clone)]
+struct DivInspector {
     style_editor: Entity<Editor>,
 }
 
+fn render_or_load_div_inspector(
+    inspector_settings: Entity<InspectorSettings>,
+    load_state: &Rc<RefCell<Option<DivInspectorLoadState>>>,
+    id: InspectorElementId,
+    state: &DivInspectorState,
+    window: &mut Window,
+    cx: &mut App,
+) -> impl IntoElement + use<> {
+    let mut load_state = load_state.borrow_mut();
+    let mut start_load = true;
+    if let Some(load_state) = &*load_state {
+        if load_state.id.as_ref() == &id {
+            if let Some(last_div_inspector) = load_state.task.clone().now_or_never() {
+                return last_div_inspector
+                    .render(&load_state.id.as_ref(), inspector_settings, cx)
+                    .into_any_element();
+            } else {
+                start_load = false;
+            }
+        }
+    }
+
+    if start_load {
+        // todo! Better error handling
+        let json_text = serde_json::to_string_pretty(state).unwrap();
+        let id = Rc::new(id);
+        *load_state = Some(DivInspectorLoadState {
+            id: id.clone(),
+            task: window
+                .spawn(cx, async move |cx| {
+                    DivInspector::load(inspector_settings, &id, json_text, cx).await
+                })
+                .shared(),
+        });
+    }
+
+    return Label::new("Loading...").into_any_element();
+}
+
 impl DivInspector {
-    fn render(&self, inspector_settings: Entity<InspectorSettings>, cx: &App) -> Div {
+    // todo! no unwraps / maybe no log_err
+    async fn load(
+        inspector_settings: Entity<InspectorSettings>,
+        id: &InspectorElementId,
+        json_text: String,
+        cx: &mut AsyncWindowContext,
+    ) -> DivInspector {
+        let project = cx
+            .update(|window, cx| {
+                let workspace = window.root::<Workspace>().flatten();
+                workspace.map(|workspace| workspace.read(cx).project().clone())
+            })
+            .unwrap()
+            .unwrap();
+
+        let worktree_id = project
+            .read_with(cx, |project, cx| {
+                project
+                    .worktrees(cx)
+                    .filter(|worktree| {
+                        let worktree = worktree.read(cx);
+                        !worktree.is_single_file() && worktree.is_local()
+                    })
+                    .next()
+                    .unwrap()
+                    .read(cx)
+                    .id()
+            })
+            .unwrap();
+        let project_path = ProjectPath {
+            worktree_id,
+            path: Path::new("zed-style-inspector.json").into(),
+        };
+
+        let style_buffer = project
+            .update(cx, |project, cx| project.open_path(project_path, cx))
+            .unwrap()
+            .await
+            .unwrap()
+            .1;
+
+        let style_editor = cx
+            .new_window_entity(|window, cx| {
+                /*
+                let buffer = cx.new(|cx| {
+                    let mut buffer = Buffer::local(json_text, cx);
+                    // todO!
+                    // buffer.file_updated(new_file, cx);
+                    buffer.set_language(json_language.clone().now_or_never().flatten(), cx);
+                    buffer
+                });
+                */
+                style_buffer.update(cx, |style_buffer, cx| style_buffer.set_text(json_text, cx));
+                let multi_buffer = cx.new(|cx| MultiBuffer::singleton(style_buffer, cx));
+                let mut editor =
+                    Editor::new(EditorMode::full(), multi_buffer, Some(project), window, cx);
+                editor.set_soft_wrap_mode(SoftWrap::EditorWidth, cx);
+                editor.set_show_line_numbers(false, cx);
+                editor.set_show_code_actions(false, cx);
+                editor.set_show_breakpoints(false, cx);
+                editor.set_show_git_diff_gutter(false, cx);
+                editor.set_show_runnables(false, cx);
+                editor.set_show_edit_predictions(Some(false), window, cx);
+                editor
+            })
+            .unwrap();
+
+        cx.update(|window, cx| {
+            window
+                .subscribe(&style_editor, cx, {
+                    let id = id.clone();
+                    move |editor, event: &EditorEvent, window, cx| {
+                        match event {
+                            EditorEvent::BufferEdited => {
+                                let json_text = editor.read(cx).text(cx);
+                                // todo! error handling
+                                let Some(new_state) =
+                                    serde_json_lenient::from_str(&json_text).log_err()
+                                else {
+                                    return;
+                                };
+                                window.update_inspector_state::<DivInspectorState, _>(
+                                    &id,
+                                    |state, _window| {
+                                        *state = new_state;
+                                    },
+                                )
+                            }
+                            _ => {}
+                        }
+                    }
+                })
+                .detach()
+        })
+        .log_err();
+
+        inspector_settings
+            .read_with(cx, |inspector_settings, cx| {
+                if inspector_settings.open_code_on_inspect {
+                    cx.background_spawn(open_zed_source_location(id.source))
+                        .detach_and_log_err(cx);
+                }
+            })
+            .log_err();
+
+        return DivInspector { style_editor };
+    }
+
+    fn render(
+        &self,
+        id: &InspectorElementId,
+        inspector_settings: Entity<InspectorSettings>,
+        cx: &App,
+    ) -> Div {
         v_flex()
             .size_full()
             .bg(cx.theme().colors().panel_background)
             .p_2()
             .gap_2()
             .child(inspector_settings)
-            .child(Label::new(self.id.to_string()).size(LabelSize::Small))
+            .child(Label::new(id.to_string()).size(LabelSize::Small))
             .child(
                 v_flex().gap_1().child(Label::new("Style")).child(
                     div()
@@ -81,82 +233,6 @@ impl DivInspector {
                 ),
             )
     }
-}
-
-fn div_inspector(
-    inspector_settings: Entity<InspectorSettings>,
-    last_div_inspector: &Rc<RefCell<Option<DivInspector>>>,
-    json_language: &Shared<Task<Option<Arc<Language>>>>,
-    id: InspectorElementId,
-    state: &DivInspectorState,
-    window: &mut Window,
-    cx: &mut App,
-) -> impl IntoElement + use<> {
-    let mut last_div_inspector = last_div_inspector.borrow_mut();
-    if let Some(last_div_inspector) = &*last_div_inspector {
-        if last_div_inspector.id.as_ref() == &id {
-            return last_div_inspector.render(inspector_settings, cx);
-        }
-    }
-
-    // todo! Better error handling
-    let Some(json_text) = serde_json::to_string_pretty(state).log_err() else {
-        return div();
-    };
-    let style_editor = cx.new(|cx| {
-        let buffer = cx.new(|cx| {
-            let mut buffer = Buffer::local(json_text, cx);
-            buffer.set_language(json_language.clone().now_or_never().flatten(), cx);
-            buffer
-        });
-        let buffer = cx.new(|cx| MultiBuffer::singleton(buffer, cx));
-        let mut editor = Editor::new(EditorMode::full(), buffer, None, window, cx);
-        editor.set_soft_wrap_mode(SoftWrap::EditorWidth, cx);
-        editor.set_show_line_numbers(false, cx);
-        editor.set_show_code_actions(false, cx);
-        editor.set_show_breakpoints(false, cx);
-        editor.set_show_git_diff_gutter(false, cx);
-        editor.set_show_runnables(false, cx);
-        editor.set_show_edit_predictions(Some(false), window, cx);
-        editor
-    });
-
-    let id = Rc::new(id);
-
-    window
-        .subscribe(&style_editor, cx, {
-            let id = id.clone();
-            move |editor, event: &EditorEvent, window, cx| {
-                match event {
-                    EditorEvent::BufferEdited => {
-                        let json_text = editor.read(cx).text(cx);
-                        // todo! error handling
-                        let Some(new_state) = serde_json_lenient::from_str(&json_text).log_err()
-                        else {
-                            return;
-                        };
-                        window.update_inspector_state::<DivInspectorState, _>(
-                            &id,
-                            |state, _window| {
-                                *state = new_state;
-                            },
-                        )
-                    }
-                    _ => {}
-                }
-            }
-        })
-        .detach();
-
-    if inspector_settings.read(cx).open_code_on_inspect {
-        cx.background_spawn(open_zed_source_location(id.source))
-            .detach_and_log_err(cx);
-    }
-
-    let div_inspector = DivInspector { id, style_editor };
-    let rendered = div_inspector.render(inspector_settings, cx);
-    *last_div_inspector = Some(div_inspector);
-    return rendered;
 }
 
 struct InspectorSettings {
