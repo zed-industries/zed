6025bfc5df3a2288a7f988eb9303ca7ad8129865
Remco Smits <djsmits12@gmail.com>
2025-05-21 00:55:21 +0200

    Clipppppyyyy

--- crates/dap_adapters/src/ruby.rs
+++ crates/dap_adapters/src/ruby.rs
@@ -90,7 +90,7 @@ impl DebugAdapter for RubyDebugAdapter {
                 port,
                 timeout,
             }),
-            cwd: launch.cwd.map(|cwd| cwd.into()),
+            cwd: launch.cwd,
             envs: launch.env.into_iter().collect(),
             request_args: StartDebuggingRequestArguments {
                 configuration: serde_json::Value::Object(Default::default()),
