d7b83fc2b5891875485c8be2b63e4e86b1c7fd06
Jason Lee <huacnlee@gmail.com>
2025-05-21 10:59:43 +0800

    .

--- crates/gpui/src/elements/div.rs
+++ crates/gpui/src/elements/div.rs
@@ -2212,7 +2212,6 @@ impl Interactivity {
                     }
                     scroll_offset.y += delta_y;
                     scroll_offset.x += delta_x;
-                    cx.stop_propagation();
                     if *scroll_offset != old_scroll_offset {
                         cx.notify(current_view);
                     }
