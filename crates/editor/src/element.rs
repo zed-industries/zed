49ea9905ccadbd0fcb3dfd87fd2b7a58ca69f5ac
Smit Barmase <heysmitbarmase@gmail.com>
2025-05-21 16:29:50 +0530

    do not overlap signature popover with context menu

--- crates/editor/src/element.rs
+++ crates/editor/src/element.rs
@@ -3791,14 +3791,12 @@ impl EditorElement {
                 None
             }
         }) {
-            let combined_bounds = if let Some(aside_bounds) = aside_bounds {
+            let bounds = if let Some(aside_bounds) = aside_bounds {
                 menu_bounds.union(&aside_bounds)
             } else {
                 menu_bounds
             };
-            return Some(ContextMenuLayout {
-                bounds: combined_bounds,
-            });
+            return Some(ContextMenuLayout { y_flipped, bounds });
         }
 
         None
@@ -4473,22 +4471,87 @@ impl EditorElement {
         let target_point = content_origin + point(target_x, target_y);
 
         let actual_size = element.layout_as_root(Size::<AvailableSpace>::default(), window, cx);
-        let overall_height = actual_size.height + HOVER_POPOVER_GAP;
 
-        let popover_origin = if target_point.y > overall_height {
-            point(target_point.x, target_point.y - actual_size.height)
-        } else {
-            point(
-                target_point.x,
-                target_point.y + line_height + HOVER_POPOVER_GAP,
+        let (popover_bounds_above, popover_bounds_below) = {
+            let horizontal_offset = (hitbox.top_right().x
+                - POPOVER_RIGHT_OFFSET
+                - (target_point.x + actual_size.width))
+                .min(Pixels::ZERO);
+            let initial_x = target_point.x + horizontal_offset;
+            (
+                Bounds::new(
+                    point(initial_x, target_point.y - actual_size.height),
+                    actual_size,
+                ),
+                Bounds::new(
+                    point(initial_x, target_point.y + line_height + HOVER_POPOVER_GAP),
+                    actual_size,
+                ),
             )
         };
 
-        let horizontal_offset = (text_hitbox.top_right().x
-            - POPOVER_RIGHT_OFFSET
-            - (popover_origin.x + actual_size.width))
-            .min(Pixels::ZERO);
-        let final_origin = point(popover_origin.x + horizontal_offset, popover_origin.y);
+        let intersects_menu = |bounds: Bounds<Pixels>| -> bool {
+            context_menu_layout
+                .as_ref()
+                .map_or(false, |menu| bounds.intersects(&menu.bounds))
+        };
+
+        let final_origin = if popover_bounds_above.is_contained_within(hitbox)
+            && !intersects_menu(popover_bounds_above)
+        {
+            // try placing above cursor
+            popover_bounds_above.origin
+        } else if popover_bounds_below.is_contained_within(hitbox)
+            && !intersects_menu(popover_bounds_below)
+        {
+            // try placing below cursor
+            popover_bounds_below.origin
+        } else {
+            // try surrounding context menu if exists
+            let origin_surrounding_menu = context_menu_layout.as_ref().and_then(|menu| {
+                let y_for_horizontal_positioning = if menu.y_flipped {
+                    menu.bounds.bottom() - actual_size.height
+                } else {
+                    menu.bounds.top()
+                };
+                let possible_origins = vec![
+                    // left of context menu
+                    point(
+                        menu.bounds.left() - actual_size.width - HOVER_POPOVER_GAP,
+                        y_for_horizontal_positioning,
+                    ),
+                    // right of context menu
+                    point(
+                        menu.bounds.right() + HOVER_POPOVER_GAP,
+                        y_for_horizontal_positioning,
+                    ),
+                    // top of context menu
+                    point(
+                        menu.bounds.left(),
+                        menu.bounds.top() - actual_size.height - HOVER_POPOVER_GAP,
+                    ),
+                    // bottom of context menu
+                    point(menu.bounds.left(), menu.bounds.bottom() + HOVER_POPOVER_GAP),
+                ];
+                // TODO: In future we can re-layout in case of left/right
+                // to fit available space if not too short
+                for origin in possible_origins {
+                    if Bounds::new(origin, actual_size).is_contained_within(hitbox) {
+                        return Some(origin);
+                    }
+                }
+                None
+            });
+            origin_surrounding_menu.unwrap_or_else(|| {
+                // fallback to existing above/below cursor logic
+                // this might overlap menu or overflow in rare case
+                if popover_bounds_above.is_contained_within(hitbox) {
+                    popover_bounds_above.origin
+                } else {
+                    popover_bounds_below.origin
+                }
+            })
+        };
 
         window.defer_draw(element, final_origin, 2);
     }
@@ -8244,8 +8307,9 @@ pub(super) fn gutter_bounds(
 }
 
 #[derive(Clone, Copy)]
-pub struct ContextMenuLayout {
-    pub bounds: Bounds<Pixels>,
+struct ContextMenuLayout {
+    y_flipped: bool,
+    bounds: Bounds<Pixels>,
 }
 
 /// Holds information required for layouting the editor scrollbars.
