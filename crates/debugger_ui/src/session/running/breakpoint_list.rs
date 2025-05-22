dc04f1740ad61b5cb852029d3f762d532ebd235f
Cole Miller <cole@zed.dev>
2025-05-22 16:34:29 -0400

    make breakpoints selectable

--- crates/debugger_ui/src/session/running/breakpoint_list.rs
+++ crates/debugger_ui/src/session/running/breakpoint_list.rs
@@ -6,8 +6,8 @@ use std::{
 use dap::ExceptionBreakpointsFilter;
 use editor::Editor;
 use gpui::{
-    AppContext, Entity, FocusHandle, Focusable, ListState, MouseButton, ScrollHandle, Stateful,
-    Task, UniformListScrollHandle, WeakEntity, list, uniform_list,
+    AppContext, Entity, FocusHandle, Focusable, ListState, MouseButton, ScrollHandle,
+    ScrollStrategy, Stateful, Task, UniformListScrollHandle, WeakEntity, list, uniform_list,
 };
 use language::Point;
 use project::{
@@ -19,10 +19,10 @@ use project::{
     worktree_store::WorktreeStore,
 };
 use ui::{
-    App, Clickable, Color, Context, Div, Icon, IconButton, IconName, Indicator, InteractiveElement,
-    IntoElement, Label, LabelCommon, LabelSize, ListItem, ParentElement, Render, RenderOnce,
-    Scrollbar, ScrollbarState, SharedString, StatefulInteractiveElement, Styled, Tooltip, Window,
-    div, h_flex, px, v_flex,
+    ActiveTheme as _, App, Clickable, Color, Context, Div, FluentBuilder as _, Icon, IconButton,
+    IconName, Indicator, InteractiveElement, IntoElement, Label, LabelCommon, LabelSize, ListItem,
+    ParentElement, Render, RenderOnce, Scrollbar, ScrollbarState, SharedString,
+    StatefulInteractiveElement, Styled, Toggleable, Tooltip, Window, div, h_flex, px, v_flex,
 };
 use util::{ResultExt, maybe};
 use workspace::Workspace;
@@ -39,6 +39,7 @@ pub(crate) struct BreakpointList {
     show_scrollbar: bool,
     focus_handle: FocusHandle,
     scroll_handle: UniformListScrollHandle,
+    selected_ix: Option<usize>,
 }
 
 impl Focusable for BreakpointList {
@@ -89,8 +90,76 @@ impl BreakpointList {
                 session,
                 focus_handle,
                 scroll_handle,
+                selected_ix: None,
             }
         })
+    }
+
+    fn select_ix(&mut self, ix: Option<usize>, cx: &mut Context<Self>) {
+        self.selected_ix = ix;
+        if let Some(ix) = ix {
+            self.scroll_handle
+                .scroll_to_item(ix, ScrollStrategy::Center);
+        }
+        cx.notify();
+    }
+
+    fn select_next(&mut self, _: &menu::SelectNext, _window: &mut Window, cx: &mut Context<Self>) {
+        let ix = match self.selected_ix {
+            _ if self.breakpoints.len() == 0 => None,
+            None => Some(0),
+            Some(ix) => {
+                if ix == self.breakpoints.len() - 1 {
+                    Some(0)
+                } else {
+                    Some(ix + 1)
+                }
+            }
+        };
+        self.select_ix(ix, cx);
+    }
+
+    fn select_previous(
+        &mut self,
+        _: &menu::SelectPrevious,
+        _window: &mut Window,
+        cx: &mut Context<Self>,
+    ) {
+        let ix = match self.selected_ix {
+            _ if self.breakpoints.len() == 0 => None,
+            None => Some(self.breakpoints.len() - 1),
+            Some(ix) => {
+                if ix == 0 {
+                    Some(self.breakpoints.len() - 1)
+                } else {
+                    Some(ix - 1)
+                }
+            }
+        };
+        self.select_ix(ix, cx);
+    }
+
+    fn select_first(
+        &mut self,
+        _: &menu::SelectFirst,
+        _window: &mut Window,
+        cx: &mut Context<Self>,
+    ) {
+        let ix = if self.breakpoints.len() > 0 {
+            Some(0)
+        } else {
+            None
+        };
+        self.select_ix(ix, cx);
+    }
+
+    fn select_last(&mut self, _: &menu::SelectLast, _window: &mut Window, cx: &mut Context<Self>) {
+        let ix = if self.breakpoints.len() > 0 {
+            Some(self.breakpoints.len() - 1)
+        } else {
+            None
+        };
+        self.select_ix(ix, cx);
     }
 
     fn hide_scrollbar(&mut self, window: &mut Window, cx: &mut Context<Self>) {
@@ -109,14 +178,21 @@ impl BreakpointList {
     }
 
     fn render_list(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        let selected_ix = self.selected_ix;
         uniform_list(
             cx.entity(),
             "breakpoint-list",
             self.breakpoints.len(),
-            |this, range, window, cx| {
-                this.breakpoints[range]
-                    .iter_mut()
-                    .map(|breakpoint| breakpoint.render(window, cx).into_any_element())
+            move |this, range, window, cx| {
+                range
+                    .clone()
+                    .zip(&mut this.breakpoints[range])
+                    .map(|(ix, breakpoint)| {
+                        breakpoint
+                            .render(window, cx)
+                            .toggle_state(Some(ix) == selected_ix)
+                            .into_any_element()
+                    })
                     .collect()
             },
         )
@@ -241,6 +317,10 @@ impl Render for BreakpointList {
                     this.hide_scrollbar(window, cx);
                 }
             }))
+            .on_action(cx.listener(Self::select_next))
+            .on_action(cx.listener(Self::select_previous))
+            .on_action(cx.listener(Self::select_first))
+            .on_action(cx.listener(Self::select_last))
             .size_full()
             .m_0p5()
             .child(self.render_list(window, cx))
@@ -498,7 +578,7 @@ struct BreakpointEntry {
 }
 
 impl BreakpointEntry {
-    fn render(&mut self, _: &mut Window, _: &mut App) -> impl ui::IntoElement {
+    fn render(&mut self, _: &mut Window, _: &mut App) -> ListItem {
         match &mut self.kind {
             BreakpointEntryKind::LineBreakpoint(line_breakpoint) => {
                 line_breakpoint.render(self.weak.clone())
