e1685cb718f0e221669b390791a3bbc220e3a4d5
Fedor Nezhivoi <gyzerok@users.noreply.github.com>
2025-05-24 16:54:54 +0700

    fix after rebase to the current main

--- crates/language_models/src/provider/lmstudio.rs
+++ crates/language_models/src/provider/lmstudio.rs
@@ -7,7 +7,7 @@ use http_client::HttpClient;
 use language_model::{
     AuthenticateError, LanguageModelCompletionError, LanguageModelCompletionEvent,
     LanguageModelToolChoice, LanguageModelToolResultContent, LanguageModelToolUse, MessageContent,
-    StopReason,
+    StopReason, WrappedTextContent,
 };
 use language_model::{
     LanguageModel, LanguageModelId, LanguageModelName, LanguageModelProvider,
@@ -293,7 +293,11 @@ impl LmStudioLanguageModel {
                     }
                     MessageContent::ToolResult(tool_result) => {
                         match &tool_result.content {
-                            LanguageModelToolResultContent::Text(text) => {
+                            LanguageModelToolResultContent::Text(text)
+                            | LanguageModelToolResultContent::WrappedText(WrappedTextContent {
+                                text,
+                                ..
+                            }) => {
                                 messages.push(lmstudio::ChatMessage::Tool {
                                     content: text.to_string(),
                                     tool_call_id: tool_result.tool_use_id.to_string(),
