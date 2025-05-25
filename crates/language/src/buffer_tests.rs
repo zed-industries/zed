aadda426915c131e1fe58c1ccfc6ec8ee2f85e95
Daquisu <50253469+Daquisu@users.noreply.github.com>
2025-05-25 19:23:12 -0300

    Update test_language_scope_at_with_javascript due to new block_comment

--- crates/language/src/buffer_tests.rs
+++ crates/language/src/buffer_tests.rs
@@ -2218,6 +2218,7 @@ fn test_language_scope_at_with_javascript(cx: &mut App) {
             LanguageConfig {
                 name: "JavaScript".into(),
                 line_comments: vec!["// ".into()],
+                block_comment: Some(("/*".into(), "*/".into())),
                 brackets: BracketPairConfig {
                     pairs: vec![
                         BracketPair {
@@ -2281,6 +2282,7 @@ fn test_language_scope_at_with_javascript(cx: &mut App) {
 
         let config = snapshot.language_scope_at(0).unwrap();
         assert_eq!(config.line_comment_prefixes(), &[Arc::from("// ")]);
+        assert_eq!(config.block_comment_delimiters(), Some((&"/*".into(), &"*/".into())));
         // Both bracket pairs are enabled
         assert_eq!(
             config.brackets().map(|e| e.1).collect::<Vec<_>>(),
@@ -2299,6 +2301,7 @@ fn test_language_scope_at_with_javascript(cx: &mut App) {
             .language_scope_at(text.find("b\"").unwrap())
             .unwrap();
         assert_eq!(string_config.line_comment_prefixes(), &[Arc::from("// ")]);
+        assert_eq!(string_config.block_comment_delimiters(), Some((&"/*".into(), &"*/".into())));
         // Second bracket pair is disabled
         assert_eq!(
             string_config.brackets().map(|e| e.1).collect::<Vec<_>>(),
@@ -2326,6 +2329,7 @@ fn test_language_scope_at_with_javascript(cx: &mut App) {
             .language_scope_at(text.find(" d=").unwrap() + 1)
             .unwrap();
         assert_eq!(tag_config.line_comment_prefixes(), &[Arc::from("// ")]);
+        assert_eq!(tag_config.block_comment_delimiters(), Some((&"/*".into(), &"*/".into())));
         assert_eq!(
             tag_config.brackets().map(|e| e.1).collect::<Vec<_>>(),
             &[true, true]
@@ -2338,6 +2342,10 @@ fn test_language_scope_at_with_javascript(cx: &mut App) {
         assert_eq!(
             expression_in_element_config.line_comment_prefixes(),
             &[Arc::from("// ")]
+        );
+        assert_eq!(
+            expression_in_element_config.block_comment_delimiters(),
+            Some((&"/*".into(), &"*/".into()))
         );
         assert_eq!(
             expression_in_element_config
