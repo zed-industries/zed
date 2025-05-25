cbd32dd942b8a340ee74a2a62c445c812e945167
Matin Aniss <76515905+MatinAniss@users.noreply.github.com>
2025-05-26 07:53:02 +1000

    gpui: Implement dynamic window controls

--- crates/title_bar/src/title_bar.rs
+++ crates/title_bar/src/title_bar.rs
@@ -22,7 +22,8 @@ use client::{Client, UserStore};
 use gpui::{
     Action, AnyElement, App, Context, Corner, Decorations, Element, Entity, InteractiveElement,
     Interactivity, IntoElement, MouseButton, ParentElement, Render, Stateful,
-    StatefulInteractiveElement, Styled, Subscription, WeakEntity, Window, actions, div, px,
+    StatefulInteractiveElement, Styled, Subscription, WeakEntity, Window, WindowControlArea,
+    actions, div, px,
 };
 use onboarding_banner::OnboardingBanner;
 use project::Project;
@@ -143,6 +144,7 @@ impl Render for TitleBar {
 
         h_flex()
             .id("titlebar")
+            .window_control_area(WindowControlArea::Drag)
             .w_full()
             .h(height)
             .map(|this| {
