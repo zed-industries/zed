3f373a3bf63fe7a2375d1a88a2c339088c678a73
Dave Waggoner <waggoner.dave@gmail.com>
2025-05-29 13:04:28 -0700

    Simplify, formatting

--- crates/terminal/src/terminal_hyperlinks.rs
+++ crates/terminal/src/terminal_hyperlinks.rs
@@ -678,7 +678,9 @@ mod tests {
                 // The test uses Url::to_file_path(), but it seems that the Url crate doesn't
                 // support relative file IRIs.
                 #[test]
-                #[should_panic(expected = r#"Failed to interpret file IRI `file:/test/cool/index.rs` as a path: ()"#)]
+                #[should_panic(
+                    expected = r#"Failed to interpret file IRI `file:/test/cool/index.rs` as a path"#
+                )]
                 fn issue_relative_file_iri() {
                     test_file_iri!("file:/test/cool/index.rs");
                     test_file_iri!("file:/test/cool/");
@@ -968,12 +970,13 @@ mod tests {
         }
 
         if hyperlink_kind == HyperlinkKind::FileIri {
-            iri_or_path = Url::parse(&iri_or_path)
-                .expect(&format!("Failed to parse file IRI `{iri_or_path}`"))
-                .to_file_path()
-                .expect(&format!("Failed to interpret file IRI `{iri_or_path}` as a path"))
-                .to_string_lossy()
-                .to_string();
+            let Ok(url) = Url::parse(&iri_or_path) else {
+                panic!("Failed to parse file IRI `{iri_or_path}`");
+            };
+            let Ok(path) = url.to_file_path() else {
+                panic!("Failed to interpret file IRI `{iri_or_path}` as a path");
+            };
+            iri_or_path = path.to_string_lossy().to_string();
         }
 
         if cfg!(windows) {
