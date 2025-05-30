4302f00135a60b3846851d354fbf2c4c5b6a708e
Sunli <scott_s829@163.com>
2025-05-30 15:06:55 +0800

    .

--- crates/gpui/src/platform/blade/blade_renderer.rs
+++ crates/gpui/src/platform/blade/blade_renderer.rs
@@ -778,8 +778,6 @@ impl BladeRenderer {
             }
         }
 
-        // draw to screen
-
         self.command_encoder.present(frame);
         let sync_point = self.gpu.submit(&mut self.command_encoder);
 
