52f0f857caa0ffd5a30380fb79698d640abc8e8c
Kirill Bulatov <kirill@zed.dev>
2025-05-31 23:53:29 +0300

    Fix status update bugs

--- crates/project/src/lsp_store.rs
+++ crates/project/src/lsp_store.rs
@@ -2417,7 +2417,6 @@ impl LocalLspStore {
                     });
                 (false, lsp_delegate, servers)
             });
-        dbg!("?", buffer_id, servers.len());
         let servers_and_adapters = servers
             .into_iter()
             .filter_map(|server_node| {
@@ -3576,6 +3575,7 @@ struct BufferLspData {
     colors: Option<Vec<DocumentColor>>,
 }
 
+#[derive(Debug)]
 pub enum LspStoreEvent {
     LanguageServerAdded(LanguageServerId, LanguageServerName, Option<WorktreeId>),
     LanguageServerRemoved(LanguageServerId),
@@ -4341,7 +4341,6 @@ impl LspStore {
 
             if let Some(local) = self.as_local_mut() {
                 if local.registered_buffers.contains_key(&buffer_id) {
-                    dbg!("????????????", buffer_id);
                     local.register_buffer_with_language_servers(buffer_entity, cx);
                 }
             }
