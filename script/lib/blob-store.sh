65014612ba43675a65e8f4a6449bc21fc1aa5b04
Conrad Irwin <conrad.irwin@gmail.com>
2025-06-04 17:07:24 -0600

    Try to store the real version in Digital ocean

--- script/lib/blob-store.sh
+++ script/lib/blob-store.sh
@@ -4,6 +4,7 @@ function upload_to_blob_store_with_acl
     file_to_upload="$2"
     blob_store_key="$3"
     acl="$4"
+    custom_headers="${5:-}"
 
     date=$(date +"%a, %d %b %Y %T %z")
     content_type="application/octet-stream"
@@ -11,22 +12,29 @@ function upload_to_blob_store_with_acl
     string="PUT\n\n${content_type}\n${date}\n${acl}\n${storage_type}\n/${bucket_name}/${blob_store_key}"
     signature=$(echo -en "${string}" | openssl sha1 -hmac "${DIGITALOCEAN_SPACES_SECRET_KEY}" -binary | base64)
 
-    curl --fail -vv -s -X PUT -T "$file_to_upload" \
-        -H "Host: ${bucket_name}.nyc3.digitaloceanspaces.com" \
-        -H "Date: $date" \
-        -H "Content-Type: $content_type" \
-        -H "$storage_type" \
-        -H "$acl" \
-        -H "Authorization: AWS ${DIGITALOCEAN_SPACES_ACCESS_KEY}:$signature" \
-        "https://${bucket_name}.nyc3.digitaloceanspaces.com/${blob_store_key}"
+    curl_cmd="curl --fail -vv -s -X PUT -T \"$file_to_upload\" \
+        -H \"Host: ${bucket_name}.nyc3.digitaloceanspaces.com\" \
+        -H \"Date: $date\" \
+        -H \"Content-Type: $content_type\" \
+        -H \"$storage_type\" \
+        -H \"$acl\" \
+        -H \"Authorization: AWS ${DIGITALOCEAN_SPACES_ACCESS_KEY}:$signature\""
+
+    if [ -n "$custom_headers" ]; then
+        curl_cmd="$curl_cmd -H \"$custom_headers\""
+    fi
+
+    curl_cmd="$curl_cmd \"https://${bucket_name}.nyc3.digitaloceanspaces.com/${blob_store_key}\""
+
+    eval $curl_cmd
 }
 
 function upload_to_blob_store_public
 {
-    upload_to_blob_store_with_acl "$1" "$2" "$3" "x-amz-acl:public-read"
+    upload_to_blob_store_with_acl "$1" "$2" "$3" "x-amz-acl:public-read" "${4:-}"
 }
 
 function upload_to_blob_store
 {
-    upload_to_blob_store_with_acl "$1" "$2" "$3" "x-amz-acl:private"
+    upload_to_blob_store_with_acl "$1" "$2" "$3" "x-amz-acl:private" "${4:-}"
 }
