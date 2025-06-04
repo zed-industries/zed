8fcc03074ee63a9667255bdb2c47108a84d0456e
Sunli <scott_s829@163.com>
2025-06-04 12:02:17 +0800

    .

--- crates/gpui/src/platform/test/window.rs
+++ crates/gpui/src/platform/test/window.rs
@@ -1,10 +1,8 @@
 use crate::{
     AnyWindowHandle, AtlasKey, AtlasTextureId, AtlasTile, Bounds, DispatchEventResult, GpuSpecs,
     Pixels, PlatformAtlas, PlatformDisplay, PlatformInput, PlatformInputHandler, PlatformWindow,
-    Point, Point, PromptButton, RequestFrameOptions, RequestFrameOptions, ScaledPixels,
-    ScaledPixels, Size, Size, TestPlatform, TestPlatform, TileId, TileId, WindowAppearance,
-    WindowAppearance, WindowBackgroundAppearance, WindowBackgroundAppearance, WindowBounds,
-    WindowBounds, WindowParams, WindowParams,
+    Point, PromptButton, RequestFrameOptions, ScaledPixels, Size, TestPlatform, TileId,
+    WindowAppearance, WindowBackgroundAppearance, WindowBounds, WindowParams,
 };
 use collections::HashMap;
 use parking_lot::Mutex;
