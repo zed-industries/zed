971f97d86cb54bfe7bb1530b67fe72e755385c7a
Conrad Irwin <conrad.irwin@gmail.com>
2025-06-04 10:09:01 -0600

    Don't show invisibles from inlays

--- crates/editor/src/element.rs
+++ crates/editor/src/element.rs
@@ -6871,6 +6871,7 @@ impl LineWithInvisibles {
             text: "\n",
             style: None,
             is_tab: false,
+            is_inlay: false,
             replacement: None,
         }]) {
             if let Some(replacement) = highlighted_chunk.replacement {
@@ -7004,7 +7005,7 @@ impl LineWithInvisibles {
                             strikethrough: text_style.strikethrough,
                         });
 
-                        if editor_mode.is_full() {
+                        if editor_mode.is_full() && !highlighted_chunk.is_inlay {
                             // Line wrap pads its contents with fake whitespaces,
                             // avoid printing them
                             let is_soft_wrapped = is_row_soft_wrapped(row);
