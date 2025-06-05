7e13d237a10a617df3f5fa3cfacee4157c4936f5
Junkui Zhang <364772080@qq.com>
2025-06-05 19:01:26 +0800

    remove debug print

--- crates/gpui/src/platform/windows/events.rs
+++ crates/gpui/src/platform/windows/events.rs
@@ -688,7 +688,6 @@ fn handle_ime_composition_inner(
         return Some(0);
     }
     let mut ime_input = None;
-    println!("IME composition lparam: {lparam:?}");
     if lparam.0 as u32 & GCS_COMPSTR.0 > 0 {
         let comp_string = parse_ime_compostion_string(ctx)?;
         with_input_handler(&state_ptr, |input_handler| {
@@ -1329,7 +1328,6 @@ fn parse_normal_key(
 fn parse_ime_compostion_string(ctx: HIMC) -> Option<String> {
     unsafe {
         let string_len = ImmGetCompositionStringW(ctx, GCS_COMPSTR, None, 0);
-        println!("Comp str len: {}", string_len);
         if string_len >= 0 {
             let mut buffer = vec![0u8; string_len as usize + 2];
             ImmGetCompositionStringW(
@@ -1357,7 +1355,6 @@ fn retrieve_composition_cursor_position(ctx: HIMC) -> usize {
 fn parse_ime_compostion_result(ctx: HIMC) -> Option<String> {
     unsafe {
         let string_len = ImmGetCompositionStringW(ctx, GCS_RESULTSTR, None, 0);
-        println!("Comp str res len: {}", string_len);
         if string_len >= 0 {
             let mut buffer = vec![0u8; string_len as usize + 2];
             ImmGetCompositionStringW(
