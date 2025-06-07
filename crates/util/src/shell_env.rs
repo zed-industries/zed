649fc7ba7219caf14eb2b91ddf5c5bc9c9be6e39
Haru Kim <haruleekim@icloud.com>
2025-06-07 18:54:03 +0900

    fix

--- crates/util/src/shell_env.rs
+++ crates/util/src/shell_env.rs
@@ -182,7 +182,7 @@ mod tests {
     fn test_spawn_and_read_fd() -> anyhow::Result<()> {
         let mut command = std::process::Command::new("sh");
         super::super::set_pre_exec_to_start_new_session(&mut command);
-        command.args(["-lic", "printf 'abc%.0s' {1..65536} >&0"]);
+        command.args(["-lic", "printf 'abc%.0s' $(seq 1 65536) >&0"]);
         let (bytes, _) = spawn_and_read_fd(command, 0)?;
         assert_eq!(bytes.len(), 65536 * 3);
         Ok(())
