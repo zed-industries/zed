6d8ea1231be4d1f5f25d3f727de8476e45c70242
Junkui Zhang <364772080@qq.com>
2025-06-09 20:25:40 +0800

    bring checks back

--- crates/gpui/src/platform/windows/events.rs
+++ crates/gpui/src/platform/windows/events.rs
@@ -956,6 +956,10 @@ fn handle_nc_mouse_move_msg(
     lparam: LPARAM,
     state_ptr: Rc<WindowsWindowStatePtr>,
 ) -> Option<isize> {
+    if !state_ptr.hide_title_bar {
+        return None;
+    }
+
     start_tracking_mouse(handle, &state_ptr, TME_LEAVE | TME_NONCLIENT);
 
     let mut lock = state_ptr.state.borrow_mut();
@@ -986,6 +990,10 @@ fn handle_nc_mouse_down_msg(
     lparam: LPARAM,
     state_ptr: Rc<WindowsWindowStatePtr>,
 ) -> Option<isize> {
+    if !state_ptr.hide_title_bar {
+        return None;
+    }
+
     let mut lock = state_ptr.state.borrow_mut();
     if let Some(mut func) = lock.callbacks.input.take() {
         let scale_factor = lock.scale_factor;
@@ -1037,6 +1045,10 @@ fn handle_nc_mouse_up_msg(
     lparam: LPARAM,
     state_ptr: Rc<WindowsWindowStatePtr>,
 ) -> Option<isize> {
+    if !state_ptr.hide_title_bar {
+        return None;
+    }
+
     let mut lock = state_ptr.state.borrow_mut();
     if let Some(mut func) = lock.callbacks.input.take() {
         let scale_factor = lock.scale_factor;
