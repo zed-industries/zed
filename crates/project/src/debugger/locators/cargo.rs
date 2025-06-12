c46867fca73cb405bb33d09d649479a7310f780d
Anthony <anthony@zed.dev>
2025-06-12 15:32:14 -0400

    Pass --nocapture to cargo tests when build debug tasks with locator
    Co-authored-by: Cole Miller <m@cole-miller.net>

--- crates/project/src/debugger/locators/cargo.rs
+++ crates/project/src/debugger/locators/cargo.rs
@@ -57,10 +57,10 @@ impl DapLocator for CargoLocator {
         }
 
         match cargo_action.as_ref() {
-            "run" => {
+            "run" | "r" => {
                 *cargo_action = "build".to_owned();
             }
-            "test" | "bench" => {
+            "test" | "t" | "bench" => {
                 let delimiter = task_template
                     .args
                     .iter()
@@ -133,7 +133,10 @@ impl DapLocator for CargoLocator {
             !executables.is_empty(),
             "Couldn't get executable in cargo locator"
         );
-        let is_test = build_config.args.first().map_or(false, |arg| arg == "test");
+        let is_test = build_config
+            .args
+            .first()
+            .map_or(false, |arg| arg == "test" || arg == "t");
 
         let mut test_name = None;
         if is_test {
@@ -161,7 +164,10 @@ impl DapLocator for CargoLocator {
             anyhow::bail!("Couldn't get executable in cargo locator");
         };
 
-        let args = test_name.into_iter().collect();
+        let mut args: Vec<_> = test_name.into_iter().collect();
+        if is_test {
+            args.push("--nocapture".to_owned());
+        }
 
         Ok(DebugRequest::Launch(task::LaunchRequest {
             program: executable,
