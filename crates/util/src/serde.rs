79c6d4a2496a5bc4c0c0761c28a1aae5e4549b6f
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2025-06-17 12:19:15 -0700

    feat: clone item to pane [in direction]

--- crates/util/src/serde.rs
+++ crates/util/src/serde.rs
@@ -2,6 +2,10 @@ pub const fn default_true() -> bool {
     true
 }
 
+pub const fn default_false() -> bool {
+    false
+}
+
 pub fn is_default<T: Default + PartialEq>(value: &T) -> bool {
     *value == T::default()
 }
