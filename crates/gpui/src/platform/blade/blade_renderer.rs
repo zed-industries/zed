1b112093def4c92f6ccc45d3d2ae1d3c1672dc78
Peter Tripp <peter@zed.dev>
2025-06-21 12:57:50 -0400

    more rustfmt

--- crates/gpui/src/platform/blade/blade_renderer.rs
+++ crates/gpui/src/platform/blade/blade_renderer.rs
@@ -421,7 +421,10 @@ impl BladeRenderer {
     /// Like `update_drawable_size` but skips the check that the size has changed. This is useful in
     /// cases like restoring a window from minimization where the size is the same but the
     /// renderer's swap chain needs to be recreated.
-    #[cfg_attr(any(target_os = "macos", target_os = "linux", target_os = "freebsd"), allow(dead_code))]
+    #[cfg_attr(
+        any(target_os = "macos", target_os = "linux", target_os = "freebsd"),
+        allow(dead_code)
+    )]
     pub fn update_drawable_size_even_if_unchanged(&mut self, size: Size<DevicePixels>) {
         self.update_drawable_size_impl(size, true);
     }
