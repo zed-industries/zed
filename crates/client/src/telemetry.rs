0436e50c718ca71a1c2fdb276fde5af55580672b
Hiroki Tagato <tagattie@gmail.com>
2025-06-21 21:58:33 +0900

    Identify OS as FreeBSD, not Linux in OS name telemetry detection

--- crates/client/src/telemetry.rs
+++ crates/client/src/telemetry.rs
@@ -83,9 +83,13 @@ pub fn os_name() -> String {
     {
         "macOS".to_string()
     }
-    #[cfg(any(target_os = "linux", target_os = "freebsd"))]
+    #[cfg(target_os = "linux")]
     {
         format!("Linux {}", gpui::guess_compositor())
+    }
+    #[cfg(target_os = "freebsd")]
+    {
+        format!("FreeBSD {}", gpui::guess_compositor())
     }
 
     #[cfg(target_os = "windows")]
