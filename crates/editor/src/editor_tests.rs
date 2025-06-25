228deef91c66e534d0a9a25246438c77ff977c3a
Smit Barmase <heysmitbarmase@gmail.com>
2025-06-25 12:11:19 +0530

    tests will be correct after rebase

--- crates/editor/src/editor_tests.rs
+++ crates/editor/src/editor_tests.rs
@@ -21838,23 +21838,22 @@ async fn test_outdent_after_input_for_python(cx: &mut TestAppContext) {
             else:ˇ
     "});
 
-    // TODO: blocked on https://github.com/tree-sitter/tree-sitter-python/pull/304
     // test `except` auto outdents when typed inside `try` block
-    // cx.set_state(indoc! {"
-    //     def main():
-    //         try:
-    //             i = 2
-    //             ˇ
-    // "});
-    // cx.update_editor(|editor, window, cx| {
-    //     editor.handle_input("except:", window, cx);
-    // });
-    // cx.assert_editor_state(indoc! {"
-    //     def main():
-    //         try:
-    //             i = 2
-    //         except:ˇ
-    // "});
+    cx.set_state(indoc! {"
+        def main():
+            try:
+                i = 2
+                ˇ
+    "});
+    cx.update_editor(|editor, window, cx| {
+        editor.handle_input("except:", window, cx);
+    });
+    cx.assert_editor_state(indoc! {"
+        def main():
+            try:
+                i = 2
+            except:ˇ
+    "});
 
     // test `else` auto outdents when typed inside `except` block
     cx.set_state(indoc! {"
