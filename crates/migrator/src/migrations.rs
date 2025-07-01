f616e2a10ec08d739978d24d40c1d2e3a80b8b11
Bennet Bo Fenner <bennetbo@gmx.de>
2025-07-01 15:02:23 +0200

    settings: Remove `version` field migration

--- crates/migrator/src/migrations.rs
+++ crates/migrator/src/migrations.rs
@@ -82,12 +82,6 @@ pub(crate) mod m_2025_06_16 {
     pub(crate) use settings::SETTINGS_PATTERNS;
 }
 
-pub(crate) mod m_2025_06_25 {
-    mod settings;
-
-    pub(crate) use settings::SETTINGS_PATTERNS;
-}
-
 pub(crate) mod m_2025_06_27 {
     mod settings;
 
