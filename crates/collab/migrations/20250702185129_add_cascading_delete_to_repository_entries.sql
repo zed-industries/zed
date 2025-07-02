ad9d5f25d80e179d5349e920e00bd94ba63d3aec
Cole Miller <cole@zed.dev>
2025-07-02 16:47:10 -0400

    squawk?

--- crates/collab/migrations/20250702185129_add_cascading_delete_to_repository_entries.sql
+++ crates/collab/migrations/20250702185129_add_cascading_delete_to_repository_entries.sql
@@ -5,7 +5,11 @@ ALTER TABLE project_repositories
     ADD CONSTRAINT fk_project_repositories_project_id
         FOREIGN KEY (project_id)
         REFERENCES projects (id)
-        ON DELETE CASCADE;
+        ON DELETE CASCADE
+        NOT VALID;
+
+ALTER TABLE project_repositories
+    VALIDATE CONSTRAINT fk_project_repositories_project_id;
 
 DELETE FROM project_repository_statuses
 WHERE project_id NOT IN (SELECT id FROM projects);
@@ -14,4 +18,8 @@ ALTER TABLE project_repository_statuses
     ADD CONSTRAINT fk_project_repository_statuses_project_id
         FOREIGN KEY (project_id)
         REFERENCES projects (id)
-        ON DELETE CASCADE;
+        ON DELETE CASCADE
+        NOT VALID;
+
+ALTER TABLE project_repository_statuses
+    VALIDATE CONSTRAINT fk_project_repository_statuses_project_id;
