204ff75ba2ab5838b0c265e8f61ed04cda9c7406
teapo <75266237+4teapo@users.noreply.github.com>
2025-07-07 19:23:09 +0200

    Add DiagnosticsBatchUpdated assert to test

--- crates/project/src/project_tests.rs
+++ crates/project/src/project_tests.rs
@@ -1621,6 +1621,7 @@ async fn test_disk_based_diagnostics_progress(cx: &mut gpui::TestAppContext) {
             path: (worktree_id, Path::new("a.rs")).into()
         }
     );
+    assert_eq!(events.next().await.unwrap(), Event::DiagnosticsBatchUpdated);
 
     fake_server.end_progress(format!("{}/0", progress_token));
     assert_eq!(
@@ -1669,6 +1670,7 @@ async fn test_disk_based_diagnostics_progress(cx: &mut gpui::TestAppContext) {
             path: (worktree_id, Path::new("a.rs")).into()
         }
     );
+    assert_eq!(events.next().await.unwrap(), Event::DiagnosticsBatchUpdated);
 
     fake_server.notify::<lsp::notification::PublishDiagnostics>(&lsp::PublishDiagnosticsParams {
         uri: Url::from_file_path(path!("/dir/a.rs")).unwrap(),
