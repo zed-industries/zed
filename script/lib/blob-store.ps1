a663c5ddc203cc35dcef2e92b759559f424c9c4d
Kate <kate@zed.dev>
2025-07-08 13:21:37 +0200

    fix s3 upload 1

--- script/lib/blob-store.ps1
+++ script/lib/blob-store.ps1
@@ -10,33 +10,33 @@ function UploadToBlobStoreWithACL {
     $Date = (Get-Date).ToUniversalTime().ToString("r")
     # Note: Original script had a bug where it overrode the ACL parameter
     # I'm keeping the same behavior for compatibility
-    $ACL = "x-amz-acl:public-read"
+    $ACL = "public-read"
     $ContentType = "application/octet-stream"
-    $StorageType = "x-amz-storage-class:STANDARD"
-    
+    $StorageClass = "STANDARD"
+
     # Create string to sign
     $StringToSign = "PUT`n`n${ContentType}`n${Date}`n${ACL}`n${StorageType}`n/${BucketName}/${BlobStoreKey}"
-    
+
     # Generate HMAC-SHA1 signature
     $HMACSHA1 = New-Object System.Security.Cryptography.HMACSHA1
     $HMACSHA1.Key = [System.Text.Encoding]::UTF8.GetBytes($env:DIGITALOCEAN_SPACES_SECRET_KEY)
     $Signature = [System.Convert]::ToBase64String($HMACSHA1.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($StringToSign)))
-    
+
     # Upload file using Invoke-WebRequest (equivalent to curl)
     $Headers = @{
         "Host" = "${BucketName}.nyc3.digitaloceanspaces.com"
         "Date" = $Date
         "Content-Type" = $ContentType
-        $StorageType = ""
-        $ACL = ""
+        "x-amz-storage-class" = $StorageClass
+        "x-amz-acl" = $ACL
         "Authorization" = "AWS ${env:DIGITALOCEAN_SPACES_ACCESS_KEY}:$Signature"
     }
-    
+
     $Uri = "https://${BucketName}.nyc3.digitaloceanspaces.com/${BlobStoreKey}"
-    
+
     # Read file content
     $FileContent = Get-Content $FileToUpload -Raw -AsByteStream
-    
+
     try {
         Invoke-WebRequest -Uri $Uri -Method PUT -Headers $Headers -Body $FileContent -ContentType $ContentType -Verbose
         Write-Host "Successfully uploaded $FileToUpload to $Uri" -ForegroundColor Green
@@ -53,8 +53,8 @@ function UploadToBlobStorePublic {
         [string]$FileToUpload,
         [string]$BlobStoreKey
     )
-    
-    UploadToBlobStoreWithACL -BucketName $BucketName -FileToUpload $FileToUpload -BlobStoreKey $BlobStoreKey -ACL "x-amz-acl:public-read"
+
+    UploadToBlobStoreWithACL -BucketName $BucketName -FileToUpload $FileToUpload -BlobStoreKey $BlobStoreKey -ACL "public-read"
 }
 
 function UploadToBlobStore {
@@ -63,6 +63,6 @@ function UploadToBlobStore {
         [string]$FileToUpload,
         [string]$BlobStoreKey
     )
-    
-    UploadToBlobStoreWithACL -BucketName $BucketName -FileToUpload $FileToUpload -BlobStoreKey $BlobStoreKey -ACL "x-amz-acl:private"
+
+    UploadToBlobStoreWithACL -BucketName $BucketName -FileToUpload $FileToUpload -BlobStoreKey $BlobStoreKey -ACL "private"
 }
