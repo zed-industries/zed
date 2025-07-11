5aa42509db0fbda4d6235f557b377662236decfe
Moses Miller <moses@memiller.net>
2025-07-10 23:59:29 -0700

    Merge the application menu and title bar when menus are always shown and project items are hidden

--- crates/title_bar/src/title_bar.rs
+++ crates/title_bar/src/title_bar.rs
@@ -138,7 +138,9 @@ impl Render for TitleBar {
     fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         let title_bar_settings = *TitleBarSettings::get_global(cx);
 
-        let show_menus = show_menus(cx);
+        let mut render_project_items =
+            title_bar_settings.show_branch_name || title_bar_settings.show_project_items;
+        let show_seperate_menus = show_menus(cx) && render_project_items;
 
         let mut children = Vec::new();
 
@@ -146,14 +148,14 @@ impl Render for TitleBar {
             h_flex()
                 .gap_1()
                 .map(|title_bar| {
-                    let mut render_project_items = title_bar_settings.show_branch_name
-                        || title_bar_settings.show_project_items;
                     title_bar
                         .when_some(
-                            self.application_menu.clone().filter(|_| !show_menus),
+                            self.application_menu
+                                .clone()
+                                .filter(|_| !show_seperate_menus),
                             |title_bar, menu| {
                                 render_project_items &=
-                                    !menu.update(cx, |menu, cx| menu.all_menus_shown(cx));
+                                    !menu.read_with(cx, |menu, cx| menu.all_menus_shown(cx));
                                 title_bar.child(menu)
                             },
                         )
@@ -201,7 +203,7 @@ impl Render for TitleBar {
                 .into_any_element(),
         );
 
-        if show_menus {
+        if show_seperate_menus {
             self.platform_titlebar.update(cx, |this, _| {
                 this.set_children(
                     self.application_menu
