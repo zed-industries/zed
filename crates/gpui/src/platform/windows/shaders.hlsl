ffef9fd25a017a0a213683755e52d9faf2487e31
Junkui Zhang <364772080@qq.com>
2025-07-15 23:38:18 +0800

    bringback our colorful avatar

--- crates/gpui/src/platform/windows/shaders.hlsl
+++ crates/gpui/src/platform/windows/shaders.hlsl
@@ -1111,7 +1111,7 @@ float4 polychrome_sprite_fragment(PolychromeSpriteFragmentInput input): SV_Targe
     float distance = quad_sdf(input.position.xy, sprite.bounds, sprite.corner_radii);
 
     float4 color = sample;
-    if (sprite.grayscale) {
+    if ((sprite.grayscale & 0xFFu) != 0u) {
         float3 grayscale = dot(color.rgb, GRAYSCALE_FACTORS);
         color = float4(grayscale, sample.a);
     }
