8358b7f50d54e7f1fd77657c7a7c156f0ade0a60
Richard Feldman <oss@rtfeldman.com>
2025-07-17 14:41:09 -0400

    Just say "Error" when there's an Agent error

--- crates/agent_ui/src/active_thread.rs
+++ crates/agent_ui/src/active_thread.rs
@@ -1036,7 +1036,7 @@ impl ActiveThread {
                             .collect::<Vec<_>>()
                             .join("\n");
                         self.last_error = Some(ThreadError::Message {
-                            header: "Error interacting with language model".into(),
+                            header: "Error".into(),
                             message: error_message.into(),
                         });
                     }
