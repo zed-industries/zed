7bd27c25dd641d15cba75e045f66eddbe27e0790
Anthony Eid <56899983+Anthony-Eid@users.noreply.github.com>
2025-07-18 14:09:28 -0400

    keymap ui: Fix remove key mapping bug (#34683)
    Release Notes:
    
    - N/A
    
    ---------
    
    Co-authored-by: Ben Kunkle <ben@zed.dev>

--- crates/settings/src/keymap_file.rs
+++ crates/settings/src/keymap_file.rs
@@ -1623,4 +1623,44 @@ mod tests {
             .unindent(),
         );
     }
+
+    #[test]
+    fn test_keymap_remove() {
+        zlog::init_test();
+
+        check_keymap_update(
+            r#"
+            [
+              {
+                "context": "Editor",
+                "bindings": {
+                  "cmd-k cmd-u": "editor::ConvertToUpperCase",
+                  "cmd-k cmd-l": "editor::ConvertToLowerCase",
+                  "cmd-[": "pane::GoBack",
+                }
+              },
+            ]
+            "#,
+            KeybindUpdateOperation::Remove {
+                target: KeybindUpdateTarget {
+                    context: Some("Editor"),
+                    keystrokes: &parse_keystrokes("cmd-k cmd-l"),
+                    action_name: "editor::ConvertToLowerCase",
+                    action_arguments: None,
+                },
+                target_keybind_source: KeybindSource::User,
+            },
+            r#"
+            [
+              {
+                "context": "Editor",
+                "bindings": {
+                  "cmd-k cmd-u": "editor::ConvertToUpperCase",
+                  "cmd-[": "pane::GoBack",
+                }
+              },
+            ]
+            "#,
+        );
+    }
 }
