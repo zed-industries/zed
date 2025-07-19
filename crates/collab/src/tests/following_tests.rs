98534e6dc85917675f447da24d125295c09e6c38
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-07-19 12:47:31 +0200

    fixup! Yay, we're fixing tests now

--- crates/collab/src/tests/following_tests.rs
+++ crates/collab/src/tests/following_tests.rs
@@ -456,7 +456,7 @@ async fn test_basic_following(
             .await
             .unwrap();
         cx_b.set_screen_capture_sources(vec![display]);
-        let source = cx_b.screen_capture_sources().next().unwrap();
+        let source = cx_b.read(|cx| cx.screen_capture_sources().next().unwrap());
         active_call_b
             .update(cx_b, |call, cx| {
                 call.room()
