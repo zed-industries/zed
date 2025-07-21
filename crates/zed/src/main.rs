c7a2f28a136b9ca4142d464f077f8e6a5f5cfeba
Anthony <anthony@zed.dev>
2025-07-21 12:01:43 -0400

    Remove todo comment

--- crates/zed/src/main.rs
+++ crates/zed/src/main.rs
@@ -1041,7 +1041,6 @@ async fn restore_or_create_workspace(app_state: Arc<AppState>, cx: &mut AsyncApp
                 );
             }
         }
-        // todo! make this false
     } else if matches!(KEY_VALUE_STORE.read_kvp(FIRST_OPEN), Ok(None)) {
         let state = app_state.clone();
         cx.update(|cx| show_onboarding_view(state, cx))?.await?;
