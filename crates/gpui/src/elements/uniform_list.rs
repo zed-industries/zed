a65b1a42f5ae01b04e58b5c12f6bce9a06609959
Jason Lee <huacnlee@gmail.com>
2025-07-24 15:51:15 +0800

    Remove set_bounds

--- crates/gpui/src/elements/uniform_list.rs
+++ crates/gpui/src/elements/uniform_list.rs
@@ -296,8 +296,7 @@ impl Element for UniformList {
                 );
 
                 let y_flipped = if let Some(scroll_handle) = self.scroll_handle.as_mut() {
-                    let mut scroll_state = scroll_handle.0.borrow_mut();
-                    scroll_state.base_handle.set_bounds(bounds);
+                    let scroll_state = scroll_handle.0.borrow();
                     scroll_state.y_flipped
                 } else {
                     false
