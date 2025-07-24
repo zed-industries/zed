fb4beacc5a4a479e8b187d7dbd6d36aa58ef54fb
Junkui Zhang <364772080@qq.com>
2025-07-24 17:11:22 +0800

    clippy

--- crates/gpui/src/platform/blade/blade_renderer.rs
+++ crates/gpui/src/platform/blade/blade_renderer.rs
@@ -3,8 +3,8 @@
 
 use super::{BladeAtlas, BladeContext};
 use crate::{
-    Background, Bounds, DevicePixels, GpuSpecs, MonochromeSprite, Path, PathVertex, Point,
-    PolychromeSprite, PrimitiveBatch, Quad, ScaledPixels, Scene, Shadow, Size, Underline,
+    Background, Bounds, DevicePixels, GpuSpecs, MonochromeSprite, Path, Point, PolychromeSprite,
+    PrimitiveBatch, Quad, ScaledPixels, Scene, Shadow, Size, Underline,
 };
 use blade_graphics as gpu;
 use blade_util::{BufferBelt, BufferBeltDescriptor};
