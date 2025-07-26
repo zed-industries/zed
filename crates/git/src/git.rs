f6b81071f64457dfd549dd649b80fc21d34a6cb9
Guillaume Launay <guillaume.launay@paylead.fr>
2025-07-26 20:37:55 +0200

    Add branch rename action to Git panel
    Also add it to the menu next to branch name

--- crates/git/src/git.rs
+++ crates/git/src/git.rs
@@ -93,6 +93,8 @@ actions!(
         Init,
         /// Opens all modified files in the editor.
         OpenModifiedFiles,
+        /// Renames the current branch.
+        RenameBranch,
     ]
 );
 
