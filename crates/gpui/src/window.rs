a92c1f188aa131a42f6d40c60d0cea166393dbc9
Finn Evers <finn@zed.dev>
2025-07-28 23:33:20 +0200

    gpui: Ensure tab index handles are properly reused across frames (#35235)
    This fixes an issue where focus handles with a tab index would get lost
    between rendered frames because the focus handles were not reused for
    the following paint cycle.
    
    Release Notes:
    
    - N/A

--- crates/gpui/src/window.rs
+++ crates/gpui/src/window.rs
@@ -702,6 +702,7 @@ pub(crate) struct PaintIndex {
     input_handlers_index: usize,
     cursor_styles_index: usize,
     accessed_element_states_index: usize,
+    tab_handle_index: usize,
     line_layout_index: LineLayoutIndex,
 }
 
@@ -2208,6 +2209,7 @@ impl Window {
             input_handlers_index: self.next_frame.input_handlers.len(),
             cursor_styles_index: self.next_frame.cursor_styles.len(),
             accessed_element_states_index: self.next_frame.accessed_element_states.len(),
+            tab_handle_index: self.next_frame.tab_handles.handles.len(),
             line_layout_index: self.text_system.layout_index(),
         }
     }
@@ -2236,6 +2238,12 @@ impl Window {
                 ..range.end.accessed_element_states_index]
                 .iter()
                 .map(|(id, type_id)| (GlobalElementId(id.0.clone()), *type_id)),
+        );
+        self.next_frame.tab_handles.handles.extend(
+            self.rendered_frame.tab_handles.handles
+                [range.start.tab_handle_index..range.end.tab_handle_index]
+                .iter()
+                .cloned(),
         );
 
         self.text_system
