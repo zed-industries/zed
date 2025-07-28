be574f2af08780721134bc55211ad3a870464622
Kirill Bulatov <kirill@zed.dev>
2025-07-28 23:45:49 +0000

    Fix tasks leaked despite workspace window close (#35246)
    Closes https://github.com/zed-industries/zed/issues/34932
    
    Release Notes:
    
    - Fixed tasks leaked despite workspace window close

--- crates/workspace/src/workspace.rs
+++ crates/workspace/src/workspace.rs
@@ -1097,6 +1097,7 @@ pub struct Workspace {
     serialized_ssh_project: Option<SerializedSshProject>,
     _items_serializer: Task<Result<()>>,
     session_id: Option<String>,
+    scheduled_tasks: Vec<Task<()>>,
 }
 
 impl EventEmitter<Event> for Workspace {}
@@ -1428,6 +1429,7 @@ impl Workspace {
             _items_serializer,
             session_id: Some(session_id),
             serialized_ssh_project: None,
+            scheduled_tasks: Vec::new(),
         }
     }
 
