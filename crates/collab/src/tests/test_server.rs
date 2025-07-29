77dc65d8261f38d8c2c8648de26786f253d8de5a
Marshall Bowers <git@maxdeviant.com>
2025-07-29 13:06:27 -0400

    collab: Attach `User-Agent` to `handle connection` span (#35282)
    This PR makes it so we attach the value from the `User-Agent` header to
    the `handle connection` span.
    
    We'll start sending this header in
    https://github.com/zed-industries/zed/pull/35280.
    
    Release Notes:
    
    - N/A

--- crates/collab/src/tests/test_server.rs
+++ crates/collab/src/tests/test_server.rs
@@ -256,6 +256,7 @@ impl TestServer {
                             ZedVersion(SemanticVersion::new(1, 0, 0)),
                             None,
                             None,
+                            None,
                             Some(connection_id_tx),
                             Executor::Deterministic(cx.background_executor().clone()),
                             None,
