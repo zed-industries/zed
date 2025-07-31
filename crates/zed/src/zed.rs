21ff9fd5ce59aed6cdac77cfc9df06997583f306
Matin Aniss <76515905+MatinAniss@users.noreply.github.com>
2025-07-31 18:59:53 +1000

    Fix

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -4352,6 +4352,7 @@ mod tests {
                 "keymap_editor",
                 "keystroke_input",
                 "language_selector",
+                "line_ending_selector",
                 "lsp_tool",
                 "markdown",
                 "menu",
