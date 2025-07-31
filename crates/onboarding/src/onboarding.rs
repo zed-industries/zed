a729d5d5e1bb0d0041e1645b1e7ed23a326fa21a
Danilo Leal <daniloleal09@gmail.com>
2025-07-31 09:55:16 -0300

    Put the AI page back in after merge with main

--- crates/onboarding/src/onboarding.rs
+++ crates/onboarding/src/onboarding.rs
@@ -1,4 +1,7 @@
-use crate::welcome::{ShowWelcome, WelcomePage};
+use crate::{
+    ai_setup_page::AiConfigurationPage,
+    welcome::{ShowWelcome, WelcomePage},
+};
 use command_palette_hooks::CommandPaletteFilter;
 use db::kvp::KEY_VALUE_STORE;
 use feature_flags::{FeatureFlag, FeatureFlagViewExt as _};
@@ -20,6 +23,7 @@ use workspace::{
     open_new, with_active_or_new_workspace,
 };
 
+mod ai_setup_page;
 mod basics_page;
 mod editing_page;
 mod welcome;
@@ -213,15 +217,19 @@ struct Onboarding {
     workspace: WeakEntity<Workspace>,
     focus_handle: FocusHandle,
     selected_page: SelectedPage,
+    ai_configuration_page: Entity<AiConfigurationPage>,
     _settings_subscription: Subscription,
 }
 
 impl Onboarding {
     fn new(workspace: WeakEntity<Workspace>, cx: &mut App) -> Entity<Self> {
+        let ai_configuration_page = cx.new(|_| AiConfigurationPage::new(workspace.clone()));
+
         cx.new(|cx| Self {
             workspace,
             focus_handle: cx.focus_handle(),
             selected_page: SelectedPage::Basics,
+            ai_configuration_page,
             _settings_subscription: cx.observe_global::<SettingsStore>(move |_, cx| cx.notify()),
         })
     }
@@ -353,12 +361,8 @@ impl Onboarding {
             SelectedPage::Editing => {
                 crate::editing_page::render_editing_page(window, cx).into_any_element()
             }
-            SelectedPage::AiSetup => self.render_ai_setup_page(window, cx).into_any_element(),
+            SelectedPage::AiSetup => self.ai_configuration_page.clone().into_any_element(),
         }
-    }
-
-    fn render_ai_setup_page(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
-        div().child("ai setup page")
     }
 }
 
