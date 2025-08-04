b84d821d9103fb888a069fb5ed406274590170fd
Antonio Scandurra <me@as-cii.com>
2025-08-04 10:24:42 +0200

    Undo unnecessary change

--- crates/client/src/test.rs
+++ crates/client/src/test.rs
@@ -282,14 +282,14 @@ pub fn parse_authorization_header(req: &Request<AsyncBody>) -> Option<Credential
 
 pub fn make_get_authenticated_user_response(
     user_id: i32,
-    github_login: impl Into<String>,
+    github_login: String,
 ) -> GetAuthenticatedUserResponse {
     GetAuthenticatedUserResponse {
         user: AuthenticatedUser {
             id: user_id,
             metrics_id: format!("metrics-id-{user_id}"),
             avatar_url: "".to_string(),
-            github_login: github_login.into(),
+            github_login,
             name: None,
             is_staff: false,
             accepted_tos_at: None,
