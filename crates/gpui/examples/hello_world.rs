1f19d81d16f89386d864df033dabd1ee80aba3b8
Victor Tran <vicr12345@gmail.com>
2025-08-04 22:25:38 +1000

    it works
    Co-authored-by: 143mailliw <wwokwilliam@gmail.com>

--- crates/gpui/examples/hello_world.rs
+++ crates/gpui/examples/hello_world.rs
@@ -10,6 +10,7 @@ struct HelloWorld {
 impl Render for HelloWorld {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
         div()
+            .dir_rtl()
             .flex()
             .flex_col()
             .gap_3()
