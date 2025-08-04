096c9be97c5f65d1eedc691e71ccb9066ed82caf
Antonio Scandurra <me@as-cii.com>
2025-08-04 10:23:23 +0200

    Don't trigger authentication flow unless credentials expired
    This fixes a regression introduced in #35471, where we treated stored
    credentials as invalid when failing to retrieve the authenticated user for any
    reason. This had the side effect of triggering the auth flow even when e.g. the
    client/server had temporary networking issues.
    
    This commit changes the logic to only trigger authentication when getting a 401
    from the server.

--- crates/client/src/test.rs
+++ crates/client/src/test.rs
@@ -282,14 +282,14 @@ pub fn parse_authorization_header(req: &Request<AsyncBody>) -> Option<Credential
 
 pub fn make_get_authenticated_user_response(
     user_id: i32,
-    github_login: String,
+    github_login: impl Into<String>,
 ) -> GetAuthenticatedUserResponse {
     GetAuthenticatedUserResponse {
         user: AuthenticatedUser {
             id: user_id,
             metrics_id: format!("metrics-id-{user_id}"),
             avatar_url: "".to_string(),
-            github_login,
+            github_login: github_login.into(),
             name: None,
             is_staff: false,
             accepted_tos_at: None,
