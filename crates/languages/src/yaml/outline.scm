2f0889ea1b093471a0d70c3cd47f941c67408f64
Mikayla Maki <mikayla.c.maki@gmail.com>
2025-08-03 20:57:37 -0700

    Improve the yaml outline to show the key value if it's a simple string

--- crates/languages/src/yaml/outline.scm
+++ crates/languages/src/yaml/outline.scm
@@ -1,1 +1,7 @@
-(block_mapping_pair key: (flow_node (plain_scalar (string_scalar) @name))) @item
+(block_mapping_pair
+    key: (flow_node
+            (plain_scalar
+                (string_scalar) @name))
+    value: (flow_node
+            (plain_scalar
+                (string_scalar) @context))?) @item
