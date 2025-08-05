da3bcd3f666901d1f39654262c7ba2bdfdd0f6aa
Victor Tran <vicr12345@gmail.com>
2025-08-05 22:24:32 +1000

    Add macOS menus to bidi example

--- crates/gpui/examples/bidi.rs
+++ crates/gpui/examples/bidi.rs
@@ -1,7 +1,5 @@
-use gpui::{
-    App, Application, Bounds, Context, LayoutDirection, Window, WindowBounds, WindowOptions, div,
-    prelude::*, px, rgb, size,
-};
+use metal::MTLPrimitiveType::Point;
+use gpui::{App, Application, Bounds, Context, KeyBinding, LayoutDirection, Menu, MenuItem, Window, WindowBounds, WindowOptions, actions, div, prelude::*, px, rgb, size, TitlebarOptions, point};
 
 #[derive(IntoElement)]
 struct BidiExampleComponent {
@@ -18,7 +16,6 @@ impl RenderOnce for BidiExampleComponent {
             .flex()
             .flex_col()
             .w_full()
-            .p(px(20.0))
             .gap_2()
             .child(div().text_3xl().child(self.header))
             .child(self.sub_title)
@@ -71,12 +68,16 @@ impl Render for BidiView {
         let main_color = rgb(0xF0F0F3);
 
         div()
+            .id("bidi-root")
+            .overflow_y_scroll()
             .bg(rgb(0x0c0c11))
             .text_color(main_color)
             .flex()
             .w_full()
             .h_full()
             .flex_col()
+            .p(px(20.0))
+            .gap(px(10.0))
             .child(BidiExampleComponent {
                 header: "This div uses the window's default window direction!",
                 sub_title: "Try changing layout_direction in the example code's WindowOptions!",
@@ -94,15 +95,38 @@ impl Render for BidiView {
                                   displayed on the right side, as the directionality for the \
                                   parent is set to left-to-right.",
             }))
+            .child(
+                div()
+                    .w_full()
+                    .dir_ltr()
+                    .child("If you're on macOS, the menu items are also rendered in RTL."),
+            )
     }
 }
 
 fn main() {
     Application::new().run(|cx: &mut App| {
         let bounds = Bounds::centered(None, size(px(800.), px(600.0)), cx);
+        // Set the layout direction for anything isolated from a window, for example,
+        // the menu bar on macOS
+        cx.set_default_layout_direction(LayoutDirection::RightToLeft);
+        cx.on_action(quit);
+        cx.bind_keys([KeyBinding::new("secondary-q", Quit, None)]);
+        cx.set_menus(vec![Menu {
+            name: "Bidirectionality Example".into(),
+            items: vec![MenuItem::action("Quit", Quit)],
+        }]);
+
         cx.open_window(
             WindowOptions {
                 window_bounds: Some(WindowBounds::Windowed(bounds)),
+                // Set the default layout direction for the window. Setting this to RTL will
+                // cause the window controls to be drawn RTL on supported platforms.
+
+                // NOTE: On macOS, the default window controls are always drawn RTL when the system
+                // language is RTL, and LTR otherwise. When using client-side decorations, as long
+                // as you set titlebar.traffic_light_position, GPUI will not respect this and will
+                // draw the window controls in the layout direction set here.
                 layout_direction: LayoutDirection::RightToLeft,
                 ..Default::default()
             },
@@ -111,4 +135,13 @@ fn main() {
         .unwrap();
         cx.activate(true);
     });
+}
+
+// Associate actions using the `actions!` macro (or `Action` derive macro)
+actions!(menu_actions, [Quit]);
+
+// Define the quit function that is registered with the App
+fn quit(_: &Quit, cx: &mut App) {
+    println!("Gracefully quitting the application . . .");
+    cx.quit();
 }
