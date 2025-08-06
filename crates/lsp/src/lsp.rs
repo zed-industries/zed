8966af8765a2e92261bd8359042ee2d1ebf621d9
xdBronch <51252236+xdBronch@users.noreply.github.com>
2025-08-06 09:45:39 -0400

    LSP: advertise support for markdown in completion documentation

--- crates/lsp/src/lsp.rs
+++ crates/lsp/src/lsp.rs
@@ -720,6 +720,10 @@ impl LanguageServer {
                                     InsertTextMode::ADJUST_INDENTATION,
                                 ],
                             }),
+                            documentation_format: Some(vec![
+                                MarkupKind::Markdown,
+                                MarkupKind::PlainText,
+                            ]),
                             ..Default::default()
                         }),
                         insert_text_mode: Some(InsertTextMode::ADJUST_INDENTATION),
