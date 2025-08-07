59f05279498fcc69e775a96a7ed13be5cde46c2e
Jason Lee <huacnlee@gmail.com>
2025-08-07 17:03:23 +0800

    fix typos

--- crates/collab/src/tests/integration_tests.rs
+++ crates/collab/src/tests/integration_tests.rs
@@ -4717,12 +4717,12 @@ async fn test_prettier_formatting_buffer(
         });
     });
 
-    const TS_FORMATED_PREFIX: &'static str = "<format by ts>\n";
+    const TS_FORMATTED_PREFIX: &'static str = "<format by ts>\n";
     let fake_language_server = fake_language_servers.next().await.unwrap();
     fake_language_server.set_request_handler::<lsp::request::Formatting, _, _>(|_, _| async move {
         Ok(Some(vec![lsp::TextEdit {
             range: lsp::Range::new(lsp::Position::new(0, 0), lsp::Position::new(0, 0)),
-            new_text: TS_FORMATED_PREFIX.to_string(),
+            new_text: TS_FORMATTED_PREFIX.to_string(),
         }]))
     });
 
@@ -4742,7 +4742,7 @@ async fn test_prettier_formatting_buffer(
     executor.run_until_parked();
     assert_eq!(
         buffer_b.read_with(cx_b, |buffer, _| buffer.text()),
-        TS_FORMATED_PREFIX.to_string() + buffer_text + "\n" + prettier_format_suffix,
+        TS_FORMATTED_PREFIX.to_string() + buffer_text + "\n" + prettier_format_suffix,
         "Prettier formatting was not applied to client buffer after client's request"
     );
 
@@ -4762,8 +4762,8 @@ async fn test_prettier_formatting_buffer(
     executor.run_until_parked();
     assert_eq!(
         buffer_b.read_with(cx_b, |buffer, _| buffer.text()),
-        TS_FORMATED_PREFIX.to_string()
-            + TS_FORMATED_PREFIX
+        TS_FORMATTED_PREFIX.to_string()
+            + TS_FORMATTED_PREFIX
             + buffer_text
             + "\n"
             + prettier_format_suffix
