abbbf0a65a542f85e612db237eb16448493a82fb
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-08-08 17:50:59 +0200

    wip

--- crates/agent2/src/tools.rs
+++ crates/agent2/src/tools.rs
@@ -1,9 +1,11 @@
 mod edit_file_tool;
 mod find_path_tool;
 mod read_file_tool;
+mod terminal_tool;
 mod thinking_tool;
 
 pub use edit_file_tool::*;
 pub use find_path_tool::*;
 pub use read_file_tool::*;
+pub use terminal_tool::*;
 pub use thinking_tool::*;
