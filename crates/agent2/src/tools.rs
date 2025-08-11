c61e860fe3c32c8e552fe1addafbcb550b1c949d
Antonio Scandurra <me@as-cii.com>
2025-08-11 14:34:27 +0200

    Introduce `DeletePathTool`

--- crates/agent2/src/tools.rs
+++ crates/agent2/src/tools.rs
@@ -1,5 +1,6 @@
 mod copy_path_tool;
 mod create_directory_tool;
+mod delete_path_tool;
 mod edit_file_tool;
 mod find_path_tool;
 mod list_directory_tool;
@@ -10,6 +11,7 @@ mod thinking_tool;
 
 pub use copy_path_tool::*;
 pub use create_directory_tool::*;
+pub use delete_path_tool::*;
 pub use edit_file_tool::*;
 pub use find_path_tool::*;
 pub use list_directory_tool::*;
