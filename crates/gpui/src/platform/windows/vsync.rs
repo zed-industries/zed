2872ef9b05c4f7fbe7c2df57033229e1ff04fc9b
Junkui Zhang <364772080@qq.com>
2025-08-12 01:16:27 +0800

    use raw binding instead

--- crates/gpui/src/platform/windows/vsync.rs
+++ crates/gpui/src/platform/windows/vsync.rs
@@ -5,18 +5,22 @@ use std::{
 
 use anyhow::{Context, Result};
 use util::ResultExt;
-use windows::Win32::{
-    Foundation::HWND,
-    Graphics::{
-        DirectComposition::{
-            COMPOSITION_FRAME_ID_COMPLETED, COMPOSITION_FRAME_STATS, DCompositionGetFrameId,
-            DCompositionGetStatistics, DCompositionWaitForCompositorClock,
+use windows::{
+    Win32::{
+        Foundation::{HANDLE, HWND},
+        Graphics::{
+            DirectComposition::{
+                COMPOSITION_FRAME_ID_COMPLETED, COMPOSITION_FRAME_ID_TYPE, COMPOSITION_FRAME_STATS,
+                COMPOSITION_TARGET_ID,
+            },
+            Dwm::{DWM_TIMING_INFO, DwmFlush, DwmGetCompositionTimingInfo},
         },
-        Dwm::{DWM_TIMING_INFO, DwmFlush, DwmGetCompositionTimingInfo},
+        System::{
+            LibraryLoader::GetProcAddress, Performance::QueryPerformanceFrequency,
+            Threading::INFINITE,
+        },
     },
-    System::{
-        LibraryLoader::GetProcAddress, Performance::QueryPerformanceFrequency, Threading::INFINITE,
-    },
+    core::{HRESULT, s},
 };
 
 use crate::with_dll_library;
@@ -32,6 +36,18 @@ static QPC_TICKS_PER_SECOND: LazyLock<u64> = LazyLock::new(|| {
 const VSYNC_INTERVAL_THRESHOLD: Duration = Duration::from_millis(1);
 const DEFAULT_VSYNC_INTERVAL: Duration = Duration::from_micros(16_666); // ~60Hz
 
+type DCompositionGetFrameId =
+    unsafe extern "system" fn(frameidtype: COMPOSITION_FRAME_ID_TYPE, frameid: *mut u64) -> HRESULT;
+type DCompositionGetStatistics = unsafe extern "system" fn(
+    frameid: u64,
+    framestats: *mut COMPOSITION_FRAME_STATS,
+    targetidcount: u32,
+    targetids: *mut COMPOSITION_TARGET_ID,
+    actualtargetidcount: *mut u32,
+) -> HRESULT;
+type DCompositionWaitForCompositorClock =
+    unsafe extern "system" fn(count: u32, handles: *const HANDLE, timeoutinms: u32) -> u32;
+
 pub(crate) struct VSyncProvider {
     interval: Duration,
     f: Box<dyn Fn() -> bool>,
@@ -39,34 +55,46 @@ pub(crate) struct VSyncProvider {
 
 impl VSyncProvider {
     pub(crate) fn new() -> Self {
-        let support_direct_composition =
-            with_dll_library(windows::core::s!("dcomp.dll"), |dcomp| unsafe {
-                let frame_id_fn =
-                    GetProcAddress(dcomp, windows::core::s!("DCompositionGetFrameId")).is_some();
-                let stats_fn =
-                    GetProcAddress(dcomp, windows::core::s!("DCompositionGetStatistics")).is_some();
-                let vsync_fn = GetProcAddress(
-                    dcomp,
-                    windows::core::s!("DCompositionWaitForCompositorClock"),
-                )
-                .is_some();
-                Ok(frame_id_fn && stats_fn && vsync_fn)
+        if let Ok((get_frame_id, get_statistics, wait_for_comp_clock)) =
+            with_dll_library(s!("dcomp.dll"), |dcomp| unsafe {
+                let get_frame_id = GetProcAddress(dcomp, s!("DCompositionGetFrameId"))
+                    .ok_or(anyhow::anyhow!("Function DCompositionGetFrameId not found"))?;
+                let get_statistics = GetProcAddress(dcomp, s!("DCompositionGetStatistics")).ok_or(
+                    anyhow::anyhow!("Function DCompositionGetStatistics not found"),
+                )?;
+                let wait_for_comp_clock =
+                    GetProcAddress(dcomp, s!("DCompositionWaitForCompositorClock")).ok_or(
+                        anyhow::anyhow!("Function DCompositionWaitForCompositorClock not found"),
+                    )?;
+                Ok((get_frame_id, get_statistics, wait_for_comp_clock))
             })
-            .is_ok_and(|result| result);
-        if support_direct_composition {
-            log::info!("DirectComposition is supported for VSync");
-            let interval = get_dwm_interval_from_direct_composition()
+        {
+            let get_frame_id: DCompositionGetFrameId = unsafe { std::mem::transmute(get_frame_id) };
+            let get_statistics: DCompositionGetStatistics =
+                unsafe { std::mem::transmute(get_statistics) };
+            let wait_for_comp_clock: DCompositionWaitForCompositorClock =
+                unsafe { std::mem::transmute(wait_for_comp_clock) };
+            let interval = get_dwm_interval_from_direct_composition(get_frame_id, get_statistics)
                 .context("Failed to get DWM interval from DirectComposition")
                 .log_err()
                 .unwrap_or(DEFAULT_VSYNC_INTERVAL);
-            let f = Box::new(|| unsafe { DCompositionWaitForCompositorClock(None, INFINITE) == 0 });
+            log::info!(
+                "DirectComposition is supported for VSync, interval: {:?}",
+                interval
+            );
+            let f = Box::new(move || unsafe {
+                wait_for_comp_clock(0, std::ptr::null(), INFINITE) == 0
+            });
             Self { interval, f }
         } else {
-            log::info!("DirectComposition is not supported for VSync, falling back to DWM");
             let interval = get_dwm_interval()
                 .context("Failed to get DWM interval")
                 .log_err()
                 .unwrap_or(DEFAULT_VSYNC_INTERVAL);
+            log::info!(
+                "DirectComposition is not supported for VSync, falling back to DWM, interval: {:?}",
+                interval
+            );
             let f = Box::new(|| unsafe { DwmFlush().is_ok() });
             Self { interval, f }
         }
@@ -90,10 +118,23 @@ impl VSyncProvider {
     }
 }
 
-fn get_dwm_interval_from_direct_composition() -> Result<Duration> {
-    let frame_id = unsafe { DCompositionGetFrameId(COMPOSITION_FRAME_ID_COMPLETED) }?;
+fn get_dwm_interval_from_direct_composition(
+    get_frame_id: DCompositionGetFrameId,
+    get_statistics: DCompositionGetStatistics,
+) -> Result<Duration> {
+    let mut frame_id = 0;
+    unsafe { get_frame_id(COMPOSITION_FRAME_ID_COMPLETED, &mut frame_id) }.ok()?;
     let mut stats = COMPOSITION_FRAME_STATS::default();
-    unsafe { DCompositionGetStatistics(frame_id, &mut stats, 0, None, None) }?;
+    unsafe {
+        get_statistics(
+            frame_id,
+            &mut stats,
+            0,
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+        )
+    }
+    .ok()?;
     Ok(retrieve_duration(stats.framePeriod, *QPC_TICKS_PER_SECOND))
 }
 
