325612cdacc6f2fd14375077ba14870f8fc93569
Alvaro Parker <aparkerdf@protonmail.com>
2025-08-10 23:24:40 -0400

    Fix tests

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -4369,6 +4369,7 @@ mod tests {
                 "search",
                 "settings_profile_selector",
                 "snippets",
+                "stash_picker",
                 "supermaven",
                 "svg",
                 "tab_switcher",
