81a62dcc3717eefc790a8b1285e32883082e7a98
Jason Lee <huacnlee@gmail.com>
2025-08-12 10:16:33 +0800

    Revert "fix test"
    This reverts commit ccb827e77f2cc2caea7627a7d35e4c08d3a85905.

--- crates/project/src/lsp_store.rs
+++ crates/project/src/lsp_store.rs
@@ -1438,7 +1438,7 @@ impl LocalLspStore {
         let formatters = match (trigger, &settings.format_on_save) {
             (FormatTrigger::Save, FormatOnSave::Off) => &[],
             (FormatTrigger::Save, FormatOnSave::List(formatters)) => formatters.as_ref(),
-            (FormatTrigger::Save, FormatOnSave::On) | (FormatTrigger::Manual, _) => {
+            (FormatTrigger::Manual, _) | (FormatTrigger::Save, FormatOnSave::On) => {
                 match &settings.formatter {
                     SelectedFormatter::Auto => {
                         if settings.prettier.allowed {
