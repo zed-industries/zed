0257b124ee433a4dfac2194a2960e523ca7d9137
Jason Lee <huacnlee@gmail.com>
2025-08-13 17:59:06 +0800

    Add new content_mask example, revert hello_world example

--- crates/gpui/examples/hello_world.rs
+++ crates/gpui/examples/hello_world.rs
@@ -10,78 +10,19 @@ struct HelloWorld {
 impl Render for HelloWorld {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
         div()
-            .font_family(".SystemUIFont")
             .flex()
             .flex_col()
-            .size_full()
             .gap_3()
             .bg(rgb(0x505050))
+            .size(px(500.0))
             .justify_center()
             .items_center()
             .shadow_lg()
+            .border_1()
+            .border_color(rgb(0x0000ff))
             .text_xl()
             .text_color(rgb(0xffffff))
             .child(format!("Hello, {}!", &self.text))
-            .child(
-                div()
-                    .overflow_hidden()
-                    .rounded(px(24.))
-                    .border(px(4.))
-                    .border_color(gpui::white())
-                    .text_color(gpui::white())
-                    .text_center()
-                    .child(
-                        div()
-                            .bg(gpui::black())
-                            .py_2()
-                            .px_7()
-                            .border_l_2()
-                            .border_r_2()
-                            .border_b_3()
-                            .border_color(gpui::red())
-                            .child("Let build applications with GPUI"),
-                    )
-                    .child(
-                        div()
-                            .bg(rgb(0x222222))
-                            .text_sm()
-                            .py_1()
-                            .px_7()
-                            .border_l_3()
-                            .border_r_3()
-                            .border_color(gpui::green())
-                            .child("The fast, productive UI framework for Rust"),
-                    )
-                    .child(
-                        div()
-                            .bg(rgb(0x222222))
-                            .w_full()
-                            .flex()
-                            .flex_row()
-                            .text_sm()
-                            .text_color(rgb(0xc0c0c0))
-                            .child(
-                                div()
-                                    .flex_1()
-                                    .p_2()
-                                    .border_3()
-                                    .border_dashed()
-                                    .border_color(gpui::blue())
-                                    .child("Rust"),
-                            )
-                            .child(
-                                div()
-                                    .flex_1()
-                                    .p_2()
-                                    .border_t_3()
-                                    .border_r_3()
-                                    .border_b_3()
-                                    .border_dashed()
-                                    .border_color(gpui::blue())
-                                    .child("GPU Rendering"),
-                            ),
-                    ),
-            )
             .child(
                 div()
                     .flex()
