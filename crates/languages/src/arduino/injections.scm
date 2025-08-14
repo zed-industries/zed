f5ca82b3a02abf02cfed895b8b8e86f2c01d0639
P0SlX <florian.savoure@gmail.com>
2025-08-14 09:55:16 +0200

    Add Arduino language support (.ino files)
    Add dedicated Arduino language configuration to fix clangd AST errors
    when opening .ino files. Uses C++ grammar but operates as separate
    language to avoid LSP conflicts.
    
    - Add Arduino language with .ino file extension support
    - Remove .ino from C++ language path_suffixes to prevent conflicts

---
+++ crates/languages/src/arduino/injections.scm
@@ -0,0 +1,11 @@
+(preproc_def
+    value: (preproc_arg) @injection.content
+    (#set! injection.language "c++"))
+
+(preproc_function_def
+    value: (preproc_arg) @injection.content
+    (#set! injection.language "c++"))
+
+(raw_string_literal
+  delimiter: (raw_string_delimiter) @injection.language
+  (raw_string_content) @injection.content)
