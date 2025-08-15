7d6d6c0d95fdea1dde5de6668a221c8966330bde
Lukas Wirth <lukas@zed.dev>
2025-08-15 09:56:47 +0200

    search: Fix some inconsistencies between project and buffer search bars (#36103)
    - project search query string now turns red when no results are found
    matching buffer search behavior
    - General code deduplication as well as more consistent layout between
    the two bars, as some minor details have drifted apart
    - Tab cycling in buffer search now ends up in editor focus when cycling
    backwards, matching forward cycling
    - Report parse errors in filter include and exclude editors
    
    Release Notes:
    
    - N/A

--- crates/search/src/search_bar.rs
+++ crates/search/src/search_bar.rs
@@ -1,8 +1,14 @@
-use gpui::{Action, FocusHandle, IntoElement};
+use editor::{Editor, EditorElement, EditorStyle};
+use gpui::{Action, Entity, FocusHandle, Hsla, IntoElement, TextStyle};
+use settings::Settings;
+use theme::ThemeSettings;
 use ui::{IconButton, IconButtonShape};
 use ui::{Tooltip, prelude::*};
 
-pub(super) fn render_nav_button(
+use crate::ToggleReplace;
+
+pub(super) fn render_action_button(
+    id_prefix: &'static str,
     icon: ui::IconName,
     active: bool,
     tooltip: &'static str,
@@ -10,7 +16,7 @@ pub(super) fn render_nav_button(
     focus_handle: FocusHandle,
 ) -> impl IntoElement {
     IconButton::new(
-        SharedString::from(format!("search-nav-button-{}", action.name())),
+        SharedString::from(format!("{id_prefix}-{}", action.name())),
         icon,
     )
     .shape(IconButtonShape::Square)
@@ -25,4 +31,75 @@ pub(super) fn render_nav_button(
     })
     .tooltip(move |window, cx| Tooltip::for_action_in(tooltip, action, &focus_handle, window, cx))
     .disabled(!active)
+}
+
+pub(crate) fn input_base_styles(border_color: Hsla, map: impl FnOnce(Div) -> Div) -> Div {
+    h_flex()
+        .min_w_32()
+        .map(map)
+        .h_8()
+        .pl_2()
+        .pr_1()
+        .py_1()
+        .border_1()
+        .border_color(border_color)
+        .rounded_lg()
+}
+
+pub(crate) fn toggle_replace_button(
+    id: &'static str,
+    focus_handle: FocusHandle,
+    replace_enabled: bool,
+    on_click: impl Fn(&gpui::ClickEvent, &mut Window, &mut App) + 'static,
+) -> IconButton {
+    IconButton::new(id, IconName::Replace)
+        .shape(IconButtonShape::Square)
+        .style(ButtonStyle::Subtle)
+        .when(replace_enabled, |button| button.style(ButtonStyle::Filled))
+        .on_click(on_click)
+        .toggle_state(replace_enabled)
+        .tooltip({
+            move |window, cx| {
+                Tooltip::for_action_in("Toggle Replace", &ToggleReplace, &focus_handle, window, cx)
+            }
+        })
+}
+
+pub(crate) fn render_text_input(
+    editor: &Entity<Editor>,
+    color_override: Option<Color>,
+    app: &App,
+) -> impl IntoElement {
+    let (color, use_syntax) = if editor.read(app).read_only(app) {
+        (app.theme().colors().text_disabled, false)
+    } else {
+        match color_override {
+            Some(color_override) => (color_override.color(app), false),
+            None => (app.theme().colors().text, true),
+        }
+    };
+
+    let settings = ThemeSettings::get_global(app);
+    let text_style = TextStyle {
+        color,
+        font_family: settings.buffer_font.family.clone(),
+        font_features: settings.buffer_font.features.clone(),
+        font_fallbacks: settings.buffer_font.fallbacks.clone(),
+        font_size: rems(0.875).into(),
+        font_weight: settings.buffer_font.weight,
+        line_height: relative(1.3),
+        ..TextStyle::default()
+    };
+
+    let mut editor_style = EditorStyle {
+        background: app.theme().colors().toolbar_background,
+        local_player: app.theme().players().local(),
+        text: text_style,
+        ..EditorStyle::default()
+    };
+    if use_syntax {
+        editor_style.syntax = app.theme().syntax().clone();
+    }
+
+    EditorElement::new(editor, editor_style)
 }
