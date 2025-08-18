6f73e39ed0e6577c46b78455dc0fb698a57f1bd5
Kate <work@localcc.cc>
2025-08-18 23:12:08 +0200

    fix clippy

--- crates/gpui/src/platform/windows/events.rs
+++ crates/gpui/src/platform/windows/events.rs
@@ -100,7 +100,7 @@ impl WindowsWindowInner {
             WM_SETCURSOR => self.handle_set_cursor(handle, lparam),
             WM_SETTINGCHANGE => self.handle_system_settings_changed(handle, wparam, lparam),
             WM_INPUTLANGCHANGE => self.handle_input_language_changed(lparam),
-            WM_SHOWWINDOW => self.handle_window_visibility_changed(handle, wparam, lparam),
+            WM_SHOWWINDOW => self.handle_window_visibility_changed(handle, wparam),
             WM_GPUI_CURSOR_STYLE_CHANGED => self.handle_cursor_changed(lparam),
             WM_GPUI_FORCE_UPDATE_WINDOW => self.draw_window(handle, true),
             _ => None,
@@ -1161,7 +1161,7 @@ impl WindowsWindowInner {
         Some(0)
     }
 
-    fn handle_window_visibility_changed(&self, handle: HWND, wparam: WPARAM, lparam: LPARAM) -> Option<isize> {
+    fn handle_window_visibility_changed(&self, handle: HWND, wparam: WPARAM) -> Option<isize> {
         if wparam.0 == 1 {
             self.draw_window(handle, false);
         }
