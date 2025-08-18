5615a8d5afcb7a442bb32759ea53edb6be62f429
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-08-18 23:29:27 +0200

    And both - again

--- crates/fs/src/fs.rs
+++ crates/fs/src/fs.rs
@@ -776,7 +776,7 @@ impl Fs for RealFs {
         }
 
         // Check if path is a symlink and follow the target parent
-        if let Some(mut target) = self.read_link(&path).await.ok() {
+        if let Some(mut target) = self.read_link(path).await.ok() {
             // Check if symlink target is relative path, if so make it absolute
             if target.is_relative() {
                 if let Some(parent) = path.parent() {
