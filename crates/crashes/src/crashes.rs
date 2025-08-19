9c92267a9b61c4bc76229fa2d17ef2d0456f9562
Julia Ryan <juliaryan3.14@gmail.com>
2025-08-19 14:05:20 -0700

    Attach minidump errors to uploaded crash events
    Co-authored-by: Max Brunsfeld <maxbrunsfeld@gmail.com>

--- crates/crashes/src/crashes.rs
+++ crates/crashes/src/crashes.rs
@@ -108,6 +108,7 @@ pub struct CrashServer {
 pub struct CrashInfo {
     pub init: InitCrashHandler,
     pub panic: Option<CrashPanic>,
+    pub minidump_error: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
@@ -142,16 +143,14 @@ impl minidumper::ServerHandler for CrashServer {
     }
 
     fn on_minidump_created(&self, result: Result<MinidumpBinary, minidumper::Error>) -> LoopAction {
-        match result {
+        let minidump_error = match result {
             Ok(mut md_bin) => {
                 use io::Write;
                 let _ = md_bin.file.flush();
-                info!("wrote minidump to disk {:?}", md_bin.path);
+                None
             }
-            Err(e) => {
-                info!("failed to write minidump: {:#}", e);
-            }
-        }
+            Err(e) => Some(format!("{e:?}")),
+        };
 
         let crash_info = CrashInfo {
             init: self
@@ -160,6 +159,7 @@ impl minidumper::ServerHandler for CrashServer {
                 .expect("not initialized")
                 .clone(),
             panic: self.panic_info.get().cloned(),
+            minidump_error,
         };
 
         let crash_data_path = paths::logs_dir()
