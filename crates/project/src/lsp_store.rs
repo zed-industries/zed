ae6b79a398bcb62077e358d2171d16eb8584c4a2
Smit Barmase <heysmitbarmase@gmail.com>
2025-08-25 17:58:50 +0530

    remove redudant method

--- crates/project/src/lsp_store.rs
+++ crates/project/src/lsp_store.rs
@@ -12140,20 +12140,6 @@ impl LspStore {
         Ok(())
     }
 
-    fn take_text_document_sync_options(
-        capabilities: &mut lsp::ServerCapabilities,
-    ) -> lsp::TextDocumentSyncOptions {
-        match capabilities.text_document_sync.take() {
-            Some(lsp::TextDocumentSyncCapability::Options(sync_options)) => sync_options,
-            Some(lsp::TextDocumentSyncCapability::Kind(sync_kind)) => {
-                let mut sync_options = lsp::TextDocumentSyncOptions::default();
-                sync_options.change = Some(sync_kind);
-                sync_options
-            }
-            None => lsp::TextDocumentSyncOptions::default(),
-        }
-    }
-
     #[cfg(any(test, feature = "test-support"))]
     pub fn forget_code_lens_task(&mut self, buffer_id: BufferId) -> Option<CodeLensTask> {
         let data = self.lsp_code_lens.get_mut(&buffer_id)?;
