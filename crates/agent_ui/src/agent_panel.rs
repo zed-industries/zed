628a9cd8eab0c41aee0011bfab1462c7bc54adf5
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2025-08-25 17:34:55 -0300

    thread view: Add link to docs in the toolbar plus menu (#36883)
    Release Notes:
    
    - N/A

--- crates/agent_ui/src/agent_panel.rs
+++ crates/agent_ui/src/agent_panel.rs
@@ -9,6 +9,7 @@ use agent_servers::AgentServerSettings;
 use agent2::{DbThreadMetadata, HistoryEntry};
 use db::kvp::{Dismissable, KEY_VALUE_STORE};
 use serde::{Deserialize, Serialize};
+use zed_actions::OpenBrowser;
 use zed_actions::agent::ReauthenticateAgent;
 
 use crate::acp::{AcpThreadHistory, ThreadHistoryEvent};
@@ -2689,6 +2690,15 @@ impl AgentPanel {
                                 }
 
                                 menu
+                            })
+                            .when(cx.has_flag::<GeminiAndNativeFeatureFlag>(), |menu| {
+                                menu.separator().link(
+                                    "Add Your Own Agent",
+                                    OpenBrowser {
+                                        url: "https://agentclientprotocol.com/".into(),
+                                    }
+                                    .boxed_clone(),
+                                )
                             });
                         menu
                     }))
