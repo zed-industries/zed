8cd36add6f11109eccf938e32d6a7ca50a01f44a
MrSubidubi <finn@zed.dev>
2025-09-02 16:03:13 +0200

    Update comment

--- crates/ui/src/traits/animation_ext.rs
+++ crates/ui/src/traits/animation_ext.rs
@@ -7,6 +7,11 @@ use crate::{prelude::*, traits::transformable::Transformable};
 /// An extension trait for adding common animations to animatable components.
 pub trait CommonAnimationExt: AnimationExt {
     /// Render this component as rotating over the given duration.
+    ///
+    /// NOTE: This method uses the location of the caller to generate an ID for this state.
+    ///       If this is not sufficient to identify your state (e.g. you're rendering a list item),
+    ///       you can provide a custom ElementID using the `use_keyed_rotate_animation` method.
+
     #[track_caller]
     fn with_rotate_animation(self, duration: u64) -> AnimationElement<Self>
     where
