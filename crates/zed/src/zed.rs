f9976eeae6d2ebaf76b31b8c44e0d3645e5da4e9
Matin Aniss <76515905+MatinAniss@users.noreply.github.com>
2025-09-03 18:11:07 +1000

    Fix action namespace

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -4480,7 +4480,7 @@ mod tests {
                 "keymap_editor",
                 "keystroke_input",
                 "language_selector",
-                "line_ending_selector",
+                "line_ending",
                 "lsp_tool",
                 "markdown",
                 "menu",
