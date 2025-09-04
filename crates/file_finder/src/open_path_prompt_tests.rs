ec8c4d3fbb1a06466579569944d731930487c5aa
Kirill Bulatov <kirill@zed.dev>
2025-09-04 22:06:43 +0300

    Fix tests on Windows

--- crates/file_finder/src/open_path_prompt_tests.rs
+++ crates/file_finder/src/open_path_prompt_tests.rs
@@ -43,12 +43,17 @@ async fn test_open_path_prompt(cx: &mut TestAppContext) {
     insert_query(query, &picker, cx).await;
     assert_eq!(collect_match_candidates(&picker, cx), vec!["root"]);
 
+    #[cfg(not(windows))]
+    let expected_separator = "./";
+    #[cfg(windows)]
+    let expected_separator = ".\\";
+
     // If the query ends with a slash, the picker should show the contents of the directory.
     let query = path!("/root/");
     insert_query(query, &picker, cx).await;
     assert_eq!(
         collect_match_candidates(&picker, cx),
-        vec!["./", "a1", "a2", "a3", "dir1", "dir2"]
+        vec![expected_separator, "a1", "a2", "a3", "dir1", "dir2"]
     );
 
     // Show candidates for the query "a".
@@ -72,7 +77,7 @@ async fn test_open_path_prompt(cx: &mut TestAppContext) {
     insert_query(query, &picker, cx).await;
     assert_eq!(
         collect_match_candidates(&picker, cx),
-        vec!["./", "c", "d1", "d2", "d3", "dir3", "dir4"]
+        vec![expected_separator, "c", "d1", "d2", "d3", "dir3", "dir4"]
     );
 
     // Show candidates for the query "d".
@@ -312,7 +317,7 @@ async fn test_open_path_prompt_on_windows_with_remote(cx: &mut TestAppContext) {
     insert_query(query, &picker, cx).await;
     assert_eq!(
         collect_match_candidates(&picker, cx),
-        vec![".\\", "a", "dir1", "dir2"]
+        vec!["./", "a", "dir1", "dir2"]
     );
     assert_eq!(
         confirm_completion(query, 1, &picker, cx).unwrap(),
