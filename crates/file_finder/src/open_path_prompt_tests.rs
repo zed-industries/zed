c4d076cf4d469f93531a4e604d26e464e5d525b6
Kirill Bulatov <kirill@zed.dev>
2025-09-04 19:24:01 +0300

    Fix the test
    Co-authored-by: David Kleingeld <davidsk@zed.dev>

--- crates/file_finder/src/open_path_prompt_tests.rs
+++ crates/file_finder/src/open_path_prompt_tests.rs
@@ -56,7 +56,7 @@ async fn test_open_path_prompt(cx: &mut TestAppContext) {
     insert_query(query, &picker, cx).await;
     assert_eq!(
         collect_match_candidates(&picker, cx),
-        vec!["./", "a1", "a2", "a3"]
+        vec!["a1", "a2", "a3"]
     );
 
     // Show candidates for the query "d".
@@ -72,7 +72,7 @@ async fn test_open_path_prompt(cx: &mut TestAppContext) {
     insert_query(query, &picker, cx).await;
     assert_eq!(
         collect_match_candidates(&picker, cx),
-        vec!["c", "d1", "d2", "d3", "dir3", "dir4"]
+        vec!["./", "c", "d1", "d2", "d3", "dir3", "dir4"]
     );
 
     // Show candidates for the query "d".
