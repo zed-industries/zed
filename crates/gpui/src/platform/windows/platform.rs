866cac4b04c34779564602a032b663beaa427f6c
Junkui Zhang <364772080@qq.com>
2025-09-04 22:27:09 +0800

    use `let _ = ...` instead

--- crates/gpui/src/platform/windows/platform.rs
+++ crates/gpui/src/platform/windows/platform.rs
@@ -257,7 +257,7 @@ impl WindowsPlatform {
                 };
                 for hwnd in all_windows.read().iter() {
                     unsafe {
-                        RedrawWindow(Some(hwnd.as_raw()), None, None, RDW_INVALIDATE).as_bool();
+                        let _ = RedrawWindow(Some(hwnd.as_raw()), None, None, RDW_INVALIDATE);
                     }
                 }
             }
