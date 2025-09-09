9f54c0f504a9da197843b0b49028d8f7f2cbc7db
Antonio Scandurra <me@as-cii.com>
2025-09-09 20:10:03 +0200

    Make it possible to support GPUI in the scheduler crate (#37849)
    Added features that weren't needed in our cloud code.
    
    Release Notes:
    
    - N/A
    
    ---------
    
    Co-authored-by: Nathan <nathan@zed.dev>

--- crates/scheduler/src/clock.rs
+++ crates/scheduler/src/clock.rs
@@ -1,34 +1,42 @@
-use chrono::{DateTime, Duration, Utc};
+use chrono::{DateTime, Utc};
 use parking_lot::Mutex;
+use std::time::{Duration, Instant};
 
 pub trait Clock {
-    fn now(&self) -> DateTime<Utc>;
+    fn utc_now(&self) -> DateTime<Utc>;
+    fn now(&self) -> Instant;
 }
 
-pub struct TestClock {
-    now: Mutex<DateTime<Utc>>,
+pub struct TestClock(Mutex<TestClockState>);
+
+struct TestClockState {
+    now: Instant,
+    utc_now: DateTime<Utc>,
 }
 
 impl TestClock {
     pub fn new() -> Self {
         const START_TIME: &str = "2025-07-01T23:59:58-00:00";
-        let now = DateTime::parse_from_rfc3339(START_TIME).unwrap().to_utc();
-        Self {
-            now: Mutex::new(now),
-        }
-    }
-
-    pub fn set_now(&self, now: DateTime<Utc>) {
-        *self.now.lock() = now;
+        let utc_now = DateTime::parse_from_rfc3339(START_TIME).unwrap().to_utc();
+        Self(Mutex::new(TestClockState {
+            now: Instant::now(),
+            utc_now,
+        }))
     }
 
     pub fn advance(&self, duration: Duration) {
-        *self.now.lock() += duration;
+        let mut state = self.0.lock();
+        state.now += duration;
+        state.utc_now += duration;
     }
 }
 
 impl Clock for TestClock {
-    fn now(&self) -> DateTime<Utc> {
-        *self.now.lock()
+    fn utc_now(&self) -> DateTime<Utc> {
+        self.0.lock().utc_now
+    }
+
+    fn now(&self) -> Instant {
+        self.0.lock().now
     }
 }
