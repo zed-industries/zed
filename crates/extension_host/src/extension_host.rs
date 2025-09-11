c4d75ea6d5887aee82e67888a1ac21e3bf9ccbdb
Max Brunsfeld <maxbrunsfeld@gmail.com>
2025-09-11 13:56:06 -0700

    Windows: Fix issues with paths in extensions (#37811)
    ### Background
    
    Zed extensions use WASI to access the file-system. They only have
    read-write access to one specific folder called their work dir. But
    extensions do need to be able to *refer* to other arbitrary files on the
    user's machine. For instance, extensions need to be able to look up
    existing binaries on the user's `PATH`, and request that Zed invoke them
    as language servers. Similarly, extensions can create paths to files in
    the user's project, and use them as arguments in commands that Zed
    should run. For these reasons, we pass *real* paths back and forth
    between the host and extensions; we don't try to abstract over the
    file-system with some virtualization scheme.
    
    On Windows, this results in a bit of mismatch, because `wasi-libc` uses
    *unix-like* path conventions (and thus, so does the Rust standard
    library when compiling to WASI).
    
    ### Change 1 - Fixing `current_dir`
    
    In order to keep the extension API minimal, extensions use the standard
    library function`env::current_dir()` to query the location of their
    "work" directory. Previously, when initializing extensions, we used the
    `env::set_current_dir` function to set their work directory, but on
    Windows, where absolute paths typically begin with a drive letter, like
    `C:`, the [`wasi-libc` implementation of
    `chdir`](https://github.com/WebAssembly/wasi-libc/blob/d1793637d8afcdc730408e7b6a19a050c3336ce7/libc-bottom-half/sources/chdir.c#L21)
    was prepending an extra forward slash to the path, which caused
    `current_dir()` to return an invalid path.
    
    See https://github.com/bytecodealliance/wasmtime/issues/10415
    
    In this PR, I've switched our extension initialization function to
    *bypass* wasi-libc's `chdir` function, and instead write directly to
    wasi-libc's private, internal state. This is a bit of a hack, but it
    causes the `current_dir()` function to do what we want on Windows
    without any changes to extensions' source code.
    
    ### Change 2 - Working around WASI's relative path handling
    
    Once `current_dir` was fixed (giving us correct absolute paths on
    Windows), @kubkon and I discovered that without the spurious leading `/`
    character, windows absolute paths were no longer accepted by Rust's
    `std::fs` APIs, because they were now recognized as relative paths, and
    were being appended to the working directory.
    
    We first tried to override the `__wasilibc_find_abspath` function in
    `wasi-libc` to make it recognize windows absolute paths as being
    absolute, but that functionality is difficult to override. Eventually
    @kubkon realized that we could prevent WASI-libc's CWD handling from
    being linked into the WASM file by overriding the `chdir` function.
    wasi-libc is designed so that if you don't use their `chdir` function,
    then all paths will be interpreted as relative to `/`. This makes
    absolute paths behave correctly. Then, in order to make *relative* paths
    work again, we simply add a preopen for `.`. Relative paths will match
    that.
    
    ### Next Steps
    
    This is a change to `zed-extension-api`, so we do need to update every
    Zed extension to use the new version, in order for them to work on
    windows.
    
    Release Notes:
    
    - N/A
    
    ---------
    
    Co-authored-by: Jakub Konka <kubkon@jakubkonka.com>

--- crates/extension_host/src/extension_host.rs
+++ crates/extension_host/src/extension_host.rs
@@ -587,6 +587,10 @@ impl ExtensionStore {
     /// This can be used to make certain functionality provided by extensions
     /// available out-of-the-box.
     pub fn auto_install_extensions(&mut self, cx: &mut Context<Self>) {
+        if cfg!(test) {
+            return;
+        }
+
         let extension_settings = ExtensionSettings::get_global(cx);
 
         let extensions_to_install = extension_settings
