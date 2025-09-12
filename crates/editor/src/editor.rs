2ff4fd5c232c9132cad9485bcfd33b7f908b0865
fantacell <ghub@giggo.de>
2025-09-12 23:22:49 +0200

    Fix cargo fmt

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -14400,7 +14400,12 @@ impl Editor {
         }
     }
 
-    pub fn reduce_selections(&mut self, _: &KeepNewestSelection, window: &mut Window, cx: &mut Context<Self>) {
+    pub fn reduce_selections(
+        &mut self,
+        _: &KeepNewestSelection,
+        window: &mut Window,
+        cx: &mut Context<Self>,
+    ) {
         let newest = self.selections.newest::<usize>(cx);
         self.change_selections(Default::default(), window, cx, |s| s.select(vec![newest]));
     }
