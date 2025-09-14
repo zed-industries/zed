f2ea95604a23f19bcacc64df1cd021dc57bab384
Alvaro Parker <aparkerdf@protonmail.com>
2025-09-13 23:40:43 -0300

    WIP

--- crates/git_ui/src/blame_ui.rs
+++ crates/git_ui/src/blame_ui.rs
@@ -99,25 +99,10 @@ impl BlameRenderer for GitBlameRenderer {
                             let workspace = workspace.clone();
                             move |_, window, cx| {
                                 CommitView::open(
-                                    CommitSummary {
-                                        sha: blame_entry.sha.to_string().into(),
-                                        subject: blame_entry
-                                            .summary
-                                            .clone()
-                                            .unwrap_or_default()
-                                            .into(),
-                                        commit_timestamp: blame_entry
-                                            .committer_time
-                                            .unwrap_or_default(),
-                                        author_name: blame_entry
-                                            .committer_name
-                                            .clone()
-                                            .unwrap_or_default()
-                                            .into(),
-                                        has_parent: true,
-                                    },
+                                    blame_entry.sha.to_string(),
                                     repository.downgrade(),
                                     workspace.clone(),
+                                    false,
                                     window,
                                     cx,
                                 )
@@ -326,9 +311,10 @@ impl BlameRenderer for GitBlameRenderer {
                                                 .icon_position(IconPosition::Start)
                                                 .on_click(move |_, window, cx| {
                                                     CommitView::open(
-                                                        commit_summary.clone(),
+                                                        commit_summary.sha.clone(),
                                                         repository.downgrade(),
                                                         workspace.clone(),
+                                                        false,
                                                         window,
                                                         cx,
                                                     );
@@ -366,15 +352,10 @@ impl BlameRenderer for GitBlameRenderer {
         cx: &mut App,
     ) {
         CommitView::open(
-            CommitSummary {
-                sha: blame_entry.sha.to_string().into(),
-                subject: blame_entry.summary.clone().unwrap_or_default().into(),
-                commit_timestamp: blame_entry.committer_time.unwrap_or_default(),
-                author_name: blame_entry.committer_name.unwrap_or_default().into(),
-                has_parent: true,
-            },
+            blame_entry.sha.to_string(),
             repository.downgrade(),
             workspace,
+            false,
             window,
             cx,
         )
