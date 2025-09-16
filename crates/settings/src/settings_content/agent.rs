37000a261ed878574ba377f970a035696eb89d4d
Conrad Irwin <conrad.irwin@gmail.com>
2025-09-16 09:34:34 -0600

    Agent settings
    Co-authored-by: Ben Kunkle <ben@zed.dev>

---
+++ crates/settings/src/settings_content/agent.rs
@@ -0,0 +1,264 @@
+use collections::IndexMap;
+use gpui::SharedString;
+use schemars::{JsonSchema, json_schema};
+use serde::{Deserialize, Serialize};
+use std::{borrow::Cow, sync::Arc};
+
+#[derive(Clone, Serialize, Deserialize, JsonSchema, Debug, Default)]
+pub struct AgentSettingsContent {
+    /// Whether the Agent is enabled.
+    ///
+    /// Default: true
+    pub enabled: Option<bool>,
+    /// Whether to show the agent panel button in the status bar.
+    ///
+    /// Default: true
+    pub button: Option<bool>,
+    /// Where to dock the agent panel.
+    ///
+    /// Default: right
+    pub dock: Option<AgentDockPosition>,
+    /// Default width in pixels when the agent panel is docked to the left or right.
+    ///
+    /// Default: 640
+    pub default_width: Option<f32>,
+    /// Default height in pixels when the agent panel is docked to the bottom.
+    ///
+    /// Default: 320
+    pub default_height: Option<f32>,
+    /// The default model to use when creating new chats and for other features when a specific model is not specified.
+    pub default_model: Option<LanguageModelSelection>,
+    /// Model to use for the inline assistant. Defaults to default_model when not specified.
+    pub inline_assistant_model: Option<LanguageModelSelection>,
+    /// Model to use for generating git commit messages. Defaults to default_model when not specified.
+    pub commit_message_model: Option<LanguageModelSelection>,
+    /// Model to use for generating thread summaries. Defaults to default_model when not specified.
+    pub thread_summary_model: Option<LanguageModelSelection>,
+    /// Additional models with which to generate alternatives when performing inline assists.
+    pub inline_alternatives: Option<Vec<LanguageModelSelection>>,
+    /// The default profile to use in the Agent.
+    ///
+    /// Default: write
+    pub default_profile: Option<Arc<str>>,
+    /// Which view type to show by default in the agent panel.
+    ///
+    /// Default: "thread"
+    pub default_view: Option<DefaultAgentView>,
+    /// The available agent profiles.
+    pub profiles: Option<IndexMap<Arc<str>, AgentProfileContent>>,
+    /// Whenever a tool action would normally wait for your confirmation
+    /// that you allow it, always choose to allow it.
+    ///
+    /// This setting has no effect on external agents that support permission modes, such as Claude Code.
+    ///
+    /// Set `agent_servers.claude.default_mode` to `bypassPermissions`, to disable all permission requests when using Claude Code.
+    ///
+    /// Default: false
+    pub always_allow_tool_actions: Option<bool>,
+    /// Where to show a popup notification when the agent is waiting for user input.
+    ///
+    /// Default: "primary_screen"
+    pub notify_when_agent_waiting: Option<NotifyWhenAgentWaiting>,
+    /// Whether to play a sound when the agent has either completed its response, or needs user input.
+    ///
+    /// Default: false
+    pub play_sound_when_agent_done: Option<bool>,
+    /// Whether to stream edits from the agent as they are received.
+    ///
+    /// Default: false
+    pub stream_edits: Option<bool>,
+    /// Whether to display agent edits in single-file editors in addition to the review multibuffer pane.
+    ///
+    /// Default: true
+    pub single_file_review: Option<bool>,
+    /// Additional parameters for language model requests. When making a request
+    /// to a model, parameters will be taken from the last entry in this list
+    /// that matches the model's provider and name. In each entry, both provider
+    /// and model are optional, so that you can specify parameters for either
+    /// one.
+    ///
+    /// Default: []
+    #[serde(default)]
+    pub model_parameters: Vec<LanguageModelParameters>,
+    /// What completion mode to enable for new threads
+    ///
+    /// Default: normal
+    pub preferred_completion_mode: Option<CompletionMode>,
+    /// Whether to show thumb buttons for feedback in the agent panel.
+    ///
+    /// Default: true
+    pub enable_feedback: Option<bool>,
+    /// Whether to have edit cards in the agent panel expanded, showing a preview of the full diff.
+    ///
+    /// Default: true
+    pub expand_edit_card: Option<bool>,
+    /// Whether to have terminal cards in the agent panel expanded, showing the whole command output.
+    ///
+    /// Default: true
+    pub expand_terminal_card: Option<bool>,
+    /// Whether to always use cmd-enter (or ctrl-enter on Linux or Windows) to send messages in the agent panel.
+    ///
+    /// Default: false
+    pub use_modifier_to_send: Option<bool>,
+}
+
+impl AgentSettingsContent {
+    pub fn set_dock(&mut self, dock: AgentDockPosition) {
+        self.dock = Some(dock);
+    }
+
+    pub fn set_model(&mut self, language_model: LanguageModelSelection) {
+        // let model = language_model.id().0.to_string();
+        // let provider = language_model.provider_id().0.to_string();
+        // self.default_model = Some(LanguageModelSelection {
+        //     provider: provider.into(),
+        //     model,
+        // });
+        self.default_model = Some(language_model)
+    }
+
+    pub fn set_inline_assistant_model(&mut self, provider: String, model: String) {
+        self.inline_assistant_model = Some(LanguageModelSelection {
+            provider: provider.into(),
+            model,
+        });
+    }
+
+    pub fn set_commit_message_model(&mut self, provider: String, model: String) {
+        self.commit_message_model = Some(LanguageModelSelection {
+            provider: provider.into(),
+            model,
+        });
+    }
+
+    pub fn set_thread_summary_model(&mut self, provider: String, model: String) {
+        self.thread_summary_model = Some(LanguageModelSelection {
+            provider: provider.into(),
+            model,
+        });
+    }
+
+    pub fn set_always_allow_tool_actions(&mut self, allow: bool) {
+        self.always_allow_tool_actions = Some(allow);
+    }
+
+    pub fn set_play_sound_when_agent_done(&mut self, allow: bool) {
+        self.play_sound_when_agent_done = Some(allow);
+    }
+
+    pub fn set_single_file_review(&mut self, allow: bool) {
+        self.single_file_review = Some(allow);
+    }
+
+    pub fn set_use_modifier_to_send(&mut self, always_use: bool) {
+        self.use_modifier_to_send = Some(always_use);
+    }
+
+    pub fn set_profile(&mut self, profile_id: Arc<str>) {
+        self.default_profile = Some(profile_id);
+    }
+}
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize, JsonSchema)]
+pub struct AgentProfileContent {
+    pub name: Arc<str>,
+    #[serde(default)]
+    pub tools: IndexMap<Arc<str>, bool>,
+    /// Whether all context servers are enabled by default.
+    pub enable_all_context_servers: Option<bool>,
+    #[serde(default)]
+    pub context_servers: IndexMap<Arc<str>, ContextServerPresetContent>,
+}
+
+#[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize, JsonSchema)]
+pub struct ContextServerPresetContent {
+    pub tools: IndexMap<Arc<str>, bool>,
+}
+
+#[derive(Copy, Clone, Default, Debug, Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum AgentDockPosition {
+    Left,
+    #[default]
+    Right,
+    Bottom,
+}
+
+#[derive(Copy, Clone, Default, Debug, Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum DefaultAgentView {
+    #[default]
+    Thread,
+    TextThread,
+}
+
+#[derive(Copy, Clone, Default, Debug, Serialize, Deserialize, JsonSchema, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub enum NotifyWhenAgentWaiting {
+    #[default]
+    PrimaryScreen,
+    AllScreens,
+    Never,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, JsonSchema, PartialEq)]
+pub struct LanguageModelSelection {
+    pub provider: LanguageModelProviderSetting,
+    pub model: String,
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, JsonSchema, PartialEq, Default)]
+#[serde(rename_all = "snake_case")]
+pub enum CompletionMode {
+    #[default]
+    Normal,
+    #[serde(alias = "max")]
+    Burn,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, JsonSchema, PartialEq)]
+pub struct LanguageModelParameters {
+    pub provider: Option<LanguageModelProviderSetting>,
+    pub model: Option<SharedString>,
+    pub temperature: Option<f32>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+pub struct LanguageModelProviderSetting(pub String);
+
+impl JsonSchema for LanguageModelProviderSetting {
+    fn schema_name() -> Cow<'static, str> {
+        "LanguageModelProviderSetting".into()
+    }
+
+    fn json_schema(_: &mut schemars::SchemaGenerator) -> schemars::Schema {
+        json_schema!({
+            "enum": [
+                "amazon-bedrock",
+                "anthropic",
+                "copilot_chat",
+                "deepseek",
+                "google",
+                "lmstudio",
+                "mistral",
+                "ollama",
+                "openai",
+                "openrouter",
+                "vercel",
+                "x_ai",
+                "zed.dev"
+            ]
+        })
+    }
+}
+
+impl From<String> for LanguageModelProviderSetting {
+    fn from(provider: String) -> Self {
+        Self(provider)
+    }
+}
+
+impl From<&str> for LanguageModelProviderSetting {
+    fn from(provider: &str) -> Self {
+        Self(provider.to_string())
+    }
+}
