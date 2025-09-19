ae7ae96d2e00d4d8ad4df134e59253b0eede5596
Jakub Konka <kubkon@jakubkonka.com>
2025-09-19 11:52:59 +0200

    tracy: Hook up tracing to tracy for automatic spans

--- crates/zed/src/zed-main.rs
+++ crates/zed/src/zed-main.rs
@@ -4,8 +4,14 @@
 pub fn main() {
     #[cfg(feature = "tracy")]
     {
+        use tracing_subscriber::layer::SubscriberExt;
+
         tracy_client::register_demangler!();
         tracy_client::Client::start();
+        tracing::subscriber::set_global_default(
+            tracing_subscriber::registry().with(tracing_tracy::TracyLayer::default()),
+        )
+        .expect("setup tracy layer");
     }
 
     // separated out so that the file containing the main function can be imported by other crates,
