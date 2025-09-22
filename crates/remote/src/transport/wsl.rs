9e86931924d75215b4198ca93d4c5f257ab4e071
Kate <work@localcc.cc>
2025-09-22 16:19:04 +0200

    Revert "remote: Remove excess quoting in WSL `build_command` (#38380)"
    This reverts commit 202dcb122f719036590f05430b2bac73cdfa5b07 as that
    broke opening the terminal on wsl remotes.

--- crates/remote/src/transport/wsl.rs
+++ crates/remote/src/transport/wsl.rs
@@ -409,7 +409,7 @@ impl RemoteConnection for WslRemoteConnection {
                 "--".to_string(),
                 self.shell.clone(),
                 "-c".to_string(),
-                script,
+                shlex::try_quote(&script)?.to_string(),
             ]
         } else {
             vec![
@@ -420,7 +420,7 @@ impl RemoteConnection for WslRemoteConnection {
                 "--".to_string(),
                 self.shell.clone(),
                 "-c".to_string(),
-                script,
+                shlex::try_quote(&script)?.to_string(),
             ]
         };
 
