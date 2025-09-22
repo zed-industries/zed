dbdad6be6722eabe67bead5ff26f20fa1f3b2e77
Anthony <anthony@zed.dev>
2025-09-22 15:54:44 -0400

    Planning

--- crates/settings_ui/src/settings_ui.rs
+++ crates/settings_ui/src/settings_ui.rs
@@ -1,18 +1,22 @@
 //! # settings_ui
-use std::fmt::Display;
+use std::{fmt::Display, sync::Arc};
 
-use feature_flags::{FeatureFlag, FeatureFlagAppExt as _};
+use feature_flags::FeatureFlag;
 use gpui::{
-    App, AppContext as _, Context, Div, IntoElement, ReadGlobal as _, Render, UpdateGlobal as _,
-    Window, WindowHandle, actions, div, px, size,
+    App, AppContext as _, Context, IntoElement, ReadGlobal, Render, Window, WindowHandle, actions,
+    div, px, size,
 };
-use settings::{SettingsContent, SettingsStore};
-use ui::{
-    ActiveTheme as _, Color, FluentBuilder as _, InteractiveElement as _, Label, LabelCommon as _,
-    LabelSize, ParentElement, StatefulInteractiveElement as _, Styled as _, SwitchField,
-    ToggleButton, v_flex,
+use settings::{
+    CargoWorkspaceCommandSettings, SettingsContent, SettingsStore, SlashCommandSettings,
 };
-use workspace::Workspace;
+use ui::{BorrowAppContext, SwitchField};
+// use settings::{SettingsContent, SettingsStore};
+// use ui::{
+//     ActiveTheme as _, Color, FluentBuilder as _, InteractiveElement as _, Label, LabelCommon as _,
+//     LabelSize, ParentElement, StatefulInteractiveElement as _, Styled as _, SwitchField,
+//     ToggleButton, v_flex,
+// };
+// use workspace::Workspace;
 
 pub struct SettingsUiFeatureFlag;
 
@@ -28,65 +32,7 @@ actions!(
     ]
 );
 
-#[derive(PartialEq, Eq, Copy, Clone)]
-enum SettingsPage {
-    General,
-    AppearanceAndBehavior,
-    Editor,
-    WorkbenchAndWindow,
-    PanelsAndTools,
-    AI,
-    LanguageAndFrameworks,
-    LanguageServer,
-    VersionControl,
-    Extensions,
-    SystemAndNetwork,
-    Collaboration,
-}
-
-static PAGES: &'static [SettingsPage] = &[
-    SettingsPage::General,
-    SettingsPage::AppearanceAndBehavior,
-    SettingsPage::Editor,
-    SettingsPage::WorkbenchAndWindow,
-    SettingsPage::PanelsAndTools,
-    SettingsPage::AI,
-    SettingsPage::LanguageAndFrameworks,
-    SettingsPage::LanguageServer,
-    SettingsPage::VersionControl,
-    SettingsPage::Extensions,
-    SettingsPage::SystemAndNetwork,
-    SettingsPage::Collaboration,
-];
-
-impl SettingsPage {
-    fn title(&self) -> &'static str {
-        match self {
-            SettingsPage::General => "General",
-            SettingsPage::AppearanceAndBehavior => "Appearance & Behavior",
-            SettingsPage::Editor => "Editor",
-            SettingsPage::WorkbenchAndWindow => "Workbench & Window",
-            SettingsPage::PanelsAndTools => "Panels & Tools",
-            SettingsPage::AI => "AI",
-            SettingsPage::LanguageAndFrameworks => "Language & Frameworks",
-            SettingsPage::LanguageServer => "Language Server",
-            SettingsPage::VersionControl => "Version Control",
-            SettingsPage::Extensions => "Extensions",
-            SettingsPage::SystemAndNetwork => "System & Network",
-            SettingsPage::Collaboration => "Collaboration",
-        }
-    }
-}
-
-impl Display for SettingsPage {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let title = self.title();
-        write!(f, "{title}")
-    }
-}
-
 pub fn open_settings_editor(cx: &mut App) -> anyhow::Result<WindowHandle<SettingsWindow>> {
-    // todo(settings_ui) open in a local workspace if this is remote.
     cx.open_window(
         gpui::WindowOptions {
             titlebar: Some(gpui::TitlebarOptions {
@@ -105,105 +51,199 @@ pub fn open_settings_editor(cx: &mut App) -> anyhow::Result<WindowHandle<Setting
     )
 }
 
-pub fn init(cx: &mut App) {
-    // todo(settings_ui): Make action just a global action once flag
-    cx.observe_new(|workspace: &mut Workspace, _, _| {
-        workspace.register_action_renderer(|div, _, _, cx| {
-            let settings_ui_actions = [std::any::TypeId::of::<OpenSettingsEditor>()];
-            let has_flag = cx.has_flag::<SettingsUiFeatureFlag>();
-            command_palette_hooks::CommandPaletteFilter::update_global(cx, |filter, _| {
-                if has_flag {
-                    filter.show_action_types(&settings_ui_actions);
-                } else {
-                    filter.hide_action_types(&settings_ui_actions);
-                }
-            });
-            if has_flag {
-                div.on_action(cx.listener(|_, _: &OpenSettingsEditor, _, cx| {
-                    let _ = open_settings_editor(cx).ok();
-                }))
-            } else {
-                div
-            }
-        });
-    })
-    .detach();
-}
-
-pub struct SettingsWindow {
-    page: SettingsPage,
-}
+pub struct SettingsWindow {}
 
 impl SettingsWindow {
     pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
-        let page = SettingsPage::General;
-        Self { page }
+        Self {}
     }
 }
 
-fn render_nav(page: SettingsPage, _window: &mut Window, _cx: &mut Context<SettingsWindow>) -> Div {
-    let mut nav = v_flex().p_4().gap_2();
-    for &index in PAGES {
-        nav = nav.child(
-            div().id(index.title()).child(
-                Label::new(index.title())
-                    .size(LabelSize::Large)
-                    .when(page == index, |this| this.color(Color::Selected)),
-            ),
-        );
-    }
-    nav
-}
+// fn render_nav(page: SettingsPage, _window: &mut Window, _cx: &mut Context<SettingsWindow>) -> Div {
+//     let mut nav = v_flex().p_4().gap_2();
+//     for &index in PAGES {
+//         nav = nav.child(
+//             div().id(index.title()).child(
+//                 Label::new(index.title())
+//                     .size(LabelSize::Large)
+//                     .when(page == index, |this| this.color(Color::Selected)),
+//             ),
+//         );
+//     }
+//     nav
+// }
 
-fn render_page(page: SettingsPage, _window: &mut Window, cx: &mut Context<SettingsWindow>) -> Div {
-    let store = SettingsStore::global(cx);
-    let mut defaults = store
-        .raw_user_settings()
-        .map(|settings| &*settings.content)
-        .unwrap_or_else(|| store.raw_default_settings())
-        .clone();
-    div()
-}
+// fn render_page(page: SettingsPage, _window: &mut Window, cx: &mut Context<SettingsWindow>) -> Div {
+//     let store = SettingsStore::global(cx);
+//     let mut defaults = store
+//         .raw_user_settings()
+//         .map(|settings| &*settings.content)
+//         .unwrap_or_else(|| store.raw_default_settings())
+//         .clone();
+//     div()
+// }
+
+// slash_commands.cargo_workspace.enabled Is a boolean, that shows up in multiple files.
+//
+// "slash_commands": {
+//   "cargo_workspace": {
+//     "enabled": true
+//   }
+// },
+//
+// This shows up in server, user, and project settings
+// It can be overridden in different places,
 
 fn render_toggle_button(
     title: &'static str,
     description: &'static str,
-    get_value: fn(&mut SettingsContent) -> &mut bool,
+    // source: SettingsSource,
     cx: &mut App,
+    get_value: fn(&mut SettingsContent) -> Option<&mut bool>,
 ) {
-    // // todo! in settings window state
-    // let store = SettingsStore::global(cx);
-    // let mut defaults = store
-    //     .raw_user_settings()
-    //     .map(|settings| &*settings.content)
-    //     .unwrap_or_else(|| store.raw_default_settings())
-    //     .clone();
-    // // todo! id?
-    // let toggle_state = if *get_value(&mut defaults) {
-    //     ui::ToggleState::Selected
-    // } else {
-    //     ui::ToggleState::Unselected
-    // };
-    // SwitchField::new(
-    //     title,
-    //     title,
-    //     Some(description.into()),
-    //     toggle_state,
-    //     move |state, _, cx| {
-    //         SettingsStore::update_global(cx, |store, cx| {
-    //             store.update_user_settings(cx, |settings| {
-    //                 *get_value(settings) = *state == ui::ToggleState::Selected;
-    //             });
-    //         });
-    //     },
+    // div()
+    // .child(div().child(title).children(link))
+    // .child(description
+    // .child(settingUIStuffhere)
     // )
+    
+    // todo! in settings window state
+    let store = SettingsStore::global(cx);
+
+    // This clone needs to go!!
+    let mut defaults = store.raw_default_settings().clone();
+
+    // Hey settings store, here's my current file, get me the next correct value AND the file associated with that value
+
+    // What do we render, when a setting has been modified in the user settings, but not project settings, and we are viewing project settings?
+    //  - What VSCode does is render the _setting value's default_ (confusing!) and then adds a link to the user settings value
+    // When a project setting has been modified, that takes precedence over the user setting
+
+    let toggle_state = if *get_value(&mut store.raw_default_settings().clone())
+        .unwrap_or_else(|| get_value(&mut defaults).unwrap())
+    {
+        ui::ToggleState::Selected
+    } else {
+        ui::ToggleState::Unselected
+    };
+
+    SwitchField::new(
+        title,
+        title,
+        Some(description.into()),
+        toggle_state,
+        move |state, _, cx| {
+            cx.update_global(|store: &mut SettingsStore, cx| {
+                // source.update_file(cx, |settings| {
+                //   *get_value(settings).unwrap() = *state == ui::ToggleState::Selected;
+                // })
+
+                // TODO: Make seperate
+                store.update_user_settings(cx, |settings| {
+                    *get_value(settings).unwrap() = *state == ui::ToggleState::Selected;
+                });
+            })
+        },
+    );
 }
+
+// fn render_project_settings(cx: &mut App, get_value: fn(&mut SettingsContent) -> Option<&mut SlashCommandSettings>,
+// ) {
+//     // todo! in settings window state
+//     let store = SettingsStore::global(cx);
+
+//     // This clone needs to go!!
+//     let mut defaults = store.raw_default_settings().clone();
+//     render_slash_commands(defaults.project.get_or_insert_default())
+// }
+
+// fn render_slash_commands(
+//     cx: &mut App,
+//     get_value: fn(&mut SettingsContent) -> Option<&mut SlashCommandSettings>,
+// ) {
+//     // todo! in settings window state
+//     let store = SettingsStore::global(cx);
+
+//     // This clone needs to go!!
+//     let mut defaults = store.raw_default_settings().clone();
+
+//     let value = get_value(defaults).get_or_insert_default();
+//     render_cargo_workspace(value)
+// }
+
+// fn render_cargo_workspace
+//     cx: &mut App,
+//     get_value: fn(&mut SlashCommandSettings) -> Option<&mut CargoWorkspaceCommandSettings>,
+// ) {
+//     // todo! in settings window state
+//     let store = SettingsStore::global(cx);
+//     // This clone needs to go!!
+//     let mut defaults = store.raw_default_settings().clone();
+
+//     let value = get_value(defaults);
+//     render_toggle_button(title, description, source, cx, |content| {
+//         get_value(content).enabled.as_mut()
+//     });
+//     // ^ we could just use the get_value passed in to get the parent, then get the child in the callback we pass to the ui component
+// }
+//
+// ------------------------------------------------------------------------------------------
+
+// This should hold the write(&self) method
+// enum SettingsSource {
+//     User,
+//     Project(Arc<std::path::Path>),
+// }
+
+// Good exploration:
+// All of this should go in settings.
+// struct SettingsSources {
+//     defaults: SettingsContent,
+//     user: SettingsContent,
+//     project: SettingsContent,
+//     // server: SettingsContent
+// }
+
+// enum WhereWasSettingFound {
+//     InRequestedSource,
+//     In(SettingsSource),
+// }
+
+// fn read_setting_value<T>(
+//     sources: &mut SettingsSources,
+//     source: SettingsSource,
+//     get_value: fn(&mut SettingsContent) -> Option<&mut T>,
+// ) -> (Option<&mut T>, WhereWasSettingFound) {
+//     match source {
+//         SettingsSource::User => {
+//             get_value(&mut sources.user).or_else(|| get_value(&mut sources.defaults))
+//         }
+//         SettingsSource::Project(path) => get_value(&mut sources.project)
+//             .or_else(|| get_value(&mut sources.user))
+//             .or_else(|| get_value(&mut sources.defaults)),
+//     }
+// }
+
+// fn update_settings_source<T>(
+//     source: SettingsSource,
+//     update_fn: fn(&mut SettingsContent) -> &mut T,
+//     value: T,
+//     cx: &mut App,
+// ) {
+//     cx.update_global::<SettingsStore, _>(|store, cx| match source {
+//         SettingsSource::User => store.update_user_settings(cx, |settings| {
+//             *update_fn(settings) = value;
+//         }),
+//         SettingsSource::Project(path) => {
+//             store.update_settings_file_at_path(cx, &[path], |settings| {
+//                 *update_fn(settings) = value;
+//             })
+//         }
+//     });
+// }
 
 impl Render for SettingsWindow {
     fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         div()
-            .bg(cx.theme().colors().background)
-            .child(render_nav(self.page, window, cx))
-            .child(render_page(self.page, window, cx))
     }
 }
