20b5cd30a344d07b0ba345814e6a32b099114101
Ben Kunkle <ben@zed.dev>
2025-10-02 15:33:00 -0400

    include path in error

--- crates/migrator/src/migrations/m_2025_10_02/settings.rs
+++ crates/migrator/src/migrations/m_2025_10_02/settings.rs
@@ -2,20 +2,21 @@ use anyhow::Result;
 use serde_json::Value;
 
 pub fn remove_formatters_on_save(value: &mut Value) -> Result<()> {
-    remove_formatters_on_save_inner(value)?;
+    remove_formatters_on_save_inner(value, &[])?;
     let languages = value
         .as_object_mut()
         .and_then(|obj| obj.get_mut("languages"))
         .and_then(|languages| languages.as_object_mut());
     if let Some(languages) = languages {
-        for (_, language) in languages.iter_mut() {
-            remove_formatters_on_save_inner(language)?
+        for (language_name, language) in languages.iter_mut() {
+            let path = vec!["languages", language_name];
+            remove_formatters_on_save_inner(language, &path)?;
         }
     }
     Ok(())
 }
 
-fn remove_formatters_on_save_inner(value: &mut Value) -> Result<()> {
+fn remove_formatters_on_save_inner(value: &mut Value, path: &[&str]) -> Result<()> {
     let Some(obj) = value.as_object_mut() else {
         return Ok(());
     };
@@ -29,9 +30,17 @@ fn remove_formatters_on_save_inner(value: &mut Value) -> Result<()> {
         return Ok(());
     }
 
+    fn fmt_path(path: &[&str], key: &str) -> String {
+        let mut path = path.to_vec();
+        path.push(key);
+        path.join(".")
+    }
+
     anyhow::ensure!(
         obj.get("formatter").is_none(),
-        "Setting formatters in both format_on_save and formatters is deprecated. Please migrate the formatters from `format_on_save` into `formatters`"
+        r#"Setting formatters in both "format_on_save" and "formatter" is deprecated. Please migrate the formatters from {} into {}"#,
+        fmt_path(path, "format_on_save"),
+        fmt_path(path, "formatter")
     );
 
     obj.insert("format_on_save".to_string(), serde_json::json!("on"));
