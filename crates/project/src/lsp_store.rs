f618dfab91d95798bcb14a2963c7f7872a7fd767
Neil Macneale V <neil.macneale.v@gmail.com>
2025-10-03 15:48:42 -0700

    Add a file to manage semantic tokens

--- crates/project/src/lsp_store.rs
+++ crates/project/src/lsp_store.rs
@@ -14,6 +14,7 @@ pub mod json_language_server_ext;
 pub mod log_store;
 pub mod lsp_ext_command;
 pub mod rust_analyzer_ext;
+pub mod semantic_tokens;
 
 use crate::{
     CodeAction, ColorPresentation, Completion, CompletionDisplayOptions, CompletionResponse,
---
+++ crates/project/src/lsp_store/semantic_tokens.rs
@@ -0,0 +1,104 @@
+use std::slice::ChunksExact;
+
+use crate::lsp_command::SemanticTokensEdit;
+
+/// All the semantic tokens for a buffer, in the LSP format.
+#[derive(Default, Debug, Clone)]
+pub struct SemanticTokens {
+    /// Each value is:
+    /// data[5*i] - deltaLine: token line number, relative to the start of the previous token
+    /// data[5*i+1] - deltaStart: token start character, relative to the start of the previous token (relative to 0 or the previous token’s start if they are on the same line)
+    /// data[5*i+2] - length: the length of the token.
+    /// data[5*i+3] - tokenType: will be looked up in SemanticTokensLegend.tokenTypes. We currently ask that tokenType < 65536.
+    /// data[5*i+4] - tokenModifiers: each set bit will be looked up in SemanticTokensLegend.tokenModifiers
+    ///
+    /// See https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/ for more.
+    data: Vec<u32>,
+
+    pub server_id: Option<lsp::LanguageServerId>,
+}
+
+pub struct SemanticTokensIter<'a> {
+    prev: Option<(u32, u32)>,
+    data: ChunksExact<'a, u32>,
+}
+
+// A single item from `data`.
+struct SemanticTokenValue {
+    delta_line: u32,
+    delta_start: u32,
+    length: u32,
+    token_type: u32,
+    token_modifiers: u32,
+}
+
+/// A semantic token, independent of its position.
+#[derive(Debug)]
+pub struct SemanticToken {
+    pub line: u32,
+    pub start: u32,
+    pub length: u32,
+    pub token_type: u32,
+    pub token_modifiers: u32,
+}
+
+impl SemanticTokens {
+    pub fn from_full(data: Vec<u32>) -> Self {
+        SemanticTokens {
+            data,
+            server_id: None,
+        }
+    }
+
+    pub(crate) fn apply(&mut self, edits: &[SemanticTokensEdit]) {
+        for edit in edits {
+            let start = edit.start as usize;
+            let end = start + edit.delete_count as usize;
+            self.data.splice(start..end, edit.data.iter().copied());
+        }
+    }
+
+    pub fn tokens(&self) -> SemanticTokensIter<'_> {
+        SemanticTokensIter {
+            prev: None,
+            data: self.data.chunks_exact(5),
+        }
+    }
+}
+
+impl Iterator for SemanticTokensIter<'_> {
+    type Item = SemanticToken;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let chunk = self.data.next()?;
+        let token = SemanticTokenValue {
+            delta_line: chunk[0],
+            delta_start: chunk[1],
+            length: chunk[2],
+            token_type: chunk[3],
+            token_modifiers: chunk[4],
+        };
+
+        let (line, start) = if let Some((last_line, last_start)) = self.prev {
+            let line = last_line + token.delta_line;
+            let start = if token.delta_line == 0 {
+                last_start + token.delta_start
+            } else {
+                token.delta_start
+            };
+            (line, start)
+        } else {
+            (token.delta_line, token.delta_start)
+        };
+
+        self.prev = Some((line, start));
+
+        Some(SemanticToken {
+            line,
+            start,
+            length: token.length,
+            token_type: token.token_type,
+            token_modifiers: token.token_modifiers,
+        })
+    }
+}
