e60a61f7e76d1c4c2ddc3bd230cf9c7d78bc0593
Marshall Bowers <git@maxdeviant.com>
2025-10-07 16:26:05 -0400

    languages: Add comment injections for Rust (#39714)
    This PR adds comment injections for Rust.
    
    Release Notes:
    
    - Rust: Added comment injections.

--- crates/languages/src/rust/injections.scm
+++ crates/languages/src/rust/injections.scm
@@ -1,3 +1,6 @@
+((line_comment) @injection.content
+    (#set! injection.language "comment"))
+
 (macro_invocation
     macro: (identifier) @_macro_name
     (#not-any-of? @_macro_name "view" "html")
