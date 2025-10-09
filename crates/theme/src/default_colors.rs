7ba2e8b733f2e899db47ca3dcbc96a7a587689df
willyHetland <willy.hetland@zeekit.no>
2025-10-09 19:02:54 +0200

    move vim mode colors to bottom

--- crates/theme/src/default_colors.rs
+++ crates/theme/src/default_colors.rs
@@ -173,15 +173,6 @@ impl ThemeColors {
         let system = SystemColors::default();
 
         Self {
-            vim_normal_background: system.transparent,
-            vim_insert_background: system.transparent,
-            vim_replace_background: system.transparent,
-            vim_visual_background: system.transparent,
-            vim_visual_line_background: system.transparent,
-            vim_visual_block_background: system.transparent,
-            vim_helix_normal_background: system.transparent,
-            vim_helix_select_background: system.transparent,
-            vim_mode_text: neutral().dark().step_12(),
             border: neutral().dark().step_6(),
             border_variant: neutral().dark().step_5(),
             border_focused: blue().dark().step_5(),
@@ -298,6 +289,15 @@ impl ThemeColors {
             version_control_ignored: gray().dark().step_12(),
             version_control_conflict_marker_ours: green().dark().step_10().alpha(0.5),
             version_control_conflict_marker_theirs: blue().dark().step_10().alpha(0.5),
+            vim_normal_background: system.transparent,
+            vim_insert_background: system.transparent,
+            vim_replace_background: system.transparent,
+            vim_visual_background: system.transparent,
+            vim_visual_line_background: system.transparent,
+            vim_visual_block_background: system.transparent,
+            vim_helix_normal_background: system.transparent,
+            vim_helix_select_background: system.transparent,
+            vim_mode_text: neutral().dark().step_12(),
         }
     }
 }
