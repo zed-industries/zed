4a9dc11cc2951b2d4099d66343e28655d16ced63
Jason Lee <huacnlee@gmail.com>
2025-10-09 22:11:21 +0800

    gpui: Add to support check state to MenuItem.

--- crates/gpui/examples/set_menus.rs
+++ crates/gpui/examples/set_menus.rs
@@ -1,6 +1,6 @@
 use gpui::{
-    App, Application, Context, Menu, MenuItem, SystemMenuType, Window, WindowOptions, actions, div,
-    prelude::*, rgb,
+    App, Application, Context, Global, Menu, MenuItem, SystemMenuType, Window, WindowOptions,
+    actions, div, prelude::*, rgb,
 };
 
 struct SetMenus;
@@ -21,29 +21,57 @@ impl Render for SetMenus {
 
 fn main() {
     Application::new().run(|cx: &mut App| {
+        cx.set_global(AppState::new());
+
         // Bring the menu bar to the foreground (so you can see the menu bar)
         cx.activate(true);
         // Register the `quit` function so it can be referenced by the `MenuItem::action` in the menu bar
         cx.on_action(quit);
+        cx.on_action(toggle_check);
         // Add menu items
-        cx.set_menus(vec![Menu {
-            name: "set_menus".into(),
-            items: vec![
-                MenuItem::os_submenu("Services", SystemMenuType::Services),
-                MenuItem::separator(),
-                MenuItem::action("Quit", Quit),
-            ],
-        }]);
+        set_app_menus(cx);
         cx.open_window(WindowOptions::default(), |_, cx| cx.new(|_| SetMenus {}))
             .unwrap();
     });
 }
 
+struct AppState {
+    checked: bool,
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self { checked: false }
+    }
+}
+
+impl Global for AppState {}
+
+fn set_app_menus(cx: &mut App) {
+    let app_state = cx.global::<AppState>();
+    cx.set_menus(vec![Menu {
+        name: "set_menus".into(),
+        items: vec![
+            MenuItem::os_submenu("Services", SystemMenuType::Services),
+            MenuItem::separator(),
+            MenuItem::action("Checkable Item", ToggleCheck).checked(app_state.checked),
+            MenuItem::separator(),
+            MenuItem::action("Quit", Quit),
+        ],
+    }]);
+}
+
 // Associate actions using the `actions!` macro (or `Action` derive macro)
-actions!(set_menus, [Quit]);
+actions!(set_menus, [Quit, ToggleCheck]);
 
 // Define the quit function that is registered with the App
 fn quit(_: &Quit, cx: &mut App) {
     println!("Gracefully quitting the application . . .");
     cx.quit();
+}
+
+fn toggle_check(_: &ToggleCheck, cx: &mut App) {
+    let app_state = cx.global_mut::<AppState>();
+    app_state.checked = !app_state.checked;
+    set_app_menus(cx);
 }
