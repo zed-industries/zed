3ba4b841075315071ac67fb4756cbc940792b601
Ben Kunkle <ben@zed.dev>
2025-10-10 18:01:07 -0500

    Deprecate code actions on format setting (#39983)
    Closes #ISSUE
    
    Release Notes:
    
    - settings: Deprecated `code_actions_on_format` in favor of specifying
    code actions to run on format inline in the `formatter` array.
    
    Previously, you would configure code actions to run on format like this:
    
    ```json
    {
      "code_actions_on_format": {
        "source.organizeImports": true,
        "source.fixAll.eslint": true
      }
    }
    ```
    
    This has been migrated to the new format:
    
    ```json
    {
      "formatter": [
        {
          "code_action": "source.organizeImports"
        },
        {
          "code_action": "source.fixAll.eslint"
        }
      ]
    }
    ```
    
    This change will be automatically migrated for you. If you had an
    existing `formatter` setting, the code actions are prepended to your
    formatter array (matching the existing behavior). This migration applies
    to both global settings and language-specific settings

--- crates/migrator/src/migrator.rs
+++ crates/migrator/src/migrator.rs
@@ -213,6 +213,7 @@ pub fn migrate_settings(text: &str) -> Result<Option<String>> {
             migrations::m_2025_10_03::SETTINGS_PATTERNS,
             &SETTINGS_QUERY_2025_10_03,
         ),
+        MigrationType::Json(migrations::m_2025_10_10::remove_code_actions_on_format),
     ];
     run_migrations(text, migrations)
 }
@@ -1911,6 +1912,304 @@ mod tests {
             }"#
             .unindent(),
             None,
+        );
+    }
+
+    #[test]
+    fn test_code_actions_on_format_migration_basic() {
+        assert_migrate_settings_with_migrations(
+            &[MigrationType::Json(
+                migrations::m_2025_10_10::remove_code_actions_on_format,
+            )],
+            &r#"{
+                "code_actions_on_format": {
+                    "source.organizeImports": true,
+                    "source.fixAll": true
+                }
+            }"#
+            .unindent(),
+            Some(
+                &r#"{
+                    "formatter": [
+                        {
+                            "code_action": "source.organizeImports"
+                        },
+                        {
+                            "code_action": "source.fixAll"
+                        }
+                    ]
+                }
+                "#
+                .unindent(),
+            ),
+        );
+    }
+
+    #[test]
+    fn test_code_actions_on_format_migration_filters_false_values() {
+        assert_migrate_settings_with_migrations(
+            &[MigrationType::Json(
+                migrations::m_2025_10_10::remove_code_actions_on_format,
+            )],
+            &r#"{
+                "code_actions_on_format": {
+                    "a": true,
+                    "b": false,
+                    "c": true
+                }
+            }"#
+            .unindent(),
+            Some(
+                &r#"{
+                    "formatter": [
+                        {
+                            "code_action": "a"
+                        },
+                        {
+                            "code_action": "c"
+                        }
+                    ]
+                }
+                "#
+                .unindent(),
+            ),
+        );
+    }
+
+    #[test]
+    fn test_code_actions_on_format_migration_with_existing_formatter_object() {
+        assert_migrate_settings_with_migrations(
+            &[MigrationType::Json(
+                migrations::m_2025_10_10::remove_code_actions_on_format,
+            )],
+            &r#"{
+              "formatter": "prettier",
+              "code_actions_on_format": {
+                "source.organizeImports": true
+              }
+            }"#
+            .unindent(),
+            Some(
+                &r#"{
+                  "formatter": [
+                    {
+                      "code_action": "source.organizeImports"
+                    },
+                    "prettier"
+                  ]
+                }"#
+                .unindent(),
+            ),
+        );
+    }
+
+    #[test]
+    fn test_code_actions_on_format_migration_with_existing_formatter_array() {
+        assert_migrate_settings_with_migrations(
+            &[MigrationType::Json(
+                migrations::m_2025_10_10::remove_code_actions_on_format,
+            )],
+            &r#"{
+              "formatter": ["prettier", {"language_server": "eslint"}],
+              "code_actions_on_format": {
+                "source.organizeImports": true,
+                "source.fixAll": true
+              }
+            }"#
+            .unindent(),
+            Some(
+                &r#"{
+                  "formatter": [
+                    {
+                      "code_action": "source.organizeImports"
+                    },
+                    {
+                      "code_action": "source.fixAll"
+                    },
+                    "prettier",
+                    {
+                      "language_server": "eslint"
+                    }
+                  ]
+                }"#
+                .unindent(),
+            ),
+        );
+    }
+
+    #[test]
+    fn test_code_actions_on_format_migration_in_languages() {
+        assert_migrate_settings_with_migrations(
+            &[MigrationType::Json(
+                migrations::m_2025_10_10::remove_code_actions_on_format,
+            )],
+            &r#"{
+                "languages": {
+                    "JavaScript": {
+                        "code_actions_on_format": {
+                            "source.fixAll.eslint": true
+                        }
+                    },
+                    "Go": {
+                        "code_actions_on_format": {
+                            "source.organizeImports": true
+                        }
+                    }
+                }
+            }"#
+            .unindent(),
+            Some(
+                &r#"{
+                    "languages": {
+                        "JavaScript": {
+                            "formatter": [
+                                {
+                                    "code_action": "source.fixAll.eslint"
+                                }
+                            ]
+                        },
+                        "Go": {
+                            "formatter": [
+                                {
+                                    "code_action": "source.organizeImports"
+                                }
+                            ]
+                        }
+                    }
+                }"#
+                .unindent(),
+            ),
+        );
+    }
+
+    #[test]
+    fn test_code_actions_on_format_migration_in_languages_with_existing_formatter() {
+        assert_migrate_settings_with_migrations(
+            &[MigrationType::Json(
+                migrations::m_2025_10_10::remove_code_actions_on_format,
+            )],
+            &r#"{
+              "languages": {
+                "JavaScript": {
+                  "formatter": "prettier",
+                  "code_actions_on_format": {
+                    "source.fixAll.eslint": true,
+                    "source.organizeImports": false
+                  }
+                }
+              }
+            }"#
+            .unindent(),
+            Some(
+                &r#"{
+                  "languages": {
+                    "JavaScript": {
+                      "formatter": [
+                        {
+                          "code_action": "source.fixAll.eslint"
+                        },
+                        "prettier"
+                      ]
+                    }
+                  }
+                }"#
+                .unindent(),
+            ),
+        );
+    }
+
+    #[test]
+    fn test_code_actions_on_format_migration_mixed_global_and_languages() {
+        assert_migrate_settings_with_migrations(
+            &[MigrationType::Json(
+                migrations::m_2025_10_10::remove_code_actions_on_format,
+            )],
+            &r#"{
+              "formatter": "prettier",
+              "code_actions_on_format": {
+                "source.fixAll": true
+              },
+              "languages": {
+                "Rust": {
+                  "formatter": "rust-analyzer",
+                  "code_actions_on_format": {
+                    "source.organizeImports": true
+                  }
+                },
+                "Python": {
+                  "code_actions_on_format": {
+                    "source.organizeImports": true,
+                    "source.fixAll": false
+                  }
+                }
+              }
+            }"#
+            .unindent(),
+            Some(
+                &r#"{
+                  "formatter": [
+                    {
+                      "code_action": "source.fixAll"
+                    },
+                    "prettier"
+                  ],
+                  "languages": {
+                    "Rust": {
+                      "formatter": [
+                        {
+                          "code_action": "source.organizeImports"
+                        },
+                        "rust-analyzer"
+                      ]
+                    },
+                    "Python": {
+                            "formatter": [
+                                {
+                                    "code_action": "source.organizeImports"
+                                }
+                            ]
+                        }
+                  }
+                }"#
+                .unindent(),
+            ),
+        );
+    }
+
+    #[test]
+    fn test_code_actions_on_format_no_migration_when_not_present() {
+        assert_migrate_settings_with_migrations(
+            &[MigrationType::Json(
+                migrations::m_2025_10_10::remove_code_actions_on_format,
+            )],
+            &r#"{
+              "formatter": ["prettier"]
+            }"#
+            .unindent(),
+            None,
+        );
+    }
+
+    #[test]
+    fn test_code_actions_on_format_migration_all_false_values() {
+        assert_migrate_settings_with_migrations(
+            &[MigrationType::Json(
+                migrations::m_2025_10_10::remove_code_actions_on_format,
+            )],
+            &r#"{
+                "code_actions_on_format": {
+                    "a": false,
+                    "b": false
+                },
+                "formatter": "prettier"
+            }"#
+            .unindent(),
+            Some(
+                &r#"{
+                    "formatter": "prettier"
+                }"#
+                .unindent(),
+            ),
         );
     }
 }
