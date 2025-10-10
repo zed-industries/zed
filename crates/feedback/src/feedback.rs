7538192ba2edade304707c1bf49c04a167914358
Joseph T. Lyons <JosephTLyons@gmail.com>
2025-10-10 10:57:30 -0400

    Revert namespace

--- crates/feedback/src/feedback.rs
+++ crates/feedback/src/feedback.rs
@@ -5,7 +5,7 @@ use workspace::Workspace;
 use zed_actions::feedback::{EmailZed, FileBugReport, RequestFeature};
 
 actions!(
-    contribute,
+    zed,
     [
         /// Opens the Zed repository on GitHub.
         OpenZedRepo,
