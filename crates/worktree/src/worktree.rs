32590fe33016c76721825df274b9ec2ef8fc8f3f
Cole Miller <cole@zed.dev>
2025-10-14 19:12:30 -0400

    windows: Add some trace-level logging to help dig into missing FS update bugs (#40200)
    Related to https://github.com/zed-industries/zed/issues/38109
    
    Release Notes:
    
    - N/A

--- crates/worktree/src/worktree.rs
+++ crates/worktree/src/worktree.rs
@@ -2435,6 +2435,7 @@ impl LocalSnapshot {
     }
 
     fn insert_entry(&mut self, mut entry: Entry, fs: &dyn Fs) -> Entry {
+        log::trace!("insert entry {:?}", entry.path);
         if entry.is_file() && entry.path.file_name() == Some(&GITIGNORE) {
             let abs_path = self.absolutize(&entry.path);
             match smol::block_on(build_gitignore(&abs_path, fs)) {
@@ -3758,6 +3759,7 @@ impl BackgroundScanner {
     }
 
     async fn process_events(&self, mut abs_paths: Vec<PathBuf>) {
+        log::trace!("process events: {abs_paths:?}");
         let root_path = self.state.lock().snapshot.abs_path.clone();
         let root_canonical_path = self.fs.canonicalize(root_path.as_path()).await;
         let root_canonical_path = match &root_canonical_path {
@@ -4351,7 +4353,6 @@ impl BackgroundScanner {
         // detected regardless of the order of the paths.
         for (path, metadata) in relative_paths.iter().zip(metadata.iter()) {
             if matches!(metadata, Ok(None)) || doing_recursive_update {
-                log::trace!("remove path {:?}", path);
                 state.remove_path(path);
             }
         }
