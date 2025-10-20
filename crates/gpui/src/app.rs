71ca6c4d47e3815f07fbc6d4c7557a36a4f9f1ae
Be <be.0@gmx.com>
2025-10-20 00:44:01 -0500

    Use libwebrtc's DesktopCapturer for screen capture
    This adds support for screen sharing on Wayland (fixes #28754).
    
    libwebrtc replaces scap, which aims to be cross platform but was only
    used on Windows and X11. The X11 support relied on an out-of-tree branch
    on which the original author closed their own pull request
    (https://github.com/CapSoftware/scap/pull/124). This also replaces Zed's
    platform-specific code on macOS. Switching to a single cross platform
    library simplifies the code by removing the need for Zed to have its own
    traits for cross platform abstraction.

--- crates/gpui/src/app.rs
+++ crates/gpui/src/app.rs
@@ -40,8 +40,8 @@ use crate::{
     Keymap, Keystroke, LayoutId, Menu, MenuItem, OwnedMenu, PathPromptOptions, Pixels, Platform,
     PlatformDisplay, PlatformKeyboardLayout, PlatformKeyboardMapper, Point, PromptBuilder,
     PromptButton, PromptHandle, PromptLevel, Render, RenderImage, RenderablePromptHandle,
-    Reservation, ScreenCaptureSource, SharedString, SubscriberSet, Subscription, SvgRenderer, Task,
-    TextSystem, Window, WindowAppearance, WindowHandle, WindowId, WindowInvalidator,
+    Reservation, SharedString, SubscriberSet, Subscription, SvgRenderer, Task, TextSystem, Window,
+    WindowAppearance, WindowHandle, WindowId, WindowInvalidator,
     colors::{Colors, GlobalColors},
     current_platform, hash, init_app_menus,
 };
@@ -1052,18 +1052,6 @@ impl App {
     /// Returns the primary display that will be used for new windows.
     pub fn primary_display(&self) -> Option<Rc<dyn PlatformDisplay>> {
         self.platform.primary_display()
-    }
-
-    /// Returns whether `screen_capture_sources` may work.
-    pub fn is_screen_capture_supported(&self) -> bool {
-        self.platform.is_screen_capture_supported()
-    }
-
-    /// Returns a list of available screen capture sources.
-    pub fn screen_capture_sources(
-        &self,
-    ) -> oneshot::Receiver<Result<Vec<Rc<dyn ScreenCaptureSource>>>> {
-        self.platform.screen_capture_sources()
     }
 
     /// Returns the display with the given ID, if one exists.
