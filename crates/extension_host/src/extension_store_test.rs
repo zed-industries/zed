503e6de1ea91df456703878798a1899d31074ce3
Richard Feldman <oss@rtfeldman.com>
2025-10-20 13:13:48 -0400

    Fix tests

--- crates/extension_host/src/extension_store_test.rs
+++ crates/extension_host/src/extension_store_test.rs
@@ -150,15 +150,13 @@ async fn test_extension_store(cx: &mut TestAppContext) {
                         themes: Default::default(),
                         icon_themes: Vec::new(),
                         lib: Default::default(),
-                        languages: vec!["languages/erb".into(), "languages/ruby".into()],
-                        grammars: [
-                            ("embedded_template".into(), GrammarManifestEntry::default()),
-                            ("ruby".into(), GrammarManifestEntry::default()),
-                        ]
-                        .into_iter()
-                        .collect(),
+                        languages: vec!["languages/ruby".into()],
+                        grammars: [("ruby".into(), GrammarManifestEntry::default())]
+                            .into_iter()
+                            .collect(),
                         language_servers: BTreeMap::default(),
                         context_servers: BTreeMap::default(),
+                        agent_servers: BTreeMap::default(),
                         slash_commands: BTreeMap::default(),
                         snippets: None,
                         capabilities: Vec::new(),
@@ -189,6 +187,7 @@ async fn test_extension_store(cx: &mut TestAppContext) {
                         grammars: BTreeMap::default(),
                         language_servers: BTreeMap::default(),
                         context_servers: BTreeMap::default(),
+                        agent_servers: BTreeMap::default(),
                         slash_commands: BTreeMap::default(),
                         snippets: None,
                         capabilities: Vec::new(),
@@ -368,6 +367,7 @@ async fn test_extension_store(cx: &mut TestAppContext) {
                 grammars: BTreeMap::default(),
                 language_servers: BTreeMap::default(),
                 context_servers: BTreeMap::default(),
+                agent_servers: BTreeMap::default(),
                 slash_commands: BTreeMap::default(),
                 snippets: None,
                 capabilities: Vec::new(),
