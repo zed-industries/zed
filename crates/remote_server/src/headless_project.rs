f334d1edf1b81cc51004ad6fd4e80fcd83e7bf8e
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-10-22 00:02:45 +0200

    Revert "search: New old search implementation (#39956)"
    This reverts commit 7c4fb5a899c34efaea9e52ddd84daebc8d9ccf49.

--- crates/remote_server/src/headless_project.rs
+++ crates/remote_server/src/headless_project.rs
@@ -640,15 +640,9 @@ impl HeadlessProject {
             PathStyle::local(),
         )?;
         let results = this.update(&mut cx, |this, cx| {
-            project::Search::local(
-                this.fs.clone(),
-                this.buffer_store.clone(),
-                this.worktree_store.clone(),
-                message.limit as _,
-                cx,
-            )
-            .into_handle(query, cx)
-            .matching_buffers(cx)
+            this.buffer_store.update(cx, |buffer_store, cx| {
+                buffer_store.find_search_candidates(&query, message.limit as _, this.fs.clone(), cx)
+            })
         })?;
 
         let mut response = proto::FindSearchCandidatesResponse {
