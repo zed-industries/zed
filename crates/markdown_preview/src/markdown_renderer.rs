45983e11e9c0d78f61da9b076263da0c43e0dae7
Remco Smits <djsmits12@gmail.com>
2025-10-25 22:15:17 +0200

    markdown: Add support for HTML lists (#39553)
    This PR adds support for **HTML** both ordered and unordered lists.
    
    <img width="1441" height="805" alt="Screenshot 2025-10-07 at 21 40 17"
    src="https://github.com/user-attachments/assets/8a54aec1-75aa-48fb-bf9f-c153cca48682"
    />
    
    See code example used inside the screenshot:
    
    ```html
    <ol>
      <li>First item</li>
      <li>Second item</li>
      <li>Third item
        <ol>
          <li>Indented item</li>
          <li>Indented item</li>
        </ol>
      </li>
      <li>Fourth item</li>
    </ol>
    ```
    
    TODO: 
    - [x] Add examples
    - [x] update description (screenshots, add small description)
    - [x] fix displaying of nested lists
    
    cc @bennetbo
    
    Release Notes:
    
    - markdown preview: Added support for HTML lists
    
    ---------
    
    Co-authored-by: Bennet Bo Fenner <bennetbo@gmx.de>

--- crates/markdown_preview/src/markdown_renderer.rs
+++ crates/markdown_preview/src/markdown_renderer.rs
@@ -6,10 +6,10 @@ use crate::markdown_elements::{
 };
 use fs::normalize_path;
 use gpui::{
-    AbsoluteLength, AnyElement, App, AppContext as _, ClipboardItem, Context, DefiniteLength, Div,
-    Element, ElementId, Entity, HighlightStyle, Hsla, ImageSource, InteractiveText, IntoElement,
-    Keystroke, Modifiers, ParentElement, Render, Resource, SharedString, Styled, StyledText,
-    TextStyle, WeakEntity, Window, div, img, rems,
+    AbsoluteLength, AnyElement, App, AppContext as _, ClipboardItem, Context, Div, Element,
+    ElementId, Entity, HighlightStyle, Hsla, ImageSource, InteractiveText, IntoElement, Keystroke,
+    Modifiers, ParentElement, Render, Resource, SharedString, Styled, StyledText, TextStyle,
+    WeakEntity, Window, div, img, rems,
 };
 use settings::Settings;
 use std::{
@@ -234,8 +234,6 @@ fn render_markdown_list_item(
 ) -> AnyElement {
     use ParsedMarkdownListItemType::*;
 
-    let padding = cx.scaled_rems((parsed.depth - 1) as f32);
-
     let bullet = match &parsed.item_type {
         Ordered(order) => format!("{}.", order).into_any_element(),
         Unordered => "•".into_any_element(),
@@ -294,13 +292,16 @@ fn render_markdown_list_item(
         .collect();
 
     let item = h_flex()
-        .pl(DefiniteLength::Absolute(AbsoluteLength::Rems(padding)))
+        .when(!parsed.nested, |this| {
+            this.pl(cx.scaled_rems(parsed.depth.saturating_sub(1) as f32))
+        })
+        .when(parsed.nested && parsed.depth > 1, |this| this.ml_neg_1p5())
         .items_start()
         .children(vec![
             bullet,
             v_flex()
                 .children(contents)
-                .gap(cx.scaled_rems(1.0))
+                .when(!parsed.nested, |this| this.gap(cx.scaled_rems(1.0)))
                 .pr(cx.scaled_rems(1.0))
                 .w_full(),
         ]);
