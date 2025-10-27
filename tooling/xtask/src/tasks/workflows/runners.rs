de86ae22dc9afabbb68fd382fa5bc813448b7359
Conrad Irwin <conrad.irwin@gmail.com>
2025-10-27 11:21:47 -0600

    More happier

--- tooling/xtask/src/tasks/workflows/runners.rs
+++ tooling/xtask/src/tasks/workflows/runners.rs
@@ -1,17 +1,11 @@
-pub const LINUX_CHEAP: Runner = Runner::Profile("namespace-profile-2x4-ubuntu-2404");
-pub const LINUX_DEFAULT: Runner = Runner::Profile("namespace-profile-16x32-ubuntu-2204");
-pub const MAC_DEFAULT: Runner = Runner::Labelled(&["macOS", "ARM64", "test"]);
+pub const LINUX_CHEAP: Runner = Runner("namespace-profile-2x4-ubuntu-2404");
+pub const LINUX_DEFAULT: Runner = Runner("namespace-profile-16x32-ubuntu-2204");
+pub const MAC_DEFAULT: Runner = Runner("self-mini-macos");
 
-pub enum Runner {
-    Profile(&'static str),
-    Labelled(&'static [&'static str]),
-}
+pub struct Runner(&'static str);
 
 impl Into<gh_workflow::RunsOn> for Runner {
     fn into(self) -> gh_workflow::RunsOn {
-        match self {
-            Runner::Profile(profile) => profile.into(),
-            Runner::Labelled(items) => items.into(),
-        }
+        self.0.into()
     }
 }
