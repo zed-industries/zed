57f8fe6a8503b67e3fd03ca692d272f5a369d079
dino <dinojoaocosta@gmail.com>
2025-10-27 17:34:23 +0000

    fix(agent_ui): automatically scroll after quote insertion
    Update the behavior of the `zed_actions::agent::AddSelectionToThread`
    action so that, after the selecitons are added to the current thread,
    the editor automatically scrolls to the cursor's position, fixing an
    issue where the inserted selection's UI component could wrap the cursor
    to the next line below, leaving it outside the viewable area.

--- crates/agent_ui/src/acp/message_editor.rs
+++ crates/agent_ui/src/acp/message_editor.rs
@@ -16,6 +16,7 @@ use editor::{
     MultiBuffer, ToOffset,
     actions::Paste,
     display_map::{Crease, CreaseId, FoldId},
+    scroll::Autoscroll,
 };
 use futures::{
     FutureExt as _,
@@ -591,6 +592,21 @@ impl MessageEditor {
                 ),
             );
         }
+
+        // Take this explanation with a grain of salt but, with creases being
+        // inserted, GPUI's recomputes the editor layout in the next frames, so
+        // directly calling `editor.request_autoscroll` wouldn't work as
+        // expected. We're leveraging `cx.on_next_frame` to wait 2 frames and
+        // ensure that the layout has been recalculated so that the autoscroll
+        // request actually shows the cursor's new position.
+        let editor = self.editor.clone();
+        cx.on_next_frame(window, move |_, window, cx| {
+            cx.on_next_frame(window, move |_, _, cx| {
+                editor.update(cx, |editor, cx| {
+                    editor.request_autoscroll(Autoscroll::fit(), cx)
+                });
+            });
+        });
     }
 
     fn confirm_mention_for_thread(
@@ -1030,6 +1046,7 @@ impl MessageEditor {
 
         self.editor.update(cx, |message_editor, cx| {
             message_editor.edit([(cursor_anchor..cursor_anchor, completion.new_text)], cx);
+            message_editor.request_autoscroll(Autoscroll::fit(), cx);
         });
         if let Some(confirm) = completion.confirm {
             confirm(CompletionIntent::Complete, window, cx);
