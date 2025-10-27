e683e4972a21fc2df5d15ad29bf623e3bbf444eb
cameron <cameron.studdstreet@gmail.com>
2025-10-27 07:10:56 +0000

    add rust `Regex::new()` treesitter injection

--- crates/languages/src/rust/injections.scm
+++ crates/languages/src/rust/injections.scm
@@ -16,3 +16,20 @@
     (token_tree) @injection.content
     (#set! injection.language "rstml")
     )
+
+(call_expression
+    function: (scoped_identifier
+       path: (_) @_ty_path
+       name: (identifier) @_assoc_fn_name
+    )
+    arguments: (arguments
+        [
+            (string_literal (string_content) @injection.content)
+            (raw_string_literal (string_content) @injection.content)
+        ]
+    )
+    
+    (#match? @_ty_path ".*Regex") 
+    (#eq! @_assoc_fn_name "new")
+    (#set! injection.language "regex")
+)
