62d8209dd773192197bbe5d91478ad784e9d34ab
Mayank Verma <errmayank@gmail.com>
2025-11-01 09:19:04 +0530

    Revert to using regex pattern instead of tree sitter queries

--- crates/languages/src/c/indents.scm
+++ crates/languages/src/c/indents.scm
@@ -1,36 +1,19 @@
 [
   (field_expression)
   (assignment_expression)
+  (if_statement)
   (for_statement)
   (while_statement)
   (do_statement)
+  (else_clause)
 ] @indent
 
 (_ "{" "}" @end) @indent
 (_ "(" ")" @end) @indent
 
-(if_statement
-  condition: (_) @start
-  alternative: (_) @end) @indent
-
-(if_statement
-  condition: (_) @start
-  consequence: (_)
-  !alternative) @indent
-
-(if_statement
-  alternative: (_) @indent
-  (#not-kind-eq? @indent "if_statement"))
-
-((if_statement
-  consequence: (_) @indent.begin)
-  (#not-kind-eq? @indent.begin "compound_statement"))
-
-((if_statement
-  alternative: (_) @indent.begin)
-  (#not-kind-eq? @indent.begin "compound_statement"))
-
+(if_statement) @start.if
 (for_statement) @start.for
 (while_statement) @start.while
 (do_statement) @start.do
 (switch_statement) @start.switch
+(else_clause) @start.else
