1a18eebb1afda34718c343422e6645d085446c51
Conrad Irwin <conrad.irwin@gmail.com>
2025-11-04 10:50:35 -0700

    woop

--- tooling/xtask/src/tasks/workflows/cherry_pick.rs
+++ tooling/xtask/src/tasks/workflows/cherry_pick.rs
@@ -22,20 +22,8 @@ pub fn cherry_pick() -> Workflow {
 }
 
 fn run_cherry_pick(commit: &Input, base: &Input, pr_no: &Input) -> NamedJob {
-    fn new_branch(base: &str, commit: &str) -> Step<Run> {
-        // These are the commit SHAs, so always ascii and >8 chars.
-        let commit = &commit[0..8];
-        named::bash(&format!("git checkout -b cherry-pick-{base}-{commit}"))
-    }
-
-    fn cherry_remote(commit: &str) -> Step<Run> {
-        named::bash(&format!("git fetch {commit} && git cherry-pick {commit}"))
-    }
-
-    fn make_pr(base: &str, pr_no: &str) -> Step<Run> {
-        named::bash(&format!(
-            "COMMIT=$(git log --pretty=format:%s -n 1) gh pr new --head --base {base} --title \"Cherry-pick \\\"$COMMIT\\\" to {base}\" --body \"PR #{pr_no}\" && gh pr merge --auto"
-        ))
+    fn cherry_pick(branch: &str, commit: &str) -> Step<Run> {
+        named::bash(&format!("./scripts/cherry_pick.sh {branch} {commit}"))
     }
 
     named::job(
@@ -43,8 +31,7 @@ fn run_cherry_pick(commit: &Input, base: &Input, pr_no: &Input) -> NamedJob {
             .runs_on(runners::LINUX_SMALL)
             .add_step(steps::checkout_repo())
             .add_step(steps::git_checkout(&base.var()))
-            .add_step(new_branch(&base.var(), &commit.var()))
-            .add_step(cherry_remote(&commit.var()))
+            .add_step(cherry_pick(&base.var(), &commit.var()))
             .add_step(
                 make_pr(&base.var(), &pr_no.var())
                     .add_env(("GITHUB_TOKEN", "${{ secrets.GITHUB_TOKEN }}")),
