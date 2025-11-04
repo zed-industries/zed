60b168cb23c401972dcc08d90e5fe9d729237bb3
Conrad Irwin <conrad.irwin@gmail.com>
2025-11-04 11:29:35 -0700

    Improve compare_perf.yml, cherry_pick.yml (#41606)
    Release Notes:
    
    - N/A
    
    ---------
    
    Co-authored-by: Nia Espera <nia@zed.dev>

---
+++ tooling/xtask/src/tasks/workflows/cherry_pick.rs
@@ -0,0 +1,33 @@
+use gh_workflow::*;
+
+use crate::tasks::workflows::{
+    runners,
+    steps::{self, NamedJob, named},
+    vars::Input,
+};
+
+pub fn cherry_pick() -> Workflow {
+    let branch = Input::string("branch", None);
+    let commit = Input::string("commit", None);
+    let cherry_pick = run_cherry_pick(&branch, &commit);
+    named::workflow()
+        .on(Event::default().workflow_dispatch(
+            WorkflowDispatch::default()
+                .add_input(commit.name, commit.input())
+                .add_input(branch.name, branch.input()),
+        ))
+        .add_job(cherry_pick.name, cherry_pick.job)
+}
+
+fn run_cherry_pick(branch: &Input, commit: &Input) -> NamedJob {
+    fn cherry_pick(branch: &str, commit: &str) -> Step<Run> {
+        named::bash(&format!("./scripts/cherry-pick {branch} {commit}"))
+    }
+
+    named::job(
+        Job::default()
+            .runs_on(runners::LINUX_SMALL)
+            .add_step(steps::checkout_repo())
+            .add_step(cherry_pick(&branch.var(), &commit.var())),
+    )
+}
--- tooling/xtask/src/tasks/workflows/compare_perf.rs
+++ tooling/xtask/src/tasks/workflows/compare_perf.rs
@@ -1,22 +1,56 @@
 use gh_workflow::*;
 
+use crate::tasks::workflows::run_bundling::upload_artifact;
+use crate::tasks::workflows::steps::FluentBuilder;
 use crate::tasks::workflows::{
     runners,
     steps::{self, NamedJob, named},
+    vars::Input,
 };
 
-/// Generates the danger.yml workflow
 pub fn compare_perf() -> Workflow {
-    let run_perf = run_perf();
+    let head = Input::string("head", None);
+    let base = Input::string("base", None);
+    let run_perf = run_perf(&base, &head);
     named::workflow()
-        .on(Event::default().workflow_dispatch(WorkflowDispatch::default()))
+        .on(Event::default().workflow_dispatch(
+            WorkflowDispatch::default()
+                .add_input(head.name, head.input())
+                .add_input(base.name, base.input()),
+        ))
         .add_job(run_perf.name, run_perf.job)
 }
 
-pub fn run_perf() -> NamedJob {
+pub fn run_perf(base: &Input, head: &Input) -> NamedJob {
+    fn cargo_perf_test(ref_name: String) -> Step<Run> {
+        // TODO: vim not gpui, and ideally allow args
+        named::bash(&format!("cargo perf-test -p gpui -- --json={ref_name}"))
+    }
+
+    fn install_hyperfine() -> Step<Run> {
+        named::bash("cargo install hyperfine")
+    }
+
+    fn compare_runs(head: String, base: String) -> Step<Run> {
+        // TODO: this should really be swapped...
+        named::bash(&format!(
+            "cargo perf-compare --save=results.md {base} {head}"
+        ))
+    }
+
     named::job(
         Job::default()
-            .runs_on(runners::LINUX_SMALL)
-            .add_step(steps::checkout_repo()),
+            .runs_on(runners::LINUX_DEFAULT)
+            .add_step(steps::checkout_repo())
+            .add_step(steps::setup_cargo_config(runners::Platform::Linux))
+            .map(steps::install_linux_dependencies)
+            .add_step(install_hyperfine())
+            .add_step(steps::git_checkout(&base.var()))
+            .add_step(cargo_perf_test(base.var()))
+            .add_step(steps::git_checkout(&head.var()))
+            .add_step(cargo_perf_test(head.var()))
+            .add_step(compare_runs(head.var(), base.var()))
+            .add_step(upload_artifact("results.md"))
+            .add_step(steps::cleanup_cargo_config(runners::Platform::Linux)),
     )
 }
