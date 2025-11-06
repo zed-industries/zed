e8b7b1be537b7fab1d0cb864f0457f14b081c7d9
Jakub Konka <kubkon@jakubkonka.com>
2025-11-06 23:38:58 +0100

    git: Merge pending ops snapshots in tests

--- crates/project/src/project_tests.rs
+++ crates/project/src/project_tests.rs
@@ -50,6 +50,7 @@ use std::{
     sync::{Arc, OnceLock},
     task::Poll,
 };
+use sum_tree::SumTree;
 use task::{ResolvedTask, ShellKind, TaskContext};
 use unindent::Unindent as _;
 use util::{
@@ -8369,6 +8370,47 @@ async fn test_git_status_postprocessing(cx: &mut gpui::TestAppContext) {
     });
 }
 
+#[track_caller]
+/// We merge lhs into rhs.
+fn merge_pending_ops_snapshots<'a>(
+    source: Vec<pending_op::PendingOps>,
+    mut target: Vec<pending_op::PendingOps>,
+) -> Vec<pending_op::PendingOps> {
+    for s_ops in source {
+        if let Some(idx) = target.iter().zip(0..).find_map(|(ops, idx)| {
+            if ops.repo_path == s_ops.repo_path {
+                Some(idx)
+            } else {
+                None
+            }
+        }) {
+            let t_ops = &mut target[idx];
+            for s_op in s_ops.ops {
+                if let Some(op_idx) = t_ops
+                    .ops
+                    .iter()
+                    .zip(0..)
+                    .find_map(|(op, idx)| if op.id == s_op.id { Some(idx) } else { None })
+                {
+                    let t_op = &mut t_ops.ops[op_idx];
+                    match (s_op.job_status, t_op.job_status) {
+                        (pending_op::JobStatus::Running, _) => {}
+                        (s_st, pending_op::JobStatus::Running) => t_op.job_status = s_st,
+                        (s_st, t_st) if s_st == t_st => {}
+                        _ => unreachable!(),
+                    }
+                } else {
+                    t_ops.ops.push(s_op);
+                }
+            }
+            t_ops.ops.sort_by(|l, r| l.id.cmp(&r.id));
+        } else {
+            target.push(s_ops);
+        }
+    }
+    target
+}
+
 #[gpui::test]
 async fn test_repository_pending_ops_staging(
     executor: gpui::BackgroundExecutor,
@@ -8395,10 +8437,28 @@ async fn test_repository_pending_ops_staging(
     );
 
     let project = Project::test(fs.clone(), [path!("/root/my-repo").as_ref()], cx).await;
+    let pending_ops_all = Arc::new(Mutex::new(SumTree::default()));
+    project.update(cx, |project, cx| {
+        let pending_ops_all = pending_ops_all.clone();
+        cx.subscribe(project.git_store(), move |_, _, e, _| {
+            if let GitStoreEvent::RepositoryUpdated(
+                _,
+                RepositoryEvent::PendingOpsChanged { pending_ops },
+                _,
+            ) = e
+            {
+                let merged = merge_pending_ops_snapshots(
+                    pending_ops.items(()),
+                    pending_ops_all.lock().items(()),
+                );
+                *pending_ops_all.lock() = SumTree::from_iter(merged.into_iter(), ());
+            }
+        })
+        .detach();
+    });
     project
         .update(cx, |project, cx| project.git_scans_complete(cx))
         .await;
-    cx.run_until_parked();
 
     let repo = project.read_with(cx, |project, cx| {
         project.repositories(cx).values().next().unwrap().clone()
@@ -8457,6 +8517,43 @@ async fn test_repository_pending_ops_staging(
     assert_stage(repo_path("a.txt"), true).await;
     assert_stage(repo_path("a.txt"), false).await;
     assert_stage(repo_path("a.txt"), true).await;
+
+    cx.run_until_parked();
+
+    assert_eq!(
+        pending_ops_all
+            .lock()
+            .get(&worktree::PathKey(repo_path("a.txt").0), ())
+            .unwrap()
+            .ops,
+        vec![
+            pending_op::PendingOp {
+                id: 1u16.into(),
+                git_status: pending_op::GitStatus::Staged,
+                job_status: pending_op::JobStatus::Finished
+            },
+            pending_op::PendingOp {
+                id: 2u16.into(),
+                git_status: pending_op::GitStatus::Unstaged,
+                job_status: pending_op::JobStatus::Finished
+            },
+            pending_op::PendingOp {
+                id: 3u16.into(),
+                git_status: pending_op::GitStatus::Staged,
+                job_status: pending_op::JobStatus::Finished
+            },
+            pending_op::PendingOp {
+                id: 4u16.into(),
+                git_status: pending_op::GitStatus::Unstaged,
+                job_status: pending_op::JobStatus::Finished
+            },
+            pending_op::PendingOp {
+                id: 5u16.into(),
+                git_status: pending_op::GitStatus::Staged,
+                job_status: pending_op::JobStatus::Finished
+            }
+        ],
+    );
 }
 
 #[gpui::test]
@@ -8485,7 +8582,7 @@ async fn test_repository_pending_ops_long_running_staging(
     );
 
     let project = Project::test(fs.clone(), [path!("/root/my-repo").as_ref()], cx).await;
-    let pending_ops_all = Arc::new(Mutex::new(sum_tree::SumTree::default()));
+    let pending_ops_all = Arc::new(Mutex::new(SumTree::default()));
     project.update(cx, |project, cx| {
         let pending_ops_all = pending_ops_all.clone();
         cx.subscribe(project.git_store(), move |_, _, e, _| {
@@ -8495,11 +8592,11 @@ async fn test_repository_pending_ops_long_running_staging(
                 _,
             ) = e
             {
-                let edits = pending_ops
-                    .iter()
-                    .map(|ops| sum_tree::Edit::Insert(ops.clone()))
-                    .collect();
-                pending_ops_all.lock().edit(edits, ());
+                let merged = merge_pending_ops_snapshots(
+                    pending_ops.items(()),
+                    pending_ops_all.lock().items(()),
+                );
+                *pending_ops_all.lock() = SumTree::from_iter(merged.into_iter(), ());
             }
         })
         .detach();
