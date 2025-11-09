505a346cc9eb116b6cc58d78747d029f31cf111c
Ole Jørgen Brønner <olejorgenb@gmail.com>
2025-11-09 19:42:51 +0100

    Fix editor:ExpandExcerpts when cursor is the start of an excerpt
    When the cursor is at the start of the excerpt, `range` will be `(x, x)` where
    `x` is also the excerpt's start position.
    
    So `region.range.start >= range.end` becomes true in the first iteration and no
    excerpts are returned.
    
    I assume this is an equivalent edge-case to the one fixed/supported in the very
    similar `buffer_ids_for_range` function in commit
    1c4c568068b067f08b03e6cdf53e80d86fa35966.

--- crates/multi_buffer/src/multi_buffer.rs
+++ crates/multi_buffer/src/multi_buffer.rs
@@ -3625,7 +3625,9 @@ impl MultiBufferSnapshot {
         cursor.seek(&range.start);
         std::iter::from_fn(move || {
             let region = cursor.region()?;
-            if region.range.start >= range.end {
+            if region.range.start > range.end
+                || region.range.start == range.end && region.range.start > range.start
+            {
                 return None;
             }
             cursor.next_excerpt();
