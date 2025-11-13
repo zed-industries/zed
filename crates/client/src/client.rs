f48fe4827f171ea81dfd74f0201b164b3a5d7502
Marshall Bowers <git@maxdeviant.com>
2025-11-13 17:51:17 -0500

    Remove line

--- crates/client/src/client.rs
+++ crates/client/src/client.rs
@@ -1518,7 +1518,6 @@ impl Client {
     pub async fn sign_out(self: &Arc<Self>, cx: &AsyncApp) {
         self.state.write().credentials = None;
         self.cloud_client.clear_credentials();
-
         self.disconnect(cx);
 
         if self.has_credentials(cx).await {
