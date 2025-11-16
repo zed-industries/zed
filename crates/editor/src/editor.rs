48c5e372c97176b49fe2a454dfe67c12ca929bc5
Samuel Oldham <so9010sami@gmail.com>
2025-11-16 17:07:04 +0000

    Fmt

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -19491,14 +19491,21 @@ impl Editor {
         cx.notify();
     }
 
-    pub fn toggle_syntax_highlight(&mut self, _: &ToggleSyntaxHighlight, _: &mut Window, cx: &mut Context<Self>) {
+    pub fn toggle_syntax_highlight(
+        &mut self,
+        _: &ToggleSyntaxHighlight,
+        _: &mut Window,
+        cx: &mut Context<Self>,
+    ) {
         let Some(workspace) = self.workspace() else {
             return;
         };
         let fs = workspace.read(cx).app_state().fs.clone();
-        let current_syntax_highlight = crate::language_settings::language_settings(None, None, cx).syntax_highlight;
+        let current_syntax_highlight =
+            crate::language_settings::language_settings(None, None, cx).syntax_highlight;
         update_settings_file(fs, cx, move |setting, _| {
-            setting.project.all_languages.defaults.highlight_syntax = Some(!current_syntax_highlight);
+            setting.project.all_languages.defaults.highlight_syntax =
+                Some(!current_syntax_highlight);
         });
     }
 
