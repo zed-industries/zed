c515eb430ee759a83254d0070c9443b6f9075543
MrSubidubi <finn@zed.dev>
2025-11-20 13:28:34 +0100

    Add comment

--- crates/languages/src/diff/highlights.scm
+++ crates/languages/src/diff/highlights.scm
@@ -4,11 +4,13 @@
   (addition)
   (new_file)
 ] @string
+;; TODO: This should eventually be `@diff.plus` with a fallback of `@string`
 
 [
   (deletion)
   (old_file)
 ] @keyword
+;; TODO: This should eventually be `@diff.minus` with a fallback of `@keyword`
 
 (commit) @constant
 
