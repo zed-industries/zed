66382acd52905527e38d7d15d04018febc960d53
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2025-11-20 01:52:13 -0300

    ui: Remove outdated/unused component stories (#43118)
    This PR removes basically all of the component stories, with the
    exception of the context menu, which is a bit more intricate to set up.
    All of the component that won't have a story after this PR will have an
    entry in the Component Preview, which serves basically the same purpose.
    
    Release Notes:
    
    - N/A

--- crates/ui/src/components/stories.rs
+++ crates/ui/src/components/stories.rs
@@ -1,17 +1,3 @@
 mod context_menu;
-mod icon_button;
-mod keybinding;
-mod list;
-mod list_header;
-mod list_item;
-mod tab;
-mod tab_bar;
 
 pub use context_menu::*;
-pub use icon_button::*;
-pub use keybinding::*;
-pub use list::*;
-pub use list_header::*;
-pub use list_item::*;
-pub use tab::*;
-pub use tab_bar::*;
