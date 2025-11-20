ba596267d8a234c42d73cef52b748ed4cd3720d7
Danilo Leal <67129314+danilo-leal@users.noreply.github.com>
2025-11-20 01:28:25 -0300

    ui: Remove the `ToggleButton` component (#43115)
    This PR removes the old `ToggleButton` component, replacing it with the
    newer `ToggleButtonGroup` component in the couple of places that used to
    use it. Ended up also adding a few more methods to the newer toggle
    button group so the UI for the extensions page and the debugger main
    picker didn't get visually impacted much. Then, as I was already in the
    extensions page, decided to bake in some reasonably small UI
    improvements to it as well.
    
    Release Notes:
    
    - N/A

--- crates/ui/src/components/stories.rs
+++ crates/ui/src/components/stories.rs
@@ -6,7 +6,6 @@ mod list_header;
 mod list_item;
 mod tab;
 mod tab_bar;
-mod toggle_button;
 
 pub use context_menu::*;
 pub use icon_button::*;
@@ -16,4 +15,3 @@ pub use list_header::*;
 pub use list_item::*;
 pub use tab::*;
 pub use tab_bar::*;
-pub use toggle_button::*;
