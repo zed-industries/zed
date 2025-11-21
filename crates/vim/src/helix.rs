1a415cbae7a906a6b9ba6225e802797f696dfa6f
AidanV <aidanvanduyne@gmail.com>
2025-11-21 12:00:39 -0800

    helix: Test full cursor selection

--- crates/vim/src/helix.rs
+++ crates/vim/src/helix.rs
@@ -1453,6 +1453,16 @@ mod test {
     }
 
     #[gpui::test]
+    async fn test_helix_full_cursor_selection(cx: &mut gpui::TestAppContext) {
+        let mut cx = VimTestContext::new(cx, true).await;
+        cx.enable_helix();
+
+        cx.set_state("ˇone two three", Mode::HelixNormal);
+        cx.simulate_keystrokes("l l v h h h");
+        cx.assert_state("«ˇone» two three", Mode::HelixSelect);
+    }
+
+    #[gpui::test]
     async fn test_helix_select_regex(cx: &mut gpui::TestAppContext) {
         let mut cx = VimTestContext::new(cx, true).await;
         cx.enable_helix();
