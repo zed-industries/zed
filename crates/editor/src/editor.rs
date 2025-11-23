2ae527694cfca227930b368ed1cff7e4d23de9f4
Mayank Verma <errmayank@gmail.com>
2025-11-23 22:20:49 +0530

    Remove naming of unused

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -20230,25 +20230,19 @@ impl Editor {
         _: &mut Window,
         cx: &mut Context<Self>,
     ) {
-        if let Some(file_stem) =
-            self.active_excerpt(cx)
-                .and_then(|(_excerpt_id, buffer, _range)| {
-                    let file = buffer.read(cx).file()?;
-                    file.path().file_stem()
-                })
-        {
+        if let Some(file_stem) = self.active_excerpt(cx).and_then(|(_, buffer, _)| {
+            let file = buffer.read(cx).file()?;
+            file.path().file_stem()
+        }) {
             cx.write_to_clipboard(ClipboardItem::new_string(file_stem.to_string()));
         }
     }
 
     pub fn copy_file_name(&mut self, _: &CopyFileName, _: &mut Window, cx: &mut Context<Self>) {
-        if let Some(file_name) =
-            self.active_excerpt(cx)
-                .and_then(|(_excerpt_id, buffer, _range)| {
-                    let file = buffer.read(cx).file()?;
-                    Some(file.file_name(cx))
-                })
-        {
+        if let Some(file_name) = self.active_excerpt(cx).and_then(|(_, buffer, _)| {
+            let file = buffer.read(cx).file()?;
+            Some(file.file_name(cx))
+        }) {
             cx.write_to_clipboard(ClipboardItem::new_string(file_name.to_string()));
         }
     }
@@ -20523,16 +20517,13 @@ impl Editor {
             .start
             .row
             + 1;
-        if let Some(file_location) =
-            self.active_excerpt(cx)
-                .and_then(|(_excerpt_id, buffer, _range)| {
-                    let project = self.project()?.read(cx);
-                    let file = buffer.read(cx).file()?;
-                    let path = file.path().display(project.path_style(cx));
+        if let Some(file_location) = self.active_excerpt(cx).and_then(|(_, buffer, _)| {
+            let project = self.project()?.read(cx);
+            let file = buffer.read(cx).file()?;
+            let path = file.path().display(project.path_style(cx));
 
-                    Some(format!("{path}:{selection}"))
-                })
-        {
+            Some(format!("{path}:{selection}"))
+        }) {
             cx.write_to_clipboard(ClipboardItem::new_string(file_location));
         }
     }
