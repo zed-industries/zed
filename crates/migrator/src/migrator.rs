fa93ec7bb85e7ec346d9ca19915748e30a802f5b
Bennet Bo Fenner <bennetbo@gmx.de>
2025-11-25 16:30:41 +0100

    Add migration to remove "source" key

--- crates/migrator/src/migrator.rs
+++ crates/migrator/src/migrator.rs
@@ -223,6 +223,7 @@ pub fn migrate_settings(text: &str) -> Result<Option<String>> {
             migrations::m_2025_11_20::SETTINGS_PATTERNS,
             &SETTINGS_QUERY_2025_11_20,
         ),
+        MigrationType::Json(migrations::m_2025_11_25::remove_context_server_source),
     ];
     run_migrations(text, migrations)
 }
@@ -2304,6 +2305,54 @@ mod tests {
                 {
                     "project_panel": {
                         "auto_open": { "on_paste": false }
+                    }
+                }
+                "#
+                .unindent(),
+            ),
+        );
+    }
+
+    #[test]
+    fn test_remove_context_server_source() {
+        assert_migrate_settings(
+            &r#"
+            {
+                "context_servers": {
+                    "extension_server": {
+                        "source": "extension",
+                        "settings": {
+                            "foo": "bar"
+                        }
+                    },
+                    "custom_server": {
+                        "source": "custom",
+                        "command": "foo",
+                        "args": ["bar"],
+                        "env": {
+                            "FOO": "BAR"
+                        }
+                    },
+                }
+            }
+            "#
+            .unindent(),
+            Some(
+                &r#"
+                {
+                    "context_servers": {
+                        "extension_server": {
+                            "settings": {
+                                "foo": "bar"
+                            }
+                        },
+                        "custom_server": {
+                            "command": "foo",
+                            "args": ["bar"],
+                            "env": {
+                                "FOO": "BAR"
+                            }
+                        },
                     }
                 }
                 "#
