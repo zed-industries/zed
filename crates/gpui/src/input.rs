96cbe7e6f057a8fd68a1479377a107f63a3246ee
iamnbutler <iamnbutler@github.com>
2025-11-26 19:14:41 -0500

    Update input.rs

--- crates/gpui/src/input.rs
+++ crates/gpui/src/input.rs
@@ -1,9 +1,9 @@
 mod input_handler;
 mod input_state;
 
+pub use input_handler::*;
 pub use input_state::{InputLineLayout, InputState};
 // todo: move to keymap
-pub use input_handler::*;
 pub use input_state::{
     Backspace, Copy, Cut, Delete, Down, End, Enter, Home, Left, MoveToBeginning, MoveToEnd, Paste,
     Right, SelectAll, SelectDown, SelectLeft, SelectRight, SelectToBeginning, SelectToEnd,
