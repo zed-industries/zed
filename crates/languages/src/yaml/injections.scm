84d392c1a003faff1f0e96f078d6b04f2b956fc0
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-11-29 03:33:31 +0100

    language(yaml): add injections to highlight script blocks of `actions/github-script` as JavaScript

--- crates/languages/src/yaml/injections.scm
+++ crates/languages/src/yaml/injections.scm
@@ -1,3 +1,44 @@
 ((comment) @injection.content
  (#set! injection.language "comment")
 )
+
+; GitHub actions: inline JavaScript for workflow scripting
+(block_mapping
+  (block_mapping_pair
+    key: (flow_node) @_uses (#eq? @_uses "uses")
+    value: (flow_node) @_actions_ghs (#match? @_actions_ghs "^actions/github-script"))
+  (block_mapping_pair
+    key: (flow_node) @_with (#eq? @_with "with")
+    value: (block_node
+      (block_mapping
+        (block_mapping_pair
+          key: (flow_node) @_run (#eq? @_run "script")
+          value: (flow_node
+            (plain_scalar
+              (string_scalar) @injection.content
+              (#set! injection.language "javascript")))
+        )
+      )
+    )
+  )
+)
+
+; GitHub actions: block JavaScript for workflow scripting
+(block_mapping
+  (block_mapping_pair
+    key: (flow_node) @_uses (#eq? @_uses "uses")
+    value: (flow_node) @_actions_ghs (#match? @_actions_ghs "^actions/github-script"))
+  (block_mapping_pair
+    key: (flow_node) @_with (#any-of? @_with "with")
+    value: (block_node
+      (block_mapping
+        (block_mapping_pair
+          key: (flow_node) @_run (#any-of? @_run "script")
+          value: (block_node
+            (block_scalar) @injection.content
+            (#set! injection.language "javascript"))
+        )
+      )
+    )
+  )
+)
