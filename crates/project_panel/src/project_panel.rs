5b47ea3bdb7fd3abdabfd96601e6dba1c29db370
AdamJedl <jedlad@delta-studenti.cz>
2025-11-29 15:33:29 +0100

    change "Delete" to "Delete Permanently"

--- crates/project_panel/src/project_panel.rs
+++ crates/project_panel/src/project_panel.rs
@@ -1067,7 +1067,10 @@ impl ProjectPanel {
                                 menu.action("Trash", Box::new(Trash { skip_prompt: false }))
                             })
                             .when(!is_root, |menu| {
-                                menu.action("Delete", Box::new(Delete { skip_prompt: false }))
+                                menu.action(
+                                    "Delete Permanently",
+                                    Box::new(Delete { skip_prompt: false }),
+                                )
                             })
                             .when(!is_remote && is_root, |menu| {
                                 menu.separator()
@@ -1979,7 +1982,7 @@ impl ProjectPanel {
                 return None;
             }
             let answer = if !skip_prompt {
-                let operation = if trash { "Trash" } else { "Delete" };
+                let operation = if trash { "Trash" } else { "Permanently Delete" };
                 let prompt = match file_paths.first() {
                     Some((_, path)) if file_paths.len() == 1 => {
                         let unsaved_warning = if dirty_buffers > 0 {
@@ -2027,7 +2030,18 @@ impl ProjectPanel {
                         )
                     }
                 };
-                Some(window.prompt(PromptLevel::Info, &prompt, None, &[operation, "Cancel"], cx))
+                let message = if trash {
+                    None
+                } else {
+                    Some("This cannot be undone!")
+                };
+                Some(window.prompt(
+                    PromptLevel::Info,
+                    &prompt,
+                    message,
+                    &[operation, "Cancel"],
+                    cx,
+                ))
             } else {
                 None
             };
