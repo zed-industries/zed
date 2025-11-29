66dabf40129c432dd66877e977a5c12521e0458c
Aero <aero.windwalker@gmail.com>
2025-11-29 21:46:00 +0800

    agent_ui: Fix delete icon event in history panel
    - Stop event propagation in thread removal handler

--- crates/agent_ui/src/acp/thread_history.rs
+++ crates/agent_ui/src/acp/thread_history.rs
@@ -446,9 +446,10 @@ impl AcpThreadHistory {
                                 .tooltip(move |_window, cx| {
                                     Tooltip::for_action("Delete", &RemoveSelectedThread, cx)
                                 })
-                                .on_click(
-                                    cx.listener(move |this, _, _, cx| this.remove_thread(ix, cx)),
-                                ),
+                                .on_click(cx.listener(move |this, _, _, cx| {
+                                    this.remove_thread(ix, cx);
+                                    cx.stop_propagation()
+                                })),
                         )
                     } else {
                         None
