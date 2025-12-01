8fc7102e48ef7e011fb45752e777d933fb57138e
feeiyu <feiyu0923@foxmail.com>
2025-12-01 23:42:59 +0800

    Fix circular reference issue around PopoverMenu again

--- crates/language_tools/src/lsp_log_view.rs
+++ crates/language_tools/src/lsp_log_view.rs
@@ -937,7 +937,7 @@ impl Render for LspLogToolbarItemView {
             })
             .collect();
 
-        let log_toolbar_view = cx.entity();
+        let log_toolbar_view = cx.weak_entity();
 
         let lsp_menu = PopoverMenu::new("LspLogView")
             .anchor(Corner::TopLeft)
@@ -1021,7 +1021,7 @@ impl Render for LspLogToolbarItemView {
                         .icon_color(Color::Muted),
                 )
                 .menu(move |window, cx| {
-                    let log_toolbar_view = log_toolbar_view.clone();
+                    let log_toolbar_view = log_toolbar_view.upgrade()?;
                     let log_view = log_view.clone();
                     Some(ContextMenu::build(window, cx, move |this, window, _| {
                         this.entry(
