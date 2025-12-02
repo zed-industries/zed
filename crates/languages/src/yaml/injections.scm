706a0e14882880cc09f0851417f4d44cf10bd0cd
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-12-02 16:25:31 +0100

    merge queries with an alternation

--- crates/languages/src/yaml/injections.scm
+++ crates/languages/src/yaml/injections.scm
@@ -2,7 +2,7 @@
  (#set! injection.language "comment")
 )
 
-; GitHub actions: inline JavaScript for workflow scripting
+; GitHub actions: JavaScript for workflow scripting (inline and block)
 (block_mapping
   (block_mapping_pair
     key: (flow_node) @_uses (#eq? @_uses "uses")
@@ -13,30 +13,11 @@
       (block_mapping
         (block_mapping_pair
           key: (flow_node) @_run (#eq? @_run "script")
-          value: (flow_node
-            (plain_scalar
-              (string_scalar) @injection.content
-              (#set! injection.language "javascript")))
-        )
-      )
-    )
-  )
-)
-
-; GitHub actions: block JavaScript for workflow scripting
-(block_mapping
-  (block_mapping_pair
-    key: (flow_node) @_uses (#eq? @_uses "uses")
-    value: (flow_node) @_actions_ghs (#match? @_actions_ghs "^actions/github-script"))
-  (block_mapping_pair
-    key: (flow_node) @_with (#any-of? @_with "with")
-    value: (block_node
-      (block_mapping
-        (block_mapping_pair
-          key: (flow_node) @_run (#any-of? @_run "script")
-          value: (block_node
-            (block_scalar) @injection.content
-            (#set! injection.language "javascript"))
+          value: [
+            (flow_node (plain_scalar (string_scalar) @injection.content))
+            (block_node (block_scalar) @injection.content)
+          ]
+          (#set! injection.language "javascript")
         )
       )
     )
