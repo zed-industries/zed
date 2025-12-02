a74aac88c95738b1e1c95ac583b116a253920fbf
Bhuminjay Soni <bhuminjaysoni@gmail.com>
2025-12-02 16:48:13 +0530

    Increase askpass timeout for git operations (#42946)
    Closes #29903
    
    Release Notes:
    
    - Increased timeout from 17->300 & improved the error message.
    
    ---------
    
    Signed-off-by: 11happy <soni5happy@gmail.com>
    Signed-off-by: 11happy <bhuminjaysoni@gmail.com>

--- crates/git/src/git.rs
+++ crates/git/src/git.rs
@@ -232,12 +232,14 @@ impl From<Oid> for usize {
 #[derive(Copy, Clone, Debug)]
 pub enum RunHook {
     PreCommit,
+    PrePush,
 }
 
 impl RunHook {
     pub fn as_str(&self) -> &str {
         match self {
             Self::PreCommit => "pre-commit",
+            Self::PrePush => "pre-push",
         }
     }
 
@@ -248,6 +250,7 @@ impl RunHook {
     pub fn from_proto(value: i32) -> Option<Self> {
         match value {
             0 => Some(Self::PreCommit),
+            1 => Some(Self::PrePush),
             _ => None,
         }
     }
