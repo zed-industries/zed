9ec79fa4cbd4088c45b61ce5cdaa502d276f15da
Mikayla Maki <mikayla.c.maki@gmail.com>
2025-12-03 12:19:27 -0800

    fix

--- crates/gpui/src/app.rs
+++ crates/gpui/src/app.rs
@@ -552,19 +552,25 @@ impl SystemWindowTabController {
 }
 
 pub(crate) enum GpuiMode {
-    Test { skip_drawing: bool },
+    #[cfg(any(test, feature = "test-support"))]
+    Test {
+        skip_drawing: bool,
+    },
     Production,
 }
 
 impl GpuiMode {
+    #[cfg(any(test, feature = "test-support"))]
     pub fn test() -> Self {
         GpuiMode::Test {
             skip_drawing: false,
         }
     }
 
+    #[inline]
     pub(crate) fn skip_drawing(&self) -> bool {
         match self {
+            #[cfg(any(test, feature = "test-support"))]
             GpuiMode::Test { skip_drawing } => *skip_drawing,
             GpuiMode::Production => false,
         }
