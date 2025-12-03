2b6ebc4eb2ac8601553d68ed0658b377f52ceab6
Max Brunsfeld <maxbrunsfeld@gmail.com>
2025-12-02 17:12:50 -0800

    Improve assemble excerpts - include child outline items sometimes
    Co-authored-by: Agus Zubiaga <agus@zed.dev>

--- crates/edit_prediction_context2/src/assemble_excerpts.rs
+++ crates/edit_prediction_context2/src/assemble_excerpts.rs
@@ -2,7 +2,10 @@ use crate::RelatedExcerpt;
 use language::{BufferSnapshot, OffsetRangeExt as _, Point};
 use std::ops::Range;
 
+#[cfg(not(test))]
 const MAX_OUTLINE_ITEM_BODY_SIZE: usize = 512;
+#[cfg(test)]
+const MAX_OUTLINE_ITEM_BODY_SIZE: usize = 24;
 
 pub fn assemble_excerpts(
     buffer: &BufferSnapshot,
@@ -12,48 +15,59 @@ pub fn assemble_excerpts(
 
     let mut outline_ranges = Vec::new();
     let outline_items = buffer.outline_items_as_points_containing(0..buffer.len(), false, None);
-    let mut outline_items = outline_items.into_iter().peekable();
-    for input_range in &input_ranges {
-        while let Some(mut outline_item) = outline_items.peek().cloned() {
-            if outline_item.range.start >= input_range.start {
+    let mut outline_ix = 0;
+    for input_range in &mut input_ranges {
+        *input_range = clip_range_to_lines(input_range, false, buffer);
+
+        while let Some(outline_item) = outline_items.get(outline_ix) {
+            let item_range = clip_range_to_lines(&outline_item.range, false, buffer);
+
+            if item_range.start > input_range.start {
                 break;
             }
 
-            outline_item.range.start.column = 0;
-            if outline_item.range.end.column > 0 {
-                outline_item.range.end.column = buffer.line_len(outline_item.range.end.row);
-            }
+            if item_range.end > input_range.start {
+                let body_range = outline_item
+                    .body_range(buffer)
+                    .map(|body| clip_range_to_lines(&body, true, buffer))
+                    .filter(|body_range| {
+                        body_range.to_offset(buffer).len() > MAX_OUTLINE_ITEM_BODY_SIZE
+                    });
 
-            if outline_item.range.end > input_range.start {
-                if let Some(mut body_range) = outline_item.body_range.clone()
-                    && body_range.to_offset(buffer).len() > MAX_OUTLINE_ITEM_BODY_SIZE
+                add_outline_item(
+                    item_range.clone(),
+                    body_range.clone(),
+                    buffer,
+                    &mut outline_ranges,
+                );
+
+                if let Some(body_range) = body_range
+                    && input_range.start < body_range.start
                 {
-                    if body_range.start.column > 0 {
-                        body_range.start.column = buffer.line_len(body_range.start.row);
-                    }
-                    body_range.end.column = 0;
+                    let mut child_outline_ix = outline_ix + 1;
+                    while let Some(next_outline_item) = outline_items.get(child_outline_ix) {
+                        if next_outline_item.range.end > body_range.end {
+                            break;
+                        }
+                        if next_outline_item.depth == outline_item.depth + 1 {
+                            let next_item_range =
+                                clip_range_to_lines(&next_outline_item.range, false, buffer);
 
-                    let head_range = outline_item.range.start..body_range.start;
-                    if head_range.start < head_range.end {
-                        outline_ranges.push(head_range);
+                            add_outline_item(
+                                next_item_range,
+                                next_outline_item
+                                    .body_range(buffer)
+                                    .map(|body| clip_range_to_lines(&body, true, buffer)),
+                                buffer,
+                                &mut outline_ranges,
+                            );
+                            child_outline_ix += 1;
+                        }
                     }
-
-                    let tail_range = body_range.end..outline_item.range.end;
-                    if tail_range.start < tail_range.end {
-                        outline_ranges.push(tail_range);
-                    }
-
-                    if input_range.start < body_range.start {
-                        outline_items.next();
-                    }
-                } else {
-                    let mut item_range = outline_item.range.clone();
-                    item_range.start.column = 0;
-                    item_range.end.column = buffer.line_len(item_range.end.row);
-                    outline_ranges.push(item_range);
                 }
             }
-            outline_items.next();
+
+            outline_ix += 1;
         }
     }
 
@@ -72,6 +86,55 @@ pub fn assemble_excerpts(
             }
         })
         .collect()
+}
+
+fn clip_range_to_lines(
+    range: &Range<Point>,
+    inward: bool,
+    buffer: &BufferSnapshot,
+) -> Range<Point> {
+    let mut range = range.clone();
+    if inward {
+        if range.start.column > 0 {
+            range.start.column = buffer.line_len(range.start.row);
+        }
+        range.end.column = 0;
+    } else {
+        range.start.column = 0;
+        if range.end.column > 0 {
+            range.end.column = buffer.line_len(range.end.row);
+        }
+    }
+    range
+}
+
+fn add_outline_item(
+    item_range: Range<Point>,
+    body_range: Option<Range<Point>>,
+    buffer: &BufferSnapshot,
+    outline_ranges: &mut Vec<Range<Point>>,
+) {
+    if let Some(mut body_range) = body_range {
+        if body_range.start.column > 0 {
+            body_range.start.column = buffer.line_len(body_range.start.row);
+        }
+        body_range.end.column = 0;
+
+        let head_range = item_range.start..body_range.start;
+        if head_range.start < head_range.end {
+            outline_ranges.push(head_range);
+        }
+
+        let tail_range = body_range.end..item_range.end;
+        if tail_range.start < tail_range.end {
+            outline_ranges.push(tail_range);
+        }
+    } else {
+        let mut item_range = item_range.clone();
+        item_range.start.column = 0;
+        item_range.end.column = buffer.line_len(item_range.end.row);
+        outline_ranges.push(item_range);
+    }
 }
 
 pub fn merge_ranges(ranges: &mut Vec<Range<Point>>) {
@@ -135,9 +198,7 @@ mod tests {
                     struct User {
                         first_name: String,
                         last_name: String,
-                        age: u32,
-                        email: String,
-                        create_at: Instant,
+                    …
                     }
 
                     impl User {
@@ -168,6 +229,37 @@ mod tests {
                         age: u32,
                     }
                     …
+                "#},
+            ),
+            (
+                indoc! {r#"
+                    trait «FooProvider» {
+                        const NAME: &'static str;
+
+                        fn provide_foo(&self, id: usize) -> Foo;
+
+                        fn provide_foo_batched(&self, ids: &[usize]) -> Vec<Foo> {
+                             ids.iter()
+                                .map(|id| self.provide_foo(*id))
+                                .collect()
+                        }
+
+                        fn sync(&self);
+                    }
+                    "#
+                },
+                indoc! {r#"
+                    trait FooProvider {
+                        const NAME: &'static str;
+
+                        fn provide_foo(&self, id: usize) -> Foo;
+
+                        fn provide_foo_batched(&self, ids: &[usize]) -> Vec<Foo> {
+                    …
+                        }
+
+                        fn sync(&self);
+                    }
                 "#},
             ),
         ];
