6b46a71dd0f0cdd2238e954566a7728d23e3d74a
Dino <dinojoaocosta@gmail.com>
2025-12-03 17:12:04 +0000

    tab_switcher: Fix bug where selected index after closing tab did not match pane's active item (#44006)
    Whenever an item is removed using the Tab Switcher, the list of matches
    is automatically updated, which can lead to the order of the elements
    being updated and changing in comparison to what the user was previously
    seeing. Unfortunately this can lead to a situation where the selected
    index, since it wasn't being updated, would end up in a different item
    than the one that was actually active in the pane.
    
    This Pull Request updates the handling of the `PaneEvent::RemovedItem`
    event so that the `TabSwitcherDelegate.selected_index` field is
    automatically updated to match the pane's new active item.
    
    Seeing as this is being updated, the
    `test_close_preserves_selected_position` test is also removed, as it no
    longer makes sense with the current implementation. I believe a better
    user experience would be to actually not update the order of the
    matches, simply removing the ones that no longer exist, and keep the
    selected index position, but will tackle that in a different Pull
    Request.
    
    Closes #44005 
    
    Release Notes:
    
    - Fixed a bug with the tab switcher where, after closing a tab, the
    selected entry would not match the pane's active item

--- crates/tab_switcher/src/tab_switcher.rs
+++ crates/tab_switcher/src/tab_switcher.rs
@@ -347,11 +347,23 @@ impl TabSwitcherDelegate {
         };
         cx.subscribe_in(&pane, window, |tab_switcher, _, event, window, cx| {
             match event {
-                PaneEvent::AddItem { .. }
-                | PaneEvent::RemovedItem { .. }
-                | PaneEvent::Remove { .. } => tab_switcher.picker.update(cx, |picker, cx| {
+                PaneEvent::AddItem { .. } | PaneEvent::Remove { .. } => {
+                    tab_switcher.picker.update(cx, |picker, cx| {
+                        let query = picker.query(cx);
+                        picker.delegate.update_matches(query, window, cx);
+                        cx.notify();
+                    })
+                }
+                PaneEvent::RemovedItem { .. } => tab_switcher.picker.update(cx, |picker, cx| {
                     let query = picker.query(cx);
                     picker.delegate.update_matches(query, window, cx);
+
+                    // When the Tab Switcher is being used and an item is
+                    // removed, there's a chance that the new selected index
+                    // will not match the actual tab that is now being displayed
+                    // by the pane, as such, the selected index needs to be
+                    // updated to match the pane's state.
+                    picker.delegate.sync_selected_index(cx);
                     cx.notify();
                 }),
                 _ => {}
@@ -540,10 +552,32 @@ impl TabSwitcherDelegate {
         let Some(pane) = tab_match.pane.upgrade() else {
             return;
         };
+
         pane.update(cx, |pane, cx| {
             pane.close_item_by_id(tab_match.item.item_id(), SaveIntent::Close, window, cx)
                 .detach_and_log_err(cx);
         });
+    }
+
+    /// Updates the selected index to ensure it matches the pane's active item,
+    /// as the pane's active item can be indirectly updated and this method
+    /// ensures that the picker can react to those changes.
+    fn sync_selected_index(&mut self, cx: &mut Context<Picker<TabSwitcherDelegate>>) {
+        let Ok(Some(item)) = self.pane.read_with(cx, |pane, _cx| pane.active_item()) else {
+            return;
+        };
+
+        let item_id = item.item_id();
+        let Some((index, _tab_match)) = self
+            .matches
+            .iter()
+            .enumerate()
+            .find(|(_index, tab_match)| tab_match.item.item_id() == item_id)
+        else {
+            return;
+        };
+
+        self.selected_index = index;
     }
 }
 
