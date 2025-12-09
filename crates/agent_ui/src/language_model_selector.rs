d9396373e31cd130b76dc2bdea2edff8eb138495
Richard Feldman <oss@rtfeldman.com>
2025-12-09 11:54:00 -0500

    Eliminate more code duplication

--- crates/agent_ui/src/language_model_selector.rs
+++ crates/agent_ui/src/language_model_selector.rs
@@ -140,13 +140,9 @@ impl LanguageModelPickerDelegate {
                 // Subscribe to registry events and send refresh signals through the channel
                 let registry = LanguageModelRegistry::global(cx);
                 cx.subscribe(&registry, move |_picker, _, event, _cx| match event {
-                    language_model::Event::ProviderStateChanged(_) => {
-                        refresh_tx.unbounded_send(()).ok();
-                    }
-                    language_model::Event::AddedProvider(_) => {
-                        refresh_tx.unbounded_send(()).ok();
-                    }
-                    language_model::Event::RemovedProvider(_) => {
+                    language_model::Event::ProviderStateChanged(_)
+                    | language_model::Event::AddedProvider(_)
+                    | language_model::Event::RemovedProvider(_) => {
                         refresh_tx.unbounded_send(()).ok();
                     }
                     _ => {}
