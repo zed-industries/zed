26069471829e936bf563ea27a2a26710752114ac
Mikayla Maki <mikayla.c.maki@gmail.com>
2025-12-09 21:24:05 -0800

    Add action change

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -4756,6 +4756,7 @@ mod tests {
                 "git_panel",
                 "go_to_line",
                 "icon_theme_selector",
+                "inline_assistant",
                 "journal",
                 "keymap_editor",
                 "keystroke_input",
