a00fe5d922d97c600ab8f32c6bed2fc33e4e492d
cameron <cameron.studdstreet@gmail.com>
2025-12-12 15:21:22 +0000

    add doc comments :)

--- crates/editor/src/display_map/inlay_map.rs
+++ crates/editor/src/display_map/inlay_map.rs
@@ -1,3 +1,12 @@
+//! The inlay map. See the [`display_map`][super] docs for an overview of how the inlay map fits
+//! into the rest of the [`DisplayMap`][super::DisplayMap]. Much of the documentation for this
+//! module generalizes to other layers.
+//!
+//! The core of this module is the [`InlayMap`] struct, which maintains a vec of [`Inlay`]s, and
+//! [`InlaySnapshot`], which holds a sum tree of [`Transform`]s.
+//! 
+//! todo! finish
+
 use crate::{
     ChunkRenderer, HighlightStyles,
     inlays::{Inlay, InlayContent},
@@ -19,6 +28,7 @@ use text::{ChunkBitmaps, Patch};
 use ui::{ActiveTheme, IntoElement as _, ParentElement as _, Styled as _, div};
 
 use super::{Highlights, custom_highlights::CustomHighlightsChunks, fold_map::ChunkRendererId};
+
 
 /// Decides where the [`Inlay`]s should be displayed.
 ///
