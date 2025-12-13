52f461b5b0604c8e51b8432d3275a5d9c30f7a99
Aero <aero.windwalker@gmail.com>
2025-12-13 13:29:57 +0800

    Revert "Better protobuf detection"

--- crates/livekit_api/build.rs
+++ crates/livekit_api/build.rs
@@ -1,17 +1,9 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let protoc_path = which::which("protoc").inspect_err(|e| {
-        eprintln!(
-            "Protoc not found: {}. Please install protoc or ensure it's in your PATH.",
-            e
-        );
-    })?;
-
+fn main() {
     prost_build::Config::new()
         .type_attribute("SendDataResponse", "#[allow(clippy::empty_docs)]")
-        .protoc_arg(format!("--proto_path={}", protoc_path.display()))
         .compile_protos(
             &["vendored/protocol/livekit_room.proto"],
             &["vendored/protocol"],
-        )?; // Use '?' to propagate errors
-    Ok(())
+        )
+        .unwrap();
 }
