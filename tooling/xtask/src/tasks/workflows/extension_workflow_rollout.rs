d9403d009508e44b354241d0617feb459efd9e13
MrSubidubi <finn@zed.dev>
2025-12-23 18:02:09 +0100

    Actionlint

--- tooling/xtask/src/tasks/workflows/extension_workflow_rollout.rs
+++ tooling/xtask/src/tasks/workflows/extension_workflow_rollout.rs
@@ -92,7 +92,7 @@ fn rollout_workflows_to_extension(fetch_repos_job: &NamedJob) -> NamedJob {
 
     fn get_short_sha() -> (Step<Run>, StepOutput) {
         let step = named::bash(indoc! {r#"
-            echo "sha_short=$(git rev-parse --short HEAD)" >> $GITHUB_OUTPUT
+            echo "sha_short=$(git rev-parse --short HEAD)" >> "$GITHUB_OUTPUT"
         "#})
         .id("short-sha")
         .working_directory("zed");
