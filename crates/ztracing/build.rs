bf87da731fdb115e91911fbcb643ad08c4fadb80
Richard Feldman <oss@rtfeldman.com>
2025-12-24 11:50:01 -0500

    Add dev::StartTracing action to launch Tracy profiler
    - Add is_enabled() function to ztracing crate to detect if tracy support is compiled in
    - Add StartTracing action that launches tracy-profiler if found on PATH
    - Show appropriate notifications for success/failure states
    - Show warning with icon when running debug build (profiling results won't be accurate)
    - Simplify ztracing to use just --features tracy instead of requiring ZTRACING env var

--- crates/ztracing/build.rs
+++ crates/ztracing/build.rs
@@ -1,9 +1,3 @@
-use std::env;
-
 fn main() {
-    if env::var_os("ZTRACING").is_some() {
-        println!(r"cargo::rustc-cfg=ztracing");
-    }
     println!("cargo::rerun-if-changed=build.rs");
-    println!("cargo::rerun-if-env-changed=ZTRACING");
 }
