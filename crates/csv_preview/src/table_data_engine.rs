288f7a86f382061a6bba40fb94a6260f08f14a6c
HalavicH <HalavicH@gmail.com>
2025-12-25 14:49:48 +0100

    feat: Start implementing table data engine

---
+++ crates/csv_preview/src/table_data_engine.rs
@@ -0,0 +1,8 @@
+//! This module defines core operations and config of tabular data view (CSV table)
+//! It operates in 2 coordinate systems:
+//! - `DataCellId` - indices of src data cells
+//! - `DisplayCellId` - indices of data after applied transformations like sorting/filtering, which is used to render cell on the screen
+
+pub(crate) struct TableDataEngine {
+    // TODO: Move sorting here
+}
