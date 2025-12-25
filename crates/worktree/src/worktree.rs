7ebfe5da2fc6d32f3fa2d71c761f8b2ec26d945b
Kirill Bulatov <kirill@zed.dev>
2025-12-25 16:32:59 +0200

    Add a better fix

--- crates/worktree/src/worktree.rs
+++ crates/worktree/src/worktree.rs
@@ -3996,6 +3996,10 @@ impl BackgroundScanner {
                     .root_file_handle
                     .clone()
                     .and_then(|handle| handle.current_path(&self.fs).log_err())
+                    // TODO this is a stub: on macOS, when trying to refresh handle's path when it does not exist (e.g. volume unmounted)
+                    // `current_path` will return an empty path and `libc::fcntl` inside won't error.
+                    // Ideally, we need to find a way to make `current_path` to return an error instead.
+                    .filter(|new_path| new_path != Path::new(""))
                     .map(|path| SanitizedPath::new_arc(&path))
                     .filter(|new_path| *new_path != root_path);
 
