950b3bb4d21c720c2bec2360d89ea8111e46c6ee
HalavicH <HalavicH@gmail.com>
2025-12-26 14:40:39 +0100

    feat: Add clear all button

--- crates/csv_preview/src/action_handlers.rs
+++ crates/csv_preview/src/action_handlers.rs
@@ -1,2 +1,18 @@
+use gpui::ListAlignment;
+
+use crate::{CsvPreviewView, types::AnyColumn};
+
 mod copy_selected;
 mod selection_handlers;
+
+impl CsvPreviewView {
+    pub fn clear_filters(&mut self, col_idx: AnyColumn) {
+        self.engine.clear_filters(col_idx);
+        self.engine.calculate_d2d_mapping();
+        self.list_state = gpui::ListState::new(
+            self.engine.d2d_mapping.filtered_row_count(),
+            ListAlignment::Top,
+            ui::px(1.),
+        );
+    }
+}
