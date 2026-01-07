959f933b783407d4722cdea42d5898ef402fddad
Josh Robson Chase <josh@robsonchase.com>
2026-01-07 14:36:47 -0500

    "fix" devshell

--- nix/shell.nix
+++ nix/shell.nix
@@ -16,7 +16,7 @@
   nodejs_22,
   zig,
 }:
-(mkShell.override { inherit (zed-editor) stdenv; }) {
+mkShell {
   inputsFrom = [ zed-editor ];
   packages = [
     rust-analyzer
@@ -35,29 +35,16 @@
     zig
   ];
 
-  env =
-    let
-      baseEnvs =
-        (zed-editor.overrideAttrs (attrs: {
-          passthru = { inherit (attrs) env; };
-        })).env; # exfil `env`; it's not in drvAttrs
-    in
-    (removeAttrs baseEnvs [
-      "LK_CUSTOM_WEBRTC" # download the staticlib during the build as usual
-      "ZED_UPDATE_EXPLANATION" # allow auto-updates
-      "CARGO_PROFILE" # let you specify the profile
-      "TARGET_DIR"
-    ])
-    // {
-      # note: different than `$FONTCONFIG_FILE` in `build.nix` – this refers to relative paths
-      # outside the nix store instead of to `$src`
-      FONTCONFIG_FILE = makeFontsConf {
-        fontDirectories = [
-          "./assets/fonts/lilex"
-          "./assets/fonts/ibm-plex-sans"
-        ];
-      };
-      PROTOC = "${protobuf}/bin/protoc";
-      ZED_ZSTD_MUSL_LIB = "${pkgsCross.musl64.pkgsStatic.zstd.out}/lib";
+  env = {
+    # note: different than `$FONTCONFIG_FILE` in `build.nix` – this refers to relative paths
+    # outside the nix store instead of to `$src`
+    FONTCONFIG_FILE = makeFontsConf {
+      fontDirectories = [
+        "./assets/fonts/lilex"
+        "./assets/fonts/ibm-plex-sans"
+      ];
     };
+    PROTOC = "${protobuf}/bin/protoc";
+    ZED_ZSTD_MUSL_LIB = "${pkgsCross.musl64.pkgsStatic.zstd.out}/lib";
+  };
 }
