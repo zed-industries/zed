df6a3a37c88c6e139d4976c862c7e0a4f8a56c5f
Max Brunsfeld <maxbrunsfeld@gmail.com>
2026-01-07 17:01:39 -0800

    Truncate logs to avoid wrapping, re-draw status lines after logging

--- crates/edit_prediction_cli/src/main.rs
+++ crates/edit_prediction_cli/src/main.rs
@@ -601,22 +601,13 @@ async fn handle_error(
 
     let msg = format!(
         indoc::indoc! {"
-            While processing \"{}\":
-
-            {:?}
-
-            Written to: \x1b[36m{}\x1b[0m
-
-            Cursor File: \x1b[36m{}\x1b[0m
-
-            Explore this example data with:
-            fx \x1b[36m{}\x1b[0m
-
-            Re-run this example with:
-            cargo run -p edit_prediction_cli -- {} \x1b[36m{}\x1b[0m
+            While processing \"{}\".
+                Error file: \x1b[36m{}\x1b[0m.
+                Cursor File: \x1b[36m{}\x1b[0m
+                Explore this example data with: fx \x1b[36m{}\x1b[0m
+                Re-run this example with: cargo run -p edit_prediction_cli -- {} \x1b[36m{}\x1b[0m
         "},
         example.spec.name,
-        error,
         err_path.display(),
         file_path.display(),
         failed_example_path.display(),
