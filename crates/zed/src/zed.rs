b0f4f11ea2bb1092da59f992be2ab6f4a3540367
Danilo Leal <daniloleal09@gmail.com>
2026-01-12 20:26:47 -0300

    Fix broken action namespaces test

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -4749,6 +4749,7 @@ mod tests {
                 "lsp_tool",
                 "markdown",
                 "menu",
+                "new_process_modal",
                 "notebook",
                 "notification_panel",
                 "onboarding",
