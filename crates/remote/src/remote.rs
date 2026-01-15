33a6efb31aedfc7ab3553017b8532a1b4479946a
Max Malkin <maxim_malkin@outlook.com>
2026-01-15 14:31:46 -0700

    add path conversion fn to remote

--- crates/remote/src/remote.rs
+++ crates/remote/src/remote.rs
@@ -14,3 +14,5 @@ pub use remote_client::{
 pub use transport::docker::DockerConnectionOptions;
 pub use transport::ssh::{SshConnectionOptions, SshPortForwardOption};
 pub use transport::wsl::WslConnectionOptions;
+#[cfg(target_os = "windows")]
+pub use transport::wsl::wsl_path_to_windows_path;
