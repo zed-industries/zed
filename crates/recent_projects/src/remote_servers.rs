1114f636308b831f150d976ae85181cf3efc7066
pigletfly <wangbing.adam@gmail.com>
2026-01-18 18:29:17 +0800

    Fix zed cli column index broken on non ascii
    Signed-off-by: pigletfly <wangbing.adam@gmail.com>

--- crates/recent_projects/src/remote_servers.rs
+++ crates/recent_projects/src/remote_servers.rs
@@ -14,7 +14,6 @@ use gpui::{
     FocusHandle, Focusable, PromptLevel, ScrollHandle, Subscription, Task, WeakEntity, Window,
     canvas,
 };
-use language::Point;
 use log::info;
 use paths::{global_ssh_config_file, user_ssh_config_file};
 use picker::Picker;
@@ -350,11 +349,17 @@ impl ProjectPicker {
                                         active_editor.update(cx, |editor, cx| {
                                             let row = row.saturating_sub(1);
                                             let col = path.column.unwrap_or(0).saturating_sub(1);
-                                            editor.go_to_singleton_buffer_point(
-                                                Point::new(row, col),
-                                                window,
-                                                cx,
-                                            );
+                                            let Some(buffer) = editor.buffer().read(cx).as_singleton()
+                                            else {
+                                                return;
+                                            };
+                                            let buffer_snapshot = buffer.read(cx).snapshot();
+                                            let point = buffer_snapshot
+                                                .text
+                                                .point_for_row_and_column_from_external_source(
+                                                    row, col,
+                                                );
+                                            editor.go_to_singleton_buffer_point(point, window, cx);
                                         });
                                     })
                                     .ok();
