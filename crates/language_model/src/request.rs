5bb331f01b5e58a73c10ca25096fd9dbcd3b043a
Mason Palmer <97921708+mason-palmer@users.noreply.github.com>
2026-01-19 14:13:14 -0700

    agent: Patch image format bug (#45978)
    Closes #44694
    
    Release Notes:
    
    - Fixed images being converted to png but retaining old format
    
    ## Before:
    <img width="574" height="327" alt="Screenshot 2026-01-02 at 5 34 24 PM"
    src="https://github.com/user-attachments/assets/92331939-cebc-4f53-99fc-10d5181cf87e"
    />
    
    ## After:
    <img width="638" height="489" alt="Screenshot 2026-01-02 at 5 34 36 PM"
    src="https://github.com/user-attachments/assets/47c05906-fa56-4a53-abd4-790c42230772"
    />
    
    ---------
    
    Co-authored-by: versecafe <147033096+versecafe@users.noreply.github.com>
    Co-authored-by: MrSubidubi <finn@zed.dev>

--- crates/language_model/src/request.rs
+++ crates/language_model/src/request.rs
@@ -92,6 +92,9 @@ const DEFAULT_IMAGE_MAX_BYTES: usize = 5 * 1024 * 1024;
 const MAX_IMAGE_DOWNSCALE_PASSES: usize = 8;
 
 impl LanguageModelImage {
+    // All language model images are encoded as PNGs.
+    pub const FORMAT: ImageFormat = ImageFormat::Png;
+
     pub fn empty() -> Self {
         Self {
             source: "".into(),
