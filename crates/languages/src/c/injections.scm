0dc7a8b3f2e0b483191131249a514143ca9e938b
ozacod <47009516+ozacod@users.noreply.github.com>
2026-01-24 22:46:15 +0300

    jsdoc injection for c/c++

--- crates/languages/src/c/injections.scm
+++ crates/languages/src/c/injections.scm
@@ -2,6 +2,10 @@
  (#set! injection.language "comment")
 )
 
+(((comment) @_jsdoc_comment
+  (#match? @_jsdoc_comment "(?s)^/[*][*][^*].*[*]/$")) @injection.content
+  (#set! injection.language "jsdoc"))
+
 (preproc_def
     value: (preproc_arg) @injection.content
     (#set! injection.language "c"))
