454fac8ff44a5295dd386cd0cfad8b6e93f2e91b
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-01-26 22:52:27 +0100

    WIP

---
+++ crates/workspace/src/pane_host.rs
@@ -0,0 +1,24 @@
+use gpui::WeakEntity;
+use ui::{ParentElement, Render, Styled, div};
+
+use crate::{ActivePaneDecorator, PaneGroup, Workspace};
+
+pub struct PaneHost {
+    pub(crate) center: PaneGroup,
+    pub(crate) workspace: WeakEntity<Workspace>,
+}
+
+impl Render for PaneHost {
+    fn render(
+        &mut self,
+        window: &mut ui::Window,
+        cx: &mut ui::Context<Self>,
+    ) -> impl ui::IntoElement {
+        let pane = self.center.first_pane();
+        let decorator = ActivePaneDecorator::new(&pane, &self.workspace);
+
+        div()
+            .size_full()
+            .child(self.center.render(None, &decorator, window, cx))
+    }
+}
