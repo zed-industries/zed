e2f54a12727def0be492134291cf11c7006bfd2b
ozacod <47009516+ozacod@users.noreply.github.com>
2026-01-28 13:25:49 +0300

    languages: Add highlighting for document comments in C/C++ (#47556)
    Before:
    <img width="557" height="668" alt="before"
    src="https://github.com/user-attachments/assets/5accf661-94cd-4933-8771-98f813199c39"
    />
    
    After:
    <img width="581" height="668" alt="after"
    src="https://github.com/user-attachments/assets/251101ca-6e12-4dc3-b4dd-41b2438f9bd7"
    />
    
    Release Notes:
    
    - Added highlighting for document comments in C/C++

--- crates/languages/src/cpp/injections.scm
+++ crates/languages/src/cpp/injections.scm
@@ -2,6 +2,11 @@
  (#set! injection.language "comment")
 )
 
+((comment) @injection.content
+  (#match? @injection.content "^(///|//!|/\\*\\*|/\\*!)(.*)")
+  (#set! injection.language "doxygen")
+  (#set! injection.include-children))
+
 (preproc_def
     value: (preproc_arg) @injection.content
     (#set! injection.language "c++"))
