a6ea315a89965071f5fdb88c22e109abd501c806
Conrad Irwin <conrad.irwin@gmail.com>
2026-01-30 14:24:51 -0700

    Try new runners

--- tooling/xtask/src/tasks/workflows/runners.rs
+++ tooling/xtask/src/tasks/workflows/runners.rs
@@ -12,7 +12,7 @@ pub const LINUX_ARM_BUNDLER: Runner = Runner("namespace-profile-8x32-ubuntu-2004
 pub const LINUX_LARGE_RAM: Runner = Runner("namespace-profile-8x32-ubuntu-2404");
 
 pub const MAC_DEFAULT: Runner = Runner("namespace-profile-mac-large");
-pub const WINDOWS_DEFAULT: Runner = Runner("self-32vcpu-windows-2022");
+pub const WINDOWS_DEFAULT: Runner = Runner("windows-build-test");
 
 pub struct Runner(&'static str);
 
