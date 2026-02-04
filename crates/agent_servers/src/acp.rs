5a6b1d691d72235517e7217bc564eff5523806f3
Ben Brandt <benjamin.j.brandt@gmail.com>
2026-02-04 13:21:23 +0100

    acp: Remove feature flag for session config options

--- crates/agent_servers/src/acp.rs
+++ crates/agent_servers/src/acp.rs
@@ -972,9 +972,7 @@ fn config_state(
     Option<Rc<RefCell<acp::SessionModelState>>>,
     Option<Rc<RefCell<Vec<acp::SessionConfigOption>>>>,
 ) {
-    if cx.has_flag::<AcpBetaFeatureFlag>()
-        && let Some(opts) = config_options
-    {
+    if let Some(opts) = config_options {
         return (None, None, Some(Rc::new(RefCell::new(opts))));
     }
 
