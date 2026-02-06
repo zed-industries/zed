197986ea8b34649f92b51a83a5d38591de0558dd
John Tur <john-tur@outlook.com>
2026-02-05 22:25:50 -0500

    Fix features

--- crates/gpui/src/platform/linux.rs
+++ crates/gpui/src/platform/linux.rs
@@ -2,8 +2,8 @@ mod dispatcher;
 mod headless;
 mod keyboard;
 mod platform;
+#[cfg(any(feature = "wayland", feature = "x11"))]
 mod text_system;
-
 #[cfg(feature = "wayland")]
 mod wayland;
 #[cfg(feature = "x11")]
@@ -15,15 +15,16 @@ pub(crate) use dispatcher::*;
 pub(crate) use headless::*;
 pub(crate) use keyboard::*;
 pub(crate) use platform::*;
+#[cfg(any(feature = "wayland", feature = "x11"))]
 pub(crate) use text_system::*;
 #[cfg(feature = "wayland")]
 pub(crate) use wayland::*;
 #[cfg(feature = "x11")]
 pub(crate) use x11::*;
 
-#[cfg(feature = "screen-capture")]
+#[cfg(all(feature = "screen-capture", any(feature = "wayland", feature = "x11")))]
 pub(crate) type PlatformScreenCaptureFrame = scap::frame::Frame;
-#[cfg(not(feature = "screen-capture"))]
+#[cfg(not(all(feature = "screen-capture", any(feature = "wayland", feature = "x11"))))]
 pub(crate) type PlatformScreenCaptureFrame = ();
 
 #[cfg(feature = "wayland")]
