6f3f79f81f6a05589ccd4ec00fda74b0478062a4
Danilo Leal <daniloleal09@gmail.com>
2026-02-11 20:29:02 -0300

    Fix copywriting in the right-click project panel menu

--- crates/project_panel/src/project_panel.rs
+++ crates/project_panel/src/project_panel.rs
@@ -1208,10 +1208,10 @@ impl ProjectPanel {
                             .when(!is_collab && is_root, |menu| {
                                 menu.separator()
                                     .action(
-                                        "Add Folder to Project…",
+                                        "Add Project to Workspace…",
                                         Box::new(workspace::AddFolderToProject),
                                     )
-                                    .action("Remove from Project", Box::new(RemoveFromProject))
+                                    .action("Remove from Workspace", Box::new(RemoveFromProject))
                             })
                             .when(is_dir && !is_root, |menu| {
                                 menu.separator().action(
