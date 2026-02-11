706c9c165334758a2a33ab5e354de94ba17afc36
Conrad Irwin <conrad.irwin@gmail.com>
2026-02-11 16:47:55 -0700

    Install sccache *after* we rm-rf target...

--- tooling/xtask/src/tasks/workflows/run_tests.rs
+++ tooling/xtask/src/tasks/workflows/run_tests.rs
@@ -423,13 +423,13 @@ fn run_platform_tests_impl(platform: Platform, filter_packages: bool) -> NamedJo
                 platform == Platform::Linux,
                 steps::install_linux_dependencies,
             )
-            .add_step(steps::setup_sccache(platform))
             .add_step(steps::setup_node())
             .when(
                 platform == Platform::Linux || platform == Platform::Mac,
                 |job| job.add_step(steps::cargo_install_nextest()),
             )
             .add_step(steps::clear_target_dir_if_large(platform))
+            .add_step(steps::setup_sccache(platform))
             .add_step(steps::install_cargo_mtime(platform))
             .add_step(steps::run_cargo_mtime(platform))
             .when(filter_packages, |job| {
