1ede892b6648d7b89e01deb3934df9f4d7e282d4
Tommy Han <tommyhonym@gmail.com>
2026-02-12 21:13:38 +0800

    fix theme mode actions not loading properly

--- crates/workspace/src/workspace.rs
+++ crates/workspace/src/workspace.rs
@@ -143,10 +143,7 @@ pub use workspace_settings::{
 use zed_actions::{
     Spawn,
     feedback::FileBugReport,
-    theme_mode::{
-        SetDarkGlobal, SetDarkWorkspace, SetLightGlobal, SetLightWorkspace, SetSystemGlobal,
-        SetSystemWorkspace,
-    },
+    theme_mode::{SetDark, SetLight, SetSystem},
 };
 
 use crate::{item::ItemBufferKind, notifications::NotificationId};
@@ -7017,7 +7014,7 @@ impl Workspace {
 
     fn set_theme_mode_light_workspace(
         &mut self,
-        _: &SetLightWorkspace,
+        _: &SetLight,
         _window: &mut Window,
         cx: &mut Context<Self>,
     ) {
@@ -7030,7 +7027,7 @@ impl Workspace {
 
     fn set_theme_mode_dark_workspace(
         &mut self,
-        _: &SetDarkWorkspace,
+        _: &SetDark,
         _window: &mut Window,
         cx: &mut Context<Self>,
     ) {
@@ -7043,7 +7040,7 @@ impl Workspace {
 
     fn set_theme_mode_system_workspace(
         &mut self,
-        _: &SetSystemWorkspace,
+        _: &SetSystem,
         _window: &mut Window,
         cx: &mut Context<Self>,
     ) {
@@ -7056,7 +7053,7 @@ impl Workspace {
 
     fn set_theme_mode_light_global(
         &mut self,
-        _: &SetLightGlobal,
+        _: &SetLight,
         _window: &mut Window,
         cx: &mut Context<Self>,
     ) {
@@ -7069,7 +7066,7 @@ impl Workspace {
 
     fn set_theme_mode_dark_global(
         &mut self,
-        _: &SetDarkGlobal,
+        _: &SetDark,
         _window: &mut Window,
         cx: &mut Context<Self>,
     ) {
@@ -7078,7 +7075,7 @@ impl Workspace {
 
     fn set_theme_mode_system_global(
         &mut self,
-        _: &SetSystemGlobal,
+        _: &SetSystem,
         _window: &mut Window,
         cx: &mut Context<Self>,
     ) {
