1f831e028d4de9e872a78b0af8b9d24e33f7d3c7
Redas Jefisovas <114405740+holoflash@users.noreply.github.com>
2026-02-13 15:54:56 +0100

    toggle search_on_input true by default

--- crates/search/src/project_search.rs
+++ crates/search/src/project_search.rs
@@ -2562,7 +2562,8 @@ pub mod tests {
     use project::FakeFs;
     use serde_json::json;
     use settings::{
-        InlayHintSettingsContent, SettingsStore, ThemeColorsContent, ThemeStyleContent,
+        InlayHintSettingsContent, SearchSettingsContent, SettingsStore, ThemeColorsContent,
+        ThemeStyleContent,
     };
     use util::{path, paths::PathStyle, rel_path::rel_path};
     use util_macros::perf;
@@ -4780,6 +4781,15 @@ pub mod tests {
         cx.update(|cx| {
             let settings = SettingsStore::test(cx);
             cx.set_global(settings);
+
+            SettingsStore::update_global(cx, |store, cx| {
+                store.update_user_settings(cx, |settings| {
+                    settings.editor.search = Some(SearchSettingsContent {
+                        search_on_input: Some(false),
+                        ..Default::default()
+                    });
+                });
+            });
 
             theme::init(theme::LoadThemes::JustBase, cx);
 
