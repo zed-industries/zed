0b2c74d8ba08611f72caba81b4402b23e6558ea8
Lukas Wirth <lukas@zed.dev>
2026-02-27 10:18:03 +0100

    gpui: Reduce amount of wasm cfgs in gpui (#50278)
    Release Notes:
    
    - N/A *or* Added/Fixed/Improved ...

--- crates/gpui/src/executor.rs
+++ crates/gpui/src/executor.rs
@@ -564,15 +564,9 @@ mod test {
 
         let platform = TestPlatform::new(background_executor.clone(), foreground_executor);
         let asset_source = Arc::new(());
-        #[cfg(not(target_family = "wasm"))]
         let http_client = http_client::FakeHttpClient::with_404_response();
 
-        let app = App::new_app(
-            platform,
-            asset_source,
-            #[cfg(not(target_family = "wasm"))]
-            http_client,
-        );
+        let app = App::new_app(platform, asset_source, http_client);
         (dispatcher, background_executor, app)
     }
 
