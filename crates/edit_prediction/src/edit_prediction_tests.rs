7bcbd8ac511a4fda798efc18f337ddeeb098f7ac
Rocky Shi <bangtty@gmail.com>
2026-03-07 00:33:46 +0800

    Fix untitled buffer edit prediction fallback

--- crates/edit_prediction/src/edit_prediction_tests.rs
+++ crates/edit_prediction/src/edit_prediction_tests.rs
@@ -2794,6 +2794,28 @@ async fn test_edit_prediction_settled(cx: &mut TestAppContext) {
     }
 }
 
+#[gpui::test]
+fn test_buffer_path_with_id_fallback_for_untitled_buffers(cx: &mut TestAppContext) {
+    let buffer_1 = cx.new(|cx| Buffer::local("one", cx));
+    let buffer_2 = cx.new(|cx| Buffer::local("two", cx));
+
+    let snapshot_1 = buffer_1.read_with(cx, |buffer, _| buffer.text_snapshot());
+    let snapshot_2 = buffer_2.read_with(cx, |buffer, _| buffer.text_snapshot());
+
+    let path_1 = cx.read(|cx| buffer_path_with_id_fallback(None, &snapshot_1, cx));
+    let path_2 = cx.read(|cx| buffer_path_with_id_fallback(None, &snapshot_2, cx));
+
+    assert_eq!(
+        path_1.as_ref(),
+        Path::new(&format!("untitled-{}", snapshot_1.remote_id()))
+    );
+    assert_eq!(
+        path_2.as_ref(),
+        Path::new(&format!("untitled-{}", snapshot_2.remote_id()))
+    );
+    assert_ne!(path_1.as_ref(), path_2.as_ref());
+}
+
 #[ctor::ctor]
 fn init_logger() {
     zlog::init_test();
