236abe673021dcd4c69e37466b763edc0b112a97
Kirill Bulatov <kirill@zed.dev>
2026-03-11 11:49:40 +0200

    Disable more redundant data in rhs split
    Co-authored-by: Jakub Konka <kubkon@jakubkonka.com>

--- crates/editor/src/split.rs
+++ crates/editor/src/split.rs
@@ -447,6 +447,8 @@ impl SplittableEditor {
                 Editor::for_multibuffer(rhs_multibuffer.clone(), Some(project.clone()), window, cx);
             editor.set_expand_all_diff_hunks(cx);
             editor.disable_runnables();
+            editor.disable_diagnostics(cx);
+            editor.set_minimap_visibility(crate::MinimapVisibility::Disabled, window, cx);
             editor
         });
         // TODO(split-diff) we might want to tag editor events with whether they came from rhs/lhs
