826e964e68605b627ba38029daae26ebed49bebb
Vinh Tran <daivinhtran.vt@gmail.com>
2026-03-14 19:37:58 +0700

    Add deleted color

--- crates/editor/src/items.rs
+++ crates/editor/src/items.rs
@@ -1864,6 +1864,8 @@ pub fn entry_git_aware_label_color(git_status: GitSummary, ignored: bool, select
         Color::Ignored
     } else if git_status.conflict > 0 {
         Color::Conflict
+    } else if tracked.deleted > 0 {
+        Color::Deleted
     } else if tracked.modified > 0 {
         Color::Modified
     } else if tracked.added > 0 || git_status.untracked > 0 {
