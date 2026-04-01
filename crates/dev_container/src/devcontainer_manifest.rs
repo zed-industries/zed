55c577bcf91c8f1909f8168282268255f8bffacc
KyleBarton <kjb@initialcapacity.io>
2026-03-31 20:24:38 -0700

    Address windows inconsistencies with unit tests

--- crates/dev_container/src/devcontainer_manifest.rs
+++ crates/dev_container/src/devcontainer_manifest.rs
@@ -2616,6 +2616,9 @@ mod test {
         let docker_run_command = docker_run_command.expect("ok");
 
         assert_eq!(docker_run_command.get_program(), "docker");
+        let expected_config_file_label =
+            PathBuf::from(TEST_PROJECT_PATH).join(".devcontainer/devcontainer.json");
+        let expected_config_file_label = expected_config_file_label.display();
         assert_eq!(
             docker_run_command.get_args().collect::<Vec<&OsStr>>(),
             vec![
@@ -2629,9 +2632,9 @@ mod test {
                 OsStr::new("-l"),
                 OsStr::new("devcontainer.local_folder=/path/to/local/project"),
                 OsStr::new("-l"),
-                OsStr::new(
-                    "devcontainer.config_file=/path/to/local/project/.devcontainer/devcontainer.json"
-                ),
+                OsStr::new(&format!(
+                    "devcontainer.config_file={expected_config_file_label}"
+                )),
                 OsStr::new("--entrypoint"),
                 OsStr::new("/bin/sh"),
                 OsStr::new("mcr.microsoft.com/devcontainers/base:ubuntu"),
@@ -2910,6 +2913,9 @@ mod test {
         );
     }
 
+    // updateRemoteUserUID is treated as false in Windows, so this test will fail
+    // It is covered by test_spawns_devcontainer_with_dockerfile_and_no_update_uid
+    #[cfg(not(target_os = "windows"))]
     #[gpui::test]
     async fn test_spawns_devcontainer_with_dockerfile_and_features(cx: &mut TestAppContext) {
         cx.executor().allow_parking();
@@ -3271,6 +3277,9 @@ chmod +x ./install.sh
         }))
     }
 
+    // updateRemoteUserUID is treated as false in Windows, so this test will fail
+    // It is covered by test_spawns_devcontainer_with_docker_compose_and_no_update_uid
+    #[cfg(not(target_os = "windows"))]
     #[gpui::test]
     async fn test_spawns_devcontainer_with_docker_compose(cx: &mut TestAppContext) {
         cx.executor().allow_parking();
@@ -3558,6 +3567,248 @@ ENV DOCKER_BUILDKIT=1
         )
     }
 
+    #[gpui::test]
+    async fn test_spawns_devcontainer_with_docker_compose_and_no_update_uid(
+        cx: &mut TestAppContext,
+    ) {
+        cx.executor().allow_parking();
+        env_logger::try_init().ok();
+        let given_devcontainer_contents = r#"
+        // For format details, see https://aka.ms/devcontainer.json. For config options, see the
+        // README at: https://github.com/devcontainers/templates/tree/main/src/rust-postgres
+        {
+          "features": {
+            "ghcr.io/devcontainers/features/aws-cli:1": {},
+            "ghcr.io/devcontainers/features/docker-in-docker:2": {},
+          },
+          "name": "Rust and PostgreSQL",
+          "dockerComposeFile": "docker-compose.yml",
+          "service": "app",
+          "workspaceFolder": "/workspaces/${localWorkspaceFolderBasename}",
+
+          // Features to add to the dev container. More info: https://containers.dev/features.
+          // "features": {},
+
+          // Use 'forwardPorts' to make a list of ports inside the container available locally.
+          "forwardPorts": [
+            8083,
+            "db:5432",
+            "db:1234",
+          ],
+          "updateRemoteUserUID": false,
+          "appPort": "8084",
+
+          // Use 'postCreateCommand' to run commands after the container is created.
+          // "postCreateCommand": "rustc --version",
+
+          // Configure tool-specific properties.
+          // "customizations": {},
+
+          // Uncomment to connect as root instead. More info: https://aka.ms/dev-containers-non-root.
+          // "remoteUser": "root"
+        }
+        "#;
+        let (test_dependencies, mut devcontainer_manifest) =
+            init_default_devcontainer_manifest(cx, given_devcontainer_contents)
+                .await
+                .unwrap();
+
+        test_dependencies
+        .fs
+        .atomic_write(
+            PathBuf::from(TEST_PROJECT_PATH).join(".devcontainer/docker-compose.yml"),
+            r#"
+version: '3.8'
+
+volumes:
+postgres-data:
+
+services:
+app:
+    build:
+        context: .
+        dockerfile: Dockerfile
+    env_file:
+        # Ensure that the variables in .env match the same variables in devcontainer.json
+        - .env
+
+    volumes:
+        - ../..:/workspaces:cached
+
+    # Overrides default command so things don't shut down after the process ends.
+    command: sleep infinity
+
+    # Runs app on the same network as the database container, allows "forwardPorts" in devcontainer.json function.
+    network_mode: service:db
+
+    # Use "forwardPorts" in **devcontainer.json** to forward an app port locally.
+    # (Adding the "ports" property to this file will not forward from a Codespace.)
+
+db:
+    image: postgres:14.1
+    restart: unless-stopped
+    volumes:
+        - postgres-data:/var/lib/postgresql/data
+    env_file:
+        # Ensure that the variables in .env match the same variables in devcontainer.json
+        - .env
+
+    # Add "forwardPorts": ["5432"] to **devcontainer.json** to forward PostgreSQL locally.
+    # (Adding the "ports" property to this file will not forward from a Codespace.)
+                "#.trim().to_string(),
+        )
+        .await
+        .unwrap();
+
+        test_dependencies.fs.atomic_write(
+        PathBuf::from(TEST_PROJECT_PATH).join(".devcontainer/Dockerfile"),
+        r#"
+FROM mcr.microsoft.com/devcontainers/rust:2-1-bookworm
+
+# Include lld linker to improve build times either by using environment variable
+# RUSTFLAGS="-C link-arg=-fuse-ld=lld" or with Cargo's configuration file (i.e see .cargo/config.toml).
+RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
+&& apt-get -y install clang lld \
+&& apt-get autoremove -y && apt-get clean -y
+        "#.trim().to_string()).await.unwrap();
+
+        devcontainer_manifest.parse_nonremote_vars().unwrap();
+
+        let _devcontainer_up = devcontainer_manifest.build_and_run().await.unwrap();
+
+        let files = test_dependencies.fs.files();
+        let feature_dockerfile = files
+            .iter()
+            .find(|f| {
+                f.file_name()
+                    .is_some_and(|s| s.display().to_string() == "Dockerfile.extended")
+            })
+            .expect("to be found");
+        let feature_dockerfile = test_dependencies.fs.load(feature_dockerfile).await.unwrap();
+        assert_eq!(
+            &feature_dockerfile,
+            r#"ARG _DEV_CONTAINERS_BASE_IMAGE=placeholder
+
+FROM mcr.microsoft.com/devcontainers/rust:2-1-bookworm AS dev_container_auto_added_stage_label
+
+# Include lld linker to improve build times either by using environment variable
+# RUSTFLAGS="-C link-arg=-fuse-ld=lld" or with Cargo's configuration file (i.e see .cargo/config.toml).
+RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
+&& apt-get -y install clang lld \
+&& apt-get autoremove -y && apt-get clean -y
+
+FROM $_DEV_CONTAINERS_BASE_IMAGE AS dev_containers_feature_content_normalize
+USER root
+COPY --from=dev_containers_feature_content_source ./devcontainer-features.builtin.env /tmp/build-features/
+RUN chmod -R 0755 /tmp/build-features/
+
+FROM $_DEV_CONTAINERS_BASE_IMAGE AS dev_containers_target_stage
+
+USER root
+
+RUN mkdir -p /tmp/dev-container-features
+COPY --from=dev_containers_feature_content_normalize /tmp/build-features/ /tmp/dev-container-features
+
+RUN \
+echo "_CONTAINER_USER_HOME=$( (command -v getent >/dev/null 2>&1 && getent passwd 'root' || grep -E '^root|^[^:]*:[^:]*:root:' /etc/passwd || true) | cut -d: -f6)" >> /tmp/dev-container-features/devcontainer-features.builtin.env && \
+echo "_REMOTE_USER_HOME=$( (command -v getent >/dev/null 2>&1 && getent passwd 'vscode' || grep -E '^vscode|^[^:]*:[^:]*:vscode:' /etc/passwd || true) | cut -d: -f6)" >> /tmp/dev-container-features/devcontainer-features.builtin.env
+
+
+RUN --mount=type=bind,from=dev_containers_feature_content_source,source=./aws-cli_0,target=/tmp/build-features-src/aws-cli_0 \
+cp -ar /tmp/build-features-src/aws-cli_0 /tmp/dev-container-features \
+&& chmod -R 0755 /tmp/dev-container-features/aws-cli_0 \
+&& cd /tmp/dev-container-features/aws-cli_0 \
+&& chmod +x ./devcontainer-features-install.sh \
+&& ./devcontainer-features-install.sh \
+&& rm -rf /tmp/dev-container-features/aws-cli_0
+
+RUN --mount=type=bind,from=dev_containers_feature_content_source,source=./docker-in-docker_1,target=/tmp/build-features-src/docker-in-docker_1 \
+cp -ar /tmp/build-features-src/docker-in-docker_1 /tmp/dev-container-features \
+&& chmod -R 0755 /tmp/dev-container-features/docker-in-docker_1 \
+&& cd /tmp/dev-container-features/docker-in-docker_1 \
+&& chmod +x ./devcontainer-features-install.sh \
+&& ./devcontainer-features-install.sh \
+&& rm -rf /tmp/dev-container-features/docker-in-docker_1
+
+
+ARG _DEV_CONTAINERS_IMAGE_USER=root
+USER $_DEV_CONTAINERS_IMAGE_USER
+
+# Ensure that /etc/profile does not clobber the existing path
+RUN sed -i -E 's/((^|\s)PATH=)([^\$]*)$/\1\${PATH:-\3}/g' /etc/profile || true
+
+
+ENV DOCKER_BUILDKIT=1
+"#
+        );
+
+        let runtime_override = files
+            .iter()
+            .find(|f| {
+                f.file_name()
+                    .is_some_and(|s| s.display().to_string() == "docker_compose_runtime.json")
+            })
+            .expect("to be found");
+        let runtime_override = test_dependencies.fs.load(runtime_override).await.unwrap();
+
+        let expected_runtime_override = DockerComposeConfig {
+        name: None,
+        services: HashMap::from([
+            (
+                "app".to_string(),
+                DockerComposeService {
+                    entrypoint: Some(vec![
+                        "/bin/sh".to_string(),
+                        "-c".to_string(),
+                        "echo Container started\ntrap \"exit 0\" 15\n/usr/local/share/docker-init.sh\nexec \"$@\"\nwhile sleep 1 & wait $!; do :; done".to_string(),
+                        "-".to_string(),
+                    ]),
+                    cap_add: Some(vec!["SYS_PTRACE".to_string()]),
+                    security_opt: Some(vec!["seccomp=unconfined".to_string()]),
+                    privileged: Some(true),
+                    labels: Some(vec![
+                        "devcontainer.metadata=[{\"remoteUser\":\"vscode\"}]".to_string(),
+                        "devcontainer.local_folder=/path/to/local/project".to_string(),
+                        "devcontainer.config_file=/path/to/local/project/.devcontainer/devcontainer.json".to_string()
+                    ]),
+                    volumes: vec![
+                        MountDefinition {
+                            source: "dind-var-lib-docker-42dad4b4ca7b8ced".to_string(),
+                            target: "/var/lib/docker".to_string(),
+                            mount_type: Some("volume".to_string())
+                        }
+                    ],
+                    ..Default::default()
+                },
+            ),
+            (
+                "db".to_string(),
+                DockerComposeService {
+                    ports: vec![
+                        "8083:8083".to_string(),
+                        "5432:5432".to_string(),
+                        "1234:1234".to_string(),
+                        "8084:8084".to_string()
+                    ],
+                    ..Default::default()
+                },
+            ),
+        ]),
+        volumes: HashMap::from([(
+            "dind-var-lib-docker-42dad4b4ca7b8ced".to_string(),
+            DockerComposeVolume {
+                name: "dind-var-lib-docker-42dad4b4ca7b8ced".to_string(),
+            },
+        )]),
+    };
+
+        assert_eq!(
+            serde_json_lenient::from_str::<DockerComposeConfig>(&runtime_override).unwrap(),
+            expected_runtime_override
+        )
+    }
+
+    #[cfg(not(target_os = "windows"))]
     #[gpui::test]
     async fn test_spawns_devcontainer_with_docker_compose_and_podman(cx: &mut TestAppContext) {
         cx.executor().allow_parking();
