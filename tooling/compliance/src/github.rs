18f08b42dcff27e495f75b83aa074a2b22214c3b
MrSubidubi <finn@zed.dev>
2026-04-06 14:22:24 +0200

    Another one

--- tooling/compliance/src/github.rs
+++ tooling/compliance/src/github.rs
@@ -1,4 +1,4 @@
-#[allow(clippy::disallowed_methods, reason = "This is only used in xtasks")]
+#![allow(clippy::disallowed_methods, reason = "This is only used in xtasks")]
 use std::{collections::HashMap, fmt, ops::Not};
 
 use anyhow::{Context, Result};
