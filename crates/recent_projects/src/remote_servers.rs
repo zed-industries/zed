277ffcdab2c88b2ca2c98842d1de5a50c00f6cbf
Max Brunsfeld <maxbrunsfeld@gmail.com>
2026-04-10 14:29:03 -0700

    Open new remote projects in the same window via sidebar (#53654)
    Previously, when opening a remote project via the sidebar, and picking a
    new project path via the path picker, it would incorrectly open in a new
    window.
    
    Release Notes:
    
    - N/A

--- crates/recent_projects/src/remote_servers.rs
+++ crates/recent_projects/src/remote_servers.rs
@@ -488,18 +488,21 @@ impl ProjectPicker {
                     })
                     .log_err();
 
-                    let options = cx
-                        .update(|_, cx| (app_state.build_window_options)(None, cx))
-                        .log_err()?;
-                    let window = cx
-                        .open_window(options, |window, cx| {
+                    let window = if create_new_window {
+                        let options = cx
+                            .update(|_, cx| (app_state.build_window_options)(None, cx))
+                            .log_err()?;
+                        cx.open_window(options, |window, cx| {
                             let workspace = cx.new(|cx| {
                                 telemetry::event!("SSH Project Created");
                                 Workspace::new(None, project.clone(), app_state.clone(), window, cx)
                             });
                             cx.new(|cx| MultiWorkspace::new(workspace, window, cx))
                         })
-                        .log_err()?;
+                        .log_err()
+                    } else {
+                        cx.window_handle().downcast::<MultiWorkspace>()
+                    }?;
 
                     let items = open_remote_project_with_existing_connection(
                         connection, project, paths, app_state, window, cx,
