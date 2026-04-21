39e55d26c56372de0379b5ee1377c8f8f2f74fee
Max Brunsfeld <maxbrunsfeld@gmail.com>
2026-04-21 10:34:37 -0700

    Tweak wording around multi-folder project actions (#54438)
    * Project panel "Add Folders to Project" and "Remove folder from
    Project"
    * Recent projects "Add Folders to this Project"
    
    Release Notes:
    
    - N/A
    
    (cherry picked from commit f9cb919cba171833edfe5e948eeb139369f92959)

--- crates/project_panel/src/project_panel.rs
+++ crates/project_panel/src/project_panel.rs
@@ -1217,10 +1217,10 @@ impl ProjectPanel {
                             .when(!is_collab && is_root, |menu| {
                                 menu.separator()
                                     .action(
-                                        "Add Project to Workspace…",
+                                        "Add Folders to Project…",
                                         Box::new(workspace::AddFolderToProject),
                                     )
-                                    .action("Remove from Workspace", Box::new(RemoveFromProject))
+                                    .action("Remove from Project", Box::new(RemoveFromProject))
                             })
                             .when(is_dir && !is_root, |menu| {
                                 menu.separator().action(
