961a1fc81442affa751d1d4771f1ff0cb90246c8
Garth de Wet <garth@dewet.dev>
2026-05-02 21:56:48 +0200

    Add inlay type to chunks from inlay map upwards
    Allows for customisation at the editor/element level.
    
    Remove is_inlay from buffer since it's not required at that level.

--- crates/editor/src/element.rs
+++ crates/editor/src/element.rs
@@ -9000,7 +9000,7 @@ impl LineWithInvisibles {
             text: "\n",
             style: None,
             is_tab: false,
-            is_inlay: false,
+            inlay_type: None,
             replacement: None,
         }]) {
             if let Some(replacement) = highlighted_chunk.replacement {
@@ -9153,7 +9153,7 @@ impl LineWithInvisibles {
                             strikethrough: text_style.strikethrough,
                         });
 
-                        if editor_mode.is_full() && !highlighted_chunk.is_inlay {
+                        if editor_mode.is_full() && highlighted_chunk.inlay_type.is_none() {
                             // Line wrap pads its contents with fake whitespaces,
                             // avoid printing them
                             let is_soft_wrapped = is_row_soft_wrapped(row);
