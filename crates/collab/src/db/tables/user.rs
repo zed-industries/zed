c0e554b0cf8176af092bac2b8c8d98b300fa1d42
Marshall Bowers <git@maxdeviant.com>
2026-05-15 12:49:00 -0400

    Actually remove the field

--- crates/collab/src/db/tables/user.rs
+++ crates/collab/src/db/tables/user.rs
@@ -10,7 +10,6 @@ pub struct Model {
     pub id: UserId,
     pub github_login: String,
     pub github_user_id: i32,
-    // pub email_address: Option<String>,
     pub admin: bool,
     pub connected_once: bool,
 }
