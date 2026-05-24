cbeca5e623ee5866b80e151f54b8edf9904b0483
Kirill Bulatov <mail4score@gmail.com>
2026-05-24 11:29:10 +0300

    Fix the grammar

--- crates/settings_ui/src/page_data.rs
+++ crates/settings_ui/src/page_data.rs
@@ -4346,7 +4346,7 @@ fn window_and_layout_page() -> SettingsPage {
             }),
             SettingsPageItem::SettingItem(SettingItem {
                 title: "Auto Preview",
-                description: "Whether to automatically open a preview when opening files with preview capababilities.",
+                description: "Whether to automatically open a preview when opening files with preview capabilities.",
                 field: Box::new(SettingField {
                     json_path: Some("preview_tabs.auto_preview"),
                     pick: |settings_content| {
