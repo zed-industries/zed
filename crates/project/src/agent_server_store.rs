c81abe4a993329aa5781938b7a1f98bf524c9d24
Bennet Bo Fenner <bennetbo@gmx.de>
2026-06-02 11:29:51 +0200

    Update agent_server_store.rs

--- crates/project/src/agent_server_store.rs
+++ crates/project/src/agent_server_store.rs
@@ -1155,7 +1155,7 @@ impl ExternalAgentServer for LocalRegistryArchiveAgent {
             if !fs.is_dir(&version_dir).await {
                 let mut loading_status_tx = loading_status_tx;
                 if let Some(tx) = loading_status_tx.as_mut() {
-                    tx.send(Some(format!("Installing {} …", version.as_ref())))
+                    tx.send(Some(format!("Installing {}…", version.as_ref())))
                         .ok();
                 }
 
