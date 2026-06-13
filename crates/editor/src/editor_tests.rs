47373137e41de0e30296c0e933324635155ed450
guopenghui <guopeng_hui@126.com>
2026-06-13 16:50:05 +0800

    bookmark: support bookmark with label

--- crates/editor/src/editor_tests.rs
+++ crates/editor/src/editor_tests.rs
@@ -28867,7 +28867,7 @@ impl BookmarkTestContext {
                 .get(&abs_path)
                 .unwrap()
                 .iter()
-                .map(|b| b.0)
+                .map(|b| b.row)
                 .collect();
             rows.sort();
             assert_eq!(expected_rows, rows);
