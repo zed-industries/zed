f70338f4e8307279d1f8e34d56836fdc9f822adf
Jorge Pardo <jparpar@proton.me>
2026-06-14 15:32:44 +0200

    fix(gpui): rounded not working in img with ObjectFit::Cover

--- crates/gpui/src/window.rs
+++ crates/gpui/src/window.rs
@@ -3969,6 +3969,7 @@ impl Window {
                 pad: 0,
                 grayscale: false,
                 bounds,
+                clip_bounds: bounds,
                 corner_radii: Default::default(),
                 content_mask,
                 tile,
@@ -4050,6 +4051,7 @@ impl Window {
     pub fn paint_image(
         &mut self,
         bounds: Bounds<Pixels>,
+        clip_bounds: Bounds<Pixels>,
         corner_radii: Corners<Pixels>,
         data: Arc<RenderImage>,
         frame_index: usize,
@@ -4058,6 +4060,7 @@ impl Window {
         self.invalidator.debug_assert_paint();
 
         let bounds = self.snap_bounds(bounds);
+        let clip_bounds = self.snap_bounds(clip_bounds);
         let params = RenderImageParams {
             image_id: data.id,
             frame_index,
@@ -4084,6 +4087,7 @@ impl Window {
             pad: 0,
             grayscale,
             bounds,
+            clip_bounds,
             content_mask,
             corner_radii,
             tile,
