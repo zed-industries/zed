8ba35e5eacf30a847140e82eced017f93f3f6df0
Ben Brandt <benjamin.j.brandt@gmail.com>
2026-06-22 11:04:29 +0200

    acp: Update agent client protocol crate to 0.15.0 (#59593)
    We finally have a cancellation mechanism to use! Made the non
    side-effectful handlers stop their work if we get a cancel request
    notification.
    
    Release Notes:
    
    - N/A

--- crates/acp_thread/src/connection.rs
+++ crates/acp_thread/src/connection.rs
@@ -1,5 +1,5 @@
 use crate::AcpThread;
-use agent_client_protocol::schema as acp;
+use agent_client_protocol::schema::v1 as acp;
 use anyhow::Result;
 use chrono::{DateTime, Utc};
 use collections::{HashMap, HashSet, IndexMap};
