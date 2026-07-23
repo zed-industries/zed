ab47f5937ecc66954f59e7080d416ce5d051888c
Christopher Biscardi <chris@christopherbiscardi.com>
2026-07-22 20:27:52 -0700

    fmt

--- crates/fs/src/fs.rs
+++ crates/fs/src/fs.rs
@@ -1591,9 +1591,7 @@ impl FakeFsState {
                                     if !self.case_sensitive {
                                         entries
                                             .iter()
-                                            .find(|(key, _)| {
-                                                key.eq_ignore_ascii_case(name_str)
-                                            })
+                                            .find(|(key, _)| key.eq_ignore_ascii_case(name_str))
                                             .map(|(key, entry)| (key.as_str(), entry))?
                                     } else {
                                         return None;
