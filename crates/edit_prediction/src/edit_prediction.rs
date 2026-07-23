e745219944bc43aeffe6ab482a1668b6d3ea86ce
ddeityy <ddeityy@gmail.com>
2026-07-24 02:46:04 +0300

    Remove hardcoded constants, use default values from config, undo some fmt changes

--- crates/edit_prediction/src/edit_prediction.rs
+++ crates/edit_prediction/src/edit_prediction.rs
@@ -2315,11 +2315,7 @@ impl EditPredictionStore {
 
         let trigger = predict_edits_request_trigger_from_editor_trigger(trigger);
 
-        self.queue_prediction_refresh(
-            project.clone(),
-            buffer.entity_id(),
-            debounce_duration,
-            cx,
+        self.queue_prediction_refresh(project.clone(), buffer.entity_id(), debounce_duration, cx,
             move |this, cx| {
                 let Some(request_task) = this
                     .update(cx, |this, cx| {
