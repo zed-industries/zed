ff3c42898824c7b8bbe7799ac85df7806f7180ba
medzernik <medzernik@medzernik.dev>
2026-07-28 23:28:45 +0200

    fix clippy error - rename method

--- crates/git_ui/src/git_panel.rs
+++ crates/git_ui/src/git_panel.rs
@@ -8290,7 +8290,7 @@ impl GitPanel {
         cx.notify();
     }
 
-    pub fn signoff_enabled(&self) -> bool {
+    pub fn signoff_should_be_enabled(&self) -> bool {
         self.signoff_by_default
     }
 
