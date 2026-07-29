6800a190138400af9a76b802820a035d14c82169
MrSubidubi <finn@zed.dev>
2026-07-29 21:34:35 +0200

    Do the dirty work

--- crates/grammars/src/tsx/overrides.scm
+++ crates/grammars/src/tsx/overrides.scm
@@ -6,8 +6,12 @@
   (string_fragment) @string)
 
 (jsx_element) @element
-(return_statement (parenthesized_expression) @element)
-(arrow_function body: (parenthesized_expression) @element)
+
+(return_statement
+  (parenthesized_expression) @element)
+
+(arrow_function
+  body: (parenthesized_expression) @element)
 
 [
   (jsx_opening_element)
