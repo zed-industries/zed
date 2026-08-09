1271f8b0e8f3278eed5dd3fc12ad4bd30dce2c5d
Kirill Bulatov <kirill@zed.dev>
2026-08-10 01:29:52 +0300

    Bump rustc to 1.97 (#62395)
    Release Notes:
    
    - N/A

--- crates/debugger_ui/src/session/running/variable_list.rs
+++ crates/debugger_ui/src/session/running/variable_list.rs
@@ -1158,7 +1158,7 @@ impl VariableList {
                                 },
                             )
                             .child(
-                                Label::new(format!("=  {}", &value))
+                                Label::new(format!("=  {value}"))
                                     .single_line()
                                     .truncate()
                                     .size(LabelSize::Small)
