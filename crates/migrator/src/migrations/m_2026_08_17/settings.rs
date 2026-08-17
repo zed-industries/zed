6a745e4b4f95666963946e02e636b750cb572308
Abdul Rafey Ahmed <abdul.r@hyperverge.co>
2026-08-17 22:04:10 +0530

    feat: add migration logic

---
+++ crates/migrator/src/migrations/m_2026_08_17/settings.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use serde_json::Value;
+
+use crate::migrations::migrate_settings;
+
+pub fn make_git_gutter_width_an_enum(value: &mut Value) -> Result<()> {
+    migrate_settings(value, &mut migrate_one)
+}
+
+fn migrate_one(obj: &mut serde_json::Map<String, Value>) -> Result<()> {
+    let Some(gutter) = obj
+        .get_mut("gutter")
+        .and_then(|gutter| gutter.as_object_mut())
+    else {
+        return Ok(());
+    };
+
+    let Some(git_gutter_width) = gutter.get_mut("git_gutter_width") else {
+        return Ok(());
+    };
+
+    *git_gutter_width = match git_gutter_width {
+        Value::Null => Value::String("default".to_string()),
+        Value::Number(n) => {
+            let width = n
+                .as_f64()
+                .ok_or_else(|| anyhow::anyhow!("Expected git_gutter_width to be a number"))?;
+            Value::Object(
+                [("custom".to_string(), Value::from(width))]
+                    .into_iter()
+                    .collect(),
+            )
+        }
+        Value::String(s) if s == "default" => return Ok(()),
+        Value::Object(o) if o.contains_key("custom") => return Ok(()),
+        _ => anyhow::bail!("Expected git_gutter_width to be null, a number, or a valid enum value"),
+    };
+
+    Ok(())
+}
