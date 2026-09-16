bf51d910173d67b89c7fc216668148b55613e756
Bennet Bo Fenner <bennetbo@gmx.de>
2026-09-16 10:46:22 +0200

    cleanup

--- crates/language_models/src/provider/ollama.rs
+++ crates/language_models/src/provider/ollama.rs
@@ -390,19 +390,18 @@ impl OllamaLanguageModel {
                     {
                         match tool_result {
                             MessageContent::ToolResult(tool_result) => {
-                                let mut tool_images_iter = tool_result
+                                let images = tool_result
                                     .images()
                                     .map(|image| image.source.to_string())
-                                    .peekable();
-                                let tool_images = if tool_images_iter.peek().is_some() {
-                                    Some(tool_images_iter.collect())
-                                } else {
-                                    None
-                                };
+                                    .collect::<Vec<_>>();
                                 messages.push(ChatMessage::Tool {
                                     tool_name: tool_result.tool_name.to_string(),
                                     content: tool_result.text_contents(),
-                                    images: tool_images,
+                                    images: if images.is_empty() {
+                                        None
+                                    } else {
+                                        Some(images)
+                                    },
                                 })
                             }
                             _ => unreachable!("Only tool result should be extracted"),
