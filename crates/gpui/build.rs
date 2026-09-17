cd84ab800224f832105815c4f64dd144ceaceb1c
Christopher Biscardi <chris@christopherbiscardi.com>
2026-09-17 10:18:31 -0700

    fmt

--- crates/gpui/build.rs
+++ crates/gpui/build.rs
@@ -18,10 +18,7 @@ fn embed_resource() {
     let rc_file = resource_dir.join("gpui.rc");
     println!("cargo:rerun-if-changed={}", manifest.display());
     println!("cargo:rerun-if-changed={}", rc_file.display());
-    embed_resource::compile(
-        rc_file,
-        embed_resource::ParamsIncludeDirs([resource_dir]),
-    )
+    embed_resource::compile(rc_file, embed_resource::ParamsIncludeDirs([resource_dir]))
         .manifest_required()
         .unwrap();
 }
