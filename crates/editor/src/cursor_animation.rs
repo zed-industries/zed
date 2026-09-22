9113fb29165ec9c483206c9725041b79ae24e8bb
Christopher Biscardi <chris@christopherbiscardi.com>
2026-09-21 20:20:05 -0700

    expect instead of unwrap

--- crates/editor/src/cursor_animation.rs
+++ crates/editor/src/cursor_animation.rs
@@ -602,10 +602,26 @@ pub(crate) fn animated_corners_overlap_target(
     target_bounds: Bounds<Pixels>,
     corners: &[Point<Pixels>; 4],
 ) -> bool {
-    let min_x = corners.iter().map(|p| p.x).min().unwrap();
-    let max_x = corners.iter().map(|p| p.x).max().unwrap();
-    let min_y = corners.iter().map(|p| p.y).min().unwrap();
-    let max_y = corners.iter().map(|p| p.y).max().unwrap();
+    let min_x = corners
+        .iter()
+        .map(|p| p.x)
+        .min()
+        .expect("the corners argument is a 4 item array, which should alway have an x min");
+    let max_x = corners
+        .iter()
+        .map(|p| p.x)
+        .max()
+        .expect("the corners argument is a 4 item array, which should alway have an x max");
+    let min_y = corners
+        .iter()
+        .map(|p| p.y)
+        .min()
+        .expect("the corners argument is a 4 item array, which should alway have an y min");
+    let max_y = corners
+        .iter()
+        .map(|p| p.y)
+        .max()
+        .expect("the corners argument is a 4 item array, which should alway have an x max");
     let animated_bounds = Bounds::from_corners(point(min_x, min_y), point(max_x, max_y));
 
     animated_bounds.intersects(&target_bounds)
